bf36903e722b44080a0e6ce09fa7c11167516ab3
Bastian Köcher <git@kchr.de>
2019-04-12 13:36:48 +0200

    Some docs

--- cumulus/runtime/src/lib.rs
+++ cumulus/runtime/src/lib.rs
@@ -14,6 +14,8 @@
 // You should have received a copy of the GNU General Public License
 // along with Cumulus.  If not, see <http://www.gnu.org/licenses/>.
 
+///! The Cumulus runtime to make a runtime a parachain.
+
 #![cfg_attr(not(feature = "std"), no_std)]
 
 use rstd::vec::Vec;
