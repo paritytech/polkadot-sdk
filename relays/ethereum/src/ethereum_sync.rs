94b4a309ab43db7ff19ddbb8290ebd6bccca460d
Svyatoslav Nikolsky <svyatonik@gmail.com>
2020-03-23 15:22:22 +0300

    remove duplicate licenses (#47)

--- relays/ethereum/src/ethereum_sync.rs
+++ relays/ethereum/src/ethereum_sync.rs
@@ -14,22 +14,6 @@
 // You should have received a copy of the GNU General Public License
 // along with Parity Bridges Common.  If not, see <http://www.gnu.org/licenses/>.
 
-// Copyright 2019 Parity Technologies (UK) Ltd.
-// This file is part of Parity-Bridge.
-
-// Parity-Bridge is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-
-// Parity-Bridge is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-
-// You should have received a copy of the GNU General Public License
-// along with Parity-Bridge.  If not, see <http://www.gnu.org/licenses/>.
-
 use crate::ethereum_headers::QueuedHeaders;
 use crate::ethereum_sync_loop::EthereumSyncParams;
 use crate::ethereum_types::{HeaderId, HeaderStatus, QueuedHeader};
