230df76be7f7eef81858fb140322de738519611e
Svyatoslav Nikolsky <svyatonik@gmail.com>
2020-05-20 17:54:27 +0300

    Mute warnings from Substrate <-> Substrate runtime module and relay (#104)
    * mute warnings from sub2sub module+relay
    
    * cargo fmt --all

--- bridges/relays/substrate/src/main.rs
+++ bridges/relays/substrate/src/main.rs
@@ -14,6 +14,10 @@
 // You should have received a copy of the GNU General Public License
 // along with Parity Bridges Common.  If not, see <http://www.gnu.org/licenses/>.
 
+// TODO: when (if) this relay will be resurrected, remove this
+#![allow(unused_variables)]
+#![allow(dead_code)]
+
 mod bridge;
 mod error;
 mod params;
