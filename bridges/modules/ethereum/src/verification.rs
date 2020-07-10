792497e8dfc3faa87959cc8bd6c5cc692de1ad81
Hernando Castano <HCastano@users.noreply.github.com>
2020-07-10 05:10:53 -0400

    Benchmark Ethereum Pallet (#149)
    * Add skeleton for worst case import_unsigned_header
    
    * Fix a typo
    
    * Add benchmark test for best case unsigned header import
    
    * Add finality verification to worst case bench
    
    * Move `insert_header()` from mock to test_utils
    
    Allows the benchmarking code to use this without having to pull it in from the mock.
    
    * Add a rough bench to test a finalizing a "long" chain
    
    * Try to use complexity parameter for finality bench
    
    * Improve long finality bench
    
    * Remove stray dot file
    
    * Remove old "worst" case bench
    
    * Scribble some ideas down for pruning bench
    
    * Prune headers during benchmarking
    
    * Clean up some comments
    
    * Make finality bench work for entire range of complexity parameter
    
    * Place initialization code into a function
    
    * Add bench for block finalization with caching
    
    * First attempt at bench with receipts
    
    * Try and trigger validator set change
    
    * Perform a validator set change during benchmarking
    
    * Move `validators_change_receipt()` to shared location
    
    Allows unit tests and benchmarks to access the same helper function
    and const
    
    * Extract a test receipt root into a constant
    
    * Clean up description of pruning bench
    
    * Fix cache and pruning tests
    
    * Remove unecessary `build_custom_header` usage
    
    * Get rid of warnings
    
    * Remove code duplication comment
    
    I don't think its entirely worth it to split out so few lines of code.
    The benches aren't particularly hard to read anyways.
    
    * Increase the range of the complexity parameter
    
    * Use dynamic number of receipts while benchmarking
    
    As part of this change we have removed the hardcoded TEST_RECEIPT_ROOT
    and instead chose to calculate the receipt root on the fly. This will
    make tests and benches less fragile.
    
    * Prune a dynamic number of headers

--- bridges/modules/ethereum/src/verification.rs
+++ bridges/modules/ethereum/src/verification.rs
@@ -140,6 +140,7 @@ pub fn accept_aura_header_into_pool<S: Storage>(
 
 	// the heaviest, but rare operation - we do not want invalid receipts in the pool
 	if let Some(receipts) = receipts {
+		frame_support::debug::trace!(target: "runtime", "Got receipts! {:?}", receipts);
 		if !header.verify_receipts_root(receipts) {
 			return Err(Error::TransactionsReceiptsMismatch);
 		}
@@ -354,15 +355,15 @@ mod tests {
 	use super::*;
 	use crate::mock::{
 		insert_header, run_test_with_genesis, test_aura_config, validator, validator_address, validators_addresses,
-		AccountId, HeaderBuilder, TestRuntime, GAS_LIMIT,
+		validators_change_receipt, AccountId, HeaderBuilder, TestRuntime, GAS_LIMIT,
 	};
-	use crate::validators::{tests::validators_change_recept, ValidatorsSource};
+	use crate::validators::ValidatorsSource;
 	use crate::{
 		pool_configuration, BridgeStorage, FinalizedBlock, Headers, HeadersByNumber, NextValidatorsSetId,
 		ScheduledChanges, ValidatorsSet, ValidatorsSets,
 	};
 	use frame_support::{StorageMap, StorageValue};
-	use primitives::{rlp_encode, TransactionOutcome, H520};
+	use primitives::{compute_merkle_root, rlp_encode, TransactionOutcome, H520};
 	use secp256k1::SecretKey;
 
 	const GENESIS_STEP: u64 = 42;
@@ -844,7 +845,7 @@ mod tests {
 				let header = HeaderBuilder::with_parent_number(3)
 					.log_bloom((&[0xff; 256]).into())
 					.sign_by_set(validators);
-				(header, Some(vec![validators_change_recept(Default::default())]))
+				(header, Some(vec![validators_change_receipt(Default::default())]))
 			}),
 			Err(Error::TransactionsReceiptsMismatch),
 		);
@@ -853,18 +854,17 @@ mod tests {
 	#[test]
 	fn pool_accepts_headers_with_valid_receipts() {
 		let mut hash = None;
+		let receipts = vec![validators_change_receipt(Default::default())];
+		let receipts_root = compute_merkle_root(receipts.iter().map(|r| r.rlp()));
+
 		assert_eq!(
 			default_accept_into_pool(|validators| {
 				let header = HeaderBuilder::with_parent_number(3)
 					.log_bloom((&[0xff; 256]).into())
-					.receipts_root(
-						"81ce88dc524403b796222046bf3daf543978329b87ffd50228f1d3987031dc45"
-							.parse()
-							.unwrap(),
-					)
+					.receipts_root(receipts_root)
 					.sign_by_set(validators);
 				hash = Some(header.compute_hash());
-				(header, Some(vec![validators_change_recept(Default::default())]))
+				(header, Some(receipts.clone()))
 			}),
 			Ok((
 				// no tags are required
