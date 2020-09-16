776557bec7c48c267cc899a75e8a53a7b461ae20
Svyatoslav Nikolsky <svyatonik@gmail.com>
2020-09-16 21:52:42 +0300

    Verify GRANDPA justifications from runtime (#353)
    * verify justifications from runtime
    
    * unreachable

--- bridges/modules/substrate/src/lib.rs
+++ bridges/modules/substrate/src/lib.rs
@@ -14,4 +14,5 @@
 // You should have received a copy of the GNU General Public License
 // along with Parity Bridges Common.  If not, see <http://www.gnu.org/licenses/>.
 
+mod justification;
 mod storage_proof;
