fb8c29e5aeccdf0c7dfd87ea5110ff840c298eae
Hernando Castano <HCastano@users.noreply.github.com>
2020-09-30 04:58:28 -0400

    Integrate Grandpa Proof Checker into Substrate Pallet (#375)
    * Remove the Substrate primitives crate
    
    The types here were only used in one place, the pallet itself. If other
    components start using these types we can considering moving them back
    into a standalone crate.
    
    * Start trying to integrate justification module
    
    * Make Substrate blocks configurable in Pallet
    
    * WIP: Try and generalize justification test helpers
    
    * Fix tests which use "real" justifications
    
    * Put common test helpers alongside mock code
    
    * Use common helper for creating headers
    
    * Remove usage of UintAuthorityId
    
    This change favours the use of the Ed25519Keyring authorities
    in order to keep things consistent with the tests.
    
    * Add documentation around config trait types
    
    * Make test header, hash, and number types consistent
    
    * Update modules/substrate/src/verifier.rs
    
    Co-authored-by: Svyatoslav Nikolsky <svyatonik@gmail.com>
    
    * Update modules/substrate/src/lib.rs
    
    Co-authored-by: Tomasz Drwięga <tomusdrw@users.noreply.github.com>
    
    * Update modules/substrate/Cargo.toml
    
    Co-authored-by: Svyatoslav Nikolsky <svyatonik@gmail.com>
    
    * Derive `RuntimeDebug` instead of `Debug`
    
    * Add `Paramter` as a trait constraint on config types
    
    Since we use these types as part of the dispatchable functions
    we should explicitly require this.
    
    * Enforce that hasher output matches expected hash type
    
    * Accept headers over indexes when making test justifications
    
    * Check that authority sets are valid
    
    * Make Clippy happy
    
    * Apply correct Clippy fix
    
    * Move justification code into primitives module
    
    * Use new module in verifier code
    
    * Add primitives module for Substrate test helpers
    
    * WIP
    
    * Move justification generation into test_helpers
    
    * Revert commits which move `justification` into primitives
    
    This reverts commit 03a381f0bc4a8dbe4785c30d42ab252a06ba876c.
    
    Co-authored-by: Svyatoslav Nikolsky <svyatonik@gmail.com>
    Co-authored-by: Tomasz Drwięga <tomusdrw@users.noreply.github.com>

--- bridges/modules/substrate/src/mock.rs
+++ bridges/modules/substrate/src/mock.rs
@@ -14,6 +14,10 @@
 // You should have received a copy of the GNU General Public License
 // along with Parity Bridges Common.  If not, see <http://www.gnu.org/licenses/>.
 
+//! Mock Runtime for Substrate Pallet Testing.
+//!
+//! Includes some useful testing utilities in the `helpers` module.
+
 #![cfg(test)]
 
 use crate::Trait;
@@ -68,8 +72,66 @@ impl frame_system::Trait for TestRuntime {
 	type SystemWeightInfo = ();
 }
 
-impl Trait for TestRuntime {}
+impl Trait for TestRuntime {
+	type BridgedHeader = <Self as frame_system::Trait>::Header;
+	type BridgedBlockNumber = <Self as frame_system::Trait>::BlockNumber;
+	type BridgedBlockHash = <Self as frame_system::Trait>::Hash;
+	type BridgedBlockHasher = <Self as frame_system::Trait>::Hashing;
+}
 
 pub fn run_test<T>(test: impl FnOnce() -> T) -> T {
 	sp_io::TestExternalities::new(Default::default()).execute_with(test)
+}
+
+pub mod helpers {
+	use super::*;
+	use finality_grandpa::voter_set::VoterSet;
+	use sp_finality_grandpa::{AuthorityId, AuthorityList};
+	use sp_keyring::Ed25519Keyring;
+
+	pub type TestHeader = <TestRuntime as Trait>::BridgedHeader;
+	pub type TestNumber = <TestRuntime as Trait>::BridgedBlockNumber;
+	pub type TestHash = <TestRuntime as Trait>::BridgedBlockHash;
+	pub type HeaderId = (TestHash, TestNumber);
+
+	pub fn test_header(num: TestNumber) -> TestHeader {
+		let mut header = TestHeader::new_from_number(num);
+		header.parent_hash = if num == 0 {
+			Default::default()
+		} else {
+			test_header(num - 1).hash()
+		};
+
+		header
+	}
+
+	pub fn header_id(index: u8) -> HeaderId {
+		(test_header(index.into()).hash(), index as _)
+	}
+
+	pub fn extract_keyring(id: &AuthorityId) -> Ed25519Keyring {
+		let mut raw_public = [0; 32];
+		raw_public.copy_from_slice(id.as_ref());
+		Ed25519Keyring::from_raw_public(raw_public).unwrap()
+	}
+
+	pub fn voter_set() -> VoterSet<AuthorityId> {
+		VoterSet::new(authority_list()).unwrap()
+	}
+
+	pub fn authority_list() -> AuthorityList {
+		vec![(alice(), 1), (bob(), 1), (charlie(), 1)]
+	}
+
+	pub fn alice() -> AuthorityId {
+		Ed25519Keyring::Alice.public().into()
+	}
+
+	pub fn bob() -> AuthorityId {
+		Ed25519Keyring::Bob.public().into()
+	}
+
+	pub fn charlie() -> AuthorityId {
+		Ed25519Keyring::Charlie.public().into()
+	}
 }
