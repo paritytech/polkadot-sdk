5a305cec4b1c3de07e637d9bd18f857769b9f3c9
Svyatoslav Nikolsky <svyatonik@gmail.com>
2020-10-27 15:49:41 +0300

    Integrate message-lane module RPCs into Rialto/Millau nodes (#458)
    * integrate message-lane RPCs into Millau and Rialto runtime
    
    * fmt
    
    * use instance in InboundLanes
    
    * moved RialtoMessageLaneKeys/MillauMessageLaneKeys inside rpc_extensions_builder to ease Substrate refs update

--- bridges/bin/millau/node/src/service.rs
+++ bridges/bin/millau/node/src/service.rs
@@ -28,6 +28,8 @@
 // =====================================================================================
 // =====================================================================================
 
+use bp_message_lane::{LaneId, MessageNonce};
+use bp_runtime::{InstanceId, RIALTO_BRIDGE_INSTANCE};
 use millau_runtime::{self, opaque::Block, RuntimeApi};
 use sc_client_api::{ExecutorProvider, RemoteBackend};
 use sc_executor::native_executor_instance;
@@ -36,6 +38,7 @@ use sc_finality_grandpa::{FinalityProofProvider as GrandpaFinalityProofProvider,
 use sc_finality_grandpa_rpc::GrandpaRpcHandler;
 use sc_service::{error::Error as ServiceError, Configuration, TaskManager};
 use sp_consensus_aura::sr25519::AuthorityPair as AuraPair;
+use sp_core::storage::StorageKey;
 use sp_inherents::InherentDataProviders;
 use std::sync::Arc;
 use std::time::Duration;
@@ -161,10 +164,40 @@ pub fn new_full(config: Configuration) -> Result<TaskManager, ServiceError> {
 	let telemetry_connection_sinks = sc_service::TelemetryConnectionSinks::default();
 
 	let rpc_extensions_builder = {
+		// This struct is here to ease update process.
+
+		/// Millau runtime from message-lane RPC point of view.
+		struct MillauMessageLaneKeys;
+
+		impl pallet_message_lane_rpc::Runtime for MillauMessageLaneKeys {
+			fn message_key(&self, instance: &InstanceId, lane: &LaneId, nonce: MessageNonce) -> Option<StorageKey> {
+				match *instance {
+					RIALTO_BRIDGE_INSTANCE => Some(millau_runtime::rialto_messages::message_key(lane, nonce)),
+					_ => None,
+				}
+			}
+
+			fn outbound_lane_data_key(&self, instance: &InstanceId, lane: &LaneId) -> Option<StorageKey> {
+				match *instance {
+					RIALTO_BRIDGE_INSTANCE => Some(millau_runtime::rialto_messages::outbound_lane_data_key(lane)),
+					_ => None,
+				}
+			}
+
+			fn inbound_lane_data_key(&self, instance: &InstanceId, lane: &LaneId) -> Option<StorageKey> {
+				match *instance {
+					RIALTO_BRIDGE_INSTANCE => Some(millau_runtime::rialto_messages::inbound_lane_data_key(lane)),
+					_ => None,
+				}
+			}
+		}
+
+		use pallet_message_lane_rpc::{MessageLaneApi, MessageLaneRpcHandler};
 		use sc_finality_grandpa_rpc::GrandpaApi;
 		use sc_rpc::DenyUnsafe;
 		use substrate_frame_rpc_system::{FullSystem, SystemApi};
 
+		let backend = backend.clone();
 		let client = client.clone();
 		let pool = transaction_pool.clone();
 
@@ -187,6 +220,10 @@ pub fn new_full(config: Configuration) -> Result<TaskManager, ServiceError> {
 				justification_stream.clone(),
 				subscription_executor,
 				finality_proof_provider.clone(),
+			)));
+			io.extend_with(MessageLaneApi::to_delegate(MessageLaneRpcHandler::new(
+				backend.clone(),
+				Arc::new(MillauMessageLaneKeys),
 			)));
 
 			io
