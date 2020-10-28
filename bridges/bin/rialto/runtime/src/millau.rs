7d170c92f2adffc995c227b78abd38ecd1201ebd
Svyatoslav Nikolsky <svyatonik@gmail.com>
2020-10-28 12:26:12 +0300

    Update jsonrpsee reference (#467)
    * update Millau state_root
    
    * update jsonrpsee reference

--- bridges/bin/rialto/runtime/src/millau.rs
+++ bridges/bin/rialto/runtime/src/millau.rs
@@ -36,7 +36,7 @@ pub fn initial_header() -> Header {
 	Header {
 		parent_hash: Default::default(),
 		number: Default::default(),
-		state_root: hex!("234a17bbd3fbaff8f0a799a6c8f0bdba1979e242fb2ed66d15945acb84947cbd").into(),
+		state_root: hex!("dc7567715330c666eca349a4612e82ec3b3c4f306ef941dce192a37fb3131cfa").into(),
 		extrinsics_root: hex!("03170a2e7597b7b7e3d84c05391d139a62b157e78786d8c082f29dcf4c111314").into(),
 		digest: Default::default(),
 	}
