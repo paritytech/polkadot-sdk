996219d6ec86308caa362b71e1ddc034657da6fb
Robert Habermeier <rphmeier@gmail.com>
2020-12-02 02:53:45 -0500

    Update parachain registration script to new extrinsic (#252)
    * Update parachain registration script to new extrinsic
    
    * Update parachain registration script

--- cumulus/scripts/register_parachain.sh
+++ cumulus/scripts/register_parachain.sh
@@ -2,7 +2,7 @@
 
 usage() {
     echo Usage:
-    echo "$0 <url> <seed> <wasm> <genesis> <parachain-id> <tokens> <account>"
+    echo "$0 <url> <seed> <wasm> <genesis> <parachain-id> <types>"
     exit 1
 }
 
@@ -11,24 +11,26 @@ seed=$2
 wasm=$3
 genesis=$4
 parachain_id=$5
-tokens=$6
-account=$7
+types=$6 # we can remove this once parachain types are included in polkadot-js-api
 
 [ -z "$url" ] && usage
 [ -z "$seed" ] && usage
 [ -z "$wasm" ] && usage
+[ -z "$types" ] && usage
 [ -z "$genesis" ] && usage
 [ -z "$parachain_id" ] && usage
-[ -z "$tokens" ] && usage
-[ -z "$account" ] && usage
 if ! [ -r "$wasm" ]; then
     echo "Could not read: $wasm"
+    exit 1
+fi
+if ! [ -r "$types" ]; then
+    echo "Could not read: $types"
     exit 1
 fi
 
 if ! which polkadot-js-api &> /dev/null; then
     echo 'command `polkadot-js-api` not in PATH'
-    echo "npm install -g @polkadot/api-cli"
+    echo "npm install -g @polkadot/api-cli@beta"
     exit 1
 fi
 
@@ -36,21 +38,13 @@ set -e -x
 
 test -f "$seed" && seed="$(cat "$seed")"
 
-polkadot-js-api \
-    --ws "${url?}" \
-    --sudo \
-    --seed "${seed?}" \
-    tx.registrar.registerPara \
-        "${parachain_id?}" \
-        '{"scheduling":"Always"}' \
-        @"${wasm?}" \
-        "${genesis?}"
+wasm=$(cat $wasm)
 
 polkadot-js-api \
     --ws "${url?}" \
     --sudo \
     --seed "${seed?}" \
-    tx.balances.setBalance \
-        "${account?}" \
-        $((tokens * 10 ** 12)) \
-        0
+    --types "${types?}" \
+    tx.parasSudoWrapper.sudoScheduleParaInitialize \
+        "${parachain_id?}" \
+        "{ \"genesisHead\":\"${genesis?}\", \"validationCode\":\"${wasm?}\", \"parachain\": true }" \
