ace8c7169103961b3f48ce4cc7fd2c9eac8b738c
Svyatoslav Nikolsky <svyatonik@gmail.com>
2021-01-04 23:54:51 +0300

    Estimate message fee api (#600)
    * estimate_message_delivery_and_dispatch_fee runtime API
    
    * auto-determine message fees in relay
    
    * remove fee argument from relay calls
    
    * Fix import of weight contant
    
    Co-authored-by: Hernando Castano <castano.ha@gmail.com>

--- bridges/bin/rialto/runtime/src/millau_messages.rs
+++ bridges/bin/rialto/runtime/src/millau_messages.rs
@@ -125,9 +125,9 @@ impl MessageBridge for WithMillauMessageBridge {
 		<crate::Runtime as pallet_transaction_payment::Config>::WeightToFee::calc(&weight) as _
 	}
 
-	fn this_balance_to_bridged_balance(this_balance: bp_rialto::Balance) -> bp_millau::Balance {
+	fn bridged_balance_to_this_balance(bridged_balance: bp_millau::Balance) -> bp_rialto::Balance {
 		// 1:1 conversion that will probably change in the future
-		this_balance as _
+		bridged_balance as _
 	}
 }
 
