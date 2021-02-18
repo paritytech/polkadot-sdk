02127cba39403e0b5af04930b38c97244dcb6d67
Svyatoslav Nikolsky <svyatonik@gmail.com>
2021-02-18 10:15:12 +0300

    Message lane pallet parameters + updatable conversion rate (#728)
    * message lane pallet parameters
    
    * updated comment
    
    * Update modules/message-lane/src/lib.rs
    
    Co-authored-by: Tomasz Drwięga <tomusdrw@users.noreply.github.com>
    
    * fmt
    
    Co-authored-by: Tomasz Drwięga <tomusdrw@users.noreply.github.com>

--- bridges/bin/millau/runtime/src/lib.rs
+++ bridges/bin/millau/runtime/src/lib.rs
@@ -336,6 +336,7 @@ impl pallet_message_lane::Config for Runtime {
 	type Event = Event;
 	// TODO: https://github.com/paritytech/parity-bridges-common/issues/390
 	type WeightInfo = pallet_message_lane::weights::RialtoWeight<Runtime>;
+	type Parameter = rialto_messages::MillauToRialtoMessageLaneParameter;
 	type MaxMessagesToPruneAtOnce = MaxMessagesToPruneAtOnce;
 	type MaxUnrewardedRelayerEntriesAtInboundLane = MaxUnrewardedRelayerEntriesAtInboundLane;
 	type MaxUnconfirmedMessagesAtInboundLane = MaxUnconfirmedMessagesAtInboundLane;
