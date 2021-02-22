e265309c7ef541f8c56ed54870a7aad9ad98f799
Tomasz Drwięga <tomusdrw@users.noreply.github.com>
2021-02-22 17:15:04 +0100

    Set weird substrate global state for custom SS58 prefixes (#754)
    * Issue with SS58 decoding.
    
    * Switch to test branch.
    
    * Fix set.
    
    * Revert "Switch to test branch."
    
    This reverts commit d79e42ea9bc780edb20296dae546d534f1916fc7.
    
    * cargo fmt --all
    
    Co-authored-by: adoerr <0xad@gmx.net>

--- bridges/bin/rialto/node/src/command.rs
+++ bridges/bin/rialto/node/src/command.rs
@@ -69,6 +69,9 @@ impl SubstrateCli for Cli {
 /// Parse and run command line arguments
 pub fn run() -> sc_cli::Result<()> {
 	let cli = Cli::from_args();
+	sp_core::crypto::set_default_ss58_version(sp_core::crypto::Ss58AddressFormat::Custom(
+		rialto_runtime::SS58Prefix::get() as u16,
+	));
 
 	match &cli.subcommand {
 		Some(Subcommand::Benchmark(cmd)) => {
