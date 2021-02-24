79f7b33e6d5b5e34306645cc5eed8ff957434c11
Svyatoslav Nikolsky <svyatonik@gmail.com>
2021-02-24 03:19:18 +0300

    verify ADDITIONAL_MESSAGE_BYTE_DELIVERY_WEIGHT constant value (#731)

--- bridges/bin/millau/runtime/src/rialto_messages.rs
+++ bridges/bin/millau/runtime/src/rialto_messages.rs
@@ -126,7 +126,7 @@ impl MessageBridge for WithRialtoMessageBridge {
 			message_payload_len.saturating_add(bp_millau::EXTRA_STORAGE_PROOF_SIZE as _),
 			extra_bytes_in_payload
 				.saturating_mul(bp_rialto::ADDITIONAL_MESSAGE_BYTE_DELIVERY_WEIGHT)
-				.saturating_add(bp_rialto::MAX_SINGLE_MESSAGE_DELIVERY_TX_WEIGHT),
+				.saturating_add(bp_rialto::DEFAULT_MESSAGE_DELIVERY_TX_WEIGHT),
 		)
 	}
 
