dadfcfc3ed0fc9a57990641252c2c9169cf199fd
Svyatoslav Nikolsky <svyatonik@gmail.com>
2021-02-24 03:19:18 +0300

    verify ADDITIONAL_MESSAGE_BYTE_DELIVERY_WEIGHT constant value (#731)

--- bridges/primitives/millau/src/lib.rs
+++ bridges/primitives/millau/src/lib.rs
@@ -69,15 +69,19 @@ pub const MAX_UNREWARDED_RELAYER_ENTRIES_AT_INBOUND_LANE: MessageNonce = 1024;
 /// Maximal number of unconfirmed messages at inbound lane.
 pub const MAX_UNCONFIRMED_MESSAGES_AT_INBOUND_LANE: MessageNonce = 1024;
 
-/// Maximal weight of single regular message delivery transaction on Millau chain.
+/// Weight of single regular message delivery transaction on Millau chain.
 ///
-/// This value is a result of `pallet_message_lane::Module::receive_messages_proof` weight formula computation
-/// for the case when single message is delivered. The result then must be rounded up to account possible future
-/// runtime upgrades.
-pub const MAX_SINGLE_MESSAGE_DELIVERY_TX_WEIGHT: Weight = 1_000_000_000;
+/// This value is a result of `pallet_message_lane::Module::receive_messages_proof_weight()` call
+/// for the case when single message of `pallet_message_lane::EXPECTED_DEFAULT_MESSAGE_LENGTH` bytes is delivered.
+/// The message must have dispatch weight set to zero. The result then must be rounded up to account
+/// possible future runtime upgrades.
+pub const DEFAULT_MESSAGE_DELIVERY_TX_WEIGHT: Weight = 1_000_000_000;
 
 /// Increase of delivery transaction weight on Millau chain with every additional message byte.
-pub const ADDITIONAL_MESSAGE_BYTE_DELIVERY_WEIGHT: Weight = 3_000;
+///
+/// This value is a result of `pallet_message_lane::WeightInfoExt::storage_proof_size_overhead(1)` call. The
+/// result then must be rounded up to account possible future runtime upgrades.
+pub const ADDITIONAL_MESSAGE_BYTE_DELIVERY_WEIGHT: Weight = 25_000;
 
 /// Maximal weight of single message delivery confirmation transaction on Millau chain.
 ///
