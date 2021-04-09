d4489a0a92064a37d26780cc3d1597164f2eb85d
Svyatoslav Nikolsky <svyatonik@gmail.com>
2021-04-09 15:00:05 +0300

    Install CA certificates in relay images (#880)
    * update CA certificates in relay images
    
    * clippy
    
    * clippy
    
    * clippy

--- bridges/relays/bin-substrate/src/rialto_millau/rialto_messages_to_millau.rs
+++ bridges/relays/bin-substrate/src/rialto_millau/rialto_messages_to_millau.rs
@@ -59,7 +59,7 @@ impl SubstrateMessageLane for RialtoMessagesToMillau {
 	type TargetChain = Millau;
 
 	fn source_transactions_author(&self) -> bp_rialto::AccountId {
-		self.source_sign.public().as_array_ref().clone().into()
+		(*self.source_sign.public().as_array_ref()).into()
 	}
 
 	fn make_messages_receiving_proof_transaction(
@@ -86,7 +86,7 @@ impl SubstrateMessageLane for RialtoMessagesToMillau {
 	}
 
 	fn target_transactions_author(&self) -> bp_rialto::AccountId {
-		self.target_sign.public().as_array_ref().clone().into()
+		(*self.target_sign.public().as_array_ref()).into()
 	}
 
 	fn make_messages_delivery_transaction(
@@ -141,7 +141,7 @@ pub async fn run(
 	metrics_params: MetricsParams,
 ) -> Result<(), String> {
 	let stall_timeout = Duration::from_secs(5 * 60);
-	let relayer_id_at_rialto = rialto_sign.public().as_array_ref().clone().into();
+	let relayer_id_at_rialto = (*rialto_sign.public().as_array_ref()).into();
 
 	let lane = RialtoMessagesToMillau {
 		source_client: rialto_client.clone(),
