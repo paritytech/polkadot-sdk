67102885dd73280bb6ccc3029b0a594298713283
Gavin Wood <gavin@parity.io>
2021-04-30 17:52:42 +0200

    Chain ID and ParaID don't collide (#425)

--- cumulus/rococo-parachains/src/cli.rs
+++ cumulus/rococo-parachains/src/cli.rs
@@ -62,7 +62,7 @@ pub struct ExportGenesisStateCommand {
 	/// Id of the parachain this state is for.
 	///
 	/// Default: 100
-	#[structopt(long, conflicts_with = "chain")]
+	#[structopt(long)]
 	pub parachain_id: Option<u32>,
 
 	/// Write output in binary. Default is to write in hex.
@@ -70,7 +70,7 @@ pub struct ExportGenesisStateCommand {
 	pub raw: bool,
 
 	/// The name of the chain for that the genesis state should be exported.
-	#[structopt(long, conflicts_with = "parachain-id")]
+	#[structopt(long)]
 	pub chain: Option<String>,
 }
 
