ac6bce1fb57ff1f19e995520a1137d6248d86f2b
Hernando Castano <HCastano@users.noreply.github.com>
2021-06-17 12:44:04 -0400

    Bump Substrate to `286d7ce` (#1006)
    * Bump dependencies
    
    - Substrate to `74a1bfdcc`
    - `finality-grandpa` to `0.14.1`
    - `fs-swap` to `0.2.6`
    
    * Add `pallet_randomness_collective_flip` to runtimes
    
    * Rename GenesisConfig fields
    
    See Substrate PR: https://github.com/paritytech/substrate/pull/8990
    
    * Missed PoA renames in Rialto GenesisConfig
    
    * Use `spawn_essential_handle` for transaction pool
    
    See Substrate PR: https://github.com/paritytech/substrate/pull/9074

--- bin/rialto/node/src/service.rs
+++ bin/rialto/node/src/service.rs
@@ -105,7 +105,7 @@ pub fn new_partial(
 		config.transaction_pool.clone(),
 		config.role.is_authority().into(),
 		config.prometheus_registry(),
-		task_manager.spawn_handle(),
+		task_manager.spawn_essential_handle(),
 		client.clone(),
 	);
 
@@ -393,7 +393,7 @@ pub fn new_light(mut config: Configuration) -> Result<TaskManager, ServiceError>
 	let transaction_pool = Arc::new(sc_transaction_pool::BasicPool::new_light(
 		config.transaction_pool.clone(),
 		config.prometheus_registry(),
-		task_manager.spawn_handle(),
+		task_manager.spawn_essential_handle(),
 		client.clone(),
 		on_demand.clone(),
 	));
