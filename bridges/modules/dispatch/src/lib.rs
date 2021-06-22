4a954ab7c64c0a84503db5444762d0d5e80a7ccd
kaichao <kaichaosuna@gmail.com>
2021-06-22 15:20:00 +0800

    Allow Root to assume SourceAccount. (#1011)
    * Allow Root to assume SourceAccount.
    
    * fmt code

--- bridges/modules/dispatch/src/lib.rs
+++ bridges/modules/dispatch/src/lib.rs
@@ -355,7 +355,7 @@ where
 		}
 		CallOrigin::SourceAccount(ref source_account_id) => {
 			ensure!(
-				sender_origin == &RawOrigin::Signed(source_account_id.clone()),
+				sender_origin == &RawOrigin::Signed(source_account_id.clone()) || sender_origin == &RawOrigin::Root,
 				BadOrigin
 			);
 			Ok(Some(source_account_id.clone()))
@@ -964,10 +964,7 @@ mod tests {
 			Err(BadOrigin)
 		));
 
-		// If we try and send the message from Root, it is also rejected
-		assert!(matches!(
-			verify_message_origin(&RawOrigin::Root, &message),
-			Err(BadOrigin)
-		));
+		// The Root account is allowed to assume any expected origin account
+		assert!(matches!(verify_message_origin(&RawOrigin::Root, &message), Ok(Some(1))));
 	}
 }
