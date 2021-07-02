6504acbcf5cee32553520dcc1b37da46a34b2424
Svyatoslav Nikolsky <svyatonik@gmail.com>
2021-07-02 11:08:35 +0300

    give real value to Rialto and Millau tokens (#1038)

--- bridges/relays/bin-substrate/src/chains/mod.rs
+++ bridges/relays/bin-substrate/src/chains/mod.rs
@@ -32,6 +32,15 @@ mod rococo;
 mod westend;
 mod wococo;
 
+// Millau/Rialto tokens have no any real value, so the conversion rate we use is always 1:1. But we want to
+// test our code that is intended to work with real-value chains. So to keep it close to 1:1, we'll be treating
+// Rialto as BTC and Millau as wBTC (only in relayer).
+
+/// The identifier of token, which value is associated with Rialto token value by relayer.
+pub(crate) const RIALTO_ASSOCIATED_TOKEN_ID: &str = "bitcoin";
+/// The identifier of token, which value is associated with Millau token value by relayer.
+pub(crate) const MILLAU_ASSOCIATED_TOKEN_ID: &str = "wrapped-bitcoin";
+
 use relay_utils::metrics::{FloatJsonValueMetric, MetricsParams, PrometheusError, Registry};
 
 pub(crate) fn add_polkadot_kusama_price_metrics<T: finality_relay::FinalitySyncPipeline>(
