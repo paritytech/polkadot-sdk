c299a5f7ee4cf8ef6d845bd7029c2840f3287321
Hernando Castano <HCastano@users.noreply.github.com>
2021-08-05 15:37:39 -0400

    Make CI happy again (#1086)
    * Appease `cargo-spellcheck`
    
    * Increase logging for `cargo-spellcheck`
    
    * Fix config file name

--- bridges/bin/millau/node/src/cli.rs
+++ bridges/bin/millau/node/src/cli.rs
@@ -32,7 +32,7 @@ pub enum Subcommand {
 	/// Key management CLI utilities
 	Key(sc_cli::KeySubcommand),
 
-	/// Verify a signature for a message, provided on STDIN, with a given (public or secret) key.
+	/// Verify a signature for a message, provided on `STDIN`, with a given (public or secret) key.
 	Verify(sc_cli::VerifyCmd),
 
 	/// Generate a seed that provides a vanity address.
