6c1df0a39b51be8febab0d06a473a80bd4490d6a
Hector Bulgarini <hector@parity.io>
2021-08-06 10:23:09 -0300

    Change ChainSpec::from_genesis for Rialto and Millau chains to reflect the chain names. (#1079)
    * Changing chain_spec of Rialto and Millau chains to retrieve their respective name
    
    * Adding development as well
    
    * Addressing PR feedback
    
    Co-authored-by: Svyatoslav Nikolsky <svyatonik@gmail.com>

--- bridges/bin/rialto/node/src/chain_spec.rs
+++ bridges/bin/rialto/node/src/chain_spec.rs
@@ -82,8 +82,8 @@ impl Alternative {
 		);
 		match self {
 			Alternative::Development => ChainSpec::from_genesis(
-				"Development",
-				"dev",
+				"Rialto Development",
+				"rialto_dev",
 				sc_service::ChainType::Development,
 				|| {
 					testnet_genesis(
@@ -108,8 +108,8 @@ impl Alternative {
 				None,
 			),
 			Alternative::LocalTestnet => ChainSpec::from_genesis(
-				"Local Testnet",
-				"local_testnet",
+				"Rialto Local",
+				"rialto_local",
 				sc_service::ChainType::Local,
 				|| {
 					testnet_genesis(
