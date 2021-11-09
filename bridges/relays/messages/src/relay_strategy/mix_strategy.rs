07b9686e52ef33073605724a5cdfa0541dedb9a1
Svyatoslav Nikolsky <svyatonik@gmail.com>
2021-11-09 12:58:04 +0300

    add missing license header (#1204)

--- bridges/relays/messages/src/relay_strategy/mix_strategy.rs
+++ bridges/relays/messages/src/relay_strategy/mix_strategy.rs
@@ -1,3 +1,21 @@
+// Copyright 2019-2021 Parity Technologies (UK) Ltd.
+// This file is part of Parity Bridges Common.
+
+// Parity Bridges Common is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Parity Bridges Common is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Parity Bridges Common.  If not, see <http://www.gnu.org/licenses/>.
+
+//! Adapter for using `enum RelayerMode` in a context which requires `RelayStrategy`.
+
 use async_trait::async_trait;
 
 use crate::{
@@ -9,7 +27,7 @@ use crate::{
 	relay_strategy::{AltruisticStrategy, RationalStrategy, RelayReference, RelayStrategy},
 };
 
-/// The relayer doesn't care about rewards.
+/// `RelayerMode` adapter.
 #[derive(Clone)]
 pub struct MixStrategy {
 	relayer_mode: RelayerMode,
