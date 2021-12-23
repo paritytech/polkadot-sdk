daac1a0de733c52ff9d40940b71950d83f5eaba8
Svyatoslav Nikolsky <svyatonik@gmail.com>
2021-12-23 17:18:43 +0300

    Replace InboundLaneApi::latest_received_nonce with direct storage read (#1269)
    * remove inbound_latest_received_nonce
    
    * Update relays/lib-substrate-relay/src/messages_target.rs
    
    Co-authored-by: Adrian Catangiu <adrian@parity.io>

--- bridges/modules/messages/src/lib.rs
+++ bridges/modules/messages/src/lib.rs
@@ -764,16 +764,6 @@ pub mod pallet {
 			OutboundMessages::<T, I>::get(MessageKey { lane_id: lane, nonce })
 		}
 
-		/// Get nonce of the latest generated message at given outbound lane.
-		pub fn outbound_latest_generated_nonce(lane: LaneId) -> MessageNonce {
-			OutboundLanes::<T, I>::get(&lane).latest_generated_nonce
-		}
-
-		/// Get nonce of the latest received message at given inbound lane.
-		pub fn inbound_latest_received_nonce(lane: LaneId) -> MessageNonce {
-			InboundLanes::<T, I>::get(&lane).last_delivered_nonce()
-		}
-
 		/// Get nonce of the latest confirmed message at given inbound lane.
 		pub fn inbound_latest_confirmed_nonce(lane: LaneId) -> MessageNonce {
 			InboundLanes::<T, I>::get(&lane).last_confirmed_nonce
