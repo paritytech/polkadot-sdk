d4e692e4298cb6e6f5baa51d769ee83bcb3b37b9
Svyatoslav Nikolsky <svyatonik@gmail.com>
2022-02-03 16:03:44 +0300

    increase relay balance guard limits for Polkadot<>Kusama bridge (#1308)

--- bridges/relays/bin-substrate/src/chains/kusama_headers_to_polkadot.rs
+++ bridges/relays/bin-substrate/src/chains/kusama_headers_to_polkadot.rs
@@ -24,8 +24,13 @@ use substrate_relay_helper::{finality_pipeline::SubstrateFinalitySyncPipeline, T
 /// relay as gone wild.
 ///
 /// Actual value, returned by `maximal_balance_decrease_per_day_is_sane` test is approximately 21
-/// DOT, but let's round up to 30 DOT here.
-pub(crate) const MAXIMAL_BALANCE_DECREASE_PER_DAY: bp_polkadot::Balance = 30_000_000_000;
+/// DOT, and initial value of this constant was rounded up to 30 DOT. But for actual Kusama <>
+/// Polkadot deployment we'll be using the same account for delivering finality (free for mandatory
+/// headers) and messages. It means that we can't predict maximal loss. But to protect funds against
+/// relay/deployment issues, let's limit it so something that is much larger than this estimation -
+/// e.g. to 100 DOT.
+// TODO: https://github.com/paritytech/parity-bridges-common/issues/1307
+pub(crate) const MAXIMAL_BALANCE_DECREASE_PER_DAY: bp_polkadot::Balance = 100 * 10_000_000_000;
 
 /// Description of Kusama -> Polkadot finalized headers bridge.
 #[derive(Clone, Debug)]
