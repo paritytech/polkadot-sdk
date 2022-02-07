92c87b68ff0bc248ebb76f0889b52e2a4e87e04f
Svyatoslav Nikolsky <svyatonik@gmail.com>
2022-02-07 17:37:28 +0300

    expose fee multiplier metrics in messages relay (#1312)

--- bridges/relays/bin-substrate/src/chains/millau_messages_to_rialto.rs
+++ bridges/relays/bin-substrate/src/chains/millau_messages_to_rialto.rs
@@ -38,6 +38,11 @@ impl SubstrateMessageLane for MillauMessagesToRialto {
 	const TARGET_TO_SOURCE_CONVERSION_RATE_PARAMETER_NAME: Option<&'static str> =
 		Some(bp_millau::RIALTO_TO_MILLAU_CONVERSION_RATE_PARAMETER_NAME);
 
+	const SOURCE_FEE_MULTIPLIER_PARAMETER_NAME: Option<&'static str> = None;
+	const TARGET_FEE_MULTIPLIER_PARAMETER_NAME: Option<&'static str> = None;
+	const AT_SOURCE_TRANSACTION_PAYMENT_PALLET_NAME: Option<&'static str> = None;
+	const AT_TARGET_TRANSACTION_PAYMENT_PALLET_NAME: Option<&'static str> = None;
+
 	type SourceChain = Millau;
 	type TargetChain = Rialto;
 
