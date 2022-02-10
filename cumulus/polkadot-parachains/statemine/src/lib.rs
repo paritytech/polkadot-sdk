999748757b23312718a520f97f04c6be9d94b6c4
Squirrel <gilescope@gmail.com>
2022-02-10 16:17:57 +0000

    collective flip migration removed, uniques added (#965) (#974)
    Co-authored-by: Ignacio Palacios <ignacio.palacios.santos@gmail.com>

--- cumulus/polkadot-parachains/statemine/src/lib.rs
+++ cumulus/polkadot-parachains/statemine/src/lib.rs
@@ -785,18 +785,8 @@ pub type Executive = frame_executive::Executive<
 	frame_system::ChainContext<Runtime>,
 	Runtime,
 	AllPalletsWithSystem,
-	(RemoveCollectiveFlip, UniquesV1Migration),
+	UniquesV1Migration,
 >;
-
-pub struct RemoveCollectiveFlip;
-impl frame_support::traits::OnRuntimeUpgrade for RemoveCollectiveFlip {
-	fn on_runtime_upgrade() -> Weight {
-		use frame_support::storage::migration;
-		// Remove the storage value `RandomMaterial` from removed pallet `RandomnessCollectiveFlip`
-		migration::remove_storage_prefix(b"RandomnessCollectiveFlip", b"RandomMaterial", b"");
-		<Runtime as frame_system::Config>::DbWeight::get().writes(1)
-	}
-}
 
 pub struct UniquesV1Migration;
 impl frame_support::traits::OnRuntimeUpgrade for UniquesV1Migration {
--- cumulus/polkadot-parachains/westmint/src/lib.rs
+++ cumulus/polkadot-parachains/westmint/src/lib.rs
@@ -769,16 +769,13 @@ pub type Executive = frame_executive::Executive<
 	frame_system::ChainContext<Runtime>,
 	Runtime,
 	AllPalletsWithSystem,
-	RemoveCollectiveFlip,
+	UniquesV1Migration,
 >;
 
-pub struct RemoveCollectiveFlip;
-impl frame_support::traits::OnRuntimeUpgrade for RemoveCollectiveFlip {
+pub struct UniquesV1Migration;
+impl frame_support::traits::OnRuntimeUpgrade for UniquesV1Migration {
 	fn on_runtime_upgrade() -> Weight {
-		use frame_support::storage::migration;
-		// Remove the storage value `RandomMaterial` from removed pallet `RandomnessCollectiveFlip`
-		migration::remove_storage_prefix(b"RandomnessCollectiveFlip", b"RandomMaterial", b"");
-		<Runtime as frame_system::Config>::DbWeight::get().writes(1)
+		pallet_uniques::migration::migrate_to_v1::<Runtime, _, Uniques>()
 	}
 }
 
