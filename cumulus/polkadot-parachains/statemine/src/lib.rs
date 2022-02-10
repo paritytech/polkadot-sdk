acf37d29cca422851d5a239f76b771610ee96be0
Squirrel <gilescope@gmail.com>
2022-02-10 16:15:49 +0000

    Set statemin runtime versions (#971)

--- cumulus/polkadot-parachains/statemine/src/lib.rs
+++ cumulus/polkadot-parachains/statemine/src/lib.rs
@@ -91,7 +91,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("statemine"),
 	impl_name: create_runtime_str!("statemine"),
 	authoring_version: 1,
-	spec_version: 601,
+	spec_version: 700,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 4,
--- cumulus/polkadot-parachains/statemint/src/lib.rs
+++ cumulus/polkadot-parachains/statemint/src/lib.rs
@@ -92,7 +92,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("statemint"),
 	impl_name: create_runtime_str!("statemint"),
 	authoring_version: 1,
-	spec_version: 601,
+	spec_version: 700,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 4,
