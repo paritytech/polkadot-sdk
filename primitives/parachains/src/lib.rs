f2d18f00b49eb990d33fdffbb6584ff9c5e687ed
Svyatoslav Nikolsky <svyatonik@gmail.com>
2022-05-20 11:34:52 +0300

    Parachains finality relay (#1199)

---
+++ primitives/parachains/src/lib.rs
@@ -0,0 +1,68 @@
+// Copyright 2019-2021 Parity Technologies (UK) Ltd.
+// This file is part of Parity Bridges Common.
+
+// Parity Bridges Common is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Parity Bridges Common is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Parity Bridges Common.  If not, see <http://www.gnu.org/licenses/>.
+
+//! Primitives of parachains module.
+
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use bp_polkadot_core::{
+	parachains::{ParaHash, ParaId},
+	BlockNumber as RelayBlockNumber,
+};
+use codec::{Decode, Encode};
+use frame_support::{Blake2_128Concat, RuntimeDebug, Twox64Concat};
+use scale_info::TypeInfo;
+use sp_core::storage::StorageKey;
+
+/// Best known parachain head hash.
+#[derive(Clone, Decode, Encode, PartialEq, RuntimeDebug, TypeInfo)]
+pub struct BestParaHeadHash {
+	/// Number of relay block where this head has been read.
+	///
+	/// Parachain head is opaque to relay chain. So we can't simply decode it as a header of
+	/// parachains and call `block_number()` on it. Instead, we're using the fact that parachain
+	/// head is always built on top of previous head (because it is blockchain) and relay chain
+	/// always imports parachain heads in order. What it means for us is that at any given
+	/// **finalized** relay block `B`, head of parachain will be ancestor (or the same) of all
+	/// parachain heads available at descendants of `B`.
+	pub at_relay_block_number: RelayBlockNumber,
+	/// Hash of parachain head.
+	pub head_hash: ParaHash,
+}
+
+/// Returns runtime storage key of given parachain head at the source chain.
+///
+/// The head is stored by the `paras` pallet in the `Heads` map.
+pub fn parachain_head_storage_key_at_source(
+	paras_pallet_name: &str,
+	para_id: ParaId,
+) -> StorageKey {
+	bp_runtime::storage_map_final_key::<Twox64Concat>(paras_pallet_name, "Heads", &para_id.encode())
+}
+
+/// Returns runtime storage key of best known parachain head at the target chain.
+///
+/// The head is stored by the `pallet-bridge-parachains` pallet in the `BestParaHeads` map.
+pub fn parachain_head_storage_key_at_target(
+	bridge_parachains_pallet_name: &str,
+	para_id: ParaId,
+) -> StorageKey {
+	bp_runtime::storage_map_final_key::<Blake2_128Concat>(
+		bridge_parachains_pallet_name,
+		"BestParaHeads",
+		&para_id.encode(),
+	)
+}
---
+++ relays/client-substrate/src/test_chain.rs
@@ -0,0 +1,71 @@
+// Copyright 2019-2021 Parity Technologies (UK) Ltd.
+// This file is part of Parity Bridges Common.
+
+// Parity Bridges Common is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Parity Bridges Common is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Parity Bridges Common.  If not, see <http://www.gnu.org/licenses/>.
+
+//! Pallet provides a set of guard functions that are running in background threads
+//! and are aborting process if some condition fails.
+
+//! Test chain implementation to use in tests.
+
+#![cfg(any(feature = "test-helpers", test))]
+
+use crate::{Chain, ChainWithBalances};
+use frame_support::weights::{IdentityFee, Weight};
+use std::time::Duration;
+
+/// Chain that may be used in tests.
+#[derive(Clone, Debug, PartialEq)]
+pub struct TestChain;
+
+impl bp_runtime::Chain for TestChain {
+	type BlockNumber = u32;
+	type Hash = sp_core::H256;
+	type Hasher = sp_runtime::traits::BlakeTwo256;
+	type Header = sp_runtime::generic::Header<u32, sp_runtime::traits::BlakeTwo256>;
+
+	type AccountId = u32;
+	type Balance = u32;
+	type Index = u32;
+	type Signature = sp_runtime::testing::TestSignature;
+
+	fn max_extrinsic_size() -> u32 {
+		unreachable!()
+	}
+
+	fn max_extrinsic_weight() -> Weight {
+		unreachable!()
+	}
+}
+
+impl Chain for TestChain {
+	const NAME: &'static str = "Test";
+	const TOKEN_ID: Option<&'static str> = None;
+	const BEST_FINALIZED_HEADER_ID_METHOD: &'static str = "TestMethod";
+	const AVERAGE_BLOCK_INTERVAL: Duration = Duration::from_millis(0);
+	const STORAGE_PROOF_OVERHEAD: u32 = 0;
+	const MAXIMAL_ENCODED_ACCOUNT_ID_SIZE: u32 = 0;
+
+	type SignedBlock = sp_runtime::generic::SignedBlock<
+		sp_runtime::generic::Block<Self::Header, sp_runtime::OpaqueExtrinsic>,
+	>;
+	type Call = ();
+	type WeightToFee = IdentityFee<u32>;
+}
+
+impl ChainWithBalances for TestChain {
+	fn account_info_storage_key(_account_id: &u32) -> sp_core::storage::StorageKey {
+		unreachable!()
+	}
+}
