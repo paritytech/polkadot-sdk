c2abd7bde568a08e81e6e5e51532c5105e104330
Chevdor <chevdor@users.noreply.github.com>
2022-05-23 14:37:20 +0200

    Runtime version bump to 922 (#1283)
    * Runtime version bump to 922
    
    * Switch to 9220

--- cumulus/parachains/runtimes/contracts/contracts-rococo/src/lib.rs
+++ cumulus/parachains/runtimes/contracts/contracts-rococo/src/lib.rs
@@ -111,7 +111,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("contracts-rococo"),
 	impl_name: create_runtime_str!("contracts-rococo"),
 	authoring_version: 1,
-	spec_version: 900,
+	spec_version: 9220,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
