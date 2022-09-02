fd36b6d0c887f124e8f5ba69a744040ae2dc8e55
Squirrel <gilescope@gmail.com>
2022-09-02 10:17:43 +0100

    9270 Backport of Align versions for runtimes (#1517) (#1574)
    * Align versions for runtimes (#1517)
    
    * update cargo lock
    
    Co-authored-by: Branislav Kontur <bkontur@gmail.com>

--- cumulus/parachains/runtimes/testing/penpal/src/lib.rs
+++ cumulus/parachains/runtimes/testing/penpal/src/lib.rs
@@ -186,7 +186,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("penpal-parachain"),
 	impl_name: create_runtime_str!("penpal-parachain"),
 	authoring_version: 1,
-	spec_version: 1,
+	spec_version: 9270,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
