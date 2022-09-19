46c735cb3b89b250491e4d1bb6d30aae112657f6
Alexander Theißen <alex.theissen@me.com>
2022-09-19 13:22:11 +0200

    Companion: contracts: Use `WeakBoundedVec` for instrumented code (#1595)
    * Remove `RelaxedMaxCodeLen`
    
    * update lockfile for {"substrate", "polkadot"}
    
    Co-authored-by: parity-processbot <>

--- cumulus/parachains/runtimes/contracts/contracts-rococo/src/contracts.rs
+++ cumulus/parachains/runtimes/contracts/contracts-rococo/src/contracts.rs
@@ -58,7 +58,6 @@ impl Config for Runtime {
 	type AddressGenerator = DefaultAddressGenerator;
 	type ContractAccessWeight = DefaultContractAccessWeight<RuntimeBlockWeights>;
 	type MaxCodeLen = ConstU32<{ 128 * 1024 }>;
-	type RelaxedMaxCodeLen = ConstU32<{ 256 * 1024 }>;
 	type MaxStorageKeyLen = ConstU32<128>;
 }
 
