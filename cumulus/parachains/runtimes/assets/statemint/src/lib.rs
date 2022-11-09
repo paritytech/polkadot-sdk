6307ffebf9b9d0b4cb25089794c4f31eccdd65cd
Wilfried Kopp <wilfried@parity.io>
2022-11-09 15:06:22 +0100

    Bump spec_version to 9320

--- cumulus/parachains/runtimes/assets/statemint/src/lib.rs
+++ cumulus/parachains/runtimes/assets/statemint/src/lib.rs
@@ -117,7 +117,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("statemint"),
 	impl_name: create_runtime_str!("statemint"),
 	authoring_version: 1,
-	spec_version: 9300,
+	spec_version: 9320,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 8,
--- cumulus/parachains/runtimes/collectives/collectives-polkadot/src/lib.rs
+++ cumulus/parachains/runtimes/collectives/collectives-polkadot/src/lib.rs
@@ -102,7 +102,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("collectives"),
 	impl_name: create_runtime_str!("collectives"),
 	authoring_version: 1,
-	spec_version: 9300,
+	spec_version: 9320,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 0,
