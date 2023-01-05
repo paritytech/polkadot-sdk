db663c507240e9bb682b378034c554847b761a72
Egor_P <egor@parity.io>
2023-01-05 09:57:18 +0100

    remove 9330 migrations from statemint series (#2031) (#2039)
    Co-authored-by: joe petrowski <25483142+joepetrowski@users.noreply.github.com>

--- cumulus/parachains/runtimes/assets/statemint/src/lib.rs
+++ cumulus/parachains/runtimes/assets/statemint/src/lib.rs
@@ -651,11 +651,6 @@ pub type UncheckedExtrinsic =
 /// Extrinsic type that has already been checked.
 pub type CheckedExtrinsic = generic::CheckedExtrinsic<AccountId, RuntimeCall, SignedExtra>;
 
-pub type Migrations = (
-	pallet_balances::migration::MigrateToTrackInactive<Runtime, xcm_config::CheckingAccount>,
-	pallet_assets::migration::v1::MigrateToV1<Runtime>,
-);
-
 /// Executive: handles dispatch to the various modules.
 pub type Executive = frame_executive::Executive<
 	Runtime,
@@ -663,7 +658,7 @@ pub type Executive = frame_executive::Executive<
 	frame_system::ChainContext<Runtime>,
 	Runtime,
 	AllPalletsWithSystem,
-	Migrations,
+	pallet_balances::migration::MigrateToTrackInactive<Runtime, xcm_config::CheckingAccount>,
 >;
 
 #[cfg(feature = "runtime-benchmarks")]
