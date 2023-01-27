dea2523eca94218e32bbd90e214c6c6c95a192ce
Serban Iorga <serban@parity.io>
2023-01-27 14:13:25 +0200

    Use named parameters for indirect calls (#1823)

--- bridges/relays/bin-substrate/src/chains/rococo_parachains_to_bridge_hub_wococo.rs
+++ bridges/relays/bin-substrate/src/chains/rococo_parachains_to_bridge_hub_wococo.rs
@@ -51,11 +51,11 @@ impl SubmitParachainHeadsCallBuilder<BridgeHubRococoToBridgeHubWococo>
 		parachain_heads_proof: ParaHeadsProof,
 	) -> CallOf<relay_bridge_hub_wococo_client::BridgeHubWococo> {
 		relay_bridge_hub_wococo_client::runtime::Call::BridgeRococoParachain(
-			relay_bridge_hub_wococo_client::runtime::BridgeParachainCall::submit_parachain_heads(
-				(at_relay_block.0, at_relay_block.1),
+			relay_bridge_hub_wococo_client::runtime::BridgeParachainCall::submit_parachain_heads {
+				at_relay_block: (at_relay_block.0, at_relay_block.1),
 				parachains,
 				parachain_heads_proof,
-			),
+			},
 		)
 	}
 }
