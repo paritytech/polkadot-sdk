e5caa80bb607b0c451ea5d50593b54c3c555b707
Serban Iorga <serban@parity.io>
2023-02-20 16:36:13 +0200

    RefundRelayerForMessagesFromParachain improvements (#1895)
    * RefundRelayerForMessagesFromParachain changes
    
    - some renamings and cosmetics
    - removing the GI generic param since we can get it from the parachains
      config
    - merging (Parachains Instance, Parachain Id) and (Messages Instance,
      Messages lane Id) generic params
    - removing unnecessay derives
    - renaming for the `TransactionFeeCalculation` and defining
      `ActualFeeRefund` concrete implementation
    - adding support for multiple RefundRelayerForMessagesFromParachain
      (by adding a `StaticStrProvider` generic param)
    
    * Revert to using RuntimeDebug

--- bridges/modules/parachains/src/call_ext.rs
+++ bridges/modules/parachains/src/call_ext.rs
@@ -17,14 +17,11 @@
 use crate::{Config, Pallet, RelayBlockNumber};
 use bp_parachains::BestParaHeadHash;
 use bp_polkadot_core::parachains::{ParaHash, ParaId};
-use frame_support::{dispatch::CallableCallFor, traits::IsSubType};
-use sp_runtime::{
-	transaction_validity::{InvalidTransaction, TransactionValidity, ValidTransaction},
-	RuntimeDebug,
-};
+use frame_support::{dispatch::CallableCallFor, traits::IsSubType, RuntimeDebug};
+use sp_runtime::transaction_validity::{InvalidTransaction, TransactionValidity, ValidTransaction};
 
 /// Info about a `SubmitParachainHeads` call which tries to update a single parachain.
-#[derive(Copy, Clone, PartialEq, RuntimeDebug)]
+#[derive(PartialEq, RuntimeDebug)]
 pub struct SubmitParachainHeadsInfo {
 	pub at_relay_block_number: RelayBlockNumber,
 	pub para_id: ParaId,
