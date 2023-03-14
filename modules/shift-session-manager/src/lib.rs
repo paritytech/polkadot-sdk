75c26916343aebb97b9bfddc4451e76689a0ec32
Branislav Kontur <bkontur@gmail.com>
2023-03-14 13:45:13 +0100

    Removed deprecated `#[pallet::generate_store(pub(super) trait Store)]` according to latest Cumulus (#1964)

--- modules/shift-session-manager/src/lib.rs
+++ modules/shift-session-manager/src/lib.rs
@@ -35,7 +35,6 @@ pub mod pallet {
 	pub trait Config: pallet_session::Config {}
 
 	#[pallet::pallet]
-	#[pallet::generate_store(pub(super) trait Store)]
 	#[pallet::without_storage_info]
 	pub struct Pallet<T>(PhantomData<T>);
 
