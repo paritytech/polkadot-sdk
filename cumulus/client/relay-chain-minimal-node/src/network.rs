6377d6038f2b77c825c5cb1440b921144a0093bf
Aaro Altonen <48052676+altonen@users.noreply.github.com>
2023-03-30 15:33:14 +0300

    Companion for https://github.com/paritytech/substrate/pull/13725 (#2401)
    * Companion for https://github.com/paritytech/substrate/pull/13725
    
    * Add comment
    
    * update lockfile for {"substrate", "polkadot"}
    
    ---------
    
    Co-authored-by: parity-processbot <>

--- cumulus/client/relay-chain-minimal-node/src/network.rs
+++ cumulus/client/relay-chain-minimal-node/src/network.rs
@@ -26,6 +26,7 @@ use sc_network::{
 
 use sc_network_common::{role::Roles, sync::message::BlockAnnouncesHandshake};
 use sc_service::{error::Error, Configuration, NetworkStarter, SpawnTaskHandle};
+use sc_utils::mpsc::tracing_unbounded;
 
 use std::{iter, sync::Arc};
 
@@ -49,6 +50,8 @@ pub(crate) fn build_collator_network(
 		genesis_hash,
 	);
 
+	// RX is not used for anything because syncing is not started for the minimal node
+	let (tx, _rx) = tracing_unbounded("mpsc_syncing_engine_protocol", 100_000);
 	let network_params = sc_network::config::Params::<Block> {
 		role: config.role.clone(),
 		executor: {
@@ -64,6 +67,7 @@ pub(crate) fn build_collator_network(
 		metrics_registry: config.prometheus_config.as_ref().map(|config| config.registry.clone()),
 		block_announce_config,
 		request_response_protocol_configs: Vec::new(),
+		tx,
 	};
 
 	let network_worker = sc_network::NetworkWorker::new(network_params)?;
