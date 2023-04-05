ae332ac1f29697f24dc543b37d7b5d250c7ba841
Bastian Köcher <info@kchr.de>
2023-04-05 23:27:52 +0200

    Fix compilation

--- substrate/client/api/src/execution_extensions.rs
+++ substrate/client/api/src/execution_extensions.rs
@@ -133,8 +133,6 @@ impl<Block: BlockT> ExecutionExtensions<Block> {
 
 		extensions.register(ReadRuntimeVersionExt::new(self.read_runtime_version.clone()));
 
-		extensions.register(ReadRuntimeVersionExt::new(self.read_runtime_version.clone()));
-
 		extensions
 	}
 }
