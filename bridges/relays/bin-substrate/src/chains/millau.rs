795e2ce522ead81a5463030256d3f3bd366c8d4b
Serban Iorga <serban@parity.io>
2023-05-03 10:44:06 +0300

    Fix sized messages (Follow-up on #2064) (#2103)
    * Use wasm execution on millau and rialto
    
    It's safer like this since here are some errors that aren't caught when
    using native execution
    
    * Revert "Revert "Fix max-size messages at test chains (#2064)" (#2077)"
    
    This reverts commit 62f749e124fbcf074cdc9840690bb9049d4b3b99.
    
    * Adjustments

--- bridges/relays/bin-substrate/src/chains/millau.rs
+++ bridges/relays/bin-substrate/src/chains/millau.rs
@@ -18,10 +18,38 @@
 
 use crate::cli::{encode_message::CliEncodeMessage, CliChain};
 use bp_runtime::EncodedOrDecodedCall;
+use bridge_runtime_common::CustomNetworkId;
 use relay_millau_client::Millau;
 use relay_substrate_client::SimpleRuntimeVersion;
+use xcm_executor::traits::ExportXcm;
 
 impl CliEncodeMessage for Millau {
+	fn encode_wire_message(
+		target: xcm::v3::NetworkId,
+		at_target_xcm: xcm::v3::Xcm<()>,
+	) -> anyhow::Result<Vec<u8>> {
+		anyhow::ensure!(
+			[
+				CustomNetworkId::Rialto.as_network_id(),
+				CustomNetworkId::RialtoParachain.as_network_id()
+			]
+			.contains(&target),
+			anyhow::format_err!("Unsupported target chain: {:?}", target)
+		);
+
+		Ok(millau_runtime::xcm_config::ToRialtoOrRialtoParachainSwitchExporter::validate(
+			target,
+			0,
+			&mut Some(Self::dummy_universal_source()?),
+			&mut Some(target.into()),
+			&mut Some(at_target_xcm),
+		)
+		.map_err(|e| anyhow::format_err!("Failed to prepare outbound message: {:?}", e))?
+		.0
+		 .1
+		 .0)
+	}
+
 	fn encode_execute_xcm(
 		message: xcm::VersionedXcm<Self::Call>,
 	) -> anyhow::Result<EncodedOrDecodedCall<Self::Call>> {
