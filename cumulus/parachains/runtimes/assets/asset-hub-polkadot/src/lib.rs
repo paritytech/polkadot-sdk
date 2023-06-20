43c9a9496950c7178821d9ed788572d2c13bdd94
Branislav Kontur <bkontur@gmail.com>
2023-06-20 17:48:25 +0200

    AssetHubs - split weights for pallet_assets (#2743)
    * AssetHubs - split weights for pallet_assets
    
    * ".git/.scripts/commands/bench/bench.sh" pallet asset-hub-kusama assets pallet_assets
    
    * ".git/.scripts/commands/bench/bench.sh" pallet asset-hub-kusama assets pallet_assets
    
    * ".git/.scripts/commands/bench/bench.sh" pallet asset-hub-westend assets pallet_assets
    
    * ".git/.scripts/commands/bench/bench.sh" pallet asset-hub-polkadot assets pallet_assets
    
    * ".git/.scripts/commands/bench/bench.sh" pallet asset-hub-westend assets pallet_assets
    
    * ".git/.scripts/commands/bench/bench.sh" pallet asset-hub-polkadot assets pallet_assets
    
    * Rename weight file for pallet_assets instances (polkadot)
    
    * ".git/.scripts/commands/bench/bench.sh" pallet asset-hub-polkadot assets pallet_assets
    
    * Rename weight file for pallet_assets instances (kusama/westend)
    
    * ".git/.scripts/commands/bench/bench.sh" pallet asset-hub-kusama assets pallet_assets
    
    * ".git/.scripts/commands/bench/bench.sh" pallet asset-hub-westend assets pallet_assets
    
    * Changed `pallet_assets_remote` -> `pallet_assets_foreign`
    
    * fmt
    
    * ".git/.scripts/commands/bench/bench.sh" pallet asset-hub-kusama assets pallet_assets
    
    * ".git/.scripts/commands/bench/bench.sh" pallet asset-hub-westend assets pallet_assets
    
    * ".git/.scripts/commands/bench/bench.sh" pallet asset-hub-polkadot assets pallet_assets
    
    ---------
    
    Co-authored-by: parity-processbot <>

--- cumulus/parachains/runtimes/assets/asset-hub-polkadot/src/lib.rs
+++ cumulus/parachains/runtimes/assets/asset-hub-polkadot/src/lib.rs
@@ -287,7 +287,7 @@ impl pallet_assets::Config<TrustBackedAssetsInstance> for Runtime {
 	type StringLimit = AssetsStringLimit;
 	type Freezer = ();
 	type Extra = ();
-	type WeightInfo = weights::pallet_assets::WeightInfo<Runtime>;
+	type WeightInfo = weights::pallet_assets_local::WeightInfo<Runtime>;
 	type CallbackHandle = ();
 	type AssetAccountDeposit = AssetAccountDeposit;
 	type RemoveItemsLimit = frame_support::traits::ConstU32<1000>;
@@ -329,7 +329,7 @@ impl pallet_assets::Config<ForeignAssetsInstance> for Runtime {
 	type StringLimit = ForeignAssetsAssetsStringLimit;
 	type Freezer = ();
 	type Extra = ();
-	type WeightInfo = weights::pallet_assets::WeightInfo<Runtime>;
+	type WeightInfo = weights::pallet_assets_foreign::WeightInfo<Runtime>;
 	type CallbackHandle = ();
 	type AssetAccountDeposit = ForeignAssetsAssetAccountDeposit;
 	type RemoveItemsLimit = frame_support::traits::ConstU32<1000>;
@@ -812,8 +812,8 @@ extern crate frame_benchmarking;
 mod benches {
 	define_benchmarks!(
 		[frame_system, SystemBench::<Runtime>]
-		[pallet_assets, Assets]
-		[pallet_assets, ForeignAssets]
+		[pallet_assets, Local]
+		[pallet_assets, Foreign]
 		[pallet_balances, Balances]
 		[pallet_multisig, Multisig]
 		[pallet_nfts, Nfts]
@@ -1047,6 +1047,13 @@ impl_runtime_apis! {
 			type XcmBalances = pallet_xcm_benchmarks::fungible::Pallet::<Runtime>;
 			type XcmGeneric = pallet_xcm_benchmarks::generic::Pallet::<Runtime>;
 
+			// Benchmark files generated for `Assets/ForeignAssets` instances are by default
+			// `pallet_assets_assets.rs / pallet_assets_foreign_assets`, which is not really nice,
+			// so with this redefinition we can change names to nicer:
+			// `pallet_assets_local.rs / pallet_assets_foreign.rs`.
+			type Local = pallet_assets::Pallet::<Runtime, TrustBackedAssetsInstance>;
+			type Foreign = pallet_assets::Pallet::<Runtime, ForeignAssetsInstance>;
+
 			let mut list = Vec::<BenchmarkList>::new();
 			list_benchmarks!(list, extra);
 
@@ -1170,6 +1177,9 @@ impl_runtime_apis! {
 
 			type XcmBalances = pallet_xcm_benchmarks::fungible::Pallet::<Runtime>;
 			type XcmGeneric = pallet_xcm_benchmarks::generic::Pallet::<Runtime>;
+
+			type Local = pallet_assets::Pallet::<Runtime, TrustBackedAssetsInstance>;
+			type Foreign = pallet_assets::Pallet::<Runtime, ForeignAssetsInstance>;
 
 			let whitelist: Vec<TrackedStorageKey> = vec![
 				// Block Number
