dcbe134b08d14c18294dc398234c178e47469c55
Svyatoslav Nikolsky <svyatonik@gmail.com>
2023-07-17 12:50:38 +0300

    XCM bridge hub pallet (will be deployed at bridge hubs) (#2213)
    * XCM over bridge pallet (won't ever be merged to this repo): initial commit
    
    * try both bridge-id and lane-id
    
    * flush
    
    * flush
    
    * flush
    
    * flush
    
    * first prototype
    
    * started working on xcm-over-bridge tests
    
    * proper open_bridge_works test
    
    * more open_bridge tests
    
    * request_bridge_closure tests
    
    * tests for close_bridge
    
    * report_misbehavior tests
    
    * renaming xcm-over-bridge ad xcm-bridge-hub: we'll have two similar pallets for dynamic lanes/fees support. One will be deployed at the bridge hub (xcm-bridge-hub) and another one (xcm-bridge-hub-router) at sibling/parent chains to send messages to remote network over the bridge hub
    
    * added couple of TODOs
    
    * moved BridgeQueuesState here + TODO for implementing report_bridge_queues_state + fmt
    
    * fixing TODOs
    
    * fixing TODOs
    
    * moved bridge locations to primitives
    
    * added a couple of TODOs
    
    * ref issue from TODO
    
    * clippy and spelling
    
    * fix tests
    
    * another TODOs
    
    * typo
    
    * LaneState -> force_close_bridge
    
    * start_closing_the_bridge -> start_closing_bridge
    
    * removed Closing bridge state, so now we only have the Opened -> optional temporary Closed state -> None
    
    * spelling
    
    * started prototyping suspend+resume on misbehavior
    
    * continue prototyping
    
    * more tests for open_bridge
    
    * more tests for close_bridge
    
    * more tests for report_misbehavior
    
    * started tests for resume_misbehaving_bridges
    
    * implemented tests for resume_misbehaving_bridges
    
    * remove UnsuspendedChannelWithMisbehavingOwner because now, when all bridges are resumed at once + we assume that the inbound XCM channel is suspended immediately it is no longer actual
    
    * added TODO
    
    * add comment re misbehavior reporter if he's associated with the bridge owner
    
    * ignored clippy
    
    * fmt
    
    * use versioned XCM structs in public interfaces and storage + Box XCM locations where possible
    
    * spelling
    
    * removed TODO in favor of https://github.com/paritytech/parity-bridges-common/pull/2257
    
    * LocalChannelManager -> LocalXcmChannelManager
    
    * removed code specific to #2233, because it isn't the only option now
    
    * removemisbehavior mentions
    
    * also temporary (?) remove BridgesByLocalOrigin because the storage format will likely change to be able to resume bridges from the on_iniitalize/on_idle
    
    * AllowedOpenBridgeOrigin -> OpenBridgeOrigin
    
    * - TooManyBridgesForLocalOrigin
    
    * use bridge_destination_relative_location in args
    
    * better impl of strip_low_level_junctions
    
    * get rid of xcm_into_latest
    
    * clippy
    
    * added #![warn(missing_docs)] to new crates

--- bridges/primitives/xcm-bridge-hub/src/lib.rs
+++ bridges/primitives/xcm-bridge-hub/src/lib.rs
@@ -16,9 +16,19 @@
 
 //! Primitives of the xcm-bridge-hub pallet.
 
+#![warn(missing_docs)]
 #![cfg_attr(not(feature = "std"), no_std)]
 
-use xcm::latest::prelude::*;
+use bp_messages::LaneId;
+use bp_runtime::{AccountIdOf, BalanceOf, Chain};
+use codec::{Decode, Encode, MaxEncodedLen};
+use frame_support::{
+	ensure, sp_runtime::RuntimeDebug, CloneNoBound, PalletError, PartialEqNoBound,
+	RuntimeDebugNoBound,
+};
+use scale_info::TypeInfo;
+use sp_std::boxed::Box;
+use xcm::{latest::prelude::*, VersionedLocation};
 
 /// Encoded XCM blob. We expect the bridge messages pallet to use this blob type for both inbound
 /// and outbound payloads.
@@ -45,8 +55,7 @@ pub trait LocalXcmChannelManager {
 	/// `owner` chain (in any form), we expect it to stop sending messages to us and queue
 	/// messages at that `owner` chain instead.
 	///
-	/// This method will be called if we detect a misbehavior in one of bridges, owned by
-	/// the `owner`. We expect that:
+	/// We expect that:
 	///
 	/// - no more incoming XCM messages from the `owner` will be processed until further
 	///  `resume_inbound_channel` call;
@@ -60,9 +69,8 @@ pub trait LocalXcmChannelManager {
 	/// Start handling incoming messages from from given bridge `owner` (parent/sibling chain)
 	/// again.
 	///
-	/// This method is called when the `owner` tries to resume bridge operations after
-	/// resolving "misbehavior" issues. The channel is assumed to be suspended by the previous
-	/// `suspend_inbound_channel` call, however we don't check it anywhere.
+	/// The channel is assumed to be suspended by the previous `suspend_inbound_channel` call,
+	/// however we don't check it anywhere.
 	///
 	/// This method shall not fail if the channel is already resumed.
 	fn resume_inbound_channel(owner: Location) -> Result<(), ()>;
@@ -75,5 +83,418 @@ impl LocalXcmChannelManager for () {
 
 	fn resume_inbound_channel(_owner: Location) -> Result<(), ()> {
 		Err(())
+	}
+}
+
+/// Bridge state.
+#[derive(Clone, Copy, Decode, Encode, Eq, PartialEq, TypeInfo, MaxEncodedLen, RuntimeDebug)]
+pub enum BridgeState {
+	/// Bridge is opened. Associated lanes are also opened.
+	Opened,
+	/// Bridge is closed. Associated lanes are also closed.
+	/// After all outbound messages will be pruned, the bridge will vanish without any traces.
+	Closed,
+}
+
+/// Bridge metadata.
+#[derive(
+	CloneNoBound, Decode, Encode, Eq, PartialEqNoBound, TypeInfo, MaxEncodedLen, RuntimeDebugNoBound,
+)]
+#[scale_info(skip_type_params(ThisChain))]
+pub struct Bridge<ThisChain: Chain> {
+	/// Relative location of the bridge origin chain.
+	pub bridge_origin_relative_location: Box<VersionedLocation>,
+	/// Current bridge state.
+	pub state: BridgeState,
+	/// Account with the reserved funds.
+	pub bridge_owner_account: AccountIdOf<ThisChain>,
+	/// Reserved amount on the sovereign account of the sibling bridge origin.
+	pub reserve: BalanceOf<ThisChain>,
+}
+
+/// Locations of bridge endpoints at both sides of the bridge.
+#[derive(Clone, RuntimeDebug, PartialEq, Eq)]
+pub struct BridgeLocations {
+	/// Relative (to this bridge hub) location of this side of the bridge.
+	pub bridge_origin_relative_location: Location,
+	/// Universal (unique) location of this side of the bridge.
+	pub bridge_origin_universal_location: InteriorLocation,
+	/// Universal (unique) location of other side of the bridge.
+	pub bridge_destination_universal_location: InteriorLocation,
+	/// An identifier of the dedicated bridge message lane.
+	pub lane_id: LaneId,
+}
+
+/// Errors that may happen when we check bridge locations.
+#[derive(Encode, Decode, RuntimeDebug, PartialEq, Eq, PalletError, TypeInfo)]
+pub enum BridgeLocationsError {
+	/// Origin or destination locations are not universal.
+	NonUniversalLocation,
+	/// Bridge origin location is not supported.
+	InvalidBridgeOrigin,
+	/// Bridge destination is not supported (in general).
+	InvalidBridgeDestination,
+	/// Destination location is within the same global consensus.
+	DestinationIsLocal,
+	/// Destination network is not the network we are bridged with.
+	UnreachableDestination,
+	/// Destination location is unsupported. We only support bridges with relay
+	/// chain or its parachains.
+	UnsupportedDestinationLocation,
+}
+
+/// Given XCM locations, generate lane id and universal locations of bridge endpoints.
+///
+/// The `here_universal_location` is the universal location of the bridge hub runtime.
+///
+/// The `bridge_origin_relative_location` is the relative (to the `here_universal_location`)
+/// location of the bridge endpoint at this side of the bridge. It may be the parent relay
+/// chain or the sibling parachain. All junctions below parachain level are dropped.
+///
+/// The `bridge_destination_universal_location` is the universal location of the bridge
+/// destination. It may be the parent relay or the sibling parachain of the **bridged**
+/// bridge hub. All junctions below parachain level are dropped.
+///
+/// Why we drop all junctions between parachain level - that's because the lane is a bridge
+/// between two chains. All routing under this level happens when the message is delivered
+/// to the bridge destination. So at bridge level we don't care about low level junctions.
+///
+/// Returns error if `bridge_origin_relative_location` is outside of `here_universal_location`
+/// local consensus OR if `bridge_destination_universal_location` is not a universal location.
+pub fn bridge_locations(
+	here_universal_location: Box<InteriorLocation>,
+	bridge_origin_relative_location: Box<Location>,
+	bridge_destination_universal_location: Box<InteriorLocation>,
+	expected_remote_network: NetworkId,
+) -> Result<Box<BridgeLocations>, BridgeLocationsError> {
+	fn strip_low_level_junctions(
+		location: InteriorLocation,
+	) -> Result<InteriorLocation, BridgeLocationsError> {
+		let mut junctions = location.into_iter();
+
+		let global_consensus = junctions
+			.next()
+			.filter(|junction| matches!(junction, GlobalConsensus(_)))
+			.ok_or(BridgeLocationsError::NonUniversalLocation)?;
+
+		// we only expect `Parachain` junction here. There are other junctions that
+		// may need to be supported (like `GeneralKey` and `OnlyChild`), but now we
+		// only support bridges with relay and parachans
+		//
+		// if there's something other than parachain, let's strip it
+		let maybe_parachain = junctions.next().filter(|junction| matches!(junction, Parachain(_)));
+		Ok(match maybe_parachain {
+			Some(parachain) => [global_consensus, parachain].into(),
+			None => [global_consensus].into(),
+		})
+	}
+
+	// ensure that the `here_universal_location` and `bridge_destination_universal_location`
+	// are universal locations within different consensus systems
+	let local_network = here_universal_location
+		.global_consensus()
+		.map_err(|_| BridgeLocationsError::NonUniversalLocation)?;
+	let remote_network = bridge_destination_universal_location
+		.global_consensus()
+		.map_err(|_| BridgeLocationsError::NonUniversalLocation)?;
+	ensure!(local_network != remote_network, BridgeLocationsError::DestinationIsLocal);
+	ensure!(
+		remote_network == expected_remote_network,
+		BridgeLocationsError::UnreachableDestination
+	);
+
+	// get universal location of endpoint, located at this side of the bridge
+	let bridge_origin_universal_location = here_universal_location
+		.within_global(*bridge_origin_relative_location.clone())
+		.map_err(|_| BridgeLocationsError::InvalidBridgeOrigin)?;
+	// strip low-level junctions within universal locations
+	let bridge_origin_universal_location =
+		strip_low_level_junctions(bridge_origin_universal_location)?;
+	let bridge_destination_universal_location =
+		strip_low_level_junctions(*bridge_destination_universal_location)?;
+
+	// we know that the `bridge_destination_universal_location` starts from the
+	// `GlobalConsensus` and we know that the `bridge_origin_universal_location`
+	// is also within the `GlobalConsensus`. So we know that the lane id will be
+	// the same on both ends of the bridge
+	let lane_id = LaneId::new(
+		bridge_origin_universal_location.clone(),
+		bridge_destination_universal_location.clone(),
+	);
+
+	Ok(Box::new(BridgeLocations {
+		bridge_origin_relative_location: *bridge_origin_relative_location,
+		bridge_origin_universal_location,
+		bridge_destination_universal_location,
+		lane_id,
+	}))
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	const LOCAL_NETWORK: NetworkId = Kusama;
+	const REMOTE_NETWORK: NetworkId = Polkadot;
+	const UNREACHABLE_NETWORK: NetworkId = Rococo;
+	const SIBLING_PARACHAIN: u32 = 1000;
+	const LOCAL_BRIDGE_HUB: u32 = 1001;
+	const REMOTE_PARACHAIN: u32 = 2000;
+
+	struct SuccessfulTest {
+		here_universal_location: InteriorLocation,
+		bridge_origin_relative_location: Location,
+
+		bridge_origin_universal_location: InteriorLocation,
+		bridge_destination_universal_location: InteriorLocation,
+	}
+
+	fn run_successful_test(test: SuccessfulTest) -> BridgeLocations {
+		let locations = bridge_locations(
+			Box::new(test.here_universal_location),
+			Box::new(test.bridge_origin_relative_location.clone()),
+			Box::new(test.bridge_destination_universal_location.clone()),
+			REMOTE_NETWORK,
+		);
+		assert_eq!(
+			locations,
+			Ok(Box::new(BridgeLocations {
+				bridge_origin_relative_location: test.bridge_origin_relative_location.clone(),
+				bridge_origin_universal_location: test.bridge_origin_universal_location.clone(),
+				bridge_destination_universal_location: test
+					.bridge_destination_universal_location
+					.clone(),
+				lane_id: LaneId::new(
+					test.bridge_origin_universal_location,
+					test.bridge_destination_universal_location,
+				),
+			})),
+		);
+
+		*locations.unwrap()
+	}
+
+	// successful tests that with various origins and destinations
+
+	#[test]
+	fn at_relay_from_local_relay_to_remote_relay_works() {
+		run_successful_test(SuccessfulTest {
+			here_universal_location: [GlobalConsensus(LOCAL_NETWORK)].into(),
+			bridge_origin_relative_location: Here.into(),
+
+			bridge_origin_universal_location: [GlobalConsensus(LOCAL_NETWORK)].into(),
+			bridge_destination_universal_location: [GlobalConsensus(REMOTE_NETWORK)].into(),
+		});
+	}
+
+	#[test]
+	fn at_relay_from_sibling_parachain_to_remote_relay_works() {
+		run_successful_test(SuccessfulTest {
+			here_universal_location: [GlobalConsensus(LOCAL_NETWORK)].into(),
+			bridge_origin_relative_location: [Parachain(SIBLING_PARACHAIN)].into(),
+
+			bridge_origin_universal_location: [
+				GlobalConsensus(LOCAL_NETWORK),
+				Parachain(SIBLING_PARACHAIN),
+			]
+			.into(),
+			bridge_destination_universal_location: [GlobalConsensus(REMOTE_NETWORK)].into(),
+		});
+	}
+
+	#[test]
+	fn at_relay_from_local_relay_to_remote_parachain_works() {
+		run_successful_test(SuccessfulTest {
+			here_universal_location: [GlobalConsensus(LOCAL_NETWORK)].into(),
+			bridge_origin_relative_location: Here.into(),
+
+			bridge_origin_universal_location: [GlobalConsensus(LOCAL_NETWORK)].into(),
+			bridge_destination_universal_location: [
+				GlobalConsensus(REMOTE_NETWORK),
+				Parachain(REMOTE_PARACHAIN),
+			]
+			.into(),
+		});
+	}
+
+	#[test]
+	fn at_relay_from_sibling_parachain_to_remote_parachain_works() {
+		run_successful_test(SuccessfulTest {
+			here_universal_location: [GlobalConsensus(LOCAL_NETWORK)].into(),
+			bridge_origin_relative_location: [Parachain(SIBLING_PARACHAIN)].into(),
+
+			bridge_origin_universal_location: [
+				GlobalConsensus(LOCAL_NETWORK),
+				Parachain(SIBLING_PARACHAIN),
+			]
+			.into(),
+			bridge_destination_universal_location: [
+				GlobalConsensus(REMOTE_NETWORK),
+				Parachain(REMOTE_PARACHAIN),
+			]
+			.into(),
+		});
+	}
+
+	#[test]
+	fn at_bridge_hub_from_local_relay_to_remote_relay_works() {
+		run_successful_test(SuccessfulTest {
+			here_universal_location: [GlobalConsensus(LOCAL_NETWORK), Parachain(LOCAL_BRIDGE_HUB)]
+				.into(),
+			bridge_origin_relative_location: Parent.into(),
+
+			bridge_origin_universal_location: [GlobalConsensus(LOCAL_NETWORK)].into(),
+			bridge_destination_universal_location: [GlobalConsensus(REMOTE_NETWORK)].into(),
+		});
+	}
+
+	#[test]
+	fn at_bridge_hub_from_sibling_parachain_to_remote_relay_works() {
+		run_successful_test(SuccessfulTest {
+			here_universal_location: [GlobalConsensus(LOCAL_NETWORK), Parachain(LOCAL_BRIDGE_HUB)]
+				.into(),
+			bridge_origin_relative_location: ParentThen([Parachain(SIBLING_PARACHAIN)].into())
+				.into(),
+
+			bridge_origin_universal_location: [
+				GlobalConsensus(LOCAL_NETWORK),
+				Parachain(SIBLING_PARACHAIN),
+			]
+			.into(),
+			bridge_destination_universal_location: [GlobalConsensus(REMOTE_NETWORK)].into(),
+		});
+	}
+
+	#[test]
+	fn at_bridge_hub_from_local_relay_to_remote_parachain_works() {
+		run_successful_test(SuccessfulTest {
+			here_universal_location: [GlobalConsensus(LOCAL_NETWORK), Parachain(LOCAL_BRIDGE_HUB)]
+				.into(),
+			bridge_origin_relative_location: Parent.into(),
+
+			bridge_origin_universal_location: [GlobalConsensus(LOCAL_NETWORK)].into(),
+			bridge_destination_universal_location: [
+				GlobalConsensus(REMOTE_NETWORK),
+				Parachain(REMOTE_PARACHAIN),
+			]
+			.into(),
+		});
+	}
+
+	#[test]
+	fn at_bridge_hub_from_sibling_parachain_to_remote_parachain_works() {
+		run_successful_test(SuccessfulTest {
+			here_universal_location: [GlobalConsensus(LOCAL_NETWORK), Parachain(LOCAL_BRIDGE_HUB)]
+				.into(),
+			bridge_origin_relative_location: ParentThen([Parachain(SIBLING_PARACHAIN)].into())
+				.into(),
+
+			bridge_origin_universal_location: [
+				GlobalConsensus(LOCAL_NETWORK),
+				Parachain(SIBLING_PARACHAIN),
+			]
+			.into(),
+			bridge_destination_universal_location: [
+				GlobalConsensus(REMOTE_NETWORK),
+				Parachain(REMOTE_PARACHAIN),
+			]
+			.into(),
+		});
+	}
+
+	// successful tests that show that we are ignoring low-level junctions of bridge origins
+
+	#[test]
+	fn low_level_junctions_at_bridge_origin_are_stripped() {
+		let locations1 = run_successful_test(SuccessfulTest {
+			here_universal_location: [GlobalConsensus(LOCAL_NETWORK)].into(),
+			bridge_origin_relative_location: Here.into(),
+
+			bridge_origin_universal_location: [GlobalConsensus(LOCAL_NETWORK)].into(),
+			bridge_destination_universal_location: [GlobalConsensus(REMOTE_NETWORK)].into(),
+		});
+		let locations2 = run_successful_test(SuccessfulTest {
+			here_universal_location: [GlobalConsensus(LOCAL_NETWORK)].into(),
+			bridge_origin_relative_location: [PalletInstance(0)].into(),
+
+			bridge_origin_universal_location: [GlobalConsensus(LOCAL_NETWORK)].into(),
+			bridge_destination_universal_location: [GlobalConsensus(REMOTE_NETWORK)].into(),
+		});
+
+		assert_eq!(locations1.lane_id, locations2.lane_id);
+	}
+
+	#[test]
+	fn low_level_junctions_at_bridge_destination_are_stripped() {
+		let locations1 = run_successful_test(SuccessfulTest {
+			here_universal_location: [GlobalConsensus(LOCAL_NETWORK)].into(),
+			bridge_origin_relative_location: Here.into(),
+
+			bridge_origin_universal_location: [GlobalConsensus(LOCAL_NETWORK)].into(),
+			bridge_destination_universal_location: [GlobalConsensus(REMOTE_NETWORK)].into(),
+		});
+		let locations2 = run_successful_test(SuccessfulTest {
+			here_universal_location: [GlobalConsensus(LOCAL_NETWORK)].into(),
+			bridge_origin_relative_location: Here.into(),
+
+			bridge_origin_universal_location: [GlobalConsensus(LOCAL_NETWORK)].into(),
+			bridge_destination_universal_location: [GlobalConsensus(REMOTE_NETWORK)].into(),
+		});
+
+		assert_eq!(locations1.lane_id, locations2.lane_id);
+	}
+
+	// negative tests
+
+	#[test]
+	fn bridge_locations_fails_when_here_is_not_universal_location() {
+		assert_eq!(
+			bridge_locations(
+				Box::new([Parachain(1000)].into()),
+				Box::new(Here.into()),
+				Box::new([GlobalConsensus(REMOTE_NETWORK)].into()),
+				REMOTE_NETWORK,
+			),
+			Err(BridgeLocationsError::NonUniversalLocation),
+		);
+	}
+
+	#[test]
+	fn bridge_locations_fails_when_computed_destination_is_not_universal_location() {
+		assert_eq!(
+			bridge_locations(
+				Box::new([GlobalConsensus(LOCAL_NETWORK)].into()),
+				Box::new(Here.into()),
+				Box::new([OnlyChild].into()),
+				REMOTE_NETWORK,
+			),
+			Err(BridgeLocationsError::NonUniversalLocation),
+		);
+	}
+
+	#[test]
+	fn bridge_locations_fails_when_computed_destination_is_local() {
+		assert_eq!(
+			bridge_locations(
+				Box::new([GlobalConsensus(LOCAL_NETWORK)].into()),
+				Box::new(Here.into()),
+				Box::new([GlobalConsensus(LOCAL_NETWORK), OnlyChild].into()),
+				REMOTE_NETWORK,
+			),
+			Err(BridgeLocationsError::DestinationIsLocal),
+		);
+	}
+
+	#[test]
+	fn bridge_locations_fails_when_computed_destination_is_unreachable() {
+		assert_eq!(
+			bridge_locations(
+				Box::new([GlobalConsensus(LOCAL_NETWORK)].into()),
+				Box::new(Here.into()),
+				Box::new([GlobalConsensus(UNREACHABLE_NETWORK)].into()),
+				REMOTE_NETWORK,
+			),
+			Err(BridgeLocationsError::UnreachableDestination),
+		);
 	}
 }
