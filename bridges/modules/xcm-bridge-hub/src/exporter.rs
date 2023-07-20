9e9a2f148b8b82a8a08e071a95fcb9e0e7a55031
Svyatoslav Nikolsky <svyatonik@gmail.com>
2023-07-20 17:34:41 +0300

    Implement ExportXcm and MessageDispatch for pallet-xcm-bridge-hub (#2285)
    * implement ExportXcm and MessageDispatch for pallet-xcm-bridge-hub
    
    * spelling

--- bridges/modules/xcm-bridge-hub/src/exporter.rs
+++ bridges/modules/xcm-bridge-hub/src/exporter.rs
@@ -22,16 +22,19 @@
 
 use crate::{Config, Pallet, LOG_TARGET};
 
-use bp_messages::source_chain::MessagesBridge;
 use bp_xcm_bridge_hub::XcmAsPlainPayload;
-use bridge_runtime_common::messages_xcm_extension::{LocalXcmQueueManager, SenderAndLane};
-use pallet_bridge_messages::{Config as BridgeMessagesConfig, Pallet as BridgeMessagesPallet};
+
+use bp_messages::{source_chain::MessagesBridge, LaneId};
+use frame_support::traits::Get;
+use pallet_bridge_messages::{
+	Config as BridgeMessagesConfig, Error, Pallet as BridgeMessagesPallet,
+};
 use xcm::prelude::*;
 use xcm_builder::{HaulBlob, HaulBlobError, HaulBlobExporter};
 use xcm_executor::traits::ExportXcm;
 
 /// An easy way to access `HaulBlobExporter`.
-pub type PalletAsHaulBlobExporter<T, I> = HaulBlobExporter<
+type PalletAsHaulBlobExporter<T, I> = HaulBlobExporter<
 	DummyHaulBlob,
 	<T as Config<I>>::BridgedNetwork,
 	<T as Config<I>>::DestinationVersion,
@@ -45,7 +48,7 @@ where
 	T: BridgeMessagesConfig<T::BridgeMessagesPalletInstance, OutboundPayload = XcmAsPlainPayload>,
 {
 	type Ticket = (
-		SenderAndLane,
+		LaneId,
 		<MessagesPallet<T, I> as MessagesBridge<T::OutboundPayload>>::SendMessageArgs,
 		XcmHash,
 	);
@@ -57,12 +60,12 @@ where
 		destination: &mut Option<InteriorLocation>,
 		message: &mut Option<Xcm<()>>,
 	) -> Result<(Self::Ticket, Assets), SendError> {
-		// Find supported lane_id.
-		let sender_and_lane = Self::lane_for(
-			universal_source.as_ref().ok_or(SendError::MissingArgument)?,
-			(&network, destination.as_ref().ok_or(SendError::MissingArgument)?),
-		)
-		.ok_or(SendError::NotApplicable)?;
+		// `HaulBlobExporter` may consume the `universal_source` and `destination` arguments, so
+		// let's save them before
+		let bridge_origin_universal_location =
+			universal_source.clone().take().ok_or(SendError::MissingArgument)?;
+		let bridge_destination_interior_location =
+			destination.clone().take().ok_or(SendError::MissingArgument)?;
 
 		// check if we are able to route the message. We use existing `HaulBlobExporter` for that.
 		// It will make all required changes and will encode message properly, so that the
@@ -75,23 +78,53 @@ where
 			message,
 		)?;
 
-		let bridge_message = MessagesPallet::<T, I>::validate_message(sender_and_lane.lane, &blob)
-			.map_err(|e| {
-				log::error!(
-					target: LOG_TARGET,
-					"XCM message {:?} cannot be exported because of bridge error {:?} on bridge {:?}",
-					id,
-					e,
-					sender_and_lane.lane,
-				);
-				SendError::Transport("BridgeValidateError")
-			})?;
+		// ok - now we know that the message may be routed by the pallet, let's prepare the
+		// destination universal location
+		let mut bridge_destination_universal_location: InteriorLocation =
+			GlobalConsensus(network).into();
+		bridge_destination_universal_location
+			.append_with(bridge_destination_interior_location)
+			.map_err(|_| SendError::Unroutable)?;
 
-		Ok(((sender_and_lane, bridge_message, id), price))
+		// .. and the origin relative location
+		let bridge_origin_relative_location =
+			bridge_origin_universal_location.relative_to(&T::UniversalLocation::get());
+
+		// then we are able to compute the lane id used to send messages
+		let bridge_locations = Self::bridge_locations(
+			Box::new(bridge_origin_relative_location),
+			Box::new(bridge_destination_universal_location.into()),
+		)
+		.map_err(|_| SendError::Unroutable)?;
+
+		let bridge_message =
+			MessagesPallet::<T, I>::validate_message(bridge_locations.lane_id, &blob).map_err(
+				|e| {
+					match e {
+						Error::LanesManager(ref ei) =>
+							log::error!(target: LOG_TARGET, "LanesManager: {ei:?}"),
+						Error::MessageRejectedByPallet(ref ei) =>
+							log::error!(target: LOG_TARGET, "MessageRejectedByPallet: {ei:?}"),
+						Error::ReceptionConfirmation(ref ei) =>
+							log::error!(target: LOG_TARGET, "ReceptionConfirmation: {ei:?}"),
+						_ => (),
+					};
+
+					log::error!(
+						target: LOG_TARGET,
+						"XCM message {:?} cannot be exported because of bridge error: {:?} on bridge {:?}",
+						id,
+						e,
+						bridge_locations,
+					);
+					SendError::Transport("BridgeValidateError")
+				},
+			)?;
+
+		Ok(((bridge_locations.lane_id, bridge_message, id), price))
 	}
 
-	fn deliver((sender_and_lane, bridge_message, id): Self::Ticket) -> Result<XcmHash, SendError> {
-		let lane_id = sender_and_lane.lane;
+	fn deliver((lane_id, bridge_message, id): Self::Ticket) -> Result<XcmHash, SendError> {
 		let artifacts = MessagesPallet::<T, I>::send_message(bridge_message);
 
 		log::info!(
@@ -100,12 +133,6 @@ where
 			id,
 			lane_id,
 			artifacts.nonce,
-		);
-
-		// notify XCM queue manager about updated lane state
-		LocalXcmQueueManager::<T::LanesSupport>::on_bridge_message_enqueued(
-			&sender_and_lane,
-			artifacts.enqueued_messages,
 		);
 
 		Ok(id)
@@ -119,7 +146,7 @@ where
 /// else. But bridge messages pallet may have a dedicated channel (lane) for every pair of bridged
 /// chains. So we are using our own `ExportXcm` implementation, but to utilize `HaulBlobExporter` we
 /// still need this `DummyHaulBlob`.
-pub struct DummyHaulBlob;
+struct DummyHaulBlob;
 
 impl HaulBlob for DummyHaulBlob {
 	fn haul_blob(_blob: XcmAsPlainPayload) -> Result<(), HaulBlobError> {
@@ -130,89 +157,43 @@ impl HaulBlob for DummyHaulBlob {
 #[cfg(test)]
 mod tests {
 	use super::*;
-	use crate::mock::*;
-	use bp_messages::{LaneState, OutboundLaneData};
-	use frame_support::{__private::sp_tracing, assert_ok};
+	use crate::{mock::*, LanesManagerOf};
 	use xcm_executor::traits::export_xcm;
 
-	fn universal_source() -> InteriorLocation {
-		[GlobalConsensus(RelayNetwork::get()), Parachain(SIBLING_ASSET_HUB_ID)].into()
-	}
-
-	fn universal_destination() -> InteriorLocation {
-		BridgedDestination::get()
-	}
-
 	#[test]
-	fn export_works() {
+	fn proper_lane_is_used_by_export_xcm() {
 		run_test(|| {
-			sp_tracing::try_init_simple();
-			pallet_bridge_messages::OutboundLanes::<TestRuntime>::insert(
-				test_lane_id(),
-				OutboundLaneData { state: LaneState::Opened, ..Default::default() },
-			);
-			assert_ok!(export_xcm::<XcmOverBridge>(
+			// open expected outbound lane
+			let origin = OpenBridgeOrigin::sibling_parachain_origin();
+			let with = bridged_asset_hub_location();
+			let locations =
+				XcmOverBridge::bridge_locations_from_origin(origin, Box::new(with.into())).unwrap();
+
+			let lanes_manager = LanesManagerOf::<TestRuntime, ()>::new();
+			lanes_manager.create_outbound_lane(locations.lane_id).unwrap();
+			assert!(lanes_manager
+				.active_outbound_lane(locations.lane_id)
+				.unwrap()
+				.queued_messages()
+				.is_empty());
+
+			// now let's try to enqueue message using our `ExportXcm` implementation
+			export_xcm::<XcmOverBridge>(
 				BridgedRelayNetwork::get(),
 				0,
-				universal_source(),
-				universal_destination(),
+				locations.bridge_origin_universal_location,
+				locations.bridge_destination_universal_location.split_first().0,
 				vec![Instruction::ClearOrigin].into(),
-			));
-		})
-	}
+			)
+			.unwrap();
 
-	#[test]
-	fn export_fails_if_argument_is_missing() {
-		run_test(|| {
-			assert_eq!(
-				XcmOverBridge::validate(
-					BridgedRelayNetwork::get(),
-					0,
-					&mut None,
-					&mut Some(universal_destination()),
-					&mut Some(Vec::new().into()),
-				),
-				Err(SendError::MissingArgument),
-			);
-
-			assert_eq!(
-				XcmOverBridge::validate(
-					BridgedRelayNetwork::get(),
-					0,
-					&mut Some(universal_source()),
-					&mut None,
-					&mut Some(Vec::new().into()),
-				),
-				Err(SendError::MissingArgument),
-			);
-		})
-	}
-
-	#[test]
-	fn exporter_computes_correct_lane_id() {
-		run_test(|| {
-			sp_tracing::try_init_simple();
-			let expected_lane_id = test_lane_id();
-
-			pallet_bridge_messages::OutboundLanes::<TestRuntime>::insert(
-				expected_lane_id,
-				OutboundLaneData { state: LaneState::Opened, ..Default::default() },
-			);
-
-			assert_eq!(
-				XcmOverBridge::validate(
-					BridgedRelayNetwork::get(),
-					0,
-					&mut Some(universal_source()),
-					&mut Some(universal_destination()),
-					&mut Some(Vec::new().into()),
-				)
+			// double check that the message has been pushed to the expected lane
+			// (it should already been checked during `send_message` call)
+			assert!(!lanes_manager
+				.active_outbound_lane(locations.lane_id)
 				.unwrap()
-				.0
-				 .0
-				.lane,
-				expected_lane_id,
-			);
-		})
+				.queued_messages()
+				.is_empty());
+		});
 	}
 }
