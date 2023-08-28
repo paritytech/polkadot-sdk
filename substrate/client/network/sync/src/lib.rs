5610da055ae5b263ed58ae39af0749c6ca751330
Dmitry Markin <dmitry@markin.tech>
2023-08-28 13:24:07 +0300

    minor: comment

--- substrate/client/network/sync/src/lib.rs
+++ substrate/client/network/sync/src/lib.rs
@@ -625,7 +625,7 @@ where
 		self.extra_justifications.reset();
 	}
 
-	// The implementation is similar to on_block_announce with unknown parent hash.
+	// The implementation is similar to `on_validated_block_announce` with unknown parent hash.
 	fn set_sync_fork_request(
 		&mut self,
 		mut peers: Vec<PeerId>,
