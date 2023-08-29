d4b08a959744638a64a952926019dfbb8a43cf46
Bastian Köcher <info@kchr.de>
2023-08-29 17:07:02 +0200

    Fix `test-rustdoc`

--- polkadot/node/service/src/benchmarking.rs
+++ polkadot/node/service/src/benchmarking.rs
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Polkadot.  If not, see <http://www.gnu.org/licenses/>.
 
-//! Code related to benchmarking a [`crate::Client`].
+//! Code related to benchmarking a node.
 
 use polkadot_primitives::AccountId;
 use sc_client_api::UsageProvider;
