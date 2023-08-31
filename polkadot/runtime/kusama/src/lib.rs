6c408e501b4c96c6eafa2c58656d03896a01f17d
Bryan Chen <xlchen1291@gmail.com>
2023-08-31 12:14:41 +1200

    2224 is unlocked

--- polkadot/runtime/kusama/src/lib.rs
+++ polkadot/runtime/kusama/src/lib.rs
@@ -1719,7 +1719,7 @@ pub mod migrations {
 			let id: u32 = (*id).into();
 			// ksuama parachains/parathreads that are locked and never produced block
 			match id {
-				2003 | 2077 | 2089 | 2111 | 2112 | 2127 | 2130 | 2224 | 2231 | 2233 | 2237 |
+				2003 | 2077 | 2089 | 2111 | 2112 | 2127 | 2130 | 2231 | 2233 | 2237 |
 				2256 | 2257 | 2258 | 2261 | 2268 | 2274 | 2275 => true,
 				_ => false,
 			}
