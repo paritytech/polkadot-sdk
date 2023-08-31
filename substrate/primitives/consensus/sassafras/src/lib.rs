c17ae1bf20a88a63b4e6744bd13ce0ecd1039a02
Davide Galassi <davxy@datawok.net>
2023-08-31 12:06:24 +0200

    Apply review suggestions

--- substrate/primitives/consensus/sassafras/src/lib.rs
+++ substrate/primitives/consensus/sassafras/src/lib.rs
@@ -118,6 +118,7 @@ pub struct Epoch {
 /// as such we keep this opaque representation, implementors of the runtime API will have to make
 /// sure that all usages of `OpaqueKeyOwnershipProof` refer to the same type.
 #[derive(Decode, Encode, PartialEq, TypeInfo)]
+#[repr(transparent)]
 pub struct OpaqueKeyOwnershipProof(Vec<u8>);
 
 // Runtime API.
@@ -147,26 +148,19 @@ sp_api::decl_runtime_apis! {
 		///
 		/// An example usage of this module is coupled with the session historical module to prove
 		/// that a given authority key is tied to a given staking identity during a specific
-		/// session. Proofs of key ownership are necessary for submitting equivocation reports.
+		/// session.
 		///
-		/// NOTE: even though the API takes a `slot` as parameter the current implementations
-		/// ignores this parameter and instead relies on this method being called at the correct
-		/// block height, i.e. any point at which the epoch for the given slot is live on-chain.
-		/// Future implementations will instead use indexed data through an offchain worker, not
-		/// requiring older states to be available.
-		fn generate_key_ownership_proof(
-			slot: Slot,
-			authority_id: AuthorityId,
-		) -> Option<OpaqueKeyOwnershipProof>;
+		/// Proofs of key ownership are necessary for submitting equivocation reports.
+		fn generate_key_ownership_proof(authority_id: AuthorityId) -> Option<OpaqueKeyOwnershipProof>;
 
 		/// Submits an unsigned extrinsic to report an equivocation.
 		///
 		/// The caller must provide the equivocation proof and a key ownership proof (should be
 		/// obtained using `generate_key_ownership_proof`). The extrinsic will be unsigned and
 		/// should only be accepted for local authorship (not to be broadcast to the network). This
-		/// method returns `None` when creation of the extrinsic fails, e.g. if equivocation
-		/// reporting is disabled for the given runtime (i.e. this method is hardcoded to return
-		/// `None`). Only useful in an offchain context.
+		/// method returns `false` when creation of the extrinsic fails.
+		///
+		/// Only useful in an offchain context.
 		fn submit_report_equivocation_unsigned_extrinsic(
 			equivocation_proof: EquivocationProof<Block::Header>,
 			key_owner_proof: OpaqueKeyOwnershipProof,
