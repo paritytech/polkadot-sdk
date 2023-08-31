ceb81f1c6b5129c731153662a3db974e9cd1da30
Sergej Sakac <73715684+Szegoo@users.noreply.github.com>
2023-08-31 07:52:26 +0200

    Update substrate/frame/multisig/src/tests.rs
    Co-authored-by: Liam Aharon <liam.aharon@hotmail.com>

--- substrate/frame/multisig/src/tests.rs
+++ substrate/frame/multisig/src/tests.rs
@@ -366,7 +366,7 @@ fn mutlisig_wont_get_executed_when_expired() {
 		assert!(MultisigExpiries::<Test>::get(multi, hash).is_none());
 		assert!(Multisigs::<Test>::get(multi, hash).is_none());
 
-		// The deposit got undreserved after clearing the multisig.
+		// The deposit is unreserved after clearing the multisig.
 		assert_eq!(Balances::free_balance(1), 5);
 		assert_eq!(Balances::reserved_balance(1), 0);
 
