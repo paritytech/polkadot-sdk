d2702c031189a443a441dbcda88ece41697a2785
EgorPopelyaev <egor@parity.io>
2023-09-01 12:22:24 +0200

    add ghw and scripts for docker image deployment

---
+++ docker/scripts/polkadot-parachain_build-injected-image.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+OWNER=${OWNER:-parity}
+IMAGE_NAME=${IMAGE_NAME:-polkadot-parachain}
+
+docker build --no-cache \
+    --build-arg IMAGE_NAME=$IMAGE_NAME \
+    -t $OWNER/$IMAGE_NAME \
+    -f ./docker/polkadot-parachain_injected.Dockerfile \
+    . && docker images
