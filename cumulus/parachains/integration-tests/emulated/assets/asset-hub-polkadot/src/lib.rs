241601b1c11902c1a051c9c204122fdecfe2e980
Ignacio Palacios <ignacio.palacios.santos@gmail.com>
2023-09-01 12:44:06 +0200

    [xcm-emulator] Improve hygiene and clean up (#1301)
    * improve hygiene & clean up
    
    * improve hygiene & clean up - xcm-emulator
    
    * improve hygiene & clean up - common
    
    * improve hygiene & clean up - tests
    
    * improve hygiene & clean up - tests 2
    
    * last hygiene bits
    
    * ".git/.scripts/commands/fmt/fmt.sh"
    
    ---------
    
    Co-authored-by: command-bot <>

--- cumulus/parachains/integration-tests/emulated/assets/asset-hub-polkadot/src/lib.rs
+++ cumulus/parachains/integration-tests/emulated/assets/asset-hub-polkadot/src/lib.rs
@@ -17,40 +17,28 @@
 pub use codec::Encode;
 pub use frame_support::{
 	assert_err, assert_ok,
-	instances::Instance1,
 	pallet_prelude::Weight,
-	sp_runtime::{AccountId32, DispatchError, DispatchResult, MultiAddress},
-	traits::{fungibles::Inspect, OriginTrait},
+	sp_runtime::{AccountId32, DispatchError, DispatchResult},
+	traits::fungibles::Inspect,
 };
 pub use integration_tests_common::{
 	constants::{
-		accounts::{ALICE, BOB},
-		asset_hub_polkadot::ED as ASSET_HUB_POLKADOT_ED,
-		polkadot::ED as POLKADOT_ED,
+		asset_hub_polkadot::ED as ASSET_HUB_POLKADOT_ED, polkadot::ED as POLKADOT_ED,
 		PROOF_SIZE_THRESHOLD, REF_TIME_THRESHOLD, XCM_V3,
 	},
-	lazy_static::lazy_static,
-	xcm_transact_paid_execution, xcm_transact_unpaid_execution, AssetHubPolkadot,
-	AssetHubPolkadotPallet, AssetHubPolkadotReceiver, AssetHubPolkadotSender, BridgeHubPolkadot,
-	BridgeHubPolkadotPallet, BridgeHubPolkadotReceiver, BridgeHubPolkadotSender, Collectives,
-	CollectivesPallet, CollectivesReceiver, CollectivesSender, PenpalPolkadotA,
-	PenpalPolkadotAPallet, PenpalPolkadotAReceiver, PenpalPolkadotASender, PenpalPolkadotB,
-	PenpalPolkadotBPallet, PenpalPolkadotBReceiver, PenpalPolkadotBSender, Polkadot,
-	PolkadotMockNet, PolkadotPallet, PolkadotReceiver, PolkadotSender,
+	xcm_helpers::{xcm_transact_paid_execution, xcm_transact_unpaid_execution},
+	AssetHubPolkadot, AssetHubPolkadotPallet, AssetHubPolkadotReceiver, AssetHubPolkadotSender,
+	PenpalPolkadotA, PenpalPolkadotAReceiver, Polkadot, PolkadotPallet, PolkadotReceiver,
+	PolkadotSender,
 };
 pub use parachains_common::{AccountId, Balance};
-pub use polkadot_core_primitives::InboundDownwardMessage;
-pub use polkadot_parachain_primitives::primitives::{HrmpChannelId, Id};
-pub use polkadot_runtime_parachains::inclusion::{AggregateMessageOrigin, UmpQueueId};
 pub use xcm::{
-	prelude::*,
+	prelude::{AccountId32 as AccountId32Junction, *},
 	v3::{Error, NetworkId::Polkadot as PolkadotId},
-	DoubleEncoded,
 };
 pub use xcm_emulator::{
-	assert_expected_events, bx, cumulus_pallet_dmp_queue, helpers::weight_within_threshold,
-	AccountId32Junction, Chain, ParaId, Parachain as Para, RelayChain as Relay, Test, TestArgs,
-	TestContext, TestExt, TestExternalities,
+	assert_expected_events, bx, helpers::weight_within_threshold, Chain, Parachain as Para,
+	RelayChain as Relay, Test, TestArgs, TestContext, TestExt,
 };
 
 pub const ASSET_ID: u32 = 1;
