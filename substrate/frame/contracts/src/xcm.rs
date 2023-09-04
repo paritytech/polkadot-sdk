5983dbd10c37ad510bbd7c840c507eec5b3e652f
pgherveou <pgherveou@gmail.com>
2023-09-04 18:02:48 +0200

    move stuff to pallet_xcm_extension

--- substrate/frame/contracts/src/xcm.rs
+++ substrate/frame/contracts/src/xcm.rs
@@ -1,12 +1,14 @@
-use crate::{AccountIdOf, Config, Error, RawOrigin};
+use crate::{AccountIdOf, Config, Error};
 use codec::{Decode, Encode, MaxEncodedLen};
 use frame_support::{pallet_prelude::DispatchResultWithPostInfo, weights::Weight};
 use frame_system::pallet_prelude::BlockNumberFor;
-use pallet_xcm::WeightInfo as XcmWeightInfo;
 use sp_runtime::{DispatchError, DispatchResult};
-use xcm::{v3::MultiLocation, VersionedMultiLocation, VersionedXcm};
-use xcm_executor::traits::{QueryHandler, QueryResponseStatus};
+use xcm::{VersionedMultiLocation, VersionedXcm};
+use xcm_executor::traits::{QueryResponseStatus};
 pub type CallOf<T> = <T as frame_system::Config>::RuntimeCall;
+
+// TODO move behind feature flag
+mod pallet_xcm_extension;
 
 pub trait XCM<T: Config> {
 	type QueryId: Encode + Decode + MaxEncodedLen;
@@ -106,72 +108,3 @@ impl WeightInfo for NoopXcmConfig {
 	}
 }
 
-/// TODO use feature flag to compile this only when xcm is enabled
-impl<T> WeightInfo for T
-where
-	T: pallet_xcm::Config,
-{
-	fn execute() -> Weight {
-		<T as pallet_xcm::Config>::WeightInfo::execute()
-	}
-	fn send() -> Weight {
-		<T as pallet_xcm::Config>::WeightInfo::send()
-	}
-	fn query() -> Weight {
-		<T as pallet_xcm::Config>::WeightInfo::new_query()
-	}
-	fn take_response() -> Weight {
-		<T as pallet_xcm::Config>::WeightInfo::take_response()
-	}
-}
-
-impl<T: Config> XCM<T> for T
-where
-	T: pallet_xcm::Config,
-{
-	type QueryId = <pallet_xcm::Pallet<T> as QueryHandler>::QueryId;
-	type WeightInfo = T;
-
-	fn execute(
-		origin: &AccountIdOf<T>,
-		message: VersionedXcm<CallOf<T>>,
-		max_weight: Weight,
-	) -> DispatchResultWithPostInfo {
-		let origin = RawOrigin::Signed(origin.clone()).into();
-		pallet_xcm::Pallet::<T>::execute(origin, Box::new(message), max_weight)
-	}
-	fn send(
-		origin: &AccountIdOf<T>,
-		dest: VersionedMultiLocation,
-		msg: VersionedXcm<()>,
-	) -> DispatchResult {
-		let origin = RawOrigin::Signed(origin.clone()).into();
-		pallet_xcm::Pallet::<T>::send(origin, Box::new(dest), Box::new(msg))
-	}
-
-	fn query(
-		origin: &AccountIdOf<T>,
-		timeout: BlockNumberFor<T>,
-		match_querier: VersionedMultiLocation,
-	) -> Result<Self::QueryId, DispatchError> {
-		use frame_support::traits::EnsureOrigin;
-
-		let origin = RawOrigin::Signed(origin.clone()).into();
-		let responder = <T as pallet_xcm::Config>::ExecuteXcmOrigin::ensure_origin(origin)?;
-
-		let query_id = <pallet_xcm::Pallet<T> as QueryHandler>::new_query(
-			responder,
-			timeout.into(),
-			MultiLocation::try_from(match_querier)
-				.map_err(|_| Into::<DispatchError>::into(pallet_xcm::Error::<T>::BadVersion))?,
-		);
-
-		Ok(query_id)
-	}
-
-	fn take_response(query_id: Self::QueryId) -> QueryResponseStatus<BlockNumberFor<T>> {
-		<pallet_xcm::Pallet<T> as QueryHandler>::take_response(query_id)
-	}
-}
-
-// impl
