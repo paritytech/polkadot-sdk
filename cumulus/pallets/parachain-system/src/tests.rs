f2276301fb033ae6ecfebe01b57081ef6c0bb8ac
Sam Johnson <sam@durosoft.com>
2023-09-07 19:41:25 -0400

    add more missing RuntimeTasks

--- cumulus/pallets/parachain-system/src/tests.rs
+++ cumulus/pallets/parachain-system/src/tests.rs
@@ -75,6 +75,7 @@ parameter_types! {
 impl frame_system::Config for Test {
 	type RuntimeOrigin = RuntimeOrigin;
 	type RuntimeCall = RuntimeCall;
+	type RuntimeTask = RuntimeTask;
 	type Nonce = u64;
 	type Hash = H256;
 	type Hashing = BlakeTwo256;
