d41a5571ce93ab3429f9b04fbb85f6f17a1c981c
paritytech-ci <devops-team@parity.io>
2023-09-11 18:40:57 +0000

    ___Updated docs for master___

--- master/polkadot_node_subsystem/gen/metered/sidebar-items.js
+++ master/polkadot_node_subsystem/gen/metered/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["MaybeTimeOfFlight"],"fn":["channel","unbounded"],"mod":["oneshot"],"struct":["CoarseDuration","Meter","MeteredReceiver","MeteredSender","Readout","UnboundedMeteredReceiver","UnboundedMeteredSender"]};
+window.SIDEBAR_ITEMS = {"enum":["MaybeTimeOfFlight","SendError","TrySendError"],"fn":["channel","unbounded"],"mod":["oneshot"],"struct":["CoarseDuration","Meter","MeteredReceiver","MeteredSender","Readout","RecvError","UnboundedMeteredReceiver","UnboundedMeteredSender"]};
