dcc0c03c365d1c347f20514c0043e698a0f0a8d1
paritytech-ci <devops-team@parity.io>
2023-09-11 08:57:04 +0000

    ___Updated docs for master___

--- crate-docs/implementors/bp_polkadot_core/trait.SuffixedCommonSignedExtensionExt.js
+++ crate-docs/implementors/bp_polkadot_core/trait.SuffixedCommonSignedExtensionExt.js
@@ -1,7 +1,7 @@
 (function() {var implementors = {
-"bp_bridge_hub_cumulus":[],
-"bp_bridge_hub_kusama":[],
-"bp_bridge_hub_polkadot":[],
-"bp_bridge_hub_rococo":[],
-"bp_bridge_hub_wococo":[]
+"bp_kusama":[],
+"bp_polkadot":[],
+"bp_polkadot_core":[],
+"bp_rococo":[],
+"bp_wococo":[]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
