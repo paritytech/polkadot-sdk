f83f6b7d089e086123b1defb7efdee5c4d11e026
paritytech-ci <devops-team@parity.io>
2023-09-11 20:55:01 +0000

    ___Updated docs for master___

--- master/implementors/cumulus_pallet_solo_to_para/pallet/trait.Config.js
+++ master/implementors/cumulus_pallet_solo_to_para/pallet/trait.Config.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"seedling_runtime":[["impl <a class=\"trait\" href=\"cumulus_pallet_solo_to_para/pallet/trait.Config.html\" title=\"trait cumulus_pallet_solo_to_para::pallet::Config\">Config</a> for <a class=\"struct\" href=\"seedling_runtime/struct.Runtime.html\" title=\"struct seedling_runtime::Runtime\">Runtime</a>"]]
+"seedling_runtime":[["impl Config for <a class=\"struct\" href=\"seedling_runtime/struct.Runtime.html\" title=\"struct seedling_runtime::Runtime\">Runtime</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
