bab512a049bb3e3add9078e9596fcc649b39523d
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2023-09-12 17:47:43 +0300

    Allow justifications on non-finalized blocks (#1211)
    One assertion was unnecessary because of the check right above it,
    second assertion resolves
    https://github.com/paritytech/polkadot-sdk/issues/1159
    
    ---------
    
    Co-authored-by: Bastian Köcher <git@kchr.de>

--- substrate/client/service/src/client/client.rs
+++ substrate/client/service/src/client/client.rs
@@ -603,8 +603,6 @@ where
 			.block_gap
 			.map_or(false, |(start, _)| *import_headers.post().number() == start);
 
-		assert!(justifications.is_some() && finalized || justifications.is_none() || gap_block);
-
 		// the block is lower than our last finalized block so it must revert
 		// finality, refusing import.
 		if status == blockchain::BlockStatus::Unknown &&
