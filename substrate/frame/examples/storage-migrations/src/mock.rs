222ed3acba8b27d571a2a85d1678cf481f17d042
Liam Aharon <liam.aharon@hotmail.com>
2023-09-18 04:51:00 +1000

    derive_impl

--- substrate/frame/examples/storage-migrations/src/mock.rs
+++ substrate/frame/examples/storage-migrations/src/mock.rs
@@ -16,9 +16,7 @@
 // limitations under the License.
 
 use crate::*;
-use frame_support::{traits::ConstU64, weights::constants::ParityDbWeight};
-use sp_core::H256;
-use sp_runtime::traits::{BlakeTwo256, IdentityLookup};
+use frame_support::{derive_impl, traits::ConstU64};
 
 // Re-export crate as its pallet name for construct_runtime.
 use crate as pallet_example_storage_migration;
@@ -34,30 +32,9 @@ frame_support::construct_runtime!(
 	}
 );
 
+#[derive_impl(frame_system::config_preludes::TestDefaultConfig)]
 impl frame_system::Config for Test {
-	type BaseCallFilter = frame_support::traits::Everything;
-	type BlockWeights = ();
-	type BlockLength = ();
-	type DbWeight = ParityDbWeight;
-	type RuntimeOrigin = RuntimeOrigin;
-	type Nonce = u64;
-	type Hash = H256;
-	type RuntimeCall = RuntimeCall;
-	type Hashing = BlakeTwo256;
-	type AccountId = u64;
-	type Lookup = IdentityLookup<Self::AccountId>;
 	type Block = Block;
-	type RuntimeEvent = RuntimeEvent;
-	type BlockHashCount = ConstU64<250>;
-	type Version = ();
-	type PalletInfo = PalletInfo;
-	type AccountData = pallet_balances::AccountData<u64>;
-	type OnNewAccount = ();
-	type OnKilledAccount = ();
-	type SystemWeightInfo = ();
-	type SS58Prefix = ();
-	type OnSetCode = ();
-	type MaxConsumers = frame_support::traits::ConstU32<16>;
 }
 
 impl pallet_balances::Config for Test {
