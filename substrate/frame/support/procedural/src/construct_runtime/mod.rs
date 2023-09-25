6228074101468fc9b317759645144a7a17362be0
Sam Johnson <sam@durosoft.com>
2023-09-25 14:08:59 -0400

    use proper runtime name in construct_runtime tasks expansion

--- substrate/frame/support/procedural/src/construct_runtime/mod.rs
+++ substrate/frame/support/procedural/src/construct_runtime/mod.rs
@@ -385,7 +385,7 @@ fn construct_runtime_final_expansion(
 	let pallet_to_index = decl_pallet_runtime_setup(&name, &pallets, &scrate);
 
 	let dispatch = expand::expand_outer_dispatch(&name, system_pallet, &pallets, &scrate);
-	let tasks = expand::expand_outer_task(&pallets, &scrate);
+	let tasks = expand::expand_outer_task(&name, &pallets, &scrate);
 	let metadata = expand::expand_runtime_metadata(
 		&name,
 		&pallets,
