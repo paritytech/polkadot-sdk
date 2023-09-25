5e021825343a6d60c0570b8e8a755971066cc3bc
Dmitry Markin <dmitry@markin.tech>
2023-09-25 15:54:01 +0300

    Simplify `PendingResponses` by using `StreamMap`

--- substrate/client/network/sync/src/pending_responses.rs
+++ substrate/client/network/sync/src/pending_responses.rs
@@ -17,18 +17,19 @@
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 //! [`PendingResponses`] is responsible for keeping track of pending responses and
-//! polling them. The [`Stream`] implemented by [`PendingResponses`] never terminates.
+//! polling them.
 
-use futures::{channel::oneshot, future::BoxFuture, stream::Stream, FutureExt};
+use futures::{channel::oneshot, future::BoxFuture, stream::Stream, FutureExt, StreamExt};
 use libp2p::PeerId;
 use log::error;
 use sc_network::request_responses::RequestFailure;
 use sc_network_common::sync::PeerRequest;
 use sp_runtime::traits::Block as BlockT;
 use std::{
-	collections::{HashMap, HashSet, VecDeque},
-	task::{Context, Poll, Waker},
+	pin::Pin,
+	task::{Context, Poll},
 };
+use tokio_stream::StreamMap;
 
 /// Log target for this file.
 const LOG_TARGET: &'static str = "sync";
@@ -46,29 +47,16 @@ pub(crate) struct ResponseEvent<B: BlockT> {
 	pub response: ResponseResult,
 }
 
-/// A future implementing a pending response.
-type ResponseEventFuture<B> = BoxFuture<'static, ResponseEvent<B>>;
-
 /// Stream taking care of polling pending responses.
 pub(crate) struct PendingResponses<B: BlockT> {
 	/// Pending responses
-	pending_responses: HashMap<PeerId, ResponseEventFuture<B>>,
-	/// Finished responses
-	finished_responses: VecDeque<ResponseEvent<B>>,
-	/// Finished responses peers
-	finished_peer_ids: HashSet<PeerId>,
-	/// Waker to ensure wake-up once a new pending response is added.
-	waker: Option<Waker>,
+	pending_responses:
+		StreamMap<PeerId, Pin<Box<dyn Stream<Item = (PeerRequest<B>, ResponseResult)>>>>,
 }
 
 impl<B: BlockT> PendingResponses<B> {
 	pub fn new() -> Self {
-		Self {
-			pending_responses: HashMap::new(),
-			finished_responses: VecDeque::new(),
-			finished_peer_ids: HashSet::new(),
-			waker: None,
-		}
+		Self { pending_responses: StreamMap::new() }
 	}
 
 	pub fn insert(
@@ -81,8 +69,7 @@ impl<B: BlockT> PendingResponses<B> {
 
 		if let Some(_) = self.pending_responses.insert(
 			peer_id,
-			async move { ResponseEvent { peer_id, request, response: response_future.await } }
-				.boxed(),
+			Box::pin(async move { (request, response_future.await) }.into_stream()),
 		) {
 			error!(
 				target: LOG_TARGET,
@@ -90,66 +77,14 @@ impl<B: BlockT> PendingResponses<B> {
 			);
 			debug_assert!(false);
 		}
-
-		if self.finished_peer_ids.remove(&peer_id) {
-			// This must not happen unless there is a logic error in the code,
-			// so performance penalty is not an issue.
-			self.finished_responses.retain(|event| event.peer_id != peer_id);
-
-			error!(
-				target: LOG_TARGET,
-				"Discarded finished response from peer {peer_id}, {request_type:?}.",
-			);
-			debug_assert!(false);
-		}
-
-		if let Some(waker) = self.waker.take() {
-			waker.wake();
-		}
 	}
 
 	pub fn remove(&mut self, peer_id: &PeerId) -> bool {
-		match self.pending_responses.remove(peer_id) {
-			Some(_) => true,
-			None => {
-				// then we must look in finished responses
-				if self.finished_peer_ids.remove(peer_id) {
-					// We do a linear search here. With 10 ms roud-trip network latency and 100
-					// active peers this leads to a penalty of up to 10000 `PeerId` comparisons per
-					// second. Moreover, we remove an element from a `VecDeque`, what leads to a
-					// penalty proportional to the poll iteration duration (and hence the number of
-					// elements in a `VecDeque`).
-					// TODO: probably optimize this.
-
-					let mut remove_index = None;
-
-					for (i, event) in self.finished_responses.iter().enumerate() {
-						if &event.peer_id == peer_id {
-							remove_index = Some(i);
-							break
-						}
-					}
-
-					if let Some(index) = remove_index {
-						self.finished_responses.remove(index);
-						true
-					} else {
-						error!(
-							target: LOG_TARGET,
-							"Logic error: {peer_id} is in `finished_peer_ids`, but not in `finished_responses`.",
-						);
-						debug_assert!(false);
-						false
-					}
-				} else {
-					false
-				}
-			},
-		}
+		self.pending_responses.remove(peer_id).is_some()
 	}
 
 	pub fn len(&self) -> usize {
-		self.pending_responses.len() + self.finished_responses.len()
+		self.pending_responses.len()
 	}
 }
 
@@ -162,32 +97,12 @@ impl<B: BlockT> Stream for PendingResponses<B> {
 		mut self: std::pin::Pin<&mut Self>,
 		cx: &mut Context<'_>,
 	) -> Poll<Option<Self::Item>> {
-		let ready_responses = self
-			.pending_responses
-			.values_mut()
-			.filter_map(|future| match future.poll_unpin(cx) {
-				Poll::Pending => None,
-				Poll::Ready(event) => Some(event),
-			})
-			.collect::<Vec<_>>();
-
-		for ResponseEvent { peer_id, .. } in ready_responses.iter() {
-			self.pending_responses
-				.remove(&peer_id)
-				.expect("Logic error: peer id from pending response is missing in the map.");
-			self.finished_peer_ids.insert(*peer_id);
-		}
-
-		self.finished_responses.extend(ready_responses.into_iter());
-
-		if let Some(event) = self.finished_responses.pop_front() {
-			self.finished_peer_ids.remove(&event.peer_id);
-
-			Poll::Ready(Some(event))
+		if let Some((peer_id, (request, response))) =
+			futures::ready!(self.pending_responses.poll_next_unpin(cx))
+		{
+			Poll::Ready(Some(ResponseEvent { peer_id, request, response }))
 		} else {
-			self.waker = Some(cx.waker().clone());
-
-			Poll::Pending
+			Poll::Ready(None)
 		}
 	}
 }
