286fd7986b1f10df228038024d761d558e3e7cde
Xavier Lau <xavier@inv.cafe>
2023-09-26 11:00:24 +0800

    Fix

--- substrate/bin/node/runtime/src/lib.rs
+++ substrate/bin/node/runtime/src/lib.rs
@@ -1288,7 +1288,7 @@ impl pallet_tips::Config for Runtime {
 	type TipCountdown = TipCountdown;
 	type TipFindersFee = TipFindersFee;
 	type TipReportDepositBase = TipReportDepositBase;
-	type MaxTipAmount = ConstU32<{ 500 * DOLLARS }>;
+	type MaxTipAmount = ConstU128<{ 500 * DOLLARS }>;
 	type WeightInfo = pallet_tips::weights::SubstrateWeight<Runtime>;
 }
 
