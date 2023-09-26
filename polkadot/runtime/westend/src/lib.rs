c1c66a65973ed4efc1307bf5221e6d8bca3dd10e
al3mart <11448715+al3mart@users.noreply.github.com>
2023-09-26 15:15:34 +0200

    bump

--- polkadot/runtime/westend/src/lib.rs
+++ polkadot/runtime/westend/src/lib.rs
@@ -129,7 +129,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("westend"),
 	impl_name: create_runtime_str!("parity-westend"),
 	authoring_version: 2,
-	spec_version: 1010,
+	spec_version: 1020,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 22,
