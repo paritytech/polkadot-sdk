0c5980fec48137e18fc2240545b2fb8f68ef2ff5
Francisco Aguirre <franciscoaguirreperez@gmail.com>
2023-09-27 07:01:54 -0300

    Fix license issue

--- cumulus/parachains/integration-tests/emulated/collectives/collectives-polkadot/src/xcm_helpers.rs
+++ cumulus/parachains/integration-tests/emulated/collectives/collectives-polkadot/src/xcm_helpers.rs
@@ -1,3 +1,21 @@
+// Copyright Parity Technologies (UK) Ltd.
+// This file is part of Cumulus.
+
+// Cumulus is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Cumulus is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Cumulus.  If not, see <http://www.gnu.org/licenses/>.
+
+//! XCM helpers for getting delivery fees for tests
+
 use polkadot_runtime_common::xcm_sender::PriceForParachainDelivery;
 use xcm::latest::prelude::*;
 
