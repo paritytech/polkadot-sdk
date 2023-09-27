f28e01ed587193c19c400cdce9e62a2a9d4d9f5f
alindima <alin@parity.io>
2023-09-27 18:00:39 +0300

    add v10 config migration to westend

--- polkadot/runtime/westend/src/lib.rs
+++ polkadot/runtime/westend/src/lib.rs
@@ -1508,6 +1508,7 @@ pub mod migrations {
 		parachains_configuration::migration::v9::MigrateToV9<Runtime>,
 		paras_registrar::migration::VersionCheckedMigrateToV1<Runtime, ()>,
 		pallet_referenda::migration::v1::MigrateV0ToV1<Runtime, ()>,
+		parachains_configuration::migration::v10::MigrateToV10<Runtime>,
 	);
 }
 
