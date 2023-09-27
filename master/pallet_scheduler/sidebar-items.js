3d68d4e690b4c2b29063f7cae6215df709447730
paritytech-ci <devops-team@parity.io>
2023-09-27 12:52:30 +0000

    ___Updated docs for master___

--- master/pallet_scheduler/sidebar-items.js
+++ master/pallet_scheduler/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"mod":["migration","pallet","weights"],"struct":["Scheduled"],"type":["CallOrHashOf","PeriodicIndex","ScheduledOf","ScheduledV2Of","ScheduledV3Of","TaskAddress"]};
+window.SIDEBAR_ITEMS = {"mod":["migration","pallet","weights"],"struct":["Scheduled"],"type":["BoundedCallOf","CallOrHashOf","PeriodicIndex","ScheduledOf","ScheduledV2Of","ScheduledV3Of","TaskAddress"]};
