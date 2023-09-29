5a20a18a2a6eb69bcb9463a29fcb302a7ee27153
paritytech-ci <devops-team@parity.io>
2023-09-29 15:58:50 +0000

    ___Updated docs for master___

--- master/pallet_nomination_pools/migration/sidebar-items.js
+++ master/pallet_nomination_pools/migration/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"mod":["v1","v2","v3","v4","v5"]};
+window.SIDEBAR_ITEMS = {"mod":["v1","v2","v3","v4","v5","versioned_migrations"]};
---
+++ master/pallet_nomination_pools/migration/versioned_migrations/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"type":["V5toV6"]};
