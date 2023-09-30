19f205f07eed02b3c0503fa54c4ebb71a9a1a382
paritytech-ci <devops-team@parity.io>
2023-09-30 05:01:55 +0000

    ___Updated docs for master___

--- master/implementors/pallet_treasury/trait.SpendFunds.js
+++ master/implementors/pallet_treasury/trait.SpendFunds.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
-"pallet_bounties":[["impl&lt;T: <a class=\"trait\" href=\"pallet_bounties/pallet/trait.Config.html\" title=\"trait pallet_bounties::pallet::Config\">Config</a>&lt;I&gt;, I: 'static&gt; <a class=\"trait\" href=\"pallet_treasury/trait.SpendFunds.html\" title=\"trait pallet_treasury::SpendFunds\">SpendFunds</a>&lt;T, I&gt; for <a class=\"struct\" href=\"pallet_bounties/pallet/struct.Pallet.html\" title=\"struct pallet_bounties::pallet::Pallet\">Pallet</a>&lt;T, I&gt;"]],
+"pallet_bounties":[["impl&lt;T: <a class=\"trait\" href=\"pallet_bounties/pallet/trait.Config.html\" title=\"trait pallet_bounties::pallet::Config\">Config</a>&lt;I&gt;, I: 'static&gt; SpendFunds&lt;T, I&gt; for <a class=\"struct\" href=\"pallet_bounties/pallet/struct.Pallet.html\" title=\"struct pallet_bounties::pallet::Pallet\">Pallet</a>&lt;T, I&gt;"]],
 "pallet_treasury":[]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
