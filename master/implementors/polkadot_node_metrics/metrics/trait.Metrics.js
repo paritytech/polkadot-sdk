5543234b8c085ade692fb33b112a528865ac82bc
paritytech-ci <devops-team@parity.io>
2023-10-10 14:51:32 +0000

    ___Updated docs for master___

--- master/implementors/polkadot_node_metrics/metrics/trait.Metrics.js
+++ master/implementors/polkadot_node_metrics/metrics/trait.Metrics.js
@@ -5,7 +5,7 @@
 "polkadot_node_core_approval_voting":[["impl <a class=\"trait\" href=\"polkadot_node_metrics/metrics/trait.Metrics.html\" title=\"trait polkadot_node_metrics::metrics::Metrics\">Metrics</a> for <a class=\"struct\" href=\"polkadot_node_core_approval_voting/struct.Metrics.html\" title=\"struct polkadot_node_core_approval_voting::Metrics\">Metrics</a>"]],
 "polkadot_node_core_av_store":[["impl <a class=\"trait\" href=\"polkadot_node_metrics/metrics/trait.Metrics.html\" title=\"trait polkadot_node_metrics::metrics::Metrics\">Metrics</a> for <a class=\"struct\" href=\"polkadot_node_core_av_store/struct.Metrics.html\" title=\"struct polkadot_node_core_av_store::Metrics\">Metrics</a>"]],
 "polkadot_node_core_provisioner":[["impl <a class=\"trait\" href=\"polkadot_node_metrics/metrics/trait.Metrics.html\" title=\"trait polkadot_node_metrics::metrics::Metrics\">Metrics</a> for <a class=\"struct\" href=\"polkadot_node_core_provisioner/struct.Metrics.html\" title=\"struct polkadot_node_core_provisioner::Metrics\">Metrics</a>"]],
-"polkadot_node_core_pvf":[["impl Metrics for <a class=\"struct\" href=\"polkadot_node_core_pvf/struct.Metrics.html\" title=\"struct polkadot_node_core_pvf::Metrics\">Metrics</a>"]],
+"polkadot_node_core_pvf":[["impl <a class=\"trait\" href=\"polkadot_node_metrics/metrics/trait.Metrics.html\" title=\"trait polkadot_node_metrics::metrics::Metrics\">Metrics</a> for <a class=\"struct\" href=\"polkadot_node_core_pvf/struct.Metrics.html\" title=\"struct polkadot_node_core_pvf::Metrics\">Metrics</a>"]],
 "polkadot_node_metrics":[],
 "polkadot_node_subsystem":[],
 "polkadot_node_subsystem_util":[],
