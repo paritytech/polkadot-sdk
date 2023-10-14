87de0278ebd62cbddd6803e4452135372ec449e0
Juan <juangirini@gmail.com>
2023-10-14 08:26:19 +0200

    Macros to use path instead of ident (#1474)

--- substrate/frame/support/test/stg_frame_crate/frame/src/lib.rs
+++ substrate/frame/support/test/stg_frame_crate/frame/src/lib.rs
@@ -15,30 +15,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#[frame_support::pallet]
-mod pallet {
-	use frame_support::pallet_prelude::Hooks;
-	use frame_system::pallet_prelude::BlockNumberFor;
-
-	#[pallet::config]
-	pub trait Config: frame_system::Config {
-		type Bar;
-		type RuntimeEvent;
-	}
-
-	#[pallet::pallet]
-	pub struct Pallet<T>(core::marker::PhantomData<T>);
-
-	#[pallet::hooks]
-	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {}
-
-	#[pallet::call]
-	impl<T: Config> Pallet<T> {}
-
-	#[pallet::event]
-	pub enum Event<T: Config> {
-		B { b: T::Bar },
-	}
+pub mod deps {
+	pub use frame_support;
+	pub use frame_system;
 }
-
-fn main() {}
