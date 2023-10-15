fa87f2aa90f8b48baa7f0a71cfc1598b2bb757a9
Branislav Kontur <bkontur@gmail.com>
2023-10-15 03:07:37 +0200

    Removed unnecessary import

--- cumulus/parachains/runtimes/assets/common/src/matching.rs
+++ cumulus/parachains/runtimes/assets/common/src/matching.rs
@@ -14,10 +14,7 @@
 // limitations under the License.
 
 use cumulus_primitives_core::ParaId;
-use frame_support::{
-	pallet_prelude::Get,
-	traits::{Contains, ContainsPair},
-};
+use frame_support::{pallet_prelude::Get, traits::ContainsPair};
 use xcm::{
 	latest::prelude::{MultiAsset, MultiLocation},
 	prelude::*,
