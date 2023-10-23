7644bb6aba5f312a178079d867f523b8ba3d9f8b
Marcin S. <marcin@realemail.net>
2023-10-23 15:43:24 +0200

    Remove bad execute benchmark

--- polkadot/node/core/pvf/benches/host_prepare_rococo_runtime.rs
+++ polkadot/node/core/pvf/benches/host_prepare_rococo_runtime.rs
@@ -14,13 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Polkadot.  If not, see <http://www.gnu.org/licenses/>.
 
-//! Benchmarks for preparation and execution through the host.
-//!
-//! # Examples
-//!
-//! $ cargo bench
-//! $ cargo bench prepare
-//! $ cargo bench execute
+//! Benchmarks for preparation through the host. We use a real PVF to get realistic results.
 
 use criterion::{criterion_group, criterion_main, BatchSize, Criterion, SamplingMode};
 use parity_scale_codec::Encode;
@@ -30,6 +24,7 @@ use polkadot_node_core_pvf::{
 };
 use polkadot_parachain_primitives::primitives::{BlockData, ValidationParams, ValidationResult};
 use polkadot_primitives::ExecutorParams;
+use rococo_runtime::WASM_BINARY;
 use std::time::Duration;
 use tokio::{runtime::Handle, sync::Mutex};
 
@@ -86,37 +81,6 @@ impl TestHost {
 			.unwrap();
 		result_rx.await.unwrap()
 	}
-
-	async fn validate_candidate(
-		&self,
-		code: &[u8],
-		params: ValidationParams,
-		executor_params: ExecutorParams,
-	) -> Result<ValidationResult, ValidationError> {
-		let (result_tx, result_rx) = futures::channel::oneshot::channel();
-
-		let code = sp_maybe_compressed_blob::decompress(code, 16 * 1024 * 1024)
-			.expect("Compression works");
-
-		self.host
-			.lock()
-			.await
-			.execute_pvf(
-				PvfPrepData::from_code(
-					code.into(),
-					executor_params,
-					TEST_PREPARATION_TIMEOUT,
-					PrepareJobKind::Compilation,
-				),
-				TEST_EXECUTION_TIMEOUT,
-				params.encode(),
-				polkadot_node_core_pvf::Priority::Normal,
-				result_tx,
-			)
-			.await
-			.unwrap();
-		result_rx.await.unwrap()
-	}
 }
 
 fn host_prepare_rococo_runtime(c: &mut Criterion) {
@@ -124,7 +88,7 @@ fn host_prepare_rococo_runtime(c: &mut Criterion) {
 
 	let rt = tokio::runtime::Runtime::new().unwrap();
 
-	let blob = rococo_runtime::WASM_BINARY.unwrap();
+	let blob = WASM_BINARY.expect("You need to build the WASM binaries to run the tests!");
 	let pvf = match sp_maybe_compressed_blob::decompress(&blob, 64 * 1024 * 1024) {
 		Ok(code) => PvfPrepData::from_code(
 			code.into_owned(),
@@ -162,59 +126,5 @@ fn host_prepare_rococo_runtime(c: &mut Criterion) {
 	group.finish();
 }
 
-fn host_execute_rococo_runtime(c: &mut Criterion) {
-	polkadot_node_core_pvf_common::sp_tracing::try_init_simple();
-
-	let rt = tokio::runtime::Runtime::new().unwrap();
-
-	let host = TestHost::new_with_config(rt.handle(), |cfg| {
-		cfg.execute_workers_max_num = 1;
-	});
-
-	let blob = rococo_runtime::WASM_BINARY.unwrap();
-	let pvf = match sp_maybe_compressed_blob::decompress(&blob, 64 * 1024 * 1024) {
-		Ok(code) => PvfPrepData::from_code(
-			code.into_owned(),
-			ExecutorParams::default(),
-			Duration::from_secs(360),
-			PrepareJobKind::Compilation,
-		),
-		Err(e) => {
-			panic!("Cannot decompress blob: {:?}", e);
-		},
-	};
-
-	// Prepare beforehand, so that the benchmark only measures execution.
-	rt.block_on(async {
-		let _stats = host.precheck_pvf(&pvf.code(), Default::default()).await.unwrap();
-	});
-
-	let mut group = c.benchmark_group("execute rococo");
-	group.sampling_mode(SamplingMode::Flat);
-	group.sample_size(20);
-	group.measurement_time(Duration::from_secs(240));
-	group.bench_function("host: prepare Rococo runtime", |b| {
-		b.to_async(&rt).iter(|| async {
-			// `PvfPrepData` is designed to be cheap to clone, so cloning shouldn't affect the
-			// benchmark accuracy.
-			let _result = host
-				.validate_candidate(
-					&pvf.code(),
-					ValidationParams {
-						block_data: BlockData(Vec::new()),
-						parent_head: Default::default(),
-						relay_parent_number: 1,
-						relay_parent_storage_root: Default::default(),
-					},
-					Default::default(),
-				)
-				.await
-				.unwrap();
-		})
-	});
-	group.finish();
-}
-
 criterion_group!(prepare, host_prepare_rococo_runtime);
-criterion_group!(execute, host_execute_rococo_runtime);
-criterion_main!(prepare, execute);
+criterion_main!(prepare);
