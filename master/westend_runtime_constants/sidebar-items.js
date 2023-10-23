b7be050fee6b85edc47e4009daadb0806a769d10
paritytech-ci <devops-team@parity.io>
2023-10-23 10:29:27 +0000

    ___Updated docs for master___

--- master/westend_runtime_constants/sidebar-items.js
+++ master/westend_runtime_constants/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"mod":["currency","fee","system_parachain","time","weights","xcm"]};
+window.SIDEBAR_ITEMS = {"constant":["TREASURY_PALLET_ID"],"mod":["currency","fee","system_parachain","time","weights","xcm"]};
