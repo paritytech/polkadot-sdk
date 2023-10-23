1ec54ad122cfc4dcbc32a30ea79e634aaec16ee1
Bastian Köcher <info@kchr.de>
2023-10-23 15:22:05 +0200

    Always schedule at least one job onto a core
    Even if the host configuration is returning `0` for the `lookahead`, we
    should schedule at least one job on a core if the core exists.

--- polkadot/runtime/parachains/src/scheduler.rs
+++ polkadot/runtime/parachains/src/scheduler.rs
@@ -631,7 +631,8 @@ impl<T: Config> Pallet<T> {
 		if ValidatorGroups::<T>::get().is_empty() {
 			return
 		}
-		let n_lookahead = Self::claimqueue_lookahead();
+		// If there exists a core, ensure we schedule at least one job onto it.
+		let n_lookahead = Self::claimqueue_lookahead().max(1);
 		let n_session_cores = T::AssignmentProvider::session_core_count();
 		let cq = ClaimQueue::<T>::get();
 		let ttl = <configuration::Pallet<T>>::config().on_demand_ttl;
