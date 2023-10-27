45c4a6f23393244f2c3e03c337974c60fdbf2c53
Nikhil Gupta <17176722+gupnik@users.noreply.github.com>
2023-10-27 19:09:58 +0530

    Fixes bench build

--- substrate/frame/system/benches/bench.rs
+++ substrate/frame/system/benches/bench.rs
@@ -81,6 +81,7 @@ impl frame_system::Config for Runtime {
 	type SS58Prefix = ();
 	type OnSetCode = ();
 	type MaxConsumers = ConstU32<16>;
+	type RuntimeTask = RuntimeTask;
 }
 
 impl module::Config for Runtime {
