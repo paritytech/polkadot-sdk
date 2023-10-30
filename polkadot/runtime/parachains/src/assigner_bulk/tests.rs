d7e8081e10b0f4cba3b6b9589f583f650e5f18a6
BradleyOlson64 <lotrftw9@gmail.com>
2023-10-30 14:49:21 -0700

    Stub out assign_core_test

--- polkadot/runtime/parachains/src/assigner_bulk/tests.rs
+++ polkadot/runtime/parachains/src/assigner_bulk/tests.rs
@@ -89,3 +89,9 @@ fn after_assign_core_workload_is_some() {}
 
 #[test]
 fn end_hint_always_points_to_next_work_plan_item() {}
+
+// Invariants: We assume that Workplan is append only and consumed. In other words new schedules
+// inserted for a core must have a higher block number than all of the already existing
+// schedules.
+#[test]
+fn assign_core_enforces_higher_block_number() {}
