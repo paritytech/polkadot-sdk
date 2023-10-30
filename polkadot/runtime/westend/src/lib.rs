26545075edbb1caf714d4e39d3ac37e2b11d96e2
Adrian Catangiu <adrian@parity.io>
2023-10-30 16:10:21 +0200

    bump rococo & westend spec_version

--- polkadot/runtime/westend/src/lib.rs
+++ polkadot/runtime/westend/src/lib.rs
@@ -122,7 +122,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("westend"),
 	impl_name: create_runtime_str!("parity-westend"),
 	authoring_version: 2,
-	spec_version: 102000,
+	spec_version: 102001,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 23,
