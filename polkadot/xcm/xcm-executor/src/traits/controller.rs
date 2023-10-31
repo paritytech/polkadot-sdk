0ae8763f6f4537a9a38da92ce54dfb56e8f54fa5
pgherveou <pgherveou@gmail.com>
2023-10-31 16:21:37 +0100

    fix doc

--- polkadot/xcm/xcm-executor/src/traits/controller.rs
+++ polkadot/xcm/xcm-executor/src/traits/controller.rs
@@ -52,7 +52,7 @@ pub trait ExecuteControllerWeightInfo {
 /// An implementation of that trait will handle the low-level details of the execution, such as:
 /// - Validating and Converting the origin to a MultiLocation.
 /// - Handling versioning.
-/// - Calling  the internal executor, which implements [`ExecuteXcm`].
+/// - Calling  the internal executor, which implements [`crate::ExecuteXcm`].
 pub trait ExecuteController<Origin, RuntimeCall> {
 	/// Weight information for ExecuteController functions.
 	type WeightInfo: ExecuteControllerWeightInfo;
@@ -83,7 +83,7 @@ pub trait SendControllerWeightInfo {
 /// as:
 /// - Validating and Converting the origin to an interior location.
 /// - Handling versioning.
-/// - Calling  the internal router, which implements [`SendXcm`].
+/// - Calling  the internal router, which implements [`crate::SendXcm`].
 pub trait SendController<Origin> {
 	/// Weight information for SendController functions.
 	type WeightInfo: SendControllerWeightInfo;
