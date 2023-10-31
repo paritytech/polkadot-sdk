13fc3668b2b92bf084bb48f07dd0e68b86862dc1
Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
2023-10-31 17:37:46 +0100

    Update cumulus/parachains/runtimes/collectives/collectives-polkadot/src/lib.rs
    Co-authored-by: joe petrowski <25483142+joepetrowski@users.noreply.github.com>

--- cumulus/parachains/runtimes/collectives/collectives-polkadot/src/lib.rs
+++ cumulus/parachains/runtimes/collectives/collectives-polkadot/src/lib.rs
@@ -722,7 +722,6 @@ type Migrations = (
 	import_kusama_fellowship::Migration<Runtime, FellowshipCollectiveInstance>,
 	// unreleased
 	pallet_collator_selection::migration::v1::MigrateToV1<Runtime>,
-	// unreleased
 );
 
 /// Executive: handles dispatch to the various modules.
