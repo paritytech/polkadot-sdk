b6965af493eaa277bf7983201311362cf62ce5a9
Kevin Krone <kevin@parity.io>
2023-11-01 16:28:02 +0100

    Improve FRAME storage docs (#1714)
    This is a port (and hopefully a small improvement) of @kianenigma's PR
    from the old Substrate repo:
    https://github.com/paritytech/substrate/pull/13987. Following #1689 I
    moved the documentation of all macros relevant to this PR from
    `frame_support_procedural` to `pallet_macros` while including a hint for
    RA users.
    
    Question: Again with respect to #1689: Is there a good reason why we
    should *not* enhance paths with links to our current rustdocs? For
    example, instead of
    ```rust
    /// **Rust-Analyzer users**: See the documentation of the Rust item in
    /// `frame_support::pallet_macros::storage`.
    ```
    we could write
    ```rust
    /// **Rust-Analyzer users**: See the documentation of the Rust item in
    /// [`frame_support::pallet_macros::storage`](https://paritytech.github.io/polkadot-sdk/master/frame_support/pallet_macros/attr.storage.html).
    ```
    This results in a clickable link like this:
    <img width="674" alt="image"
    src="https://github.com/paritytech/polkadot-sdk/assets/10713977/c129e622-3942-4eeb-8acf-93ee4efdc99d">
    I don't really expect the links to become outdated any time soon, but I
    think this would be a great UX improvement over just having paths.
    
    TODOs:
    - [ ] Add documentation for `constant_name` macro
    - [x] Add proper documentation for different `QueryKinds`, i.e.
    `OptionQuery`, `ValueQuery`, `ResultQuery`. One example for each. Custom
    `OnEmpty` should be moved to `QueryKinds` trait doc page.
    - [ ] Rework `type_value` docs
    
    ---------
    
    Co-authored-by: kianenigma <kian@parity.io>

--- substrate/frame/support/src/storage/types/counted_map.rs
+++ substrate/frame/support/src/storage/types/counted_map.rs
@@ -35,8 +35,8 @@ use sp_metadata_ir::StorageEntryMetadataIR;
 use sp_runtime::traits::Saturating;
 use sp_std::prelude::*;
 
-/// A wrapper around a `StorageMap` and a `StorageValue<Value=u32>` to keep track of how many items
-/// are in a map, without needing to iterate all the values.
+/// A wrapper around a [`StorageMap`] and a [`StorageValue`] (with the value being `u32`) to keep
+/// track of how many items are in a map, without needing to iterate all the values.
 ///
 /// This storage item has additional storage read and write overhead when manipulating values
 /// compared to a regular storage map.
@@ -47,6 +47,51 @@ use sp_std::prelude::*;
 ///
 /// Whenever the counter needs to be updated, an additional read and write occurs to update that
 /// counter.
+///
+/// The total number of items currently stored in the map can be retrieved with the
+/// [`CountedStorageMap::count`] method.
+///
+/// For general information regarding the `#[pallet::storage]` attribute, refer to
+/// [`crate::pallet_macros::storage`].
+///
+/// # Examples
+///
+/// Declaring a counted map:
+///
+/// ```
+/// #[frame_support::pallet]
+/// mod pallet {
+/// # 	use frame_support::pallet_prelude::*;
+/// # 	#[pallet::config]
+/// # 	pub trait Config: frame_system::Config {}
+/// # 	#[pallet::pallet]
+/// # 	pub struct Pallet<T>(_);
+/// 	/// A kitchen-sink CountedStorageMap, with all possible additional attributes.
+///     #[pallet::storage]
+/// 	#[pallet::getter(fn foo)]
+/// 	#[pallet::storage_prefix = "OtherFoo"]
+/// 	#[pallet::unbounded]
+///     pub type Foo<T> = CountedStorageMap<
+/// 		_,
+/// 		Blake2_128Concat,
+/// 		u32,
+/// 		u32,
+/// 		ValueQuery,
+/// 	>;
+///
+/// 	/// Alternative named syntax.
+/// 	#[pallet::storage]
+///     pub type Bar<T> = CountedStorageMap<
+/// 		Hasher = Blake2_128Concat,
+/// 		Key = u32,
+/// 		Value = u32,
+/// 		QueryKind = ValueQuery
+/// 	>;
+/// }
+/// ```
+///
+/// Using a counted map in action:
+#[doc = docify::embed!("src/storage/types/counted_map.rs", test_simple_count_works)]
 pub struct CountedStorageMap<
 	Prefix,
 	Hasher,
@@ -1172,5 +1217,16 @@ mod test {
 				},
 			]
 		);
+	}
+
+	#[docify::export]
+	#[test]
+	fn test_simple_count_works() {
+		type FooCountedMap = CountedStorageMap<Prefix, Twox64Concat, u16, u32>;
+		TestExternalities::default().execute_with(|| {
+			FooCountedMap::insert(1, 1);
+			FooCountedMap::insert(2, 2);
+			assert_eq!(FooCountedMap::count(), 2);
+		});
 	}
 }
