773fee801504e108b31cbdeca0a373d1608a83f4
Branislav Kontur <bkontur@gmail.com>
2023-11-01 22:31:11 +0100

    Change spec_version to `1_003_000`

--- cumulus/parachains/runtimes/bridge-hubs/bridge-hub-rococo/src/lib.rs
+++ cumulus/parachains/runtimes/bridge-hubs/bridge-hub-rococo/src/lib.rs
@@ -192,7 +192,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("bridge-hub-rococo"),
 	impl_name: create_runtime_str!("bridge-hub-rococo"),
 	authoring_version: 1,
-	spec_version: 10000,
+	spec_version: 1_003_000,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 3,
