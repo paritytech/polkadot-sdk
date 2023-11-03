a8b4f6b7813aba0dd32cd1454b4024f2dd4e0f14
Davide Galassi <davxy@datawok.net>
2023-11-03 15:27:03 +0100

    Restore master template

--- substrate/bin/node-template/node/src/service.rs
+++ substrate/bin/node-template/node/src/service.rs
@@ -15,21 +15,13 @@ use std::{sync::Arc, time::Duration};
 // Our native executor instance.
 pub struct ExecutorDispatch;
 
-#[cfg(feature = "runtime-benchmarks")]
-type ExtendedHostFunctions = (
-	sp_crypto_ec_utils::bls12_381::host_calls::HostFunctions,
-	sp_crypto_ec_utils::ed_on_bls12_381_bandersnatch::host_calls::HostFunctions,
-	frame_benchmarking::benchmarking::HostFunctions,
-);
-
-#[cfg(not(feature = "runtime-benchmarks"))]
-type ExtendedHostFunctions = (
-	sp_crypto_ec_utils::bls12_381::host_calls::HostFunctions,
-	sp_crypto_ec_utils::ed_on_bls12_381_bandersnatch::host_calls::HostFunctions,
-);
-
 impl sc_executor::NativeExecutionDispatch for ExecutorDispatch {
-	type ExtendHostFunctions = ExtendedHostFunctions;
+	/// Only enable the benchmarking host functions when we actually want to benchmark.
+	#[cfg(feature = "runtime-benchmarks")]
+	type ExtendHostFunctions = frame_benchmarking::benchmarking::HostFunctions;
+	/// Otherwise we only use the default Substrate host functions.
+	#[cfg(not(feature = "runtime-benchmarks"))]
+	type ExtendHostFunctions = ();
 
 	fn dispatch(method: &str, data: &[u8]) -> Option<Vec<u8>> {
 		node_template_runtime::api::dispatch(method, data)
