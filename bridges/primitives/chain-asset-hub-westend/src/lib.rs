2ca8b3dfe497957b0a403c86aa108e7708b13c52
Svyatoslav Nikolsky <svyatonik@gmail.com>
2023-11-03 11:32:41 +0300

    [testnet] Allow governance to control fees for Rococo <> Westend bridge (#2139)
    Right now governance could only control byte-fee component of Rococo <>
    Westend message fees (paid at Asset Hubs). This PR changes it a bit:
    1) governance now allowed to control both fee components - byte fee and
    base fee;
    2) base fee now includes cost of "default" delivery and confirmation
    transactions, in addition to `ExportMessage` instruction cost.

--- bridges/primitives/chain-asset-hub-westend/src/lib.rs
+++ bridges/primitives/chain-asset-hub-westend/src/lib.rs
@@ -42,10 +42,6 @@ pub enum Call {
 frame_support::parameter_types! {
 	/// Some sane weight to execute `xcm::Transact(pallet-xcm-bridge-hub-router::Call::report_bridge_status)`.
 	pub const XcmBridgeHubRouterTransactCallMaxWeight: frame_support::weights::Weight = frame_support::weights::Weight::from_parts(200_000_000, 6144);
-
-	/// Base delivery fee to `BridgeHubWestend`.
-	/// (initially was calculated by test `BridgeHubWestend::can_calculate_weight_for_paid_export_message_with_reserve_transfer` + `33%`)
-	pub const BridgeHubWestendBaseFeeInWnds: u128 = 487441010000;
 }
 
 /// Identifier of AssetHubWestend in the Westend relay chain.
