70a46b635f5965c6571b6e5aaa492ade5edba244
Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
2023-11-04 16:34:57 +0100

    Add EnsureInherentsAreOrdered and implement for pallets
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>

--- substrate/frame/support/src/traits/misc.rs
+++ substrate/frame/support/src/traits/misc.rs
@@ -23,6 +23,7 @@ use impl_trait_for_tuples::impl_for_tuples;
 use scale_info::{build::Fields, meta_type, Path, Type, TypeInfo, TypeParameter};
 use sp_arithmetic::traits::{CheckedAdd, CheckedMul, CheckedSub, One, Saturating};
 use sp_core::bounded::bounded_vec::TruncateFrom;
+use sp_inherents::InherentOrderError;
 #[doc(hidden)]
 pub use sp_runtime::traits::{
 	ConstBool, ConstI128, ConstI16, ConstI32, ConstI64, ConstI8, ConstU128, ConstU16, ConstU32,
