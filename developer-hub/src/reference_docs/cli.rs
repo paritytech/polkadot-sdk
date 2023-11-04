bcdc9d53a8ea04aee149ccff4527bda0857fb9ba
kianenigma <kian@parity.io>
2023-11-04 14:02:26 +0000

    add lot of ref docs

---
+++ developer-hub/src/reference_docs/cli.rs
@@ -0,0 +1,7 @@
+//! # Command Line Arguments
+//!
+//!
+//! Notes:
+//!
+//! - Command line arguments of a typical substrate based chain, and how to find and learn them.
+//! - How to extend them with your custom stuff.
