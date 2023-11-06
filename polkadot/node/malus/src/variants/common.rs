46f6fc375ae81823a85ab0d812c4a0334432677c
Overkillus <kz_sky@outlook.com>
2023-11-06 17:38:03 +0000

    fixing docs

--- polkadot/node/malus/src/variants/common.rs
+++ polkadot/node/malus/src/variants/common.rs
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Polkadot.  If not, see <http://www.gnu.org/licenses/>.
 
-//! Implements common code for nemesis. Currently, only `FakeValidationResult`
+//! Implements common code for nemesis. Currently, only `ReplaceValidationResult`
 //! interceptor is implemented.
 use crate::{
 	interceptor::*,
