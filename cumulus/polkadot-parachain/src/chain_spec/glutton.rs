75d950f33f266fae22c0af859606e80735d541b6
Dónal Murray <donal.murray@parity.io>
2023-11-06 10:02:32 +0000

    Update chain specs in light of 1256

--- cumulus/polkadot-parachain/src/chain_spec/glutton.rs
+++ cumulus/polkadot-parachain/src/chain_spec/glutton.rs
@@ -95,104 +95,69 @@ fn glutton_genesis(parachain_id: ParaId, collators: Vec<AuraId>) -> serde_json::
 }
 
 pub fn glutton_westend_development_config(para_id: ParaId) -> GluttonWestendChainSpec {
-	GluttonWestendChainSpec::from_genesis(
-		// Name
-		"Glutton Development",
-		// ID
-		"glutton_westend_dev",
-		ChainType::Local,
-		move || {
-			glutton_westend_genesis(para_id, vec![get_collator_keys_from_seed::<AuraId>("Alice")])
-		},
-		Vec::new(),
-		None,
-		None,
-		None,
-		None,
+	GluttonWestendChainSpec::builder(
+		glutton_westend_runtime::WASM_BINARY.expect("WASM binary was not built, please build it!"),
 		Extensions { relay_chain: "westend-dev".into(), para_id: para_id.into() },
 	)
+	.with_name("Glutton Development")
+	.with_id("glutton_westend_dev")
+	.with_chain_type(ChainType::Local)
+	.with_genesis_config_patch(glutton_genesis(
+		para_id,
+		vec![get_collator_keys_from_seed::<AuraId>("Alice")],
+	))
+	.build()
 }
 
 pub fn glutton_westend_local_config(para_id: ParaId) -> GluttonWestendChainSpec {
-	GluttonWestendChainSpec::from_genesis(
-		// Name
-		"Glutton Westend Local",
-		// ID
-		"glutton_westend_local",
-		ChainType::Local,
-		move || {
-			glutton_westend_genesis(
-				para_id,
-				vec![
-					get_collator_keys_from_seed::<AuraId>("Alice"),
-					get_collator_keys_from_seed::<AuraId>("Bob"),
-				],
-			)
-		},
-		Vec::new(),
-		None,
-		None,
-		None,
-		None,
+	GluttonWestendChainSpec::builder(
+		glutton_westend_runtime::WASM_BINARY.expect("WASM binary was not built, please build it!"),
 		Extensions { relay_chain: "westend-local".into(), para_id: para_id.into() },
 	)
+	.with_name("Glutton Local")
+	.with_id("glutton_westend_local")
+	.with_chain_type(ChainType::Local)
+	.with_genesis_config_patch(glutton_genesis(
+		para_id,
+		vec![
+			get_collator_keys_from_seed::<AuraId>("Alice"),
+			get_collator_keys_from_seed::<AuraId>("Bob"),
+		],
+	))
+	.build()
 }
 
 pub fn glutton_westend_config(para_id: ParaId) -> GluttonWestendChainSpec {
 	let mut properties = sc_chain_spec::Properties::new();
 	properties.insert("ss58Format".into(), 42.into());
 
-	GluttonWestendChainSpec::from_genesis(
-		// Name
-		format!("Glutton Westend {}", para_id).as_str(),
-		// ID
-		format!("glutton-westend-{}", para_id).as_str(),
-		ChainType::Live,
-		move || {
-			glutton_westend_genesis(
-				para_id,
-				vec![
-					get_collator_keys_from_seed::<AuraId>("Alice"),
-					get_collator_keys_from_seed::<AuraId>("Bob"),
-				],
-			)
-		},
-		Vec::new(),
-		None,
-		// Protocol ID
-		Some(format!("glutton-westend-{}", para_id).as_str()),
-		None,
-		Some(properties),
+	GluttonChainSpec::builder(
+		glutton_westend_runtime::WASM_BINARY.expect("WASM binary was not built, please build it!"),
 		Extensions { relay_chain: "westend".into(), para_id: para_id.into() },
 	)
+	.with_name(format!("Glutton {}", para_id).as_str())
+	.with_id(format!("glutton-westend-{}", para_id).as_str())
+	.with_chain_type(ChainType::Live)
+	.with_genesis_config_patch(glutton_westend_genesis(
+		para_id,
+		vec![
+			get_collator_keys_from_seed::<AuraId>("Alice"),
+			get_collator_keys_from_seed::<AuraId>("Bob"),
+		],
+	))
+	.with_protocol_id(format!("glutton-westend-{}", para_id).as_str())
+	.with_properties(properties)
+	.build()
 }
 
-fn glutton_westend_genesis(
-	parachain_id: ParaId,
-	collators: Vec<AuraId>,
-) -> glutton_westend_runtime::RuntimeGenesisConfig {
-	glutton_westend_runtime::RuntimeGenesisConfig {
-		system: glutton_westend_runtime::SystemConfig {
-			code: glutton_westend_runtime::WASM_BINARY
-				.expect("WASM binary was not build, please build it!")
-				.to_vec(),
-			..Default::default()
+fn glutton_westend_genesis(parachain_id: ParaId, collators: Vec<AuraId>) -> serde_json::Value {
+	serde_json::json!( {
+		"parachainInfo": {
+			"parachainId": parachain_id
 		},
-		parachain_info: glutton_westend_runtime::ParachainInfoConfig {
-			parachain_id,
-			..Default::default()
+		"sudo": {
+			"key": Some(get_account_id_from_seed::<sr25519::Public>("Alice")),
 		},
-		parachain_system: Default::default(),
-		glutton: glutton_westend_runtime::GluttonConfig {
-			compute: Default::default(),
-			storage: Default::default(),
-			trash_data_count: Default::default(),
-			..Default::default()
-		},
-		aura: glutton_westend_runtime::AuraConfig { authorities: collators },
-		aura_ext: Default::default(),
-		sudo: glutton_westend_runtime::SudoConfig {
-			key: Some(get_account_id_from_seed::<sr25519::Public>("Alice")),
-		},
-	}
+		"aura": { "authorities": collators },
+	})
 }
