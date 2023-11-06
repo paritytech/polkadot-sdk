5472c3e84a6dc5072d5d94fee58d510396a1d511
Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
2023-11-06 12:48:47 +0100

    Prefix runtime log target
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>

--- substrate/frame/support/procedural/src/pallet/expand/storage.rs
+++ substrate/frame/support/procedural/src/pallet/expand/storage.rs
@@ -855,14 +855,14 @@ pub fn expand_storages(def: &mut Def) -> proc_macro2::TokenStream {
 						::name::<#pallet_ident<#type_use_gen>>()
 						.expect("Every active pallet has a name in the runtime; qed");
 
-					#frame_support::__private::log::debug!(target: "try-decode-state", "trying to decode pallet: {pallet_name}");
+					#frame_support::__private::log::debug!(target: "runtime::try-decode-state", "trying to decode pallet: {pallet_name}");
 
 					// NOTE: for now, we have to exclude storage items that are feature gated.
 					let mut errors = #frame_support::__private::sp_std::vec::Vec::new();
 					let mut decoded = 0usize;
 
 					#(
-						#frame_support::__private::log::debug!(target: "try-decode-state", "trying to decode storage: \
+						#frame_support::__private::log::debug!(target: "runtime::try-decode-state", "trying to decode storage: \
 						{pallet_name}::{}", stringify!(#storage_names));
 
 						match <#storage_names as #frame_support::traits::TryDecodeEntireStorage>::try_decode_entire_state() {
