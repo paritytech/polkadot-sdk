dea74db79a2cd543d5966a76d77609a1c873aabc
paritytech-ci <devops-team@parity.io>
2023-11-08 15:11:56 +0000

    ___Updated docs for master___

--- master/implementors/integration_tests_common/trait.BridgeHubWococoPallet.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {
-"integration_tests_common":[]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
