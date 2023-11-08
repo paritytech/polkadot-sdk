1430484932a46fe76241e3fab13401a810eb2308
NachoPal <ignacio.palacios.santos@gmail.com>
2023-11-08 14:58:10 +0100

    address comments

--- cumulus/parachains/integration-tests/emulated/chains/parachains/assets/asset-hub-wococo/src/lib.rs
+++ cumulus/parachains/integration-tests/emulated/chains/parachains/assets/asset-hub-wococo/src/lib.rs
@@ -13,8 +13,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// pub mod genesis;
-
 // Substrate
 use frame_support::traits::OnInitialize;
 
