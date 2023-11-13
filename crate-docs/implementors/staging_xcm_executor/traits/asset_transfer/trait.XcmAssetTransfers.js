b8fc7306b81d53cec60bd0c42001c86101f5c87d
paritytech-ci <devops-team@parity.io>
2023-11-13 15:28:05 +0000

    ___Updated docs for master___

--- crate-docs/implementors/staging_xcm_executor/traits/asset_transfer/trait.XcmAssetTransfers.js
+++ crate-docs/implementors/staging_xcm_executor/traits/asset_transfer/trait.XcmAssetTransfers.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"penpal_runtime":[]
+"staging_xcm_executor":[]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
