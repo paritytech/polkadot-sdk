a15a60da935a52317d8e9975de233a9b9b9a0629
drskalman <35698397+drskalman@users.noreply.github.com>
2023-11-13 13:08:43 -0500

    Improve documentation
    Co-authored-by: Davide Galassi <davxy@datawok.net>

--- substrate/primitives/consensus/beefy/src/lib.rs
+++ substrate/primitives/consensus/beefy/src/lib.rs
@@ -174,15 +174,11 @@ pub mod ecdsa_bls_crypto {
 		fn verify(&self, signature: &<Self as RuntimeAppPublic>::Signature, msg: &[u8]) -> bool {
 			// We can not simply call
 			// `EcdsaBlsPair::verify(signature.as_inner_ref(), msg, self.as_inner_ref())`
-			// because that invokes ecdsa default verification which perfoms blake2 hash
+			// because that invokes ECDSA default verification which perfoms Blake2b hash
 			// which we don't want. This is because ECDSA signatures are meant to be verified
-			// on Ethereum network where Keccak hasher is siginificantly cheaper than Blake2b.
+			// on Ethereum network where Keccak hasher is significantly cheaper than Blake2b.
 			// See Figure 3 of [OnSc21](https://www.scitepress.org/Papers/2021/106066/106066.pdf)
 			// for comparison.
-			//
-			// - [OnSc21] Onica, E., & Schifirneţ, C. (2021). Towards efficient hashing in
-			// ethereum smart contracts. Proceedings of the 16th International
-			// Conference on Software Technologies, ()
 			EcdsaBlsPair::verify_with_hasher::<MsgHash>(
 				signature.as_inner_ref(),
 				msg,
