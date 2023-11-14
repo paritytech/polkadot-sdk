28d7bbe2f0af8400e5005638dfea40ad0c1222f0
Dmitry Markin <dmitry@markin.tech>
2023-11-14 18:22:20 +0200

    Apply suggestions from code review
    Co-authored-by: Sebastian Kunert <skunert49@gmail.com>

--- substrate/client/network/sync/src/service/mod.rs
+++ substrate/client/network/sync/src/service/mod.rs
@@ -16,7 +16,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-//! `ChainSync`-related service code
+//! `SyncingEngine`-related service code
 
 pub mod engine;
 pub mod mock;
