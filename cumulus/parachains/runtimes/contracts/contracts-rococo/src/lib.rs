b7345c1fafcae8755af7bff727561502b57998be
Liam Aharon <liam.aharon@hotmail.com>
2023-11-14 15:15:45 +0400

    remove contracts rococo dmpqueue

--- cumulus/parachains/runtimes/contracts/contracts-rococo/src/lib.rs
+++ cumulus/parachains/runtimes/contracts/contracts-rococo/src/lib.rs
@@ -405,7 +405,6 @@ construct_runtime!(
 		XcmpQueue: cumulus_pallet_xcmp_queue::{Pallet, Call, Storage, Event<T>} = 30,
 		PolkadotXcm: pallet_xcm::{Pallet, Call, Storage, Event<T>, Origin, Config<T>} = 31,
 		CumulusXcm: cumulus_pallet_xcm::{Pallet, Event<T>, Origin} = 32,
-		DmpQueue: cumulus_pallet_dmp_queue::{Pallet, Call, Storage, Event<T>} = 33,
 		MessageQueue: pallet_message_queue::{Pallet, Call, Storage, Event<T>} = 34,
 
 		// Smart Contracts.
