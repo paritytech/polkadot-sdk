3cae1d822354d9dac8b050380a99117d1ae900d2
Dónal Murray <donal.murray@parity.io>
2023-11-15 09:33:03 +0000

    Fix benchmarking after master merge

--- cumulus/parachains/runtimes/collectives/collectives-westend/src/xcm_config.rs
+++ cumulus/parachains/runtimes/collectives/collectives-westend/src/xcm_config.rs
@@ -353,8 +353,6 @@ impl pallet_xcm::Config for Runtime {
 	type SovereignAccountOf = LocationToAccountId;
 	type MaxLockers = ConstU32<8>;
 	type WeightInfo = crate::weights::pallet_xcm::WeightInfo<Runtime>;
-	#[cfg(feature = "runtime-benchmarks")]
-	type ReachableDest = ReachableDest;
 	type AdminOrigin = EnsureRoot<AccountId>;
 	type MaxRemoteLockConsumers = ConstU32<0>;
 	type RemoteLockConsumerIdentifier = ();
