72b3c830bdbb5485e6b24898949e3f16d9d05c75
Dónal Murray <donal.murray@parity.io>
2023-11-15 14:16:44 +0000

    Clean up testnets-common

--- cumulus/parachains/testnets-common/src/lib.rs
+++ cumulus/parachains/testnets-common/src/lib.rs
@@ -13,19 +13,18 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Since the parachains-common package is now published to crates.io, SP runtimes for testnets
-// will be adapted to use this package, and their config removed from the published common
-// package. Only the configs specific to rococo, westend and wococo will be moved here, and the
-// truly common logic will still be sourced from the parachains-common package.
-//
-// In practice this just means that instead of using e.g. `[parachains_common::westend::*]`, now the
-// westend configs will be in `[testnets_common::westend::*]`.
-//
-// TODO: edit all runtimes to remove the testnet configs as part of PR #1737
-// <https://github.com/paritytech/polkadot-sdk/pull/1737>
-
 #![cfg_attr(not(feature = "std"), no_std)]
 
+/// Since the parachains-common package is now published to crates.io, SP runtimes for testnets
+/// will be adapted to use this package, and their config removed from the published common
+/// package. Only the configs specific to rococo, westend and wococo will be moved here, and the
+/// truly common logic will still be sourced from the parachains-common package.
+///
+/// In practice this just means that instead of using e.g. `[parachains_common::westend::*]`, now
+/// the westend configs will be in `[testnets_common::westend::*]`.
+///
+/// TODO: edit all runtimes to remove the testnet configs as part of PR #1737
+/// <https://github.com/paritytech/polkadot-sdk/pull/1737>
 pub mod rococo;
 pub mod westend;
 pub mod wococo;
