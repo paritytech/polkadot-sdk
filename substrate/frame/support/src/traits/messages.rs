e6cb4690c594df3e5e78729af134ea388a681977
Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
2023-11-16 20:12:27 +0100

    Typo

--- substrate/frame/support/src/traits/messages.rs
+++ substrate/frame/support/src/traits/messages.rs
@@ -121,7 +121,7 @@ impl<OverweightAddr> ServiceQueues for NoopServiceQueues<OverweightAddr> {
 /// The resource footprint of a queue.
 #[derive(Default, Copy, Clone, Eq, PartialEq, RuntimeDebug)]
 pub struct QueueFootprint {
-	/// The number of pages in the queue (excluding overweight pages).
+	/// The number of pages in the queue (including overweight pages).
 	pub pages: u32,
 	/// The number of non-overweight pages in the queue.
 	pub active_pages: u32,
