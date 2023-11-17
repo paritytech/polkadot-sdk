c701e972d57ce90ad1830ab58b99c411e6915add
Francisco Aguirre <franciscoaguirreperez@gmail.com>
2023-11-17 18:03:57 +0100

    Remove unused import

--- polkadot/runtime/westend/src/lib.rs
+++ polkadot/runtime/westend/src/lib.rs
@@ -2366,7 +2366,7 @@ mod remote_tests {
 }
 
 mod clean_state_migration {
-	use super::{Runtime, Vec};
+	use super::Runtime;
 	use frame_support::{pallet_prelude::*, storage_alias, traits::OnRuntimeUpgrade};
 	use pallet_state_trie_migration::MigrationLimits;
 
