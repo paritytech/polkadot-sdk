1908d75d16d547b0fd975a82d9e857ce11c4def9
0xmovses <r.v.melkonian@gmail.com>
2023-11-20 17:19:38 +0000

    assert has_idenity

--- cumulus/parachains/integration-tests/emulated/tests/people/people-rococo/src/lib.rs
+++ cumulus/parachains/integration-tests/emulated/tests/people/people-rococo/src/lib.rs
@@ -13,8 +13,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-mod tests;
-
 pub use codec::Encode;
 
 // Substrate
@@ -59,3 +57,6 @@ pub type RelayToParaTest = Test<Rococo, PenpalA>;
 pub type SystemParaToRelayTest = Test<PeopleRococo, Rococo>;
 pub type SystemParaToParaTest = Test<PeopleRococo, PenpalA>;
 pub type ParaToSystemParaTest = Test<PenpalA, PeopleRococo>;
+
+#[cfg(test)]
+mod tests;
--- cumulus/parachains/integration-tests/emulated/tests/people/people-rococo/src/tests/reap_identity.rs
+++ cumulus/parachains/integration-tests/emulated/tests/people/people-rococo/src/tests/reap_identity.rs
@@ -1,33 +1,54 @@
 use crate::*;
 use pallet_identity::Data;
-use people_rococo_runtime::{people::IdentityInfo, RuntimeOrigin};
+use people_rococo_runtime::{
+	people::{IdentityField, IdentityInfo},
+	RuntimeOrigin,
+};
 use rococo_runtime::MaxAdditionalFields;
 
 fn ten() -> IdentityInfo {
 	IdentityInfo {
-		display: Data::Raw(b"ten".to_vec().try_into().unwrap()),
-		legal: Data::Raw(b"The Right Ordinal Ten, Esq.".to_vec().try_into().unwrap()),
-		web: Data::Raw(b"https://ten.io".to_vec().try_into().unwrap()),
-		matrix: Data::Raw(b"@ten:matrix.org".to_vec().try_into().unwrap()),
-		email: Data::Raw(b"ten@gmail.com".to_vec().try_into().unwrap()),
+		display: Data::Raw(b"xcm-test".to_vec().try_into().unwrap()),
+		legal: Data::Raw(b"The Right Ordinal Xcm Test, Esq.".to_vec().try_into().unwrap()),
+		web: Data::Raw(b"https://xcm-test.io".to_vec().try_into().unwrap()),
+		matrix: Data::Raw(b"@xcm-test:matrix.org".to_vec().try_into().unwrap()),
+		email: Data::Raw(b"xcm-test@gmail.com".to_vec().try_into().unwrap()),
 		pgp_fingerprint: None,
-		image: Data::Raw(b"ten.png".to_vec().try_into().unwrap()),
-		twitter: Data::Raw(b"@ten".to_vec().try_into().unwrap()),
-		github: Data::Raw(b"ten".to_vec().try_into().unwrap()),
-		discord: Data::Raw(b"ten#0000".to_vec().try_into().unwrap()),
+		image: Data::Raw(b"xcm-test.png".to_vec().try_into().unwrap()),
+		twitter: Data::Raw(b"@xcm-test".to_vec().try_into().unwrap()),
+		github: Data::Raw(b"xcm-test".to_vec().try_into().unwrap()),
+		discord: Data::Raw(b"xcm-test#0042".to_vec().try_into().unwrap()),
 	}
 }
 
 #[test]
 fn parachain_set_identity() {
-	// Init values for System Parachain
-	type RuntimeEvent = <PeopleRococo as Chain>::RuntimeEvent;
 	let ten_info = ten();
-
 	PeopleRococo::execute_with(|| {
+		type RuntimeEvent = <PeopleRococo as Chain>::RuntimeEvent;
 		assert_ok!(<PeopleRococo as PeopleRococoPallet>::Identity::set_identity(
 			RuntimeOrigin::signed(PeopleRococoSender::get()),
 			Box::new(ten_info.clone())
+		));
+
+		assert_expected_events!(
+			PeopleRococo,
+			vec![
+				RuntimeEvent::Identity(pallet_identity::Event::IdentitySet { ..}) => {},
+			]
+		);
+
+		assert!(<PeopleRococo as PeopleRococoPallet>::Identity::has_identity(
+			&PeopleRococoSender::get(),
+			IdentityField::Display as u64,
+		));
+		assert!(<PeopleRococo as PeopleRococoPallet>::Identity::has_identity(
+			&PeopleRococoSender::get(),
+			IdentityField::Legal as u64,
+		));
+		assert!(<PeopleRococo as PeopleRococoPallet>::Identity::has_identity(
+			&PeopleRococoSender::get(),
+			IdentityField::Web as u64,
 		));
 	});
 }
