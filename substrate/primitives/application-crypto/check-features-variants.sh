bec4955774dbe30bbee7b483ffb4a5c2df4f8916
Michal Kucharczyk <1728078+michalkucharczyk@users.noreply.github.com>
2023-11-20 21:44:07 +0100

    CI features checks added

---
+++ substrate/primitives/application-crypto/check-features-variants.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env -S bash -eux
+
+export RUSTFLAGS="-Cdebug-assertions=y -Dwarnings"
+T=wasm32-unknown-unknown
+
+T=wasm32-unknown-unknown
+cargo check --release 
+cargo check --release --target=$T --no-default-features
+cargo check --release --target=$T --no-default-features  --features="full_crypto"
+cargo check --release --target=$T --no-default-features  --features="serde"
+cargo check --release --target=$T --no-default-features  --features="serde,full_crypto"
+cargo check --release --target=$T --no-default-features  --features="bandersnatch-experimental"
