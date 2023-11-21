abd8feb8502b031ae8f6cf41bc29ead14ecce0ef
paritytech-ci <devops-team@parity.io>
2023-11-21 11:00:09 +0000

    ___Updated docs for master___

--- master/staging_chain_spec_builder/sidebar-items.js
+++ master/staging_chain_spec_builder/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["ChainSpecBuilderCmd"],"fn":["generate_authority_keys_and_store","generate_chain_spec","generate_chain_spec_for_runtime","print_seeds"],"struct":["ChainSpecBuilder","EditCmd","GenerateCmd","NewCmd","RuntimeCmd","VerifyCmd"]};
+window.SIDEBAR_ITEMS = {"enum":["ChainSpecBuilderCmd"],"fn":["generate_chain_spec_for_runtime"],"struct":["ChainSpecBuilder","ConvertToRawCmd","CreateCmd","UpdateCodeCmd","VerifyCmd"]};
