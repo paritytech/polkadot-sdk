6b68aea26b979add45ccc731192e91fa00e3b688
Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
2023-11-22 18:29:20 +0100

    Fix doctest
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>

--- substrate/utils/frame/rpc/support/src/lib.rs
+++ substrate/utils/frame/rpc/support/src/lib.rs
@@ -73,6 +73,11 @@ use sp_storage::{StorageData, StorageKey};
 /// # 	type SS58Prefix = ();
 /// # 	type OnSetCode = ();
 /// # 	type MaxConsumers = ConstU32<16>;
+/// #	type SingleBlockMigrations = ();
+/// #	type MultiBlockMigrator = ();
+/// #	type PreInherents = ();
+/// #	type PostInherents = ();
+/// #	type PostTransactions = ();
 /// # }
 /// #
 /// # impl pallet_test::Config for TestRuntime {}
