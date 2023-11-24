56c6f25053360d70e6eb028382e651581a45c968
pgherveou <pgherveou@gmail.com>
2023-11-24 15:28:26 +0100

    fix primitives

--- substrate/frame/contracts/src/primitives.rs
+++ substrate/frame/contracts/src/primitives.rs
@@ -17,8 +17,6 @@
 
 //! A crate that hosts a common definitions that are relevant for the pallet-contracts.
 
-#![cfg_attr(not(feature = "std"), no_std)]
-
 use codec::{Decode, Encode, MaxEncodedLen};
 use frame_support::weights::Weight;
 use pallet_contracts_uapi::ReturnFlags;
