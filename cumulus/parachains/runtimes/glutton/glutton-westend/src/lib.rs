230e168aa404e5aa17cfa5caa9ba6e64c91f8308
Nikhil Gupta <17176722+gupnik@users.noreply.github.com>
2023-11-24 12:03:48 +0530

    Minor fixes

--- cumulus/parachains/runtimes/glutton/glutton-westend/src/lib.rs
+++ cumulus/parachains/runtimes/glutton/glutton-westend/src/lib.rs
@@ -53,7 +53,7 @@ pub use sp_consensus_aura::sr25519::AuthorityId as AuraId;
 use sp_core::{crypto::KeyTypeId, OpaqueMetadata};
 use sp_runtime::{
 	create_runtime_str, generic, impl_opaque_keys,
-	traits::{AccountIdLookup, BlakeTwo256, Block as BlockT},
+	traits::{BlakeTwo256, Block as BlockT},
 	transaction_validity::{TransactionSource, TransactionValidity},
 	ApplyExtrinsicResult,
 };
