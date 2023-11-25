e1ec9b20bf84611cb207ee3265637ef1f3e0a9a4
Ankan <ankan.anurag@gmail.com>
2023-11-25 13:22:55 +0100

    build it as a wrapper over core staking

--- substrate/frame/delegated-staking/src/lib.rs
+++ substrate/frame/delegated-staking/src/lib.rs
@@ -28,6 +28,7 @@ use sp_std::{convert::TryInto, prelude::*};
 use pallet::*;
 use sp_runtime::{traits::Zero, DispatchError, RuntimeDebug, Saturating};
 use sp_staking::delegation::{StakeDelegatee, StakeDelegator};
+use sp_staking::{EraIndex, Stake, StakerStatus, StakingInterface};
 
 pub type BalanceOf<T> = <<T as Config>::Currency as FunInspect<<T as frame_system::Config>::AccountId>>::Balance;
 
@@ -49,6 +50,9 @@ pub mod pallet {
 		>;
 		/// Overarching hold reason.
 		type RuntimeHoldReason: From<HoldReason>;
+
+		/// Core staking implementation.
+		type Staking: StakingInterface<Balance = BalanceOf<Self>, AccountId = Self::AccountId>;
 	}
 
 	#[pallet::error]
@@ -100,6 +104,32 @@ pub mod pallet {
 		DelegationRegister<T>,
 		OptionQuery,
 	>;
+}
+
+/// Register of all delegations to a `Delegatee`.
+///
+/// This keeps track of the active balance of the delegatee that is made up from the funds that are
+/// currently delegated to this delegatee. It also tracks the pending slashes yet to be applied
+/// among other things.
+#[derive(Default, Encode, Decode, RuntimeDebug, TypeInfo, MaxEncodedLen)]
+#[scale_info(skip_type_params(T))]
+pub struct DelegationRegister<T: Config> {
+	/// Where the reward should be paid out.
+	pub payee: T::AccountId,
+	/// Sum of all delegated funds to this delegatee.
+	#[codec(compact)]
+	pub balance: BalanceOf<T>,
+	/// Slashes that are not yet applied.
+	#[codec(compact)]
+	pub pending_slash: BalanceOf<T>,
+	/// Whether this delegatee is blocked from receiving new delegations.
+	pub blocked: bool,
+}
+
+impl<T: Config> DelegationRegister<T> {
+	pub fn effective_balance(&self) -> BalanceOf<T> {
+		self.balance.saturating_sub(self.pending_slash)
+	}
 }
 
 impl<T: Config> StakeDelegatee for Pallet<T> {
@@ -247,28 +277,114 @@ impl<T: Config> StakeDelegator for Pallet<T> {
 	}
 }
 
-/// Register of all delegations to a `Delegatee`.
-///
-/// This keeps track of the active balance of the delegatee that is made up from the funds that are
-/// currently delegated to this delegatee. It also tracks the pending slashes yet to be applied
-/// among other things.
-#[derive(Default, Encode, Decode, RuntimeDebug, TypeInfo, MaxEncodedLen)]
-#[scale_info(skip_type_params(T))]
-pub struct DelegationRegister<T: Config> {
-	/// Where the reward should be paid out.
-	pub payee: T::AccountId,
-	/// Sum of all delegated funds to this delegatee.
-	#[codec(compact)]
-	pub balance: BalanceOf<T>,
-	/// Slashes that are not yet applied.
-	#[codec(compact)]
-	pub pending_slash: BalanceOf<T>,
-	/// Whether this delegatee is blocked from receiving new delegations.
-	pub blocked: bool,
-}
+impl<T: Config> StakingInterface for Pallet<T> {
+	type Balance = BalanceOf<T>;
+	type AccountId = T::AccountId;
+	type CurrencyToVote = <T::Staking as StakingInterface>::CurrencyToVote;
 
-impl<T: Config> DelegationRegister<T> {
-	pub fn effective_balance(&self) -> BalanceOf<T> {
-		self.balance.saturating_sub(self.pending_slash)
+	fn minimum_nominator_bond() -> Self::Balance {
+		T::Staking::minimum_nominator_bond()
+	}
+
+	fn minimum_validator_bond() -> Self::Balance {
+		T::Staking::minimum_validator_bond()
+	}
+
+	fn stash_by_ctrl(controller: &Self::AccountId) -> Result<Self::AccountId, DispatchError> {
+		T::Staking::stash_by_ctrl(controller)
+	}
+
+	fn bonding_duration() -> EraIndex {
+		T::Staking::bonding_duration()
+	}
+
+	fn current_era() -> EraIndex {
+		T::Staking::current_era()
+	}
+
+	fn stake(who: &Self::AccountId) -> Result<Stake<Self::Balance>, DispatchError> {
+		T::Staking::stake(who)
+	}
+
+	fn total_stake(who: &Self::AccountId) -> Result<Self::Balance, DispatchError> {
+		T::Staking::total_stake(who)
+	}
+
+	fn active_stake(who: &Self::AccountId) -> Result<Self::Balance, DispatchError> {
+		T::Staking::active_stake(who)
+	}
+
+	fn is_unbonding(who: &Self::AccountId) -> Result<bool, DispatchError> {
+		T::Staking::is_unbonding(who)
+	}
+
+	fn fully_unbond(who: &Self::AccountId) -> sp_runtime::DispatchResult {
+		T::Staking::fully_unbond(who)
+	}
+
+	fn bond(who: &Self::AccountId, value: Self::Balance, payee: &Self::AccountId) -> sp_runtime::DispatchResult {
+		T::Staking::bond(who, value, payee)
+	}
+
+	fn nominate(who: &Self::AccountId, validators: Vec<Self::AccountId>) -> sp_runtime::DispatchResult {
+		T::Staking::nominate(who, validators)
+	}
+
+	fn chill(who: &Self::AccountId) -> sp_runtime::DispatchResult {
+		T::Staking::chill(who)
+	}
+
+	fn bond_extra(who: &Self::AccountId, extra: Self::Balance) -> sp_runtime::DispatchResult {
+		T::Staking::bond_extra(who, extra)
+	}
+
+	fn unbond(stash: &Self::AccountId, value: Self::Balance) -> sp_runtime::DispatchResult {
+		T::Staking::unbond(stash, value)
+	}
+
+	fn withdraw_unbonded(stash: Self::AccountId, num_slashing_spans: u32) -> Result<bool, DispatchError> {
+		T::Staking::withdraw_unbonded(stash, num_slashing_spans)
+	}
+
+	fn desired_validator_count() -> u32 {
+		T::Staking::desired_validator_count()
+	}
+
+	fn election_ongoing() -> bool {
+		T::Staking::election_ongoing()
+	}
+
+	fn force_unstake(who: Self::AccountId) -> sp_runtime::DispatchResult {
+		T::Staking::force_unstake(who)
+	}
+
+	fn is_exposed_in_era(who: &Self::AccountId, era: &EraIndex) -> bool {
+		T::Staking::is_exposed_in_era(who, era)
+	}
+
+	fn status(who: &Self::AccountId) -> Result<StakerStatus<Self::AccountId>, DispatchError> {
+		T::Staking::status(who)
+	}
+
+	fn is_validator(who: &Self::AccountId) -> bool {
+		T::Staking::is_validator(who)
+	}
+
+	fn nominations(who: &Self::AccountId) -> Option<Vec<Self::AccountId>> {
+		T::Staking::nominations(who)
+	}
+
+	#[cfg(feature = "runtime-benchmarks")]
+	fn max_exposure_page_size() -> sp_staking::Page {
+		T::Staking::max_exposure_page_size()
+	}
+
+	#[cfg(feature = "runtime-benchmarks")]
+	fn add_era_stakers(current_era: &EraIndex, stash: &Self::AccountId, exposures: Vec<(Self::AccountId, Self::Balance)>) {
+		T::Staking::add_era_stakers(current_era, stash, exposures)
+	}
+	#[cfg(feature = "runtime-benchmarks")]
+	fn set_current_era(era: EraIndex) {
+		T::Staking::set_current_era(era)
 	}
 }
