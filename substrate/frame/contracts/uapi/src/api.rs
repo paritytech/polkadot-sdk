4c15e5bcf7fa24c1e8b43a77d5bd8214a4138729
pgherveou <pgherveou@gmail.com>
2023-11-27 20:32:38 +0100

    leftover

--- substrate/frame/contracts/uapi/src/api.rs
+++ substrate/frame/contracts/uapi/src/api.rs
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-pub mod unstable;
 use crate::{CallFlags, Result, ReturnFlags};
 use paste::paste;
 
