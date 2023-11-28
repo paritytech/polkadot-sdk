b9ee241c2fa28690f284f52d477078ff570782b3
georgepisaltu <george.pisaltu@parity.io>
2023-11-28 17:07:18 +0200

    Make benches work for basic extensions
    Signed-off-by: georgepisaltu <george.pisaltu@parity.io>

--- substrate/frame/system/benchmarking/src/mock.rs
+++ substrate/frame/system/benchmarking/src/mock.rs
@@ -20,7 +20,9 @@
 #![cfg(test)]
 
 use codec::Encode;
-use frame_support::derive_impl;
+use frame_support::{
+	derive_impl, dispatch::DispatchInfo, pallet_prelude::DispatchClass, weights::Weight,
+};
 use sp_runtime::{traits::IdentityLookup, BuildStorage};
 
 type AccountId = u64;
@@ -63,6 +65,19 @@ impl frame_system::Config for Test {
 }
 
 impl crate::Config for Test {}
+
+impl crate::extensions::Config for Test {
+	fn default_call() -> Self::RuntimeCall {
+		RuntimeCall::System(frame_system::Call::set_heap_pages { pages: 0u64 })
+	}
+
+	fn dispatch_info(
+		weight: Weight,
+		class: DispatchClass,
+	) -> <Self::RuntimeCall as sp_runtime::traits::Dispatchable>::Info {
+		DispatchInfo { weight, class, ..Default::default() }
+	}
+}
 
 struct MockedReadRuntimeVersion(Vec<u8>);
 
