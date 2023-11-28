78f8ddff406e430e98a517f295b8102e087af481
pgherveou <pgherveou@gmail.com>
2023-11-28 13:08:46 +0100

    rm leftover

--- substrate/frame/contracts/uapi/src/host/wasm32.rs
+++ substrate/frame/contracts/uapi/src/host/wasm32.rs
@@ -11,7 +11,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-// #![allow(unused_variables)]
 use super::{
 	extract_from_slice, ptr_len_or_sentinel, ptr_or_sentinel, CallFlags, HostFn, HostFnImpl, Result,
 };
