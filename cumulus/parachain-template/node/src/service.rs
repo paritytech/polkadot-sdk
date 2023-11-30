711e1f1214dd121f2245c8d12178e85fbd57b848
Sebastian Kunert <skunert49@gmail.com>
2023-11-30 14:05:46 +0100

    Remove some unwanted proof recordings

--- cumulus/parachain-template/node/src/service.rs
+++ cumulus/parachain-template/node/src/service.rs
@@ -102,11 +102,10 @@ pub fn new_partial(
 	let executor = ParachainExecutor::new_with_wasm_executor(wasm);
 
 	let (client, backend, keystore_container, task_manager) =
-		sc_service::new_full_parts_record_import::<Block, RuntimeApi, _>(
+		sc_service::new_full_parts::<Block, RuntimeApi, _>(
 			config,
 			telemetry.as_ref().map(|(_, telemetry)| telemetry.handle()),
 			executor,
-			true,
 		)?;
 	let client = Arc::new(client);
 
