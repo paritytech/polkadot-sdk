3d8bf46f893ddb5f7d44164b04ad9c8cd1ebf249
Michal Kucharczyk <1728078+michalkucharczyk@users.noreply.github.com>
2023-12-01 21:02:37 +0100

    sketch

--- substrate/client/transaction-pool/src/fork_aware_pool.rs
+++ substrate/client/transaction-pool/src/fork_aware_pool.rs
@@ -33,7 +33,8 @@ use crate::graph;
 pub use crate::{
 	api::FullChainApi,
 	graph::{
-		base_pool::Limit as PoolLimit, ChainApi, Options, Pool, Transaction, ValidatedTransaction,
+		base_pool::Limit as PoolLimit, watcher::Watcher, ChainApi, Options, Pool, Transaction,
+		ValidatedTransaction,
 	},
 };
 use async_trait::async_trait;
@@ -42,7 +43,7 @@ use futures::{
 	future::{self, ready},
 	prelude::*,
 };
-use parking_lot::Mutex;
+use parking_lot::{Mutex, RwLock};
 use std::{
 	collections::{HashMap, HashSet},
 	pin::Pin,
@@ -66,19 +67,92 @@ use sp_blockchain::{HashAndNumber, TreeRoute};
 
 pub(crate) const LOG_TARGET: &str = "txpool";
 
+pub struct View<PoolApi: graph::ChainApi>(graph::Pool<PoolApi>);
+
+pub struct ViewManager<PoolApi, Block>
+where
+	Block: BlockT,
+	PoolApi: graph::ChainApi<Block = Block>,
+{
+	api: Arc<PoolApi>,
+	views: RwLock<HashMap<Block::Hash, Arc<View<PoolApi>>>>,
+}
+
+pub enum ViewCreationError {
+	Unknown,
+}
+
+impl<PoolApi, Block> ViewManager<PoolApi, Block>
+where
+	Block: BlockT,
+	PoolApi: graph::ChainApi<Block = Block>,
+{
+	fn new(api: Arc<PoolApi>) -> Self {
+		Self { api, views: Default::default() }
+	}
+
+	// shall be called on block import
+	pub async fn create_new_view_at(
+		&self,
+		event: ChainEvent<Block>,
+	) -> Result<(), ViewCreationError> {
+		// let pool = Arc::new(graph::Pool::new(Default::default(), true.into(), pool_api.clone()));
+		unimplemented!()
+	}
+
+	/// Imports a bunch of unverified extrinsics to the pool
+	pub async fn submit_at(
+		&self,
+		at: Block::Hash,
+		source: TransactionSource,
+		xts: impl IntoIterator<Item = Block::Extrinsic>,
+	) -> Result<Vec<Result<ExtrinsicHash<PoolApi>, PoolApi::Error>>, PoolApi::Error> {
+		unimplemented!()
+	}
+
+	/// Imports one unverified extrinsic to the pool
+	pub async fn submit_one(
+		&self,
+		at: Block::Hash,
+		source: TransactionSource,
+		xt: Block::Extrinsic,
+	) -> Result<ExtrinsicHash<PoolApi>, PoolApi::Error> {
+		unimplemented!()
+	}
+
+	/// Import a single extrinsic and starts to watch its progress in the pool.
+	pub async fn submit_and_watch(
+		&self,
+		at: Block::Hash,
+		source: TransactionSource,
+		xt: Block::Extrinsic,
+	) -> Result<Watcher<ExtrinsicHash<PoolApi>, ExtrinsicHash<PoolApi>>, PoolApi::Error> {
+		unimplemented!()
+	}
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
 pub struct ForkAwareTxPool<PoolApi, Block>
 where
 	Block: BlockT,
 	PoolApi: graph::ChainApi<Block = Block>,
 {
-	pool: Arc<graph::Pool<PoolApi>>,
 	api: Arc<PoolApi>,
+	xts: RwLock<Vec<Block::Extrinsic>>,
+	views: Arc<ViewManager<PoolApi, Block>>,
 	// todo:
-	// vec[extrinsics]
 	// map: hash -> view
 	// ready_poll: Arc<Mutex<ReadyPoll<ReadyIteratorFor<PoolApi>, Block>>>,
 	// current tree? (somehow similar to enactment state?)
 	// todo: metrics
+
+	// todo: this are coming from ValidatedPool, some of them maybe needed here
+	// is_validator: IsValidator,
+	// options: Options,
+	// listener: RwLock<Listener<ExtrinsicHash<B>, B>>,
+	// import_notification_sinks: Mutex<Vec<Sender<ExtrinsicHash<B>>>>,
+	// rotator: PoolRotator<ExtrinsicHash<B>>,
 }
 
 impl<PoolApi, Block> ForkAwareTxPool<PoolApi, Block>
@@ -86,19 +160,17 @@ where
 	Block: BlockT,
 	PoolApi: graph::ChainApi<Block = Block> + 'static,
 {
-	/// Create new basic transaction pool with provided api, for tests.
+	/// Create new fork aware transaction pool with provided api, for tests.
 	pub fn new_test(
 		pool_api: Arc<PoolApi>,
 		best_block_hash: Block::Hash,
 		finalized_hash: Block::Hash,
 	) -> Self {
-		let pool = Arc::new(graph::Pool::new(Default::default(), true.into(), pool_api.clone()));
-		Self { api: pool_api, pool }
-	}
-
-	/// Gets shared reference to the underlying pool.
-	pub fn pool(&self) -> &Arc<graph::Pool<PoolApi>> {
-		&self.pool
+		Self {
+			api: pool_api.clone(),
+			xts: Default::default(),
+			views: Arc::new(ViewManager::new(pool_api)),
+		}
 	}
 
 	/// Get access to the underlying api
@@ -123,13 +195,14 @@ where
 		source: TransactionSource,
 		xts: Vec<TransactionFor<Self>>,
 	) -> PoolFuture<Vec<Result<TxHash<Self>, Self::Error>>, Self::Error> {
-		let pool = self.pool.clone();
+		let views = self.views.clone();
+		self.xts.write().extend(xts.clone());
 
 		// todo:
 		// self.metrics
 		// 	.report(|metrics| metrics.submitted_transactions.inc_by(xts.len() as u64));
 
-		async move { pool.submit_at(at, source, xts).await }.boxed()
+		async move { views.submit_at(at, source, xts).await }.boxed()
 	}
 
 	fn submit_one(
@@ -138,12 +211,13 @@ where
 		source: TransactionSource,
 		xt: TransactionFor<Self>,
 	) -> PoolFuture<TxHash<Self>, Self::Error> {
-		let pool = self.pool.clone();
-
 		// todo:
 		// self.metrics.report(|metrics| metrics.submitted_transactions.inc());
 
-		async move { pool.submit_one(at, source, xt).await }.boxed()
+		let views = self.views.clone();
+		self.xts.write().push(xt.clone());
+
+		async move { views.submit_one(at, source, xt).await }.boxed()
 	}
 
 	fn submit_and_watch(
@@ -152,49 +226,58 @@ where
 		source: TransactionSource,
 		xt: TransactionFor<Self>,
 	) -> PoolFuture<Pin<Box<TransactionStatusStreamFor<Self>>>, Self::Error> {
-		let pool = self.pool.clone();
+		let views = self.views.clone();
+		self.xts.write().push(xt.clone());
 
 		// todo:
 		// self.metrics.report(|metrics| metrics.submitted_transactions.inc());
 
 		async move {
-			let watcher = pool.submit_and_watch(at, source, xt).await?;
+			let watcher = views.submit_and_watch(at, source, xt).await?;
 
 			Ok(watcher.into_stream().boxed())
 		}
 		.boxed()
 	}
 
+	// todo: api change? we need block hash here (assuming we need it at all).
 	fn remove_invalid(&self, hashes: &[TxHash<Self>]) -> Vec<Arc<Self::InPoolTransaction>> {
-		let removed = self.pool.validated_pool().remove_invalid(hashes);
+		// let removed = self.pool.validated_pool().remove_invalid(hashes);
+		// removed
 
 		//todo:
 		// self.metrics
 		// 	.report(|metrics| metrics.validations_invalid.inc_by(removed.len() as u64));
 
-		removed
+		unimplemented!()
 	}
 
 	fn status(&self) -> PoolStatus {
-		self.pool.validated_pool().status()
+		// self.pool.validated_pool().status()
+		unimplemented!()
 	}
 
 	fn import_notification_stream(&self) -> ImportNotificationStream<TxHash<Self>> {
-		self.pool.validated_pool().import_notification_stream()
+		// self.pool.validated_pool().import_notification_stream()
+		unimplemented!()
 	}
 
 	fn hash_of(&self, xt: &TransactionFor<Self>) -> TxHash<Self> {
-		self.pool.hash_of(xt)
+		self.api().hash_and_length(xt).0
 	}
 
 	fn on_broadcasted(&self, propagations: HashMap<TxHash<Self>, Vec<String>>) {
-		self.pool.validated_pool().on_broadcasted(propagations)
+		// self.pool.validated_pool().on_broadcasted(propagations)
+		unimplemented!()
 	}
 
+	// todo: api change?
 	fn ready_transaction(&self, hash: &TxHash<Self>) -> Option<Arc<Self::InPoolTransaction>> {
-		self.pool.validated_pool().ready_by_hash(hash)
+		// self.pool.validated_pool().ready_by_hash(hash)
+		unimplemented!()
 	}
 
+	// todo: API change? ready at hash (not number)?
 	fn ready_at(
 		&self,
 		at: NumberFor<Self::Block>,
@@ -205,45 +288,22 @@ where
 				> + Send,
 		>,
 	> {
-		unimplemented!()
 		// -> PolledIterator<PoolApi>
-		// let status = self.status();
-		// // If there are no transactions in the pool, it is fine to return early.
-		// //
-		// // There could be transaction being added because of some re-org happening at the
-		// relevant // block, but this is relative unlikely.
-		// if status.ready == 0 && status.future == 0 {
-		// 	return async { Box::new(std::iter::empty()) as Box<_> }.boxed()
-		// }
-		//
-		// if self.ready_poll.lock().updated_at() >= at {
-		// 	log::trace!(target: LOG_TARGET, "Transaction pool already processed block  #{}", at);
-		// 	let iterator: ReadyIteratorFor<PoolApi> = Box::new(self.pool.validated_pool().ready());
-		// 	return async move { iterator }.boxed()
-		// }
-		//
-		// self.ready_poll
-		// 	.lock()
-		// 	.add(at)
-		// 	.map(|received| {
-		// 		received.unwrap_or_else(|e| {
-		// 			log::warn!("Error receiving pending set: {:?}", e);
-		// 			Box::new(std::iter::empty())
-		// 		})
-		// 	})
-		// 	.boxed()
+		unimplemented!()
 	}
 
+	// todo: API change? ready at block?
 	fn ready(&self) -> Box<dyn ReadyTransactions<Item = Arc<Self::InPoolTransaction>> + Send> {
 		//originally it was: -> ReadyIteratorFor<PoolApi>
 		// Box::new(self.pool.validated_pool().ready())
 		unimplemented!()
 	}
 
+	// todo: API change? futures at block?
 	fn futures(&self) -> Vec<Self::InPoolTransaction> {
-		let pool = self.pool.validated_pool().pool.read();
-
-		pool.futures().cloned().collect::<Vec<_>>()
+		// let pool = self.pool.validated_pool().pool.read();
+		// pool.futures().cloned().collect::<Vec<_>>()
+		unimplemented!()
 	}
 }
 
