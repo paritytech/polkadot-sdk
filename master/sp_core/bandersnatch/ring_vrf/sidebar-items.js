5e0f4086d9d338e0668a1b0b0abaced8c726c5e7
paritytech-ci <devops-team@parity.io>
2023-12-01 22:05:34 +0000

    ___Updated docs for master___

--- master/sp_core/bandersnatch/ring_vrf/sidebar-items.js
+++ master/sp_core/bandersnatch/ring_vrf/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":["RING_DOMAIN_MAX_SIZE","RING_MAX_SIZE"],"struct":["KZG","RingContext","RingVerifierData","RingVrfSignature"],"type":["RingProof","RingProver","RingVerifier"]};
+window.SIDEBAR_ITEMS = {"constant":["RING_DOMAIN_OVERHEAD"],"struct":["KZG","RingContext","RingVerifierData","RingVrfSignature"],"type":["RingProof","RingProver","RingVerifier"]};
