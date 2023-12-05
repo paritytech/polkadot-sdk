188e393f6d7b543187a24cdb23b695ca6210ff32
Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
2023-12-05 23:26:04 +0100

    Fixes
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>

--- substrate/frame/parameters/src/tests.rs
+++ substrate/frame/parameters/src/tests.rs
@@ -4,8 +4,8 @@
 
 use super::*;
 use frame_support::{assert_noop, assert_ok};
-use mock::*;
 use RuntimeOrigin as Origin;
+use super::mock::{*, dynamic_params::*};
 
 #[docify::export]
 #[test]
@@ -114,104 +114,99 @@ fn set_parameters() {
 	});*/
 }
 
-#[cfg(test)]
-mod tests {
-	use crate::mock::*;
+#[test]
+fn test_define_parameters_key_convert() {
+	let key1 = pallet1::Key1;
+	let parameter_key: pallet1::ParametersKey = key1.clone().into();
+	let key1_2: pallet1::Key1 = parameter_key.clone().try_into().unwrap();
 
-	#[test]
-	fn test_define_parameters_key_convert() {
-		let key1 = pallet1::Key1;
-		let parameter_key: pallet1::ParametersKey = key1.clone().into();
-		let key1_2: pallet1::Key1 = parameter_key.clone().try_into().unwrap();
+	assert_eq!(key1, key1_2);
+	assert_eq!(parameter_key, pallet1::ParametersKey::Key1(key1));
 
-		assert_eq!(key1, key1_2);
-		assert_eq!(parameter_key, pallet1::ParametersKey::Key1(key1));
+	let key2 = pallet1::Key2;
+	let parameter_key: pallet1::ParametersKey = key2.clone().into();
+	let key2_2: pallet1::Key2 = parameter_key.clone().try_into().unwrap();
 
-		let key2 = pallet1::Key2;
-		let parameter_key: pallet1::ParametersKey = key2.clone().into();
-		let key2_2: pallet1::Key2 = parameter_key.clone().try_into().unwrap();
+	assert_eq!(key2, key2_2);
+	assert_eq!(parameter_key, pallet1::ParametersKey::Key2(key2));
+}
 
-		assert_eq!(key2, key2_2);
-		assert_eq!(parameter_key, pallet1::ParametersKey::Key2(key2));
-	}
+#[test]
+fn test_define_parameters_value_convert() {
+	let value1 = pallet1::Key1Value(1);
+	let parameter_value: pallet1::ParametersValue = value1.clone().into();
+	let value1_2: pallet1::Key1Value = parameter_value.clone().try_into().unwrap();
 
-	#[test]
-	fn test_define_parameters_value_convert() {
-		let value1 = pallet1::Key1Value(1);
-		let parameter_value: pallet1::ParametersValue = value1.clone().into();
-		let value1_2: pallet1::Key1Value = parameter_value.clone().try_into().unwrap();
+	assert_eq!(value1, value1_2);
+	assert_eq!(parameter_value, pallet1::ParametersValue::Key1(1));
 
-		assert_eq!(value1, value1_2);
-		assert_eq!(parameter_value, pallet1::ParametersValue::Key1(1));
+	let value2 = pallet1::Key2Value(2);
+	let parameter_value: pallet1::ParametersValue = value2.clone().into();
+	let value2_2: pallet1::Key2Value = parameter_value.clone().try_into().unwrap();
 
-		let value2 = pallet1::Key2Value(2);
-		let parameter_value: pallet1::ParametersValue = value2.clone().into();
-		let value2_2: pallet1::Key2Value = parameter_value.clone().try_into().unwrap();
+	assert_eq!(value2, value2_2);
+	assert_eq!(parameter_value, pallet1::ParametersValue::Key2(2));
+}
 
-		assert_eq!(value2, value2_2);
-		assert_eq!(parameter_value, pallet1::ParametersValue::Key2(2));
-	}
+#[test]
+fn test_define_parameters_aggregrated_key_value() {
+	use super::AggregratedKeyValue;
 
-	#[test]
-	fn test_define_parameters_aggregrated_key_value() {
-		use super::AggregratedKeyValue;
+	let kv1 = pallet1::Parameters::Key1(pallet1::Key1, None);
+	let (key1, value1) = kv1.clone().into_parts();
 
-		let kv1 = pallet1::Parameters::Key1(pallet1::Key1, None);
-		let (key1, value1) = kv1.clone().into_parts();
+	assert_eq!(key1, pallet1::ParametersKey::Key1(pallet1::Key1));
+	assert_eq!(value1, None);
 
-		assert_eq!(key1, pallet1::ParametersKey::Key1(pallet1::Key1));
-		assert_eq!(value1, None);
+	let kv2 = pallet1::Parameters::Key2(pallet1::Key2, Some(2));
+	let (key2, value2) = kv2.clone().into_parts();
 
-		let kv2 = pallet1::Parameters::Key2(pallet1::Key2, Some(2));
-		let (key2, value2) = kv2.clone().into_parts();
+	assert_eq!(key2, pallet1::ParametersKey::Key2(pallet1::Key2));
+	assert_eq!(value2, Some(pallet1::ParametersValue::Key2(2)));
+}
 
-		assert_eq!(key2, pallet1::ParametersKey::Key2(pallet1::Key2));
-		assert_eq!(value2, Some(pallet1::ParametersValue::Key2(2)));
-	}
+#[test]
+fn test_define_aggregrated_parameters_key_convert() {
+	use codec::Encode;
+	use frame_support::traits::Into2;
 
-	#[test]
-	fn test_define_aggregrated_parameters_key_convert() {
-		use codec::Encode;
-		use frame_support::traits::Into2;
+	let key1 = pallet1::Key1;
+	let parameter_key: pallet1::ParametersKey = key1.clone().into();
+	let runtime_key: RuntimeParametersKey = parameter_key.clone().into2();
 
-		let key1 = pallet1::Key1;
-		let parameter_key: pallet1::ParametersKey = key1.clone().into();
-		let runtime_key: RuntimeParametersKey = parameter_key.clone().into2();
+	assert_eq!(runtime_key, RuntimeParametersKey::Pallet1(pallet1::ParametersKey::Key1(key1)));
+	assert_eq!(runtime_key.encode(), vec![0, 0]);
 
-		assert_eq!(runtime_key, RuntimeParametersKey::Pallet1(pallet1::ParametersKey::Key1(key1)));
-		assert_eq!(runtime_key.encode(), vec![0, 0]);
+	let key2 = pallet2::Key2;
+	let parameter_key: pallet2::ParametersKey = key2.clone().into();
+	let runtime_key: RuntimeParametersKey = parameter_key.clone().into2();
 
-		let key2 = pallet2::Key2;
-		let parameter_key: pallet2::ParametersKey = key2.clone().into();
-		let runtime_key: RuntimeParametersKey = parameter_key.clone().into2();
+	assert_eq!(runtime_key, RuntimeParametersKey::Pallet2(pallet2::ParametersKey::Key2(key2)));
+	assert_eq!(runtime_key.encode(), vec![1, 1]);
+}
 
-		assert_eq!(runtime_key, RuntimeParametersKey::Pallet2(pallet2::ParametersKey::Key2(key2)));
-		assert_eq!(runtime_key.encode(), vec![1, 1]);
-	}
+#[test]
+fn test_define_aggregrated_parameters_aggregrated_key_value() {
+	use super::AggregratedKeyValue;
 
-	#[test]
-	fn test_define_aggregrated_parameters_aggregrated_key_value() {
-		use super::AggregratedKeyValue;
+	let kv1 = RuntimeParameters::Pallet1(pallet1::Parameters::Key1(pallet1::Key1, None));
+	let (key1, value1) = kv1.clone().into_parts();
 
-		let kv1 = RuntimeParameters::Pallet1(pallet1::Parameters::Key1(pallet1::Key1, None));
-		let (key1, value1) = kv1.clone().into_parts();
+	assert_eq!(
+		key1,
+		RuntimeParametersKey::Pallet1(pallet1::ParametersKey::Key1(pallet1::Key1))
+	);
+	assert_eq!(value1, None);
 
-		assert_eq!(
-			key1,
-			RuntimeParametersKey::Pallet1(pallet1::ParametersKey::Key1(pallet1::Key1))
-		);
-		assert_eq!(value1, None);
+	let kv2 = RuntimeParameters::Pallet2(pallet2::Parameters::Key2(pallet2::Key2, Some(2)));
+	let (key2, value2) = kv2.clone().into_parts();
 
-		let kv2 = RuntimeParameters::Pallet2(pallet2::Parameters::Key2(pallet2::Key2, Some(2)));
-		let (key2, value2) = kv2.clone().into_parts();
-
-		assert_eq!(
-			key2,
-			RuntimeParametersKey::Pallet2(pallet2::ParametersKey::Key2(pallet2::Key2))
-		);
-		assert_eq!(
-			value2,
-			Some(RuntimeParametersValue::Pallet2(pallet2::ParametersValue::Key2(2)))
-		);
-	}
+	assert_eq!(
+		key2,
+		RuntimeParametersKey::Pallet2(pallet2::ParametersKey::Key2(pallet2::Key2))
+	);
+	assert_eq!(
+		value2,
+		Some(RuntimeParametersValue::Pallet2(pallet2::ParametersValue::Key2(2)))
+	);
 }
