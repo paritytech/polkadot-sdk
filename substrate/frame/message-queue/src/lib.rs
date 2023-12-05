7cbebf0fe49d34b180aa91649fcbb5eceeef8da9
Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
2023-12-05 14:38:54 +0100

    Update substrate/frame/message-queue/src/lib.rs
    Co-authored-by: Francisco Aguirre <franciscoaguirreperez@gmail.com>

--- substrate/frame/message-queue/src/lib.rs
+++ substrate/frame/message-queue/src/lib.rs
@@ -61,7 +61,7 @@
 //! This pallet has two entry points for executing (possibly recursive) logic;
 //! [`Pallet::service_queues`] and [`Pallet::execute_overweight`]. Both entry points are guarded by
 //! the same mutex to error on reentrancy. The only functions that are explicitly **allowed** to be
-//! called by a message processor are: [`Pallet::`enqueue_message`] and
+//! called by a message processor are: [`Pallet::enqueue_message`] and
 //! [`Pallet::enqueue_messages`]. All other functions are forbidden and error with
 //! [`Error::RecursiveDisallowed`].
 //!
