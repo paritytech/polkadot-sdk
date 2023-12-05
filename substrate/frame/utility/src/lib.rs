3bd126c29e216425bc59db00301316a2ee87b8f0
Daniel Olano <daniel@olanod.com>
2023-12-05 16:26:38 +0100

    Change utility::as_derivative index to u32

--- substrate/frame/utility/src/lib.rs
+++ substrate/frame/utility/src/lib.rs
@@ -266,7 +266,7 @@ pub mod pallet {
 		})]
 		pub fn as_derivative(
 			origin: OriginFor<T>,
-			index: u16,
+			index: u32,
 			call: Box<<T as Config>::RuntimeCall>,
 		) -> DispatchResultWithPostInfo {
 			let mut origin = origin;
@@ -504,8 +504,13 @@ impl TypeId for IndexedUtilityPalletId {
 
 impl<T: Config> Pallet<T> {
 	/// Derive a derivative account ID from the owner account and the sub-account index.
-	pub fn derivative_account_id(who: T::AccountId, index: u16) -> T::AccountId {
-		let entropy = (b"modlpy/utilisuba", who, index).using_encoded(blake2_256);
+	pub fn derivative_account_id(who: T::AccountId, index: u32) -> T::AccountId {
+		// Derive the same addresses as when index was u16
+		let entropy = if index <= u16::MAX as u32 {
+			(b"modlpy/utilisuba", who, index as u16).using_encoded(blake2_256)
+		} else {
+			(b"modlpy/utilisuba", who, index).using_encoded(blake2_256)
+		};
 		Decode::decode(&mut TrailingZeroInput::new(entropy.as_ref()))
 			.expect("infinite length input; no invalid inputs for type; qed")
 	}
