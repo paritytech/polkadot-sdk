c0676a57f328047d1b93f0de9eade0d94401d304
Juan Girini <juangirini@gmail.com>
2023-12-05 10:22:11 +0100

    Weighing and Metering

--- docs/sdk/src/reference_docs/runtime_vs_smart_contract.rs
+++ docs/sdk/src/reference_docs/runtime_vs_smart_contract.rs
@@ -1,14 +1,8 @@
 //! # Runtime vs. Smart Contracts
 //!
-//! Notes:
-//!
-//! Why one can be weighed, and one MUST be metered.
-//! https://forum.polkadot.network/t/where-contracts-fail-and-runtimes-chains-are-needed/4464/3
-//!
-//!
-//! Here you will find a comparative analysis of Substrate-based Runtimes and Smart Contracts,
-//! highlighting their main differences. The aim is to equip you with a clear understanding of how
-//! these two methods of deploying on-chain logic diverge in their design, usage, and implications.
+//! This is a comparative analysis of Substrate-based Runtimes and Smart Contracts, highlighting
+//! their main differences. Our aim is to equip you with a clear understanding of how these two
+//! methods of deploying on-chain logic diverge in their design, usage, and implications.
 //!
 //! Both Runtimes and Smart Contracts serve distinct purposes. Runtimes offer deep customization for
 //! blockchain development, while Smart Contracts provide a more accessible approach for
@@ -43,3 +37,48 @@
 //! | **Performance and Efficiency** | More efficient, optimized for specific needs of the blockchain.        | Can be less efficient due to the overhead of a virtual machine.     |
 //! | **Security Considerations** | Security flaws can affect the entire blockchain.                        | Security risks usually localized to the individual contract.        |
 //! | **Use Cases and Applicability** | Ideal for creating new blockchains or fundamentally altering blockchain functionality. | Best suited for decentralized applications on an existing blockchain infrastructure. |
+//!
+//! ## Weighing and Metering
+//! In the context of Substrate, resource management is a critical aspect of ensuring network
+//! stability and efficiency. This leads to the fundamental difference in how resources are handled
+//! in FRAME-based runtimes versus smart contracts, specifically focusing on why runtime operations
+//! are weighed and smart contract executions must be metered.
+//!
+//! ### Weighing
+//! In FRAME-based runtimes, operations are "weighed". This means that each operation in the runtime
+//! has a fixed cost, known in advance, determined through benchmarking. Weighing is practical here
+//! because:
+//!
+//! - *Predictability*: Runtime operations are part of the blockchain's core logic, which is static
+//!   until an upgrade occurs. This predictability allows for precise benchmarking.
+//! - *Controlled Environment*: The blockchain's governance mechanisms control runtime upgrades,
+//!   ensuring that any new operations or changes to existing ones are vetted for their resource
+//!   usage.
+//! - *Prevention of Abuse*: By having a fixed cost, it becomes infeasible for an attacker to create
+//!   transactions that could unpredictably consume excessive resources.
+//!
+//! ### Metering
+//! For smart contracts, particularly in the case of pallet_contracts and pallet_evm, resource
+//! consumption is metered. Metering is essential due to:
+//!
+//! - *Dynamic Nature*: Unlike runtime operations, smart contracts can be deployed by any user, and
+//!   their behavior isn’t known in advance. Metering dynamically measures resource consumption as
+//!   the contract executes.
+//! - *Safety Against Infinite Loops*: Metering protects the blockchain from poorly designed
+//!   contracts that might run into infinite loops, consuming an indefinite amount of resources.
+//! - *Fair Cost Allocation*: Metering ensures users deploying and interacting with smart contracts
+//!   pay for the exact amount of resources their contract consumes.
+//!
+//! ### Implications for Developers and Users
+//! - *For Runtime Developers*: Understanding the cost of each operation is essential. Misjudging
+//!   the weight of operations can lead to network congestion or vulnerability exploitation.
+//! - *For Smart Contract Developers*: Being mindful of the gas cost associated with contract
+//!   execution is crucial. Efficiently written contracts save costs and are less likely to hit gas
+//!   limits, ensuring smoother execution on the blockchain.
+//!
+//! Conclusion
+//! The contrasting approaches to resource management - weighing in runtimes and metering in smart
+//! contracts - highlight a key difference in the underlying architecture and philosophy between
+//! these two methods of on-chain logic implementation in Substrate. It underscores the importance
+//! of efficient and secure code, whether you're working on runtime development or smart contract
+//! creation.
