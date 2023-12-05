d624ec65917a6d33b6eafe31929fad5024c82771
paritytech-ci <devops-team@parity.io>
2023-12-05 03:20:27 +0000

    ___Updated docs for master___

--- master/frame_system/pallet/config_preludes/sidebar-items.js
+++ master/frame_system/pallet/config_preludes/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"struct":["SolochainDefaultConfig","TestDefaultConfig"]};
+window.SIDEBAR_ITEMS = {"struct":["ParaChainDefaultConfig","RelayChainDefaultConfig","SolochainDefaultConfig","TestDefaultConfig"]};
