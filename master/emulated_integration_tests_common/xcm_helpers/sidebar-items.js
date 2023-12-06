28c55e6f7b7e9c50154ef9454e92f9fd1096b47c
paritytech-ci <devops-team@parity.io>
2023-12-06 11:38:08 +0000

    ___Updated docs for master___

--- master/emulated_integration_tests_common/xcm_helpers/sidebar-items.js
+++ master/emulated_integration_tests_common/xcm_helpers/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["xcm_transact_paid_execution","xcm_transact_unpaid_execution"]};
+window.SIDEBAR_ITEMS = {"fn":["non_fee_asset","xcm_transact_paid_execution","xcm_transact_unpaid_execution"]};
