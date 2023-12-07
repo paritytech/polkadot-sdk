03b7735ee023a75df0981c331485038650e950c8
Liam Aharon <liam.aharon@hotmail.com>
2023-12-07 16:13:08 +0400

    keep current_storage_version around for 3 months

--- substrate/frame/support/src/traits/metadata.rs
+++ substrate/frame/support/src/traits/metadata.rs
@@ -302,6 +302,18 @@ pub trait GetStorageVersion {
 	/// case that they wish to compare the in-code storage version to the on-chain storage version.
 	type InCodeStorageVersion;
 
+	#[deprecated(
+		note = "Deprecated and will be removed after March 2024. Use `in_code_storage_version` instead."
+	)]
+	/// DEPRECATED: Use [`Self::current_storage_version`] instead.
+	///
+	/// Returns the in-code storage version as specified in the
+	/// [`storage_version`](crate::pallet_macros::storage_version) attribute, or
+	/// [`NoStorageVersionSet`] if the attribute is missing.
+	fn current_storage_version() -> Self::InCodeStorageVersion {
+		Self::in_code_storage_version()
+	}
+
 	/// Returns the in-code storage version as specified in the
 	/// [`storage_version`](crate::pallet_macros::storage_version) attribute, or
 	/// [`NoStorageVersionSet`] if the attribute is missing.
