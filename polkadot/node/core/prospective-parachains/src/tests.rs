526be44ccb755b13aace6c2f7a1ea630ed14ba9a
Alexander Samusev <41779041+alvicsam@users.noreply.github.com>
2023-12-07 09:57:43 +0100

    [ci] Update rust to 1.74 (#2545)
    cc https://github.com/paritytech/ci_cd/issues/900
    
    ---------
    
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
    Co-authored-by: Dónal Murray <donal.murray@parity.io>
    Co-authored-by: Bastian Köcher <info@kchr.de>
    Co-authored-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
    Co-authored-by: command-bot <>

--- polkadot/node/core/prospective-parachains/src/tests.rs
+++ polkadot/node/core/prospective-parachains/src/tests.rs
@@ -101,11 +101,9 @@ fn test_harness<T: Future<Output = VirtualOverseer>>(
 
 	let mut view = View::new();
 	let subsystem = async move {
-		loop {
-			match run_iteration(&mut context, &mut view, &Metrics(None)).await {
-				Ok(()) => break,
-				Err(e) => panic!("{:?}", e),
-			}
+		match run_iteration(&mut context, &mut view, &Metrics(None)).await {
+			Ok(()) => {},
+			Err(e) => panic!("{:?}", e),
 		}
 
 		view
