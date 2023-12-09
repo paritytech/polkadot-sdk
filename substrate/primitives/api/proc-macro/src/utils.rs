08473530f5e754cb7dfe8754bf82a44d9016b909
Branislav Kontur <bkontur@gmail.com>
2023-12-09 22:32:28 +0100

    Fix?

--- substrate/primitives/api/proc-macro/src/utils.rs
+++ substrate/primitives/api/proc-macro/src/utils.rs
@@ -261,6 +261,7 @@ pub fn versioned_trait_name(trait_ident: &Ident, version: u64) -> Ident {
 /// Extract the documentation from the provided attributes.
 #[cfg(feature = "frame-metadata")]
 pub fn get_doc_literals(attrs: &[syn::Attribute]) -> Vec<syn::Lit> {
+	use quote::ToTokens;
 	attrs
 		.iter()
 		.filter_map(|attr| {
