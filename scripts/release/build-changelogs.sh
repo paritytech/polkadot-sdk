08a9235169af894e1bb4a8594108637e3197880c
Wilfried Kopp <wilfried@parity.io>
2023-12-12 11:31:41 +0100

    Fix templates and create changelog template

--- scripts/release/build-changelogs.sh
+++ scripts/release/build-changelogs.sh
@@ -1,40 +1,53 @@
 #!/usr/bin/env bash
 
 export PRODUCT=polkadot
-export VERSION=${VERSION:-v1.0.1}
+export VERSION=${VERSION:-v1.5.0}
 
 PROJECT_ROOT=`git rev-parse --show-toplevel`
 echo $PROJECT_ROOT
 
 TMP=$(mktemp -d)
-TEMPLATE="${PROJECT_ROOT}/scripts/release/templates/prdoc.md.tera"
+TEMPLATE_AUDIENCE="${PROJECT_ROOT}/scripts/release/templates/audience.md.tera"
+TEMPLATE_CHANGELOG="${PROJECT_ROOT}/scripts/release/templates/changelog.md.tera"
+
 DATA_JSON="${TMP}/data.json"
 CONTEXT_JSON="${TMP}/context.json"
-
+echo "TEMPLATE_AUDIENCE=$TEMPLATE_AUDIENCE"
+echo "DATA_JSON=$DATA_JSON"
 echo "CONTEXT_JSON=$CONTEXT_JSON"
 
 prdoc load -d "$PROJECT_ROOT/prdoc" --json > $DATA_JSON
-ls -al $DATA_JSON
+# ls -al $DATA_JSON
 
-cat $DATA_JSON | jq ' { prdoc : .}' > $CONTEXT_JSON
-
-ls -al $CONTEXT_JSON
+cat $DATA_JSON | jq ' { "prdoc" : .}' > $CONTEXT_JSON
+# ls -al $CONTEXT_JSON
 
 # Fetch the list of valid audiences
-SCHEMA_URL=https://raw.githubusercontent.com/paritytech/prdoc/master/schema_user.json
+SCHEMA_URL=https://raw.githubusercontent.com/paritytech/polkadot-sdk/master/prdoc/schema_user.json
 SCHEMA=$(curl -s $SCHEMA_URL | sed 's|^//.*||')
-AUDIENCE_ARRAY=$(echo -E $SCHEMA | jq -r '."$defs".audience.enum')
-readarray -t audiences < <(jq -r '.[]' <<<"$AUDIENCE_ARRAY")
+AUDIENCE_ARRAY=$(echo -E $SCHEMA | jq -r '."$defs".audience.oneOf[] | .const')
+
+readarray -t audiences < <(echo "$AUDIENCE_ARRAY")
 declare -p audiences
 
 # Create output folder
-OUTPUT=/tmp/changelogs/$PRODUCT/$VERSION
+OUTPUT="${TMP}/changelogs/$PRODUCT/$VERSION"
+echo "OUTPUT=$OUTPUT"
 mkdir -p $OUTPUT
 
-# Generate a changelog per audience
+# Generate a changelog
+echo "Generating changelog..."
+tera -t "${TEMPLATE_CHANGELOG}" --env --env-key env "${CONTEXT_JSON}" > "$OUTPUT/changelog.md"
+
+code $OUTPUT/changelog.md
+
+# Generate a release notes doc per audience
 for audience in "${audiences[@]}"; do
+    audience_id="$(tr [A-Z] [a-z] <<< "$audience")"
+    audience_id="$(tr ' ' '_' <<< "$audience_id")"
+    echo "Processing audience: $audience ($audience_id)"
     export TARGET_AUDIENCE=$audience
-    tera -t "${TEMPLATE}" --env --env-key env "${CONTEXT_JSON}" > "$OUTPUT/changelog_${audience}.md"
+    tera -t "${TEMPLATE_AUDIENCE}" --env --env-key env "${CONTEXT_JSON}" > "$OUTPUT/relnote_${audience_id}.md"
 done
 
 # Show the files
