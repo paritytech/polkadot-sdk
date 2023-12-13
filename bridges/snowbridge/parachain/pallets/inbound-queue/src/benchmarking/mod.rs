2294a575f7cec51e96e1314472e6c21d78ba712b
claravanstaden <Cats 4 life!>
2023-12-13 21:44:11 +0200

    fund sovereign account

--- bridges/snowbridge/parachain/pallets/inbound-queue/src/benchmarking/mod.rs
+++ bridges/snowbridge/parachain/pallets/inbound-queue/src/benchmarking/mod.rs
@@ -31,19 +31,18 @@ mod benchmarks {
 			.try_into()
 			.unwrap_or_else(|_| panic!("unable to cast minimum balance to u32"));
 
-		// Make sure the sovereign balance is enough. This is a funny number, because
-		// in some cases the minimum balance is really high, in other cases very low.
-		// e.g. on bridgehub the minimum balance is 33333, on test it is 1. So this equation makes
-		// it is at least twice the minimum balance (so as to satisfy the minimum balance
-		// requirement, and then some (in case the minimum balance is very low, even lower
-		// than the relayer reward fee).
 		let sovereign_balance = (minimum_balance_u32 * 2) + 5000;
 
 		// So that the receiving account exists
 		let _ = T::Token::mint_into(&caller, minimum_balance.into());
 		// Fund the sovereign account (parachain sovereign account) so it can transfer a reward
 		// fee to the caller account
-		let _ = T::Token::mint_into(&sovereign_account, sovereign_balance.into());
+		let _ = T::Token::mint_into(
+			&sovereign_account,
+			3_000_000_000_000u128
+				.try_into()
+				.unwrap_or_else(|_| panic!("unable to cast sovereign account balance")),
+		);
 
 		#[block]
 		{
