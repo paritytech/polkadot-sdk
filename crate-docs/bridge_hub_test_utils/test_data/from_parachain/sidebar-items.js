f5bc79a18dae9db15a6634e615ba3d9d1ccab442
paritytech-ci <devops-team@parity.io>
2023-12-13 09:40:56 +0000

    ___Updated docs for master___

---
+++ crate-docs/bridge_hub_test_utils/test_data/from_parachain/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"fn":["make_complex_bridged_parachain_heads_proof","make_complex_relayer_confirmation_batch","make_complex_relayer_confirmation_proofs","make_complex_relayer_delivery_batch","make_complex_relayer_delivery_proofs"]};
--- master/bridge_hub_test_utils/test_cases/test_data/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-window.SIDEBAR_ITEMS = {"fn":["initialization_data","make_complex_bridged_heads_proof","make_complex_relayer_confirmation_batch","make_complex_relayer_confirmation_proofs","make_complex_relayer_delivery_batch","make_complex_relayer_delivery_proofs","prepare_inbound_xcm"]};
