28d50552d35d7fcf685697c0c9eba7e7529efd1c
Tsvetomir Dimitrov <tsvetomir@parity.io>
2023-12-14 11:13:37 +0200

    Fix a comment

--- substrate/primitives/staking/src/offence.rs
+++ substrate/primitives/staking/src/offence.rs
@@ -149,9 +149,6 @@ pub trait OnOffenceHandler<Reporter, Offender, Res> {
 	///
 	/// The `session` parameter is the session index of the offence.
 	///
-	/// NOTE: All offenders are disabled immediately unless there are already `byzantine threshold`
-	/// offenders. In this case they are slashed but not disabled.
-	///
 	/// The receiver might decide to not accept this offence. In this case, the call site is
 	/// responsible for queuing the report and re-submitting again.
 	fn on_offence(
