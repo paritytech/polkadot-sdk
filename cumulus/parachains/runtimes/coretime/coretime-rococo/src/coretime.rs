fc2eb3ddd70c3f41c2b1544e1d3f8ff2f8925011
Dónal Murray <donal.murray@parity.io>
2023-12-14 15:19:30 +0000

    Use more permissive weight limit, pending benchmarks

--- cumulus/parachains/runtimes/coretime/coretime-rococo/src/coretime.rs
+++ cumulus/parachains/runtimes/coretime/coretime-rococo/src/coretime.rs
@@ -128,7 +128,7 @@ impl CoretimeInterface for CoretimeAllocator {
 			},
 			Instruction::Transact {
 				origin_kind: OriginKind::Native,
-				require_weight_at_most: Weight::from_parts(1, 1),
+				require_weight_at_most: Weight::from_parts(1000000000, 200000),
 				call: request_core_count_call.encode().into(),
 			},
 		]);
@@ -164,7 +164,7 @@ impl CoretimeInterface for CoretimeAllocator {
 			},
 			Instruction::Transact {
 				origin_kind: OriginKind::Native,
-				require_weight_at_most: Weight::from_parts(1, 1),
+				require_weight_at_most: Weight::from_parts(1000000000, 200000),
 				call: request_revenue_info_at_call.encode().into(),
 			},
 		]);
@@ -200,7 +200,7 @@ impl CoretimeInterface for CoretimeAllocator {
 			},
 			Instruction::Transact {
 				origin_kind: OriginKind::Native,
-				require_weight_at_most: Weight::from_parts(1, 1),
+				require_weight_at_most: Weight::from_parts(1000000000, 200000),
 				call: credit_account_call.encode().into(),
 			},
 		]);
@@ -241,7 +241,7 @@ impl CoretimeInterface for CoretimeAllocator {
 			},
 			Instruction::Transact {
 				origin_kind: OriginKind::Native,
-				require_weight_at_most: Weight::from_parts(1, 1),
+				require_weight_at_most: Weight::from_parts(1000000000, 200000),
 				call: assign_core_call.encode().into(),
 			},
 		]);
