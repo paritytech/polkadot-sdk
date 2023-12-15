ffb2125f4a135906bc2aaddc6f31a04891309a32
Ankan <10196091+Ank4n@users.noreply.github.com>
2023-12-15 20:59:39 +0100

    [NPoS] Remove better solution threshold for unsigned submissions (#2694)
    closes https://github.com/paritytech-secops/srlabs_findings/issues/78.
    
    Removes `BetterUnsignedThreshold` from pallet EPM. This will essentially
    mean any solution submitted by the validator that is strictly better
    than the current queued solution would be accepted.
    
    The reason for having these thresholds is to limit number of solutions
    submitted on-chain. However for unsigned submissions, the number of
    solutions that could be submitted on average is limited even without
    thresholding (calculation shown in the corresponding issue).

--- polkadot/runtime/westend/src/lib.rs
+++ polkadot/runtime/westend/src/lib.rs
@@ -531,7 +531,6 @@ parameter_types! {
 	pub const SignedDepositByte: Balance = deposit(0, 10) / 1024;
 	// Each good submission will get 1 WND as reward
 	pub SignedRewardBase: Balance = 1 * UNITS;
-	pub BetterUnsignedThreshold: Perbill = Perbill::from_rational(5u32, 10_000);
 
 	// 1 hour session, 15 minutes unsigned phase, 4 offchain executions.
 	pub OffchainRepeat: BlockNumber = UnsignedPhase::get() / 4;
@@ -608,7 +607,6 @@ impl pallet_election_provider_multi_phase::Config for Runtime {
 	type MinerConfig = Self;
 	type SlashHandler = (); // burn slashes
 	type RewardHandler = (); // nothing to do upon rewards
-	type BetterUnsignedThreshold = BetterUnsignedThreshold;
 	type BetterSignedThreshold = ();
 	type OffchainRepeat = OffchainRepeat;
 	type MinerTxPriority = NposSolutionPriority;
