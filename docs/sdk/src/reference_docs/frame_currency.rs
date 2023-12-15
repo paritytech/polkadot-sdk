9a940420a6bcde5f4ffbeeabcd48a32fd1802465
Juan Girini <juangirini@gmail.com>
2023-12-15 16:47:00 +0100

    remove fungibles and assets

--- docs/sdk/src/reference_docs/frame_currency.rs
+++ docs/sdk/src/reference_docs/frame_currency.rs
@@ -1,29 +1,19 @@
-//! FRAME Currency Abstractions and Traits
-//!
-//! TODO:
-//!
-//! - [x] History, `Currency` trait.
-//! - [ ] `Hold` and `Freeze` with diagram.
-//! - [x] `HoldReason` and `FreezeReason`
-//! - [ ] This footgun: https://github.com/paritytech/polkadot-sdk/pull/1900#discussion_r1363783609
-//!
-//!
+//! # FRAME Currency Abstractions and Traits
 //!
 //! Currency related traits in FRAME provide standardized interfaces for implementing various
 //! currency functionalities. These traits enable developers to create, transfer, and manage
 //! different forms of digital assets within the blockchain environment, ensuring that the economic
 //! aspects of the chain are robust and adaptable to different use cases.
 //!
-//! ## The `Currency` Trait
+//! ## The Currency Trait
 //!
 //! The [`Currency`](../../../frame_support/traits/tokens/currency/index.html) trait was initially
 //! introduced in Substrate to manage the native token balances. This trait was later deprecated in
-//! favor of the [`fungible`](../../../frame_support/traits/tokens/fungible/index.html) and
-//! [`fungibles`](../../../frame_support/traits/tokens/fungibles/index.html) traits in Substrate's
-//! PR [#12951](https://github.com/paritytech/substrate/pull/12951). This shift is part of a broader
-//! initiative to enhance token management capabilities within the framework. This deprecation is
-//! aimed at providing improved safety and more flexibility for managing assets, beyond the
-//! capabilities of the original
+//! favor of the [`fungible`](../../../frame_support/traits/tokens/fungible/index.html) traits in
+//! Substrate's PR [#12951](https://github.com/paritytech/substrate/pull/12951). This shift is part
+//! of a broader initiative to enhance token management capabilities within the framework. This
+//! deprecation is aimed at providing improved safety and more flexibility for managing assets,
+//! beyond the capabilities of the original
 //! [`Currency`](../../../frame_support/traits/tokens/currency/index.html) trait. This transition
 //! enables more diverse economic models in Substrate. For more details, you can view the discussion
 //! on the [Tokens Horizon issue](https://github.com/paritytech/polkadot-sdk/issues/327). The
@@ -35,13 +25,11 @@
 //! still using the [`Currency`](../../../frame_support/traits/tokens/currency/index.html) trait.
 //! There one could find the relevant code examples for upgrading.
 //!
-//! ## Fungible and Fungibles Traits
+//! ## Fungible Traits
 //!
-//! The [`fungible`](../../../frame_support/traits/tokens/fungible/index.html) trait is designed for
-//! managing single currency types, providing a streamlined approach for single-asset operations.
-//! The [`fungibles`](../../../frame_support/traits/tokens/fungibles/index.html) trait, in contrast,
-//! allows for handling multiple types of currencies or assets, offering greater flexibility in
-//! multi-asset environments.
+//! The [`fungible`](../../../frame_support/traits/tokens/fungible/index.html) traits are designed
+//! for managing currency types, providing a streamlined approach for single-asset operations.
+//! Fungible is currently preferred over currency as the latter is deprecated.
 //!
 //! #### Holds and Freezes
 //!
@@ -51,22 +39,12 @@
 //! [frame_support::traits::tokens::fungible::freeze](../../../frame_support/traits/tokens/fungible/
 //! freeze/index.html).
 //!
-//! ## Common Pallets
-//!
-//! #### Pallet Balances
+//! ## Pallet Balances
 //! The [`pallet_balances`](../../../pallet_balances/index.html) is a key component in FRAME. It
 //! is designed for managing native token balances. It plays a crucial role in tracking and
 //! manipulating the balance of accounts, providing functionalities essential for a wide range of
 //! financial operations. The key functions of
 //! [`pallet_balances`](../../../pallet_balances/index.html) include transferring tokens between
 //! accounts, checking account balances, and adjusting balances for staking or fees. This pallet
-//! integrates with the [`fungible`](../../../frame_support/traits/tokens/fungible/index.html)
-//! trait, aligning with the standardized approach for asset management in Substrate.
-//!
-//! #### Pallet Assets
-//! The [`pallet_assets`](../../../pallet_assets/index.html) is designed to manage
-//! multiple asset types within a blockchain. It provides functionalities for asset creation,
-//! management, and destruction. This pallet allows for the handling of a diverse range of assets,
-//! facilitating complex economic models and token systems. Its interaction with the
-//! [`fungibles`](../../../frame_support/traits/tokens/fungibles/index.html) trait enables seamless
-//! integration with multi-asset systems.
+//! implements the [`fungible`](../../../frame_support/traits/tokens/fungible/index.html)
+//! traits, aligning with the standardized approach for asset management in Substrate.
