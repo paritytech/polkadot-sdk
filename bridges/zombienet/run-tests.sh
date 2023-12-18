e0382ea3d796bf79dd44a49c83945b88bf9df2a2
Svyatoslav Nikolsky <svyatonik@gmail.com>
2023-12-18 13:42:37 +0300

    fix paths for CI

--- bridges/zombienet/run-tests.sh
+++ bridges/zombienet/run-tests.sh
@@ -9,16 +9,20 @@ trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 # (we need absolute paths here, because they're used when scripts are called by zombienet from tmp folders)
 export POLKADOT_SDK_FOLDER=`realpath $(dirname "$0")/../..`
 export BRIDGE_TESTS_FOLDER=$POLKADOT_SDK_FOLDER/bridges/zombienet/tests
-#export POLKADOT_BINARY_PATH=/usr/local/bin/polkadot
-#export POLKADOT_PARACHAIN_BINARY_PATH=/usr/local/bin/polkadot-parachain
-#export POLKADOT_PARACHAIN_BINARY_PATH_FOR_ASSET_HUB_ROCOCO=/usr/local/bin/polkadot-parachain
-#export POLKADOT_PARACHAIN_BINARY_PATH_FOR_ASSET_HUB_WESTEND=/usr/local/bin/polkadot-parachain
-#export ZOMBIENET_BINARY_PATH=/usr/local/bin/zombie
-export POLKADOT_BINARY_PATH=$POLKADOT_SDK_FOLDER/target/release/polkadot
-export POLKADOT_PARACHAIN_BINARY_PATH=$POLKADOT_SDK_FOLDER/target/release/polkadot-parachain
-export POLKADOT_PARACHAIN_BINARY_PATH_FOR_ASSET_HUB_ROCOCO=$POLKADOT_PARACHAIN_BINARY_PATH
-export POLKADOT_PARACHAIN_BINARY_PATH_FOR_ASSET_HUB_WESTEND=$POLKADOT_PARACHAIN_BINARY_PATH
+
+# for CI:
+export POLKADOT_BINARY_PATH=/usr/local/bin/polkadot
+export POLKADOT_PARACHAIN_BINARY_PATH=/usr/local/bin/polkadot-parachain
+export POLKADOT_PARACHAIN_BINARY_PATH_FOR_ASSET_HUB_ROCOCO=/usr/local/bin/polkadot-parachain
+export POLKADOT_PARACHAIN_BINARY_PATH_FOR_ASSET_HUB_WESTEND=/usr/local/bin/polkadot-parachain
 export ZOMBIENET_BINARY_PATH=/usr/local/bin/zombie
+
+# for local tests:
+#export POLKADOT_BINARY_PATH=$POLKADOT_SDK_FOLDER/target/release/polkadot
+#export POLKADOT_PARACHAIN_BINARY_PATH=$POLKADOT_SDK_FOLDER/target/release/polkadot-parachain
+#export POLKADOT_PARACHAIN_BINARY_PATH_FOR_ASSET_HUB_ROCOCO=$POLKADOT_PARACHAIN_BINARY_PATH
+#export POLKADOT_PARACHAIN_BINARY_PATH_FOR_ASSET_HUB_WESTEND=$POLKADOT_PARACHAIN_BINARY_PATH
+#export ZOMBIENET_BINARY_PATH=/usr/local/bin/zombie
 
 # check if `wait` supports -p flag
 if [ `printf "$BASH_VERSION\n5.1" | sort -V | head -n 1` = "5.1" ]; then IS_BASH_5_1=1; else IS_BASH_5_1=0; fi
