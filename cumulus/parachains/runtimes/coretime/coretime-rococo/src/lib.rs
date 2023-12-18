a4dfce2b32abde5fc0e6b70f794c6fc35c0f6de1
Dónal Murray <donal.murray@parity.io>
2023-12-18 16:00:41 +0000

    Add XcmpQueue v4 migration to coretime-rococo

--- cumulus/parachains/runtimes/coretime/coretime-rococo/src/lib.rs
+++ cumulus/parachains/runtimes/coretime/coretime-rococo/src/lib.rs
@@ -98,7 +98,7 @@ pub type UncheckedExtrinsic =
 	generic::UncheckedExtrinsic<Address, RuntimeCall, Signature, SignedExtra>;
 
 /// Migrations to apply on runtime upgrade.
-pub type Migrations = ();
+pub type Migrations = (cumulus_pallet_xcmp_queue::migration::v4::MigrationToV4<Runtime>,);
 
 /// Executive: handles dispatch to the various modules.
 pub type Executive = frame_executive::Executive<
