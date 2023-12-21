69434d9a32af6103d7cbde9798cf594027fb620e
eskimor <eskimor@users.noreply.github.com>
2023-12-21 19:06:58 +0100

    Coretime Feature branch (relay chain) (#1694)
    Also fixes: https://github.com/paritytech/polkadot-sdk/issues/1417
    
    - [x] CoreIndex -> AssignmentProvider mapping will be able to change any
    time.
    - [x] Implement
    - [x] Provide Migrations
    - [x] Add and fix tests
    - [x] Implement bulk assigner logic
    - [x] bulk assigner tests
    - [x] Port over current assigner to use bulk designer (+ share on-demand
    with bulk): top-level assigner has core ranges: legacy, bulk
    - [x] Adjust migrations to reflect new assigner structure
    - [x] Move migration code to Assignment code directly and make it
    recursive (make it possible to skip releases) -> follow up ticket.
    - [x] Test migrations
    - [x] Add migration PR to runtimes repo -> follow up ticket.
    - [x] Wire up with actual UMP messages
    - [x] Write PR docs
    
    ---------
    
    Co-authored-by: eskimor <eskimor@no-such-url.com>
    Co-authored-by: Bradley Olson <34992650+BradleyOlson64@users.noreply.github.com>
    Co-authored-by: BradleyOlson64 <lotrftw9@gmail.com>
    Co-authored-by: Anton Vilhelm Ásgeirsson <antonva@users.noreply.github.com>
    Co-authored-by: antonva <anton.asgeirsson@parity.io>
    Co-authored-by: Bastian Köcher <git@kchr.de>
    Co-authored-by: Marcin S. <marcin@realemail.net>
    Co-authored-by: Bastian Köcher <info@kchr.de>
    Co-authored-by: command-bot <>

--- polkadot/runtime/westend/constants/src/lib.rs
+++ polkadot/runtime/westend/constants/src/lib.rs
@@ -107,6 +107,8 @@ pub mod system_parachain {
 	pub const COLLECTIVES_ID: u32 = 1001;
 	/// BridgeHub parachain ID.
 	pub const BRIDGE_HUB_ID: u32 = 1002;
+	/// Brokerage parachain ID.
+	pub const BROKER_ID: u32 = 1005;
 
 	/// All system parachains of Westend.
 	pub type SystemParachains = IsChildSystemParachain<Id>;
