7015621a42cd1d2ce2d44d04c818f0762a2caa92
Szegoo <sakacszergej@gmail.com>
2023-12-24 08:02:27 +0100

    fix

--- polkadot/runtime/common/src/paras_registrar/mod.rs
+++ polkadot/runtime/common/src/paras_registrar/mod.rs
@@ -152,7 +152,7 @@ pub mod pallet {
 	use frame_system::pallet_prelude::*;
 
 	/// The current storage version.
-	const STORAGE_VERSION: StorageVersion = StorageVersion::new(1);
+	const STORAGE_VERSION: StorageVersion = StorageVersion::new(2);
 
 	#[pallet::pallet]
 	#[pallet::without_storage_info]
