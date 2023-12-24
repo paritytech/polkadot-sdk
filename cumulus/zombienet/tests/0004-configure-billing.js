490f8b97144722a90d6641008908abaecd7c6f8c
Szegoo <sakacszergej@gmail.com>
2023-12-24 09:55:50 +0100

    fix zombienet tests

---
+++ cumulus/zombienet/tests/0004-configure-billing.js
@@ -0,0 +1,42 @@
+const assert = require("assert");
+
+async function run(nodeName, networkInfo, _jsArgs) {
+  const { wsUri, userDefinedTypes } = networkInfo.nodesByName[nodeName];
+  const api = await zombie.connect(wsUri, userDefinedTypes);
+
+  await zombie.util.cryptoWaitReady();
+
+  // account to submit tx
+  const keyring = new zombie.Keyring({ type: "sr25519" });
+  const charlie = keyring.addFromUri("//Charlie");
+
+  const paraId = 2000;
+  const setBillingAccountCall = 
+    api.tx.registrar.setParachainBillingAccountToSelf(paraId);
+  const sudoCall = api.tx.sudo.sudo(setBillingAccountCall);
+
+  await new Promise(async (resolve, reject) => {
+    const unsub = await sudoCall.signAndSend(charlie, (result) => {
+      console.log(`Current status is ${result.status}`);
+      if (result.status.isInBlock) {
+        console.log(
+          `Transaction included at blockHash ${result.status.asInBlock}`
+        );
+      } else if (result.status.isFinalized) {
+        console.log(
+          `Transaction finalized at blockHash ${result.status.asFinalized}`
+        );
+        unsub();
+        return resolve();
+      } else if (result.isError) {
+        console.log(`Transaction Error`);
+        unsub();
+        return reject();
+      }
+    });
+  });
+
+  return 0;
+}
+
+module.exports = { run };
