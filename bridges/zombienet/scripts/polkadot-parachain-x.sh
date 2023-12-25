e02121c2d3d6bc3fc8cbc29f7f2dadada698fae8
Svyatoslav Nikolsky <svyatonik@gmail.com>
2023-12-25 15:57:31 +0300

    too long lines

--- bridges/zombienet/scripts/polkadot-parachain-x.sh
+++ bridges/zombienet/scripts/polkadot-parachain-x.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
+
+ARGS="$@"
 LOG_FILE=`mktemp -d /tmp/polkadot-parachain.XXXXX`
-echo "Starting polkadot-parachain with arguments: $@. Log: $LOG_FILE" >$LOG_FILE/log
-/usr/local/bin/polkadot-parachain "$@" 2>&1 | tee -a $LOG_FILE/log
-echo "Stopping polkadot-parachain with arguments: $@" >>$LOG_FILE/log
+BUILD_SPEC=0
+while test $# -gt 0
+do
+    case "$1" in
+        build-spec)
+            BUILD_SPEC=1
+            ;;
+    esac
+    shift
+done
+
+if [ $BUILD_SPEC -eq 1 ]; then
+	/usr/local/bin/polkadot-parachain $ARGS
+else
+	echo "Starting polkadot-parachain with arguments: $ARGS. Log: $LOG_FILE" >$LOG_FILE/log
+	/usr/local/bin/polkadot-parachain $ARGS 2>&1 | tee -a $LOG_FILE/log
+	echo "Stopping polkadot-parachain with arguments: $ARGS" >>$LOG_FILE/log
+fi
--- bridges/zombienet/scripts/polkadot-x.sh
+++ bridges/zombienet/scripts/polkadot-x.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
+
+ARGS="$@"
 LOG_FILE=`mktemp -d /tmp/polkadot.XXXXX`
-echo "Starting polkadot with arguments: $@. Log: $LOG_FILE" >$LOG_FILE/log
-/usr/local/bin/polkadot "$@" 2>&1 | tee -a $LOG_FILE/log
-echo "Stopping polkadot with arguments: $@" >>$LOG_FILE/log
+BUILD_SPEC=0
+while test $# -gt 0
+do
+    case "$1" in
+        build-spec)
+            BUILD_SPEC=1
+            ;;
+    esac
+    shift
+done
+
+if [ $BUILD_SPEC -eq 1 ]; then
+	/usr/local/bin/polkadot $ARGS
+else
+	echo "Starting polkadot with arguments: $ARGS. Log: $LOG_FILE" >$LOG_FILE/log
+	/usr/local/bin/polkadot $ARGS 2>&1 | tee -a $LOG_FILE/log
+	echo "Stopping polkadot with arguments: $ARGS" >>$LOG_FILE/log
+fi
