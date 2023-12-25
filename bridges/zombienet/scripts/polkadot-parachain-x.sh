c3ebf00bc5c0dca69f65bcf562e9c3d13ca8d792
Svyatoslav Nikolsky <svyatonik@gmail.com>
2023-12-25 15:19:24 +0300

    xxx

--- bridges/zombienet/scripts/polkadot-parachain-x.sh
+++ bridges/zombienet/scripts/polkadot-parachain-x.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 LOG_FILE=`mktemp -d /tmp/polkadot-parachain.XXXXX`
-echo "Starting polkadot-parachain with arguments: $@. Log: $LOG_FILE"
-/usr/local/bin/polkadot-parachain "$@" 2>&1 | tee $LOG_FILE/log
-echo "Stopping polkadot-parachain with arguments: $@"
+echo "Starting polkadot-parachain with arguments: $@. Log: $LOG_FILE" >$LOG_FILE/log
+/usr/local/bin/polkadot-parachain "$@" 2>&1 | tee -a $LOG_FILE/log
+echo "Stopping polkadot-parachain with arguments: $@" >>$LOG_FILE/log
--- bridges/zombienet/scripts/polkadot-x.sh
+++ bridges/zombienet/scripts/polkadot-x.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 LOG_FILE=`mktemp -d /tmp/polkadot.XXXXX`
-echo "Starting polkadot with arguments: $@. Log: $LOG_FILE"
-/usr/local/bin/polkadot "$@" 2>&1 | tee $LOG_FILE/log
-echo "Stopping polkadot with arguments: $@"
+echo "Starting polkadot with arguments: $@. Log: $LOG_FILE" >$LOG_FILE/log
+/usr/local/bin/polkadot "$@" 2>&1 | tee -a $LOG_FILE/log
+echo "Stopping polkadot with arguments: $@" >>$LOG_FILE/log
