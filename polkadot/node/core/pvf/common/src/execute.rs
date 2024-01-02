c138f8fc93fd24373e6a9ef286e2ed8b3b9f46c0
Marcin S. <marcin@realemail.net>
2024-01-02 19:44:53 +0100

    Remove more unused code

--- polkadot/node/core/pvf/common/src/execute.rs
+++ polkadot/node/core/pvf/common/src/execute.rs
@@ -99,6 +99,4 @@ pub enum JobError {
 	CouldNotSpawnThread(String),
 	#[error("An error occurred in the CPU time monitor thread: {0}")]
 	CpuTimeMonitorThread(String),
-	#[error("Could not set pdeathsig: {0}")]
-	CouldNotSetPdeathsig(String),
 }
