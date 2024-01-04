a4f07ce112fdeb5a059c9a377f95ef1ccfff9fe8
georgepisaltu <george.pisaltu@parity.io>
2024-01-04 18:39:24 +0200

    Remove whitelisted caller from system ext benches
    Signed-off-by: georgepisaltu <george.pisaltu@parity.io>

--- substrate/frame/system/benchmarking/src/extensions.rs
+++ substrate/frame/system/benchmarking/src/extensions.rs
@@ -19,7 +19,7 @@
 
 #![cfg(feature = "runtime-benchmarks")]
 
-use frame_benchmarking::{impl_benchmark_test_suite, v2::*, whitelisted_caller, BenchmarkError};
+use frame_benchmarking::{account, impl_benchmark_test_suite, v2::*, BenchmarkError};
 use frame_support::{
 	dispatch::{DispatchClass, DispatchInfo, PostDispatchInfo},
 	weights::Weight,
@@ -47,7 +47,7 @@ mod benchmarks {
 	#[benchmark]
 	fn check_genesis() -> Result<(), BenchmarkError> {
 		let len = 0_usize;
-		let caller = whitelisted_caller();
+		let caller = account("caller", 0, 0);
 		let info = DispatchInfo { weight: Weight::zero(), ..Default::default() };
 		let call: T::RuntimeCall = frame_system::Call::remark { remark: vec![] }.into();
 
@@ -71,7 +71,7 @@ mod benchmarks {
 		let prev_block: BlockNumberFor<T> = 16u32.into();
 		let default_hash: T::Hash = Default::default();
 		frame_system::BlockHash::<T>::insert(prev_block, default_hash);
-		let caller = whitelisted_caller();
+		let caller = account("caller", 0, 0);
 		let info = DispatchInfo {
 			weight: Weight::from_parts(100, 0),
 			class: DispatchClass::Normal,
@@ -92,7 +92,7 @@ mod benchmarks {
 	fn check_non_zero_sender() -> Result<(), BenchmarkError> {
 		let len = 0_usize;
 		let ext = CheckNonZeroSender::<T>::new();
-		let caller = whitelisted_caller();
+		let caller = account("caller", 0, 0);
 		let info = DispatchInfo { weight: Weight::zero(), ..Default::default() };
 		let call: T::RuntimeCall = frame_system::Call::remark { remark: vec![] }.into();
 
@@ -107,7 +107,7 @@ mod benchmarks {
 
 	#[benchmark]
 	fn check_nonce() -> Result<(), BenchmarkError> {
-		let caller: T::AccountId = whitelisted_caller();
+		let caller: T::AccountId = account("caller", 0, 0);
 		let mut info = frame_system::AccountInfo::default();
 		info.nonce = 1u32.into();
 		info.providers = 1;
@@ -135,7 +135,7 @@ mod benchmarks {
 	#[benchmark]
 	fn check_spec_version() -> Result<(), BenchmarkError> {
 		let len = 0_usize;
-		let caller = whitelisted_caller();
+		let caller = account("caller", 0, 0);
 		let info = DispatchInfo { weight: Weight::zero(), ..Default::default() };
 		let call: T::RuntimeCall = frame_system::Call::remark { remark: vec![] }.into();
 
@@ -152,7 +152,7 @@ mod benchmarks {
 	#[benchmark]
 	fn check_tx_version() -> Result<(), BenchmarkError> {
 		let len = 0_usize;
-		let caller = whitelisted_caller();
+		let caller = account("caller", 0, 0);
 		let info = DispatchInfo { weight: Weight::zero(), ..Default::default() };
 		let call: T::RuntimeCall = frame_system::Call::remark { remark: vec![] }.into();
 
@@ -168,7 +168,7 @@ mod benchmarks {
 
 	#[benchmark]
 	fn check_weight() -> Result<(), BenchmarkError> {
-		let caller = whitelisted_caller();
+		let caller = account("caller", 0, 0);
 		let base_extrinsic = <T as frame_system::Config>::BlockWeights::get()
 			.get(DispatchClass::Normal)
 			.base_extrinsic;
