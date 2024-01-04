bae672d2711f4798f3e9513c6df945bd83a6e152
paritytech-ci <devops-team@parity.io>
2024-01-04 15:00:05 +0000

    ___Updated docs for master___

--- master/src-files.js
+++ master/src-files.js
@@ -150,7 +150,6 @@ var srcIndex = JSON.parse('{\
 "pallet_collective_content":["",[],["lib.rs","weights.rs"]],\
 "pallet_contracts":["",[["migration",[],["v09.rs","v10.rs","v11.rs","v12.rs","v13.rs","v14.rs","v15.rs"]],["storage",[],["meter.rs"]],["wasm",[],["mod.rs","prepare.rs","runtime.rs"]]],["address.rs","chain_extension.rs","debug.rs","exec.rs","gas.rs","lib.rs","migration.rs","primitives.rs","schedule.rs","storage.rs","weights.rs"]],\
 "pallet_contracts_fixtures":["",[],["lib.rs"]],\
-"pallet_contracts_fixtures_common":["",[],["lib.rs"]],\
 "pallet_contracts_mock_network":["",[["mocks",[],["msg_queue.rs","relay_message_queue.rs"]],["parachain",[],["contracts_config.rs"]]],["lib.rs","mocks.rs","parachain.rs","primitives.rs","relay_chain.rs"]],\
 "pallet_contracts_proc_macro":["",[],["lib.rs"]],\
 "pallet_contracts_uapi":["",[],["flags.rs","lib.rs"]],\
