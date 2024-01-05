57c3bbfec9b8e60f4a3bc9b5614cd5263e9d0572
Niklas Adolfsson <niklasadolfsson1@gmail.com>
2024-01-05 16:20:14 +0100

    revert more stuff that doesn't compile

--- cumulus/parachain-template/node/src/rpc.rs
+++ cumulus/parachain-template/node/src/rpc.rs
@@ -45,19 +45,12 @@ where
 	P: TransactionPool + Sync + Send + 'static,
 {
 	use pallet_transaction_payment_rpc::{TransactionPayment, TransactionPaymentApiServer};
-	use sc_rpc_spec_v2::chain_spec::{ChainSpec, ChainSpecApiServer};
 	use substrate_frame_rpc_system::{System, SystemApiServer};
 
 	let mut module = RpcExtension::new(());
 	let FullDeps { client, pool, deny_unsafe } = deps;
 
-	let chain_name = chain_spec.name().to_string();
-	let genesis_hash = client.block_hash(0).ok().flatten().expect("Genesis block exists; qed");
-	let properties = chain_spec.properties();
-
-	module.merge(ChainSpec::new(chain_name, genesis_hash, properties).into_rpc())?;
 	module.merge(System::new(client.clone(), pool, deny_unsafe).into_rpc())?;
 	module.merge(TransactionPayment::new(client).into_rpc())?;
-
 	Ok(module)
 }
