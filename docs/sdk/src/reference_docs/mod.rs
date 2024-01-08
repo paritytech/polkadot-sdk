fdd26df91850b9ddf645205e4587ebdb46b1acb3
Bader Youssef <ibnbassem@gmail.com>
2024-01-08 10:40:40 -0500

    new branch

--- docs/sdk/src/reference_docs/mod.rs
+++ docs/sdk/src/reference_docs/mod.rs
@@ -49,7 +49,7 @@ pub mod frame_origin;
 
 /// Learn about how to write safe and defensive code in your FRAME runtime.
 // TODO: @CrackTheCode016 https://github.com/paritytech/polkadot-sdk-docs/issues/44
-pub mod safe_defensive_programming;
+pub mod defensive_programming;
 
 /// Learn about composite enums in FRAME-based runtimes, such as "RuntimeEvent" and "RuntimeCall".
 pub mod frame_composite_enums;
