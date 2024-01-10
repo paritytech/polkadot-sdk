6a0058482f38535459b68b617bc9c360716247a0
Michal Kucharczyk <1728078+michalkucharczyk@users.noreply.github.com>
2024-01-10 21:04:07 +0100

    missing line

--- substrate/frame/support/src/genesis_builder_helper.rs
+++ substrate/frame/support/src/genesis_builder_helper.rs
@@ -37,7 +37,6 @@ where
 /// Build `GenesisConfig` from a JSON blob not using any defaults and store it in the storage. For
 /// more info refer to [`sp_genesis_builder::GenesisBuilder::build_config`].
 pub fn build_config<GC: BuildGenesisConfig>(json: sp_std::vec::Vec<u8>) -> BuildResult {
-{
 	let gc = serde_json::from_slice::<GC>(&json)
 		.map_err(|e| format_runtime_string!("Invalid JSON blob: {}", e))?;
 	<GC as BuildGenesisConfig>::build(&gc);
