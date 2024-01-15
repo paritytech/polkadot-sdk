1e0b07e1855bfe59526e8819e9bfcf49138f624a
paritytech-ci <devops-team@parity.io>
2024-01-15 16:06:44 +0000

    ___Updated docs for master___

--- master/implementors/staging_xcm_builder/location_conversion/trait.DescribeLocation.js
+++ master/implementors/staging_xcm_builder/location_conversion/trait.DescribeLocation.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
-"snowbridge_core":[["impl DescribeLocation for <a class=\"struct\" href=\"snowbridge_core/struct.DescribeHere.html\" title=\"struct snowbridge_core::DescribeHere\">DescribeHere</a>"]],
+"snowbridge_core":[["impl <a class=\"trait\" href=\"staging_xcm_builder/location_conversion/trait.DescribeLocation.html\" title=\"trait staging_xcm_builder::location_conversion::DescribeLocation\">DescribeLocation</a> for <a class=\"struct\" href=\"snowbridge_core/struct.DescribeHere.html\" title=\"struct snowbridge_core::DescribeHere\">DescribeHere</a>"]],
 "staging_xcm_builder":[]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
