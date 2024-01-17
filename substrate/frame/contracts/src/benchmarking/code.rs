b431dadbbcfca887f5db2437220440cac31e693d
Robin Freyler <robin.freyler@gmail.com>
2024-01-17 10:58:52 +0100

    make call_with_code_per_byte benchmark more robust
    It now uses local.get(0) instead of i32.const for the if condition which makes it impossible for Wasmi (register) to aggressively optimize whole parts of the if away at compilation time thus creating an unfair advantage in benchmarks towards Wasmi (stack).

--- substrate/frame/contracts/src/benchmarking/code.rs
+++ substrate/frame/contracts/src/benchmarking/code.rs
@@ -281,17 +281,21 @@ impl<T: Config> WasmModule<T> {
 	/// instrumentation runtime by nesting blocks as deeply as possible given the byte budget.
 	/// `code_location`: Whether to place the code into `deploy` or `call`.
 	pub fn sized(target_bytes: u32, code_location: Location) -> Self {
-		use self::elements::Instruction::{End, I32Const, If, Return};
+		use self::elements::Instruction::{End, If, GetLocal, Return};
 		// Base size of a contract is 63 bytes and each expansion adds 6 bytes.
 		// We do one expansion less to account for the code section and function body
 		// size fields inside the binary wasm module representation which are leb128 encoded
 		// and therefore grow in size when the contract grows. We are not allowed to overshoot
 		// because of the maximum code size that is enforced by `instantiate_with_code`.
 		let expansions = (target_bytes.saturating_sub(63) / 6).saturating_sub(1);
-		const EXPANSION: [Instruction; 4] = [I32Const(0), If(BlockType::NoResult), Return, End];
+		const EXPANSION: [Instruction; 4] = [GetLocal(0), If(BlockType::NoResult), Return, End];
 		let mut module =
 			ModuleDefinition { memory: Some(ImportedMemory::max::<T>()), ..Default::default() };
-		let body = Some(body::repeated(expansions, &EXPANSION));
+		let body = Some(body::repeated_with_locals(
+			&[Local::new(1, ValueType::I32)],
+			expansions,
+			&EXPANSION,
+		));
 		match code_location {
 			Location::Call => module.call_body = body,
 			Location::Deploy => module.deploy_body = body,
