d833e19eb9c8f0de0d2b8f71e8b587b18c93290e
muharem <ismailov.m.h@gmail.com>
2024-01-18 16:08:55 +0800

    clippy

--- cumulus/parachains/runtimes/assets/asset-hub-rococo/tests/tests.rs
+++ cumulus/parachains/runtimes/assets/asset-hub-rococo/tests/tests.rs
@@ -28,7 +28,7 @@ use asset_hub_rococo_runtime::{
 	AllPalletsWithoutSystem, AssetConversion, AssetDeposit, Assets, Balances, CollatorSelection,
 	ExistentialDeposit, ForeignAssets, ForeignAssetsInstance, MetadataDepositBase,
 	MetadataDepositPerByte, ParachainSystem, Runtime, RuntimeCall, RuntimeEvent, SessionKeys,
-	System, ToWestendXcmRouterInstance, TrustBackedAssetsInstance, XcmpQueue,
+	ToWestendXcmRouterInstance, TrustBackedAssetsInstance, XcmpQueue,
 };
 use asset_test_utils::{
 	test_cases_over_bridge::TestBridgingConfig, CollatorSessionKey, CollatorSessionKeys, ExtBuilder,
