38f0003e0605203ee037587067fc1dafc7c7912c
Aaro Altonen <aaro.altonen@parity.io>
2024-01-18 16:00:13 +0200

    Apply review comments

--- substrate/client/mixnet/src/peer_id.rs
+++ substrate/client/mixnet/src/peer_id.rs
@@ -16,7 +16,6 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-// use libp2p_identity::PeerId;
 use mixnet::core::PeerId as CorePeerId;
 use sc_network_types::PeerId;
 
