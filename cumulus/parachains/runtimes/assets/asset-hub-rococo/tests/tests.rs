9ff3316dbb1ff6b7eef4f16f577af3799d555625
Marcin S. <marcin@realemail.net>
2024-01-19 15:24:18 +0100

    fmt

--- cumulus/parachains/runtimes/assets/asset-hub-rococo/tests/tests.rs
+++ cumulus/parachains/runtimes/assets/asset-hub-rococo/tests/tests.rs
@@ -28,7 +28,7 @@ use asset_hub_rococo_runtime::{
 	AllPalletsWithoutSystem, AssetConversion, AssetDeposit, Assets, Balances, CollatorSelection,
 	ExistentialDeposit, ForeignAssets, ForeignAssetsInstance, MetadataDepositBase,
 	MetadataDepositPerByte, ParachainSystem, Runtime, RuntimeCall, RuntimeEvent, SessionKeys,
-	ToWestendXcmRouterInstance, TrustBackedAssetsInstance, XcmpQueue, SLOT_DURATION
+	ToWestendXcmRouterInstance, TrustBackedAssetsInstance, XcmpQueue, SLOT_DURATION,
 };
 use asset_test_utils::{
 	test_cases_over_bridge::TestBridgingConfig, CollatorSessionKey, CollatorSessionKeys,
