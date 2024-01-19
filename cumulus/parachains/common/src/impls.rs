f4cfc2c8006fab46214b6b6fe582f308c623f1df
Branislav Kontur <bkontur@gmail.com>
2024-01-19 14:23:29 +0100

    Hit me, baby, one more time...

--- cumulus/parachains/common/src/impls.rs
+++ cumulus/parachains/common/src/impls.rs
@@ -257,7 +257,6 @@ mod tests {
 		type RuntimeHoldReason = RuntimeHoldReason;
 		type RuntimeFreezeReason = RuntimeFreezeReason;
 		type FreezeIdentifier = ();
-
 		type MaxFreezes = ConstU32<1>;
 	}
 
