c4cdde1754c04cf6e8b1e3191a290045f84c4363
Svyatoslav Nikolsky <svyatonik@gmail.com>
2024-01-19 16:41:08 +0300

    then we need to increase timeouts :(

--- bridges/zombienet/helpers/wait-hrmp-channel-opened.js
+++ bridges/zombienet/helpers/wait-hrmp-channel-opened.js
@@ -4,18 +4,20 @@ async function run(nodeName, networkInfo, args) {
 
     const sibling = args[0];
 
-    while (true) {
-        const messagingStateAsObj = await api.query.parachainSystem.relevantMessagingState();
-        const messagingState = api.createType("Option<CumulusPalletParachainSystemRelayStateSnapshotMessagingStateSnapshot>", messagingStateAsObj);
+    api.rpc.chain.subscribeNewHeads(async function (header) {
+        const apiAtCurrent = await api.at(header.hash);
+console.log("=== WaitForHrmp: " + new Date().toLocaleString() + " " + header.number);
+        const messagingStateAsObj = await apiAtCurrent.query.parachainSystem.relevantMessagingState();
+        const messagingState = apiAtCurrent.createType(
+            "Option<CumulusPalletParachainSystemRelayStateSnapshotMessagingStateSnapshot>",
+            messagingStateAsObj,
+        );
         if (messagingState.isSome) {
             const egressChannels = messagingState.unwrap().egressChannels;
             if (egressChannels.find(x => x[0] == sibling)) {
                 return;
             }
         }
-
-        // else sleep and retry
-        await new Promise((resolve) => setTimeout(resolve, 12000));
     }
 }
 
