f2956c02249f390bfa7f7b33804da985e234733d
Skalman <skalman@riseup.net>
2024-01-20 15:01:34 -0500

    fix wrong doc for `BeefySignerAuthority::sign_with_hasher`

--- substrate/primitives/consensus/beefy/src/test_utils.rs
+++ substrate/primitives/consensus/beefy/src/test_utils.rs
@@ -53,9 +53,7 @@ pub enum Keyring {
 ///
 /// Accepts custom hashing fn for the message and custom convertor fn for the signer.
 pub trait BeefySignerAuthority<MsgHash: Hash>: AppPair {
-	/// Generate a signature.
-	///
-	/// Return `true` if signature over `msg` is valid for this id.
+	/// Generate and return signature for `message` using custom hashing `MsgHash`
 	fn sign_with_hasher(&self, message: &[u8]) -> <Self as AppCrypto>::Signature;
 }
 
