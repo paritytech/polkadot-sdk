38a5b55ef00ffa1529450ff2de04c7235ef3d2e2
Davide Galassi <davxy@datawok.net>
2024-01-22 13:20:13 +0100

    More deprecation warnings

--- substrate/deprecated/hashing/proc-macro/src/lib.rs
+++ substrate/deprecated/hashing/proc-macro/src/lib.rs
@@ -15,4 +15,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//! This package has been deprecated. Please use `sp-crypto-hashing-proc-macro`.
+//!
+//! Removal scheduled after June 2024.
+
 pub use sp_crypto_hashing_proc_macro::*;
