7e750da0a776d80e6f5ca68d9af8cfe437a01345
Svyatoslav Nikolsky <svyatonik@gmail.com>
2024-01-23 17:24:54 +0300

    fix wait-account-nonce-finalized.js

--- bridges/zombienet/helpers/wait-account-nonce-finalized.js
+++ bridges/zombienet/helpers/wait-account-nonce-finalized.js
@@ -2,15 +2,22 @@ async function run(nodeName, networkInfo, args) {
     const {wsUri, userDefinedTypes} = networkInfo.nodesByName[nodeName];
     const api = await zombie.connect(wsUri, userDefinedTypes);
 
-    const account = args[0];
-    const expectedNonce = Number(args[1]);
-    
+    const exitAfterSeconds = Number(args[0]);
+    const account = args[1];
+    const expectedNonce = Number(args[2]);
+
     api.rpc.chain.subscribeFinalizedHeads(async function (header) {
-        const accountState = await api.query.system.account(account);
-        if (accountState.nonce.toNumber() >= expectedNonce) {
+        const apiAtCurrent = await api.at(header.hash);
+        const accountState = await apiAtCurrent.query.system.account(account);
+        const accountNonce = accountState.nonce.toNumber();
+        if (accountNonce >= expectedNonce) {
+            console.log("Nonce of account " + account + " is " + accountNonce + " at block " + header.hash);
             process.exit();
         }
     });
+
+    await new Promise(resolve => setTimeout(resolve, exitAfterSeconds * 1000));
+    throw new Error("Account transactions are not included into finalized block. Too many reorgs?");
 }
 
 module.exports = { run }
