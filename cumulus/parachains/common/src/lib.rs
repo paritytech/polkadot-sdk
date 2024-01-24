82114c3e025fbeddc92877faa322708f606bfe6e
Branislav Kontur <bkontur@gmail.com>
2024-01-24 11:02:04 +0100

    Removed `-native` suffix

--- cumulus/parachains/common/src/lib.rs
+++ cumulus/parachains/common/src/lib.rs
@@ -17,9 +17,9 @@
 
 pub mod impls;
 pub mod message_queue;
-#[cfg(feature = "rococo-native")]
+#[cfg(feature = "rococo")]
 pub mod rococo;
-#[cfg(feature = "westend-native")]
+#[cfg(feature = "westend")]
 pub mod westend;
 pub mod xcm_config;
 pub use constants::*;
