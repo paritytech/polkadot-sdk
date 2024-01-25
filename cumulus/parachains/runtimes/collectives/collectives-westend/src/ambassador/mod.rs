5c6b2cf55e7ac52706c7e84ab8a5e6d0b00c5a6f
Damilare <dharjeezy@gmail.com>
2024-01-26 00:21:20 +0100

    nit

--- cumulus/parachains/runtimes/collectives/collectives-westend/src/ambassador/mod.rs
+++ cumulus/parachains/runtimes/collectives/collectives-westend/src/ambassador/mod.rs
@@ -34,6 +34,7 @@ mod tracks;
 use super::*;
 use crate::xcm_config::{FellowshipAdminBodyId, LocationToAccountId, WndAssetHub};
 use frame_support::traits::{EitherOf, MapSuccess, TryMapSuccess};
+use frame_system::EnsureRootWithSuccess;
 pub use origins::pallet_origins as pallet_ambassador_origins;
 use origins::pallet_origins::{
 	EnsureAmbassadorsVoice, EnsureAmbassadorsVoiceFrom, EnsureHeadAmbassadorsVoice, Origin,
@@ -110,6 +111,7 @@ impl pallet_ranked_collective::Config<AmbassadorCollectiveInstance> for Runtime 
 	type RuntimeEvent = RuntimeEvent;
 	type PromoteOrigin = PromoteOrigin;
 	type DemoteOrigin = DemoteOrigin;
+	type ExchangeOrigin = ExchangeOrigin;
 	type Polls = AmbassadorReferenda;
 	type MinRankOfClass = sp_runtime::traits::Identity;
 	type VoteWeight = pallet_ranked_collective::Linear;
