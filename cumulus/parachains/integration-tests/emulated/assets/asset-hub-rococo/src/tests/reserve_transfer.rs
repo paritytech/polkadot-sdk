2b80f3b327fd8f4a7f07215f08f2f48d14769451
Adrian Catangiu <adrian@parity.io>
2024-01-26 18:50:03 +0200

    fix regression test
    Signed-off-by: Adrian Catangiu <adrian@parity.io>

--- cumulus/parachains/integration-tests/emulated/assets/asset-hub-rococo/src/tests/reserve_transfer.rs
+++ cumulus/parachains/integration-tests/emulated/assets/asset-hub-rococo/src/tests/reserve_transfer.rs
@@ -102,10 +102,7 @@ fn system_para_to_para_assets_assertions(t: SystemParaToParaTest) {
 
 fn para_to_para_sender_assertions(t: ParaToParaTest) {
 	type RuntimeEvent = <PenpalRococoA as Chain>::RuntimeEvent;
-	PenpalRococoA::assert_xcm_pallet_attempted_complete(Some(Weight::from_parts(
-		864_610_000,
-		8_799,
-	)));
+	PenpalRococoA::assert_xcm_pallet_attempted_complete(None);
 	assert_expected_events!(
 		PenpalRococoA,
 		vec![
@@ -130,7 +127,6 @@ fn para_to_para_relay_hop_assertions(t: ParaToParaTest) {
 		Rococo::sovereign_account_id_of(Rococo::child_location_of(PenpalRococoA::para_id()));
 	let sov_penpal_b_on_rococo =
 		Rococo::sovereign_account_id_of(Rococo::child_location_of(PenpalRococoB::para_id()));
-	println!("Rococo events: {:?}", <Rococo as Chain>::events());
 	assert_expected_events!(
 		Rococo,
 		vec![
@@ -147,23 +143,25 @@ fn para_to_para_relay_hop_assertions(t: ParaToParaTest) {
 			) => {
 				who: *who == sov_penpal_b_on_rococo,
 			},
-			// RuntimeEvent::MessageQueue(
-			// 	pallet_message_queue::Event::Processed { success: true, .. }
-			// ) => {},
+			RuntimeEvent::MessageQueue(
+				pallet_message_queue::Event::Processed { success: true, .. }
+			) => {},
 		]
 	);
 }
 
 fn para_to_para_receiver_assertions(_: ParaToParaTest) {
 	type RuntimeEvent = <PenpalRococoB as Chain>::RuntimeEvent;
-	println!("PenpalRococoB events: {:?}", <Rococo as Chain>::events());
 	assert_expected_events!(
 		PenpalRococoB,
 		vec![
 			RuntimeEvent::Balances(pallet_balances::Event::Deposit { .. }) => {},
-			// RuntimeEvent::MessageQueue(
-			// 	pallet_message_queue::Event::Processed { success: true, .. }
-			// ) => {},
+			RuntimeEvent::DmpQueue(
+				cumulus_pallet_dmp_queue::Event::ExecutedDownward {
+					outcome: Outcome::Complete(..),
+					..
+				}
+			) => {},
 		]
 	);
 }
@@ -231,15 +229,72 @@ fn system_para_to_para_reserve_transfer_assets(t: SystemParaToParaTest) -> Dispa
 	)
 }
 
-fn para_to_para_limited_reserve_transfer_assets(t: ParaToParaTest) -> DispatchResult {
-	<PenpalRococoA as PenpalRococoAPallet>::PolkadotXcm::limited_reserve_transfer_assets(
-		t.signed_origin,
-		bx!(t.args.dest.into()),
-		bx!(t.args.beneficiary.into()),
-		bx!(t.args.assets.into()),
-		t.args.fee_asset_item,
+// function assumes fees and assets have the same remote reserve
+fn remote_reserve_transfer_program(
+	reserve: MultiLocation,
+	dest: MultiLocation,
+	beneficiary: MultiLocation,
+	assets: Vec<MultiAsset>,
+	fees: MultiAsset,
+	weight_limit: WeightLimit,
+) -> Xcm<penpal_runtime::RuntimeCall> {
+	use xcm_emulator::{Get, Parachain};
+	let max_assets = assets.len() as u32;
+	let context = X1(Parachain(<PenpalRococoA as Parachain>::ParachainInfo::get().into()));
+	// we spend up to half of fees for execution on reserve and other half for execution on
+	// destination
+	let (fees_half_1, fees_half_2) = match fees.fun {
+		Fungible(amount) => {
+			let fee1 = amount.saturating_div(2);
+			let fee2 = amount.saturating_sub(fee1);
+			assert!(fee1 > 0);
+			assert!(fee2 > 0);
+			(MultiAsset::from((fees.id.clone(), fee1)), MultiAsset::from((fees.id.clone(), fee2)))
+		},
+		NonFungible(_) => unreachable!(),
+	};
+	// identifies fee item as seen by `reserve` - to be used at reserve chain
+	let reserve_fees = fees_half_1.reanchored(&reserve, context).unwrap();
+	// identifies fee item as seen by `dest` - to be used at destination chain
+	let dest_fees = fees_half_2.reanchored(&dest, context).unwrap();
+	// identifies `dest` as seen by `reserve`
+	let dest = dest.reanchored(&reserve, context).unwrap();
+	// xcm to be executed at dest
+	let xcm_on_dest = Xcm(vec![
+		BuyExecution { fees: dest_fees, weight_limit: weight_limit.clone() },
+		DepositAsset { assets: Wild(AllCounted(max_assets)), beneficiary },
+	]);
+	// xcm to be executed on reserve
+	let xcm_on_reserve = Xcm(vec![
+		BuyExecution { fees: reserve_fees, weight_limit },
+		DepositReserveAsset { assets: Wild(AllCounted(max_assets)), dest, xcm: xcm_on_dest },
+	]);
+	Xcm(vec![
+		WithdrawAsset(assets.into()),
+		InitiateReserveWithdraw {
+			assets: Wild(AllCounted(max_assets)),
+			reserve,
+			xcm: xcm_on_reserve,
+		},
+	])
+}
+
+fn para_to_para_remote_reserve_transfer_native_assets(t: ParaToParaTest) -> DispatchResult {
+	let xcm = remote_reserve_transfer_program(
+		Parent.into(),
+		t.args.dest,
+		t.args.beneficiary,
+		t.args.assets.clone().into_inner(),
+		t.args.assets.into_inner().pop().unwrap(),
 		t.args.weight_limit,
+	);
+	<PenpalRococoA as PenpalRococoAPallet>::PolkadotXcm::execute(
+		t.signed_origin,
+		bx!(xcm::VersionedXcm::V3(xcm)),
+		Weight::MAX,
 	)
+	.unwrap();
+	Ok(())
 }
 
 /// Limited Reserve Transfers of native asset from Relay Chain to the System Parachain shouldn't
@@ -449,55 +504,6 @@ fn reserve_transfer_native_asset_from_system_para_to_para() {
 	// transfers
 }
 
-/// Reserve Transfers of native asset from Parachain to Parachain (through Relay reserve) should
-/// work
-#[test]
-fn reserve_transfer_native_asset_from_para_to_para() {
-	use integration_tests_common::PenpalRococoBReceiver;
-	// Init values for Penpal Parachain
-	let destination = PenpalRococoA::sibling_location_of(PenpalRococoB::para_id());
-	let beneficiary_id = PenpalRococoBReceiver::get();
-	let amount_to_send: Balance = ASSET_HUB_ROCOCO_ED * 10000;
-	let assets = (Parent, amount_to_send).into();
-
-	let test_args = TestContext {
-		sender: PenpalRococoASender::get(),
-		receiver: PenpalRococoBReceiver::get(),
-		args: para_test_args(destination, beneficiary_id, amount_to_send, assets, None, 0),
-	};
-
-	let mut test = ParaToParaTest::new(test_args);
-
-	let sender_balance_before = test.sender.balance;
-	let receiver_balance_before = test.receiver.balance;
-
-	let sender_as_seen_by_relay = Rococo::child_location_of(PenpalRococoA::para_id());
-	let sov_of_sender_on_relay = Rococo::sovereign_account_id_of(sender_as_seen_by_relay);
-
-	// fund the PenpalA's SA on Rococo with the native tokens held in reserve
-	Rococo::fund_accounts(vec![(sov_of_sender_on_relay.into(), amount_to_send * 2)]);
-
-	test.set_assertion::<PenpalRococoA>(para_to_para_sender_assertions);
-	test.set_assertion::<Rococo>(para_to_para_relay_hop_assertions);
-	test.set_assertion::<PenpalRococoB>(para_to_para_receiver_assertions);
-	test.set_dispatchable::<PenpalRococoA>(para_to_para_limited_reserve_transfer_assets);
-	test.assert();
-
-	let sender_balance_after = test.sender.balance;
-	let receiver_balance_after = test.receiver.balance;
-
-	let delivery_fees = PenpalRococoA::execute_with(|| {
-		xcm_helpers::transfer_assets_delivery_fees::<
-			<PenpalRococoXcmConfig as xcm_executor::Config>::XcmSender,
-		>(test.args.assets.clone(), 0, test.args.weight_limit, test.args.beneficiary, test.args.dest)
-	});
-
-	// Sender's balance is reduced
-	assert_eq!(sender_balance_before - amount_to_send - delivery_fees, sender_balance_after);
-	// Receiver's balance is increased
-	assert!(receiver_balance_after > receiver_balance_before);
-}
-
 /// Limited Reserve Transfers of a local asset from System Parachain to Parachain should work
 #[test]
 fn limited_reserve_transfer_asset_from_system_para_to_para() {
@@ -568,4 +574,53 @@ fn reserve_transfer_asset_from_system_para_to_para() {
 	system_para_test
 		.set_dispatchable::<AssetHubRococo>(system_para_to_para_reserve_transfer_assets);
 	system_para_test.assert();
+}
+
+/// Reserve Transfers of native asset from Parachain to Parachain (through Relay reserve) should
+/// work
+#[test]
+fn reserve_transfer_native_asset_from_para_to_para() {
+	use integration_tests_common::PenpalRococoBReceiver;
+	// Init values for Penpal Parachain
+	let destination = PenpalRococoA::sibling_location_of(PenpalRococoB::para_id());
+	let beneficiary_id = PenpalRococoBReceiver::get();
+	let amount_to_send: Balance = ASSET_HUB_ROCOCO_ED * 10000;
+	let assets = (Parent, amount_to_send).into();
+
+	let test_args = TestContext {
+		sender: PenpalRococoASender::get(),
+		receiver: PenpalRococoBReceiver::get(),
+		args: para_test_args(destination, beneficiary_id, amount_to_send, assets, None, 0),
+	};
+
+	let mut test = ParaToParaTest::new(test_args);
+
+	let sender_balance_before = test.sender.balance;
+	let receiver_balance_before = test.receiver.balance;
+
+	let sender_as_seen_by_relay = Rococo::child_location_of(PenpalRococoA::para_id());
+	let sov_of_sender_on_relay = Rococo::sovereign_account_id_of(sender_as_seen_by_relay);
+
+	// fund the PenpalA's SA on Rococo with the native tokens held in reserve
+	Rococo::fund_accounts(vec![(sov_of_sender_on_relay.into(), amount_to_send * 2)]);
+
+	test.set_assertion::<PenpalRococoA>(para_to_para_sender_assertions);
+	test.set_assertion::<Rococo>(para_to_para_relay_hop_assertions);
+	test.set_assertion::<PenpalRococoB>(para_to_para_receiver_assertions);
+	test.set_dispatchable::<PenpalRococoA>(para_to_para_remote_reserve_transfer_native_assets);
+	test.assert();
+
+	let sender_balance_after = test.sender.balance;
+	let receiver_balance_after = test.receiver.balance;
+
+	let delivery_fees = PenpalRococoA::execute_with(|| {
+		xcm_helpers::transfer_assets_delivery_fees::<
+			<PenpalRococoXcmConfig as xcm_executor::Config>::XcmSender,
+		>(test.args.assets.clone(), 0, test.args.weight_limit, test.args.beneficiary, test.args.dest)
+	});
+
+	// Sender's balance is reduced
+	assert_eq!(sender_balance_before - amount_to_send - delivery_fees, sender_balance_after);
+	// Receiver's balance is increased
+	assert!(receiver_balance_after > receiver_balance_before);
 }
--- cumulus/parachains/runtimes/testing/penpal/src/xcm_config.rs
+++ cumulus/parachains/runtimes/testing/penpal/src/xcm_config.rs
@@ -318,7 +318,7 @@ impl pallet_xcm::Config for Runtime {
 	type SendXcmOrigin = EnsureXcmOrigin<RuntimeOrigin, LocalOriginToLocation>;
 	type XcmRouter = XcmRouter;
 	type ExecuteXcmOrigin = EnsureXcmOrigin<RuntimeOrigin, LocalOriginToLocation>;
-	type XcmExecuteFilter = Nothing;
+	type XcmExecuteFilter = Everything;
 	// ^ Disable dispatchable execute on the XCM pallet.
 	// Needs to be `Everything` for local testing.
 	type XcmExecutor = XcmExecutor<XcmConfig>;
