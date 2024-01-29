366db13f82632ee590edd0cee31ee001ce4eb9f7
paritytech-ci <devops-team@parity.io>
2024-01-29 10:44:23 +0000

    ___Updated docs for master___

--- master/polkadot_cli/sidebar-items.js
+++ master/polkadot_cli/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":["NODE_VERSION"],"enum":["Error","Subcommand"],"fn":["prepared_overseer_builder","run","run_node"],"struct":["Cli","RunCmd"],"trait":["CoreApi","IdentifyVariant","ProvideRuntimeApi"],"type":["Block","BlockId","Result","TFullClient"]};
+window.SIDEBAR_ITEMS = {"constant":["NODE_VERSION"],"enum":["Error","Subcommand"],"fn":["run","run_node","validator_overseer_builder"],"struct":["Cli","RunCmd"],"trait":["CoreApi","IdentifyVariant","ProvideRuntimeApi"],"type":["Block","BlockId","Result","TFullClient"]};
