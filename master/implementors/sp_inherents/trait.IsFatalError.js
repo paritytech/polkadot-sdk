a89fc85f0b1371bf8ce9f0c934dd80ee3d3898c0
paritytech-ci <devops-team@parity.io>
2024-01-30 19:09:26 +0000

    ___Updated docs for master___

--- master/implementors/sp_inherents/trait.IsFatalError.js
+++ master/implementors/sp_inherents/trait.IsFatalError.js
@@ -1,6 +1,6 @@
 (function() {var implementors = {
 "frame_support":[],
 "sp_inherents":[],
-"sp_timestamp":[["impl <a class=\"trait\" href=\"sp_inherents/trait.IsFatalError.html\" title=\"trait sp_inherents::IsFatalError\">IsFatalError</a> for <a class=\"enum\" href=\"sp_timestamp/enum.InherentError.html\" title=\"enum sp_timestamp::InherentError\">InherentError</a>"]],
-"sp_transaction_storage_proof":[["impl <a class=\"trait\" href=\"sp_inherents/trait.IsFatalError.html\" title=\"trait sp_inherents::IsFatalError\">IsFatalError</a> for <a class=\"enum\" href=\"sp_transaction_storage_proof/enum.InherentError.html\" title=\"enum sp_transaction_storage_proof::InherentError\">InherentError</a>"]]
+"sp_timestamp":[["impl IsFatalError for <a class=\"enum\" href=\"sp_timestamp/enum.InherentError.html\" title=\"enum sp_timestamp::InherentError\">InherentError</a>"]],
+"sp_transaction_storage_proof":[["impl IsFatalError for <a class=\"enum\" href=\"sp_transaction_storage_proof/enum.InherentError.html\" title=\"enum sp_transaction_storage_proof::InherentError\">InherentError</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
