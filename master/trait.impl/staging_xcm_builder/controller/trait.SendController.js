0e6761ce1747b74bf9ac2ca033e6f035b9037d34
paritytech-ci <devops-team@parity.io>
2024-02-02 22:00:40 +0000

    ___Updated docs for master___

--- master/trait.impl/staging_xcm_builder/controller/trait.SendController.js
+++ master/trait.impl/staging_xcm_builder/controller/trait.SendController.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
-"pallet_xcm":[["impl&lt;T: <a class=\"trait\" href=\"pallet_xcm/pallet/trait.Config.html\" title=\"trait pallet_xcm::pallet::Config\">Config</a>&gt; SendController&lt;&lt;T as <a class=\"trait\" href=\"frame_system/pallet/trait.Config.html\" title=\"trait frame_system::pallet::Config\">Config</a>&gt;::<a class=\"associatedtype\" href=\"frame_system/pallet/trait.Config.html#associatedtype.RuntimeOrigin\" title=\"type frame_system::pallet::Config::RuntimeOrigin\">RuntimeOrigin</a>&gt; for <a class=\"struct\" href=\"pallet_xcm/pallet/struct.Pallet.html\" title=\"struct pallet_xcm::pallet::Pallet\">Pallet</a>&lt;T&gt;"]],
+"pallet_xcm":[["impl&lt;T: <a class=\"trait\" href=\"pallet_xcm/pallet/trait.Config.html\" title=\"trait pallet_xcm::pallet::Config\">Config</a>&gt; SendController&lt;&lt;T as Config&gt;::RuntimeOrigin&gt; for <a class=\"struct\" href=\"pallet_xcm/pallet/struct.Pallet.html\" title=\"struct pallet_xcm::pallet::Pallet\">Pallet</a>&lt;T&gt;"]],
 "staging_xcm_builder":[]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
