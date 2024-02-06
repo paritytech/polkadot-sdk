c2e28c1e9810527ac2e338b4295450f8d1a7d39e
Squirrel <gilescope@gmail.com>
2024-02-06 13:01:29 +0000

    sp-std -> core (#3199)
    First in a series of PRs that reduces our use of sp-std with a view to
    deprecating it.
    
    This is just looking at /substrate and moving some of the references
    from `sp-std` to `core`.
    These particular changes should be uncontroversial.
    
    Where macros are used `::core` should be used to remove any ambiguity.
    
    part of https://github.com/paritytech/polkadot-sdk/issues/2101

--- substrate/frame/assets/src/migration.rs
+++ substrate/frame/assets/src/migration.rs
@@ -64,7 +64,7 @@ pub mod v1 {
 		}
 	}
 
-	pub struct MigrateToV1<T>(sp_std::marker::PhantomData<T>);
+	pub struct MigrateToV1<T>(core::marker::PhantomData<T>);
 	impl<T: Config> OnRuntimeUpgrade for MigrateToV1<T> {
 		fn on_runtime_upgrade() -> Weight {
 			let current_version = Pallet::<T>::current_storage_version();
