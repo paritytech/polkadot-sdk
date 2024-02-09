363b22e32ee2acc04bee3164a2db3764895521d9
Serban Iorga <serban@parity.io>
2024-02-09 12:15:55 +0100

    Fix paths

--- bridges/zombienet/run-new-test.sh
+++ bridges/zombienet/run-new-test.sh
@@ -11,8 +11,8 @@ export POLKADOT_SDK_PATH=`realpath ${BASH_SOURCE%/*}/../..`
 export POLKADOT_BINARY=$POLKADOT_SDK_PATH/target/release/polkadot
 export POLKADOT_PARACHAIN_BINARY=$POLKADOT_SDK_PATH/target/release/polkadot-parachain
 
-export ZOMBIENET_BINARY=/home/serban/scripts/zombienet-linux-x64
-export SUBSTRATE_RELAY_BINARY=/home/serban/workplace/sources/parity-bridges-common/target/release/substrate-relay
+export ZOMBIENET_BINARY=~/local_bridge_testing/bin/zombienet-linux-x64
+export SUBSTRATE_RELAY_BINARY=~/local_bridge_testing/bin/substrate-relay
 
 export TEST_DIR=`mktemp -d /tmp/bridges-tests-run-XXXXX`
 echo -e "Test folder: $TEST_DIR\n"
