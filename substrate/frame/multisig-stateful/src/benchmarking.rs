36650a70184817e3e94a6f7626ee91344b9acdc7
asoliman <abdelrahman.ihab.soliman@gmail.com>
2024-02-13 04:22:22 +0800

    Fix: non-used variable benchmarks

--- substrate/frame/multisig-stateful/src/benchmarking.rs
+++ substrate/frame/multisig-stateful/src/benchmarking.rs
@@ -18,7 +18,7 @@ mod benchmarks {
 		owners.try_insert(signatory.clone()).unwrap();
 		// owners.try_insert(1).unwrap();
 
-		let multisig_account =
+		let _multisig_account =
 			Multisig::<T>::get_multisig_account_id(&owners, Multisig::<T>::timepoint());
 
 		#[extrinsic_call]
@@ -26,16 +26,6 @@ mod benchmarks {
 		assert_eq!(true, false);
 		// assert_eq!(MultisigAccount::<Test>::get(multisig_account).unwrap().owners, owners);
 	}
-
-	// #[benchmark]
-	// fn cause_error() {
-	// 	Something::<T>::put(100u32);
-	// 	let caller: T::AccountId = whitelisted_caller();
-	// 	#[extrinsic_call]
-	// 	cause_error(RawOrigin::Signed(caller));
-
-	// 	assert_eq!(MultisigAccount::<T>::get(1), Some());
-	// }
 
 	impl_benchmark_test_suite!(Multisig, crate::mock::new_test_ext(), crate::mock::Test);
 }
