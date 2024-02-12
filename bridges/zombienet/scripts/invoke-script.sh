dfc8e4696c6edfb76ccb05f469a221ebb5b270ff
Serban Iorga <serban@parity.io>
2024-02-12 10:55:57 +0100

    Bridge zombienet tests refactoring (#3260)
    Related to https://github.com/paritytech/polkadot-sdk/issues/3242
    
    Reorganizing the bridge zombienet tests in order to:
    - separate the environment spawning from the actual tests
    - offer better control over the tests and some possibility to
    orchestrate them as opposed to running everything from the zndsl file
    
    Only rewrote the asset transfer test using this new "framework". The old
    logic and old tests weren't functionally modified or deleted. The plan
    is to get feedback on this approach first and if this is agreed upon,
    migrate the other 2 tests later in separate PRs and also do other
    improvements later.

--- bridges/zombienet/scripts/invoke-script.sh
+++ bridges/zombienet/scripts/invoke-script.sh
@@ -2,6 +2,6 @@
 
 INVOKE_LOG=`mktemp -p $TEST_FOLDER invoke.XXXXX`
 
-pushd $POLKADOT_SDK_FOLDER/cumulus/scripts
+pushd $POLKADOT_SDK_PATH/cumulus/scripts
 ./bridges_rococo_westend.sh $1 >$INVOKE_LOG 2>&1
 popd
