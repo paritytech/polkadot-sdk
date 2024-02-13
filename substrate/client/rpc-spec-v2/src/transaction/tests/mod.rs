52c3afb83b8071280292db749702be5bcbf9d58a
Alexandru Vasile <alexandru.vasile@parity.io>
2024-02-13 13:36:26 +0200

    tx/tests: Add mock transaction pool to gain access to tx status
    Signed-off-by: Alexandru Vasile <alexandru.vasile@parity.io>

--- substrate/client/rpc-spec-v2/src/transaction/tests/mod.rs
+++ substrate/client/rpc-spec-v2/src/transaction/tests/mod.rs
@@ -16,5 +16,6 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-#[cfg(test)]
+mod middleware_pool;
+
 mod transaction_broadcast_tests;
