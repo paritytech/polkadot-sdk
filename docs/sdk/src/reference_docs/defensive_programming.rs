89926c1eafa78eb68493491704f26377a73fe192
bader y <ibnbassem@gmail.com>
2024-02-13 10:26:12 -0500

    Update docs/sdk/src/reference_docs/defensive_programming.rs
    Co-authored-by: Radha <86818441+DrW3RK@users.noreply.github.com>

--- docs/sdk/src/reference_docs/defensive_programming.rs
+++ docs/sdk/src/reference_docs/defensive_programming.rs
@@ -248,9 +248,6 @@
 //! that type. Imagine this in the context of a blockchain, where there are account balances, voting
 //! counters, nonces for transactions, and other aspects of a blockchain.
 //!
-//! Some of these mechanisms can be more critical than others. It's for this reason that we may
-//! consider some other ways of dealing with runtime arithmetic, such as saturated or checked
-//! operations, that won't carry these potential consequences.
 //!
 //! While it may seem trivial, choosing how to handle numbers is quite important. As a thought
 //! exercise, here are some scenarios of which will shed more light on when to use which.
