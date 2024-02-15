d2c6154ad631572a85c02b97838e4836e7b28db9
Niklas Adolfsson <niklasadolfsson1@gmail.com>
2024-02-15 09:20:29 +0100

    Update substrate/client/rpc-servers/src/middleware/mod.rs

--- substrate/client/rpc-servers/src/middleware/mod.rs
+++ substrate/client/rpc-servers/src/middleware/mod.rs
@@ -16,7 +16,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-//! RPC middleware to collect prometheus metrics on RPC calls.
+//! JSON-RPC specific middleware.
 
 /// Grafana metrics middleware.
 pub mod metrics;
