77dc46302904872eb462cead200c3b16802a5b5c
Dmitry Sinyavin <dmitry.sinyavin@parity.io>
2024-02-18 20:38:46 +0100

    Add docs

--- substrate/frame/im-online/src/migration.rs
+++ substrate/frame/im-online/src/migration.rs
@@ -116,6 +116,8 @@ pub mod v1 {
 	}
 }
 
+/// Clears the pallet's offchain storage. Should be used in [offchain_primitives::OffchainWorkerApi]
+/// implementation in runtime after the pallet was removed.
 pub fn clear_offchain_storage(validator_set_size: u32) {
 	(0..validator_set_size).for_each(|idx| {
 		let key = {
