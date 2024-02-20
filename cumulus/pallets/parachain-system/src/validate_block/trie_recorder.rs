872f17053e5b2c0caabc5cdc04e5056e1fe51a77
Sebastian Kunert <skunert49@gmail.com>
2024-02-20 12:04:08 +0100

    Add small test for reset

--- cumulus/pallets/parachain-system/src/validate_block/trie_recorder.rs
+++ cumulus/pallets/parachain-system/src/validate_block/trie_recorder.rs
@@ -289,6 +289,9 @@ mod tests {
 				reference_recorder.estimate_encoded_size(),
 				recorder_for_test.estimate_encoded_size()
 			);
+
+			recorder_for_test.reset();
+			assert_eq!(recorder_for_test.estimate_encoded_size(), 0)
 		}
 	}
 }
