9f843c22e7b2f54d138ad934ed87dbac183bd932
paritytech-ci <devops-team@parity.io>
2024-02-21 14:34:52 +0000

    ___Updated docs for master___

--- master/trait.impl/sp_mmr_primitives/trait.OnNewRoot.js
+++ master/trait.impl/sp_mmr_primitives/trait.OnNewRoot.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
-"pallet_beefy_mmr":[["impl&lt;T&gt; OnNewRoot&lt;H256&gt; for <a class=\"struct\" href=\"pallet_beefy_mmr/struct.DepositBeefyDigest.html\" title=\"struct pallet_beefy_mmr::DepositBeefyDigest\">DepositBeefyDigest</a>&lt;T&gt;<span class=\"where fmt-newline\">where\n    T: Config&lt;Hashing = MmrHashing&gt; + <a class=\"trait\" href=\"pallet_beefy/pallet/trait.Config.html\" title=\"trait pallet_beefy::pallet::Config\">Config</a>,</span>"]],
+"pallet_beefy_mmr":[["impl&lt;T&gt; OnNewRoot&lt;H256&gt; for <a class=\"struct\" href=\"pallet_beefy_mmr/struct.DepositBeefyDigest.html\" title=\"struct pallet_beefy_mmr::DepositBeefyDigest\">DepositBeefyDigest</a>&lt;T&gt;<span class=\"where fmt-newline\">where\n    T: Config&lt;Hashing = MmrHashing&gt; + Config,</span>"]],
 "sp_mmr_primitives":[]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
