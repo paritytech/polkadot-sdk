bdc8c28993536d6409e2c9b840ad9ac03614f5c3
Ankan <ankan.anurag@gmail.com>
2024-02-22 11:39:46 +0100

    remove unused fn

--- substrate/frame/delegated-staking/src/mock.rs
+++ substrate/frame/delegated-staking/src/mock.rs
@@ -325,11 +325,6 @@ pub(crate) fn eq_stake(who: AccountId, total: Balance, active: Balance) -> bool 
 	Staking::stake(&who).unwrap() == Stake { total, active }
 }
 
-pub(crate) fn delegate_bonded(delegate: &AccountId) -> bool {
-	let delegate = Delegate::<T>::from(delegate).expect("delegate should exist");
-	delegate.is_bonded()
-}
-
 pub(crate) fn get_delegate(delegate: &AccountId) -> Delegate<T> {
 	Delegate::<T>::from(delegate).expect("delegate should exist")
 }
