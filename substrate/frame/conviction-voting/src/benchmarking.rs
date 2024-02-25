620f7e7cf951d0af1d4356052a9a528023538c3f
Piet Wolff <piet.wolff@gmail.com>
2024-02-25 15:41:07 +0100

    thaw previous freezes

--- substrate/frame/conviction-voting/src/benchmarking.rs
+++ substrate/frame/conviction-voting/src/benchmarking.rs
@@ -55,6 +55,8 @@ fn fill_voting<T: Config<I>, I: 'static>(
 fn funded_account<T: Config<I>, I: 'static>(name: &'static str, index: u32) -> T::AccountId {
 	let caller: T::AccountId = account(name, index, SEED);
 	T::Currency::set_balance(&caller, 100_000_000u32.into());
+	// remove all freezes from previous benchmarks so we never run out of available funds
+	T::Currency::thaw(&FreezeReason::ConvictionVoting.into(), &caller);
 	caller
 }
 
