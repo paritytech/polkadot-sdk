5304fa1fee7d66aed59906326fc146eb301c37bf
claravanstaden <Cats 4 life!>
2024-02-26 10:57:24 +0200

    add comment

--- cumulus/parachains/runtimes/bridge-hubs/bridge-hub-rococo/src/lib.rs
+++ cumulus/parachains/runtimes/bridge-hubs/bridge-hub-rococo/src/lib.rs
@@ -365,10 +365,14 @@ parameter_types! {
 impl pallet_message_queue::Config for Runtime {
 	type RuntimeEvent = RuntimeEvent;
 	type WeightInfo = weights::pallet_message_queue::WeightInfo<Runtime>;
-	#[cfg(all(not(feature = "std"), feature = "runtime-benchmarks"))]
+	// Use the NoopMessageProcessor exclusively for benchmarks, not for tests with the
+	// runtime-benchmarks feature as tests require the BridgeHubMessageRouter to process messages.
+	// The "test" feature flag doesn't work, hence the reliance on the "std" feature, which is
+	// enabled during tests.
+	#[cfg(all(not(feature = "std"), not(test), feature = "runtime-benchmarks"))]
 	type MessageProcessor =
 		pallet_message_queue::mock_helpers::NoopMessageProcessor<AggregateMessageOrigin>;
-	#[cfg(not(all(not(feature = "std"), feature = "runtime-benchmarks")))]
+	#[cfg(not(all(not(feature = "std"), not(test), feature = "runtime-benchmarks")))]
 	type MessageProcessor = bridge_hub_common::BridgeHubMessageRouter<
 		xcm_builder::ProcessXcmMessage<
 			AggregateMessageOrigin,
