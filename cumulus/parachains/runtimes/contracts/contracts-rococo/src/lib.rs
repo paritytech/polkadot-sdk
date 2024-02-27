cbde16264c4bc37db46129a1fc6f72ec24f14ff1
EgorPopelyaev <egor@parity.io>
2024-02-27 12:07:19 +0100

    bump rococo runtimes spec_version

--- cumulus/parachains/runtimes/contracts/contracts-rococo/src/lib.rs
+++ cumulus/parachains/runtimes/contracts/contracts-rococo/src/lib.rs
@@ -134,7 +134,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("contracts-rococo"),
 	impl_name: create_runtime_str!("contracts-rococo"),
 	authoring_version: 1,
-	spec_version: 1_007_000,
+	spec_version: 1_007_002,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 6,
