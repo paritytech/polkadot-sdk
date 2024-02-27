8cc28515080e81544d9ef1b855c35332979c37dd
Gilt0 <gil-arnaud.coche.2006@polytechnique.org>
2024-02-28 07:59:14 +0900

    Setting BlockNumber as u32 in tests for asset-rate pallet

--- substrate/frame/asset-rate/src/mock.rs
+++ substrate/frame/asset-rate/src/mock.rs
@@ -21,7 +21,7 @@ use crate as pallet_asset_rate;
 use frame_support::{derive_impl, traits::ConstU64};
 use sp_runtime::BuildStorage;
 
-type Block = frame_system::mocking::MockBlock<Test>;
+type Block = frame_system::mocking::MockBlockU32<Test>;
 
 frame_support::construct_runtime!(
 	pub enum Test
@@ -36,6 +36,7 @@ frame_support::construct_runtime!(
 impl frame_system::Config for Test {
 	type Block = Block;
 	type AccountData = pallet_balances::AccountData<u64>;
+	type BlockHashCount = frame_support::traits::ConstU32<10>;
 }
 
 impl pallet_balances::Config for Test {
