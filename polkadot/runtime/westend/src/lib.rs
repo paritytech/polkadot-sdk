0d7ca59fd4b4d15f9400971f32bc6a51b2fe837f
EgorPopelyaev <egor@parity.io>
2024-02-27 13:40:53 +0100

    Bump spec_version

--- polkadot/runtime/westend/src/lib.rs
+++ polkadot/runtime/westend/src/lib.rs
@@ -147,7 +147,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("westend"),
 	impl_name: create_runtime_str!("parity-westend"),
 	authoring_version: 2,
-	spec_version: 1_007_000,
+	spec_version: 1_007_002,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 24,
