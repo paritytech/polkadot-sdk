6c008e4aadc016f64ab7f7512ac662059f4df0f1
claravanstaden <Cats 4 life!>
2024-02-28 10:38:47 +0200

    cleanup

--- bridges/snowbridge/scripts/contribute-upstream.sh
+++ bridges/snowbridge/scripts/contribute-upstream.sh
@@ -78,8 +78,3 @@ git fetch parity master
 git checkout parity/master -- .github
 git add -- .github
 
-# Fetch the latest from parity master
-echo "Fetching latest from Parity master. Resolve merge conflicts, if there are any."
-git fetch parity master
-git merge parity/master
-echo "OK"
