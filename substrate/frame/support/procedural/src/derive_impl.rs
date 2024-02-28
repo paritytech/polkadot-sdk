bcf62b8030ae744b5c08971e4a0b633d03b8ce4a
Nikhil Gupta <17176722+gupnik@users.noreply.github.com>
2024-02-28 16:53:58 +0530

    Computes disambiguation path

--- substrate/frame/support/procedural/src/derive_impl.rs
+++ substrate/frame/support/procedural/src/derive_impl.rs
@@ -172,6 +172,31 @@ fn combine_impls(
 	final_impl
 }
 
+fn compute_disambiguation_path(
+	disambiguation_path: Option<Path>,
+	foreign_impl: ItemImpl,
+	default_impl_path: Path,
+) -> Result<Path> {
+	match (disambiguation_path, foreign_impl.clone().trait_) {
+		(Some(disambiguation_path), _) => Ok(disambiguation_path),
+		// In case `as [disambiguation_path]` is not specified in the macro attr, we try to
+		// infer the disambiguation path from the `foreign_impl_path` and the computed scope.
+		// Else, we default to the foreign_impl_path.
+		(None, Some((_, foreign_impl_path, _))) =>
+			if default_impl_path.segments.len() > 1 {
+				let scope = default_impl_path.segments.first();
+				Ok(parse_quote!(#scope :: #foreign_impl_path))
+			} else {
+				Ok(foreign_impl_path)
+			},
+		_ => Err(syn::Error::new(
+			default_impl_path.span(),
+			"Impl statement must have a defined type being implemented \
+			for a defined type such as `impl A for B`",
+		)),
+	}
+}
+
 /// Internal implementation behind [`#[derive_impl(..)]`](`macro@crate::derive_impl`).
 ///
 /// `default_impl_path`: the module path of the external `impl` statement whose tokens we are
@@ -194,18 +219,11 @@ pub fn derive_impl(
 	let foreign_impl = parse2::<ItemImpl>(foreign_tokens)?;
 	let default_impl_path = parse2::<Path>(default_impl_path)?;
 
-	// have disambiguation_path default to the item being impl'd in the foreign impl if we
-	// don't specify an `as [disambiguation_path]` in the macro attr
-	let disambiguation_path = match (disambiguation_path, foreign_impl.clone().trait_) {
-		(Some(disambiguation_path), _) => disambiguation_path,
-		(None, Some((_, foreign_impl_path, _))) => foreign_impl_path,
-		_ =>
-			return Err(syn::Error::new(
-				foreign_impl.span(),
-				"Impl statement must have a defined type being implemented \
-				for a defined type such as `impl A for B`",
-			)),
-	};
+	let disambiguation_path = compute_disambiguation_path(
+		disambiguation_path,
+		foreign_impl.clone(),
+		default_impl_path.clone(),
+	)?;
 
 	// generate the combined impl
 	let combined_impl = combine_impls(
