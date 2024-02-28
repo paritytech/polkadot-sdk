bddefbabf8a1d9659fdade3ddaa46345162cba3e
Andrei Sandu <andrei-mihail@parity.io>
2024-02-28 15:00:22 +0200

    enable elastic scaling node feature by genesis for local testnets
    Signed-off-by: Andrei Sandu <andrei-mihail@parity.io>

--- polkadot/node/service/src/chain_spec.rs
+++ polkadot/node/service/src/chain_spec.rs
@@ -120,7 +120,9 @@ pub fn wococo_config() -> Result<RococoChainSpec, String> {
 fn default_parachains_host_configuration(
 ) -> polkadot_runtime_parachains::configuration::HostConfiguration<polkadot_primitives::BlockNumber>
 {
-	use polkadot_primitives::{AsyncBackingParams, MAX_CODE_SIZE, MAX_POV_SIZE};
+	use polkadot_primitives::{
+		vstaging::node_features::FeatureIndex, AsyncBackingParams, MAX_CODE_SIZE, MAX_POV_SIZE,
+	};
 
 	polkadot_runtime_parachains::configuration::HostConfiguration {
 		validation_upgrade_cooldown: 2u32,
@@ -156,6 +158,9 @@ fn default_parachains_host_configuration(
 			max_candidate_depth: 3,
 			allowed_ancestry_len: 2,
 		},
+		node_features: bitvec::vec::BitVec::from_element(
+			1u8 << (FeatureIndex::ElasticScalingMVP as usize),
+		),
 		..Default::default()
 	}
 }
