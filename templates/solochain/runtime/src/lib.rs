3a83c7f252d444953a41dde614e4d3b7a69450b4
kianenigma <kian@parity.io>
2024-03-04 12:42:53 +0000

    revert to enum for now

--- templates/solochain/runtime/src/lib.rs
+++ templates/solochain/runtime/src/lib.rs
@@ -253,7 +253,7 @@ impl pallet_template::Config for Runtime {
 
 // Create the runtime by composing the FRAME pallets that were previously configured.
 construct_runtime!(
-	pub struct Runtime {
+	pub enum Runtime {
 		System: frame_system,
 		Timestamp: pallet_timestamp,
 		Aura: pallet_aura,
