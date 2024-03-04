545931678c2441c7fcedc73847c7834db0771b1b
kianenigma <kian@parity.io>
2024-03-04 12:42:20 +0000

    unify versions and spec names

--- templates/parachain/runtime/src/lib.rs
+++ templates/parachain/runtime/src/lib.rs
@@ -180,8 +180,8 @@ impl_opaque_keys! {
 
 #[sp_version::runtime_version]
 pub const VERSION: RuntimeVersion = RuntimeVersion {
-	spec_name: create_runtime_str!("template-parachain"),
-	impl_name: create_runtime_str!("template-parachain"),
+	spec_name: create_runtime_str!("parachain-template-runtime"),
+	impl_name: create_runtime_str!("parachain-template-runtime"),
 	authoring_version: 1,
 	spec_version: 1,
 	impl_version: 0,
--- templates/solochain/runtime/src/lib.rs
+++ templates/solochain/runtime/src/lib.rs
@@ -91,8 +91,8 @@ pub mod opaque {
 // https://docs.substrate.io/main-docs/build/upgrade#runtime-versioning
 #[sp_version::runtime_version]
 pub const VERSION: RuntimeVersion = RuntimeVersion {
-	spec_name: create_runtime_str!("node-template"),
-	impl_name: create_runtime_str!("node-template"),
+	spec_name: create_runtime_str!("solochain-template-runtime"),
+	impl_name: create_runtime_str!("solochain-template-runtime"),
 	authoring_version: 1,
 	// The version of the runtime specification. A full node will not attempt to use its native
 	//   runtime in substitute for the on-chain Wasm runtime unless all of `spec_name`,
