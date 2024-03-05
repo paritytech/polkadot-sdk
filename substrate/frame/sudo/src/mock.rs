7968266c1c46babe08080883bdc17950fcf6bdc3
Gilt0 <gil-arnaud.coche.2006@polytechnique.org>
2024-03-05 17:21:38 +0900

    Setting MockBlock as u32 in sudo pallet

--- substrate/frame/sudo/src/mock.rs
+++ substrate/frame/sudo/src/mock.rs
@@ -86,7 +86,7 @@ pub mod logger {
 	pub(super) type I32Log<T> = StorageValue<_, BoundedVec<i32, ConstU32<1_000>>, ValueQuery>;
 }
 
-type Block = frame_system::mocking::MockBlock<Test>;
+type Block = frame_system::mocking::MockBlockU32<Test>;
 
 frame_support::construct_runtime!(
 	pub enum Test
@@ -107,6 +107,7 @@ impl Contains<RuntimeCall> for BlockEverything {
 #[derive_impl(frame_system::config_preludes::TestDefaultConfig as frame_system::DefaultConfig)]
 impl frame_system::Config for Test {
 	type Block = Block;
+	type BlockHashCount = frame_support::traits::ConstU32<10>;
 }
 
 // Implement the logger module's `Config` on the Test runtime.
