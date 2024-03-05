8809e90c10ff2636c2d578f0756b562d7647d1eb
paritytech-ci <devops-team@parity.io>
2024-03-05 18:10:04 +0000

    ___Updated docs for master___

--- master/trait.impl/frame_support/migrations/trait.SteppedMigrations.js
+++ master/trait.impl/frame_support/migrations/trait.SteppedMigrations.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
 "frame_support":[],
-"pallet_migrations":[["impl <a class=\"trait\" href=\"frame_support/migrations/trait.SteppedMigrations.html\" title=\"trait frame_support::migrations::SteppedMigrations\">SteppedMigrations</a> for <a class=\"struct\" href=\"pallet_migrations/mock_helpers/struct.MockedMigrations.html\" title=\"struct pallet_migrations::mock_helpers::MockedMigrations\">MockedMigrations</a>"]]
+"pallet_migrations":[["impl SteppedMigrations for <a class=\"struct\" href=\"pallet_migrations/mock_helpers/struct.MockedMigrations.html\" title=\"struct pallet_migrations::mock_helpers::MockedMigrations\">MockedMigrations</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
