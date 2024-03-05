7c3b25325ef72bf04146fb6cab341391f42e2a87
Gilt0 <gil-arnaud.coche.2006@polytechnique.org>
2024-03-05 14:30:13 +0900

    Setting MockBlock as u32 in paged-list pallet

--- substrate/frame/paged-list/src/mock.rs
+++ substrate/frame/paged-list/src/mock.rs
@@ -22,15 +22,14 @@
 use crate::{paged_list::StoragePagedListMeta, Config, ListPrefix};
 use frame_support::{
 	derive_impl,
-	traits::{ConstU16, ConstU64},
+	traits::{ConstU16, ConstU32},
 };
-use sp_core::H256;
 use sp_runtime::{
-	traits::{BlakeTwo256, IdentityLookup},
+	traits::IdentityLookup,
 	BuildStorage,
 };
 
-type Block = frame_system::mocking::MockBlock<Test>;
+type Block = frame_system::mocking::MockBlockU32<Test>;
 
 // Configure a mock runtime to test the pallet.
 frame_support::construct_runtime!(
@@ -43,29 +42,8 @@ frame_support::construct_runtime!(
 
 #[derive_impl(frame_system::config_preludes::TestDefaultConfig as frame_system::DefaultConfig)]
 impl frame_system::Config for Test {
-	type BaseCallFilter = frame_support::traits::Everything;
-	type BlockWeights = ();
-	type BlockLength = ();
-	type DbWeight = ();
-	type RuntimeOrigin = RuntimeOrigin;
-	type RuntimeCall = RuntimeCall;
-	type Nonce = u64;
-	type Hash = H256;
-	type Hashing = BlakeTwo256;
-	type AccountId = u64;
-	type Lookup = IdentityLookup<Self::AccountId>;
 	type Block = Block;
-	type RuntimeEvent = RuntimeEvent;
-	type BlockHashCount = ConstU64<250>;
-	type Version = ();
-	type PalletInfo = PalletInfo;
-	type AccountData = ();
-	type OnNewAccount = ();
-	type OnKilledAccount = ();
-	type SystemWeightInfo = ();
-	type SS58Prefix = ConstU16<42>;
-	type OnSetCode = ();
-	type MaxConsumers = frame_support::traits::ConstU32<16>;
+	type BlockHashCount = ConstU32<250>;
 }
 
 frame_support::parameter_types! {
