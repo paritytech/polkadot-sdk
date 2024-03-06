9a0de59c3b38ec3979bbd45d7169707ab3dc967c
paritytech-ci <devops-team@parity.io>
2024-03-06 16:11:11 +0000

    ___Updated docs for master___

---
+++ crate-docs/sc_network_sync/types/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["PeerRequest","PeerRequestType","SyncEvent","SyncState"],"struct":["BadPeer","ExtendedPeerInfo","OpaqueStateRequest","OpaqueStateResponse","PeerInfo","SyncStatus"],"trait":["SyncEventStream","SyncStatusProvider"]};
--- master/sc_network_sync/sidebar-items.js
+++ master/sc_network_sync/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["SyncEvent","SyncState"],"mod":["block_relay_protocol","block_request_handler","blocks","engine","mock","service","state_request_handler","strategy","warp_request_handler"],"struct":["SyncStatus"],"trait":["SyncEventStream","SyncStatusProvider"]};
+window.SIDEBAR_ITEMS = {"mod":["block_relay_protocol","block_request_handler","blocks","engine","mock","service","state_request_handler","strategy","types","warp_request_handler"]};
