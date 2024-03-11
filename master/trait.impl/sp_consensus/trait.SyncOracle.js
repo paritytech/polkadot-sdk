ca680ccc2a0d2c03d9581a210a8580fe0608415c
paritytech-ci <devops-team@parity.io>
2024-03-11 15:31:01 +0000

    ___Updated docs for master___

--- master/trait.impl/sp_consensus/trait.SyncOracle.js
+++ master/trait.impl/sp_consensus/trait.SyncOracle.js
@@ -4,7 +4,7 @@
 "polkadot_test_client":[],
 "sc_consensus_aura":[],
 "sc_consensus_babe":[],
-"sc_network_sync":[["impl&lt;B: <a class=\"trait\" href=\"sp_runtime/traits/trait.Block.html\" title=\"trait sp_runtime::traits::Block\">BlockT</a>&gt; SyncOracle for <a class=\"struct\" href=\"sc_network_sync/service/syncing_service/struct.SyncingService.html\" title=\"struct sc_network_sync::service::syncing_service::SyncingService\">SyncingService</a>&lt;B&gt;"]],
+"sc_network_sync":[["impl&lt;B: BlockT&gt; <a class=\"trait\" href=\"sp_consensus/trait.SyncOracle.html\" title=\"trait sp_consensus::SyncOracle\">SyncOracle</a> for <a class=\"struct\" href=\"sc_network_sync/service/syncing_service/struct.SyncingService.html\" title=\"struct sc_network_sync::service::syncing_service::SyncingService\">SyncingService</a>&lt;B&gt;"]],
 "sp_consensus":[],
 "substrate_test_runtime_client":[]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
