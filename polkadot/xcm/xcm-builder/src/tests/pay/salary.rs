4117b949a960c91d240a966a99212a83f3ddb272
doordashcon <jessechejieh@doordashcon.local>
2024-03-11 12:03:18 +0100

    xcm & salary

--- polkadot/xcm/xcm-builder/src/tests/pay/salary.rs
+++ polkadot/xcm/xcm-builder/src/tests/pay/salary.rs
@@ -84,6 +84,9 @@ impl RankedMembers for TestClub {
 			},
 		})
 	}
+	fn max_rank() -> Self::Rank {
+		CLUB.with(|club| club.borrow().values().max().cloned().unwrap_or(Self::min_rank()))
+	}
 }
 
 fn set_rank(who: AccountId, rank: u128) {
