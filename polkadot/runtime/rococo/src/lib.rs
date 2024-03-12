09cc5f865da0b897cdffee5a1c5e55c7410ac2e5
Matteo Muraca <mmuraca247@gmail.com>
2024-03-12 16:17:40 +0100

    fixed rococo and westend fmt
    Signed-off-by: Matteo Muraca <mmuraca247@gmail.com>

--- polkadot/runtime/rococo/src/lib.rs
+++ polkadot/runtime/rococo/src/lib.rs
@@ -1267,7 +1267,9 @@ impl BeefyDataProvider<H256> for ParaHeadsRootProvider {
 	fn extra_data() -> H256 {
 		let mut para_heads: Vec<(u32, Vec<u8>)> = parachains_paras::Parachains::<Runtime>::get()
 			.into_iter()
-			.filter_map(|id| parachains_paras::Heads::<Runtime>::get(&id).map(|head| (id.into(), head.0)))
+			.filter_map(|id| {
+				parachains_paras::Heads::<Runtime>::get(&id).map(|head| (id.into(), head.0))
+			})
 			.collect();
 		para_heads.sort();
 		binary_merkle_tree::merkle_root::<mmr::Hashing, _>(
