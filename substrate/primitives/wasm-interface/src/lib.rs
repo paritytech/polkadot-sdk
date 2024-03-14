3b87116f8a76c797cdbf4e0c718a69aea8618c94
Jan Bujak <jan@parity.io>
2024-03-14 10:08:01 +0000

    Remove `sp-std` dependency from `sp-wasm-interface`

--- substrate/primitives/wasm-interface/src/lib.rs
+++ substrate/primitives/wasm-interface/src/lib.rs
@@ -19,7 +19,10 @@
 
 #![cfg_attr(not(feature = "std"), no_std)]
 
-use sp_std::{borrow::Cow, iter::Iterator, marker::PhantomData, mem, result, vec, vec::Vec};
+extern crate alloc;
+
+use alloc::{borrow::Cow, string::String, vec, vec::Vec};
+use core::{iter::Iterator, marker::PhantomData, mem};
 
 #[cfg(not(all(feature = "std", feature = "wasmtime")))]
 #[macro_export]
@@ -45,9 +48,9 @@ if_wasmtime_is_enabled! {
 
 /// Result type used by traits in this crate.
 #[cfg(feature = "std")]
-pub type Result<T> = result::Result<T, String>;
+pub type Result<T> = core::result::Result<T, String>;
 #[cfg(not(feature = "std"))]
-pub type Result<T> = result::Result<T, &'static str>;
+pub type Result<T> = core::result::Result<T, &'static str>;
 
 /// Value types supported by Substrate on the boundary between host/Wasm.
 #[derive(Copy, Clone, PartialEq, Debug, Eq)]
@@ -76,7 +79,7 @@ impl From<ValueType> for u8 {
 impl TryFrom<u8> for ValueType {
 	type Error = ();
 
-	fn try_from(val: u8) -> sp_std::result::Result<ValueType, ()> {
+	fn try_from(val: u8) -> core::result::Result<ValueType, ()> {
 		match val {
 			0 => Ok(Self::I32),
 			1 => Ok(Self::I64),
