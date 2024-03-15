574320345f196043d56352aa3e271f8c76b86121
paritytech-ci <devops-team@parity.io>
2024-03-15 18:02:37 +0000

    ___Updated docs for master___

--- master/trait.impl/sc_network/service/traits/trait.NetworkBlock.js
+++ master/trait.impl/sc_network/service/traits/trait.NetworkBlock.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
 "sc_network":[],
-"sc_network_sync":[["impl&lt;B: BlockT&gt; <a class=\"trait\" href=\"sc_network/service/traits/trait.NetworkBlock.html\" title=\"trait sc_network::service::traits::NetworkBlock\">NetworkBlock</a>&lt;&lt;B as Block&gt;::Hash, &lt;&lt;B as Block&gt;::Header as Header&gt;::Number&gt; for <a class=\"struct\" href=\"sc_network_sync/service/syncing_service/struct.SyncingService.html\" title=\"struct sc_network_sync::service::syncing_service::SyncingService\">SyncingService</a>&lt;B&gt;"]]
+"sc_network_sync":[["impl&lt;B: BlockT&gt; NetworkBlock&lt;&lt;B as Block&gt;::Hash, &lt;&lt;B as Block&gt;::Header as Header&gt;::Number&gt; for <a class=\"struct\" href=\"sc_network_sync/service/syncing_service/struct.SyncingService.html\" title=\"struct sc_network_sync::service::syncing_service::SyncingService\">SyncingService</a>&lt;B&gt;"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
