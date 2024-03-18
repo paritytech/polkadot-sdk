feeda1aeaf4906e482e8bf56b18bfe12b39fbedc
Jan Bujak <jan@parity.io>
2024-03-18 08:42:56 +0000

    Fix `check-features-variants.sh` scripts

--- substrate/primitives/core/check-features-variants.sh
+++ substrate/primitives/core/check-features-variants.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env -S bash -eux
 
-export RUSTFLAGS="-Cdebug-assertions=y -Dwarnings"
+export RUSTFLAGS="-Cdebug-assertions=y -Dwarnings --cfg substrate_runtime"
 T=wasm32-unknown-unknown
 
 cargo check --target=$T --release --no-default-features  --features="bls-experimental"
