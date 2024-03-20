af05963937547832a6bb4d265c88e3d2e93e1e9d
Liam Aharon <liam.aharon@hotmail.com>
2024-03-20 20:20:23 +1100

    add note regarding ItemOf

--- substrate/frame/support/src/traits/tokens/fungibles/mod.rs
+++ substrate/frame/support/src/traits/tokens/fungibles/mod.rs
@@ -18,6 +18,9 @@
 //! The traits for *sets* of [`fungible`](`frame_support::traits::fungible`) tokens and any
 //! associated types.
 //!
+//! Individual tokens in the `fungibles` set may be used when a `fungible` trait is expected using
+//! [`crate::traits::tokens::fungible::ItemOf`].
+//!
 //! Also see the [`frame_tokens`] reference docs for more information about the place of
 //! `fungible` traits in Substrate.
 //!
