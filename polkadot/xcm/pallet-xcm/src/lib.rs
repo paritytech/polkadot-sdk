710ef0d403551ab2538e6f518c218cead8cc4b8b
girazoki <gorka.irazoki@gmail.com>
2024-03-22 09:00:01 +0100

    Add jit_withdraw for initiate-reserve-withdraw

--- polkadot/xcm/pallet-xcm/src/lib.rs
+++ polkadot/xcm/pallet-xcm/src/lib.rs
@@ -1991,6 +1991,7 @@ impl<T: Config> Pallet<T> {
 		]);
 		Ok(Xcm(vec![
 			WithdrawAsset(assets.into()),
+			SetFeesMode { jit_withdraw: true },
 			InitiateReserveWithdraw {
 				assets: Wild(AllCounted(max_assets)),
 				reserve,
