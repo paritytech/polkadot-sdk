78dd521b9f344d504ff1d277771fccbe08ff6f9f
Adrian Catangiu <adrian@parity.io>
2024-03-22 14:52:21 +0200

    pallet-xcm: fix transport fees for remote reserve transfers
    Add SetFeesMode { jit_withdraw: true } before InitiateReserveWithdraw
    instruction to allow paying JIT fees in the executor.

--- polkadot/xcm/pallet-xcm/src/lib.rs
+++ polkadot/xcm/pallet-xcm/src/lib.rs
@@ -1875,6 +1875,7 @@ impl<T: Config> Pallet<T> {
 		]);
 		Ok(Xcm(vec![
 			WithdrawAsset(assets.into()),
+			SetFeesMode { jit_withdraw: true },
 			InitiateReserveWithdraw {
 				assets: Wild(AllCounted(max_assets)),
 				reserve,
