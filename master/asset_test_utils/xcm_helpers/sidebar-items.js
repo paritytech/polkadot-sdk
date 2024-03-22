7f9493d9cb96145e7002ff5b22b2098e17ee337f
paritytech-ci <devops-team@parity.io>
2024-03-22 19:25:05 +0000

    ___Updated docs for master___

--- master/asset_test_utils/xcm_helpers/sidebar-items.js
+++ master/asset_test_utils/xcm_helpers/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["pay_over_xcm_delivery_fees","query_response_delivery_fees","transfer_assets_delivery_fees"]};
+window.SIDEBAR_ITEMS = {"fn":["pay_over_xcm_delivery_fees","query_response_delivery_fees","teleport_assets_delivery_fees"]};
