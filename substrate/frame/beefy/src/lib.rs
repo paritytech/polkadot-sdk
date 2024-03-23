bc48b0b2e3d33c61ccf568685bcd6ef1d4c337a7
dharjeezy <dharjeezy@gmail.com>
2024-03-23 23:26:29 +0100

    nit

--- substrate/frame/beefy/src/lib.rs
+++ substrate/frame/beefy/src/lib.rs
@@ -63,7 +63,6 @@ const LOG_TARGET: &str = "runtime::beefy";
 pub mod pallet {
 	use super::*;
 	use frame_system::{ensure_root, pallet_prelude::BlockNumberFor};
-	use sp_runtime::TryRuntimeError;
 
 	#[pallet::config]
 	pub trait Config: frame_system::Config {
