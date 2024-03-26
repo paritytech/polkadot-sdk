paritytech/polkadot-sdk

commit 07dbf9b9cb64f42fca2e69add4901cdc9308641b

approval-voting: remove some inefficiences on startup (#3747)
Small refactoring to reduce the algorithmic complexity of the initial
message distribution in approval voting after a sync from O(n_candidates
^ 2) to O(n_candidates).

polkadot/node/core/approval-voting/src/lib.rs
@@ -55,9 +55,9 @@ use polkadot_node_subsystem_util::{
 };
 use polkadot_primitives::{
 	vstaging::{ApprovalVoteMultipleCandidates, ApprovalVotingParams},
-	BlockNumber, CandidateHash, CandidateIndex, CandidateReceipt, DisputeStatement, ExecutorParams,
-	GroupIndex, Hash, PvfExecKind, SessionIndex, SessionInfo, ValidDisputeStatementKind,
-	ValidatorId, ValidatorIndex, ValidatorPair, ValidatorSignature,
+	BlockNumber, CandidateHash, CandidateIndex, CandidateReceipt, CoreIndex, DisputeStatement,
+	ExecutorParams, GroupIndex, Hash, PvfExecKind, SessionIndex, SessionInfo,
+	ValidDisputeStatementKind, ValidatorId, ValidatorIndex, ValidatorPair, ValidatorSignature,
 };
 use sc_keystore::LocalKeystore;
 use sp_application_crypto::Pair;
@@ -1285,16 +1285,31 @@ fn cores_to_candidate_indices(
 
 	// Map from core index to candidate index.
 	for claimed_core_index in core_indices.iter_ones() {
-		if let Some(candidate_index) = block_entry
+		// Candidates are sorted by core index.
+		if let Ok(candidate_index) = block_entry
 			.candidates()
-			.iter()
-			.position(|(core_index, _)| core_index.0 == claimed_core_index as u32)
+			.binary_search_by_key(&(claimed_core_index as u32), |(core_index, _)| core_index.0)
 		{
 			candidate_indices.push(candidate_index as _);
 		}
 	}
 
 	CandidateBitfield::try_from(candidate_indices)
+}
+
+// Returns the claimed core bitfield from the assignment cert and the core index
+// from the block entry.
+fn get_core_indices_on_startup(
+	assignment: &AssignmentCertKindV2,
+	block_entry_core_index: CoreIndex,
+) -> CoreBitfield {
+	match &assignment {
+		AssignmentCertKindV2::RelayVRFModuloCompact { core_bitfield } => core_bitfield.clone(),
+		AssignmentCertKindV2::RelayVRFModulo { sample: _ } =>
+			CoreBitfield::try_from(vec![block_entry_core_index]).expect("Not an empty vec; qed"),
+		AssignmentCertKindV2::RelayVRFDelay { core_index } =>
+			CoreBitfield::try_from(vec![*core_index]).expect("Not an empty vec; qed"),
+	}
 }
 
 // Returns the claimed core bitfield from the assignment cert, the candidate hash and a
@@ -1367,7 +1382,7 @@ async fn distribution_messages_for_activation<Context>(
 			session: block_entry.session(),
 		});
 		let mut signatures_queued = HashSet::new();
-		for (_, candidate_hash) in block_entry.candidates() {
+		for (core_index, candidate_hash) in block_entry.candidates() {
 			let _candidate_span =
 				distribution_message_span.child("candidate").with_candidate(*candidate_hash);
 			let candidate_entry = match db.load_candidate_entry(&candidate_hash)? {
@@ -1389,152 +1404,121 @@ async fn distribution_messages_for_activation<Context>(
 					match approval_entry.local_statements() {
 						(None, None) | (None, Some(_)) => {}, // second is impossible case.
 						(Some(assignment), None) => {
-							if let Some(claimed_core_indices) = get_assignment_core_indices(
-								&assignment.cert().kind,
-								&candidate_hash,
-								&block_entry,
-							) {
-								if block_entry.has_candidates_pending_signature() {
-									delayed_approvals_timers.maybe_arm_timer(
-										state.clock.tick_now(),
-										state.clock.as_ref(),
-										block_entry.block_hash(),
-										assignment.validator_index(),
-									)
-								}
+							let claimed_core_indices =
+								get_core_indices_on_startup(&assignment.cert().kind, *core_index);
 
-								match cores_to_candidate_indices(
-									&claimed_core_indices,
-									&block_entry,
-								) {
-									Ok(bitfield) => {
-										gum::debug!(
-											target: LOG_TARGET,
-											candidate_hash = ?candidate_entry.candidate_receipt().hash(),
-											?block_hash,
-											"Discovered, triggered assignment, not approved yet",
-										);
+							if block_entry.has_candidates_pending_signature() {
+								delayed_approvals_timers.maybe_arm_timer(
+									state.clock.tick_now(),
+									state.clock.as_ref(),
+									block_entry.block_hash(),
+									assignment.validator_index(),
+								)
+							}
 
-										let indirect_cert = IndirectAssignmentCertV2 {
-											block_hash,
-											validator: assignment.validator_index(),
-											cert: assignment.cert().clone(),
-										};
-										messages.push(
-											ApprovalDistributionMessage::DistributeAssignment(
-												indirect_cert.clone(),
-												bitfield.clone(),
-											),
-										);
+							match cores_to_candidate_indices(&claimed_core_indices, &block_entry) {
+								Ok(bitfield) => {
+									gum::debug!(
+										target: LOG_TARGET,
+										candidate_hash = ?candidate_entry.candidate_receipt().hash(),
+										?block_hash,
+										"Discovered, triggered assignment, not approved yet",
+									);
 
-										if !block_entry
-											.candidate_is_pending_signature(*candidate_hash)
-										{
-											let ExtendedSessionInfo { ref executor_params, .. } =
-												match get_extended_session_info(
-													session_info_provider,
-													ctx.sender(),
-													block_entry.block_hash(),
-													block_entry.session(),
-												)
-												.await
-												{
-													Some(i) => i,
-													None => continue,
-												};
+									let indirect_cert = IndirectAssignmentCertV2 {
+										block_hash,
+										validator: assignment.validator_index(),
+										cert: assignment.cert().clone(),
+									};
+									messages.push(
+										ApprovalDistributionMessage::DistributeAssignment(
+											indirect_cert.clone(),
+											bitfield.clone(),
+										),
+									);
 
-											actions.push(Action::LaunchApproval {
-												claimed_candidate_indices: bitfield,
-												candidate_hash: candidate_entry
-													.candidate_receipt()
-													.hash(),
-												indirect_cert,
-												assignment_tranche: assignment.tranche(),
-												relay_block_hash: block_hash,
-												session: block_entry.session(),
-												executor_params: executor_params.clone(),
-												candidate: candidate_entry
-													.candidate_receipt()
-													.clone(),
-												backing_group: approval_entry.backing_group(),
-												distribute_assignment: false,
-											});
-										}
-									},
-									Err(err) => {
-										// Should never happen. If we fail here it means the
-										// assignment is null (no cores claimed).
-										gum::warn!(
-											target: LOG_TARGET,
-											?block_hash,
-											?candidate_hash,
-											?err,
-											"Failed to create assignment bitfield",
-										);
-									},
-								}
-							} else {
-								gum::warn!(
-									target: LOG_TARGET,
-									?block_hash,
-									?candidate_hash,
-									"Cannot get assignment claimed core indices",
-								);
+									if !block_entry.candidate_is_pending_signature(*candidate_hash)
+									{
+										let ExtendedSessionInfo { ref executor_params, .. } =
+											match get_extended_session_info(
+												session_info_provider,
+												ctx.sender(),
+												block_entry.block_hash(),
+												block_entry.session(),
+											)
+											.await
+											{
+												Some(i) => i,
+												None => continue,
+											};
+
+										actions.push(Action::LaunchApproval {
+											claimed_candidate_indices: bitfield,
+											candidate_hash: candidate_entry
+												.candidate_receipt()
+												.hash(),
+											indirect_cert,
+											assignment_tranche: assignment.tranche(),
+											relay_block_hash: block_hash,
+											session: block_entry.session(),
+											executor_params: executor_params.clone(),
+											candidate: candidate_entry.candidate_receipt().clone(),
+											backing_group: approval_entry.backing_group(),
+											distribute_assignment: false,
+										});
+									}
+								},
+								Err(err) => {
+									// Should never happen. If we fail here it means the
+									// assignment is null (no cores claimed).
+									gum::warn!(
+										target: LOG_TARGET,
+										?block_hash,
+										?candidate_hash,
+										?err,
+										"Failed to create assignment bitfield",
+									);
+								},
 							}
 						},
 						(Some(assignment), Some(approval_sig)) => {
-							if let Some(claimed_core_indices) = get_assignment_core_indices(
-								&assignment.cert().kind,
-								&candidate_hash,
-								&block_entry,
-							) {
-								match cores_to_candidate_indices(
-									&claimed_core_indices,
-									&block_entry,
-								) {
-									Ok(bitfield) => messages.push(
-										ApprovalDistributionMessage::DistributeAssignment(
-											IndirectAssignmentCertV2 {
-												block_hash,
-												validator: assignment.validator_index(),
-												cert: assignment.cert().clone(),
-											},
-											bitfield,
-										),
-									),
-									Err(err) => {
-										gum::warn!(
-											target: LOG_TARGET,
-											?block_hash,
-											?candidate_hash,
-											?err,
-											"Failed to create assignment bitfield",
-										);
-										// If we didn't send assignment, we don't send approval.
-										continue
-									},
-								}
-								if signatures_queued
-									.insert(approval_sig.signed_candidates_indices.clone())
-								{
-									messages.push(ApprovalDistributionMessage::DistributeApproval(
-										IndirectSignedApprovalVoteV2 {
+							let claimed_core_indices =
+								get_core_indices_on_startup(&assignment.cert().kind, *core_index);
+							match cores_to_candidate_indices(&claimed_core_indices, &block_entry) {
+								Ok(bitfield) => messages.push(
+									ApprovalDistributionMessage::DistributeAssignment(
+										IndirectAssignmentCertV2 {
 											block_hash,
-											candidate_indices: approval_sig
-												.signed_candidates_indices,
 											validator: assignment.validator_index(),
-											signature: approval_sig.signature,
+											cert: assignment.cert().clone(),
 										},
-									))
-								};
-							} else {
-								gum::warn!(
-									target: LOG_TARGET,
-									?block_hash,
-									?candidate_hash,
-									"Cannot get assignment claimed core indices",
-								);
+										bitfield,
+									),
+								),
+								Err(err) => {
+									gum::warn!(
+										target: LOG_TARGET,
+										?block_hash,
+										?candidate_hash,
+										?err,
+										"Failed to create assignment bitfield",
+									);
+									// If we didn't send assignment, we don't send approval.
+									continue
+								},
 							}
+							if signatures_queued
+								.insert(approval_sig.signed_candidates_indices.clone())
+							{
+								messages.push(ApprovalDistributionMessage::DistributeApproval(
+									IndirectSignedApprovalVoteV2 {
+										block_hash,
+										candidate_indices: approval_sig.signed_candidates_indices,
+										validator: assignment.validator_index(),
+										signature: approval_sig.signature,
+									},
+								))
+							};
 						},
 					}
 				},

commit 225632a9fd1de363f0657b72c5ea8eca82083ab1

fix regression in approval-voting introduced in #3747 (#3831)
Fixes #3826.

The docs on the `candidates` field of `BlockEntry` were incorrectly
stating that they are sorted by core index. The (incorrect) optimization
was introduced in #3747 based on this assumption. The actual ordering is
based on `CandidateIncluded` events ordering in the runtime. We revert
this optimization here.

- [x] verify the underlying issue
- [x] add a regression test

---------

Co-authored-by: Bastian Köcher <git@kchr.de>

polkadot/node/core/approval-voting/src/lib.rs
@@ -1285,10 +1285,10 @@ fn cores_to_candidate_indices(
 
 	// Map from core index to candidate index.
 	for claimed_core_index in core_indices.iter_ones() {
-		// Candidates are sorted by core index.
-		if let Ok(candidate_index) = block_entry
+		if let Some(candidate_index) = block_entry
 			.candidates()
-			.binary_search_by_key(&(claimed_core_index as u32), |(core_index, _)| core_index.0)
+			.iter()
+			.position(|(core_index, _)| core_index.0 == claimed_core_index as u32)
 		{
 			candidate_indices.push(candidate_index as _);
 		}

polkadot/node/core/approval-voting/src/persisted_entries.rs
@@ -454,7 +454,7 @@ pub struct BlockEntry {
 	slot: Slot,
 	relay_vrf_story: RelayVRFStory,
 	// The candidates included as-of this block and the index of the core they are
-	// leaving. Sorted ascending by core index.
+	// leaving.
 	candidates: Vec<(CoreIndex, CandidateHash)>,
 	// A bitfield where the i'th bit corresponds to the i'th candidate in `candidates`.
 	// The i'th bit is `true` iff the candidate has been approved in the context of this

polkadot/node/core/approval-voting/src/tests.rs
@@ -2479,6 +2479,173 @@ fn subsystem_import_checked_approval_sets_one_block_bit_at_a_time() {
 	});
 }
 
+// See https://github.com/paritytech/polkadot-sdk/issues/3826
+#[test]
+fn inclusion_events_can_be_unordered_by_core_index() {
+	let assignment_criteria = Box::new(MockAssignmentCriteria(
+		|| {
+			let mut assignments = HashMap::new();
+			for core in 0..3 {
+				let _ = assignments.insert(
+					CoreIndex(core),
+					approval_db::v2::OurAssignment {
+						cert: garbage_assignment_cert_v2(
+							AssignmentCertKindV2::RelayVRFModuloCompact {
+								core_bitfield: vec![CoreIndex(0), CoreIndex(1), CoreIndex(2)]
+									.try_into()
+									.unwrap(),
+							},
+						),
+						tranche: 0,
+						validator_index: ValidatorIndex(0),
+						triggered: false,
+					}
+					.into(),
+				);
+			}
+			assignments
+		},
+		|_| Ok(0),
+	));
+	let config = HarnessConfigBuilder::default().assignment_criteria(assignment_criteria).build();
+	let store = config.backend();
+
+	test_harness(config, |test_harness| async move {
+		let TestHarness {
+			mut virtual_overseer,
+			clock,
+			sync_oracle_handle: _sync_oracle_handle,
+			..
+		} = test_harness;
+
+		assert_matches!(
+			overseer_recv(&mut virtual_overseer).await,
+			AllMessages::ChainApi(ChainApiMessage::FinalizedBlockNumber(rx)) => {
+				rx.send(Ok(0)).unwrap();
+			}
+		);
+
+		let block_hash = Hash::repeat_byte(0x01);
+
+		let candidate_receipt0 = {
+			let mut receipt = dummy_candidate_receipt(block_hash);
+			receipt.descriptor.para_id = ParaId::from(0_u32);
+			receipt
+		};
+		let candidate_receipt1 = {
+			let mut receipt = dummy_candidate_receipt(block_hash);
+			receipt.descriptor.para_id = ParaId::from(1_u32);
+			receipt
+		};
+		let candidate_receipt2 = {
+			let mut receipt = dummy_candidate_receipt(block_hash);
+			receipt.descriptor.para_id = ParaId::from(2_u32);
+			receipt
+		};
+		let candidate_index0 = 0;
+		let candidate_index1 = 1;
+		let candidate_index2 = 2;
+
+		let validator0 = ValidatorIndex(0);
+		let validator1 = ValidatorIndex(1);
+		let validator2 = ValidatorIndex(2);
+		let validator3 = ValidatorIndex(3);
+
+		let validators = vec![
+			Sr25519Keyring::Alice,
+			Sr25519Keyring::Bob,
+			Sr25519Keyring::Charlie,
+			Sr25519Keyring::Dave,
+			Sr25519Keyring::Eve,
+		];
+		let session_info = SessionInfo {
+			validator_groups: IndexedVec::<GroupIndex, Vec<ValidatorIndex>>::from(vec![
+				vec![validator0, validator1],
+				vec![validator2],
+				vec![validator3],
+			]),
+			needed_approvals: 1,
+			zeroth_delay_tranche_width: 1,
+			relay_vrf_modulo_samples: 1,
+			n_delay_tranches: 1,
+			no_show_slots: 1,
+			..session_info(&validators)
+		};
+
+		ChainBuilder::new()
+			.add_block(
+				block_hash,
+				ChainBuilder::GENESIS_HASH,
+				1,
+				BlockConfig {
+					slot: Slot::from(0),
+					candidates: Some(vec![
+						(candidate_receipt0.clone(), CoreIndex(2), GroupIndex(2)),
+						(candidate_receipt1.clone(), CoreIndex(1), GroupIndex(0)),
+						(candidate_receipt2.clone(), CoreIndex(0), GroupIndex(1)),
+					]),
+					session_info: Some(session_info),
+					end_syncing: true,
+				},
+			)
+			.build(&mut virtual_overseer)
+			.await;
+
+		assert_eq!(clock.inner.lock().next_wakeup().unwrap(), 2);
+		clock.inner.lock().wakeup_all(100);
+
+		assert_eq!(clock.inner.lock().wakeups.len(), 0);
+
+		futures_timer::Delay::new(Duration::from_millis(100)).await;
+
+		// Assignment is distributed only once from `approval-voting`
+		assert_matches!(
+			overseer_recv(&mut virtual_overseer).await,
+			AllMessages::ApprovalDistribution(ApprovalDistributionMessage::DistributeAssignment(
+				_,
+				c_indices,
+			)) => {
+				assert_eq!(c_indices, vec![candidate_index0, candidate_index1, candidate_index2].try_into().unwrap());
+			}
+		);
+
+		// Candidate 0
+		recover_available_data(&mut virtual_overseer).await;
+		fetch_validation_code(&mut virtual_overseer).await;
+
+		// Candidate 1
+		recover_available_data(&mut virtual_overseer).await;
+		fetch_validation_code(&mut virtual_overseer).await;
+
+		// Candidate 2
+		recover_available_data(&mut virtual_overseer).await;
+		fetch_validation_code(&mut virtual_overseer).await;
+
+		// Check if assignment was triggered for candidate 0.
+		let candidate_entry =
+			store.load_candidate_entry(&candidate_receipt0.hash()).unwrap().unwrap();
+		let our_assignment =
+			candidate_entry.approval_entry(&block_hash).unwrap().our_assignment().unwrap();
+		assert!(our_assignment.triggered());
+
+		// Check if assignment was triggered for candidate 1.
+		let candidate_entry =
+			store.load_candidate_entry(&candidate_receipt1.hash()).unwrap().unwrap();
+		let our_assignment =
+			candidate_entry.approval_entry(&block_hash).unwrap().our_assignment().unwrap();
+		assert!(our_assignment.triggered());
+
+		// Check if assignment was triggered for candidate 2.
+		let candidate_entry =
+			store.load_candidate_entry(&candidate_receipt2.hash()).unwrap().unwrap();
+		let our_assignment =
+			candidate_entry.approval_entry(&block_hash).unwrap().our_assignment().unwrap();
+		assert!(our_assignment.triggered());
+
+		virtual_overseer
+	});
+}
+
 fn approved_ancestor_test(
 	skip_approval: impl Fn(BlockNumber) -> bool,
 	approved_height: BlockNumber,