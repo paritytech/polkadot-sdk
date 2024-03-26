207ad92121d577ed7f42653d62f9da792c8d6b43
Nikhil Gupta <17176722+gupnik@users.noreply.github.com>
2024-03-26 10:34:25 +0530

    Fixes template

--- templates/minimal/pallets/template/src/lib.rs
+++ templates/minimal/pallets/template/src/lib.rs
@@ -2,17 +2,17 @@
 
 #![cfg_attr(not(feature = "std"), no_std)]
 
-use polkadot_sdk_frame::deps::frame_support;
+use polkadot_sdk_frame::prelude::*;
 
 // Re-export all pallet parts, this is needed to properly import the pallet into the runtime.
 pub use pallet::*;
 
-#[frame_support::pallet]
+#[polkadot_sdk_frame::pallet]
 pub mod pallet {
 	use super::*;
 
 	#[pallet::config]
-	pub trait Config: polkadot_sdk_frame::deps::frame_system::Config {}
+	pub trait Config: frame_system::Config {}
 
 	#[pallet::pallet]
 	pub struct Pallet<T>(_);
