ca8d49fe349462ab8079fefb7502ea84a09998ad
Ermal Kaleci <ermalkaleci@gmail.com>
2024-03-26 20:30:50 +0100

    update config

--- substrate/frame/message-queue/src/integration_test.rs
+++ substrate/frame/message-queue/src/integration_test.rs
@@ -73,7 +73,7 @@ impl Config for Test {
 	type HeapSize = HeapSize;
 	type MaxStale = MaxStale;
 	type ServiceWeight = ServiceWeight;
-	type IdleMaxServiceWeight = ServiceWeight;
+	type IdleMaxServiceWeight = ();
 }
 
 /// Simulates heavy usage by enqueueing and processing large amounts of messages.
--- substrate/frame/message-queue/src/mock.rs
+++ substrate/frame/message-queue/src/mock.rs
@@ -56,7 +56,7 @@ impl Config for Test {
 	type HeapSize = HeapSize;
 	type MaxStale = MaxStale;
 	type ServiceWeight = ServiceWeight;
-	type IdleMaxServiceWeight = ServiceWeight;
+	type IdleMaxServiceWeight = ();
 }
 
 /// Mocked `WeightInfo` impl with allows to set the weight per call.
