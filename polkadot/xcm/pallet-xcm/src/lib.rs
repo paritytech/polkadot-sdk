d09c833acdba9ad7485d862521d7fc19945fa288
Branislav Kontur <bkontur@gmail.com>
2024-03-26 12:54:57 +0100

    pallet-xcm: fix transport fees for remote reserve transfers (#3798) - backport for 1.8 (#3839)
    This is backport of https://github.com/paritytech/polkadot-sdk/pull/3792
    
    Expected patches for:
    - pallet-xcm
    
    ---------
    
    Co-authored-by: Adrian Catangiu <adrian@parity.io>

--- polkadot/xcm/pallet-xcm/src/lib.rs
+++ polkadot/xcm/pallet-xcm/src/lib.rs
@@ -1928,6 +1928,7 @@ impl<T: Config> Pallet<T> {
 		]);
 		Ok(Xcm(vec![
 			WithdrawAsset(assets.into()),
+			SetFeesMode { jit_withdraw: true },
 			InitiateReserveWithdraw {
 				assets: Wild(AllCounted(max_assets)),
 				reserve,
