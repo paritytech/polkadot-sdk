6976d0873b2ff5675111a876be5db2a2ee2dad8e
Serban Iorga <serban@parity.io>
2024-03-27 17:38:38 +0100

    Remove bridges subtree

--- bridges/testing/scripts/invoke-script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-INVOKE_LOG=`mktemp -p $TEST_FOLDER invoke.XXXXX`
-
-pushd $POLKADOT_SDK_PATH/bridges/testing/environments/rococo-westend
-./bridges_rococo_westend.sh $1 >$INVOKE_LOG 2>&1
-popd
