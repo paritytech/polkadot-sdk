af1f3372fa258f1cc46d28cc57faf4a606ee5d13
Bryan Chen <xlchen1291@gmail.com>
2024-03-27 20:05:41 +1300

    fix

--- polkadot/runtime/parachains/src/mock.rs
+++ polkadot/runtime/parachains/src/mock.rs
@@ -248,7 +248,7 @@ impl crate::dmp::Config for Test {}
 
 parameter_types! {
 	pub const FirstMessageFactorPercent: u64 = 100;
-	pub const DefaultChannelSizeAndCapacityWithSystem: (u32, u32) = (51200, 500);
+	pub const DefaultChannelSizeAndCapacityWithSystem: (u32, u32) = (1, 4);
 }
 
 impl crate::hrmp::Config for Test {
