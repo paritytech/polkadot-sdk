f8e0d605e57f0e8ba50820967e1e6d6c34e32588
Andrei Eres <eresav@me.com>
2024-03-27 16:51:06 +0100

    Update

--- polkadot/node/core/approval-voting/benches/approval-voting-regression-bench.rs
+++ polkadot/node/core/approval-voting/benches/approval-voting-regression-bench.rs
@@ -14,22 +14,30 @@
 // You should have received a copy of the GNU General Public License
 // along with Polkadot.  If not, see <http://www.gnu.org/licenses/>.
 
+//! approval-voting regression tests
+//!
+//! Approval Voting benchmark based on Kusama parameters and scale.
+//!
+//! Subsystems involved:
+//! - approval-distribution
+//! - approval-voting
+
 use polkadot_subsystem_bench::{
 	approval::{bench_approvals, prepare_test, ApprovalsOptions},
 	configuration::TestConfiguration,
 	usage::BenchmarkUsage,
+	utils::save_to_file,
 };
+use std::io::Write;
 
 const BENCH_COUNT: usize = 3;
-const WARM_UP_COUNT: usize = 20;
-const WARM_UP_PRECISION: f64 = 0.01;
 
 fn main() -> Result<(), String> {
 	let mut messages = vec![];
 
 	messages.extend(approvals_no_shows()?);
-	// messages.extend(approvals_throughput()?);
-	// messages.extend(approvals_throughput_best_case()?);
+	messages.extend(approvals_throughput()?);
+	messages.extend(approvals_throughput_best_case()?);
 
 	if messages.is_empty() {
 		Ok(())
@@ -41,49 +49,34 @@ fn main() -> Result<(), String> {
 
 fn test_configuration() -> TestConfiguration {
 	let mut config = TestConfiguration::default();
-	config.n_validators = 500;
-	config.n_cores = 100;
-	config.min_pov_size = 1120;
-	config.max_pov_size = 5120;
-	config.peer_bandwidth = 524288000000;
-	config.bandwidth = 524288000000;
-	config.num_blocks = 10;
+	config.num_blocks = 3;
 	config.generate_pov_sizes();
 
 	config
 }
 
-fn warm_up(test_case: &str, options: ApprovalsOptions) -> Result<(), String> {
-	println!("Warming up...");
-	let mut prev_run: Option<BenchmarkUsage> = None;
-	for _ in 0..WARM_UP_COUNT {
-		let curr = run(test_case, options.clone());
-		if let Some(ref prev) = prev_run {
-			let ap_distr_diff =
-				curr.cpu_usage_diff(prev, "approval-distribution").expect("Must exist");
-			let ap_vot_diff = curr.cpu_usage_diff(prev, "approval-voting").expect("Must exist");
-			if ap_distr_diff < WARM_UP_PRECISION && ap_vot_diff < WARM_UP_PRECISION {
-				return Ok(())
-			}
-		}
-		prev_run = Some(curr);
-	}
-
-	Err("Can't warm up".to_string())
-}
-
-fn benchmark(test_case: &str, options: ApprovalsOptions) -> BenchmarkUsage {
+fn run(test_case: &str, options: ApprovalsOptions) -> Result<BenchmarkUsage, String> {
 	println!("Benchmarking...");
-	let usages: Vec<BenchmarkUsage> =
-		(0..BENCH_COUNT).map(|_| run(test_case, options.clone())).collect();
-	let usage = BenchmarkUsage::average(&usages);
-	println!("{}", usage);
-	usage
-}
+	let usages: Vec<BenchmarkUsage> = (0..BENCH_COUNT)
+		.map(|n| {
+			print!("\r[{}{}]", "#".repeat(n), "_".repeat(BENCH_COUNT - n));
+			std::io::stdout().flush().unwrap();
 
-fn run(test_case: &str, options: ApprovalsOptions) -> BenchmarkUsage {
-	let (mut env, state) = prepare_test(test_configuration(), options.clone(), false);
-	env.runtime().block_on(bench_approvals(test_case, &mut env, state))
+			let (mut env, state) = prepare_test(test_configuration(), options.clone(), false);
+			env.runtime().block_on(bench_approvals(test_case, &mut env, state))
+		})
+		.collect();
+	println!("\rDone!{}", " ".repeat(BENCH_COUNT));
+
+	let average_usage = BenchmarkUsage::average(&usages);
+	save_to_file(
+		&format!("charts/availability-recovery-regression-bench-{}.json", test_case),
+		average_usage.to_chart_json().map_err(|e| e.to_string())?,
+	)
+	.map_err(|e| e.to_string())?;
+	println!("{}", average_usage);
+
+	Ok(average_usage)
 }
 
 fn approvals_no_shows() -> Result<Vec<String>, String> {
@@ -100,12 +93,13 @@ fn approvals_no_shows() -> Result<Vec<String>, String> {
 		workdir_prefix: "/tmp".to_string(),
 	};
 
-	warm_up(test_case, options.clone())?;
-	let usage = benchmark(test_case, options.clone());
+	let usage = run(test_case, options)?;
 
+	// We expect no variance for received and sent
+	// but use 0.001 because we operate with floats
 	messages.extend(usage.check_network_usage(&[
-		("Received from peers", 7000.0, 0.05),
-		("Sent to peers", 8100.0, 0.05),
+		("Received from peers", 7000.0, 0.001),
+		("Sent to peers", 8100.0, 0.001),
 	]));
 	messages.extend(usage.check_cpu_usage(&[
 		("approval-distribution", 0.990, 0.05),
@@ -115,7 +109,6 @@ fn approvals_no_shows() -> Result<Vec<String>, String> {
 	Ok(messages)
 }
 
-#[allow(dead_code)]
 fn approvals_throughput() -> Result<Vec<String>, String> {
 	let mut messages = vec![];
 	let test_case = "approvals_throughput";
@@ -130,12 +123,13 @@ fn approvals_throughput() -> Result<Vec<String>, String> {
 		workdir_prefix: "/tmp".to_string(),
 	};
 
-	warm_up(test_case, options.clone())?;
-	let usage = benchmark(test_case, options.clone());
+	let usage = run(test_case, options)?;
 
+	// We expect no variance for received and sent
+	// but use 0.001 because we operate with floats
 	messages.extend(usage.check_network_usage(&[
-		("Received from peers", 52850.0, 0.05),
-		("Sent to peers", 63500.0, 0.05),
+		("Received from peers", 52850.0, 0.001),
+		("Sent to peers", 63500.0, 0.001),
 	]));
 	messages.extend(usage.check_cpu_usage(&[
 		("approval-distribution", 8.645, 0.05),
@@ -145,7 +139,6 @@ fn approvals_throughput() -> Result<Vec<String>, String> {
 	Ok(messages)
 }
 
-#[allow(dead_code)]
 fn approvals_throughput_best_case() -> Result<Vec<String>, String> {
 	let mut messages = vec![];
 	let test_case = "approvals_throughput_best_case";
@@ -160,9 +153,10 @@ fn approvals_throughput_best_case() -> Result<Vec<String>, String> {
 		workdir_prefix: "/tmp".to_string(),
 	};
 
-	warm_up(test_case, options.clone())?;
-	let usage = benchmark(test_case, options.clone());
+	let usage = run(test_case, options)?;
 
+	// We expect no variance for received and sent
+	// but use 0.001 because we operate with floats
 	messages.extend(usage.check_network_usage(&[
 		("Received from peers", 2950.0, 0.05),
 		("Sent to peers", 3250.0, 0.05),
