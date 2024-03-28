79ada5e4aba1962151688b934a6104f72d3eed9f
kianenigma <kian@parity.io>
2024-03-28 15:06:43 +0000

    jot down ideas for node section

--- docs/sdk/src/guides/your_first_node.rs
+++ docs/sdk/src/guides/your_first_node.rs
@@ -1,3 +1,11 @@
 //! # Your first Node
 //!
-//! In this guide, we will walk through the process of adding a
+//! Ideas:
+//! 2. talk about the 3 options of the future:
+//! 	- 1. maintain your own node. needed for custom consensus, RPC and ideal optimizations.
+//! 	- 2. use the node-builder, if and when it comes
+//! 	- 3. use an omni-node.
+//!
+//! 1. run the runtime of prev step with omni-node
+//! 2. suggest interactions with pjs related things.
+//! 3. add genesis state
