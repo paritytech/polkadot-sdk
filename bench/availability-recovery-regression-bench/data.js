4c8256a56017e730f585a8dbc232dca29d408400
github-action-benchmark <github@users.noreply.github.com>
2024-03-28 18:22:35 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for eb6f5abee64e979dba25924f71ef86d2b3ca2deb

---
+++ bench/availability-recovery-regression-bench/data.js
@@ -0,0 +1,50 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1711650155146,
+  "repoUrl": "https://github.com/paritytech/polkadot-sdk",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "Alexander Samusev",
+            "username": "alvicsam",
+            "email": "41779041+alvicsam@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "eb6f5abee64e979dba25924f71ef86d2b3ca2deb",
+          "message": "[ci] fix subsystem-benchmarks gha (#3876)\n\nPR adds variables validation and app credentials for pushing into\ngh-pages\n\ncc https://github.com/paritytech/ci_cd/issues/934",
+          "timestamp": "2024-03-28T16:42:20Z",
+          "url": "https://github.com/paritytech/polkadot-sdk/commit/eb6f5abee64e979dba25924f71ef86d2b3ca2deb"
+        },
+        "date": 1711650130067,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Received from peers",
+            "value": 307203,
+            "unit": "KiB"
+          },
+          {
+            "name": "Sent to peers",
+            "value": 1.6666666666666672,
+            "unit": "KiB"
+          },
+          {
+            "name": "test-environment",
+            "value": 0.14907673656666665,
+            "unit": "seconds"
+          },
+          {
+            "name": "availability-recovery",
+            "value": 11.19076506852,
+            "unit": "seconds"
+          }
+        ]
+      }
+    ]
+  }
+}
