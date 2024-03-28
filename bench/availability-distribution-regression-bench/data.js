256ae60006cd4fb0be99f18db45f9ecf8e48c1f2
github-action-benchmark <github@users.noreply.github.com>
2024-03-28 16:19:11 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 4a5a2e732a9c4594efd76db5e519ba23a053cbcd

---
+++ bench/availability-distribution-regression-bench/data.js
@@ -0,0 +1,62 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1711642750737,
+  "repoUrl": "https://github.com/paritytech/polkadot-sdk",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "alvicsam@gmail.com",
+            "name": "alvicsam",
+            "username": "alvicsam"
+          },
+          "committer": {
+            "email": "alvicsam@gmail.com",
+            "name": "alvicsam",
+            "username": "alvicsam"
+          },
+          "distinct": true,
+          "id": "4a5a2e732a9c4594efd76db5e519ba23a053cbcd",
+          "message": "test app creds",
+          "timestamp": "2024-03-28T17:04:09+01:00",
+          "tree_id": "71d77953457f33f2462ac00030c586c93152c411",
+          "url": "https://github.com/paritytech/polkadot-sdk/commit/4a5a2e732a9c4594efd76db5e519ba23a053cbcd"
+        },
+        "date": 1711642726953,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Received from peers",
+            "value": 433.3333333333332,
+            "unit": "KiB"
+          },
+          {
+            "name": "Sent to peers",
+            "value": 18479.92,
+            "unit": "KiB"
+          },
+          {
+            "name": "bitfield-distribution",
+            "value": 0.026277995219999996,
+            "unit": "seconds"
+          },
+          {
+            "name": "availability-store",
+            "value": 0.15444720176666668,
+            "unit": "seconds"
+          },
+          {
+            "name": "test-environment",
+            "value": 0.01033215506,
+            "unit": "seconds"
+          },
+          {
+            "name": "availability-distribution",
+            "value": 0.012231637953333327,
+            "unit": "seconds"
+          }
+        ]
+      }
+    ]
+  }
+}
