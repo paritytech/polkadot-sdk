a930f798d82797f140fe28d96dba54cbcf6b38e4
github-action-benchmark <github@users.noreply.github.com>
2024-03-28 18:17:21 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for eb6f5abee64e979dba25924f71ef86d2b3ca2deb

---
+++ bench/availability-distribution-regression-bench/data.js
@@ -0,0 +1,60 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1711649840882,
+  "repoUrl": "https://github.com/paritytech/polkadot-sdk",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "Alexander Samusev",
+            "username": "alvicsam",
+            "email": "41779041+alvicsam@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "eb6f5abee64e979dba25924f71ef86d2b3ca2deb",
+          "message": "[ci] fix subsystem-benchmarks gha (#3876)\n\nPR adds variables validation and app credentials for pushing into\ngh-pages\n\ncc https://github.com/paritytech/ci_cd/issues/934",
+          "timestamp": "2024-03-28T16:42:20Z",
+          "url": "https://github.com/paritytech/polkadot-sdk/commit/eb6f5abee64e979dba25924f71ef86d2b3ca2deb"
+        },
+        "date": 1711649817534,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Received from peers",
+            "value": 433.3333333333332,
+            "unit": "KiB"
+          },
+          {
+            "name": "Sent to peers",
+            "value": 18479.926666666663,
+            "unit": "KiB"
+          },
+          {
+            "name": "test-environment",
+            "value": 0.009493043573333335,
+            "unit": "seconds"
+          },
+          {
+            "name": "availability-store",
+            "value": 0.15172178257999994,
+            "unit": "seconds"
+          },
+          {
+            "name": "bitfield-distribution",
+            "value": 0.02619895733333334,
+            "unit": "seconds"
+          },
+          {
+            "name": "availability-distribution",
+            "value": 0.011713924659999996,
+            "unit": "seconds"
+          }
+        ]
+      }
+    ]
+  }
+}
