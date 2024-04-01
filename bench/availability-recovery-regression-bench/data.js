086c894d7768062013158d931b56259ac3e978c9
github-action-benchmark <github@users.noreply.github.com>
2024-04-01 14:19:02 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for d6f68bb9062167537211cc05286809771fc8861a

--- bench/availability-recovery-regression-bench/data.js
+++ bench/availability-recovery-regression-bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1711971521470,
+  "lastUpdate": 1711981141528,
   "repoUrl": "https://github.com/paritytech/polkadot-sdk",
   "entries": {
     "Benchmark": [
@@ -503,6 +503,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "test-environment",
             "value": 0.16771078979999998,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Alexandru Gheorghe",
+            "username": "alexggh",
+            "email": "49718502+alexggh@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "d6f68bb9062167537211cc05286809771fc8861a",
+          "message": "primitives: Move out of staging released APIs (#3925)\n\nRuntime release 1.2 includes bumping of the ParachainHost APIs up to\nv10, so let's move all the released APIs out of vstaging folder, this PR\ndoes not include any logic changes only renaming of the modules and some\nmoving around.\n\nSigned-off-by: Alexandru Gheorghe <alexandru.gheorghe@parity.io>",
+          "timestamp": "2024-04-01T13:03:26Z",
+          "url": "https://github.com/paritytech/polkadot-sdk/commit/d6f68bb9062167537211cc05286809771fc8861a"
+        },
+        "date": 1711981117398,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Sent to peers",
+            "value": 1.6666666666666672,
+            "unit": "KiB"
+          },
+          {
+            "name": "Received from peers",
+            "value": 307203,
+            "unit": "KiB"
+          },
+          {
+            "name": "availability-recovery",
+            "value": 11.973230799573328,
+            "unit": "seconds"
+          },
+          {
+            "name": "test-environment",
+            "value": 0.18138847673333333,
             "unit": "seconds"
           }
         ]
