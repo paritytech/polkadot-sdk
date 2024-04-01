9309d6bc78519a4de04548f253dbc8098b4f36da
Nikhil Gupta <17176722+gupnik@users.noreply.github.com>
2024-04-01 11:53:23 +0530

    Uses renamed crate in template

--- substrate/primitives/api/proc-macro/src/utils.rs
+++ substrate/primitives/api/proc-macro/src/utils.rs
@@ -34,7 +34,9 @@ pub fn generate_crate_access() -> TokenStream {
 			quote!(#renamed_name::__private)
 		},
 		Err(e) =>
-			if let Ok(FoundCrate::Name(name)) = crate_name(&"polkadot-sdk-frame") {
+			if let Ok(FoundCrate::Name(name)) =
+				crate_name(&"polkadot-sdk-frame").map_err(|_| crate_name(&"frame"))
+			{
 				let path = format!("{}::deps::sp_api::__private", name);
 				let path = syn::parse_str::<syn::Path>(&path).expect("is a valid path; qed");
 				quote!( #path )
