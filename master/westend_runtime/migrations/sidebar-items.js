6724109f6bdf1c4e3dfde919a3ae70bedf0e4a5f
paritytech-ci <devops-team@parity.io>
2024-04-01 23:01:37 +0000

    ___Updated docs for master___

--- master/westend_runtime/migrations/sidebar-items.js
+++ master/westend_runtime/migrations/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"struct":["GetLegacyLeaseImpl","ImOnlinePalletName","UpgradeSessionKeys"],"type":["Unreleased"]};
+window.SIDEBAR_ITEMS = {"struct":["GetLegacyLeaseImpl"],"type":["Unreleased"]};
