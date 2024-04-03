7bde9d89b20a7094f4302fa89d8d7926e5556577
Andrei Sandu <andrei-mihail@parity.io>
2024-04-03 12:03:09 +0300

    minor script change
    Signed-off-by: Andrei Sandu <andrei-mihail@parity.io>

--- polkadot/zombienet_tests/elastic_scaling/assign-core.js
+++ polkadot/zombienet_tests/elastic_scaling/assign-core.js
@@ -2,8 +2,8 @@ async function run(nodeName, networkInfo, args) {
   const { wsUri, userDefinedTypes } = networkInfo.nodesByName[nodeName];
   const api = await zombie.connect(wsUri, userDefinedTypes);
 
-  let para = args[0];
-  let core = args[1];
+  let para = Number(args[0]);
+  let core = Number(args[1]);
   console.log(`Assigning para ${para} to core ${core}`);
 
   await zombie.util.cryptoWaitReady();
@@ -14,7 +14,7 @@ async function run(nodeName, networkInfo, args) {
 
   await new Promise(async (resolve, reject) => {
     const unsub = await api.tx.sudo
-      .sudo(api.tx.coretime.assignCore(Number(core), 0, [[{ task: para }, 57600]], null))
+      .sudo(api.tx.coretime.assignCore(core, 0, [[{ task: para }, 57600]], null))
       .signAndSend(alice, ({ status, isError }) => {
         if (status.isInBlock) {
           console.log(
