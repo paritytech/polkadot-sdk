2de1f1acfe14838ccdcff7c9ad6b3236e2bf9c54
paritytech-ci <devops-team@parity.io>
2024-04-03 09:27:29 +0000

    ___Updated docs for master___

--- master/polkadot_node_subsystem_util/vstaging/sidebar-items.js
+++ master/polkadot_node_subsystem_util/vstaging/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["get_disabled_validators_with_fallback"]};
+window.SIDEBAR_ITEMS = {"fn":["fetch_claim_queue","fetch_next_scheduled_on_core","get_disabled_validators_with_fallback"],"type":["ClaimQueueSnapshot"]};
