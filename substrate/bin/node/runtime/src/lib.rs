f842cb6033661fafae36c189d34d00dbbf608f1a
Michal Kucharczyk <1728078+michalkucharczyk@users.noreply.github.com>
2024-04-04 20:30:54 +0200

    `GenesisConfig` presets for runtime (#2714)
    The runtime now can provide a number of predefined presets of
    `RuntimeGenesisConfig` struct. This presets are intended to be used in
    different deployments, e.g.: `local`, `staging`, etc, and should be
    included into the corresponding chain-specs.
    
    Having `GenesisConfig` presets in runtime allows to fully decouple node
    from runtime types (the problem is described in #1984).
    
    **Summary of changes:**
    - The `GenesisBuilder` API was adjusted to enable this functionality
    (and provide better naming - #150):
       ```rust
        fn preset_names() -> Vec<PresetId>;
    fn get_preset(id: Option<PresetId>) -> Option<serde_json::Value>;
    //`None` means default
        fn build_state(value: serde_json::Value);
        pub struct PresetId(Vec<u8>);
       ```
    
    - **Breaking change**: Old `create_default_config` method was removed,
    `build_config` was renamed to `build_state`. As a consequence a node
    won't be able to interact with genesis config for older runtimes. The
    cleanup was made for sake of API simplicity. Also IMO maintaining
    compatibility with old API is not so crucial.
    - Reference implementation was provided for `substrate-test-runtime` and
    `rococo` runtimes. For rococo new
    [`genesis_configs_presets`](https://github.com/paritytech/polkadot-sdk/blob/3b41d66b97c5ff0ec4a1989da5ffd8b9f3f588e3/polkadot/runtime/rococo/src/genesis_config_presets.rs#L530)
    module was added and is used in `GenesisBuilder`
    [_presets-related_](https://github.com/paritytech/polkadot-sdk/blob/3b41d66b97c5ff0ec4a1989da5ffd8b9f3f588e3/polkadot/runtime/rococo/src/lib.rs#L2462-L2485)
    methods.
    
    - The `chain-spec-builder` util was also improved and allows to
    ([_doc_](https://github.com/paritytech/polkadot-sdk/blob/3b41d66b97c5ff0ec4a1989da5ffd8b9f3f588e3/substrate/bin/utils/chain-spec-builder/src/lib.rs#L19)):
       - list presets provided by given runtime (`list-presets`),
    - display preset or default config provided by the runtime
    (`display-preset`),
       - build chain-spec using named preset (`create ... named-preset`),
    
    
    - The `ChainSpecBuilder` is extended with
    [`with_genesis_config_preset_name`](https://github.com/paritytech/polkadot-sdk/blob/3b41d66b97c5ff0ec4a1989da5ffd8b9f3f588e3/substrate/client/chain-spec/src/chain_spec.rs#L447)
    method which allows to build chain-spec using named preset provided by
    the runtime. Sample usage on the node side
    [here](https://github.com/paritytech/polkadot-sdk/blob/2caffaae803e08a3d5b46c860e8016da023ff4ce/polkadot/node/service/src/chain_spec.rs#L404).
    
    Implementation of #1984.
    fixes: #150
    part of: #25
    
    ---------
    
    Co-authored-by: Sebastian Kunert <skunert49@gmail.com>
    Co-authored-by: Kian Paimani <5588131+kianenigma@users.noreply.github.com>
    Co-authored-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>

--- substrate/bin/node/runtime/src/lib.rs
+++ substrate/bin/node/runtime/src/lib.rs
@@ -31,7 +31,7 @@ use frame_support::{
 	derive_impl,
 	dispatch::DispatchClass,
 	dynamic_params::{dynamic_pallet_params, dynamic_params},
-	genesis_builder_helper::{build_config, create_default_config},
+	genesis_builder_helper::{build_state, get_preset},
 	instances::{Instance1, Instance2},
 	ord_parameter_types,
 	pallet_prelude::Get,
@@ -3194,12 +3194,16 @@ impl_runtime_apis! {
 	}
 
 	impl sp_genesis_builder::GenesisBuilder<Block> for Runtime {
-		fn create_default_config() -> Vec<u8> {
-			create_default_config::<RuntimeGenesisConfig>()
+		fn build_state(config: Vec<u8>) -> sp_genesis_builder::Result {
+			build_state::<RuntimeGenesisConfig>(config)
 		}
 
-		fn build_config(config: Vec<u8>) -> sp_genesis_builder::Result {
-			build_config::<RuntimeGenesisConfig>(config)
+		fn get_preset(id: &Option<sp_genesis_builder::PresetId>) -> Option<Vec<u8>> {
+			get_preset::<RuntimeGenesisConfig>(id, |_| None)
+		}
+
+		fn preset_names() -> Vec<sp_genesis_builder::PresetId> {
+			vec![]
 		}
 	}
 }
