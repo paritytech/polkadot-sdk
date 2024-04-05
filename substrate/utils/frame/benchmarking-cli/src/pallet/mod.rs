02f0514dba503fb99cda132f63cc682e49a6d997
Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
2024-04-05 18:58:12 +0200

    Cannot doc internal crate
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>

--- substrate/utils/frame/benchmarking-cli/src/pallet/mod.rs
+++ substrate/utils/frame/benchmarking-cli/src/pallet/mod.rs
@@ -177,8 +177,7 @@ pub struct PalletCmd {
 
 	/// How to construct the genesis state.
 	///
-	/// Uses [`GenesisBuilder::Spec`] by default and  [`GenesisConstructor::Runtime`] if `runtime`
-	/// is set.
+	/// Uses `GenesisBuilder::Spec` by default and  `GenesisBuilder::Runtime` if `runtime` is set.
 	#[arg(long, value_enum)]
 	pub genesis_builder: Option<GenesisBuilder>,
 
