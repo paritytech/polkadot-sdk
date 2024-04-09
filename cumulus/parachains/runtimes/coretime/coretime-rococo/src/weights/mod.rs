a21e2fbc882ca5e368c85722100f0a34c5444b64
Dónal Murray <donal.murray@parity.io>
2024-04-09 17:19:22 +0100

    Use DefaultConfig in coretime-rococo as POC

--- cumulus/parachains/runtimes/coretime/coretime-rococo/src/weights/mod.rs
+++ cumulus/parachains/runtimes/coretime/coretime-rococo/src/weights/mod.rs
@@ -17,10 +17,8 @@
 
 //! Expose the auto generated weight files.
 
-pub mod block_weights;
 pub mod cumulus_pallet_parachain_system;
 pub mod cumulus_pallet_xcmp_queue;
-pub mod extrinsic_weights;
 pub mod frame_system;
 pub mod pallet_balances;
 pub mod pallet_broker;
@@ -31,10 +29,4 @@ pub mod pallet_session;
 pub mod pallet_timestamp;
 pub mod pallet_utility;
 pub mod pallet_xcm;
-pub mod paritydb_weights;
-pub mod rocksdb_weights;
 pub mod xcm;
-
-pub use block_weights::constants::BlockExecutionWeight;
-pub use extrinsic_weights::constants::ExtrinsicBaseWeight;
-pub use rocksdb_weights::constants::RocksDbWeight;
