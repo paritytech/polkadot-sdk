c52d42d4419f8deaab5c0965a4a14886ac3626df
Alexandru Vasile <alexandru.vasile@parity.io>
2024-04-09 16:57:15 +0300

    Fix clippy
    Signed-off-by: Alexandru Vasile <alexandru.vasile@parity.io>

--- substrate/client/rpc-spec-v2/src/chain_head/tests.rs
+++ substrate/client/rpc-spec-v2/src/chain_head/tests.rs
@@ -3661,6 +3661,7 @@ async fn follow_unique_pruned_blocks() {
 			subscription_max_ongoing_operations: MAX_OPERATIONS,
 			operation_max_storage_items: MAX_PAGINATION_LIMIT,
 			max_follow_subscriptions_per_connection: MAX_FOLLOW_SUBSCRIPTIONS_PER_CONNECTION,
+			max_lagging_distance: MAX_LAGGING_DISTANCE,
 		},
 	)
 	.into_rpc();
