9a82b095e17268ff9109bda480bffb7fc6bdf007
Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
2024-04-10 18:33:09 +0200

    Dont increase the wrong config 🤦
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>

--- cumulus/parachains/runtimes/coretime/coretime-westend/src/lib.rs
+++ cumulus/parachains/runtimes/coretime/coretime-westend/src/lib.rs
@@ -299,9 +299,7 @@ impl pallet_message_queue::Config for Runtime {
 	// The XCMP queue pallet is only ever able to handle the `Sibling(ParaId)` origin:
 	type QueueChangeHandler = NarrowOriginToSibling<XcmpQueue>;
 	type QueuePausedQuery = NarrowOriginToSibling<XcmpQueue>;
-	// Most on-chain HRMP channels are configured to use 102400 bytes of max message size, so we
-	// need to set the page size larger than that until we reduce the channel size on-chain.
-	type HeapSize = sp_core::ConstU32<{ 103 * 1024 }>;
+	type HeapSize = sp_core::ConstU32<{ 64 * 1024 }>;
 	type MaxStale = sp_core::ConstU32<8>;
 	type ServiceWeight = MessageQueueServiceWeight;
 	type IdleMaxServiceWeight = MessageQueueServiceWeight;
