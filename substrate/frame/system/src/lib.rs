3a2a8c52d685171a72c6f5fceceb3db7810b650b
Nikhil Gupta <17176722+gupnik@users.noreply.github.com>
2024-04-11 16:28:57 +0530

    Minor fix

--- substrate/frame/system/src/lib.rs
+++ substrate/frame/system/src/lib.rs
@@ -741,7 +741,7 @@ pub mod pallet {
 		#[cfg(feature = "experimental")]
 		#[pallet::call_index(8)]
 		#[pallet::weight(task.weight())]
-		pub fn do_task(origin: OriginFor<T>, task: T::RuntimeTask) -> DispatchResultWithPostInfo {
+		pub fn do_task(_origin: OriginFor<T>, task: T::RuntimeTask) -> DispatchResultWithPostInfo {
 			if !task.is_valid() {
 				return Err(Error::<T>::InvalidTask.into())
 			}
