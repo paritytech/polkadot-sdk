6702b20f9485c36de973b5824fe18f39a0d85d23
paritytech-ci <devops-team@parity.io>
2024-04-12 23:17:06 +0000

    ___Updated docs for master___

--- master/trait.impl/staging_xcm/v2/traits/trait.GetWeight.js
+++ master/trait.impl/staging_xcm/v2/traits/trait.GetWeight.js
@@ -1,4 +1,3 @@
 (function() {var implementors = {
-"emulated_integration_tests_common":[],
 "staging_xcm":[]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- master/type.impl/assets_common/type.LocationConvertedConcreteId.js
+++
@@ -1,4 +0,0 @@
-(function() {var type_impls = {
-"assets_common":[],
-"penpal_runtime":[]
-};if (window.register_type_impls) {window.register_type_impls(type_impls);} else {window.pending_type_impls = type_impls;}})()
