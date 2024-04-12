paritytech/parity-bridges-common

slash registered relayer if it has submitted failed GRANDPA transaction

bin/runtime-common/src/extensions/check_obsolete_extension.rs
@@ -19,11 +19,14 @@
 //! checks.
 
 use crate::messages_call_ext::MessagesCallSubType;
-use pallet_bridge_grandpa::CallSubType as GrandpaCallSubType;
+use bp_relayers::ExplicitOrAccountParams;
+use pallet_bridge_grandpa::{
+	BridgedBlockNumber, CallSubType as GrandpaCallSubType, SubmitFinalityProofHelper,
+};
 use pallet_bridge_parachains::CallSubType as ParachainsCallSubtype;
 use pallet_bridge_relayers::Pallet as RelayersPallet;
 use sp_runtime::{
-	traits::{AsSystemOriginSigner, Get, PhantomData},
+	traits::{Get, PhantomData},
 	transaction_validity::{TransactionPriority, TransactionValidity},
 };
 
@@ -31,9 +34,23 @@ use sp_runtime::{
 ///
 /// We need this trait in order to be able to implement it for the messages pallet,
 /// since the implementation is done outside of the pallet crate.
-pub trait BridgeRuntimeFilterCall<Origin, Call> {
-	/// Checks if a runtime call is valid.
-	fn validate(who: &Origin, call: &Call) -> TransactionValidity;
+pub trait BridgeRuntimeFilterCall<AccountId, Call> {
+	/// Data that may be passed from the validate to `on_failure`.
+	type ToPostDispatch;
+	/// Called during validation. Needs to checks whether a runtime call, submitted
+	/// by the `who` is valid. `who` may be `None` if transaction is not signed
+	/// by a regular account.
+	fn validate(
+		who: &Option<AccountId>,
+		call: &Call,
+	) -> (Self::ToPostDispatch, TransactionValidity);
+	/// Called after transaction is dispatched.
+	fn post_dispatch(
+		_who: &Option<AccountId>,
+		_has_failed: bool,
+		_to_on_failure: Self::ToPostDispatch,
+	) {
+	}
 }
 
 /// Wrapper for the bridge GRANDPA pallet that checks calls for obsolete submissions
@@ -42,61 +59,95 @@ pub trait BridgeRuntimeFilterCall<Origin, Call> {
 /// `(BundledHeaderNumber - 1 - BestFinalizedHeaderNumber) * Priority::get()`.
 /// The boost is only applied if submitter has active registration in the relayers
 /// pallet.
-pub struct CheckAndBoostBridgeGrandpaTransactions<T, I, Priority>(PhantomData<(T, I, Priority)>);
+pub struct CheckAndBoostBridgeGrandpaTransactions<T, I, Priority, SlashAccount>(
+	PhantomData<(T, I, Priority, SlashAccount)>,
+);
 
-impl<T, I: 'static, Priority: Get<TransactionPriority>>
-	BridgeRuntimeFilterCall<T::RuntimeOrigin, T::RuntimeCall>
-	for CheckAndBoostBridgeGrandpaTransactions<T, I, Priority>
+impl<T, I: 'static, Priority: Get<TransactionPriority>, SlashAccount: Get<T::AccountId>>
+	BridgeRuntimeFilterCall<T::AccountId, T::RuntimeCall>
+	for CheckAndBoostBridgeGrandpaTransactions<T, I, Priority, SlashAccount>
 where
 	T: pallet_bridge_relayers::Config + pallet_bridge_grandpa::Config<I>,
 	T::RuntimeCall: GrandpaCallSubType<T, I>,
-	T::RuntimeOrigin: AsSystemOriginSigner<T::AccountId>,
 {
-	fn validate(who: &T::RuntimeOrigin, call: &T::RuntimeCall) -> TransactionValidity {
+	// bridged header number, bundled in transaction
+	type ToPostDispatch = Option<BridgedBlockNumber<T, I>>;
+
+	fn validate(
+		who: &Option<T::AccountId>,
+		call: &T::RuntimeCall,
+	) -> (Self::ToPostDispatch, TransactionValidity) {
 		// we only boost priority if relayer has staked required balance
 		let is_relayer_registration_active = who
-			.as_system_origin_signer()
+			.as_ref()
 			.map(|relayer| RelayersPallet::<T>::is_registration_active(relayer))
 			.unwrap_or(false);
 		let boost_per_header = if is_relayer_registration_active { Priority::get() } else { 0 };
 
 		GrandpaCallSubType::<T, I>::check_obsolete_submit_finality_proof(call, boost_per_header)
 	}
+
+	fn post_dispatch(
+		who: &Option<T::AccountId>,
+		has_failed: bool,
+		bundled_block_number: Self::ToPostDispatch,
+	) {
+		// we are only interested in associated pallet submissions
+		let Some(bundled_block_number) = bundled_block_number else { return };
+		// we are only interested in failed or unneeded transactions
+		let has_failed =
+			has_failed || !SubmitFinalityProofHelper::<T, I>::was_successful(bundled_block_number);
+
+		if !has_failed {
+			return
+		}
+
+		// let's slash registered relayer
+		if let Some(ref relayer) = *who {
+			RelayersPallet::<T>::slash_and_deregister(
+				relayer,
+				ExplicitOrAccountParams::Explicit(SlashAccount::get()),
+			);
+		}
+	}
 }
 
-impl<T, I: 'static> BridgeRuntimeFilterCall<T::RuntimeOrigin, T::RuntimeCall>
+impl<T, I: 'static> BridgeRuntimeFilterCall<T::AccountId, T::RuntimeCall>
 	for pallet_bridge_grandpa::Pallet<T, I>
 where
 	T: pallet_bridge_grandpa::Config<I>,
 	T::RuntimeCall: GrandpaCallSubType<T, I>,
 {
-	fn validate(_who: &T::RuntimeOrigin, call: &T::RuntimeCall) -> TransactionValidity {
-		GrandpaCallSubType::<T, I>::check_obsolete_submit_finality_proof(call, 0)
+	type ToPostDispatch = ();
+	fn validate(_who: &Option<T::AccountId>, call: &T::RuntimeCall) -> ((), TransactionValidity) {
+		((), GrandpaCallSubType::<T, I>::check_obsolete_submit_finality_proof(call, 0).1)
 	}
 }
 
-impl<T, I: 'static> BridgeRuntimeFilterCall<T::RuntimeOrigin, T::RuntimeCall>
+impl<T, I: 'static> BridgeRuntimeFilterCall<T::AccountId, T::RuntimeCall>
 	for pallet_bridge_parachains::Pallet<T, I>
 where
 	T: pallet_bridge_parachains::Config<I>,
 	T::RuntimeCall: ParachainsCallSubtype<T, I>,
 {
-	fn validate(_who: &T::RuntimeOrigin, call: &T::RuntimeCall) -> TransactionValidity {
-		ParachainsCallSubtype::<T, I>::check_obsolete_submit_parachain_heads(call)
+	type ToPostDispatch = ();
+	fn validate(_who: &Option<T::AccountId>, call: &T::RuntimeCall) -> ((), TransactionValidity) {
+		((), ParachainsCallSubtype::<T, I>::check_obsolete_submit_parachain_heads(call))
 	}
 }
 
 impl<T: pallet_bridge_messages::Config<I>, I: 'static>
-	BridgeRuntimeFilterCall<T::RuntimeOrigin, T::RuntimeCall> for pallet_bridge_messages::Pallet<T, I>
+	BridgeRuntimeFilterCall<T::AccountId, T::RuntimeCall> for pallet_bridge_messages::Pallet<T, I>
 where
 	T::RuntimeCall: MessagesCallSubType<T, I>,
 {
+	type ToPostDispatch = ();
 	/// Validate messages in order to avoid "mining" messages delivery and delivery confirmation
 	/// transactions, that are delivering outdated messages/confirmations. Without this validation,
 	/// even honest relayers may lose their funds if there are multiple relays running and
 	/// submitting the same messages/confirmations.
-	fn validate(_who: &T::RuntimeOrigin, call: &T::RuntimeCall) -> TransactionValidity {
-		call.check_obsolete_call()
+	fn validate(_who: &Option<T::AccountId>, call: &T::RuntimeCall) -> ((), TransactionValidity) {
+		((), call.check_obsolete_call())
 	}
 }
 
@@ -124,9 +175,21 @@ macro_rules! generate_bridge_reject_obsolete_headers_and_messages {
 			const IDENTIFIER: &'static str = "BridgeRejectObsoleteHeadersAndMessages";
 			type Implicit = ();
 		}
-		impl<Context> sp_runtime::traits::TransactionExtension<$call, Context> for BridgeRejectObsoleteHeadersAndMessages {
-			type Pre = ();
-			type Val = ();
+		impl<Context> sp_runtime::traits::TransactionExtension<$call, Context> for BridgeRejectObsoleteHeadersAndMessages
+		where
+			$account_id: Clone,
+			<$call as sp_runtime::traits::Dispatchable>::RuntimeOrigin: sp_runtime::traits::AsSystemOriginSigner<$account_id>,
+		{
+			type Val = (
+				Option<$account_id>,
+				( $(
+					<$filter_call as $crate::extensions::check_obsolete_extension::BridgeRuntimeFilterCall<
+						$account_id,
+						$call,
+					>>::ToPostDispatch,
+				)* ),
+			);
+			type Pre = Self::Val;
 
 			fn validate(
 				&self,
@@ -144,28 +207,61 @@ macro_rules! generate_bridge_reject_obsolete_headers_and_messages {
 					<$call as sp_runtime::traits::Dispatchable>::RuntimeOrigin,
 				), sp_runtime::transaction_validity::TransactionValidityError
 			> {
+				use tuplex::PushBack;
+				use sp_runtime::traits::AsSystemOriginSigner;
+				let maybe_relayer = origin.as_system_origin_signer().cloned();
 				let tx_validity = sp_runtime::transaction_validity::ValidTransaction::default();
+				let to_prepare = ();
 				$(
-					let call_filter_validity = <
+					let (from_validate, call_filter_validity) = <
 						$filter_call as
 						$crate::extensions::check_obsolete_extension::BridgeRuntimeFilterCall<
-							<$call as sp_runtime::traits::Dispatchable>::RuntimeOrigin,
+							$account_id,
 							$call,
-						>>::validate(&origin, call)?;
-					let tx_validity = tx_validity.combine_with(call_filter_validity);
+						>>::validate(&maybe_relayer, call);
+					let tx_validity = tx_validity.combine_with(call_filter_validity?);
+					let to_prepare = to_prepare.push_back(from_validate);
 				)*
-				Ok((tx_validity, (), origin))
+				Ok((tx_validity, (maybe_relayer, to_prepare), origin))
 			}
 
 			fn prepare(
 				self,
-				_val: Self::Val,
+				to_post_dispatch: Self::Val,
 				_origin: &<$call as sp_runtime::traits::Dispatchable>::RuntimeOrigin,
 				_call: &$call,
 				_info: &sp_runtime::traits::DispatchInfoOf<$call>,
 				_len: usize,
 				_context: &Context,
 			) -> Result<Self::Pre, sp_runtime::transaction_validity::TransactionValidityError> {
+				Ok(to_post_dispatch)
+			}
+
+			#[allow(unused_variables)]
+			fn post_dispatch(
+				to_post_dispatch: Self::Pre,
+				_info: &sp_runtime::traits::DispatchInfoOf<$call>,
+				_post_info: &sp_runtime::traits::PostDispatchInfoOf<$call>,
+				_len: usize,
+				result: &sp_runtime::DispatchResult,
+				_context: &Context,
+			) -> Result<(), sp_runtime::transaction_validity::TransactionValidityError> {
+				if result.is_ok() {
+					return Ok(());
+				}
+
+				use tuplex::PopFront;
+				let has_failed = result.is_err();
+				let (maybe_relayer, to_post_dispatch) = to_post_dispatch;
+				$(
+					let (item, to_post_dispatch) = to_post_dispatch.pop_front();
+					<
+						$filter_call as
+						$crate::extensions::check_obsolete_extension::BridgeRuntimeFilterCall<
+							$account_id,
+							$call,
+						>>::post_dispatch(&maybe_relayer, has_failed, item);
+				)*
 				Ok(())
 			}
 		}
@@ -188,13 +284,22 @@ mod tests {
 		transaction_validity::{InvalidTransaction, TransactionValidity, ValidTransaction},
 	};
 
+	#[derive(Clone, Debug, PartialEq)]
+	pub struct AccountId;
+
+	impl sp_runtime::traits::AsSystemOriginSigner<AccountId> for AccountId {
+		fn as_system_origin_signer(&self) -> Option<&AccountId> {
+			None
+		}
+	}
+
 	#[derive(Encode)]
 	pub struct MockCall {
 		data: u32,
 	}
 
 	impl sp_runtime::traits::Dispatchable for MockCall {
-		type RuntimeOrigin = u64;
+		type RuntimeOrigin = AccountId;
 		type Config = ();
 		type Info = ();
 		type PostInfo = ();
@@ -207,25 +312,27 @@ mod tests {
 		}
 	}
 
-	struct FirstFilterCall;
-	impl BridgeRuntimeFilterCall<u64, MockCall> for FirstFilterCall {
-		fn validate(_who: &u64, call: &MockCall) -> TransactionValidity {
+	pub struct FirstFilterCall;
+	impl BridgeRuntimeFilterCall<AccountId, MockCall> for FirstFilterCall {
+		type ToPostDispatch = u64;
+		fn validate(_who: &Option<AccountId>, call: &MockCall) -> (u64, TransactionValidity) {
 			if call.data <= 1 {
-				return InvalidTransaction::Custom(1).into()
+				return (1, InvalidTransaction::Custom(1).into())
 			}
 
-			Ok(ValidTransaction { priority: 1, ..Default::default() })
+			(1, Ok(ValidTransaction { priority: 1, ..Default::default() }))
 		}
 	}
 
-	struct SecondFilterCall;
-	impl BridgeRuntimeFilterCall<u64, MockCall> for SecondFilterCall {
-		fn validate(_who: &u64, call: &MockCall) -> TransactionValidity {
+	pub struct SecondFilterCall;
+	impl BridgeRuntimeFilterCall<AccountId, MockCall> for SecondFilterCall {
+		type ToPostDispatch = u64;
+		fn validate(_who: &Option<AccountId>, call: &MockCall) -> (u64, TransactionValidity) {
 			if call.data <= 2 {
-				return InvalidTransaction::Custom(2).into()
+				return (2, InvalidTransaction::Custom(2).into())
 			}
 
-			Ok(ValidTransaction { priority: 2, ..Default::default() })
+			(2, Ok(ValidTransaction { priority: 2, ..Default::default() }))
 		}
 	}
 
@@ -233,32 +340,44 @@ mod tests {
 	fn test() {
 		generate_bridge_reject_obsolete_headers_and_messages!(
 			MockCall,
-			u64,
+			AccountId,
 			FirstFilterCall,
 			SecondFilterCall
 		);
 
 		assert_err!(
-			BridgeRejectObsoleteHeadersAndMessages.validate_only(0, &MockCall { data: 1 }, &(), 0),
+			BridgeRejectObsoleteHeadersAndMessages.validate_only(
+				AccountId,
+				&MockCall { data: 1 },
+				&(),
+				0
+			),
 			InvalidTransaction::Custom(1)
 		);
 
 		assert_err!(
-			BridgeRejectObsoleteHeadersAndMessages.validate_only(0, &MockCall { data: 2 }, &(), 0),
+			BridgeRejectObsoleteHeadersAndMessages.validate_only(
+				AccountId,
+				&MockCall { data: 2 },
+				&(),
+				0
+			),
 			InvalidTransaction::Custom(2)
 		);
 
-		assert_eq!(
-			BridgeRejectObsoleteHeadersAndMessages
-				.validate_only(0, &MockCall { data: 3 }, &(), 0)
-				.unwrap()
-				.0,
-			ValidTransaction { priority: 3, ..Default::default() }
-		)
+		let result = BridgeRejectObsoleteHeadersAndMessages
+			.validate_only(AccountId, &MockCall { data: 3 }, &(), 0)
+			.unwrap();
+		assert_eq!(result.0, ValidTransaction { priority: 3, ..Default::default() });
+		assert_eq!(result.1, (None, (1, 2)));
+	}
+
+	frame_support::parameter_types! {
+		pub SlashDestination: ThisChainAccountId = 42;
 	}
 
 	type BridgeGrandpaWrapper =
-		CheckAndBoostBridgeGrandpaTransactions<TestRuntime, (), ConstU64<1_000>>;
+		CheckAndBoostBridgeGrandpaTransactions<TestRuntime, (), ConstU64<1_000>, SlashDestination>;
 
 	#[test]
 	fn grandpa_wrapper_does_not_boost_extensions_for_unregistered_relayer() {
@@ -266,9 +385,10 @@ mod tests {
 			initialize_environment(100, 100, 100);
 
 			let priority_boost = BridgeGrandpaWrapper::validate(
-				&RuntimeOrigin::signed(relayer_account_at_this_chain()),
+				&Some(relayer_account_at_this_chain()),
 				&submit_relay_header_call_ex(200),
 			)
+			.1
 			.unwrap()
 			.priority;
 			assert_eq!(priority_boost, 0);
@@ -283,12 +403,30 @@ mod tests {
 				.unwrap();
 
 			let priority_boost = BridgeGrandpaWrapper::validate(
-				&RuntimeOrigin::signed(relayer_account_at_this_chain()),
+				&Some(relayer_account_at_this_chain()),
 				&submit_relay_header_call_ex(200),
 			)
+			.1
 			.unwrap()
 			.priority;
 			assert_eq!(priority_boost, 99_000);
+		})
+	}
+
+	#[test]
+	fn grandpa_wrapper_slashes_registered_relayer_if_transaction_fails() {
+		run_test(|| {
+			initialize_environment(100, 100, 100);
+			BridgeRelayers::register(RuntimeOrigin::signed(relayer_account_at_this_chain()), 1000)
+				.unwrap();
+
+			assert!(BridgeRelayers::is_registration_active(&relayer_account_at_this_chain()));
+			BridgeGrandpaWrapper::post_dispatch(
+				&Some(relayer_account_at_this_chain()),
+				true,
+				Some(150),
+			);
+			assert!(!BridgeRelayers::is_registration_active(&relayer_account_at_this_chain()));
 		})
 	}
 }

paritytech/polkadot-sdk

bridges/bin/runtime-common/src/extensions/check_obsolete_extension.rs
@@ -19,7 +19,10 @@
 //! checks.
 
 use crate::messages_call_ext::MessagesCallSubType;
-use pallet_bridge_grandpa::CallSubType as GrandpaCallSubType;
+use bp_relayers::ExplicitOrAccountParams;
+use pallet_bridge_grandpa::{
+	BridgedBlockNumber, CallSubType as GrandpaCallSubType, SubmitFinalityProofHelper,
+};
 use pallet_bridge_parachains::CallSubType as ParachainsCallSubtype;
 use pallet_bridge_relayers::Pallet as RelayersPallet;
 use sp_runtime::{
@@ -32,8 +35,14 @@ use sp_runtime::{
 /// We need this trait in order to be able to implement it for the messages pallet,
 /// since the implementation is done outside of the pallet crate.
 pub trait BridgeRuntimeFilterCall<AccountId, Call> {
-	/// Checks if a runtime call is valid.
-	fn validate(who: &AccountId, call: &Call) -> TransactionValidity;
+	/// Data that may be passed from the validate to `on_failure`.
+	type ToPostDispatch;
+	/// Called during validation. Needs to checks whether a runtime call, submitted
+	/// by the `who` is valid. `who` may be `None` if transaction is not signed
+	/// by a regular account.
+	fn validate(who: &AccountId, call: &Call) -> (Self::ToPostDispatch, TransactionValidity);
+	/// Called after transaction is dispatched.
+	fn post_dispatch(_who: &AccountId, _has_failed: bool, _to_on_failure: Self::ToPostDispatch) {}
 }
 
 /// Wrapper for the bridge GRANDPA pallet that checks calls for obsolete submissions
@@ -42,21 +51,51 @@ pub trait BridgeRuntimeFilterCall<AccountId, Call> {
 /// `(BundledHeaderNumber - 1 - BestFinalizedHeaderNumber) * Priority::get()`.
 /// The boost is only applied if submitter has active registration in the relayers
 /// pallet.
-pub struct CheckAndBoostBridgeGrandpaTransactions<T, I, Priority>(PhantomData<(T, I, Priority)>);
+pub struct CheckAndBoostBridgeGrandpaTransactions<T, I, Priority, SlashAccount>(
+	PhantomData<(T, I, Priority, SlashAccount)>,
+);
 
-impl<T, I: 'static, Priority: Get<TransactionPriority>>
+impl<T, I: 'static, Priority: Get<TransactionPriority>, SlashAccount: Get<T::AccountId>>
 	BridgeRuntimeFilterCall<T::AccountId, T::RuntimeCall>
-	for CheckAndBoostBridgeGrandpaTransactions<T, I, Priority>
+	for CheckAndBoostBridgeGrandpaTransactions<T, I, Priority, SlashAccount>
 where
 	T: pallet_bridge_relayers::Config + pallet_bridge_grandpa::Config<I>,
 	T::RuntimeCall: GrandpaCallSubType<T, I>,
 {
-	fn validate(who: &T::AccountId, call: &T::RuntimeCall) -> TransactionValidity {
+	// bridged header number, bundled in transaction
+	type ToPostDispatch = Option<BridgedBlockNumber<T, I>>;
+
+	fn validate(
+		who: &T::AccountId,
+		call: &T::RuntimeCall,
+	) -> (Self::ToPostDispatch, TransactionValidity) {
 		// we only boost priority if relayer has staked required balance
 		let is_relayer_registration_active = RelayersPallet::<T>::is_registration_active(who);
 		let boost_per_header = if is_relayer_registration_active { Priority::get() } else { 0 };
 
 		GrandpaCallSubType::<T, I>::check_obsolete_submit_finality_proof(call, boost_per_header)
+	}
+
+	fn post_dispatch(
+		relayer: &T::AccountId,
+		has_failed: bool,
+		bundled_block_number: Self::ToPostDispatch,
+	) {
+		// we are only interested in associated pallet submissions
+		let Some(bundled_block_number) = bundled_block_number else { return };
+		// we are only interested in failed or unneeded transactions
+		let has_failed =
+			has_failed || !SubmitFinalityProofHelper::<T, I>::was_successful(bundled_block_number);
+
+		if !has_failed {
+			return
+		}
+
+		// let's slash registered relayer
+		RelayersPallet::<T>::slash_and_deregister(
+			relayer,
+			ExplicitOrAccountParams::Explicit(SlashAccount::get()),
+		);
 	}
 }
 
@@ -66,8 +105,9 @@ where
 	T: pallet_bridge_grandpa::Config<I>,
 	T::RuntimeCall: GrandpaCallSubType<T, I>,
 {
-	fn validate(_who: &T::AccountId, call: &T::RuntimeCall) -> TransactionValidity {
-		GrandpaCallSubType::<T, I>::check_obsolete_submit_finality_proof(call, 0)
+	type ToPostDispatch = ();
+	fn validate(_who: &T::AccountId, call: &T::RuntimeCall) -> ((), TransactionValidity) {
+		((), GrandpaCallSubType::<T, I>::check_obsolete_submit_finality_proof(call, 0).1)
 	}
 }
 
@@ -77,8 +117,9 @@ where
 	T: pallet_bridge_parachains::Config<I>,
 	T::RuntimeCall: ParachainsCallSubtype<T, I>,
 {
-	fn validate(_who: &T::AccountId, call: &T::RuntimeCall) -> TransactionValidity {
-		ParachainsCallSubtype::<T, I>::check_obsolete_submit_parachain_heads(call)
+	type ToPostDispatch = ();
+	fn validate(_who: &T::AccountId, call: &T::RuntimeCall) -> ((), TransactionValidity) {
+		((), ParachainsCallSubtype::<T, I>::check_obsolete_submit_parachain_heads(call))
 	}
 }
 
@@ -87,12 +128,13 @@ impl<T: pallet_bridge_messages::Config<I>, I: 'static>
 where
 	T::RuntimeCall: MessagesCallSubType<T, I>,
 {
+	type ToPostDispatch = ();
 	/// Validate messages in order to avoid "mining" messages delivery and delivery confirmation
 	/// transactions, that are delivering outdated messages/confirmations. Without this validation,
 	/// even honest relayers may lose their funds if there are multiple relays running and
 	/// submitting the same messages/confirmations.
-	fn validate(_who: &T::AccountId, call: &T::RuntimeCall) -> TransactionValidity {
-		call.check_obsolete_call()
+	fn validate(_who: &T::AccountId, call: &T::RuntimeCall) -> ((), TransactionValidity) {
+		((), call.check_obsolete_call())
 	}
 }
 
@@ -121,7 +163,15 @@ macro_rules! generate_bridge_reject_obsolete_headers_and_messages {
 			type AccountId = $account_id;
 			type Call = $call;
 			type AdditionalSigned = ();
-			type Pre = ();
+			type Pre = (
+				$account_id,
+				( $(
+					<$filter_call as $crate::extensions::check_obsolete_extension::BridgeRuntimeFilterCall<
+						$account_id,
+						$call,
+					>>::ToPostDispatch,
+				)* ),
+			);
 
 			fn additional_signed(&self) -> sp_std::result::Result<
 				(),
@@ -130,6 +180,7 @@ macro_rules! generate_bridge_reject_obsolete_headers_and_messages {
 				Ok(())
 			}
 
+			#[allow(unused_variables)]
 			fn validate(
 				&self,
 				who: &Self::AccountId,
@@ -138,26 +189,65 @@ macro_rules! generate_bridge_reject_obsolete_headers_and_messages {
 				_len: usize,
 			) -> sp_runtime::transaction_validity::TransactionValidity {
 				let tx_validity = sp_runtime::transaction_validity::ValidTransaction::default();
+				let to_prepare = ();
 				$(
-					let call_filter_validity = <
+					let (from_validate, call_filter_validity) = <
 						$filter_call as
 						$crate::extensions::check_obsolete_extension::BridgeRuntimeFilterCall<
 							Self::AccountId,
 							$call,
-						>>::validate(&who, call)?;
-					let tx_validity = tx_validity.combine_with(call_filter_validity);
+						>>::validate(&who, call);
+					let tx_validity = tx_validity.combine_with(call_filter_validity?);
 				)*
 				Ok(tx_validity)
 			}
 
+			#[allow(unused_variables)]
 			fn pre_dispatch(
 				self,
-				who: &Self::AccountId,
+				relayer: &Self::AccountId,
 				call: &Self::Call,
 				info: &sp_runtime::traits::DispatchInfoOf<Self::Call>,
 				len: usize,
 			) -> Result<Self::Pre, sp_runtime::transaction_validity::TransactionValidityError> {
-				self.validate(who, call, info, len).map(drop)
+				use tuplex::PushBack;
+				let to_post_dispatch = ();
+				$(
+					let (from_validate, call_filter_validity) = <
+						$filter_call as
+						$crate::extensions::check_obsolete_extension::BridgeRuntimeFilterCall<
+							$account_id,
+							$call,
+						>>::validate(&relayer, call);
+					let _ = call_filter_validity?;
+					let to_post_dispatch = to_post_dispatch.push_back(from_validate);
+				)*
+				Ok((relayer.clone(), to_post_dispatch))
+			}
+
+			#[allow(unused_variables)]
+			fn post_dispatch(
+				to_post_dispatch: Option<Self::Pre>,
+				info: &sp_runtime::traits::DispatchInfoOf<Self::Call>,
+				post_info: &sp_runtime::traits::PostDispatchInfoOf<Self::Call>,
+				len: usize,
+				result: &sp_runtime::DispatchResult,
+			) -> Result<(), sp_runtime::transaction_validity::TransactionValidityError> {
+				// TODO: check me: removed if result.is_ok() { return Ok(()); }
+				use tuplex::PopFront;
+				let has_failed = result.is_err();
+				// TODO: check me: return if `to_post_dispatch` is `None`
+				let Some((relayer, to_post_dispatch)) = to_post_dispatch else { return Ok(()) };
+				$(
+					let (item, to_post_dispatch) = to_post_dispatch.pop_front();
+					<
+						$filter_call as
+						$crate::extensions::check_obsolete_extension::BridgeRuntimeFilterCall<
+							$account_id,
+							$call,
+						>>::post_dispatch(&relayer, has_failed, item);
+				)*
+				Ok(())
 			}
 		}
 	};
@@ -196,36 +286,40 @@ mod tests {
 		}
 	}
 
-	struct FirstFilterCall;
+	pub struct FirstFilterCall;
 	impl BridgeRuntimeFilterCall<u64, MockCall> for FirstFilterCall {
-		fn validate(_who: &u64, call: &MockCall) -> TransactionValidity {
+		type ToPostDispatch = u64;
+		fn validate(_who: &u64, call: &MockCall) -> (u64, TransactionValidity) {
 			if call.data <= 1 {
-				return InvalidTransaction::Custom(1).into()
+				return (1, InvalidTransaction::Custom(1).into())
 			}
 
-			Ok(ValidTransaction { priority: 1, ..Default::default() })
+			(1, Ok(ValidTransaction { priority: 1, ..Default::default() }))
 		}
 	}
 
-	struct SecondFilterCall;
+	pub struct SecondFilterCall;
 	impl BridgeRuntimeFilterCall<u64, MockCall> for SecondFilterCall {
-		fn validate(_who: &u64, call: &MockCall) -> TransactionValidity {
+		type ToPostDispatch = u64;
+		fn validate(_who: &u64, call: &MockCall) -> (u64, TransactionValidity) {
 			if call.data <= 2 {
-				return InvalidTransaction::Custom(2).into()
+				return (2, InvalidTransaction::Custom(2).into())
 			}
 
-			Ok(ValidTransaction { priority: 2, ..Default::default() })
+			(2, Ok(ValidTransaction { priority: 2, ..Default::default() }))
 		}
 	}
 
 	#[test]
-	fn test() {
+	fn test_generated_obsolete_extension() {
 		generate_bridge_reject_obsolete_headers_and_messages!(
 			MockCall,
 			u64,
 			FirstFilterCall,
 			SecondFilterCall
 		);
+
+		// TODO: add tests for both validate and pre_dispatch here?
 
 		assert_err!(
 			BridgeRejectObsoleteHeadersAndMessages.validate(&42, &MockCall { data: 1 }, &(), 0),
@@ -238,13 +332,25 @@ mod tests {
 		);
 
 		assert_eq!(
-			BridgeRejectObsoleteHeadersAndMessages.validate(&42, &MockCall { data: 3 }, &(), 0),
-			Ok(ValidTransaction { priority: 3, ..Default::default() })
-		)
+			BridgeRejectObsoleteHeadersAndMessages
+				.validate(&42, &MockCall { data: 3 }, &(), 0)
+				.unwrap(),
+			ValidTransaction { priority: 3, ..Default::default() },
+		);
+		assert_eq!(
+			BridgeRejectObsoleteHeadersAndMessages
+				.pre_dispatch(&42, &MockCall { data: 3 }, &(), 0)
+				.unwrap(),
+			(42, (1, 2)),
+		);
+	}
+
+	frame_support::parameter_types! {
+		pub SlashDestination: ThisChainAccountId = 42;
 	}
 
 	type BridgeGrandpaWrapper =
-		CheckAndBoostBridgeGrandpaTransactions<TestRuntime, (), ConstU64<1_000>>;
+		CheckAndBoostBridgeGrandpaTransactions<TestRuntime, (), ConstU64<1_000>, SlashDestination>;
 
 	#[test]
 	fn grandpa_wrapper_does_not_boost_extensions_for_unregistered_relayer() {
@@ -255,6 +361,7 @@ mod tests {
 				&relayer_account_at_this_chain(),
 				&submit_relay_header_call_ex(200),
 			)
+			.1
 			.unwrap()
 			.priority;
 			assert_eq!(priority_boost, 0);
@@ -272,9 +379,23 @@ mod tests {
 				&relayer_account_at_this_chain(),
 				&submit_relay_header_call_ex(200),
 			)
+			.1
 			.unwrap()
 			.priority;
 			assert_eq!(priority_boost, 99_000);
+		})
+	}
+
+	#[test]
+	fn grandpa_wrapper_slashes_registered_relayer_if_transaction_fails() {
+		run_test(|| {
+			initialize_environment(100, 100, 100);
+			BridgeRelayers::register(RuntimeOrigin::signed(relayer_account_at_this_chain()), 1000)
+				.unwrap();
+
+			assert!(BridgeRelayers::is_registration_active(&relayer_account_at_this_chain()));
+			BridgeGrandpaWrapper::post_dispatch(&relayer_account_at_this_chain(), true, Some(150));
+			assert!(!BridgeRelayers::is_registration_active(&relayer_account_at_this_chain()));
 		})
 	}
 }