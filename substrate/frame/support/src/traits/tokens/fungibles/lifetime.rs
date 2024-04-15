0b025b36d9e4b1d042a852c05fa900d1f41e3070
muharem <ismailov.m.h@gmail.com>
2024-04-15 17:12:16 +0200

    Revert "update refund trait"
    This reverts commit 290c0f1e84597374f6760e28aff731f96f2b4826.

--- substrate/frame/support/src/traits/tokens/fungibles/lifetime.rs
+++ substrate/frame/support/src/traits/tokens/fungibles/lifetime.rs
@@ -34,7 +34,11 @@ pub trait Create<AccountId>: Inspect<AccountId> {
 }
 
 /// Trait for refunding the deposit of a target asset account.
-pub trait Refund<AccountId>: Inspect<AccountId> {
+pub trait Refund<AccountId> {
+	/// Means of identifying one asset class from another.
+	type AssetId: AssetId;
+	/// Scalar type for representing balance of an account.
+	type Balance: Balance;
 	/// Returns the amount of account deposit and depositor address, if any.
 	fn deposit_held(id: Self::AssetId, who: AccountId) -> Option<(AccountId, Self::Balance)>;
 	/// Return the deposit (if any) of a target asset account.
