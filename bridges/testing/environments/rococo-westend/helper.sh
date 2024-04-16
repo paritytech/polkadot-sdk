aa890401bd762fc6ce8b5baef02d481118bbee4c
Svyatoslav Nikolsky <svyatonik@gmail.com>
2024-04-16 09:11:45 +0300

    adapted 0001-asset-transfer test for new fee scheme

--- bridges/testing/environments/rococo-westend/helper.sh
+++ bridges/testing/environments/rococo-westend/helper.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-$ENV_PATH/bridges_rococo_westend.sh "$@"
+if [ $1 == "auto-log" ]; then
+    shift # ignore "auto-log"
+    log_name=$1
+    $ENV_PATH/bridges_rococo_westend.sh "$@" >$TEST_DIR/logs/$log_name.log
+else
+    $ENV_PATH/bridges_rococo_westend.sh "$@"
+fi
