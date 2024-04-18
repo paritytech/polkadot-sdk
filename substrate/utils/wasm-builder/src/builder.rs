0aef998199f793120f107cc7579b27483686841e
Bastian Köcher <info@kchr.de>
2024-04-18 09:59:51 +0200

    Fix CI

--- substrate/utils/wasm-builder/src/builder.rs
+++ substrate/utils/wasm-builder/src/builder.rs
@@ -120,7 +120,7 @@ impl WasmBuilder {
 	///
 	/// This is the same as calling:
 	/// ```no_run
-	/// WasmBuilder::new()
+	/// substrate_wasm_builder::WasmBuilder::new()
 	///    .with_current_project()
 	///    .import_memory()
 	///    .export_heap_base()
@@ -140,10 +140,10 @@ impl WasmBuilder {
 	///
 	/// This is the same as calling:
 	/// ```no_run
-	/// WasmBuilder::new()
+	/// substrate_wasm_builder::WasmBuilder::new()
 	///    .with_current_project()
 	///    .import_memory()
-	///    .export_heap_base()
+	///    .export_heap_base();
 	/// ```
 	pub fn init_with_defaults() -> Self {
 		WasmBuilder::new().with_current_project().import_memory().export_heap_base()
