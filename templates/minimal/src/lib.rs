6eb2866dde34cb4f258309492f7a5555dc39cd19
Nikhil Gupta <17176722+gupnik@users.noreply.github.com>
2024-04-18 14:59:46 +0530

    Fmt

--- templates/minimal/src/lib.rs
+++ templates/minimal/src/lib.rs
@@ -18,15 +18,15 @@
 //! is capable of being run on the node. It is built using the [`FRAME`](`frame`) framework
 //! that enables the composition of the core logic via separate modules called "pallets".
 //! FRAME defines a complete DSL for building such pallets and the runtime itself.
-//! 
+//!
 //! #### Transaction Fees
-//! 
+//!
 //! The runtime charges a transaction fee for every transaction that is executed. The fee is
-//! calculated based on the weight of the transaction (accouting for the execution time) and 
-//! length of the call data. Please refer to 
-//! [`benchmarking docs`](`polkadot_sdk_docs::reference_docs::frame_benchmarking_weight`) for 
+//! calculated based on the weight of the transaction (accouting for the execution time) and
+//! length of the call data. Please refer to
+//! [`benchmarking docs`](`polkadot_sdk_docs::reference_docs::frame_benchmarking_weight`) for
 //! more information on how the weight is calculated.
-//! 
+//!
 //! This template sets the fee as independent of the weight of the extrinsic and fixed for any
 //! length of the call data for demo purposes.
 //!
@@ -34,33 +34,32 @@
 //!
 //! A minimal [`pallet`](`pallet_minimal_template`) that is built using FRAME. It is a unit of
 //! encapsulated logic that has a clearly defined responsibility and can be linked to other pallets.
-//! 
+//!
 //! ## Getting Started
-//! 
+//!
 //! To get started with the template, follow the steps below:
-//! 
+//!
 //! ### Build the Node
-//! 
+//!
 //! Build the node using the following command:
-//! 
+//!
 //! ```bash
 //! cargo build -p minimal-template-node --release
 //! ```
-//! 
+//!
 //! ### Run the Node
-//! 
+//!
 //! Run the node using the following command:
-//! 
+//!
 //! ```bash
 //! ./target/release/minimal-template-node --dev
 //! ```
-//! 
+//!
 //! ### CLI Commands
-//! 
+//!
 //! The node exposes a few commands that can be used to interact with the node. To see the list of
 //! available commands, run the following command:
-//! 
+//!
 //! ```bash
 //! ./target/release/minimal_template_node --help
 //! ```
-//! 
