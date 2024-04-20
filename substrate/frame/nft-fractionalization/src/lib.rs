881c32acd389a6dc6519af5988185a832f12d87d
Diana Popa <pdiana.maria91@gmail.com>
2024-04-20 12:03:24 +0300

    fix pallet-nft-fractionalization

--- substrate/frame/nft-fractionalization/src/lib.rs
+++ substrate/frame/nft-fractionalization/src/lib.rs
@@ -364,7 +364,7 @@ pub mod pallet {
 			beneficiary: &T::AccountId,
 			amount: AssetBalanceOf<T>,
 		) -> DispatchResult {
-			T::Assets::mint_into(asset_id, beneficiary, amount)?;
+			T::Assets::mint_into(&asset_id, beneficiary, amount)?;
 			Ok(())
 		}
 
@@ -374,7 +374,7 @@ pub mod pallet {
 			account: &T::AccountId,
 			amount: AssetBalanceOf<T>,
 		) -> DispatchResult {
-			T::Assets::burn_from(asset_id.clone(), account, amount, Exact, Polite)?;
+			T::Assets::burn_from(&asset_id, account, amount, Exact, Polite)?;
 			T::Assets::start_destroy(asset_id, None)
 		}
 
