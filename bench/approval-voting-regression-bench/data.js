715886989129a81ce399cfb8a7233a19f26c70ed
github-action-benchmark <github@users.noreply.github.com>
2024-04-23 14:21:14 +0000

    add approval-voting-regression-bench (customSmallerIsBetter) benchmark result for eda5e5c31f9bffafd6afd6d14fb95001a10dba9a

--- bench/approval-voting-regression-bench/data.js
+++ bench/approval-voting-regression-bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1713877593047,
+  "lastUpdate": 1713882073960,
   "repoUrl": "https://github.com/paritytech/polkadot-sdk",
   "entries": {
     "approval-voting-regression-bench": [
@@ -234,6 +234,53 @@ window.BENCHMARK_DATA = {
           {
             "name": "test-environment",
             "value": 2.982703402650149,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "joe petrowski",
+            "username": "joepetrowski",
+            "email": "25483142+joepetrowski@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "eda5e5c31f9bffafd6afd6d14fb95001a10dba9a",
+          "message": "Fix Stuck Collator Funds (#4229)\n\nFixes https://github.com/paritytech/polkadot-sdk/issues/4206\n\nIn #1340 one of the storage types was changed from `Candidates` to\n`CandidateList`. Since the actual key includes the hash of this value,\nall of the candidates stored here are (a) \"missing\" and (b) unable to\nunreserve their candidacy bond.\n\nThis migration kills the storage values and refunds the deposit held for\neach candidate.\n\n---------\n\nSigned-off-by: georgepisaltu <george.pisaltu@parity.io>\nCo-authored-by: georgepisaltu <52418509+georgepisaltu@users.noreply.github.com>\nCo-authored-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>\nCo-authored-by: georgepisaltu <george.pisaltu@parity.io>",
+          "timestamp": "2024-04-23T12:53:20Z",
+          "url": "https://github.com/paritytech/polkadot-sdk/commit/eda5e5c31f9bffafd6afd6d14fb95001a10dba9a"
+        },
+        "date": 1713882048731,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Received from peers",
+            "value": 52941.7,
+            "unit": "KiB"
+          },
+          {
+            "name": "Sent to peers",
+            "value": 63544.55,
+            "unit": "KiB"
+          },
+          {
+            "name": "approval-distribution",
+            "value": 7.171994607919993,
+            "unit": "seconds"
+          },
+          {
+            "name": "approval-voting",
+            "value": 9.915962875349962,
+            "unit": "seconds"
+          },
+          {
+            "name": "test-environment",
+            "value": 3.0689954355900957,
             "unit": "seconds"
           }
         ]
