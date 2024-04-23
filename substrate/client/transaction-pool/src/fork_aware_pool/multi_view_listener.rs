da84b803ebd2e3824ff3c3f990a58cc93fb1d0af
Michal Kucharczyk <1728078+michalkucharczyk@users.noreply.github.com>
2024-04-23 10:32:33 +0200

    multi_view: tests added

--- substrate/client/transaction-pool/src/fork_aware_pool/multi_view_listener.rs
+++ substrate/client/transaction-pool/src/fork_aware_pool/multi_view_listener.rs
@@ -16,7 +16,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-//! Multi view listener. Combines streams from many views into single pool watcher.
+//! Multi view listener. Combines streams from many views into single transaction status stream.
 
 const LOG_TARGET: &str = "txpool::mvlistener";
 
@@ -28,38 +28,42 @@ use sp_runtime::traits::{Block as BlockT, Extrinsic, Hash as HashT};
 use std::{
 	collections::{HashMap, HashSet},
 	pin::Pin,
-	sync::TryLockError,
 };
 use tokio::sync::mpsc;
 use tokio_stream::StreamMap;
 
+// pub type TransactionStatusStream<Hash, BlockHash> =
+// 	dyn Stream<Item = TransactionStatus<Hash, BlockHash>> + Send;
 pub type TxStatusStream<T> = Pin<Box<TransactionStatusStream<TxHash<T>, BlockHash<T>>>>;
 
-enum ViewEvent<PoolApi: ChainApi> {
+enum ListenerAction<PoolApi: ChainApi> {
 	ViewAdded(BlockHash<PoolApi>, TxStatusStream<PoolApi>),
-	Invalid,
+	InvalidateTransaction,
 }
 
-impl<PoolApi> std::fmt::Debug for ViewEvent<PoolApi>
+impl<PoolApi> std::fmt::Debug for ListenerAction<PoolApi>
 where
 	PoolApi: ChainApi,
 {
 	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
 		match self {
-			ViewEvent::ViewAdded(h, _) => write!(f, "ViewEvent::ViewAdded({})", h),
-			ViewEvent::Invalid => write!(f, "ViewEvent::Invalid"),
+			ListenerAction::ViewAdded(h, _) => write!(f, "ListenerAction::ViewAdded({})", h),
+			ListenerAction::InvalidateTransaction => {
+				write!(f, "ListenerAction::InvalidateTransaction")
+			},
 		}
 	}
 }
 
 pub struct MultiViewListener<PoolApi: ChainApi> {
 	//todo: rwlock not needed here (mut?)
-	controllers: tokio::sync::RwLock<HashMap<TxHash<PoolApi>, mpsc::Sender<ViewEvent<PoolApi>>>>,
+	controllers:
+		tokio::sync::RwLock<HashMap<TxHash<PoolApi>, mpsc::Sender<ListenerAction<PoolApi>>>>,
 }
 
 struct ExternalWatcherContext<PoolApi: ChainApi> {
 	fused: futures::stream::Fuse<StreamMap<BlockHash<PoolApi>, TxStatusStream<PoolApi>>>,
-	rx: mpsc::Receiver<ViewEvent<PoolApi>>,
+	rx: mpsc::Receiver<ListenerAction<PoolApi>>,
 	terminate: bool,
 	future_seen: bool,
 	ready_seen: bool,
@@ -73,7 +77,7 @@ impl<PoolApi: ChainApi> ExternalWatcherContext<PoolApi>
 where
 	<<PoolApi as ChainApi>::Block as BlockT>::Hash: Unpin,
 {
-	fn new(rx: mpsc::Receiver<ViewEvent<PoolApi>>) -> Self {
+	fn new(rx: mpsc::Receiver<ListenerAction<PoolApi>>) -> Self {
 		let mut stream_map: StreamMap<BlockHash<PoolApi>, TxStatusStream<PoolApi>> =
 			StreamMap::new();
 		stream_map.insert(Default::default(), stream::pending().boxed());
@@ -131,6 +135,26 @@ where
 			TransactionStatus::Invalid => false,
 		}
 	}
+
+	fn handle_invalidate_transaction(&mut self) -> bool {
+		let keys =
+			HashSet::<BlockHash<PoolApi>>::from_iter(self.fused.get_ref().keys().map(Clone::clone));
+		trace!(
+		target: LOG_TARGET, "got invalidate_transaction: streams:{:#?}",
+		self.fused.get_ref().keys().collect::<Vec<_>>()
+		);
+		if self.views_keeping_tx_valid.is_disjoint(&keys) {
+			self.terminate = true;
+			true
+		} else {
+			false
+		}
+	}
+
+	fn add_stream(&mut self, block_hash: BlockHash<PoolApi>, stream: TxStatusStream<PoolApi>) {
+		trace!(target: LOG_TARGET, "got ViewAdded {:#?} streams:{:#?}", block_hash, self.fused.get_ref().keys().collect::<Vec<_>>());
+		self.fused.get_mut().insert(block_hash, stream);
+	}
 }
 
 impl<PoolApi> MultiViewListener<PoolApi>
@@ -180,18 +204,11 @@ where
 					},
 					cmd = ctx.rx.recv() => {
 						match cmd {
-							Some(ViewEvent::ViewAdded(h,stream)) => {
-								trace!(target: LOG_TARGET, "got viewEvent added {:#?}", h);
-								ctx.fused.get_mut().insert(h, stream);
+							Some(ListenerAction::ViewAdded(h,stream)) => {
+								ctx.add_stream(h, stream);
 							},
-							Some(ViewEvent::Invalid) => {
-								let keys = HashSet::<BlockHash<PoolApi>>::from_iter(ctx.fused.get_ref().keys().map(Clone::clone));
-								trace!(
-									target: LOG_TARGET, "got Invalid: streams:{:#?}",
-									ctx.fused.get_ref().keys().collect::<Vec<_>>()
-									);
-								if ctx.views_keeping_tx_valid.is_disjoint(&keys) {
-									ctx.terminate = true;
+							Some(ListenerAction::InvalidateTransaction) => {
+								if ctx.handle_invalidate_transaction() {
 									return Some((TransactionStatus::Invalid, ctx))
 								}
 							},
@@ -217,7 +234,7 @@ where
 		let mut controllers = self.controllers.write().await;
 		if let Some(tx) = controllers.get(&tx_hash) {
 			trace!(target: LOG_TARGET, "add_view_watcher_for_tx {:#?}: sent viewEvent", tx_hash);
-			match tx.send(ViewEvent::ViewAdded(block_hash, stream)).await {
+			match tx.send(ListenerAction::ViewAdded(block_hash, stream)).await {
 				Err(mpsc::error::SendError(e)) => {
 					trace!(target: LOG_TARGET, "add_view_watcher_for_tx: SendError: {:?}", e);
 					controllers.remove(&tx_hash);
@@ -234,7 +251,10 @@ where
 		let futs = invalid_hashes.into_iter().filter_map(|tx_hash| {
 			if let Some(tx) = controllers.get(&tx_hash) {
 				trace!(target: LOG_TARGET, "invalidate_transaction {:#?}", tx_hash);
-				Some(tx.send(ViewEvent::Invalid).map(move |result| (result, tx_hash)))
+				Some(
+					tx.send(ListenerAction::InvalidateTransaction)
+						.map(move |result| (result, tx_hash)),
+				)
 			} else {
 				None
 			}
@@ -250,5 +270,264 @@ where
 				},
 				Ok(_) => {},
 			});
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	use futures::StreamExt;
+	use sp_core::H256;
+
+	type MultiViewListener = super::MultiViewListener<crate::tests::TestApi>;
+
+	#[tokio::test]
+	async fn test01() {
+		sp_tracing::try_init_simple();
+		let listener = MultiViewListener::new();
+
+		let block_hash = H256::repeat_byte(0x01);
+		let events = vec![
+			TransactionStatus::Ready,
+			TransactionStatus::InBlock((block_hash, 0)),
+			TransactionStatus::Finalized((block_hash, 0)),
+		];
+
+		let tx_hash = H256::repeat_byte(0x0a);
+		let external_watcher = listener.create_external_watcher_for_tx(tx_hash).await.unwrap();
+
+		let view_stream = futures::stream::iter(events.clone());
+
+		listener.add_view_watcher_for_tx(tx_hash, block_hash, view_stream.boxed()).await;
+
+		let out = external_watcher.collect::<Vec<_>>().await;
+		assert_eq!(out, events);
+		log::info!("out: {:#?}", out);
+	}
+
+	#[tokio::test]
+	async fn test02() {
+		sp_tracing::try_init_simple();
+		let listener = MultiViewListener::new();
+
+		let block_hash0 = H256::repeat_byte(0x01);
+		let events0 = vec![
+			TransactionStatus::Future,
+			TransactionStatus::Ready,
+			TransactionStatus::InBlock((block_hash0, 0)),
+		];
+
+		let block_hash1 = H256::repeat_byte(0x02);
+		let events1 = vec![
+			TransactionStatus::Ready,
+			TransactionStatus::InBlock((block_hash1, 0)),
+			TransactionStatus::Finalized((block_hash1, 0)),
+		];
+
+		let tx_hash = H256::repeat_byte(0x0a);
+		let external_watcher = listener.create_external_watcher_for_tx(tx_hash).await.unwrap();
+
+		let view_stream0 = futures::stream::iter(events0.clone());
+		let view_stream1 = futures::stream::iter(events1.clone());
+
+		listener
+			.add_view_watcher_for_tx(tx_hash, block_hash0, view_stream0.boxed())
+			.await;
+		listener
+			.add_view_watcher_for_tx(tx_hash, block_hash1, view_stream1.boxed())
+			.await;
+
+		let out = external_watcher.collect::<Vec<_>>().await;
+		log::info!("out: {:#?}", out);
+		assert!(out.iter().all(|v| vec![
+			TransactionStatus::Future,
+			TransactionStatus::Ready,
+			TransactionStatus::InBlock((block_hash0, 0)),
+			TransactionStatus::InBlock((block_hash1, 0)),
+			TransactionStatus::Finalized((block_hash1, 0)),
+		]
+		.contains(v)));
+		assert_eq!(out.len(), 5);
+	}
+
+	#[tokio::test]
+	async fn test03() {
+		sp_tracing::try_init_simple();
+		let listener = MultiViewListener::new();
+
+		let block_hash0 = H256::repeat_byte(0x01);
+		let events0 = vec![
+			TransactionStatus::Future,
+			TransactionStatus::Ready,
+			TransactionStatus::InBlock((block_hash0, 0)),
+		];
+
+		let block_hash1 = H256::repeat_byte(0x02);
+		let events1 = vec![TransactionStatus::Future];
+
+		let tx_hash = H256::repeat_byte(0x0a);
+		let external_watcher = listener.create_external_watcher_for_tx(tx_hash).await.unwrap();
+
+		let view_stream0 = futures::stream::iter(events0.clone());
+		let view_stream1 = futures::stream::iter(events1.clone());
+
+		listener
+			.add_view_watcher_for_tx(tx_hash, block_hash0, view_stream0.boxed())
+			.await;
+		listener
+			.add_view_watcher_for_tx(tx_hash, block_hash1, view_stream1.boxed())
+			.await;
+
+		listener.invalidate_transactions(vec![tx_hash]).await;
+
+		let out = external_watcher.collect::<Vec<_>>().await;
+		log::info!("out: {:#?}", out);
+		assert!(out.iter().all(|v| vec![
+			TransactionStatus::Future,
+			TransactionStatus::Ready,
+			TransactionStatus::InBlock((block_hash0, 0)),
+			TransactionStatus::Invalid
+		]
+		.contains(v)));
+		assert_eq!(out.len(), 4);
+	}
+
+	#[tokio::test]
+	async fn test032() {
+		sp_tracing::try_init_simple();
+		let listener = MultiViewListener::new();
+
+		let block_hash0 = H256::repeat_byte(0x01);
+		let events0_tx0 = vec![TransactionStatus::Future];
+		let events0_tx1 = vec![TransactionStatus::Ready];
+
+		let block_hash1 = H256::repeat_byte(0x02);
+		let events1_tx0 =
+			vec![TransactionStatus::Ready, TransactionStatus::InBlock((block_hash1, 0))];
+		let events1_tx1 = vec![
+			TransactionStatus::Ready,
+			TransactionStatus::InBlock((block_hash1, 1)),
+			TransactionStatus::Finalized((block_hash1, 1)),
+		];
+
+		let tx0_hash = H256::repeat_byte(0x0a);
+		let tx1_hash = H256::repeat_byte(0x0b);
+		let external_watcher_tx0 = listener.create_external_watcher_for_tx(tx0_hash).await.unwrap();
+		let external_watcher_tx1 = listener.create_external_watcher_for_tx(tx1_hash).await.unwrap();
+
+		let view0_tx0_stream = futures::stream::iter(events0_tx0.clone());
+		let view0_tx1_stream = futures::stream::iter(events0_tx1.clone());
+
+		let view1_tx0_stream = futures::stream::iter(events1_tx0.clone());
+		let view1_tx1_stream = futures::stream::iter(events1_tx1.clone());
+
+		listener
+			.add_view_watcher_for_tx(tx0_hash, block_hash0, view0_tx0_stream.boxed())
+			.await;
+		listener
+			.add_view_watcher_for_tx(tx0_hash, block_hash1, view1_tx0_stream.boxed())
+			.await;
+		listener
+			.add_view_watcher_for_tx(tx1_hash, block_hash0, view0_tx1_stream.boxed())
+			.await;
+		listener
+			.add_view_watcher_for_tx(tx1_hash, block_hash1, view1_tx1_stream.boxed())
+			.await;
+
+		listener.invalidate_transactions(vec![tx0_hash]).await;
+		listener.invalidate_transactions(vec![tx1_hash]).await;
+
+		let out_tx0 = external_watcher_tx0.collect::<Vec<_>>().await;
+		let out_tx1 = external_watcher_tx1.collect::<Vec<_>>().await;
+		log::info!("out_tx0: {:#?}", out_tx0);
+		log::info!("out_tx1: {:#?}", out_tx1);
+		assert!(out_tx0.iter().all(|v| vec![
+			TransactionStatus::Future,
+			TransactionStatus::Ready,
+			TransactionStatus::InBlock((block_hash1, 0)),
+			TransactionStatus::Invalid
+		]
+		.contains(v)));
+
+		assert!(out_tx1.iter().all(|v| vec![
+			TransactionStatus::Ready,
+			TransactionStatus::InBlock((block_hash1, 1)),
+			TransactionStatus::Finalized((block_hash1, 1))
+		]
+		.contains(v)));
+		assert_eq!(out_tx0.len(), 4);
+		assert_eq!(out_tx1.len(), 3);
+	}
+
+	#[tokio::test]
+	async fn test04() {
+		sp_tracing::try_init_simple();
+		let listener = MultiViewListener::new();
+
+		let block_hash0 = H256::repeat_byte(0x01);
+		let events0 = vec![
+			TransactionStatus::Future,
+			TransactionStatus::Ready,
+			TransactionStatus::InBlock((block_hash0, 0)),
+		];
+
+		let block_hash1 = H256::repeat_byte(0x02);
+		let events1 = vec![TransactionStatus::Future];
+
+		let tx_hash = H256::repeat_byte(0x0a);
+		let external_watcher = listener.create_external_watcher_for_tx(tx_hash).await.unwrap();
+
+		let view_stream0 = futures::stream::iter(events0.clone()).chain(stream::pending().boxed());
+		let view_stream1 = futures::stream::iter(events1.clone()).chain(stream::pending().boxed());
+
+		listener
+			.add_view_watcher_for_tx(tx_hash, block_hash0, view_stream0.boxed())
+			.await;
+		listener
+			.add_view_watcher_for_tx(tx_hash, block_hash1, view_stream1.boxed())
+			.await;
+
+		listener.invalidate_transactions(vec![tx_hash]).await;
+
+		// stream is pending, we need to fetch 3 events
+		let out = external_watcher.take(3).collect::<Vec<_>>().await;
+		log::info!("out: {:#?}", out);
+
+		// invalid shall not be sent
+		assert!(out.iter().all(|v| vec![
+			TransactionStatus::Future,
+			TransactionStatus::Ready,
+			TransactionStatus::InBlock((block_hash0, 0)),
+		]
+		.contains(v)));
+		assert_eq!(out.len(), 3);
+	}
+
+	#[tokio::test]
+	async fn test05() {
+		sp_tracing::try_init_simple();
+		let listener = MultiViewListener::new();
+
+		let block_hash0 = H256::repeat_byte(0x01);
+		let events0 = vec![TransactionStatus::Invalid];
+
+		let tx_hash = H256::repeat_byte(0x0a);
+		let external_watcher = listener.create_external_watcher_for_tx(tx_hash).await.unwrap();
+
+		let view_stream0 = futures::stream::iter(events0.clone()).chain(stream::pending().boxed());
+
+		// Note: this generates actual Invalid event.
+		// Invalid event from View's stream is intentionally ignored.
+		listener.invalidate_transactions(vec![tx_hash]).await;
+
+		listener
+			.add_view_watcher_for_tx(tx_hash, block_hash0, view_stream0.boxed())
+			.await;
+
+		let out = external_watcher.collect::<Vec<_>>().await;
+		log::info!("out: {:#?}", out);
+
+		assert!(out.iter().all(|v| vec![TransactionStatus::Invalid].contains(v)));
+		assert_eq!(out.len(), 1);
 	}
 }
