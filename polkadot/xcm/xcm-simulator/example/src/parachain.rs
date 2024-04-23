82387a89a847948940dc585a5d7e894586d6072b
Branislav Kontur <bkontur@gmail.com>
2024-04-23 13:40:05 +0200

    Ensure outbound XCMs are decodable with limits + add `EnsureDecodableXcm` router (for testing purposes) (#4186)
    This PR:
    - adds `EnsureDecodableXcm` (testing) router that attempts to *encode*
    and *decode* passed XCM `message` to ensure that the receiving side will
    be able to decode, at least with the same XCM version.
    - fixes `pallet_xcm` / `pallet_xcm_benchmarks` assets data generation
    
    Relates to investigation of
    https://substrate.stackexchange.com/questions/11288 and missing fix
    https://github.com/paritytech/polkadot-sdk/pull/2129 which did not get
    into the fellows 1.1.X release.
    
    ## Questions/TODOs
    
    - [x] fix XCM benchmarks, which produces undecodable data - new router
    catched at least two cases
      - `BoundedVec exceeds its limit`
      - `Fungible asset of zero amount is not allowed`  
    - [x] do we need to add `sort` to the `prepend_with` as we did for
    reanchor [here](https://github.com/paritytech/polkadot-sdk/pull/2129)?
    @serban300 (**created separate/follow-up PR**:
    https://github.com/paritytech/polkadot-sdk/pull/4235)
    - [x] We added decoding check to `XcmpQueue` -> `validate_xcm_nesting`,
    why not to added to the `ParentAsUmp` or `ChildParachainRouter`?
    @franciscoaguirre (**created separate/follow-up PR**:
    https://github.com/paritytech/polkadot-sdk/pull/4236)
    - [ ] `SendController::send_blob` replace `VersionedXcm::<()>::decode(`
    with `VersionedXcm::<()>::decode_with_depth_limit(MAX_XCM_DECODE_DEPTH,
    data)` ?
    
    ---------
    
    Co-authored-by: Adrian Catangiu <adrian@parity.io>

--- polkadot/xcm/xcm-simulator/example/src/parachain.rs
+++ polkadot/xcm/xcm-simulator/example/src/parachain.rs
@@ -40,10 +40,10 @@ use polkadot_parachain_primitives::primitives::{
 use xcm::{latest::prelude::*, VersionedXcm};
 use xcm_builder::{
 	Account32Hash, AccountId32Aliases, AllowUnpaidExecutionFrom, ConvertedConcreteId,
-	EnsureXcmOrigin, FixedRateOfFungible, FixedWeightBounds, FrameTransactionalProcessor,
-	FungibleAdapter, IsConcrete, NativeAsset, NoChecking, NonFungiblesAdapter, ParentIsPreset,
-	SiblingParachainConvertsVia, SignedAccountId32AsNative, SignedToAccountId32,
-	SovereignSignedViaLocation,
+	EnsureDecodableXcm, EnsureXcmOrigin, FixedRateOfFungible, FixedWeightBounds,
+	FrameTransactionalProcessor, FungibleAdapter, IsConcrete, NativeAsset, NoChecking,
+	NonFungiblesAdapter, ParentIsPreset, SiblingParachainConvertsVia, SignedAccountId32AsNative,
+	SignedToAccountId32, SovereignSignedViaLocation,
 };
 use xcm_executor::{
 	traits::{ConvertLocation, JustTry},
@@ -212,7 +212,7 @@ pub type LocalAssetTransactor = (
 	>,
 );
 
-pub type XcmRouter = super::ParachainXcmRouter<MsgQueue>;
+pub type XcmRouter = EnsureDecodableXcm<super::ParachainXcmRouter<MsgQueue>>;
 pub type Barrier = AllowUnpaidExecutionFrom<Everything>;
 
 parameter_types! {
