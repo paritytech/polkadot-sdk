42379402c52f445c92fa787ea6b6cc398eced74f
Branislav Kontur <bkontur@gmail.com>
2024-04-24 16:23:23 +0200

    `AllowHrmpNotificationsFromRelayChain` barrier for HRMP notifications from the relaychain (#4156)
    This PR:
    - introduces `AllowHrmpNotificationsFromRelayChain` barrier for allowing
    HRMP notifications just from the relay chain (to fulfill safety
    assumptions -
    [see](https://github.com/paritytech/polkadot-sdk/blob/master/polkadot/xcm/src/v4/mod.rs#L532))
    - sets it up for all testnet SP parachains
    
    Continuation of: https://github.com/paritytech/polkadot-sdk/pull/3696

--- polkadot/xcm/xcm-builder/src/lib.rs
+++ polkadot/xcm/xcm-builder/src/lib.rs
@@ -35,10 +35,11 @@ pub use asset_conversion::{
 
 mod barriers;
 pub use barriers::{
-	AllowExplicitUnpaidExecutionFrom, AllowKnownQueryResponses, AllowSubscriptionsFrom,
-	AllowTopLevelPaidExecutionFrom, AllowUnpaidExecutionFrom, DenyReserveTransferToRelayChain,
-	DenyThenTry, IsChildSystemParachain, IsParentsOnly, IsSiblingSystemParachain,
-	RespectSuspension, TakeWeightCredit, TrailingSetTopicAsId, WithComputedOrigin,
+	AllowExplicitUnpaidExecutionFrom, AllowHrmpNotificationsFromRelayChain,
+	AllowKnownQueryResponses, AllowSubscriptionsFrom, AllowTopLevelPaidExecutionFrom,
+	AllowUnpaidExecutionFrom, DenyReserveTransferToRelayChain, DenyThenTry, IsChildSystemParachain,
+	IsParentsOnly, IsSiblingSystemParachain, RespectSuspension, TakeWeightCredit,
+	TrailingSetTopicAsId, WithComputedOrigin,
 };
 
 mod controller;
