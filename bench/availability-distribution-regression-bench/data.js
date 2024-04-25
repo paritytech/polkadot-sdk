80c34858ac774068067146d276f251d97e64344e
github-action-benchmark <github@users.noreply.github.com>
2024-04-25 08:53:26 +0000

    add availability-distribution-regression-bench (customSmallerIsBetter) benchmark result for 7e68b2b8da9caf634ff4f6c6d96d2d7914c44fb7

--- bench/availability-distribution-regression-bench/data.js
+++ bench/availability-distribution-regression-bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1714028656208,
+  "lastUpdate": 1714035205753,
   "repoUrl": "https://github.com/paritytech/polkadot-sdk",
   "entries": {
     "availability-distribution-regression-bench": [
@@ -6551,6 +6551,58 @@ window.BENCHMARK_DATA = {
           {
             "name": "availability-distribution",
             "value": 0.013946326326666671,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Svyatoslav Nikolsky",
+            "username": "svyatonik",
+            "email": "svyatonik@gmail.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "7e68b2b8da9caf634ff4f6c6d96d2d7914c44fb7",
+          "message": "Bridge: added free headers submission support to the substrate-relay (#4157)\n\nOriginal PR:\nhttps://github.com/paritytech/parity-bridges-common/pull/2884. Since\nchain-specific code lives in the `parity-bridges-common` repo, some\nparts of original PR will require another PR\n\n---------\n\nCo-authored-by: Adrian Catangiu <adrian@parity.io>",
+          "timestamp": "2024-04-25T07:20:17Z",
+          "url": "https://github.com/paritytech/polkadot-sdk/commit/7e68b2b8da9caf634ff4f6c6d96d2d7914c44fb7"
+        },
+        "date": 1714035180952,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Received from peers",
+            "value": 433.3333333333332,
+            "unit": "KiB"
+          },
+          {
+            "name": "Sent to peers",
+            "value": 18479.893333333326,
+            "unit": "KiB"
+          },
+          {
+            "name": "bitfield-distribution",
+            "value": 0.022032969920000006,
+            "unit": "seconds"
+          },
+          {
+            "name": "availability-distribution",
+            "value": 0.011833041986666671,
+            "unit": "seconds"
+          },
+          {
+            "name": "availability-store",
+            "value": 0.15668121515999991,
+            "unit": "seconds"
+          },
+          {
+            "name": "test-environment",
+            "value": 0.009417726440000003,
             "unit": "seconds"
           }
         ]
