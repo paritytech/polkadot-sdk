810f12d519c84d27110a663bc71fabf34689265b
Bader Youssef <ibnbassem@gmail.com>
2024-04-25 23:22:31 -0400

    add async backing docs, and fix parachain module

--- docs/sdk/src/guides/parachains/mod.rs
+++ docs/sdk/src/guides/parachains/mod.rs
@@ -3,5 +3,4 @@
 //! This crate contains a collection of guides for developers and maintainers of parachains built
 //! with Polkadot SDK.
 
-/// Upgrade Parachain for Asynchronous Backing Compatibility
 pub mod async_backing_guide;
