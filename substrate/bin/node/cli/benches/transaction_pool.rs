b3787db0b98953905c1665be7269f0b8d27dec19
Niklas Adolfsson <niklasadolfsson1@gmail.com>
2024-04-26 18:15:06 +0200

    fix build

--- substrate/bin/node/cli/benches/transaction_pool.rs
+++ substrate/bin/node/cli/benches/transaction_pool.rs
@@ -83,6 +83,7 @@ fn new_node(tokio_handle: Handle) -> node_cli::service::NewFullBase {
 		rpc_batch_config: RpcBatchRequestConfig::Unlimited,
 		rpc_rate_limit: None,
 		rpc_rate_limit_whitelisted_ips: Default::default(),
+		rpc_rate_limit_trust_proxy_headers: Default::default(),
 		prometheus_config: None,
 		telemetry_endpoints: None,
 		default_heap_pages: None,
