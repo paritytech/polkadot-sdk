12e6f44072ca424116977d9247125fab3d215e72
dastansam <dastan.samatov.33@gmail.com>
2024-04-29 02:21:08 +0200

    Introduce mutation policy, make guard more generic

--- substrate/frame/support/src/storage/state_diff_guard.rs
+++ substrate/frame/support/src/storage/state_diff_guard.rs
@@ -37,15 +37,21 @@
 //! Use the `StateDiffGuard` in a migration:
 //!
 //! ```rust
-//! 
 //! use frame_support::storage::generator::{StorageMap, StorageValue};
-//! use frame_support::storage::StateDiffGuard;
+//! use frame_support::storage::state_diff_guard::StateDiffGuard;
 //! use frame_support::traits::Get;
+//! use frame_support::pallet_prelude::*;
 //! use sp_io::TestExternalities;
 //!
+//! pub use pallet::*;
+//!
 //! #[frame_support::pallet]
-//! mod pallet {
+//! pub mod pallet {
 //! 	use frame_support::pallet_prelude::*;
+//! 	use frame_system::pallet_prelude::*;
+//!
+//! 	#[pallet::config]
+//! 	pub trait Config: frame_system::Config {}
 //!
 //! 	#[pallet::pallet]
 //! 	pub struct Pallet<T>(_);
@@ -69,108 +75,105 @@
 //! 	pub type SomeDoubleMap<T> = StorageMap<_, Twox64Concat, u32, u32>;
 //! }
 //!
-//! mod migrations {
-//! 	use super::*;
+//! pub struct UncheckedMigrateToV1<T: Config>(sp_std::marker::PhantomData<T>);
 //!
-//!     pub struct UncheckedMigrateToV1<T: crate::Config>(sp_std::marker::PhantomData<T>);
-//!
-//!     impl<T: crate::Config> frame_support::traits::UncheckedOnRuntimeUpgrade for UncheckedMigrateToV1<T> {
-//!         fn on_runtime_upgrade() -> frame_support::weights::Weight {
-//! 		   // migration logic here
-//!            Weight::default()
-//!         }
-//!         
-//!         #[cfg(feature = "try-runtime")]
-//!         fn try_on_runtime_upgrade() -> Result<frame_support::weights::Weight, &'static str> {
-//! 		   // migration logic here
-//!           let guard = StateDiffGuard::builder()
-//! 			.must_change(StoragePrefix {
-//! 				pallet_name: SomeMap::<T>::pallet_prefix(),
-//! 				storage_name: SomeMap::<T>::storage_prefix(),
-//! 			})
-//! 			.must_change(StoragePrefix {
-//! 				pallet_name: SomeDoubleMap::<T>::pallet_prefix(),
-//! 				storage_name: SomeDoubleMap::<T>::storage_prefix(),
-//! 			})
-//! 			.build();
-//!
-//!           // try runtime upgrade logic here
-//! 		  let weight = Self::on_runtime_upgrade();
-//!           
-//!           // any other logic
-//! 		  Ok(weight)
-//! 	   }
+//! impl<T: Config> frame_support::traits::UncheckedOnRuntimeUpgrade for UncheckedMigrateToV1<T> {
+//!     fn on_runtime_upgrade() -> frame_support::weights::Weight {
+//!         // migration logic here
+//!         Weight::default()
 //!     }
 //!
-//!     pub type MigrateToV1<T> = frame_support::migrations::VersionedMigration<
-//!     	1,
-//!      	2,
-//!      	UncheckedMigrateToV1<T>,
-//!      	Pallet<T>,
-//! 		<T as frame_system::Config>::DbWeight
-//!     >;
+//!     #[cfg(feature = "try-runtime")]
+//!     fn try_on_runtime_upgrade() -> Result<frame_support::weights::Weight, &'static str> {
+//!         // migration logic here
+//!         let guard = StateDiffGuard::builder()
+//!             .must_change(GuardSubject::Prefix(SomeMap::<T>::storage_info().first().unwrap().clone()))
+//!             .must_not_change(GuardSubject::Prefix(SomeDoubleMap::<T>::storage_info().first().unwrap().clone()))
+//! 			.can_not_change(GuardSubject::AnythingElse)
+//!             .build();
+//!
+//!         // try runtime upgrade logic here
+//!         let weight = Self::on_runtime_upgrade();
+//!
+//!         // any other logic
+//!         Ok(weight)
+//!     }
 //! }
+//!
+//! pub type MigrateToV1<T> = frame_support::migrations::VersionedMigration<
+//!     1,
+//!     2,
+//!     UncheckedMigrateToV1<T>,
+//!     Pallet<T>,
+//!     <T as frame_system::Config>::DbWeight
+//! >;
 //! ```
 //!
-//! In the example above, the guard will panic if any storage entry that doesn't match `SomeMap` or `SomeDoubleMap`
-//! prefixes is changed.
+//! In the example above, the guard will panic if any storage entry that doesn't match `SomeMap` or
+//! `SomeDoubleMap` prefixes is changed.
 
-use core::fmt::{Debug, Formatter};
-use sp_std::{
-	collections::{btree_map::BTreeMap, btree_set::BTreeSet},
-	vec::Vec,
-};
+use core::fmt::Debug;
+use sp_std::{collections::btree_map::BTreeMap, vec::Vec};
 
-use super::storage_prefix;
+use crate::traits::StorageInfo;
 
 /// Raw storage key.
 type StorageKey = Vec<u8>;
 /// Raw storage value.
 type StorageValue = Vec<u8>;
+/// Raw storage state.
+/// BTreeMap is used to ensure that the keys are sorted.
+type State = BTreeMap<StorageKey, StorageValue>;
 
-/// Storage prefix: pallet name and storage name.
-#[derive(Clone, Copy, Ord, PartialOrd, Eq, PartialEq)]
-pub struct StoragePrefix {
-	/// Name of the pallet.
-	pub pallet_name: &'static [u8],
-	/// Name of the storage.
-	pub storage_name: &'static [u8],
+/// Mutation policy for a storage prefix.
+#[derive(Debug)]
+pub enum MutationPolicy {
+	/// The storage prefix is expected to change.
+	CanChange,
+	/// The storage prefix is expected to not change.
+	CanNotChange,
+	/// The storage prefix must change.
+	MustChange,
+	/// The storage prefix must not change.
+	MustNotChange,
 }
 
-impl StoragePrefix {
-	/// Computes the storage prefix.
-	pub fn storage_prefix(&self) -> [u8; 32] {
-		storage_prefix(self.pallet_name, self.storage_name)
-	}
+/// Guard subject.
+#[derive(Debug, Ord, PartialOrd, Eq, PartialEq, Clone)]
+pub enum GuardSubject {
+	/// Explicit storage prefix to guard.
+	Prefix(StorageInfo),
+	/// Any storage prefix.
+	AnythingElse,
 }
 
-impl Debug for StoragePrefix {
-	fn fmt(&self, f: &mut Formatter<'_>) -> core::fmt::Result {
-		write!(
-			f,
-			"{}::{}",
-			sp_std::str::from_utf8(self.pallet_name).unwrap_or(""),
-			sp_std::str::from_utf8(self.storage_name).unwrap_or("")
-		)
+impl GuardSubject {
+	/// Get the raw prefix.
+	fn raw_prefix(&self) -> Option<&Vec<u8>> {
+		match self {
+			GuardSubject::Prefix(info) => Some(&info.prefix),
+			GuardSubject::AnythingElse => None,
+		}
 	}
 }
 
 /// A guard that asserts that a specific storage prefix has been mutated or not.
 #[derive(Default, Debug)]
 pub struct StateDiffGuard {
-	// Storage prefixes that are expected to change.
-	whitelisted_prefixes: BTreeSet<StoragePrefix>,
-	// Snapshot of the storage state when the guard is created.
-	initial_state: BTreeMap<StorageKey, StorageValue>,
+	/// Storage prefixes that are expected to change.
+	/// Default for `AnythingElse` is `Must(NotChange)`.
+	mutation_policy: BTreeMap<GuardSubject, MutationPolicy>,
+	/// Snapshot of the storage state when the guard is created.
+	initial_state: State,
 }
 
 impl StateDiffGuard {
 	pub fn builder() -> StateDiffGuardBuilder {
-		StateDiffGuardBuilder { whitelisted_prefixes: BTreeSet::new() }
+		StateDiffGuardBuilder { mutation_policy: BTreeMap::new() }
 	}
 
 	/// Take a snapshot of the current storage state.
-	fn read_state(&self) -> BTreeMap<StorageKey, StorageValue> {
+	fn read_state(&self) -> State {
 		let mut state = BTreeMap::new();
 
 		let mut previous_key = Vec::new();
@@ -184,51 +187,109 @@ impl StateDiffGuard {
 		state
 	}
 
-	/// Reads state difference.
-	///
-	/// This includes:
-	/// - all entries where the value has changed
-	/// - new entries
-	/// - removed entries
-	fn read_difference(&self) -> BTreeMap<StorageKey, StorageValue> {
-		let mut diff = BTreeMap::new();
+	/// Given the storage key, get it's prefix mutation policy.
+	fn prefix_mutation_policy(&self, key: &StorageKey) -> Option<&MutationPolicy> {
+		let guard_anything_else = self.mutation_policy.get(&GuardSubject::AnythingElse);
+		self.mutation_policy
+			.iter()
+			.find(|(info, _)| info.raw_prefix().map_or(false, |prefix| key.starts_with(prefix)))
+			.map_or(guard_anything_else, |(_, policy)| Some(policy))
+	}
 
-		// start with an empty key
+	/// Apply the mutation policy to the current storage state.
+	///
+	/// Returns `true` if all the mutation policies are satisfied, `false` otherwise.
+	///
+	/// The first iteration compares initial and current value of the storage keys by taking
+	/// the initial value from the initial state. This way, once the iteration is done, the
+	/// initial state will only contain the keys that were removed.
+	///
+	/// Then for each removed key, it applies the mutation policy again.
+	fn apply_mutation_policy(&mut self) -> bool {
 		let mut previous_key = Vec::new();
-		let mut initial_state = self.initial_state.clone();
+
+		// check has passed
+		let mut check_passed = true;
 
 		while let Some(next) = sp_io::storage::next_key(&previous_key) {
 			if let Some(value) = sp_io::storage::get(&next) {
-				if let Some(old_value) = initial_state.remove(&next) {
-					if value != old_value {
-						diff.insert(next.clone(), value.to_vec());
+				let (maybe_old_value, value) = (self.initial_state.remove(&next), value);
+
+				if let Some(policy) = self.prefix_mutation_policy(&next) {
+					match policy {
+						MutationPolicy::CanChange => {
+							// expected to change, no need to check anything
+						},
+						MutationPolicy::CanNotChange | MutationPolicy::MustNotChange => {
+							check_passed = maybe_old_value == Some(value.to_vec());
+						},
+						MutationPolicy::MustChange =>
+							if let Some(old_value) = maybe_old_value {
+								if old_value == value {
+									check_passed = false;
+									log::info!(
+										"Storage value for key must have been changed, but it is not {:?} -> {:?}",
+										next,
+										value
+									);
+								}
+							},
 					}
-				} else {
-					diff.insert(next.clone(), value.to_vec());
 				}
 			}
 
 			previous_key = next;
 		}
 
-		// Add all remaining initial state to the diff
-		for (key, value) in initial_state.iter() {
-			diff.insert(key.clone(), value.to_vec());
+		// if there are any keys left in initial state, it means that they were removed
+		for (key, _) in self.initial_state.iter() {
+			if let Some(policy) = self.prefix_mutation_policy(key) {
+				match policy {
+					MutationPolicy::CanChange | MutationPolicy::MustChange => {
+						// expected to change, no need to check anything
+					},
+					MutationPolicy::CanNotChange | MutationPolicy::MustNotChange => {
+						check_passed = false;
+						log::info!("Storage key must not have been removed, but it is {:?}", key);
+					},
+				}
+			}
 		}
 
-		diff
+		check_passed
 	}
 }
 
 /// Builder for the `StateDiffGuard`.
 pub struct StateDiffGuardBuilder {
-	whitelisted_prefixes: BTreeSet<StoragePrefix>,
+	mutation_policy: BTreeMap<GuardSubject, MutationPolicy>,
 }
 
 impl StateDiffGuardBuilder {
-	/// Add a storage prefix that is expected to change.
-	pub fn must_change(mut self, prefix: StoragePrefix) -> Self {
-		self.whitelisted_prefixes.insert(prefix);
+	/// Add a storage prefix that must change.
+	pub fn must_change(mut self, prefix: GuardSubject) -> Self {
+		self.mutation_policy.insert(prefix, MutationPolicy::MustChange);
+
+		self
+	}
+
+	/// Add a storage prefix that must not change.
+	pub fn must_not_change(mut self, prefix: GuardSubject) -> Self {
+		self.mutation_policy.insert(prefix, MutationPolicy::MustNotChange);
+
+		self
+	}
+
+	/// Add a storage prefix that can change.
+	pub fn can_change(mut self, prefix: GuardSubject) -> Self {
+		self.mutation_policy.insert(prefix, MutationPolicy::CanChange);
+
+		self
+	}
+
+	/// Add a storage prefix that must not change.
+	pub fn can_not_change(mut self, prefix: GuardSubject) -> Self {
+		self.mutation_policy.insert(prefix, MutationPolicy::CanNotChange);
 
 		self
 	}
@@ -236,13 +297,10 @@ impl StateDiffGuardBuilder {
 	/// Build the guard
 	pub fn build(self) -> StateDiffGuard {
 		let mut state_diff_guard = StateDiffGuard {
-			whitelisted_prefixes: BTreeSet::new(),
+			mutation_policy: self.mutation_policy,
 			initial_state: BTreeMap::new(),
 		};
-		state_diff_guard.whitelisted_prefixes = self.whitelisted_prefixes;
 		state_diff_guard.initial_state = state_diff_guard.read_state();
-
-		sp_io::storage::start_transaction();
 
 		state_diff_guard
 	}
@@ -250,30 +308,7 @@ impl StateDiffGuardBuilder {
 
 impl Drop for StateDiffGuard {
 	fn drop(&mut self) {
-		// this ensures that we read only difference from `initial_state`
-		let diff = self.read_difference();
-		let mut check_passed = true;
-		for (key, value) in diff.iter() {
-			let prefix_whitelisted = match key.get(0..32) {
-				Some(key_prefix) => self
-					.whitelisted_prefixes
-					.iter()
-					.any(|prefix| prefix.storage_prefix() == key_prefix),
-				None => false,
-			};
-
-			if !prefix_whitelisted {
-				check_passed = false;
-				if let Some(old_value) = self.initial_state.get(key) {
-					log::info!("+ {:?}:{:?}", key, value);
-					log::info!("- {:?}:{:?}", key, old_value);
-				} else if sp_io::storage::exists(&key) {
-					log::info!("++ {:?}:{:?}", key, value);
-				} else {
-					log::info!("-- {:?}:{:?}", key, value);
-				}
-			}
-		}
+		let check_passed = self.apply_mutation_policy();
 
 		// No need to double panic, eg. inside a test assertion failure.
 		if sp_std::thread::panicking() {
@@ -289,41 +324,130 @@ mod tests {
 	use crate::{Blake2_128Concat, Twox128};
 
 	use super::*;
+	#[allow(unused_imports)]
 	use crate::storage::generator::{StorageDoubleMap, StorageMap, StorageValue};
+	use frame_support::traits::StorageInfoTrait;
 	use frame_support_procedural::storage_alias;
 	use sp_io::TestExternalities;
 
+	#[storage_alias]
+	type TestMap = StorageMap<TestModule, Twox128, u32, u32>;
+	#[storage_alias]
+	type TestDoubleMapBlake2 =
+		StorageDoubleMap<TestModule, Blake2_128Concat, u32, Blake2_128Concat, u32, u32>;
+	#[storage_alias]
+	type TestStorageValue = StorageValue<TestModule, u32>;
+
 	#[test]
-	#[should_panic(expected = "`StateDiffGuard` detected an unexpected storage change")]
-	fn test_diff_guard_panic_works() {
-		let mut ext = TestExternalities::default();
-		ext.execute_with(|| {
-			#[storage_alias]
-			type TestMap = StorageMap<TestModule, Twox128, u32, u32>;
-			#[storage_alias]
-			type TestDoubleMapBlake2 =
-				StorageDoubleMap<TestModule, Blake2_128Concat, u32, Blake2_128Concat, u32, u32>;
-			#[storage_alias]
-			type TestStorageValue = StorageValue<TestModule, u32>;
+	fn diff_guard_default_works() {
+		TestExternalities::default().execute_with(|| {
+			let guard = StateDiffGuard::builder().build();
 
 			TestMap::insert(1, 1);
+
+			// this will not panic because guard is not guarding any storage keys
+			sp_std::mem::drop(guard);
+		});
+	}
+
+	#[test]
+	#[should_panic(expected = "`StateDiffGuard` detected an unexpected storage change")]
+	fn diff_guard_anything_else_works() {
+		TestExternalities::default().execute_with(|| {
+			TestMap::insert(1, 1);
+			TestDoubleMapBlake2::insert(1, 1, 1);
+			TestStorageValue::put(1);
+
+			let guard =
+				StateDiffGuard::builder().can_not_change(GuardSubject::AnythingElse).build();
+
+			TestMap::insert(1, 2);
+			TestDoubleMapBlake2::insert(1, 1, 2);
+			TestStorageValue::put(2);
+
+			sp_std::mem::drop(guard);
+		});
+	}
+
+	#[test]
+	#[should_panic(expected = "`StateDiffGuard` detected an unexpected storage change")]
+	fn guard_storage_key_types_works() {
+		TestExternalities::default().execute_with(|| {
+			let guard = StateDiffGuard::builder()
+				.must_not_change(GuardSubject::Prefix(
+					TestDoubleMapBlake2::storage_info().first().unwrap().clone(),
+				))
+				.build();
+
+			mod v2 {
+				use super::*;
+
+				#[storage_alias]
+				pub type TestMap = StorageMap<TestModule, Twox128, u128, u32>;
+				#[storage_alias]
+				pub type TestDoubleMapBlake2 = StorageDoubleMap<
+					TestModule,
+					Blake2_128Concat,
+					u128,
+					Blake2_128Concat,
+					u64,
+					u32,
+				>;
+			}
+
+			v2::TestMap::insert(1, 1);
+			v2::TestDoubleMapBlake2::insert(1, 1, 12);
+
+			// this will panic because the whitelisted storage key is expected to change
+			sp_std::mem::drop(guard);
+		});
+	}
+
+	#[test]
+	#[should_panic(expected = "`StateDiffGuard` detected an unexpected storage change")]
+	fn diff_guard_basic_panic_works() {
+		TestExternalities::default().execute_with(|| {
 			TestDoubleMapBlake2::insert(1, 1, 1);
 			TestDoubleMapBlake2::insert(1, 2, 1);
 			TestStorageValue::put(1);
 
 			let guard = StateDiffGuard::builder()
-				.must_change(StoragePrefix {
-					pallet_name: TestDoubleMapBlake2::pallet_prefix(),
-					storage_name: TestDoubleMapBlake2::storage_prefix(),
-				})
+				.can_change(GuardSubject::Prefix(
+					TestDoubleMapBlake2::storage_info().first().unwrap().clone(),
+				))
+				.must_not_change(GuardSubject::Prefix(
+					TestMap::storage_info().first().unwrap().clone(),
+				))
 				.build();
 
 			TestDoubleMapBlake2::remove(1, 1);
-			TestStorageValue::put(2);
-			frame_support::storage::unhashed::put(b":CODE", b"value");
-			TestMap::insert(2, 2);
+			TestMap::insert(1, 1);
 
-			// this will panic because the storage keys are expected to change
+			// this will panic because by default any other whitelisted prefix is not expected to
+			// change
+			sp_std::mem::drop(guard);
+		});
+	}
+
+	#[test]
+	#[should_panic(expected = "`StateDiffGuard` detected an unexpected storage change")]
+	fn diff_guard_basic_must_change() {
+		TestExternalities::default().execute_with(|| {
+			TestDoubleMapBlake2::insert(1, 1, 1);
+			TestDoubleMapBlake2::insert(1, 1, 2);
+			TestStorageValue::put(1);
+
+			// must change all entries of `TestDoubleMapBlake2`
+			let guard = StateDiffGuard::builder()
+				.must_change(GuardSubject::Prefix(
+					TestDoubleMapBlake2::storage_info().first().unwrap().clone(),
+				))
+				.can_change(GuardSubject::Prefix(TestMap::storage_info().first().unwrap().clone()))
+				.build();
+
+			TestDoubleMapBlake2::remove(1, 2);
+			TestMap::insert(1, 1);
+			// this will panic because the whitelisted prefix is expected to change
 			sp_std::mem::drop(guard);
 		});
 	}
@@ -332,50 +456,17 @@ mod tests {
 	fn test_diff_guard_no_panic_works() {
 		let mut ext = TestExternalities::default();
 		ext.execute_with(|| {
-			#[storage_alias]
-			type TestMap = StorageMap<TestModule, Twox128, u32, u32>;
-			#[storage_alias]
-			type TestDoubleMapBlake2 =
-				StorageDoubleMap<TestModule, Blake2_128Concat, u32, Blake2_128Concat, u32, u32>;
-			#[storage_alias]
-			type TestStorageValue = StorageValue<TestModule, u32>;
-
 			TestMap::insert(1, 1);
 			TestDoubleMapBlake2::insert(1, 1, 1);
 			TestDoubleMapBlake2::insert(1, 2, 1);
 			TestStorageValue::put(1);
 
-			let guard = StateDiffGuard::builder()
-				.must_change(StoragePrefix {
-					pallet_name: TestDoubleMapBlake2::pallet_prefix(),
-					storage_name: TestDoubleMapBlake2::storage_prefix(),
-				})
-				.build();
+			let guard = StateDiffGuard::builder().must_change(GuardSubject::Prefix(
+				TestDoubleMapBlake2::storage_info().first().unwrap().clone(),
+			));
 
 			TestDoubleMapBlake2::remove(1, 1);
-
-			// this will not panic because the non-whitelisted storage keys are not expected to
-			// change
 			sp_std::mem::drop(guard);
-
-			let _guard = StateDiffGuard::builder()
-				.must_change(StoragePrefix {
-					pallet_name: TestDoubleMapBlake2::pallet_prefix(),
-					storage_name: TestDoubleMapBlake2::storage_prefix(),
-				})
-				.must_change(StoragePrefix {
-					pallet_name: TestMap::pallet_prefix(),
-					storage_name: TestMap::storage_prefix(),
-				})
-				.must_change(StoragePrefix {
-					pallet_name: TestStorageValue::pallet_prefix(),
-					storage_name: TestStorageValue::storage_prefix(),
-				})
-				.build();
-
-			TestDoubleMapBlake2::remove(1, 2);
-			TestStorageValue::put(2);
-			TestMap::mutate(1, |v| *v = Some(2));
 		});
 	}
 }
