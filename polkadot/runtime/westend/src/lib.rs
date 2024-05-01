c3f3def81c819b82ef9f27a4729ef41703836741
Branislav Kontur <bkontur@gmail.com>
2024-05-01 22:01:55 +0200

    HRMP - set `DefaultChannelSizeAndCapacityWithSystem` with dynamic values according to the `ActiveConfig` (#4332)
    ## Summary
    This PR enhances the capability to set
    `DefaultChannelSizeAndCapacityWithSystem` for HRMP. Currently, all
    testnets (Rococo, Westend) have a hard-coded value set as 'half of the
    maximum' determined by the live `ActiveConfig`. While this approach
    appears satisfactory, potential issues could arise if the live
    `ActiveConfig` are adjusted below these hard-coded values, necessitating
    a new runtime release with updated values. Additionally, hard-coded
    values have consequences, such as Rococo's benchmarks not functioning:
    https://gitlab.parity.io/parity/mirrors/polkadot-sdk/-/jobs/6082656.
    
    ## Solution
    The proposed solution here is to utilize
    `ActiveConfigHrmpChannelSizeAndCapacityRatio`, which reads the current
    `ActiveConfig` and calculates `DefaultChannelSizeAndCapacityWithSystem`,
    for example, "half of the maximum" based on live data. This way,
    whenever `ActiveConfig` is modified,
    `ActiveConfigHrmpChannelSizeAndCapacityRatio` automatically returns
    adjusted values with the appropriate ratio. Thus, manual adjustments and
    new runtime releases become unnecessary.
    
    
    Relates to a comment/discussion:
    https://github.com/paritytech/polkadot-sdk/pull/3721/files#r1541001420
    Relates to a comment/discussion:
    https://github.com/paritytech/polkadot-sdk/pull/3721/files#r1549291588
    
    ---------
    
    Co-authored-by: command-bot <>

--- polkadot/runtime/westend/src/lib.rs
+++ polkadot/runtime/westend/src/lib.rs
@@ -68,6 +68,7 @@ use runtime_common::{
 use runtime_parachains::{
 	assigner_coretime as parachains_assigner_coretime,
 	assigner_on_demand as parachains_assigner_on_demand, configuration as parachains_configuration,
+	configuration::ActiveConfigHrmpChannelSizeAndCapacityRatio,
 	coretime, disputes as parachains_disputes,
 	disputes::slashing as parachains_slashing,
 	dmp as parachains_dmp, hrmp as parachains_hrmp, inclusion as parachains_inclusion,
@@ -1163,7 +1164,7 @@ impl pallet_message_queue::Config for Runtime {
 impl parachains_dmp::Config for Runtime {}
 
 parameter_types! {
-	pub const DefaultChannelSizeAndCapacityWithSystem: (u32, u32) = (4096, 4);
+	pub const HrmpChannelSizeAndCapacityWithSystemRatio: Percent = Percent::from_percent(100);
 }
 
 impl parachains_hrmp::Config for Runtime {
@@ -1171,7 +1172,10 @@ impl parachains_hrmp::Config for Runtime {
 	type RuntimeEvent = RuntimeEvent;
 	type ChannelManager = EnsureRoot<AccountId>;
 	type Currency = Balances;
-	type DefaultChannelSizeAndCapacityWithSystem = DefaultChannelSizeAndCapacityWithSystem;
+	type DefaultChannelSizeAndCapacityWithSystem = ActiveConfigHrmpChannelSizeAndCapacityRatio<
+		Runtime,
+		HrmpChannelSizeAndCapacityWithSystemRatio,
+	>;
 	type WeightInfo = weights::runtime_parachains_hrmp::WeightInfo<Self>;
 }
 
