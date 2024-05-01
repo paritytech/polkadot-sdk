29470d260df1c578e3a1b875eaa8438a54f70115
dastansam <dastan.samatov.33@gmail.com>
2024-05-01 04:01:37 +0200

    Fix checks

--- substrate/utils/frame/sub-du/src/main.rs
+++ substrate/utils/frame/sub-du/src/main.rs
@@ -1,3 +1,19 @@
+// This file is part of Substrate.
+
+// Copyright (C) Parity Technologies (UK) Ltd.
+// SPDX-License-Identifier: Apache-2.0
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 //! # `sub-du`
 //!
 //! A tool like [`du`](https://en.wikipedia.org/wiki/Du_(Unix)) that calculates storage size of pallets of a Substrate chain.
@@ -50,9 +66,6 @@ use sp_core::{storage::StorageKey, twox_128};
 use sp_runtime::testing::{Header, H256 as Hash};
 use structopt::StructOpt;
 use substrate_rpc_client::WsClient;
-
-#[cfg(test)]
-mod tests;
 
 const KB: usize = 1024;
 const MB: usize = KB * KB;
