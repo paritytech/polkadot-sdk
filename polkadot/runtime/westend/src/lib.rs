d45bc3371249fb841ebea1acf8488b76316ac189
Egor_P <egor@parity.io>
2024-05-02 09:21:11 +0200

    [Backport] Version bumps and reorg prdocs from 1.11.0 (#4336)
    This PR backports version bumps and reorganization of the `prdocs` from
    `1.11.0` release branch back to master

--- polkadot/runtime/westend/src/lib.rs
+++ polkadot/runtime/westend/src/lib.rs
@@ -154,10 +154,10 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("westend"),
 	impl_name: create_runtime_str!("parity-westend"),
 	authoring_version: 2,
-	spec_version: 1_010_000,
+	spec_version: 1_011_000,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
-	transaction_version: 24,
+	transaction_version: 25,
 	state_version: 1,
 };
 
