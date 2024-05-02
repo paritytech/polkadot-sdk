789cc7772018ac5fc6998e2fc9cc8cc243188805
paritytech-ci <devops-team@parity.io>
2024-05-02 21:25:51 +0000

    ___Updated docs for master___

--- master/trait.impl/bp_messages/trait.ChainWithMessages.js
+++ master/trait.impl/bp_messages/trait.ChainWithMessages.js
@@ -5,5 +5,5 @@
 "bp_bridge_hub_westend":[["impl ChainWithMessages for <a class=\"struct\" href=\"bp_bridge_hub_westend/struct.BridgeHubWestend.html\" title=\"struct bp_bridge_hub_westend::BridgeHubWestend\">BridgeHubWestend</a>"]],
 "bp_messages":[],
 "bp_polkadot_bulletin":[["impl ChainWithMessages for <a class=\"struct\" href=\"bp_polkadot_bulletin/struct.PolkadotBulletin.html\" title=\"struct bp_polkadot_bulletin::PolkadotBulletin\">PolkadotBulletin</a>"]],
-"relay_substrate_client":[["impl <a class=\"trait\" href=\"bp_messages/trait.ChainWithMessages.html\" title=\"trait bp_messages::ChainWithMessages\">ChainWithMessages</a> for <a class=\"struct\" href=\"relay_substrate_client/test_chain/struct.TestChain.html\" title=\"struct relay_substrate_client::test_chain::TestChain\">TestChain</a>"]]
+"relay_substrate_client":[["impl ChainWithMessages for <a class=\"struct\" href=\"relay_substrate_client/test_chain/struct.TestChain.html\" title=\"struct relay_substrate_client::test_chain::TestChain\">TestChain</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
