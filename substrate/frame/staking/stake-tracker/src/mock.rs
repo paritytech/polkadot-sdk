97d254e31031b8ee31e2a4a84f9058fed67dcea2
Gonçalo Pestana <g6pestana@gmail.com>
2024-05-02 13:57:21 +0100

    Update substrate/frame/staking/stake-tracker/src/mock.rs
    Co-authored-by: Kian Paimani <5588131+kianenigma@users.noreply.github.com>

--- substrate/frame/staking/stake-tracker/src/mock.rs
+++ substrate/frame/staking/stake-tracker/src/mock.rs
@@ -45,7 +45,7 @@ parameter_types! {
 	pub static ExistentialDeposit: Balance = 1;
 }
 
-#[derive_impl(frame_system::config_preludes::TestDefaultConfig as frame_system::DefaultConfig)]
+#[derive_impl(frame_system::config_preludes::TestDefaultConfig)]
 impl frame_system::Config for Test {
 	type Block = Block;
 	type BlockHashCount = ConstU32<10>;
