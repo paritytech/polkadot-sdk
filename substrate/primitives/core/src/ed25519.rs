e203c93ab6c1bc96fcb67dc226ddac64434a86ba
Jun Jiang <jasl9187@hotmail.com>
2024-05-06 19:49:14 +0800

    Upgrade a few deps (#4381)
    Split from #4374
    
    This PR helps to reduce dependencies and align versions, which would
    help to move them to workspace dep

--- substrate/primitives/core/src/ed25519.rs
+++ substrate/primitives/core/src/ed25519.rs
@@ -110,7 +110,9 @@ impl TraitPair for Pair {
 	/// Returns true if the signature is good.
 	fn verify<M: AsRef<[u8]>>(sig: &Signature, message: M, public: &Public) -> bool {
 		let Ok(public) = VerificationKey::try_from(public.as_slice()) else { return false };
-		let Ok(signature) = ed25519_zebra::Signature::try_from(sig.as_ref()) else { return false };
+		let Ok(signature) = ed25519_zebra::Signature::try_from(sig.as_slice()) else {
+			return false
+		};
 		public.verify(&signature, message.as_ref()).is_ok()
 	}
 
