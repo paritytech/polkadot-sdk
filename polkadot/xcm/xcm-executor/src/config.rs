eea690bd5dce704bebcbf0e36e5a39f157338f62
Francisco Aguirre <franciscoaguirreperez@gmail.com>
2024-05-08 02:16:47 +0200

    feat(xcm-executor): add AssetConverter config item to support multiple fees

--- polkadot/xcm/xcm-executor/src/config.rs
+++ polkadot/xcm/xcm-executor/src/config.rs
@@ -15,7 +15,7 @@
 // along with Polkadot.  If not, see <http://www.gnu.org/licenses/>.
 
 use crate::traits::{
-	AssetExchange, AssetLock, CallDispatcher, ClaimAssets, ConvertOrigin, DropAssets, ExportXcm,
+	AssetExchange, AssetConversion, AssetLock, CallDispatcher, ClaimAssets, ConvertOrigin, DropAssets, ExportXcm,
 	FeeManager, HandleHrmpChannelAccepted, HandleHrmpChannelClosing,
 	HandleHrmpNewChannelOpenRequest, OnResponse, ProcessTransaction, ShouldExecute, TransactAsset,
 	VersionChangeNotifier, WeightBounds, WeightTrader,
@@ -122,4 +122,7 @@ pub trait Config {
 	type HrmpChannelAcceptedHandler: HandleHrmpChannelAccepted;
 	/// Allows optional logic execution for the `HrmpChannelClosing` XCM notification.
 	type HrmpChannelClosingHandler: HandleHrmpChannelClosing;
+
+	/// Allows converting a balance of one asset into another.
+	type AssetConverter: AssetConversion;
 }
--- polkadot/xcm/xcm-executor/src/traits/fee_manager.rs
+++ polkadot/xcm/xcm-executor/src/traits/fee_manager.rs
@@ -58,3 +58,43 @@ impl FeeManager for () {
 
 	fn handle_fee(_: Assets, _: Option<&XcmContext>, _: FeeReason) {}
 }
+
+/// Not about exchanging assets, just converting an amount of one
+/// into one of another.
+/// Used for paying fees in different assets.
+pub trait AssetConversion {
+	/// Convert `asset` to the specified `asset_id`.
+	/// If the conversion can be done, the returned Asset
+	/// has the specified `asset_id` and a new balance.
+	/// If it can't be converted, an error is returned.
+	fn convert_asset(asset: &Asset, asset_id: &AssetId) -> Result<Asset, XcmError>;
+}
+
+#[impl_trait_for_tuples::impl_for_tuples(30)]
+impl AssetConversion for Tuple {
+	fn convert_asset(asset: &Asset, asset_id: &AssetId) -> Result<Asset, XcmError> {
+		let mut last_error = None;
+
+		for_tuples!( #(
+			match Tuple::convert_asset(asset, asset_id) {
+				Ok(new_asset) => {
+					log::trace!(
+						target: "xcm::AssetConversion::convert_asset",
+						"Found successful implementation in tuple.",
+					);
+					return Ok(new_asset)
+				},
+				Err(error) => {
+					log::error!(
+						target: "xcm::AssetConversion::convert_asset",
+						"Implementation in tuple errored: {:?}, trying with next one, unless this was the last one.",
+						error,
+					);
+					last_error = Some(error);
+				}
+			}
+		)* );
+
+		Err(last_error.unwrap_or(XcmError::TooExpensive))
+	}
+}
