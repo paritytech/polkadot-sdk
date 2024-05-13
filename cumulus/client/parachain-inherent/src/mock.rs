f3bab06ed0773fee12ad99e59dbabee869484b58
Éloïs <c@elo.tf>
2024-05-13 16:29:39 +0200

    rustfmt

--- cumulus/client/parachain-inherent/src/mock.rs
+++ cumulus/client/parachain-inherent/src/mock.rs
@@ -167,12 +167,12 @@ impl<R: Send + Sync + GenerateRandomness<u64>> InherentDataProvider
 		// Use the "sproof" (spoof proof) builder to build valid mock state root and proof.
 		let mut sproof_builder =
 			RelayStateSproofBuilder { para_id: self.xcm_config.para_id, ..Default::default() };
-
 		
 		// Calculate the mocked relay block based on the current para block
 		let relay_parent_number =
 			self.relay_offset + self.relay_blocks_per_para_block * self.current_para_block;
-		sproof_builder.current_slot = (relay_parent_number as u64).into() / RELAY_CHAIN_SLOT_DURATION_MILLIS;
+		sproof_builder.current_slot =
+			(relay_parent_number as u64).into() / RELAY_CHAIN_SLOT_DURATION_MILLIS;
 
 		// Process the downward messages and set up the correct head
 		let mut downward_messages = Vec::new();
