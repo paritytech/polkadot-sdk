ceb9ddf359aa7de2f4dccfb709a6da9d69c7472c
Bastian Köcher <info@kchr.de>
2024-05-14 22:39:42 +0200

    Fix compile errors

--- polkadot/node/service/src/benchmarking.rs
+++ polkadot/node/service/src/benchmarking.rs
@@ -201,6 +201,7 @@ fn westend_sign_call(
 		frame_system::CheckNonce::<runtime::Runtime>::from(nonce),
 		frame_system::CheckWeight::<runtime::Runtime>::new(),
 		pallet_transaction_payment::ChargeTransactionPayment::<runtime::Runtime>::from(0),
+		frame_metadata_hash_extension::CheckMetadataHash::<runtime::Runtime>::new(false),
 	);
 
 	let payload = runtime::SignedPayload::from_raw(
@@ -215,6 +216,7 @@ fn westend_sign_call(
 			(),
 			(),
 			(),
+			frame_metadata_hash_extension::EncodeNoneToEmpty(None),
 		),
 	);
 
