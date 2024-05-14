7321721093511296d10c5347d18127d12bc58c8f
Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
2024-05-14 13:39:04 +0200

    Fix compile
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>

--- substrate/bin/node/cli/build.rs
+++ substrate/bin/node/cli/build.rs
@@ -23,7 +23,7 @@ fn main() {
 
 #[cfg(feature = "cli")]
 mod cli {
-	//include!("src/cli.rs");
+	include!("src/cli.rs");
 
 	use clap::{CommandFactory, ValueEnum};
 	use clap_complete::{generate_to, Shell};
@@ -33,13 +33,13 @@ mod cli {
 	use std::{env, fs, path::Path};
 
 	pub fn main() {
-		//build_shell_completion();
+		build_shell_completion();
 		generate_cargo_keys();
 
 		rerun_if_git_head_changed();
 	}
 
-	/*/ Build shell completion scripts for all known shells.
+	// Build shell completion scripts for all known shells.
 	fn build_shell_completion() {
 		for shell in Shell::value_variants() {
 			build_completion(shell);
@@ -64,5 +64,5 @@ mod cli {
 		fs::create_dir(&path).ok();
 
 		let _ = generate_to(*shell, &mut Cli::command(), "substrate-node", &path);
-	}*/
+	}
 }
