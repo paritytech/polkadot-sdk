6d27949219f6431aa0e6b197a9706290c82c12ab
Bastian Köcher <info@kchr.de>
2024-05-14 09:19:13 +0200

    Start working on a guide

---
+++ docs/sdk/src/guides/enable_metadata_hash.rs
@@ -0,0 +1,3 @@
+//! # Enable metadata hash verification
+//!
+//! This guide will teach you how to enable the metadata hash verification in your runtime.
--- templates/parachain/runtime/build.rs
+++ templates/parachain/runtime/build.rs
@@ -1,4 +1,11 @@
-#[cfg(feature = "std")]
+#[cfg(all(feature = "std", feature = "metadata-hash"))]
+fn main() {
+	substrate_wasm_builder::WasmBuilder::init_with_defaults()
+		.enable_metadata_hash("UNIT", 12)
+		.build();
+}
+
+#[cfg(all(feature = "std", not(feature = "metadata-hash")))]
 fn main() {
 	substrate_wasm_builder::WasmBuilder::build_using_defaults();
 }
