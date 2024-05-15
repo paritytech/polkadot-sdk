5c989229d420cbc0a76175c77cea55396cb0cf96
paritytech-ci <devops-team@parity.io>
2024-05-15 21:33:33 +0000

    ___Updated docs for master___

--- master/trait.impl/frame_support/traits/validation/trait.DisabledValidators.js
+++ master/trait.impl/frame_support/traits/validation/trait.DisabledValidators.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {
 "frame_support":[],
-"pallet_session":[["impl&lt;T: <a class=\"trait\" href=\"pallet_session/pallet/trait.Config.html\" title=\"trait pallet_session::pallet::Config\">Config</a>&gt; <a class=\"trait\" href=\"frame_support/traits/validation/trait.DisabledValidators.html\" title=\"trait frame_support::traits::validation::DisabledValidators\">DisabledValidators</a> for <a class=\"struct\" href=\"pallet_session/pallet/struct.Pallet.html\" title=\"struct pallet_session::pallet::Pallet\">Pallet</a>&lt;T&gt;"]],
+"pallet_session":[["impl&lt;T: <a class=\"trait\" href=\"pallet_session/pallet/trait.Config.html\" title=\"trait pallet_session::pallet::Config\">Config</a>&gt; DisabledValidators for <a class=\"struct\" href=\"pallet_session/pallet/struct.Pallet.html\" title=\"struct pallet_session::pallet::Pallet\">Pallet</a>&lt;T&gt;"]],
 "polkadot_sdk_frame":[]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
