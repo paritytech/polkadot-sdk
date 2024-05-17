6713988fcf73514a60e43043609e4d02f3a19c78
Bastian Köcher <info@kchr.de>
2024-05-17 21:49:54 +0200

    Add `CheckMetadataHash` to the `kitchensink-runtime`

--- substrate/bin/node/testing/src/keyring.rs
+++ substrate/bin/node/testing/src/keyring.rs
@@ -82,6 +82,7 @@ pub fn signed_extra(nonce: Nonce, extra_fee: Balance) -> SignedExtra {
 		pallet_skip_feeless_payment::SkipCheckIfFeeless::from(
 			pallet_asset_conversion_tx_payment::ChargeAssetTxPayment::from(extra_fee, None),
 		),
+		frame_metadata_hash_extension::CheckMetadataHash::new(false),
 	)
 }
 
