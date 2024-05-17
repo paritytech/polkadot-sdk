23844c95885cfa641be59cc103cafcb4939765a8
paritytech-ci <devops-team@parity.io>
2024-05-17 12:24:59 +0000

    ___Updated docs for master___

--- master/snowbridge_pallet_ethereum_client/types/sidebar-items.js
+++ master/snowbridge_pallet_ethereum_client/types/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"struct":["AncestryProof","ExecutionProof"],"type":["CheckpointUpdate","NextSyncCommitteeUpdate","SyncAggregate","SyncCommittee","SyncCommitteePrepared","Update"]};
+window.SIDEBAR_ITEMS = {"struct":["AncestryProof","ExecutionProof"],"type":["CheckpointUpdate","FinalizedBeaconStateBuffer","NextSyncCommitteeUpdate","SyncAggregate","SyncCommittee","SyncCommitteePrepared","Update"]};
