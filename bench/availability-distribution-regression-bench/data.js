edbd75e32599341ed14682574e53308dd698241a
github-action-benchmark <github@users.noreply.github.com>
2024-05-19 19:30:37 +0000

    add availability-distribution-regression-bench (customSmallerIsBetter) benchmark result for e7b6d7dffd6459174f02598bd8b84fe4b1cb6e72

--- bench/availability-distribution-regression-bench/data.js
+++ bench/availability-distribution-regression-bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1716138327873,
+  "lastUpdate": 1716147037212,
   "repoUrl": "https://github.com/paritytech/polkadot-sdk",
   "entries": {
     "availability-distribution-regression-bench": [
@@ -10295,6 +10295,58 @@ window.BENCHMARK_DATA = {
           {
             "name": "test-environment",
             "value": 0.011848364740000008,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Liam Aharon",
+            "username": "liamaharon",
+            "email": "liam.aharon@hotmail.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "e7b6d7dffd6459174f02598bd8b84fe4b1cb6e72",
+          "message": "`remote-externalities`: `rpc_child_get_keys` to use paged scraping (#4512)\n\nReplace usage of deprecated\n`substrate_rpc_client::ChildStateApi::storage_keys` with\n`substrate_rpc_client::ChildStateApi::storage_keys_paged`.\n\nRequired for successful scraping of Aleph Zero state.",
+          "timestamp": "2024-05-19T17:53:12Z",
+          "url": "https://github.com/paritytech/polkadot-sdk/commit/e7b6d7dffd6459174f02598bd8b84fe4b1cb6e72"
+        },
+        "date": 1716147009828,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Received from peers",
+            "value": 433.3333333333332,
+            "unit": "KiB"
+          },
+          {
+            "name": "Sent to peers",
+            "value": 18479.87333333333,
+            "unit": "KiB"
+          },
+          {
+            "name": "availability-store",
+            "value": 0.1573437672733334,
+            "unit": "seconds"
+          },
+          {
+            "name": "availability-distribution",
+            "value": 0.011570199106666665,
+            "unit": "seconds"
+          },
+          {
+            "name": "bitfield-distribution",
+            "value": 0.0218602114,
+            "unit": "seconds"
+          },
+          {
+            "name": "test-environment",
+            "value": 0.009043280346666675,
             "unit": "seconds"
           }
         ]
