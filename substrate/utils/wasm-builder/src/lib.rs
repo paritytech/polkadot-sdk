c9079411cb585634847cf87fb114c3b80d159734
Bastian Köcher <git@kchr.de>
2024-05-22 16:21:32 +0200

    Implement `CheckMetadataHash` extension (#4274)
    This implements the `CheckMetadataHash` extension as described in
    [RFC78](https://polkadot-fellows.github.io/RFCs/approved/0078-merkleized-metadata.html).
    
    Besides the signed extension, the `substrate-wasm-builder` is extended
    to support generating the metadata-hash.
    
    Closes: https://github.com/paritytech/polkadot-sdk/issues/291
    
    ---------
    
    Co-authored-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
    Co-authored-by: joe petrowski <25483142+joepetrowski@users.noreply.github.com>
    Co-authored-by: Liam Aharon <liam.aharon@hotmail.com>
    Co-authored-by: Kian Paimani <5588131+kianenigma@users.noreply.github.com>

--- substrate/utils/wasm-builder/src/lib.rs
+++ substrate/utils/wasm-builder/src/lib.rs
@@ -115,6 +115,8 @@ use std::{
 use version::Version;
 
 mod builder;
+#[cfg(feature = "metadata-hash")]
+mod metadata_hash;
 mod prerequisites;
 mod version;
 mod wasm_project;
@@ -236,7 +238,7 @@ fn get_rustup_command() -> Option<CargoCommand> {
 }
 
 /// Wraps a specific command which represents a cargo invocation.
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct CargoCommand {
 	program: String,
 	args: Vec<String>,
@@ -310,6 +312,7 @@ impl CargoCommand {
 }
 
 /// Wraps a [`CargoCommand`] and the version of `rustc` the cargo command uses.
+#[derive(Clone)]
 struct CargoCommandVersioned {
 	command: CargoCommand,
 	version: String,
