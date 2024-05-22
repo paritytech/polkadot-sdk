e7a6c26ecd5b502e26e44c1976fcb7373348e9de
Branislav Kontur <bkontur@gmail.com>
2024-05-22 14:59:23 +0200

    clippy

--- substrate/frame/paged-list/src/mock.rs
+++ substrate/frame/paged-list/src/mock.rs
@@ -20,10 +20,7 @@
 #![cfg(feature = "std")]
 
 use crate::{paged_list::StoragePagedListMeta, Config, ListPrefix};
-use frame_support::{
-	derive_impl,
-	traits::{ConstU16, ConstU64},
-};
+use frame_support::{derive_impl, traits::ConstU16};
 use sp_core::H256;
 use sp_runtime::{
 	traits::{BlakeTwo256, IdentityLookup},
