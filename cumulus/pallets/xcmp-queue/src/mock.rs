9e09b7acfa4301230271100b51475244b0fb0eb2
Branislav Kontur <bkontur@gmail.com>
2024-05-24 12:01:10 +0200

    Attempt to avoid specifying `BlockHashCount` for different `mocking::{MockBlock, MockBlockU32, MockBlockU128}` (#4543)
    While doing some migration/rebase I came in to the situation, where I
    needed to change `mocking::MockBlock` to `mocking::MockBlockU32`:
    ```
    #[derive_impl(frame_system::config_preludes::TestDefaultConfig)]
    impl frame_system::Config for TestRuntime {
    	type Block = frame_system::mocking::MockBlockU32<TestRuntime>;
    	type AccountData = pallet_balances::AccountData<ThisChainBalance>;
    }
    ```
    But actual `TestDefaultConfig` for `frame_system` is using `ConstU64`
    for `type BlockHashCount = frame_support::traits::ConstU64<10>;`
    [here](https://github.com/paritytech/polkadot-sdk/blob/master/substrate/frame/system/src/lib.rs#L303).
    Because of this, it force me to specify and add override for `type
    BlockHashCount = ConstU32<10>`.
    
    This PR tries to fix this with `TestBlockHashCount` implementation for
    `TestDefaultConfig` which supports `u32`, `u64` and `u128` as a
    `BlockNumber`.
    
    ### How to simulate error
    Just by removing `type BlockHashCount = ConstU32<250>;`
    [here](https://github.com/paritytech/polkadot-sdk/blob/master/substrate/frame/multisig/src/tests.rs#L44)
    ```
    :~/parity/olkadot-sdk$ cargo test -p pallet-multisig
       Compiling pallet-multisig v28.0.0 (/home/bparity/parity/aaa/polkadot-sdk/substrate/frame/multisig)
    error[E0277]: the trait bound `ConstU64<10>: frame_support::traits::Get<u32>` is not satisfied
       --> substrate/frame/multisig/src/tests.rs:41:1
        |
    41  | #[derive_impl(frame_system::config_preludes::TestDefaultConfig)]
        | ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ the trait `frame_support::traits::Get<u32>` is not implemented for `ConstU64<10>`
        |
        = help: the following other types implement trait `frame_support::traits::Get<T>`:
                  <ConstU64<T> as frame_support::traits::Get<u64>>
                  <ConstU64<T> as frame_support::traits::Get<std::option::Option<u64>>>
    note: required by a bound in `frame_system::Config::BlockHashCount`
       --> /home/bparity/parity/aaa/polkadot-sdk/substrate/frame/system/src/lib.rs:535:24
        |
    535 |         type BlockHashCount: Get<BlockNumberFor<Self>>;
        |                              ^^^^^^^^^^^^^^^^^^^^^^^^^ required by this bound in `Config::BlockHashCount`
        = note: this error originates in the attribute macro `derive_impl` which comes from the expansion of the macro `frame_support::macro_magic::forward_tokens_verbatim` (in Nightly builds, run with -Z macro-backtrace for more info)
    
    For more information about this error, try `rustc --explain E0277`.
    error: could not compile `pallet-multisig` (lib test) due to 1 previous error 
    ```
    
    
    
    
    ## For reviewers:
    
    (If there is a better solution, please let me know!)
    
    The first commit contains actual attempt to fix the problem:
    https://github.com/paritytech/polkadot-sdk/commit/3c5499e539f2218503fbd6ce9be085b03c31ee13.
    The second commit is just removal of `BlockHashCount` from all other
    places where not needed by default.
    
    Closes: https://github.com/paritytech/polkadot-sdk/issues/1657
    
    ---------
    
    Co-authored-by: Bastian Köcher <git@kchr.de>

--- cumulus/pallets/xcmp-queue/src/mock.rs
+++ cumulus/pallets/xcmp-queue/src/mock.rs
@@ -52,7 +52,6 @@ frame_support::construct_runtime!(
 );
 
 parameter_types! {
-	pub const BlockHashCount: u64 = 250;
 	pub const SS58Prefix: u8 = 42;
 }
 
@@ -73,7 +72,6 @@ impl frame_system::Config for Test {
 	type Lookup = IdentityLookup<Self::AccountId>;
 	type Block = Block;
 	type RuntimeEvent = RuntimeEvent;
-	type BlockHashCount = BlockHashCount;
 	type Version = ();
 	type PalletInfo = PalletInfo;
 	type AccountData = pallet_balances::AccountData<u64>;
