fc7bee35f782064d4e926adafd62e49304a89f60
Bastian Köcher <git@kchr.de>
2024-05-27 15:01:40 +0200

    `CheckMetadataHash` extension backport to 1.4.0 (#4577)
    Co-authored-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
    Co-authored-by: joe petrowski <25483142+joepetrowski@users.noreply.github.com>
    Co-authored-by: Liam Aharon <liam.aharon@hotmail.com>
    Co-authored-by: Kian Paimani <5588131+kianenigma@users.noreply.github.com>

--- cumulus/parachains/runtimes/assets/asset-hub-rococo/src/lib.rs
+++ cumulus/parachains/runtimes/assets/asset-hub-rococo/src/lib.rs
@@ -954,6 +954,7 @@ pub type SignedExtra = (
 	frame_system::CheckNonce<Runtime>,
 	frame_system::CheckWeight<Runtime>,
 	pallet_asset_conversion_tx_payment::ChargeAssetTxPayment<Runtime>,
+	frame_metadata_hash_extension::CheckMetadataHash<Runtime>,
 );
 /// Unchecked extrinsic type as expected by this runtime.
 pub type UncheckedExtrinsic =
