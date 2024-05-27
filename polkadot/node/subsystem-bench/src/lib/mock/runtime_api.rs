a7097681b76bdaef21dcde9aec8c33205f480e44
Andrei Eres <eresav@me.com>
2024-05-27 21:23:58 +0200

    [subsystem-benchmarks] Add statement-distribution benchmarks (#3863)
    Fixes https://github.com/paritytech/polkadot-sdk/issues/3748
    
    Adds a subsystem benchmark for statements-distribution subsystem.
    
    Results in CI (reference hw):
    ```
    $ cargo bench -p polkadot-statement-distribution --bench statement-distribution-regression-bench --features subsystem-benchmarks
    
    [Sent to peers] standart_deviation 0.07%
    [Received from peers] standart_deviation 0.00%
    [statement-distribution] standart_deviation 0.97%
    [test-environment] standart_deviation 1.03%
    
    Network usage, KiB                     total   per block
    Received from peers                1088.0000    108.8000
    Sent to peers                      1238.1800    123.8180
    
    CPU usage, seconds                     total   per block
    statement-distribution                0.3897      0.0390
    test-environment                      0.4715      0.0472
    ```

--- polkadot/node/subsystem-bench/src/lib/mock/runtime_api.rs
+++ polkadot/node/subsystem-bench/src/lib/mock/runtime_api.rs
@@ -26,8 +26,9 @@ use polkadot_node_subsystem::{
 };
 use polkadot_node_subsystem_types::OverseerSignal;
 use polkadot_primitives::{
-	CandidateEvent, CandidateReceipt, CoreState, GroupIndex, IndexedVec, NodeFeatures,
-	OccupiedCore, SessionIndex, SessionInfo, ValidatorIndex,
+	AsyncBackingParams, CandidateEvent, CandidateReceipt, CoreState, GroupIndex, GroupRotationInfo,
+	IndexedVec, NodeFeatures, OccupiedCore, ScheduledCore, SessionIndex, SessionInfo,
+	ValidatorIndex,
 };
 use sp_consensus_babe::Epoch as BabeEpoch;
 use sp_core::H256;
@@ -49,11 +50,20 @@ pub struct RuntimeApiState {
 	session_index: SessionIndex,
 }
 
+#[derive(Clone)]
+pub enum MockRuntimeApiCoreState {
+	Occupied,
+	Scheduled,
+	#[allow(dead_code)]
+	Free,
+}
+
 /// A mocked `runtime-api` subsystem.
 #[derive(Clone)]
 pub struct MockRuntimeApi {
 	state: RuntimeApiState,
 	config: TestConfiguration,
+	core_state: MockRuntimeApiCoreState,
 }
 
 impl MockRuntimeApi {
@@ -64,6 +74,7 @@ impl MockRuntimeApi {
 		included_candidates: HashMap<H256, Vec<CandidateEvent>>,
 		babe_epoch: Option<BabeEpoch>,
 		session_index: SessionIndex,
+		core_state: MockRuntimeApiCoreState,
 	) -> MockRuntimeApi {
 		Self {
 			state: RuntimeApiState {
@@ -74,6 +85,7 @@ impl MockRuntimeApi {
 				session_index,
 			},
 			config,
+			core_state,
 		}
 	}
 
@@ -198,16 +210,26 @@ impl MockRuntimeApi {
 									// Ensure test breaks if badly configured.
 									assert!(index < validator_group_count);
 
-									CoreState::Occupied(OccupiedCore {
-										next_up_on_available: None,
-										occupied_since: 0,
-										time_out_at: 0,
-										next_up_on_time_out: None,
-										availability: BitVec::default(),
-										group_responsible: GroupIndex(index as u32),
-										candidate_hash: candidate_receipt.hash(),
-										candidate_descriptor: candidate_receipt.descriptor.clone(),
-									})
+									use MockRuntimeApiCoreState::*;
+									match self.core_state {
+										Occupied => CoreState::Occupied(OccupiedCore {
+											next_up_on_available: None,
+											occupied_since: 0,
+											time_out_at: 0,
+											next_up_on_time_out: None,
+											availability: BitVec::default(),
+											group_responsible: GroupIndex(index as u32),
+											candidate_hash: candidate_receipt.hash(),
+											candidate_descriptor: candidate_receipt
+												.descriptor
+												.clone(),
+										}),
+										Scheduled => CoreState::Scheduled(ScheduledCore {
+											para_id: (index + 1).into(),
+											collator: None,
+										}),
+										Free => todo!(),
+									}
 								})
 								.collect::<Vec<_>>();
 
@@ -222,6 +244,43 @@ impl MockRuntimeApi {
 								.babe_epoch
 								.clone()
 								.expect("Babe epoch unpopulated")));
+						},
+						RuntimeApiMessage::Request(
+							_block_hash,
+							RuntimeApiRequest::AsyncBackingParams(sender),
+						) => {
+							let _ = sender.send(Ok(AsyncBackingParams {
+								max_candidate_depth: self.config.max_candidate_depth,
+								allowed_ancestry_len: self.config.allowed_ancestry_len,
+							}));
+						},
+						RuntimeApiMessage::Request(_parent, RuntimeApiRequest::Version(tx)) => {
+							tx.send(Ok(RuntimeApiRequest::DISABLED_VALIDATORS_RUNTIME_REQUIREMENT))
+								.unwrap();
+						},
+						RuntimeApiMessage::Request(
+							_parent,
+							RuntimeApiRequest::DisabledValidators(tx),
+						) => {
+							tx.send(Ok(vec![])).unwrap();
+						},
+						RuntimeApiMessage::Request(
+							_parent,
+							RuntimeApiRequest::MinimumBackingVotes(_session_index, tx),
+						) => {
+							tx.send(Ok(self.config.minimum_backing_votes)).unwrap();
+						},
+						RuntimeApiMessage::Request(
+							_parent,
+							RuntimeApiRequest::ValidatorGroups(tx),
+						) => {
+							let groups = self.session_info().validator_groups.to_vec();
+							let group_rotation_info = GroupRotationInfo {
+								session_start_block: 1,
+								group_rotation_frequency: 12,
+								now: 1,
+							};
+							tx.send(Ok((groups, group_rotation_info))).unwrap();
 						},
 						// Long term TODO: implement more as needed.
 						message => {
