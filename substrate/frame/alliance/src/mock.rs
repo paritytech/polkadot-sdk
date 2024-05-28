ff47fddb409e5fabd10cf486760a8d0b4c6a999d
polka.dom <polkadotdom@gmail.com>
2024-05-28 07:44:03 -0400

    Fix pallet alliance issues

--- substrate/frame/alliance/src/mock.rs
+++ substrate/frame/alliance/src/mock.rs
@@ -31,7 +31,7 @@ pub use frame_support::{
 use frame_system::{EnsureRoot, EnsureSignedBy};
 use pallet_identity::{
 	legacy::{IdentityField, IdentityInfo},
-	Data, Judgement,
+	Data, Judgement, IdentityOf, SuperOf,
 };
 
 pub use crate as pallet_alliance;
@@ -161,7 +161,7 @@ impl IdentityVerifier<AccountId> for AllianceIdentityVerifier {
 
 	fn has_good_judgement(who: &AccountId) -> bool {
 		if let Some(judgements) =
-			Identity::identity(who).map(|(registration, _)| registration.judgements)
+			IdentityOf::<Test>::get(who).map(|(registration, _)| registration.judgements)
 		{
 			judgements
 				.iter()
@@ -172,7 +172,7 @@ impl IdentityVerifier<AccountId> for AllianceIdentityVerifier {
 	}
 
 	fn super_account_id(who: &AccountId) -> Option<AccountId> {
-		Identity::super_of(who).map(|parent| parent.0)
+		SuperOf::<Test>::get(who).map(|parent| parent.0)
 	}
 }
 
