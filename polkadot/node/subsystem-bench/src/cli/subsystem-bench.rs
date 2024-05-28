bab3fde731b59a6cca7d9a4acabbf33b68b5f0f0
Andrei Eres <eresav@me.com>
2024-05-28 09:00:59 +0200

    Update statement-distribution bench

--- polkadot/node/subsystem-bench/src/cli/subsystem-bench.rs
+++ polkadot/node/subsystem-bench/src/cli/subsystem-bench.rs
@@ -166,11 +166,8 @@ impl BenchCli {
 				TestObjective::StatementDistribution => {
 					let state = statement::TestState::new(&test_config);
 					let (mut env, _protocol_config) = statement::prepare_test(&state, true);
-					env.runtime().block_on(statement::benchmark_statement_distribution(
-						&benchmark_name,
-						&mut env,
-						&state,
-					))
+					env.runtime()
+						.block_on(statement::benchmark_statement_distribution(&mut env, &state))
 				},
 			};
 			println!("\n{}\n{}", benchmark_name.purple(), usage);
