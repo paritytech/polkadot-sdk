4673cfc85071f7071e990d779daca0f0b680e300
paritytech-ci <devops-team@parity.io>
2024-05-29 10:53:58 +0000

    ___Updated docs for master___

--- master/trait.impl/sp_staking/trait.StakingUnchecked.js
+++ master/trait.impl/sp_staking/trait.StakingUnchecked.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"pallet_staking":[["impl&lt;T: <a class=\"trait\" href=\"pallet_staking/trait.Config.html\" title=\"trait pallet_staking::Config\">Config</a>&gt; StakingUnchecked for <a class=\"struct\" href=\"pallet_staking/struct.Pallet.html\" title=\"struct pallet_staking::Pallet\">Pallet</a>&lt;T&gt;"]]
+"pallet_staking":[["impl&lt;T: <a class=\"trait\" href=\"pallet_staking/trait.Config.html\" title=\"trait pallet_staking::Config\">Config</a>&gt; <a class=\"trait\" href=\"sp_staking/trait.StakingUnchecked.html\" title=\"trait sp_staking::StakingUnchecked\">StakingUnchecked</a> for <a class=\"struct\" href=\"pallet_staking/struct.Pallet.html\" title=\"struct pallet_staking::Pallet\">Pallet</a>&lt;T&gt;"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
