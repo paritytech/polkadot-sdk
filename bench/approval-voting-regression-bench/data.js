d5662d3e6e71839301cf152986e5a9ec23b4c8b1
github-action-benchmark <github@users.noreply.github.com>
2024-05-31 17:11:52 +0000

    add approval-voting-regression-bench (customSmallerIsBetter) benchmark result for fc6c31829fc2e24e11a02b6a2adec27bc5d8918f

--- bench/approval-voting-regression-bench/data.js
+++ bench/approval-voting-regression-bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1717156328542,
+  "lastUpdate": 1717175511924,
   "repoUrl": "https://github.com/paritytech/polkadot-sdk",
   "entries": {
     "approval-voting-regression-bench": [
@@ -5921,6 +5921,53 @@ window.BENCHMARK_DATA = {
           {
             "name": "test-environment",
             "value": 3.844264029350183,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Francisco Aguirre",
+            "username": "franciscoaguirre",
+            "email": "franciscoaguirreperez@gmail.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "fc6c31829fc2e24e11a02b6a2adec27bc5d8918f",
+          "message": "Implement `XcmPaymentApi` and `DryRunApi` on all system parachains (#4634)\n\nDepends on https://github.com/paritytech/polkadot-sdk/pull/4621.\n\nImplemented the\n[`XcmPaymentApi`](https://github.com/paritytech/polkadot-sdk/pull/3607)\nand [`DryRunApi`](https://github.com/paritytech/polkadot-sdk/pull/3872)\non all system parachains.\n\nMore scenarios can be tested on both rococo and westend if all system\nparachains implement this APIs.\nThe objective is for all XCM-enabled runtimes to implement them.\nAfter demonstrating fee estimation in a UI on the testnets, come the\nfellowship runtimes.\n\nStep towards https://github.com/paritytech/polkadot-sdk/issues/690.",
+          "timestamp": "2024-05-31T15:38:56Z",
+          "url": "https://github.com/paritytech/polkadot-sdk/commit/fc6c31829fc2e24e11a02b6a2adec27bc5d8918f"
+        },
+        "date": 1717175484487,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Sent to peers",
+            "value": 63545.8,
+            "unit": "KiB"
+          },
+          {
+            "name": "Received from peers",
+            "value": 52942.2,
+            "unit": "KiB"
+          },
+          {
+            "name": "approval-distribution",
+            "value": 8.054261614340039,
+            "unit": "seconds"
+          },
+          {
+            "name": "test-environment",
+            "value": 3.6903176934502717,
+            "unit": "seconds"
+          },
+          {
+            "name": "approval-voting",
+            "value": 10.606826697999894,
             "unit": "seconds"
           }
         ]
