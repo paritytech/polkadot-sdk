353c56a5bb59a5c5aad11b1c16b4aa4602d31c7d
alindima <alin@parity.io>
2024-06-03 10:29:40 +0300

    some review comments

--- docs/sdk/src/guides/enable_elastic_scaling_mvp.rs
+++ docs/sdk/src/guides/enable_elastic_scaling_mvp.rs
@@ -7,8 +7,8 @@
 //! ## Quick introduction to elastic scaling
 //!
 //! [Elastic scaling](https://polkadot.network/blog/elastic-scaling-streamling-growth-on-polkadot)
-//! is a feature that will enable parachains to seamlessly scale up or down the amount of block
-//! validated and backed in a single relay chain block, in order to keep up with demand.
+//! is a feature that will enable parachains to seamlessly scale up/down their block space usage in
+//! order to increase throughput or lower their latency.
 //!
 //! At present, with Asynchronous Backing enabled, a parachain can only include a block on the relay
 //! chain every 6 seconds, irregardless of how many cores the parachain acquires. Elastic scaling
@@ -21,21 +21,26 @@
 //! still [work in progress](https://github.com/paritytech/polkadot-sdk/issues/1829).
 //! Below are described the current limitations of the MVP:
 //!
-//! 1. **A parachain can use at most 3 cores at a time.** This limitation stems from the fact that
-//!    every parablock has an execution timeout of 2 seconds and the relay chain block authoring
-//!    takes 6 seconds. Therefore, assuming parablock authoring is sequential, a collator only has
-//!    enough time to build 3 candidates in a relay chain slot.
-//! 2. **Collator set must be of size 1 for consistently scaling beyond a core at full authorship
-//!    duration of 2 seconds per block.** In practice, parablock authorship takes about the same
-//!    amount of time as parablock import. Therefore, each collator must first import the previous
-//!    block before authoring a new one. For a core count of 3, this would amount to 12 seconds and
-//!    for a core count of 2, 8 seconds.
+//! 1. **Limited core count**. Parachain block authoring is sequential, so the second block will
+//!    start being built only after the previous block is imported. The current block production is
+//!    capped at 2 seconds of execution. Therefore, assuming the full 2 seconds are used, a
+//!    parachain can only utilise at most 3 cores in a relay chain slot of 6 seconds. If the full
+//!    execution time is not being used, higher core counts can be achieved.
+//! 2. **Single collator requirement for consistently scaling beyond a core at full authorship
+//!    duration of 2 seconds per block.** Using the current implementation with multiple collators
+//!    adds additional latency to the block production pipeline. Assuming block execution takes
+//!    about the same as authorship, the additional overhead is equal the duration of the authorship
+//!    plus the block announcement. Since each collator must first import the previous block before
+//!    authoring a new one, this would amount to 12 seconds for 3 cores and 8 seconds for 2 cores.
 //! 3. **Trusted collator set.** The collator set needs to be trusted until there’s a mitigation
 //!    that would prevent or deter multiple collators from submitting the same collation.
+//!    A solution is being discussed [here](https://github.com/polkadot-fellows/RFCs/issues/92).
 //! 4. **Fixed scaling.** For true elasticity, the parachain must be able to seamlessly acquire or
 //!    sell coretime as the user demand grows and shrinks over time, in an automated manner. This is
 //!    currently lacking - a parachain can only scale up or down by “manually” acquiring coretime.
-//!    We call this fixed scaling.
+//!    This is not in the scope of the relay chain functionality. Parachains can already start
+//!    implementing such autoscaling, but we aim to provide a framework/examples for developing
+//!    autoscaling strategies.
 //!
 //! ## Using elastic scaling MVP
 //!
@@ -43,12 +48,13 @@
 //!
 //! - Ensure Asynchronous Backing is enabled on the network and you have enabled it on the parachain
 //!   using [this guide](https://wiki.polkadot.network/docs/maintain-guides-async-backing).
-//! - Ensure the `AsyncBackingParams.max_candidate_depth` value is configured as 6 on the relay
-//!   chain (which will allow a max velocity of 3 for each parachain).
-//! - Have a trusted collator set for the parachain (of size 1 for full throughput)
-//! - The parachain has bought coretime for one or more cores (up to three) and is scheduled on the
-//!   relay chain.
-//! - Use the latest cumulus release, which includes the necessary elastic scaling changes
+//! - Ensure the `AsyncBackingParams.max_candidate_depth` value is configured to a value that is at
+//!   least double the maximum targeted parachain velocity. For example, if the parachain will build
+//!   at most 3 candidates per relay chain block, the `max_candidate_depth` should be at least 6.
+//! - Use a trusted single collator for maximum throughput.
+//! - Ensure enough coretime is assigned to the parachain. For maximum throughput the upper bound is
+//!   3 cores.
+//! - Use the latest cumulus release, which includes the necessary elastic scaling changes.
 //!
 //! The following steps assume using the cumulus parachain template.
 //!
@@ -97,9 +103,9 @@
 //! This phase consists of a couple of changes needed to be made to the parachain’s runtime in order
 //! to utilise fixed factor scaling.
 //!
-//! First of all, you need to decide how many parachain blocks you need to produce per relay chain
-//! block (in direct correlation with the number of acquired cores). This should be either 1 (no
-//! scaling), 2 or 3. This is called the parachain velocity.
+//! First of all, you need to decide the upper limit to how many parachain blocks you need to
+//! produce per relay chain block (in direct correlation with the number of acquired cores). This
+//! should be either 1 (no scaling), 2 or 3. This is called the parachain velocity.
 //!
 //! If you configure a velocity which is different from the number of assigned cores, the measured
 //! velocity in practice will be the minimum of these two.
@@ -119,17 +125,18 @@
 //!      const BLOCK_PROCESSING_VELOCITY: u32 = 3;
 //!      ```
 //!
-//! 2. Decrease the `MILLISECS_PER_BLOCK` to the desired value. In this example, 2000.
+//! 2. Set the `MILLISECS_PER_BLOCK` to the desired value.
 //!
 //!      ```rust
-//!      const MILLISECS_PER_BLOCK: u32 = 2000;
+//!      const MILLISECS_PER_BLOCK: u32 =
+//!          RELAY_CHAIN_SLOT_DURATION_MILLIS / BLOCK_PROCESSING_VELOCITY;
 //!      ```
 //!     Note: for a parachain which measures time in terms of its own block number, changing block
 //!     time may cause complications, requiring additional changes.  See the section ["Timing by
 //!     block number" of the async backing guide](https://wiki.polkadot.network/docs/maintain-guides-async-backing#timing-by-block-number).
 //!
-//! 3. Increase the `UNINCLUDED_SEGMENT_CAPACITY` to the desired value. In this example, 7.
+//! 3. Increase the `UNINCLUDED_SEGMENT_CAPACITY` to the desired value.
 //!
 //!     ```rust
-//!     const UNINCLUDED_SEGMENT_CAPACITY: u32 = 7;
+//!     const UNINCLUDED_SEGMENT_CAPACITY: u32 = 2 * BLOCK_PROCESSING_VELOCITY + 1;
 //!     ```
