7a155de51374956e2266ed1b70a03401d78c4606
github-action-benchmark <github@users.noreply.github.com>
2024-06-04 19:37:36 +0000

    add approval-voting-regression-bench (customSmallerIsBetter) benchmark result for 42ddb5b06578f6c2b2fc469de5161035b04fc79a

--- bench/approval-voting-regression-bench/data.js
+++ bench/approval-voting-regression-bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1717513253402,
+  "lastUpdate": 1717529855580,
   "repoUrl": "https://github.com/paritytech/polkadot-sdk",
   "entries": {
     "approval-voting-regression-bench": [
@@ -6344,6 +6344,53 @@ window.BENCHMARK_DATA = {
           {
             "name": "test-environment",
             "value": 3.2672337660501554,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Michal Kucharczyk",
+            "username": "michalkucharczyk",
+            "email": "1728078+michalkucharczyk@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "42ddb5b06578f6c2b2fc469de5161035b04fc79a",
+          "message": "`chain-spec`/presets reference docs added (#4678)\n\nAdded reference doc about:\n- the pallet genesis config and genesis build, \n- runtime `genesis-builder` API,\n- presets,\n- interacting with the `chain-spec-builder` tool\n\nI've added [minimal\nruntime](https://github.com/paritytech/polkadot-sdk/tree/mku-chain-spec-guide/docs/sdk/src/reference_docs/chain_spec_runtime)\nto demonstrate above topics.\n\nI also sneaked in some little improvement to `chain-spec-builder` which\nallows to parse output of the `list-presets` command.\n\n---------\n\nCo-authored-by: Alexandru Vasile <60601340+lexnv@users.noreply.github.com>\nCo-authored-by: Sebastian Kunert <skunert49@gmail.com>",
+          "timestamp": "2024-06-04T18:04:51Z",
+          "url": "https://github.com/paritytech/polkadot-sdk/commit/42ddb5b06578f6c2b2fc469de5161035b04fc79a"
+        },
+        "date": 1717529828281,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Sent to peers",
+            "value": 63546.02,
+            "unit": "KiB"
+          },
+          {
+            "name": "Received from peers",
+            "value": 52938.40000000001,
+            "unit": "KiB"
+          },
+          {
+            "name": "test-environment",
+            "value": 3.299292141040168,
+            "unit": "seconds"
+          },
+          {
+            "name": "approval-voting",
+            "value": 9.734819543859954,
+            "unit": "seconds"
+          },
+          {
+            "name": "approval-distribution",
+            "value": 7.288513080389859,
             "unit": "seconds"
           }
         ]
