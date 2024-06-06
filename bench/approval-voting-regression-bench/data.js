9a3e83bc94f4a1c4a0e94e60218ffa4fe0233011
github-action-benchmark <github@users.noreply.github.com>
2024-06-06 20:11:59 +0000

    add approval-voting-regression-bench (customSmallerIsBetter) benchmark result for 494448b7fed02e098fbf38bad517d9245b056d1d

--- bench/approval-voting-regression-bench/data.js
+++ bench/approval-voting-regression-bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1717691341804,
+  "lastUpdate": 1717704718707,
   "repoUrl": "https://github.com/paritytech/polkadot-sdk",
   "entries": {
     "approval-voting-regression-bench": [
@@ -6673,6 +6673,53 @@ window.BENCHMARK_DATA = {
           {
             "name": "approval-voting",
             "value": 9.890217099679903,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Andrei Eres",
+            "username": "AndreiEres",
+            "email": "eresav@me.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "494448b7fed02e098fbf38bad517d9245b056d1d",
+          "message": "Cleanup PVF artifact by cache limit and stale time (#4662)\n\nPart of https://github.com/paritytech/polkadot-sdk/issues/4324\nWe don't change but extend the existing cleanup strategy. \n- We still don't touch artifacts being stale less than 24h\n- First time we attempt pruning only when we hit cache limit (10 GB)\n- If somehow happened that after we hit 10 GB and least used artifact is\nstale less than 24h we don't remove it.\n\n---------\n\nCo-authored-by: s0me0ne-unkn0wn <48632512+s0me0ne-unkn0wn@users.noreply.github.com>\nCo-authored-by: Andrei Sandu <54316454+sandreim@users.noreply.github.com>",
+          "timestamp": "2024-06-06T19:22:22Z",
+          "url": "https://github.com/paritytech/polkadot-sdk/commit/494448b7fed02e098fbf38bad517d9245b056d1d"
+        },
+        "date": 1717704689661,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Sent to peers",
+            "value": 63539.340000000004,
+            "unit": "KiB"
+          },
+          {
+            "name": "Received from peers",
+            "value": 52943.8,
+            "unit": "KiB"
+          },
+          {
+            "name": "test-environment",
+            "value": 3.0147633474300837,
+            "unit": "seconds"
+          },
+          {
+            "name": "approval-distribution",
+            "value": 6.924857882679956,
+            "unit": "seconds"
+          },
+          {
+            "name": "approval-voting",
+            "value": 9.241718071089954,
             "unit": "seconds"
           }
         ]
