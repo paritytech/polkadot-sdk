2f98550b34710f78dcf68248fb5c28d991370223
kianenigma <kian@parity.io>
2024-06-07 14:33:39 +0800

    remove unused

--- substrate/frame/system/benches/bench.rs
+++ substrate/frame/system/benches/bench.rs
@@ -16,12 +16,8 @@
 // limitations under the License.
 
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use frame_support::{derive_impl, traits::ConstU32};
-use sp_core::H256;
-use sp_runtime::{
-	traits::{BlakeTwo256, IdentityLookup},
-	BuildStorage, Perbill,
-};
+use frame_support::derive_impl;
+use sp_runtime::{BuildStorage, Perbill};
 #[frame_support::pallet]
 mod module {
 	use frame_support::pallet_prelude::*;
