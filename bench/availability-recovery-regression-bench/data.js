6afebf96465e213336a5c0cde38e3143db250315
github-action-benchmark <github@users.noreply.github.com>
2024-06-08 09:28:57 +0000

    add availability-recovery-regression-bench (customSmallerIsBetter) benchmark result for 07cfcf0b3c9df971c673162b9d16cb5c17fbe97d

--- bench/availability-recovery-regression-bench/data.js
+++ bench/availability-recovery-regression-bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1717777717731,
+  "lastUpdate": 1717838937324,
   "repoUrl": "https://github.com/paritytech/polkadot-sdk",
   "entries": {
     "availability-recovery-regression-bench": [
@@ -10919,6 +10919,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "test-environment",
             "value": 0.19022879846666668,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Alexandru Vasile",
+            "username": "lexnv",
+            "email": "60601340+lexnv@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "07cfcf0b3c9df971c673162b9d16cb5c17fbe97d",
+          "message": "frame/proc-macro: Refactor code for better readability (#4712)\n\nSmall refactoring PR to improve the readability of the proc macros.\n- small improvement in docs\n- use new `let Some(..) else` expression\n- removed extra indentations by early returns\n\nDiscovered during metadata v16 poc, extracted from:\nhttps://github.com/paritytech/polkadot-sdk/pull/4358\n\n---------\n\nSigned-off-by: Alexandru Vasile <alexandru.vasile@parity.io>\nCo-authored-by: Bastian Köcher <git@kchr.de>\nCo-authored-by: command-bot <>\nCo-authored-by: gupnik <mail.guptanikhil@gmail.com>",
+          "timestamp": "2024-06-08T07:48:42Z",
+          "url": "https://github.com/paritytech/polkadot-sdk/commit/07cfcf0b3c9df971c673162b9d16cb5c17fbe97d"
+        },
+        "date": 1717838909383,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Sent to peers",
+            "value": 1.6666666666666665,
+            "unit": "KiB"
+          },
+          {
+            "name": "Received from peers",
+            "value": 307203,
+            "unit": "KiB"
+          },
+          {
+            "name": "availability-recovery",
+            "value": 12.841941453799999,
+            "unit": "seconds"
+          },
+          {
+            "name": "test-environment",
+            "value": 0.1814288942666667,
             "unit": "seconds"
           }
         ]
