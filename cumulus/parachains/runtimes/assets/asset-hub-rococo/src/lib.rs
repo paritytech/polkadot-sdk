8c8edacf8942298c3807a2e192860da9e7e4996a
EgorPopelyaev <egor@parity.io>
2024-06-10 14:12:10 +0200

    Bump spec_version

--- cumulus/parachains/runtimes/assets/asset-hub-rococo/src/lib.rs
+++ cumulus/parachains/runtimes/assets/asset-hub-rococo/src/lib.rs
@@ -112,7 +112,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("statemine"),
 	impl_name: create_runtime_str!("statemine"),
 	authoring_version: 1,
-	spec_version: 1_011_000,
+	spec_version: 1_011_001,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 16,
