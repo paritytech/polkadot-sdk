a725bbd71e12c4a5ea7d376eee133a21ff419f2c
Daniel Shiposha <mrshiposha@gmail.com>
2024-06-11 23:43:36 +0200

    fix: add derives to NonFungibleAsset

--- polkadot/xcm/xcm-builder/src/unique_instances/mod.rs
+++ polkadot/xcm/xcm-builder/src/unique_instances/mod.rs
@@ -1,3 +1,5 @@
+use parity_scale_codec::{Decode, Encode, MaxEncodedLen};
+use scale_info::TypeInfo;
 use xcm::latest::prelude::*;
 
 pub mod adapter;
@@ -8,6 +10,7 @@ pub use adapter::*;
 pub use derivatives::*;
 pub use ops::*;
 
+#[derive(Clone, Eq, PartialEq, Ord, PartialOrd, Debug, Encode, Decode, TypeInfo, MaxEncodedLen)]
 pub struct NonFungibleAsset {
 	pub id: AssetId,
 	pub instance: AssetInstance,
