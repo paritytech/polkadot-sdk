765ca1f50fd02f07c31a45405295ef3de884c357
Bastian Köcher <info@kchr.de>
2024-06-17 15:58:01 +0200

    Adds missing license

--- substrate/client/service/src/client/code_provider.rs
+++ substrate/client/service/src/client/code_provider.rs
@@ -1,3 +1,21 @@
+// This file is part of Substrate.
+
+// Copyright (C) Parity Technologies (UK) Ltd.
+// SPDX-License-Identifier: GPL-3.0-or-later WITH Classpath-exception-2.0
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 use super::{client::ClientConfig, wasm_override::WasmOverride, wasm_substitutes::WasmSubstitutes};
 use sc_client_api::backend;
 use sc_executor::{RuntimeVersion, RuntimeVersionOf};
