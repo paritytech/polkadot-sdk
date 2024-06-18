93d241ea2a327873e210eef9f016d2fa20c4324c
José Molina <jose@blockdeep.io>
2024-06-18 12:57:10 +0200

    fmt

--- substrate/frame/nfts/src/benchmarking.rs
+++ substrate/frame/nfts/src/benchmarking.rs
@@ -30,10 +30,7 @@ use frame_support::{
 	BoundedVec,
 };
 use frame_system::{pallet_prelude::BlockNumberFor, RawOrigin as SystemOrigin};
-use sp_runtime::{
-	traits::{Bounded, One},
-	AccountId32, MultiSignature,
-};
+use sp_runtime::traits::{Bounded, One};
 use sp_std::prelude::*;
 
 use crate::Pallet as Nfts;
