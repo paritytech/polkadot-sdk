c0f02178a0fe6f8364f4f407c4d85f87563e88a7
Francisco Aguirre <franciscoaguirreperez@gmail.com>
2024-06-19 12:09:48 +0800

    fix(xcm-v5): fix most compilation errors

--- polkadot/xcm/src/v4/location.rs
+++ polkadot/xcm/src/v4/location.rs
@@ -20,6 +20,7 @@ use super::{traits::Reanchorable, Junction, Junctions};
 use crate::{v3::MultiLocation as OldLocation, v5::Location as NewLocation, VersionedLocation};
 use codec::{Decode, Encode, MaxEncodedLen};
 use core::result;
+use codec::{Encode, Decode, MaxEncodedLen};
 use scale_info::TypeInfo;
 
 /// A relative path between state-bearing consensus systems.
