4dbe93f01f116d5357465aff7017bd847ea0a501
Adrian Catangiu <adrian@parity.io>
2024-06-19 15:18:57 +0300

    emulated-tests: add regression test

--- cumulus/parachains/integration-tests/emulated/tests/people/people-westend/src/tests/teleport.rs
+++ cumulus/parachains/integration-tests/emulated/tests/people/people-westend/src/tests/teleport.rs
@@ -216,7 +216,7 @@ fn limited_teleport_native_assets_back_from_system_para_to_relay_works() {
 }
 
 /// Limited Teleport of native asset from System Parachain to Relay Chain
-/// should't work when there is not enough balance in Relay Chain's `CheckAccount`
+/// shouldn't work when there is not enough balance in Relay Chain's `CheckAccount`
 #[test]
 fn limited_teleport_native_assets_from_system_para_to_relay_fails() {
 	// Init values for Relay Chain
