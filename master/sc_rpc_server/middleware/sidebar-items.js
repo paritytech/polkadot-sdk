2650606ca1f8e62b95139fa5591912ee5ec3b916
paritytech-ci <devops-team@parity.io>
2024-06-20 09:51:20 +0000

    ___Updated docs for master___

--- master/sc_rpc_server/middleware/sidebar-items.js
+++ master/sc_rpc_server/middleware/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"struct":["Metrics","Middleware","MiddlewareLayer","RateLimit","RpcMetrics"]};
+window.SIDEBAR_ITEMS = {"struct":["Metrics","Middleware","MiddlewareLayer","NodeHealthProxy","NodeHealthProxyLayer","RateLimit","RpcMetrics"]};
