e3f02ce46e0f732af84f59de214e4cbcea4af057
Niklas Adolfsson <niklasadolfsson1@gmail.com>
2024-06-21 14:47:28 +0200

    Update substrate/client/rpc-spec-v2/src/chain_head/chain_head.rs
    Co-authored-by: Bastian Köcher <git@kchr.de>

--- substrate/client/rpc-spec-v2/src/chain_head/chain_head.rs
+++ substrate/client/rpc-spec-v2/src/chain_head/chain_head.rs
@@ -583,7 +583,7 @@ where
 		let conn_id = ext
 			.get::<ConnectionId>()
 			.copied()
-			.expect("ConnectionId is always set by jsonrpsee");
+			.expect("ConnectionId is always set by jsonrpsee; qed");
 
 		if !self.subscriptions.contains_subscription(conn_id, &follow_subscription) {
 			return Ok(())
