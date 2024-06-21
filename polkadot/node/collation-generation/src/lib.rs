2bba3644772766a3f39d7f98eba6cf2f867995c6
girazoki <gorka.irazoki@gmail.com>
2024-06-21 15:33:20 +0200

    remove new messages and override config in reinitialize

--- polkadot/node/collation-generation/src/lib.rs
+++ polkadot/node/collation-generation/src/lib.rs
@@ -147,23 +147,7 @@ impl CollationGenerationSubsystem {
 			Ok(FromOrchestra::Communication {
 				msg: CollationGenerationMessage::Reinitialize(config),
 			}) => {
-				if self.config.is_none() {
-					gum::error!(target: LOG_TARGET, "no initial initialization");
-				} else {
-					self.config = Some(Arc::new(config));
-				}
-				false
-			},
-			Ok(FromOrchestra::Communication {
-				msg: CollationGenerationMessage::SetConfig(config),
-			}) => {
 				self.config = Some(Arc::new(config));
-				false
-			},
-			Ok(FromOrchestra::Communication {
-				msg: CollationGenerationMessage::ClearExistingConfig,
-			}) => {
-				self.config = None;
 				false
 			},
 			Ok(FromOrchestra::Communication {
