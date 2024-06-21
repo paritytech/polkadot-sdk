fc80004cda45aeb91258f819285c4c96d49ad149
Niklas Adolfsson <niklasadolfsson1@gmail.com>
2024-06-21 14:47:59 +0200

    Update substrate/client/rpc-spec-v2/src/transaction/transaction_broadcast.rs
    Co-authored-by: Bastian Köcher <git@kchr.de>

--- substrate/client/rpc-spec-v2/src/transaction/transaction_broadcast.rs
+++ substrate/client/rpc-spec-v2/src/transaction/transaction_broadcast.rs
@@ -250,7 +250,7 @@ where
 		let conn_id = ext
 			.get::<ConnectionId>()
 			.copied()
-			.expect("ConnectionId is always set by jsonrpsee");
+			.expect("ConnectionId is always set by jsonrpsee; qed");
 
 		// The operation ID must correlate to the same connection ID.
 		if !self.rpc_connections.contains_identifier(conn_id, &operation_id) {
