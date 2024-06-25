190ba06fb4b309875e5fdd93ed0e515f04292e90
Adrian Catangiu <adrian@parity.io>
2024-06-25 17:04:46 +0300

    Update substrate/primitives/trie/src/recorder.rs

--- substrate/primitives/trie/src/recorder.rs
+++ substrate/primitives/trie/src/recorder.rs
@@ -145,7 +145,7 @@ impl<H: Hasher> Recorder<H> {
 
 	/// Convert the recording to a [`StorageProof`].
 	///
-	/// In contrast to [`Self::drain_storage_proof`] this doesn't consume and doesn't clears the
+	/// In contrast to [`Self::drain_storage_proof`] this doesn't consume and doesn't clear the
 	/// recordings.
 	///
 	/// Returns the [`StorageProof`].
