4511ecd86929003fb0def406145a97690191e204
Branislav Kontur <bkontur@gmail.com>
2024-06-26 12:17:27 +0200

    Update cumulus/parachains/runtimes/collectives/collectives-westend/src/lib.rs
    Co-authored-by: Bastian Köcher <git@kchr.de>

--- cumulus/parachains/runtimes/collectives/collectives-westend/src/lib.rs
+++ cumulus/parachains/runtimes/collectives/collectives-westend/src/lib.rs
@@ -163,7 +163,7 @@ parameter_types! {
 		})
 		.avg_block_initialization(AVERAGE_ON_INITIALIZE_RATIO)
 		.build_or_panic();
-	pub const SS58Prefix: u8 = 0;
+	pub const SS58Prefix: u8 = 42;
 }
 
 // Configure FRAME pallets to include in runtime.
