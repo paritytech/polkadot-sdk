45855287b8647f34a4b3015facc714232c2ebe3e
Daniel Shiposha <mrshiposha@gmail.com>
2024-06-27 14:06:47 +0200

    chore: cargo fmt

--- polkadot/xcm/xcm-builder/src/unique_instances/mod.rs
+++ polkadot/xcm/xcm-builder/src/unique_instances/mod.rs
@@ -1,5 +1,6 @@
 //! XCM utilities to work with NFT-like entities (unique instances).
-//! The adapters and other utility types use the [`asset_ops`](frame_support::traits::tokens::asset_ops) traits.
+//! The adapters and other utility types use the
+//! [`asset_ops`](frame_support::traits::tokens::asset_ops) traits.
 
 use xcm::latest::prelude::*;
 
