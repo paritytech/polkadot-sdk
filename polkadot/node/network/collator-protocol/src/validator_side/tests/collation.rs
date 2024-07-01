c07890b0bef348188ab58c7022c1f0109157a40b
Tsvetomir Dimitrov <tsvetomir@parity.io>
2024-07-01 14:54:07 +0300

    Remove unnecessary clone

--- polkadot/node/network/collator-protocol/src/validator_side/tests/collation.rs
+++ polkadot/node/network/collator-protocol/src/validator_side/tests/collation.rs
@@ -9,35 +9,35 @@ use super::Collations;
 fn cant_add_more_than_claim_queue() {
 	let para_a = ParaId::from(1);
 	let para_b = ParaId::from(2);
-	let assignments = vec![para_a.clone(), para_b.clone(), para_a.clone()];
+	let assignments = vec![para_a, para_b, para_a];
 	let relay_parent_mode =
 		ProspectiveParachainsMode::Enabled { max_candidate_depth: 4, allowed_ancestry_len: 3 };
 
 	let mut collations = Collations::new(&assignments);
 
 	// first collation for `para_a` is in the limit
-	assert!(!collations.is_collations_limit_reached(relay_parent_mode, para_a.clone(), 0));
-	collations.note_fetched(para_a.clone());
+	assert!(!collations.is_collations_limit_reached(relay_parent_mode, para_a, 0));
+	collations.note_fetched(para_a);
 	// and `para_b` is not affected
-	assert!(!collations.is_collations_limit_reached(relay_parent_mode, para_b.clone(), 0));
+	assert!(!collations.is_collations_limit_reached(relay_parent_mode, para_b, 0));
 
 	// second collation for `para_a` is also in the limit
-	assert!(!collations.is_collations_limit_reached(relay_parent_mode, para_a.clone(), 0));
-	collations.note_fetched(para_a.clone());
+	assert!(!collations.is_collations_limit_reached(relay_parent_mode, para_a, 0));
+	collations.note_fetched(para_a);
 
 	// `para_b`` is still not affected
-	assert!(!collations.is_collations_limit_reached(relay_parent_mode, para_b.clone(), 0));
+	assert!(!collations.is_collations_limit_reached(relay_parent_mode, para_b, 0));
 
 	// third collation for `para_a`` will be above the limit
-	assert!(collations.is_collations_limit_reached(relay_parent_mode, para_a.clone(), 0));
+	assert!(collations.is_collations_limit_reached(relay_parent_mode, para_a, 0));
 
 	// one fetch for b
-	assert!(!collations.is_collations_limit_reached(relay_parent_mode, para_b.clone(), 0));
-	collations.note_fetched(para_b.clone());
+	assert!(!collations.is_collations_limit_reached(relay_parent_mode, para_b, 0));
+	collations.note_fetched(para_b);
 
 	// and now both paras are over limit
-	assert!(collations.is_collations_limit_reached(relay_parent_mode, para_a.clone(), 0));
-	assert!(collations.is_collations_limit_reached(relay_parent_mode, para_b.clone(), 0));
+	assert!(collations.is_collations_limit_reached(relay_parent_mode, para_a, 0));
+	assert!(collations.is_collations_limit_reached(relay_parent_mode, para_b, 0));
 }
 
 #[test]
@@ -45,7 +45,7 @@ fn pending_fetches_are_counted() {
 	let para_a = ParaId::from(1);
 	let collator_id_a = CollatorId::from(sr25519::Public::from_raw([10u8; 32]));
 	let para_b = ParaId::from(2);
-	let assignments = vec![para_a.clone(), para_b.clone(), para_a.clone()];
+	let assignments = vec![para_a, para_b, para_a];
 	let relay_parent_mode =
 		ProspectiveParachainsMode::Enabled { max_candidate_depth: 4, allowed_ancestry_len: 3 };
 
@@ -53,9 +53,9 @@ fn pending_fetches_are_counted() {
 	collations.fetching_from = Some((collator_id_a, None));
 
 	// first collation for `para_a` is in the limit
-	assert!(!collations.is_collations_limit_reached(relay_parent_mode, para_a.clone(), 1));
-	collations.note_fetched(para_a.clone());
+	assert!(!collations.is_collations_limit_reached(relay_parent_mode, para_a, 1));
+	collations.note_fetched(para_a);
 
 	// second collation for `para_a`` is not in the limit due to the pending fetch
-	assert!(collations.is_collations_limit_reached(relay_parent_mode, para_a.clone(), 1));
+	assert!(collations.is_collations_limit_reached(relay_parent_mode, para_a, 1));
 }
