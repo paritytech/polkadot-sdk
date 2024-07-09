28ff9f5a5f18e9b968077b70f252a84fcf7dbfbc
github-action-benchmark <github@users.noreply.github.com>
2024-07-09 10:15:55 +0000

    add approval-voting-regression-bench (customSmallerIsBetter) benchmark result for 01e0fc23d8844461adcd4501815dac64f3a4986f

--- bench/approval-voting-regression-bench/data.js
+++ bench/approval-voting-regression-bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1720502372973,
+  "lastUpdate": 1720520154270,
   "repoUrl": "https://github.com/paritytech/polkadot-sdk",
   "entries": {
     "approval-voting-regression-bench": [
@@ -10057,6 +10057,53 @@ window.BENCHMARK_DATA = {
           {
             "name": "test-environment",
             "value": 2.9940491189501874,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Serban Iorga",
+            "username": "serban300",
+            "email": "serban@parity.io"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "01e0fc23d8844461adcd4501815dac64f3a4986f",
+          "message": "`polkadot-parachain` simplifications and deduplications (#4916)\n\n`polkadot-parachain` simplifications and deduplications\n\nDetails in the commit messages. Just copy-pasting the last commit\ndescription since it introduces the biggest changes:\n\n```\n    Implement a more structured way to define a node spec\n    \n    - use traits instead of bounds for `rpc_ext_builder()`,\n      `build_import_queue()`, `start_consensus()`\n    - add a `NodeSpec` trait for defining the specifications of a node\n    - deduplicate the code related to building a node's components /\n      starting a node\n```\n\nThe other changes are much smaller, most of them trivial and are\nisolated in separate commits.",
+          "timestamp": "2024-07-09T08:30:48Z",
+          "url": "https://github.com/paritytech/polkadot-sdk/commit/01e0fc23d8844461adcd4501815dac64f3a4986f"
+        },
+        "date": 1720520125420,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Received from peers",
+            "value": 52937.59999999999,
+            "unit": "KiB"
+          },
+          {
+            "name": "Sent to peers",
+            "value": 63778.3,
+            "unit": "KiB"
+          },
+          {
+            "name": "approval-distribution",
+            "value": 7.001450311909932,
+            "unit": "seconds"
+          },
+          {
+            "name": "test-environment",
+            "value": 3.1312336581301765,
+            "unit": "seconds"
+          },
+          {
+            "name": "approval-voting",
+            "value": 9.763205755850091,
             "unit": "seconds"
           }
         ]
