6cdb77e3ebea01875bdf43758bc3929a6bb1421d
github-action-benchmark <github@users.noreply.github.com>
2024-07-09 05:19:33 +0000

    add approval-voting-regression-bench (customSmallerIsBetter) benchmark result for 72dab6d897d9519881b619d466ef5be3c7df9a34

--- bench/approval-voting-regression-bench/data.js
+++ bench/approval-voting-regression-bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1720461601340,
+  "lastUpdate": 1720502372973,
   "repoUrl": "https://github.com/paritytech/polkadot-sdk",
   "entries": {
     "approval-voting-regression-bench": [
@@ -10010,6 +10010,53 @@ window.BENCHMARK_DATA = {
           {
             "name": "approval-voting",
             "value": 9.497530255079953,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Or Grinberg",
+            "username": "orgr",
+            "email": "or.grin@gmail.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "72dab6d897d9519881b619d466ef5be3c7df9a34",
+          "message": "allow clear_origin in safe xcm builder (#4777)\n\nFixes #3770 \n\nAdded `clear_origin` as an allowed command after commands that load the\nholdings register, in the safe xcm builder.\n\nChecklist\n- [x] My PR includes a detailed description as outlined in the\n\"Description\" section above\n- [x] My PR follows the [labeling\nrequirements](https://github.com/paritytech/polkadot-sdk/blob/master/docs/contributor/CONTRIBUTING.md#Process)\nof this project (at minimum one label for T required)\n- [x] I have made corresponding changes to the documentation (if\napplicable)\n- [x] I have added tests that prove my fix is effective or that my\nfeature works (if applicable)\n\n---------\n\nCo-authored-by: Francisco Aguirre <franciscoaguirreperez@gmail.com>\nCo-authored-by: gupnik <mail.guptanikhil@gmail.com>",
+          "timestamp": "2024-07-09T03:40:39Z",
+          "url": "https://github.com/paritytech/polkadot-sdk/commit/72dab6d897d9519881b619d466ef5be3c7df9a34"
+        },
+        "date": 1720502343844,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Sent to peers",
+            "value": 63797.659999999996,
+            "unit": "KiB"
+          },
+          {
+            "name": "Received from peers",
+            "value": 52941,
+            "unit": "KiB"
+          },
+          {
+            "name": "approval-voting",
+            "value": 9.539204766300012,
+            "unit": "seconds"
+          },
+          {
+            "name": "approval-distribution",
+            "value": 6.838206757860023,
+            "unit": "seconds"
+          },
+          {
+            "name": "test-environment",
+            "value": 2.9940491189501874,
             "unit": "seconds"
           }
         ]
