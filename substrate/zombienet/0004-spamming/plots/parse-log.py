8bad061e1c405e5dc886749328f8b3dfe6d0a700
Michal Kucharczyk <1728078+michalkucharczyk@users.noreply.github.com>
2024-07-11 12:20:28 +0200

    tool: parse-log improvement

--- substrate/zombienet/0004-spamming/plots/parse-log.py
+++ substrate/zombienet/0004-spamming/plots/parse-log.py
@@ -2,6 +2,13 @@
 import re
 import sys
 import os
+import subprocess
+
+def extract_time_point(command, file_path):
+    result = subprocess.run(command, shell=True, capture_output=True, text=True)
+    time_point = result.stdout.strip()
+    with open(file_path, 'w') as f:
+        f.write(time_point)
 
 def convert_to_microseconds(value, unit):
     if unit == 'µs':
@@ -78,8 +85,8 @@ def main():
         },
         {
             "type": "import",
-            "regex": r'(\d{4}-\d{2}-\d{2}) (\d{2}:\d{2}:\d{2}\.\d{3})  INFO.* Imported #(\d+) \((0x[a-f0-9…]+) → (0x[a-f0-9…]+)\)',
-            "guard": "Imported",
+            "regex": r'(\d{4}-\d{2}-\d{2}) (\d{2}:\d{2}:\d{2}(?:\.\d{3})?).* Imported #(\d+) \((0x[a-f0-9…]+) → (0x[a-f0-9…]+)\)',
+            "guard": "Imported #",
             "column_names": ["date", "time", "block_number"],
             "extract_data": lambda match: (
                     match.group(1),
@@ -89,7 +96,7 @@ def main():
         },
         {
             "type": "txpool_maintain",
-            "regex": "(\d{4}-\d{2}-\d{2}) (\d{2}:\d{2}:\d{2}\.\d{3})  INFO.*txpool:.*maintain: txs:\((\d+), (\d+)\) views:\[(\d+);.*\] event:(NewBestBlock|Finalized) {.*}  took:(\d+\.\d+)([µms]+)",
+            "regex": "(\d{4}-\d{2}-\d{2}) (\d{2}:\d{2}:\d{2}(?:\.\d{3})?).*maintain: txs:\((\d+), (\d+)\) views:\[(\d+);.*\] event:(NewBestBlock|Finalized) {.*}  took:(\d+\.\d+)([µms]+)",
             "guard": "maintain: txs:",
             "column_names": ["date", "time", "unwatched_txs", "watched_txs", "views_count", "event", "duration"],
             "extract_data": lambda match: (
@@ -126,7 +133,7 @@ def main():
         },
         {
             "type": "block_proposing",
-            "regex": r'^(\d{4}-\d{2}-\d{2}) (\d{2}:\d{2}:\d{2}\.\d{3})  INFO.*sc_basic_authorship::basic_authorship:.*🎁 Prepared block for proposing at \d+ \(\d+ ms\).* extrinsics \((\d+)\)',
+            "regex": r'^(\d{4}-\d{2}-\d{2}) (\d{2}:\d{2}:\d{2}(?:\.\d{3})?).*🎁 Prepared block for proposing at \d+ \(\d+ ms\).* extrinsics_count: (\d+)',
             "guard": "Prepared block for proposing",
             "column_names": ["date", "time", "extrinsics_count"],
             "extract_data": lambda match: (
@@ -137,7 +144,7 @@ def main():
         },
         {
             "type": "block_proposing_start",
-            "regex": r'^(\d{4}-\d{2}-\d{2}) (\d{2}:\d{2}:\d{2}\.\d{3})  INFO.*Starting consensus session on top of parent.*',
+            "regex": r'^(\d{4}-\d{2}-\d{2}) (\d{2}:\d{2}:\d{2}(?:\.\d{3})?).*Starting consensus session on top of parent.*',
             "guard": "Starting consensus session on top of parent",
             "column_names": ["date", "time", "value"],
             "extract_data": lambda match: (
@@ -166,6 +173,17 @@ def main():
     parsed_data = parse_log_file(patterns, log_file_path)
     for key, value in parsed_data.items():
         save_parsed_data(value['data'], value['columns'], f"{output_dir}/{key}.csv")
+
+    start_file = f"{output_dir}/../start"
+    end_file = f"{output_dir}/../end"
+
+    timestamp_command = f"grep 'INFO.*maintain' {log_file_path} | head -n 1 | cut -f2 -d' ' | cut -f1 -d'.'"
+    if not os.path.isfile(start_file):
+        extract_time_point(timestamp_command, start_file)
+
+    timestamp_command = f"grep 'INFO.*maintain' {log_file_path} | tail -n 1 | cut -f2 -d' ' | cut -f1 -d'.'"
+    if not os.path.isfile(end_file):
+        extract_time_point(timestamp_command, end_file)
 
 if __name__ == "__main__":
     main()
