f7e2b484b1554bd8d0cfd96a536b46a9577102ab
Gonçalo Pestana <g6pestana@gmail.com>
2024-07-17 20:12:18 +0200

    Gates log::error call in timestamp pallet against non-std

--- substrate/frame/timestamp/src/lib.rs
+++ substrate/frame/timestamp/src/lib.rs
@@ -368,6 +368,7 @@ impl<T: Config> UnixTime for Pallet<T> {
 		// `sp_timestamp::InherentDataProvider`.
 		let now = Now::<T>::get();
 
+		#[cfg(not(feature = "std"))]
 		log::error!(
 			target: "runtime::timestamp",
 			"`pallet_timestamp::UnixTime::now` is called at genesis, invalid value returned: 0",
