822629bcbc5154cc4663ce0269cdb3a5057b4db5
Evgeny Snitko <evgeny@parity.io>
2024-07-19 03:44:20 +0400

    try nextest -p

---
+++ .github/scripts/group-crates.py
@@ -0,0 +1,21 @@
+import subprocess, sys
+
+# Get all crates
+output = subprocess.check_output(["cargo", "tree", "--locked", "--workspace", "--depth", "0", "--prefix", "none"])
+
+# Convert the output into a proper list
+crates = []
+for line in output.splitlines():
+	if line != b"":
+		line = line.decode('utf8').split(" ")
+		crate_name = line[0]
+		# The crate path is always the last element in the line.
+		crate_path = line[len(line) - 1].replace("(", "").replace(")", "")
+		crates.append(crate_name)
+
+# Make the list unique and sorted
+crates = list(set(crates))
+crates.sort()
+
+part = crates[:len(crates)//5]
+print( ','.join(part) )
