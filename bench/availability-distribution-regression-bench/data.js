8daa534ac894c000d1111901bc9f67a147927e1b
github-action-benchmark <github@users.noreply.github.com>
2024-07-18 21:29:06 +0000

    add availability-distribution-regression-bench (customSmallerIsBetter) benchmark result for ad9804f7b1e707d5144bcdc14b67c19db2975da8

--- bench/availability-distribution-regression-bench/data.js
+++ bench/availability-distribution-regression-bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1721317371444,
+  "lastUpdate": 1721338146481,
   "repoUrl": "https://github.com/paritytech/polkadot-sdk",
   "entries": {
     "availability-distribution-regression-bench": [
@@ -18251,6 +18251,58 @@ window.BENCHMARK_DATA = {
           {
             "name": "test-environment",
             "value": 0.008789509753333343,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Juan Ignacio Rios",
+            "username": "JuaniRios",
+            "email": "54085674+JuaniRios@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "ad9804f7b1e707d5144bcdc14b67c19db2975da8",
+          "message": "Add `pub` to xcm::v4::PalletInfo (#4976)\n\nv3 PalletInfo had the fields public, but not v4. Any reason why?\nI need the PalletInfo fields public so I can read the values and do some\nlogic based on that at Polimec\n@franciscoaguirre \n\nIf this could be backported would be highly appreciated 🙏🏻\n\n---------\n\nCo-authored-by: Adrian Catangiu <adrian@parity.io>\nCo-authored-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>",
+          "timestamp": "2024-07-18T19:35:00Z",
+          "url": "https://github.com/paritytech/polkadot-sdk/commit/ad9804f7b1e707d5144bcdc14b67c19db2975da8"
+        },
+        "date": 1721338115883,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Received from peers",
+            "value": 433.3333333333332,
+            "unit": "KiB"
+          },
+          {
+            "name": "Sent to peers",
+            "value": 18481.666666666653,
+            "unit": "KiB"
+          },
+          {
+            "name": "test-environment",
+            "value": 0.00949206249333334,
+            "unit": "seconds"
+          },
+          {
+            "name": "bitfield-distribution",
+            "value": 0.021862867433333332,
+            "unit": "seconds"
+          },
+          {
+            "name": "availability-distribution",
+            "value": 0.01211899554,
+            "unit": "seconds"
+          },
+          {
+            "name": "availability-store",
+            "value": 0.16988666838666663,
             "unit": "seconds"
           }
         ]
