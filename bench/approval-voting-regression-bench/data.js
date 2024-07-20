2efbceb03d53dfea8e39a1dd9ceca8941ed2244a
github-action-benchmark <github@users.noreply.github.com>
2024-07-20 00:48:58 +0000

    add approval-voting-regression-bench (customSmallerIsBetter) benchmark result for 59e3315f7fd74b95e08e6409d1b3f53f6c2123f4

--- bench/approval-voting-regression-bench/data.js
+++ bench/approval-voting-regression-bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1721430558919,
+  "lastUpdate": 1721436538587,
   "repoUrl": "https://github.com/paritytech/polkadot-sdk",
   "entries": {
     "approval-voting-regression-bench": [
@@ -11467,6 +11467,53 @@ window.BENCHMARK_DATA = {
           {
             "name": "test-environment",
             "value": 3.178613848510085,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Parth Mittal",
+            "username": "mittal-parth",
+            "email": "76661350+mittal-parth@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "59e3315f7fd74b95e08e6409d1b3f53f6c2123f4",
+          "message": "Balances Pallet: Emit events when TI is updated in currency impl (#4936)\n\n# Description\n\nPreviously, in the `Currency` impl, the implementation of\n`pallet_balances` was not emitting any instances of `Issued` and\n`Rescinded` events, even though the `Fungible` equivalent was.\n\nThis PR adds the `Issued` and `Rescinded` events in appropriate places\nin `impl_currency` along with tests.\n\nCloses #4028 \n\npolkadot address: 5GsLutpKjbzsbTphebs9Uy4YK6gTN47MAaz6njPktidjR5cp\n\n---------\n\nCo-authored-by: Bastian Köcher <git@kchr.de>\nCo-authored-by: Bastian Köcher <info@kchr.de>",
+          "timestamp": "2024-07-19T23:13:59Z",
+          "url": "https://github.com/paritytech/polkadot-sdk/commit/59e3315f7fd74b95e08e6409d1b3f53f6c2123f4"
+        },
+        "date": 1721436509651,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Received from peers",
+            "value": 52940.5,
+            "unit": "KiB"
+          },
+          {
+            "name": "Sent to peers",
+            "value": 63795.31,
+            "unit": "KiB"
+          },
+          {
+            "name": "approval-distribution",
+            "value": 6.264828571280008,
+            "unit": "seconds"
+          },
+          {
+            "name": "approval-voting",
+            "value": 9.894223107789973,
+            "unit": "seconds"
+          },
+          {
+            "name": "test-environment",
+            "value": 3.170546287170135,
             "unit": "seconds"
           }
         ]
