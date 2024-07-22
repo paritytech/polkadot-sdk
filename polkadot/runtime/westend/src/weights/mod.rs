cded428a0bcf765eee26bbb6256a924256f7cfca
Marcus Pang Yu Yang <marcuspangyuyang@gmail.com>
2024-07-23 05:42:11 +0800

    feat: introduce pallet-parameters to Westend to parameterize inflation (#4938)
    Fixes #4907
    
    ## Changes
    - Add `pallet-parameters` to Westend runtime
    - Add `relay_era_payout` function to Polkadot runtime common + deprecate
    previous `era_payout` function
    - Add unit tests for `relay_era_payout`, matching previous unit tests
    for `era_payout`
    - Remove `pallet_staking_reward_curve` in Westend runtime
    - Add `dynamic_params` to replace constants used for
    `pallet_staking_reward_curve`
    
    Polkadot address: 5DybbVdLvVWd7MiA1YwkzLUNL1ThMDicozbuLc8v9pQEScPQ
    
    ---------
    
    Co-authored-by: Kian Paimani <5588131+kianenigma@users.noreply.github.com>
    Co-authored-by: kianenigma <kian@parity.io>
    Co-authored-by: command-bot <>

--- polkadot/runtime/westend/src/weights/mod.rs
+++ polkadot/runtime/westend/src/weights/mod.rs
@@ -29,6 +29,7 @@ pub mod pallet_message_queue;
 pub mod pallet_mmr;
 pub mod pallet_multisig;
 pub mod pallet_nomination_pools;
+pub mod pallet_parameters;
 pub mod pallet_preimage;
 pub mod pallet_proxy;
 pub mod pallet_referenda_fellowship_referenda;
