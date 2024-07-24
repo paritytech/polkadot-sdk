ed9cd7d02d1b10159b430d9f5df6362f79c1ee69
github-action-benchmark <github@users.noreply.github.com>
2024-07-24 14:08:13 +0000

    add approval-voting-regression-bench (customSmallerIsBetter) benchmark result for 71109c5fa3f7f4446d859d2b53d1b90d79aa13a1

--- bench/approval-voting-regression-bench/data.js
+++ bench/approval-voting-regression-bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1721821894476,
+  "lastUpdate": 1721830093368,
   "repoUrl": "https://github.com/paritytech/polkadot-sdk",
   "entries": {
     "approval-voting-regression-bench": [
@@ -12031,6 +12031,53 @@ window.BENCHMARK_DATA = {
           {
             "name": "test-environment",
             "value": 3.1256373535901294,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Parth Mittal",
+            "username": "mittal-parth",
+            "email": "76661350+mittal-parth@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "71109c5fa3f7f4446d859d2b53d1b90d79aa13a1",
+          "message": "Remove `pallet-getter` usage from pallet-transaction-payment (#4970)\n\nAs per #3326, removes usage of the `pallet::getter` macro from the\n`transaction-payment` pallet. The syntax `StorageItem::<T, I>::get()`\nshould be used instead.\n\nAlso, adds public functions for compatibility.\n\nNOTE: The `Releases` enum has been made public to transition\n`StorageVersion` from `pub(super) type` to `pub type`.\n\ncc @muraca\n\npolkadot address: 5GsLutpKjbzsbTphebs9Uy4YK6gTN47MAaz6njPktidjR5cp\n\n---------\n\nSigned-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>\nCo-authored-by: Bastian Köcher <git@kchr.de>\nCo-authored-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>",
+          "timestamp": "2024-07-24T12:21:35Z",
+          "url": "https://github.com/paritytech/polkadot-sdk/commit/71109c5fa3f7f4446d859d2b53d1b90d79aa13a1"
+        },
+        "date": 1721830062844,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Sent to peers",
+            "value": 64054.18000000001,
+            "unit": "KiB"
+          },
+          {
+            "name": "Received from peers",
+            "value": 52937.90000000001,
+            "unit": "KiB"
+          },
+          {
+            "name": "approval-voting",
+            "value": 11.201209226340039,
+            "unit": "seconds"
+          },
+          {
+            "name": "test-environment",
+            "value": 3.8501844944302532,
+            "unit": "seconds"
+          },
+          {
+            "name": "approval-distribution",
+            "value": 7.375836038329932,
             "unit": "seconds"
           }
         ]
