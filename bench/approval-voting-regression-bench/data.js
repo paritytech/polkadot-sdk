4dffc53a5dfd5ea8470c6d787b126ca79dfbdbc5
github-action-benchmark <github@users.noreply.github.com>
2024-07-24 18:48:47 +0000

    add approval-voting-regression-bench (customSmallerIsBetter) benchmark result for 60d21e9c4314d5144c9ebdd4fd851ee5f06b0f0d

--- bench/approval-voting-regression-bench/data.js
+++ bench/approval-voting-regression-bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1721836287777,
+  "lastUpdate": 1721846927218,
   "repoUrl": "https://github.com/paritytech/polkadot-sdk",
   "entries": {
     "approval-voting-regression-bench": [
@@ -12125,6 +12125,53 @@ window.BENCHMARK_DATA = {
           {
             "name": "test-environment",
             "value": 4.059841770510236,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Przemek Rzad",
+            "username": "rzadp",
+            "email": "przemek@parity.io"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "60d21e9c4314d5144c9ebdd4fd851ee5f06b0f0d",
+          "message": "Introduce a workflow updating the wishlist leaderboards (#5085)\n\n- Closes https://github.com/paritytech/eng-automation/issues/11\n\nThe workflow periodically updates the leaderboards of the wishlist\nissues: https://github.com/paritytech/polkadot-sdk/issues/3900 and\nhttps://github.com/paritytech/polkadot-sdk/issues/3901\n\nThe code is adopted from\n[here](https://github.com/kianenigma/wishlist-tracker), with slight\nmodifications.\n\nPreviously, the score could be increased by the same person adding\ndifferent reactions. Also, some wishes have a score of 0 - even thought\nthere is a wish for them, because the author was not counted.\n\nNow, the score is a unique count of upvoters of the desired issue,\nupvoters of the wish comment, and the author of the wish comment.\n\nI changed the format to include the `Last updated:` at the bottom - it\nwill be automatically updated.",
+          "timestamp": "2024-07-24T18:18:58Z",
+          "url": "https://github.com/paritytech/polkadot-sdk/commit/60d21e9c4314d5144c9ebdd4fd851ee5f06b0f0d"
+        },
+        "date": 1721846897842,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Sent to peers",
+            "value": 64140.61000000001,
+            "unit": "KiB"
+          },
+          {
+            "name": "Received from peers",
+            "value": 52945.8,
+            "unit": "KiB"
+          },
+          {
+            "name": "approval-distribution",
+            "value": 8.808411406210173,
+            "unit": "seconds"
+          },
+          {
+            "name": "approval-voting",
+            "value": 12.898820268049898,
+            "unit": "seconds"
+          },
+          {
+            "name": "test-environment",
+            "value": 4.571164236480146,
             "unit": "seconds"
           }
         ]
