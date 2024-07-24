66a26e1a9fa2a26450b83d6da0f9c2cce7bcf00b
github-action-benchmark <github@users.noreply.github.com>
2024-07-24 12:06:15 +0000

    add statement-distribution-regression-bench (customSmallerIsBetter) benchmark result for 8a96d07e5c0d9479f857b0c3053d3e68497f4430

--- bench/statement-distribution-regression-bench/data.js
+++ bench/statement-distribution-regression-bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1721777879545,
+  "lastUpdate": 1721822775278,
   "repoUrl": "https://github.com/paritytech/polkadot-sdk",
   "entries": {
     "statement-distribution-regression-bench": [
@@ -5963,6 +5963,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "test-environment",
             "value": 0.04535562995000003,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Guillaume Thiolliere",
+            "username": "gui1117",
+            "email": "gui.thiolliere@gmail.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "8a96d07e5c0d9479f857b0c3053d3e68497f4430",
+          "message": "pallet macro: do not generate try-runtime related code when frame-support doesn't have try-runtime. (#5099)\n\nStatus: Ready for review\n\nFix https://github.com/paritytech/polkadot-sdk/issues/5092\n\nIntroduce a new macro in frame-support which discard content if\n`try-runtime` is not enabled.\n\nUse this macro inside `frame-support-procedural` to generate code only\nwhen `frame-support` is compiled with `try-runtime`.\n\n---------\n\nCo-authored-by: Bastian Köcher <git@kchr.de>",
+          "timestamp": "2024-07-24T10:02:23Z",
+          "url": "https://github.com/paritytech/polkadot-sdk/commit/8a96d07e5c0d9479f857b0c3053d3e68497f4430"
+        },
+        "date": 1721822745554,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Received from peers",
+            "value": 106.39999999999996,
+            "unit": "KiB"
+          },
+          {
+            "name": "Sent to peers",
+            "value": 127.94799999999998,
+            "unit": "KiB"
+          },
+          {
+            "name": "statement-distribution",
+            "value": 0.036245586804,
+            "unit": "seconds"
+          },
+          {
+            "name": "test-environment",
+            "value": 0.045573296392,
             "unit": "seconds"
           }
         ]
