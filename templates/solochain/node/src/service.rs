e96928e0c5b48704c7ce8b1b08cd5685d249545d
Jun Jiang <jasl9187@hotmail.com>
2024-07-26 01:20:15 +0800

    enhancing solochain template (#5143)
    Since we have a minimal template, I propose enhancing the solochain
    template, which would be easier for startup projects.
    
    - Sync separates `api`, `configs`, and `benchmarks` from the parachain
    template
    - introducing `frame-metadata-hash-extension`
    - Some style update
    
    ---------
    
    Co-authored-by: Kian Paimani <5588131+kianenigma@users.noreply.github.com>

--- templates/solochain/node/src/service.rs
+++ templates/solochain/node/src/service.rs
@@ -7,7 +7,7 @@ use sc_consensus_grandpa::SharedVoterState;
 use sc_service::{error::Error as ServiceError, Configuration, TaskManager, WarpSyncParams};
 use sc_telemetry::{Telemetry, TelemetryWorker};
 use sc_transaction_pool_api::OffchainTransactionPoolFactory;
-use solochain_template_runtime::{self, opaque::Block, RuntimeApi};
+use solochain_template_runtime::{self, apis::RuntimeApi, opaque::Block};
 use sp_consensus_aura::sr25519::AuthorityPair as AuraPair;
 use std::{sync::Arc, time::Duration};
 
