e114a819b6207997cf6c88215510b1891ca76f35
github-action-benchmark <github@users.noreply.github.com>
2024-07-26 10:47:13 +0000

    add availability-distribution-regression-bench (customSmallerIsBetter) benchmark result for 0d7d2177807ec6b3094f4491a45b0bc0d74d3c8b

--- bench/availability-distribution-regression-bench/data.js
+++ bench/availability-distribution-regression-bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1721933910925,
+  "lastUpdate": 1721990833534,
   "repoUrl": "https://github.com/paritytech/polkadot-sdk",
   "entries": {
     "availability-distribution-regression-bench": [
@@ -19707,6 +19707,58 @@ window.BENCHMARK_DATA = {
           {
             "name": "availability-store",
             "value": 0.16663545934666668,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Oliver Tale-Yazdi",
+            "username": "ggwpez",
+            "email": "oliver.tale-yazdi@parity.io"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "0d7d2177807ec6b3094f4491a45b0bc0d74d3c8b",
+          "message": "CI: Prevent breaking backports (#4812)\n\n- Prevent `major` changes to be merged into a `stable` branch.\n- Place a comment on backport MRs to provide context of what it means.\n\nComment looks like this:\n\n![Screenshot 2024-07-24 at 17 36\n35](https://github.com/user-attachments/assets/6393549b-7b15-41e5-a804-8581c625ceff)\n\n---------\n\nSigned-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>",
+          "timestamp": "2024-07-26T09:10:49Z",
+          "url": "https://github.com/paritytech/polkadot-sdk/commit/0d7d2177807ec6b3094f4491a45b0bc0d74d3c8b"
+        },
+        "date": 1721990802581,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Sent to peers",
+            "value": 18481.666666666653,
+            "unit": "KiB"
+          },
+          {
+            "name": "Received from peers",
+            "value": 433.3333333333332,
+            "unit": "KiB"
+          },
+          {
+            "name": "availability-distribution",
+            "value": 0.012795053799999996,
+            "unit": "seconds"
+          },
+          {
+            "name": "test-environment",
+            "value": 0.01060622403333334,
+            "unit": "seconds"
+          },
+          {
+            "name": "bitfield-distribution",
+            "value": 0.022441549766666666,
+            "unit": "seconds"
+          },
+          {
+            "name": "availability-store",
+            "value": 0.17419450039333342,
             "unit": "seconds"
           }
         ]
