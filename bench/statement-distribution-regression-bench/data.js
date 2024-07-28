0a5c239a1f7b6588a2af1f52bbfae299ef1f4d9d
github-action-benchmark <github@users.noreply.github.com>
2024-07-28 22:14:49 +0000

    add statement-distribution-regression-bench (customSmallerIsBetter) benchmark result for dc4047c7d05f593757328394d1accf0eb382d709

--- bench/statement-distribution-regression-bench/data.js
+++ bench/statement-distribution-regression-bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1722158522606,
+  "lastUpdate": 1722204889197,
   "repoUrl": "https://github.com/paritytech/polkadot-sdk",
   "entries": {
     "statement-distribution-regression-bench": [
@@ -6761,6 +6761,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "test-environment",
             "value": 0.04742970789,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "André Silva",
+            "username": "andresilva",
+            "email": "123550+andresilva@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "dc4047c7d05f593757328394d1accf0eb382d709",
+          "message": "grandpa: handle error from SelectChain::finality_target (#5153)\n\nFix https://github.com/paritytech/polkadot-sdk/issues/3487.\n\n---------\n\nCo-authored-by: Dmitry Lavrenov <39522748+dmitrylavrenov@users.noreply.github.com>\nCo-authored-by: Bastian Köcher <info@kchr.de>\nCo-authored-by: Bastian Köcher <git@kchr.de>",
+          "timestamp": "2024-07-28T20:03:48Z",
+          "url": "https://github.com/paritytech/polkadot-sdk/commit/dc4047c7d05f593757328394d1accf0eb382d709"
+        },
+        "date": 1722204858487,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Sent to peers",
+            "value": 127.93199999999996,
+            "unit": "KiB"
+          },
+          {
+            "name": "Received from peers",
+            "value": 106.40199999999997,
+            "unit": "KiB"
+          },
+          {
+            "name": "test-environment",
+            "value": 0.044923026972000006,
+            "unit": "seconds"
+          },
+          {
+            "name": "statement-distribution",
+            "value": 0.036619518356,
             "unit": "seconds"
           }
         ]
