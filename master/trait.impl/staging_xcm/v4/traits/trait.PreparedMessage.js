24016209c6ce4e9da99b5aa04ca0d63e64fce7e7
paritytech-ci <devops-team@parity.io>
2024-07-28 20:25:04 +0000

    ___Updated docs for master___

--- master/trait.impl/staging_xcm/v4/traits/trait.PreparedMessage.js
+++ master/trait.impl/staging_xcm/v4/traits/trait.PreparedMessage.js
@@ -1,6 +1,6 @@
 (function() {var implementors = {
 "collectives_westend_integration_tests":[],
 "staging_xcm_builder":[],
-"staging_xcm_executor":[["impl&lt;C&gt; PreparedMessage for <a class=\"struct\" href=\"staging_xcm_executor/struct.WeighedMessage.html\" title=\"struct staging_xcm_executor::WeighedMessage\">WeighedMessage</a>&lt;C&gt;"]],
+"staging_xcm_executor":[["impl&lt;C&gt; <a class=\"trait\" href=\"staging_xcm/v4/traits/trait.PreparedMessage.html\" title=\"trait staging_xcm::v4::traits::PreparedMessage\">PreparedMessage</a> for <a class=\"struct\" href=\"staging_xcm_executor/struct.WeighedMessage.html\" title=\"struct staging_xcm_executor::WeighedMessage\">WeighedMessage</a>&lt;C&gt;"]],
 "xcm_simulator":[]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
