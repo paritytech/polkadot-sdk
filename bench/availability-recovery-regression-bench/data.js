9133595380a901a1bfeef5b9de1d811e5e36a70d
github-action-benchmark <github@users.noreply.github.com>
2024-07-30 12:00:06 +0000

    add availability-recovery-regression-bench (customSmallerIsBetter) benchmark result for 07544295e45faedbd2bd6f903b76653527e0b6cf

--- bench/availability-recovery-regression-bench/data.js
+++ bench/availability-recovery-regression-bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1722297485718,
+  "lastUpdate": 1722340806357,
   "repoUrl": "https://github.com/paritytech/polkadot-sdk",
   "entries": {
     "availability-recovery-regression-bench": [
@@ -16253,6 +16253,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "test-environment",
             "value": 0.19147406506666664,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Andrei Eres",
+            "username": "AndreiEres",
+            "email": "eresav@me.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "07544295e45faedbd2bd6f903b76653527e0b6cf",
+          "message": "[subsystem-benchmark] Update availability-distribution-regression-bench baseline after recent subsystem changes (#5180)",
+          "timestamp": "2024-07-30T08:54:48Z",
+          "url": "https://github.com/paritytech/polkadot-sdk/commit/07544295e45faedbd2bd6f903b76653527e0b6cf"
+        },
+        "date": 1722340775518,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Received from peers",
+            "value": 307203,
+            "unit": "KiB"
+          },
+          {
+            "name": "Sent to peers",
+            "value": 1.6666666666666665,
+            "unit": "KiB"
+          },
+          {
+            "name": "availability-recovery",
+            "value": 13.295975607999997,
+            "unit": "seconds"
+          },
+          {
+            "name": "test-environment",
+            "value": 0.2689207864,
             "unit": "seconds"
           }
         ]
