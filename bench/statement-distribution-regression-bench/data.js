8e7a1da40fbef68125212eeedcc4aebc7a6966aa
github-action-benchmark <github@users.noreply.github.com>
2024-07-30 00:03:01 +0000

    add statement-distribution-regression-bench (customSmallerIsBetter) benchmark result for 839ead3441b4012cbf6451af5b19a923bcbc379b

--- bench/statement-distribution-regression-bench/data.js
+++ bench/statement-distribution-regression-bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1722286746474,
+  "lastUpdate": 1722297781482,
   "repoUrl": "https://github.com/paritytech/polkadot-sdk",
   "entries": {
     "statement-distribution-regression-bench": [
@@ -6929,6 +6929,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "test-environment",
             "value": 0.045726413072,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Pankaj",
+            "username": "Polkaverse",
+            "email": "pankajchaudhary172@gmail.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "839ead3441b4012cbf6451af5b19a923bcbc379b",
+          "message": "Remove pallet::getter usage from proxy (#4963)\n\nISSUE\nLink to the issue:\nhttps://github.com/paritytech/polkadot-sdk/issues/3326\n\nDeliverables\n\n[Deprecation] remove pallet::getter usage from pallet-proxy\n\n---------\n\nCo-authored-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>",
+          "timestamp": "2024-07-29T22:12:43Z",
+          "url": "https://github.com/paritytech/polkadot-sdk/commit/839ead3441b4012cbf6451af5b19a923bcbc379b"
+        },
+        "date": 1722297751227,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Sent to peers",
+            "value": 127.94799999999994,
+            "unit": "KiB"
+          },
+          {
+            "name": "Received from peers",
+            "value": 106.40599999999996,
+            "unit": "KiB"
+          },
+          {
+            "name": "test-environment",
+            "value": 0.047862193694,
+            "unit": "seconds"
+          },
+          {
+            "name": "statement-distribution",
+            "value": 0.038024580797999995,
             "unit": "seconds"
           }
         ]
