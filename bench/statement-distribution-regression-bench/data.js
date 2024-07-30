00a860f6faaa5a8b6070341998ba1915e0b4e95e
github-action-benchmark <github@users.noreply.github.com>
2024-07-30 14:25:21 +0000

    add statement-distribution-regression-bench (customSmallerIsBetter) benchmark result for 686ee99c7c5de56d1416d47e4db4f3a2420c6c82

--- bench/statement-distribution-regression-bench/data.js
+++ bench/statement-distribution-regression-bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1722341103265,
+  "lastUpdate": 1722349521292,
   "repoUrl": "https://github.com/paritytech/polkadot-sdk",
   "entries": {
     "statement-distribution-regression-bench": [
@@ -7013,6 +7013,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "test-environment",
             "value": 0.04677198309600002,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Oliver Tale-Yazdi",
+            "username": "ggwpez",
+            "email": "oliver.tale-yazdi@parity.io"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "686ee99c7c5de56d1416d47e4db4f3a2420c6c82",
+          "message": "[CI] Cache try-runtime check (#5179)\n\nAdds a snapshot step to the try-runtime check that tries to download a\ncached snapshot.\nThe cache is valid for the current day and is otherwise re-created.\n\nCheck is now only limited by build time and docker startup.\n\n![Screenshot 2024-07-30 at 02 02\n58](https://github.com/user-attachments/assets/0773e9b9-4a52-4572-a891-74b9d725ba70)\n\n![Screenshot 2024-07-30 at 02 02\n20](https://github.com/user-attachments/assets/4685ef17-a04c-4bdc-9d61-311d0010f71c)\n\n---------\n\nSigned-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>",
+          "timestamp": "2024-07-30T12:29:12Z",
+          "url": "https://github.com/paritytech/polkadot-sdk/commit/686ee99c7c5de56d1416d47e4db4f3a2420c6c82"
+        },
+        "date": 1722349490455,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Sent to peers",
+            "value": 127.96,
+            "unit": "KiB"
+          },
+          {
+            "name": "Received from peers",
+            "value": 106.40399999999998,
+            "unit": "KiB"
+          },
+          {
+            "name": "statement-distribution",
+            "value": 0.03715819613799999,
+            "unit": "seconds"
+          },
+          {
+            "name": "test-environment",
+            "value": 0.04697318362199998,
             "unit": "seconds"
           }
         ]
