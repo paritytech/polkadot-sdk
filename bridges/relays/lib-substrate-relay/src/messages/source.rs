paritytech/parity-bridges-common

Change LaneId underlying type from [u8; 4] to H256 (#2221)
* change LaneId underlying type from [u8; 4] to H256

* fixed typo

* added some tests

* spelling

* started fixing testnets

* uncommented call size test

* changed RewardsAccountParams encoding + added values separator when computing LaneId

* review suggestions

relays/lib-substrate-relay/src/messages/source.rs
@@ -667,7 +667,7 @@ mod tests {
 		}
 
 		let maybe_batches =
-			split_msgs_to_refine::<Rialto, Rococo>(LaneId([0, 0, 0, 0]), msgs_to_refine);
+			split_msgs_to_refine::<Rialto, Rococo>(LaneId::new(1, 2), msgs_to_refine);
 		match expected_batches {
 			Ok(expected_batches) => {
 				let batches = maybe_batches.unwrap();

paritytech/polkadot-sdk

bridges/relays/lib-substrate-relay/src/messages/source.rs
@@ -661,7 +661,7 @@ mod tests {
 		}
 
 		let maybe_batches =
-			split_msgs_to_refine::<TestChain, TestChain>(Default::default(), msgs_to_refine);
+			split_msgs_to_refine::<TestChain, TestChain>(LaneId::new(1, 2), msgs_to_refine);
 		match expected_batches {
 			Ok(expected_batches) => {
 				let batches = maybe_batches.unwrap();