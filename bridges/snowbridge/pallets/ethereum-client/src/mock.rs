ea85bf5e8d71c1442b1c937bad0927978912996c
claravanstaden <claravanstaden64@gmail.com>
2024-08-01 10:47:55 +0200

    import fix

--- bridges/snowbridge/pallets/ethereum-client/src/mock.rs
+++ bridges/snowbridge/pallets/ethereum-client/src/mock.rs
@@ -10,6 +10,7 @@ use sp_std::default::Default;
 use std::{fs::File, path::PathBuf};
 
 type Block = frame_system::mocking::MockBlock<Test>;
+use frame_support::traits::ConstU32;
 use sp_runtime::BuildStorage;
 
 fn load_fixture<T>(basename: String) -> Result<T, serde_json::Error>
