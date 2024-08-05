2144701307529619116e563024e5735d6e46e6c5
Alexandru Vasile <alexandru.vasile@parity.io>
2024-08-05 13:56:45 +0300

    net_params: Request for public-addr when starting a validator node
    Signed-off-by: Alexandru Vasile <alexandru.vasile@parity.io>

--- substrate/client/cli/src/config.rs
+++ substrate/client/cli/src/config.rs
@@ -172,7 +172,7 @@ pub trait CliConfiguration<DCV: DefaultConfigurationValues = ()>: Sized {
 		node_key: NodeKeyConfig,
 		default_listen_port: u16,
 	) -> Result<NetworkConfiguration> {
-		Ok(if let Some(network_params) = self.network_params() {
+		let network_config = if let Some(network_params) = self.network_params() {
 			network_params.network_config(
 				chain_spec,
 				is_dev,
@@ -185,7 +185,17 @@ pub trait CliConfiguration<DCV: DefaultConfigurationValues = ()>: Sized {
 			)
 		} else {
 			NetworkConfiguration::new(node_name, client_id, node_key, Some(net_config_dir))
-		})
+		};
+
+		if is_validator && network_config.public_addresses.is_empty() {
+			eprintln!(
+				"WARNING: No public address specified, validator node may not be reachable.
+				Consider setting `--public-addr` to the public IP address of this node.
+				This will become a hard requirement in future versions."
+			);
+		}
+
+		Ok(network_config)
 	}
 
 	/// Get the keystore configuration.
