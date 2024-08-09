002d6f39f15f9be4bd55e2072e02639cce0902d0
github-action-benchmark <github@users.noreply.github.com>
2024-08-09 12:58:26 +0000

    add statement-distribution-regression-bench (customSmallerIsBetter) benchmark result for 87280eb52537a355c67c9b9cee8ff1f97d02d67a

--- bench/statement-distribution-regression-bench/data.js
+++ bench/statement-distribution-regression-bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1723197504023,
+  "lastUpdate": 1723208305575,
   "repoUrl": "https://github.com/paritytech/polkadot-sdk",
   "entries": {
     "statement-distribution-regression-bench": [
@@ -7769,6 +7769,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "test-environment",
             "value": 0.04718024213000002,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Przemek Rzad",
+            "username": "rzadp",
+            "email": "przemek@parity.io"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "87280eb52537a355c67c9b9cee8ff1f97d02d67a",
+          "message": "Synchronize templates through PRs, instead of pushes (#5291)\n\nDespite what we had in the [original\nrequest](https://github.com/paritytech/polkadot-sdk/issues/3155#issuecomment-1979037109),\nI'm proposing a change to open a PR to the destination template\nrepositories instead of pushing the code.\n\nThis will give it a chance to run through the destination CI before\nmaking changes, and to set stricter branch protection in the destination\nrepos.",
+          "timestamp": "2024-08-09T11:03:07Z",
+          "url": "https://github.com/paritytech/polkadot-sdk/commit/87280eb52537a355c67c9b9cee8ff1f97d02d67a"
+        },
+        "date": 1723208274722,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Received from peers",
+            "value": 106.39999999999996,
+            "unit": "KiB"
+          },
+          {
+            "name": "Sent to peers",
+            "value": 127.93599999999995,
+            "unit": "KiB"
+          },
+          {
+            "name": "statement-distribution",
+            "value": 0.037343891114000015,
+            "unit": "seconds"
+          },
+          {
+            "name": "test-environment",
+            "value": 0.04606659208599998,
             "unit": "seconds"
           }
         ]
