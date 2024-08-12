157ef1d09d52b58c6e51f2a9c682b290c729d549
github-action-benchmark <github@users.noreply.github.com>
2024-08-12 22:12:02 +0000

    add approval-voting-regression-bench (customSmallerIsBetter) benchmark result for 819a5818284a96a5a5bd65ce67e69bab860d4534

--- bench/approval-voting-regression-bench/data.js
+++ bench/approval-voting-regression-bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1723484296935,
+  "lastUpdate": 1723500722205,
   "repoUrl": "https://github.com/paritytech/polkadot-sdk",
   "entries": {
     "approval-voting-regression-bench": [
@@ -14522,6 +14522,53 @@ window.BENCHMARK_DATA = {
           {
             "name": "approval-distribution",
             "value": 6.110596386890091,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "eskimor",
+            "username": "eskimor",
+            "email": "eskimor@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "819a5818284a96a5a5bd65ce67e69bab860d4534",
+          "message": "Bump authoring duration for async backing to 2s. (#5195)\n\nShould be safe on all production network. \n\nI noticed that Paseo needs to be updated, it is lacking behind in a\ncouple of things.\n\nExecution environment parameters should be updated to those of Polkadot:\n\n```\n[\n      {\n        MaxMemoryPages: 8,192\n      }\n      {\n        PvfExecTimeout: [\n          Backing\n          2,500\n        ]\n      }\n      {\n        PvfExecTimeout: [\n          Approval\n          15,000\n        ]\n      }\n    ]\n  ]\n  ```\n\n---------\n\nCo-authored-by: eskimor <eskimor@no-such-url.com>",
+          "timestamp": "2024-08-12T20:34:22Z",
+          "url": "https://github.com/paritytech/polkadot-sdk/commit/819a5818284a96a5a5bd65ce67e69bab860d4534"
+        },
+        "date": 1723500690680,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Received from peers",
+            "value": 52940.40000000001,
+            "unit": "KiB"
+          },
+          {
+            "name": "Sent to peers",
+            "value": 63977.880000000005,
+            "unit": "KiB"
+          },
+          {
+            "name": "test-environment",
+            "value": 2.988955419560179,
+            "unit": "seconds"
+          },
+          {
+            "name": "approval-distribution",
+            "value": 6.026731753649996,
+            "unit": "seconds"
+          },
+          {
+            "name": "approval-voting",
+            "value": 9.763066269360008,
             "unit": "seconds"
           }
         ]
