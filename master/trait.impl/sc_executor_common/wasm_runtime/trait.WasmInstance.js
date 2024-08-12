d10f594f82bfaefaf451334c9aefd60fd2957174
paritytech-ci <devops-team@parity.io>
2024-08-12 20:42:28 +0000

    ___Updated docs for master___

--- master/trait.impl/sc_executor_common/wasm_runtime/trait.WasmInstance.js
+++ master/trait.impl/sc_executor_common/wasm_runtime/trait.WasmInstance.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"sc_executor_polkavm":[["impl <a class=\"trait\" href=\"sc_executor_common/wasm_runtime/trait.WasmInstance.html\" title=\"trait sc_executor_common::wasm_runtime::WasmInstance\">WasmInstance</a> for <a class=\"struct\" href=\"sc_executor_polkavm/struct.Instance.html\" title=\"struct sc_executor_polkavm::Instance\">Instance</a>"]]
+"sc_executor_polkavm":[["impl WasmInstance for <a class=\"struct\" href=\"sc_executor_polkavm/struct.Instance.html\" title=\"struct sc_executor_polkavm::Instance\">Instance</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
