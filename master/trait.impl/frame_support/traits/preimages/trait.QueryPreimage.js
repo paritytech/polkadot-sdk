72318f448a4a566f3dd975d0488e2bbfdc98635e
paritytech-ci <devops-team@parity.io>
2024-08-13 14:23:18 +0000

    ___Updated docs for master___

--- master/trait.impl/frame_support/traits/preimages/trait.QueryPreimage.js
+++ master/trait.impl/frame_support/traits/preimages/trait.QueryPreimage.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {
 "frame_support":[],
-"pallet_preimage":[["impl&lt;T: <a class=\"trait\" href=\"pallet_preimage/pallet/trait.Config.html\" title=\"trait pallet_preimage::pallet::Config\">Config</a>&gt; <a class=\"trait\" href=\"frame_support/traits/preimages/trait.QueryPreimage.html\" title=\"trait frame_support::traits::preimages::QueryPreimage\">QueryPreimage</a> for <a class=\"struct\" href=\"pallet_preimage/pallet/struct.Pallet.html\" title=\"struct pallet_preimage::pallet::Pallet\">Pallet</a>&lt;T&gt;"]],
+"pallet_preimage":[["impl&lt;T: <a class=\"trait\" href=\"pallet_preimage/pallet/trait.Config.html\" title=\"trait pallet_preimage::pallet::Config\">Config</a>&gt; QueryPreimage for <a class=\"struct\" href=\"pallet_preimage/pallet/struct.Pallet.html\" title=\"struct pallet_preimage::pallet::Pallet\">Pallet</a>&lt;T&gt;"]],
 "polkadot_sdk_frame":[]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
