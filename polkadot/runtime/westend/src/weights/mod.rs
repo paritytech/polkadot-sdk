81d8f0c0fa0be50dcd2c0291840a1b5b7ace8b98
Serban Iorga <serban@parity.io>
2024-08-14 21:28:18 +0300

    Beefy: add benchmarks for `report_fork_voting()` (#5188)
    Related to #4523 
    
    This PR adds benchmarks for `report_fork_voting()`.
    
    **Important: Even though the benchmarks are now available, we still use
    `Weight::MAX`. That's because I realized while working on this PR that
    there's still one missing piece. We should also check that the ancestry
    proof is optimal. I plan to do this in a future PR, hopefully the last
    one related to #4523.**
    
    ---------
    
    Co-authored-by: Branislav Kontur <bkontur@gmail.com>
    Co-authored-by: command-bot <>

--- polkadot/runtime/westend/src/weights/mod.rs
+++ polkadot/runtime/westend/src/weights/mod.rs
@@ -20,6 +20,7 @@ pub mod frame_system;
 pub mod pallet_asset_rate;
 pub mod pallet_bags_list;
 pub mod pallet_balances;
+pub mod pallet_beefy_mmr;
 pub mod pallet_conviction_voting;
 pub mod pallet_election_provider_multi_phase;
 pub mod pallet_fast_unstake;
