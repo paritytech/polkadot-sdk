b0c3a6578090161c5dabf8cd7dea8ff6285233c5
Bastian Köcher <git@kchr.de>
2024-08-14 22:23:34 +0200

    Update cumulus/client/consensus/aura/src/collators/basic.rs
    Co-authored-by: Davide Galassi <davxy@datawok.net>

--- cumulus/client/consensus/aura/src/collators/basic.rs
+++ cumulus/client/consensus/aura/src/collators/basic.rs
@@ -216,7 +216,7 @@ where
 			//
 			// Most parachains currently run with 12 seconds slots and thus, they would try to
 			// produce multiple blocks per slot which very likely would fail on chain. Thus, we have
-			// this "hack" to only produce on block per slot per relay chain fork.
+			// this "hack" to only produce one block per slot per relay chain fork.
 			//
 			// With https://github.com/paritytech/polkadot-sdk/issues/3168 this implementation will be
 			// obsolete and also the underlying issue will be fixed.
