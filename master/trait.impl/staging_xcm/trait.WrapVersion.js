47463559f0868cebf8d8681310f3ff227ecf0413
paritytech-ci <devops-team@parity.io>
2024-08-14 20:15:19 +0000

    ___Updated docs for master___

--- master/trait.impl/staging_xcm/trait.WrapVersion.js
+++ master/trait.impl/staging_xcm/trait.WrapVersion.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {
 "collectives_westend_integration_tests":[],
-"pallet_xcm":[["impl&lt;T: <a class=\"trait\" href=\"pallet_xcm/pallet/trait.Config.html\" title=\"trait pallet_xcm::pallet::Config\">Config</a>&gt; WrapVersion for <a class=\"struct\" href=\"pallet_xcm/pallet/struct.Pallet.html\" title=\"struct pallet_xcm::pallet::Pallet\">Pallet</a>&lt;T&gt;"]],
+"pallet_xcm":[["impl&lt;T: <a class=\"trait\" href=\"pallet_xcm/pallet/trait.Config.html\" title=\"trait pallet_xcm::pallet::Config\">Config</a>&gt; <a class=\"trait\" href=\"staging_xcm/trait.WrapVersion.html\" title=\"trait staging_xcm::WrapVersion\">WrapVersion</a> for <a class=\"struct\" href=\"pallet_xcm/pallet/struct.Pallet.html\" title=\"struct pallet_xcm::pallet::Pallet\">Pallet</a>&lt;T&gt;"]],
 "staging_xcm":[]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
