67ddbb844c7796104b6c221dee9c9ae88433a234
ndkazu <ndongmefane@gmail.com>
2024-08-15 23:44:23 +0900

    Fixing errors

--- substrate/frame/distribution/src/benchmarking.rs
+++ substrate/frame/distribution/src/benchmarking.rs
@@ -1,3 +1,22 @@
+// This file is part of Substrate.
+
+// Copyright (C) Parity Technologies (UK) Ltd.
+// SPDX-License-Identifier: Apache-2.0
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//! Distribution pallet benchmarking.
+
 #![cfg(feature = "runtime-benchmarks")]
 
 use super::*;
@@ -7,13 +26,7 @@ use frame_benchmarking::{
 	v1::{account, BenchmarkError},
 	v2::*,
 };
-use frame_support::{
-	ensure,
-	traits::{
-		tokens::{ConversionFromAssetBalance, PaymentStatus},
-		EnsureOrigin,
-	},
-};
+use frame_support::ensure;
 use frame_system::RawOrigin;
 use sp_runtime::traits::One;
 
@@ -79,7 +92,7 @@ mod benchmarks {
 
 		ensure!(<Projects<T>>::get().len() as u32 == r, "Project list setting failed !!");
 
-		let pot = setup_pot_account::<T>();
+		let _pot = setup_pot_account::<T>();
 		let caller: T::AccountId = whitelisted_caller();
 		let epoch = T::EpochDurationBlocks::get();
 		let mut when = T::BlockNumberProvider::current_block_number().saturating_add(epoch);
@@ -88,7 +101,6 @@ mod benchmarks {
 		let project = <Spends<T>>::get(r - 1).unwrap();
 		when = when.saturating_add(project.valid_from);
 		let project_id = project.whitelisted_project.unwrap();
-		let amount = project.amount;
 		run_to_block::<T>(when);
 
 		#[extrinsic_call]
