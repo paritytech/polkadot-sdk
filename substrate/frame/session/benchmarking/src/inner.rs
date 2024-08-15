165eadb1d3473cae402f297bbfcea59fa6827c43
Bastian Köcher <info@kchr.de>
2024-08-15 11:59:06 +0200

    Fix warning

--- substrate/frame/session/benchmarking/src/inner.rs
+++ substrate/frame/session/benchmarking/src/inner.rs
@@ -18,7 +18,7 @@
 //! Benchmarks for the Session Pallet.
 // This is separated into its own crate due to cyclic dependency issues.
 
-use alloc::{vec, vec::Vec};
+use alloc::vec::Vec;
 use sp_runtime::traits::{One, StaticLookup, TrailingZeroInput};
 
 use codec::Decode;
