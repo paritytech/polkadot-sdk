f1e2721ed7c8bf709f5cba5e2d810d066fe55a7b
alindima <alin@parity.io>
2024-08-16 14:45:28 +0300

    rollback changes to rococo-parachain

--- cumulus/parachains/runtimes/testing/rococo-parachain/src/lib.rs
+++ cumulus/parachains/runtimes/testing/rococo-parachain/src/lib.rs
@@ -283,8 +283,8 @@ parameter_types! {
 type ConsensusHook = cumulus_pallet_aura_ext::FixedVelocityConsensusHook<
 	Runtime,
 	RELAY_CHAIN_SLOT_DURATION_MILLIS,
-	3,
-	7,
+	BLOCK_PROCESSING_VELOCITY,
+	UNINCLUDED_SEGMENT_CAPACITY,
 >;
 
 impl cumulus_pallet_parachain_system::Config for Runtime {
@@ -604,7 +604,7 @@ impl pallet_aura::Config for Runtime {
 	type DisabledValidators = ();
 	type MaxAuthorities = ConstU32<100_000>;
 	type AllowMultipleBlocksPerSlot = ConstBool<true>;
-	type SlotDuration = ConstU64<2000>;
+	type SlotDuration = ConstU64<SLOT_DURATION>;
 }
 
 construct_runtime! {
