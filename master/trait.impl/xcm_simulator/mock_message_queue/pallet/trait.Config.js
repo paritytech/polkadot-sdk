8fd2da746f394ec19c88b381dc211605da08b0a7
paritytech-ci <devops-team@parity.io>
2024-08-16 15:07:05 +0000

    ___Updated docs for master___

--- master/trait.impl/xcm_simulator/mock_message_queue/pallet/trait.Config.js
+++ master/trait.impl/xcm_simulator/mock_message_queue/pallet/trait.Config.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"xcm_docs":[["impl <a class=\"trait\" href=\"xcm_simulator/mock_message_queue/pallet/trait.Config.html\" title=\"trait xcm_simulator::mock_message_queue::pallet::Config\">Config</a> for <a class=\"struct\" href=\"xcm_docs/cookbook/relay_token_transactor/parachain/struct.Runtime.html\" title=\"struct xcm_docs::cookbook::relay_token_transactor::parachain::Runtime\">Runtime</a>"]]
+"xcm_docs":[["impl Config for <a class=\"struct\" href=\"xcm_docs/cookbook/relay_token_transactor/parachain/struct.Runtime.html\" title=\"struct xcm_docs::cookbook::relay_token_transactor::parachain::Runtime\">Runtime</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
