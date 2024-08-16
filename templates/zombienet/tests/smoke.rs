6794739f79a9da9d0066d2bf349cde6274cf8501
Javier Viola <javier@parity.io>
2024-08-16 11:43:16 -0300

    clean code and update CI

--- templates/zombienet/tests/smoke.rs
+++ templates/zombienet/tests/smoke.rs
@@ -11,8 +11,33 @@
 
 #[cfg(feature = "zombienet")]
 mod smoke {
-	use zombienet_sdk::NetworkConfigExt;
-	use template_zombienet_tests::{get_config, requirements_are_meet};
+	use anyhow::anyhow;
+	use zombienet_sdk::{NetworkConfig, NetworkConfigBuilder, NetworkConfigExt};
+
+	pub fn get_config(cmd: &str, para_cmd: Option<&str>) -> Result<NetworkConfig, anyhow::Error> {
+		let chain = if cmd == "polkadot" { "rococo-local" } else { "dev" };
+		let config = NetworkConfigBuilder::new().with_relaychain(|r| {
+			r.with_chain(chain)
+				.with_default_command(cmd)
+				.with_node(|node| node.with_name("alice"))
+				.with_node(|node| node.with_name("bob"))
+		});
+
+		let config = if let Some(para_cmd) = para_cmd {
+			config.with_parachain(|p| {
+				p.with_id(1000)
+					.with_default_command(para_cmd)
+					.with_collator(|n| n.with_name("collator"))
+			})
+		} else {
+			config
+		};
+
+		Ok(config.build().map_err(|e| {
+			let errs = e.into_iter().map(|e| e.to_string()).collect::<Vec<_>>().join(" ");
+			anyhow!("config errs: {errs}")
+		})?)
+	}
 
 	#[tokio::test(flavor = "multi_thread")]
 	async fn parachain_template_block_production_test() -> Result<(), anyhow::Error> {
@@ -20,7 +45,7 @@ mod smoke {
 			env_logger::Env::default().filter_or(env_logger::DEFAULT_FILTER_ENV, "info"),
 		);
 
-		requirements_are_meet(&vec!["polkadot", "parachain-template-node"])?;
+		// requirements_are_meet(&vec!["polkadot", "parachain-template-node"])?;
 
 		let config = get_config("polkadot", Some("parachain-template-node"))?;
 
@@ -36,14 +61,13 @@ mod smoke {
 		Ok(())
 	}
 
-
 	#[tokio::test(flavor = "multi_thread")]
 	async fn solochain_template_block_production_test() -> Result<(), anyhow::Error> {
 		let _ = env_logger::try_init_from_env(
 			env_logger::Env::default().filter_or(env_logger::DEFAULT_FILTER_ENV, "info"),
 		);
 
-		requirements_are_meet(&vec!["solochain-template-node"])?;
+		// requirements_are_meet(&vec!["solochain-template-node"])?;
 
 		let config = get_config("solochain-template-node", None)?;
 
@@ -51,14 +75,10 @@ mod smoke {
 
 		// wait 6 blocks
 		let alice = network.get_node("alice")?;
-		assert!(alice
-			.wait_metric("block_height{status=\"best\"}", |b| b > 5_f64)
-			.await
-			.is_ok());
+		assert!(alice.wait_metric("block_height{status=\"best\"}", |b| b > 5_f64).await.is_ok());
 
 		Ok(())
 	}
-
 
 	#[tokio::test(flavor = "multi_thread")]
 	async fn minimal_template_block_production_test() -> Result<(), anyhow::Error> {
@@ -66,7 +86,7 @@ mod smoke {
 			env_logger::Env::default().filter_or(env_logger::DEFAULT_FILTER_ENV, "info"),
 		);
 
-		requirements_are_meet(&vec!["minimal-template-node"])?;
+		// requirements_are_meet(&vec!["minimal-template-node"])?;
 
 		let config = get_config("minimal-template-node", None)?;
 
@@ -74,10 +94,7 @@ mod smoke {
 
 		// wait 6 blocks
 		let alice = network.get_node("alice")?;
-		assert!(alice
-			.wait_metric("block_height{status=\"best\"}", |b| b > 5_f64)
-			.await
-			.is_ok());
+		assert!(alice.wait_metric("block_height{status=\"best\"}", |b| b > 5_f64).await.is_ok());
 
 		Ok(())
 	}
