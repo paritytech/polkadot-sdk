549245909bb370aef31235d77ed908eb76fad354
github-action-benchmark <github@users.noreply.github.com>
2024-08-21 17:31:53 +0000

    add approval-voting-regression-bench (customSmallerIsBetter) benchmark result for 8b17f0f4c697793e0080836f175596a5ac2a0b3a

--- bench/approval-voting-regression-bench/data.js
+++ bench/approval-voting-regression-bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1724172956036,
+  "lastUpdate": 1724261513352,
   "repoUrl": "https://github.com/paritytech/polkadot-sdk",
   "entries": {
     "approval-voting-regression-bench": [
@@ -15603,6 +15603,53 @@ window.BENCHMARK_DATA = {
           {
             "name": "approval-distribution",
             "value": 6.1760360518800494,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Alexandru Vasile",
+            "username": "lexnv",
+            "email": "60601340+lexnv@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "8b17f0f4c697793e0080836f175596a5ac2a0b3a",
+          "message": "peerstore: Clarify peer report warnings (#5407)\n\nThis PR aims to make the logging from the peer store a bit more clear.\n\nIn the past, we aggressively produced warning logs from the peer store\ncomponent, even in cases where the reputation change was not malicious.\nThis has led to an extensive number of logs, as well to node operator\nconfusion.\n\nIn this PR, we produce a warning message if:\n- The peer crosses the banned threshold for the first time. This is the\nactual reason of a ban\n- The peer misbehaves again while being banned. This may happen during a\nbatch peer report\n\ncc @paritytech/networking \n\nPart of: https://github.com/paritytech/polkadot-sdk/issues/5379.\n\n---------\n\nSigned-off-by: Alexandru Vasile <alexandru.vasile@parity.io>\nCo-authored-by: Dmitry Markin <dmitry@markin.tech>",
+          "timestamp": "2024-08-21T15:50:50Z",
+          "url": "https://github.com/paritytech/polkadot-sdk/commit/8b17f0f4c697793e0080836f175596a5ac2a0b3a"
+        },
+        "date": 1724261485043,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Received from peers",
+            "value": 52943.3,
+            "unit": "KiB"
+          },
+          {
+            "name": "Sent to peers",
+            "value": 63950.95000000001,
+            "unit": "KiB"
+          },
+          {
+            "name": "test-environment",
+            "value": 3.146330659830144,
+            "unit": "seconds"
+          },
+          {
+            "name": "approval-voting",
+            "value": 9.879855602569958,
+            "unit": "seconds"
+          },
+          {
+            "name": "approval-distribution",
+            "value": 5.943248398629958,
             "unit": "seconds"
           }
         ]
