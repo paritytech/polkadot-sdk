1810c24caaa25f70d78a8ddc01db5c732c7c2a69
Serban Iorga <serban@parity.io>
2024-08-22 16:35:24 +0300

    fix fmt

--- cumulus/polkadot-parachain/polkadot-parachain-lib/src/common/runtime.rs
+++ cumulus/polkadot-parachain/polkadot-parachain-lib/src/common/runtime.rs
@@ -21,42 +21,42 @@ use sc_chain_spec::ChainSpec;
 /// The Aura ID used by the Aura consensus
 #[derive(PartialEq)]
 pub enum AuraConsensusId {
-    /// Ed25519
-    Ed25519,
-    /// Sr25519
-    Sr25519,
+	/// Ed25519
+	Ed25519,
+	/// Sr25519
+	Sr25519,
 }
 
 /// The choice of consensus for the parachain omni-node.
 #[derive(PartialEq)]
 pub enum Consensus {
-    /// Aura consensus.
-    Aura(AuraConsensusId),
+	/// Aura consensus.
+	Aura(AuraConsensusId),
 }
 
 /// The choice of block number for the parachain omni-node.
 #[derive(PartialEq)]
 pub enum BlockNumber {
-    /// u32
-    U32,
-    /// u64
-    U64,
+	/// u32
+	U32,
+	/// u64
+	U64,
 }
 
 /// Helper enum listing the supported Runtime types
 #[derive(PartialEq)]
 pub enum Runtime {
-    /// None of the system-chain runtimes, rather the node will act agnostic to the runtime ie. be
-    /// an omni-node, and simply run a node with the given consensus algorithm.
-    Omni(BlockNumber, Consensus),
-    /// Shell
-    Shell,
+	/// None of the system-chain runtimes, rather the node will act agnostic to the runtime ie. be
+	/// an omni-node, and simply run a node with the given consensus algorithm.
+	Omni(BlockNumber, Consensus),
+	/// Shell
+	Shell,
 }
 
 /// Helper trait used for extracting the Runtime variant from the chain spec ID.
 pub trait RuntimeResolver {
-    /// Extract the Runtime variant from the chain spec ID.
-    fn runtime(&self, chain_spec: &dyn ChainSpec) -> sc_cli::Result<Runtime>;
+	/// Extract the Runtime variant from the chain spec ID.
+	fn runtime(&self, chain_spec: &dyn ChainSpec) -> sc_cli::Result<Runtime>;
 }
 
 /// Default implementation for `RuntimeResolver` that just returns
@@ -64,7 +64,7 @@ pub trait RuntimeResolver {
 pub struct DefaultRuntimeResolver;
 
 impl RuntimeResolver for DefaultRuntimeResolver {
-    fn runtime(&self, _chain_spec: &dyn ChainSpec) -> sc_cli::Result<Runtime> {
-        Ok(Runtime::Omni(BlockNumber::U32, Consensus::Aura(AuraConsensusId::Sr25519)))
-    }
+	fn runtime(&self, _chain_spec: &dyn ChainSpec) -> sc_cli::Result<Runtime> {
+		Ok(Runtime::Omni(BlockNumber::U32, Consensus::Aura(AuraConsensusId::Sr25519)))
+	}
 }
