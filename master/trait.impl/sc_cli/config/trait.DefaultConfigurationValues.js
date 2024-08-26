c254ba4d6c10d110fb3f8f19af2afbe5a2ebb94c
paritytech-ci <devops-team@parity.io>
2024-08-26 15:35:36 +0000

    ___Updated docs for master___

--- master/trait.impl/sc_cli/config/trait.DefaultConfigurationValues.js
+++ master/trait.impl/sc_cli/config/trait.DefaultConfigurationValues.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {
-"parachain_template_node":[["impl <a class=\"trait\" href=\"sc_cli/config/trait.DefaultConfigurationValues.html\" title=\"trait sc_cli::config::DefaultConfigurationValues\">DefaultConfigurationValues</a> for <a class=\"struct\" href=\"parachain_template_node/cli/struct.RelayChainCli.html\" title=\"struct parachain_template_node::cli::RelayChainCli\">RelayChainCli</a>"]],
+"parachain_template_node":[["impl DefaultConfigurationValues for <a class=\"struct\" href=\"parachain_template_node/cli/struct.RelayChainCli.html\" title=\"struct parachain_template_node::cli::RelayChainCli\">RelayChainCli</a>"]],
 "sc_cli":[],
-"test_parachain":[["impl <a class=\"trait\" href=\"sc_cli/config/trait.DefaultConfigurationValues.html\" title=\"trait sc_cli::config::DefaultConfigurationValues\">DefaultConfigurationValues</a> for <a class=\"struct\" href=\"test_parachain/cli/struct.RelayChainCli.html\" title=\"struct test_parachain::cli::RelayChainCli\">RelayChainCli</a>"]]
+"test_parachain":[["impl DefaultConfigurationValues for <a class=\"struct\" href=\"test_parachain/cli/struct.RelayChainCli.html\" title=\"struct test_parachain::cli::RelayChainCli\">RelayChainCli</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
