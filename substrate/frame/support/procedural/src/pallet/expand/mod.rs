263376f34bc899e74843e30ec679d29d3008ef3b
gui <gui.thiolliere@gmail.com>
2024-08-27 13:17:03 +0900

    fmt

--- substrate/frame/support/procedural/src/pallet/expand/mod.rs
+++ substrate/frame/support/procedural/src/pallet/expand/mod.rs
@@ -15,7 +15,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
 /// Helper to generate better error in case of internal error.
 macro_rules! msg {
 	($t:literal) => {
