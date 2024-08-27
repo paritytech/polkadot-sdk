1e0947470ae68f66571ea740f3879d8987605c84
github-action-benchmark <github@users.noreply.github.com>
2024-08-27 11:43:27 +0000

    add approval-voting-regression-bench (customSmallerIsBetter) benchmark result for 7e7c33453eeb14f47c6c4d0f98cc982e485edc77

--- bench/approval-voting-regression-bench/data.js
+++ bench/approval-voting-regression-bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1724753445004,
+  "lastUpdate": 1724759006969,
   "repoUrl": "https://github.com/paritytech/polkadot-sdk",
   "entries": {
     "approval-voting-regression-bench": [
@@ -16214,6 +16214,53 @@ window.BENCHMARK_DATA = {
           {
             "name": "test-environment",
             "value": 3.397132808970126,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Oliver Tale-Yazdi",
+            "username": "ggwpez",
+            "email": "oliver.tale-yazdi@parity.io"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "7e7c33453eeb14f47c6c4d0f98cc982e485edc77",
+          "message": "frame-omni-bencher maintenance (#5466)\n\nChanges:\n- Set default level to `Info` again. Seems like a dependency update set\nit to something higher.\n- Fix docs to not use `--locked` since we rely on dependency bumps via\ncargo.\n- Add README with rust docs.\n- Fix bug where the node ignored `--heap-pages` argument.\n\nYou can test the `--heap-pages` bug by running this command on master\nand then on this branch. Note that it should fail because of the very\nlow heap pages arg:\n`cargo run --release --bin polkadot --features=runtime-benchmarks --\nbenchmark pallet --chain=dev --steps=10 --repeat=30\n--wasm-execution=compiled --heap-pages=8 --pallet=frame-system\n--extrinsic=\"*\"`\n\n---------\n\nSigned-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>\nCo-authored-by: ggwpez <ggwpez@users.noreply.github.com>",
+          "timestamp": "2024-08-27T10:05:15Z",
+          "url": "https://github.com/paritytech/polkadot-sdk/commit/7e7c33453eeb14f47c6c4d0f98cc982e485edc77"
+        },
+        "date": 1724758979820,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Sent to peers",
+            "value": 63984.93000000001,
+            "unit": "KiB"
+          },
+          {
+            "name": "Received from peers",
+            "value": 52943,
+            "unit": "KiB"
+          },
+          {
+            "name": "approval-distribution",
+            "value": 5.941658466960047,
+            "unit": "seconds"
+          },
+          {
+            "name": "test-environment",
+            "value": 3.116074911960094,
+            "unit": "seconds"
+          },
+          {
+            "name": "approval-voting",
+            "value": 9.838725640100085,
             "unit": "seconds"
           }
         ]
