2c9d8bab8715c0c4e4b9dad4a248bbfd1bd00e35
paritytech-ci <devops-team@parity.io>
2024-08-27 17:12:43 +0000

    ___Updated docs for master___

--- master/trait.impl/cumulus_client_pov_recovery/trait.RecoveryHandle.js
+++ master/trait.impl/cumulus_client_pov_recovery/trait.RecoveryHandle.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
 "cumulus_client_pov_recovery":[],
-"cumulus_test_service":[["impl <a class=\"trait\" href=\"cumulus_client_pov_recovery/trait.RecoveryHandle.html\" title=\"trait cumulus_client_pov_recovery::RecoveryHandle\">RecoveryHandle</a> for <a class=\"struct\" href=\"cumulus_test_service/struct.FailingRecoveryHandle.html\" title=\"struct cumulus_test_service::FailingRecoveryHandle\">FailingRecoveryHandle</a>"]]
+"cumulus_test_service":[["impl RecoveryHandle for <a class=\"struct\" href=\"cumulus_test_service/struct.FailingRecoveryHandle.html\" title=\"struct cumulus_test_service::FailingRecoveryHandle\">FailingRecoveryHandle</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
