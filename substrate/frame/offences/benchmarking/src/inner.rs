994c6c570816cf3a280cd64cf60624aa87c3e777
Ankan <ankan.anurag@gmail.com>
2024-08-28 14:40:32 +0200

    refactor bench test since moving from currency to fungible change events

--- substrate/frame/offences/benchmarking/src/inner.rs
+++ substrate/frame/offences/benchmarking/src/inner.rs
@@ -172,6 +172,14 @@ fn make_offenders<T: Config>(
 }
 
 benchmarks! {
+	where_clause {
+		where
+		<T as frame_system::Config>::RuntimeEvent: TryInto<pallet_staking::Event<T>>,
+		<T as frame_system::Config>::RuntimeEvent: TryInto<pallet_balances::Event<T>>,
+		<T as frame_system::Config>::RuntimeEvent: TryInto<pallet_offences::Event>,
+		<T as frame_system::Config>::RuntimeEvent: TryInto<frame_system::Event<T>>,
+	}
+
 	report_offence_grandpa {
 		let n in 0 .. MAX_NOMINATORS.min(MaxNominationsOf::<T>::get());
 
@@ -197,16 +205,14 @@ benchmarks! {
 	}
 	verify {
 		// make sure that all slashes have been applied
-		#[cfg(test)]
-		assert_eq!(
-			System::<T>::event_count(), 0
-			+ 1 // offence
-			+ 3 // reporter (reward + endowment)
-			+ 1 // offenders reported
-			+ 3 // offenders slashed
-			+ 1 // offenders chilled
-			+ 3 * n // nominators slashed
-		);
+		// (n+1) * (slash + unlocked) + deposit + endowment + rescinded
+		assert_eq!(System::<T>::read_events_for_pallet::<pallet_balances::Event<T>>().len(), 2 * (n + 1) as usize + 3);
+		// (n+1) * slashed + Slash Reported
+		assert_eq!(System::<T>::read_events_for_pallet::<pallet_staking::Event<T>>().len(), 1 * (n + 1) as usize + 1);
+		// offence
+		assert_eq!(System::<T>::read_events_for_pallet::<pallet_offences::Event>().len(), 1);
+		// new account
+		assert_eq!(System::<T>::read_events_for_pallet::<frame_system::Event<T>>().len(), 1);
 	}
 
 	report_offence_babe {
@@ -234,16 +240,14 @@ benchmarks! {
 	}
 	verify {
 		// make sure that all slashes have been applied
-		#[cfg(test)]
-		assert_eq!(
-			System::<T>::event_count(), 0
-			+ 1 // offence
-			+ 3 // reporter (reward + endowment)
-			+ 1 // offenders reported
-			+ 3 // offenders slashed
-			+ 1 // offenders chilled
-			+ 3 * n // nominators slashed
-		);
+		// (n+1) * (slash + unlocked) + deposit + endowment + rescinded
+		assert_eq!(System::<T>::read_events_for_pallet::<pallet_balances::Event<T>>().len(), 2 * (n + 1) as usize + 3);
+		// (n+1) * slashed + Slash Reported
+		assert_eq!(System::<T>::read_events_for_pallet::<pallet_staking::Event<T>>().len(), 1 * (n + 1) as usize + 1);
+		// offence
+		assert_eq!(System::<T>::read_events_for_pallet::<pallet_offences::Event>().len(), 1);
+		// new account
+		assert_eq!(System::<T>::read_events_for_pallet::<frame_system::Event<T>>().len(), 1);
 	}
 
 	impl_benchmark_test_suite!(Pallet, crate::mock::new_test_ext(), crate::mock::Test);
