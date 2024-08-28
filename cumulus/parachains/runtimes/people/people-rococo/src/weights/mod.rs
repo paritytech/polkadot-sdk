54e6cad9279820987842983a6648b525a4f83198
Dónal Murray <donal.murray@parity.io>
2024-08-28 13:12:47 +0100

    Add pallet-proxy to Rococo People

--- cumulus/parachains/runtimes/people/people-rococo/src/weights/mod.rs
+++ cumulus/parachains/runtimes/people/people-rococo/src/weights/mod.rs
@@ -25,6 +25,7 @@ pub mod pallet_collator_selection;
 pub mod pallet_identity;
 pub mod pallet_message_queue;
 pub mod pallet_multisig;
+pub mod pallet_proxy;
 pub mod pallet_session;
 pub mod pallet_timestamp;
 pub mod pallet_utility;
