7dff1117024b75938f4237867a5cde4d98930b40
Pablo Andrés Dorado Suárez <hola@pablodorado.com>
2024-08-27 22:14:36 -0500

    fix(pallet-revive-mock-network): include Holder on pallet_assets configuration

--- substrate/frame/revive/mock-network/src/parachain.rs
+++ substrate/frame/revive/mock-network/src/parachain.rs
@@ -119,6 +119,7 @@ impl pallet_assets::Config for Runtime {
 	type AssetAccountDeposit = AssetAccountDeposit;
 	type ApprovalDeposit = ApprovalDeposit;
 	type StringLimit = AssetsStringLimit;
+	type Holder = ();
 	type Freezer = ();
 	type Extra = ();
 	type WeightInfo = ();
