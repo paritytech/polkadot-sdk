7e82cae8ffcc76397b1cbce5b826875975fe5f34
Michal Kucharczyk <1728078+michalkucharczyk@users.noreply.github.com>
2024-08-30 12:59:06 +0200

    Update cumulus/parachains/common/src/genesis_config_helpers.rs
    Co-authored-by: Bastian Köcher <git@kchr.de>

--- cumulus/parachains/common/src/genesis_config_helpers.rs
+++ cumulus/parachains/common/src/genesis_config_helpers.rs
@@ -24,7 +24,7 @@ use sp_std::alloc::format;
 
 /// Helper function to generate a crypto pair from seed.
 pub fn get_from_seed<TPublic: Public>(seed: &str) -> <TPublic::Pair as Pair>::Public {
-	TPublic::Pair::from_string(&format!("//{}", seed), None)
+	TPublic::Pair::from_string(&format!("//{seed}"), None)
 		.expect("static values are valid; qed")
 		.public()
 }
