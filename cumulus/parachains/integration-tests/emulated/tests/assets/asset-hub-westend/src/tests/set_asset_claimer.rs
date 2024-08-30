6800d9fbce93343f031aef0a3c408bd0ba3c7f94
ndk <ndk@parity.io>
2024-08-30 13:54:58 +0300

    WIP e2e tests - claimAssets step

--- cumulus/parachains/integration-tests/emulated/tests/assets/asset-hub-westend/src/tests/set_asset_claimer.rs
+++ cumulus/parachains/integration-tests/emulated/tests/assets/asset-hub-westend/src/tests/set_asset_claimer.rs
@@ -15,7 +15,10 @@
 
 //! Tests related to claiming assets trapped during XCM execution.
 
-use crate::imports::*;
+use emulated_integration_tests_common::accounts::BOB;
+use crate::{
+    imports::*,
+};
 
 use frame_support::{
     dispatch::RawOrigin,
@@ -30,41 +33,109 @@ use xcm_runtime_apis::{
 
 #[test]
 fn azs() {
-    let bob = Location::new(0, [AccountId32 { id: [2; 32], network: None }]);
     let destination = PenpalA::sibling_location_of(PenpalB::para_id());
     let sender = PenpalASender::get();
-    let beneficiary_id = PenpalBReceiver::get();
-    let amount_to_send = 1_000_000_000_000;
+
+    let amount_to_send = 16_000_000_000_000;
+    let asset_owner = PenpalAssetOwner::get();
+    let native_asset_location = RelayLocation::get();
     let assets: Assets = (Parent, amount_to_send).into();
 
-    // Fund accounts again.
+
+
+    let bob_account: AccountId = PenpalA::account_id_of(BOB);
+    let bob_location: Location =
+        [Junction::AccountId32 { network: None, id: PenpalA::account_id_of(BOB).into() }]
+            .into();
+
+
+
+    // Fund accounts.
+    let relay_native_asset_location = Location::parent();
     PenpalA::mint_foreign_asset(
-        <PenpalA as Chain>::RuntimeOrigin::signed(PenpalAssetOwner::get()),
-        Location::parent().clone(),
+        <PenpalA as Chain>::RuntimeOrigin::signed(asset_owner),
+        relay_native_asset_location.clone(),
         sender.clone(),
         amount_to_send * 2,
     );
 
+    let sender_assets_before = PenpalA::execute_with(|| {
+        type ForeignAssets = <PenpalA as PenpalAPallet>::ForeignAssets;
+        <ForeignAssets as Inspect<_>>::balance(native_asset_location.clone(), &sender)
+    });
+
+    dbg!(sender_assets_before);
+
+    // Init values for Parachain Destination
+    let beneficiary_id = PenpalBReceiver::get();
     let test_args = TestContext {
-        sender: PenpalASender::get(),     // Bob in PenpalB.
-        receiver: PenpalBReceiver::get(), // Alice.
+        sender: PenpalASender::get(),
+        receiver: PenpalBReceiver::get(),
         args: TestArgs::new_para(
-            destination,
+            destination.clone(),
             beneficiary_id.clone(),
             amount_to_send,
-            assets,
+            assets.clone(),
             None,
             0,
         ),
     };
     let mut test = ParaToParaThroughAHTest::new(test_args);
-    transfer_assets(test.clone(), bob.clone());
-    // let call = transfer_assets(test.clone(), bob.clone());
+    let call = transfer_assets(test.clone(), bob_location.clone());
+    PenpalA::execute_with(|| {
+        assert!(call.dispatch(test.signed_origin).is_ok());
+    });
 
+    let sender_assets_after = PenpalA::execute_with(|| {
+        type ForeignAssets = <PenpalA as PenpalAPallet>::ForeignAssets;
+        <ForeignAssets as Inspect<_>>::balance(native_asset_location.clone(), &sender)
+    });
 
-    // test.set_assertion::<PenpalA>(sender_assertions);
-    // test.set_call(call);
-    // test.assert();
+    dbg!(sender_assets_after);
+
+    let test_args = TestContext {
+        sender: PenpalASender::get(),
+        receiver: PenpalBReceiver::get(),
+        args: TestArgs::new_para(
+            destination.clone(),
+            beneficiary_id.clone(),
+            amount_to_send,
+            assets.clone(),
+            None,
+            0,
+        ),
+    };
+    let mut test = ParaToParaThroughAHTest::new(test_args);
+    let call = claim_assets(test.clone(), bob_location.clone());
+    call.dispatch(<PenpalA as Chain>::RuntimeOrigin::signed(bob_account.clone()));
+    // PenpalA::execute_with(|| {
+    //     assert!(call.dispatch(test.signed_origin).is_ok());
+    // });
+
+    let bob_assets_after = PenpalA::execute_with(|| {
+        type ForeignAssets = <PenpalA as PenpalAPallet>::ForeignAssets;
+        <ForeignAssets as Inspect<_>>::balance(native_asset_location.clone(), &bob_account)
+    });
+
+    dbg!(bob_assets_after);
+}
+
+fn claim_assets(
+    test: ParaToParaThroughAHTest,
+    claimer: Location
+) -> <PenpalA as Chain>::RuntimeCall {
+    type RuntimeCall = <PenpalA as Chain>::RuntimeCall;
+
+    let local_xcm = Xcm::<RuntimeCall>::builder_unsafe()
+        .claim_asset(test.args.assets.clone(), Here)
+        .deposit_asset(AllCounted(test.args.assets.len() as u32), claimer)
+        .pay_fees((Parent, 10u128))
+        .build();
+
+    RuntimeCall::PolkadotXcm(pallet_xcm::Call::execute {
+        message: bx!(VersionedXcm::from(local_xcm)),
+        max_weight: Weight::from_parts(4_000_000_000_000, 300_000),
+    })
 }
 
 fn transfer_assets(
@@ -73,34 +144,15 @@ fn transfer_assets(
 ) -> <PenpalA as Chain>::RuntimeCall {
     type RuntimeCall = <PenpalA as Chain>::RuntimeCall;
 
-
-
     let local_xcm = Xcm::<RuntimeCall>::builder_unsafe()
-        .clear_origin()
         .set_asset_claimer(claimer.clone())
         .withdraw_asset(test.args.assets.clone())
-        .pay_fees((Parent, 0))
+        .clear_origin()
+        // .pay_fees((Parent, 4_000_000_000_000u128))
         .build();
 
     RuntimeCall::PolkadotXcm(pallet_xcm::Call::execute {
         message: bx!(VersionedXcm::from(local_xcm)),
-        max_weight: Weight::from_parts(3_000_000_000, 200_000),
+        max_weight: Weight::from_parts(4_000_000_000_000, 300_000),
     })
-}
-
-fn sender_assertions(test: ParaToParaThroughAHTest) {
-    type RuntimeEvent = <PenpalA as Chain>::RuntimeEvent;
-    // PenpalA::assert_xcm_pallet_attempted_complete(None);
-    assert_expected_events!(
-		PenpalA,
-		vec![
-			RuntimeEvent::ForeignAssets(
-				pallet_assets::Event::Burned { asset_id, owner, balance }
-			) => {
-				asset_id: *asset_id == Location::new(1, []),
-				owner: *owner == test.sender.account_id,
-				balance: *balance == test.args.amount,
-			},
-		]
-	);
 }
