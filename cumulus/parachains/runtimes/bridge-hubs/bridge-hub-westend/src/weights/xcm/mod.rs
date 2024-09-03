5ced85ab451b1e6d2ad8c0b8ecf11dfa9c65ef6c
ndk <ndk@parity.io>
2024-09-03 18:00:47 +0300

    resolved merge conflicts[3]

--- cumulus/parachains/runtimes/bridge-hubs/bridge-hub-westend/src/weights/xcm/mod.rs
+++ cumulus/parachains/runtimes/bridge-hubs/bridge-hub-westend/src/weights/xcm/mod.rs
@@ -240,4 +240,8 @@ impl<Call> XcmWeightInfo<Call> for BridgeHubWestendXcmWeight<Call> {
 	fn unpaid_execution(_: &WeightLimit, _: &Option<Location>) -> Weight {
 		XcmGeneric::<Runtime>::unpaid_execution()
 	}
+
+	fn pay_fees(asset: &Asset) -> Weight {
+		todo!()
+	}
 }
