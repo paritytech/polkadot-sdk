baa68548939d748ad331d84c4b6c8376bba0e173
paritytech-ci <devops-team@parity.io>
2024-09-05 15:07:04 +0000

    ___Updated docs for master___

--- master/trait.impl/frame_support/traits/tokens/fungible/hold/trait.Inspect.js
+++ master/trait.impl/frame_support/traits/tokens/fungible/hold/trait.Inspect.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {
 "frame_support":[],
-"pallet_balances":[["impl&lt;T: <a class=\"trait\" href=\"pallet_balances/pallet/trait.Config.html\" title=\"trait pallet_balances::pallet::Config\">Config</a>&lt;I&gt;, I: 'static&gt; Inspect&lt;&lt;T as Config&gt;::AccountId&gt; for <a class=\"struct\" href=\"pallet_balances/pallet/struct.Pallet.html\" title=\"struct pallet_balances::pallet::Pallet\">Pallet</a>&lt;T, I&gt;"]],
+"pallet_balances":[["impl&lt;T: <a class=\"trait\" href=\"pallet_balances/pallet/trait.Config.html\" title=\"trait pallet_balances::pallet::Config\">Config</a>&lt;I&gt;, I: 'static&gt; <a class=\"trait\" href=\"frame_support/traits/tokens/fungible/hold/trait.Inspect.html\" title=\"trait frame_support::traits::tokens::fungible::hold::Inspect\">Inspect</a>&lt;&lt;T as Config&gt;::AccountId&gt; for <a class=\"struct\" href=\"pallet_balances/pallet/struct.Pallet.html\" title=\"struct pallet_balances::pallet::Pallet\">Pallet</a>&lt;T, I&gt;"]],
 "polkadot_sdk_frame":[]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
