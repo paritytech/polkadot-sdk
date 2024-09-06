9ba5f5f532fa0d735d144793f4e7ab7889f1feff
paritytech-ci <devops-team@parity.io>
2024-09-06 14:59:45 +0000

    ___Updated docs for master___

--- master/trait.impl/polkadot_node_subsystem_types/runtime_client/trait.RuntimeApiSubsystemClient.js
+++ master/trait.impl/polkadot_node_subsystem_types/runtime_client/trait.RuntimeApiSubsystemClient.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {
-"cumulus_relay_chain_minimal_node":[["impl RuntimeApiSubsystemClient for <a class=\"struct\" href=\"cumulus_relay_chain_minimal_node/struct.BlockChainRpcClient.html\" title=\"struct cumulus_relay_chain_minimal_node::BlockChainRpcClient\">BlockChainRpcClient</a>"]],
+"cumulus_relay_chain_minimal_node":[["impl <a class=\"trait\" href=\"polkadot_node_subsystem_types/runtime_client/trait.RuntimeApiSubsystemClient.html\" title=\"trait polkadot_node_subsystem_types::runtime_client::RuntimeApiSubsystemClient\">RuntimeApiSubsystemClient</a> for <a class=\"struct\" href=\"cumulus_relay_chain_minimal_node/struct.BlockChainRpcClient.html\" title=\"struct cumulus_relay_chain_minimal_node::BlockChainRpcClient\">BlockChainRpcClient</a>"]],
 "polkadot_node_subsystem":[],
 "polkadot_node_subsystem_types":[],
 "polkadot_overseer":[]
