dc4a9a4e1f14b1165cdd26b71553e370a8669871
Ankan <ankan.anurag@gmail.com>
2024-09-06 13:55:59 +0200

    fmt

--- substrate/frame/offences/benchmarking/src/inner.rs
+++ substrate/frame/offences/benchmarking/src/inner.rs
@@ -205,15 +205,15 @@ benchmarks! {
 	verify {
 		#[cfg(test)]
 		{
-		// make sure that all slashes have been applied
-		// deposit to reporter + reporter account endowed.
-		assert_eq!(System::<T>::read_events_for_pallet::<pallet_balances::Event<T>>().len(), 2);
-		// (n nominators + one validator) * slashed + Slash Reported
-		assert_eq!(System::<T>::read_events_for_pallet::<pallet_staking::Event<T>>().len(), 1 * (n + 1) as usize + 1);
-		// offence
-		assert_eq!(System::<T>::read_events_for_pallet::<pallet_offences::Event>().len(), 1);
-		// reporter new account
-		assert_eq!(System::<T>::read_events_for_pallet::<frame_system::Event<T>>().len(), 1);
+			// make sure that all slashes have been applied
+			// deposit to reporter + reporter account endowed.
+			assert_eq!(System::<T>::read_events_for_pallet::<pallet_balances::Event<T>>().len(), 2);
+			// (n nominators + one validator) * slashed + Slash Reported
+			assert_eq!(System::<T>::read_events_for_pallet::<pallet_staking::Event<T>>().len(), 1 * (n + 1) as usize + 1);
+			// offence
+			assert_eq!(System::<T>::read_events_for_pallet::<pallet_offences::Event>().len(), 1);
+			// reporter new account
+			assert_eq!(System::<T>::read_events_for_pallet::<frame_system::Event<T>>().len(), 1);
 		}
 	}
 
@@ -243,15 +243,15 @@ benchmarks! {
 	verify {
 		#[cfg(test)]
 		{
-		// make sure that all slashes have been applied
-		// deposit to reporter + reporter account endowed.
-		assert_eq!(System::<T>::read_events_for_pallet::<pallet_balances::Event<T>>().len(), 2);
-		// (n nominators + one validator) * slashed + Slash Reported
-		assert_eq!(System::<T>::read_events_for_pallet::<pallet_staking::Event<T>>().len(), 1 * (n + 1) as usize + 1);
-		// offence
-		assert_eq!(System::<T>::read_events_for_pallet::<pallet_offences::Event>().len(), 1);
-		// reporter new account
-		assert_eq!(System::<T>::read_events_for_pallet::<frame_system::Event<T>>().len(), 1);
+			// make sure that all slashes have been applied
+			// deposit to reporter + reporter account endowed.
+			assert_eq!(System::<T>::read_events_for_pallet::<pallet_balances::Event<T>>().len(), 2);
+			// (n nominators + one validator) * slashed + Slash Reported
+			assert_eq!(System::<T>::read_events_for_pallet::<pallet_staking::Event<T>>().len(), 1 * (n + 1) as usize + 1);
+			// offence
+			assert_eq!(System::<T>::read_events_for_pallet::<pallet_offences::Event>().len(), 1);
+			// reporter new account
+			assert_eq!(System::<T>::read_events_for_pallet::<frame_system::Event<T>>().len(), 1);
 		}
 	}
 
