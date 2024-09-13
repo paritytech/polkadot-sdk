aab86143094767fb89a322059a0a77073f889248
ndkazu <ndongmefane@gmail.com>
2024-09-13 15:41:18 +0900

    Reverted where it was necessary

--- polkadot/node/network/protocol/src/request_response/outgoing.rs
+++ polkadot/node/network/protocol/src/request_response/outgoing.rs
@@ -23,7 +23,6 @@ use sc_network as network;
 use sc_network_types::PeerId;
 
 use polkadot_primitives::AuthorityDiscoveryId;
-use sc_network::request_responses::OutboundFailure;
 
 use super::{v1, v2, IsRequest, Protocol};
 
@@ -98,7 +97,7 @@ impl RequestError {
 			Self::Canceled(_) |
 			Self::NetworkError(network::RequestFailure::Obsolete) |
 			Self::NetworkError(network::RequestFailure::Network(
-				OutboundFailure::Timeout,
+				network::OutboundFailure::Timeout,
 			)) => true,
 			_ => false,
 		}
