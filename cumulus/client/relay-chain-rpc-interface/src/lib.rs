d465d1b3b8849b5d37d5c3a709b25e2fa4698018
Iulian Barbu <iulian.barbu@parity.io>
2024-09-17 16:12:12 +0300

    one more semicolumn removed
    Signed-off-by: Iulian Barbu <iulian.barbu@parity.io>

--- cumulus/client/relay-chain-rpc-interface/src/lib.rs
+++ cumulus/client/relay-chain-rpc-interface/src/lib.rs
@@ -215,7 +215,7 @@ impl RelayChainInterface for RelayChainRpcInterface {
 		let mut head_stream = self.rpc_client.get_imported_heads_stream()?;
 
 		if self.rpc_client.chain_get_header(Some(wait_for_hash)).await?.is_some() {
-			return Ok(());
+			return Ok(())
 		}
 
 		let mut timeout = futures_timer::Delay::new(Duration::from_secs(TIMEOUT_IN_SECONDS)).fuse();
