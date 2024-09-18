aa5859b73fb62a0e2a35e3377e203e46e75fe0c7
Ron <yrong1997@gmail.com>
2024-09-18 22:05:51 +0800

    Update cumulus/parachains/runtimes/bridge-hubs/bridge-hub-westend/src/lib.rs
    Co-authored-by: Adrian Catangiu <adrian@parity.io>

--- cumulus/parachains/runtimes/bridge-hubs/bridge-hub-westend/src/lib.rs
+++ cumulus/parachains/runtimes/bridge-hubs/bridge-hub-westend/src/lib.rs
@@ -216,7 +216,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("bridge-hub-westend"),
 	impl_name: create_runtime_str!("bridge-hub-westend"),
 	authoring_version: 1,
-	spec_version: 1_017_000,
+	spec_version: 1_016_001,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 5,
