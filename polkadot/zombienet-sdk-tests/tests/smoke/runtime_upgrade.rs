a90e9b86ddbe1f9d4906eff72d13adafd8cae8ce
Javier Viola <javier@parity.io>
2024-09-21 11:41:06 +0200

    ci fixes

--- polkadot/zombienet-sdk-tests/tests/smoke/runtime_upgrade.rs
+++ polkadot/zombienet-sdk-tests/tests/smoke/runtime_upgrade.rs
@@ -1,3 +1,6 @@
+// Copyright (C) Parity Technologies (UK) Ltd.
+// SPDX-License-Identifier: Apache-2.0
+
 use anyhow::anyhow;
 use std::env;
 use zombienet_sdk::{tx_helper::RuntimeUpgradeOptions, NetworkConfigBuilder};
