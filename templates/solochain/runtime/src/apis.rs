4bb97892f1c831936ff6796893188b4e770e52b4
Iulian Barbu <iulian.barbu@parity.io>
2024-09-24 10:08:01 +0300

    added solochain dev genesis config preset
    Signed-off-by: Iulian Barbu <iulian.barbu@parity.io>

--- templates/solochain/runtime/src/apis.rs
+++ templates/solochain/runtime/src/apis.rs
@@ -285,11 +285,11 @@ impl_runtime_apis! {
 		}
 
 		fn get_preset(id: &Option<sp_genesis_builder::PresetId>) -> Option<Vec<u8>> {
-			get_preset::<RuntimeGenesisConfig>(id, |_| None)
+			get_preset::<RuntimeGenesisConfig>(id, crate::genesis_config_presets::get_preset)
 		}
 
 		fn preset_names() -> Vec<sp_genesis_builder::PresetId> {
-			vec![]
+			crate::genesis_config_presets::preset_names()
 		}
 	}
 }
