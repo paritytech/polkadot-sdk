87971b3e92721bdf10bf40b410eaae779d494ca0
EgorPopelyaev <egor@parity.io>
2024-09-26 10:55:32 +0200

    Bump bridge-hubs transaction_version

--- cumulus/parachains/runtimes/bridge-hubs/bridge-hub-westend/src/lib.rs
+++ cumulus/parachains/runtimes/bridge-hubs/bridge-hub-westend/src/lib.rs
@@ -226,7 +226,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_version: 1_016_001,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
-	transaction_version: 5,
+	transaction_version: 6,
 	state_version: 1,
 };
 
