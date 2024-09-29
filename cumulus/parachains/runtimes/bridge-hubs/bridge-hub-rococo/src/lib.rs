9424af2d302c213c685aadac97691d58c74d5f11
Bastian Köcher <git@kchr.de>
2024-09-29 22:53:47 +0200

    Update cumulus/parachains/runtimes/bridge-hubs/bridge-hub-rococo/src/lib.rs
    Co-authored-by: Nazar Mokrynskyi <nazar@mokrynskyi.com>

--- cumulus/parachains/runtimes/bridge-hubs/bridge-hub-rococo/src/lib.rs
+++ cumulus/parachains/runtimes/bridge-hubs/bridge-hub-rococo/src/lib.rs
@@ -242,7 +242,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 6,
-	state_version: 1,
+	system_version: 1,
 };
 
 /// The version information used to identify this runtime when compiled natively.
