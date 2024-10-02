f79deaf8dfc37784d8a7c459801d68083bc071fa
Dmitry Sinyavin <dmitry.sinyavin@parity.io>
2024-10-02 10:00:51 +0200

    Remove unused import

--- polkadot/runtime/parachains/src/configuration.rs
+++ polkadot/runtime/parachains/src/configuration.rs
@@ -29,7 +29,7 @@ use polkadot_parachain_primitives::primitives::{
 use polkadot_primitives::{
 	ApprovalVotingParams, AsyncBackingParams, Balance, ExecutorParamError, ExecutorParams,
 	NodeFeatures, SessionIndex, LEGACY_MIN_BACKING_VOTES, MAX_CODE_SIZE, MAX_HEAD_DATA_SIZE,
-	MAX_POV_SIZE, ON_DEMAND_MAX_QUEUE_MAX_SIZE,
+	ON_DEMAND_MAX_QUEUE_MAX_SIZE,
 };
 use sp_runtime::{traits::Zero, Perbill, Percent};
 
