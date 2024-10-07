5da82a68697ad5d78ce50db53aeb7f9a8edc59fe
Michal Kucharczyk <1728078+michalkucharczyk@users.noreply.github.com>
2024-10-07 18:07:00 +0200

    zombie test for parachain tx finalizing

--- cumulus/zombienet/tests/0008-transaction_gets_finalized.js
+++ cumulus/zombienet/tests/0008-transaction_gets_finalized.js
@@ -3,9 +3,7 @@
 const assert = require("assert");
 
 async function run(nodeName, networkInfo, args) {
-  // console.log('xxxxxxxxxxx 1');
   const {wsUri, userDefinedTypes} = networkInfo.nodesByName[nodeName];
-  // console.log('xxxxxxxxxxx 2');
   // Create the API and wait until ready
   var api = null;
   var keyring = null;
@@ -18,23 +16,18 @@ async function run(nodeName, networkInfo, args) {
     keyring = testKeyring.createTestKeyring({ type: "sr25519" });
   } else {
     keyring = new zombie.Keyring({ type: "sr25519" });
-    // console.log('xxxxxxxxxxx 3');
     api = await zombie.connect(wsUri, userDefinedTypes);
-    // console.log('xxxxxxxxxxx 4');
   }
 
 
   // Add Alice to our keyring with a hard-derivation path (empty phrase, so uses dev)
   const alice = keyring.addFromUri('//Alice');
-  // console.log('xxxxxxxxxxx 5');
 
-  // Create a extrinsic: 
+  // Create an extrinsic:
   const extrinsic = api.tx.system.remark("xxx");
-  // console.log('xxxxxxxxxxx 6');
 
   let extrinsic_success_event = false;
   try {
-    // console.log('xxxxxxxxxxx 7');
     await new Promise( async (resolve, reject) => {
       const unsubscribe = await extrinsic
         .signAndSend(alice, { nonce: -1 }, ({ events = [], status }) => {
@@ -66,7 +59,6 @@ async function run(nodeName, networkInfo, args) {
 
         });
     });
-  // console.log('xxxxxxxxxxx 8');
   } catch (error) {
     assert.fail("Transfer promise failed, error: " + error);
   }
