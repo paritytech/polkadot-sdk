e7c96cfb24d7aebb2933f76773c1e4d032df67ed
EgorPopelyaev <egor@parity.io>
2024-10-08 10:06:14 +0200

    cleanup

--- .github/scripts/release/build-deb.sh
+++ .github/scripts/release/build-deb.sh
@@ -2,13 +2,14 @@
 set -e
 
 PRODUCT=$1
+VERSION=$2
 PROFILE=${PROFILE:-production}
 
 cargo install cargo-deb
 echo "Using cargo-deb v$(cargo-deb --version)"
 echo "Building a Debian package for '$PRODUCT' in '$PROFILE' profile"
 
-cargo deb --profile $PROFILE --no-strip --no-build -p $PRODUCT
+cargo deb --profile $PROFILE --no-strip --no-build -p $PRODUCT --deb-version $VERSION
 
 deb=target/debian/$PRODUCT_*_amd64.deb
 
