66a89c3cbe93e07fe171fff63445292cfdbff78e
EgorPopelyaev <egor@parity.io>
2024-10-09 14:38:21 +0200

    fix deb version

--- .github/scripts/release/build-deb.sh
+++ .github/scripts/release/build-deb.sh
@@ -9,7 +9,8 @@ cargo install --version 2.7.0 cargo-deb --locked -q
 echo "Using cargo-deb v$(cargo-deb --version)"
 echo "Building a Debian package for '$PRODUCT' in '$PROFILE' profile"
 
-cargo deb --profile $PROFILE --no-strip --no-build -p $PRODUCT --deb-version $VERSION
+# we need to start  the custom version with a didgit as requires it cargo-deb
+cargo deb --profile $PROFILE --no-strip --no-build -p $PRODUCT --deb-version 1-$VERSION
 
 deb=target/debian/$PRODUCT_*_amd64.deb
 
