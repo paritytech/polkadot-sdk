b20001c03ff906afc362b9458db25151a0f9c6e3
Iulian Barbu <iulian.barbu@parity.io>
2024-10-15 19:43:58 +0300

    added zombienet tests for omni node
    Signed-off-by: Iulian Barbu <iulian.barbu@parity.io>

--- templates/zombienet/tests/smoke.rs
+++ templates/zombienet/tests/smoke.rs
@@ -7,28 +7,69 @@
 //! `cargo build --package minimal-template-node --release`
 //! `export PATH=<path-to-polkadot-sdk-repo>/target/release:$PATH
 //!
-//! The you can run the test with
-//! `cargo test -p template-zombienet-tests`
+//! There are also some tests related to omni node which run basaed on pre-generated chain specs,
+//! so to be able to run them you would need to generate the right chain spec (just minimal and
+//! parachain tests supported for now).
+//!
+//! You can run the following command to generate a minimal chainspec, once the runtime wasm file is
+//! compiled:
+//!`chain-spec-builder create -r <path_to_template_wasm_file> named-preset
+//! development`
+//!
+//! Both chainspecs needs adding two additional fields for "relay_chain" (set to "dev" for minimal,
+//! and "rococo-local" for parachain), and "para_id" (set to 1000 as number).
+//!
+//! Once the files are generated, you must export in the environment a variable called
+//! `RELAY_CHAIN_SPECS_DIR` which should point to the absolute path to the directory that holds the
+//! generated chain specs. The chain specs file names should be `minimal_chain_spec.json` for
+//! minimal and `parachain_chain_spec.json` for parachain templates.
+//!
+//! To start all tests here we should run:
+//! `cargo test -p template-zombienet-tests --features zombienet`
 
 #[cfg(feature = "zombienet")]
 mod smoke {
+	use std::path::PathBuf;
+
 	use anyhow::anyhow;
 	use zombienet_sdk::{NetworkConfig, NetworkConfigBuilder, NetworkConfigExt};
 
-	pub fn get_config(cmd: &str, para_cmd: Option<&str>) -> Result<NetworkConfig, anyhow::Error> {
-		let chain = if cmd == "polkadot" { "rococo-local" } else { "dev" };
+	const RELAY_CHAIN_SPECS_DIR_PATH: &'static str = std::env!("RELAY_CHAIN_SPECS_DIR");
+
+	#[derive(Default)]
+	struct NetworkSpec {
+		relaychain_cmd: &'static str,
+		relaychain_spec_path: Option<PathBuf>,
+		// TODO: update the type to something like Option<Vec<Arg>> after
+		// `zombienet-sdk` exposes `shared::types::Arg`.
+		relaychain_cmd_args: Option<Vec<(&'static str, &'static str)>>,
+		para_cmd: Option<&'static str>,
+		para_cmd_args: Option<Vec<(&'static str, &'static str)>>,
+	}
+
+	pub fn get_config(network_spec: NetworkSpec) -> Result<NetworkConfig, anyhow::Error> {
+		let chain = if network_spec.relaychain_cmd == "polkadot" { "rococo-local" } else { "dev" };
 		let config = NetworkConfigBuilder::new().with_relaychain(|r| {
-			r.with_chain(chain)
-				.with_default_command(cmd)
-				.with_node(|node| node.with_name("alice"))
+			let mut r = r.with_chain(chain).with_default_command(network_spec.relaychain_cmd);
+			if let Some(path) = network_spec.relaychain_spec_path {
+				r = r.with_chain_spec_path(path);
+			}
+
+			if let Some(args) = network_spec.relaychain_cmd_args {
+				r = r.with_default_args(args.into_iter().map(|arg| arg.into()).collect());
+			}
+
+			r.with_node(|node| node.with_name("alice"))
 				.with_node(|node| node.with_name("bob"))
 		});
 
-		let config = if let Some(para_cmd) = para_cmd {
+		let config = if let Some(para_cmd) = network_spec.para_cmd {
 			config.with_parachain(|p| {
-				p.with_id(1000)
-					.with_default_command(para_cmd)
-					.with_collator(|n| n.with_name("collator"))
+				let mut p = p.with_id(1000).with_default_command(para_cmd);
+				if let Some(args) = network_spec.para_cmd_args {
+					p = p.with_default_args(args.into_iter().map(|arg| arg.into()).collect());
+				}
+				p.with_collator(|n| n.with_name("collator"))
 			})
 		} else {
 			config
@@ -46,7 +87,11 @@ mod smoke {
 			env_logger::Env::default().filter_or(env_logger::DEFAULT_FILTER_ENV, "info"),
 		);
 
-		let config = get_config("polkadot", Some("parachain-template-node"))?;
+		let config = get_config(NetworkSpec {
+			relaychain_cmd: "polkadot",
+			para_cmd: Some("parachain-template-node"),
+			..Default::default()
+		})?;
 
 		let network = config.spawn_native().await?;
 
@@ -66,7 +111,10 @@ mod smoke {
 			env_logger::Env::default().filter_or(env_logger::DEFAULT_FILTER_ENV, "info"),
 		);
 
-		let config = get_config("solochain-template-node", None)?;
+		let config = get_config(NetworkSpec {
+			relaychain_cmd: "solochain-template-node",
+			..Default::default()
+		})?;
 
 		let network = config.spawn_native().await?;
 
@@ -83,13 +131,70 @@ mod smoke {
 			env_logger::Env::default().filter_or(env_logger::DEFAULT_FILTER_ENV, "info"),
 		);
 
-		let config = get_config("minimal-template-node", None)?;
+		let config = get_config(NetworkSpec {
+			relaychain_cmd: "minimal-template-node",
+			..Default::default()
+		})?;
 
 		let network = config.spawn_native().await?;
 
 		// wait 6 blocks
 		let alice = network.get_node("alice")?;
 		assert!(alice.wait_metric("block_height{status=\"best\"}", |b| b > 5_f64).await.is_ok());
+
+		Ok(())
+	}
+
+	#[tokio::test(flavor = "multi_thread")]
+	async fn omni_node_with_minimal_runtime_block_production_test() -> Result<(), anyhow::Error> {
+		let _ = env_logger::try_init_from_env(
+			env_logger::Env::default().filter_or(env_logger::DEFAULT_FILTER_ENV, "info"),
+		);
+
+		let chain_spec_path = RELAY_CHAIN_SPECS_DIR_PATH.to_string() + "/minimal_chain_spec.json";
+		let config = get_config(NetworkSpec {
+			relaychain_cmd: "polkadot-parachain",
+			relaychain_cmd_args: Some(vec![("--dev-block-time", "1000")]),
+			relaychain_spec_path: Some(chain_spec_path.into()),
+			..Default::default()
+		})?;
+		let network = config.spawn_native().await?;
+
+		// wait 6 blocks
+		let alice = network.get_node("alice")?;
+		assert!(alice
+			.wait_metric("block_height{status=\"finalized\"}", |b| b > 5_f64)
+			.await
+			.is_ok());
+
+		Ok(())
+	}
+
+	#[tokio::test(flavor = "multi_thread")]
+	async fn omni_node_with_parachain_runtime_block_production_test() -> Result<(), anyhow::Error> {
+		let _ = env_logger::try_init_from_env(
+			env_logger::Env::default().filter_or(env_logger::DEFAULT_FILTER_ENV, "info"),
+		);
+
+		let chain_spec_path = RELAY_CHAIN_SPECS_DIR_PATH.to_string() + "/parachain_chain_spec.json";
+
+		let config = get_config(NetworkSpec {
+			relaychain_cmd: "polkadot",
+			para_cmd: Some("polkadot-parachain"),
+			// Leaking the `String` to be able to use it below as a static str,
+			// required by the `FromStr` implementation for zombienet-configuration
+			// `Arg` type, which is not exposed yet through `zombienet-sdk`.
+			para_cmd_args: Some(vec![("--chain", chain_spec_path.leak())]),
+			..Default::default()
+		})?;
+		let network = config.spawn_native().await?;
+
+		// wait 6 blocks
+		let alice = network.get_node("collator")?;
+		assert!(alice
+			.wait_metric("block_height{status=\"finalized\"}", |b| b > 5_f64)
+			.await
+			.is_ok());
 
 		Ok(())
 	}
