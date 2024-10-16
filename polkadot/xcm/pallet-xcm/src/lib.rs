cbeb3691227983a22a2777c7928cfbaf0fda26b0
ndk <ndk@parity.io>
2024-10-16 12:32:57 +0300

    changed log::error! to log::trace

--- polkadot/xcm/pallet-xcm/src/lib.rs
+++ polkadot/xcm/pallet-xcm/src/lib.rs
@@ -2575,7 +2575,7 @@ impl<T: Config> Pallet<T> {
 		location: VersionedLocation,
 	) -> Result<bool, TrustQueryApiError> {
 		let location: Location = location.try_into().map_err(|e| {
-			log::error!(
+			log::trace!(
 				target: "xcm::pallet_xcm::is_trusted_reserve",
 				"Asset version conversion failed with error: {:?}",
 				e,
@@ -2584,7 +2584,7 @@ impl<T: Config> Pallet<T> {
 		})?;
 
 		let a: Asset = asset.try_into().map_err(|e| {
-			log::error!(
+			log::trace!(
 				target: "xcm::pallet_xcm::is_trusted_reserve",
 				"Location version conversion failed with error: {:?}",
 				e,
@@ -2600,7 +2600,7 @@ impl<T: Config> Pallet<T> {
 		location: VersionedLocation,
 	) -> Result<bool, TrustQueryApiError> {
 		let location: Location = location.try_into().map_err(|e| {
-			log::error!(
+			log::trace!(
 				target: "xcm::pallet_xcm::is_trusted_teleporter",
 				"Asset version conversion failed with error: {:?}",
 				e,
@@ -2608,7 +2608,7 @@ impl<T: Config> Pallet<T> {
 			TrustQueryApiError::VersionedLocationConversionFailed
 		})?;
 		let a: Asset = asset.try_into().map_err(|e| {
-			log::error!(
+			log::trace!(
 				target: "xcm::pallet_xcm::is_trusted_teleporter",
 				"Location version conversion failed with error: {:?}",
 				e,
