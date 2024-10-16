71e2d70f8685372d4aac7b1fa622979f090c1c32
Gonçalo Pestana <g6pestana@gmail.com>
2024-10-16 15:51:52 +0200

    nits: dependency tests fixes

--- cumulus/test/client/src/lib.rs
+++ cumulus/test/client/src/lib.rs
@@ -214,6 +214,7 @@ pub fn validate_block(
 			false,
 			"validate_block",
 			&validation_params.encode(),
+			Default::default(),
 		)
 		.map(|v| ValidationResult::decode(&mut &v[..]).expect("Decode `ValidationResult`."))
 }
