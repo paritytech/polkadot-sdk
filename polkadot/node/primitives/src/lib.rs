7445a0c96072c6aeb488c1c42cefcee91753bf25
EgorPopelyaev <egor@parity.io>
2024-10-16 17:30:40 +0200

    Bump polkadot node version

--- polkadot/node/primitives/src/lib.rs
+++ polkadot/node/primitives/src/lib.rs
@@ -59,7 +59,7 @@ pub use disputes::{
 /// relatively rare.
 ///
 /// The associated worker binaries should use the same version as the node that spawns them.
-pub const NODE_VERSION: &'static str = "1.16.0";
+pub const NODE_VERSION: &'static str = "1.16.1";
 
 // For a 16-ary Merkle Prefix Trie, we can expect at most 16 32-byte hashes per node
 // plus some overhead:
