216f6191cce214d998511189851038643861df38
MattHalpinParity <matt.halpin@parity.io>
2024-10-18 11:00:15 +0100

    Compile fixes

--- substrate/primitives/state-machine/src/fuzzing.rs
+++ substrate/primitives/state-machine/src/fuzzing.rs
@@ -22,12 +22,11 @@ use crate::ext::StorageAppend;
 use arbitrary::Arbitrary;
 #[cfg(test)]
 use codec::Encode;
-use hash_db::Hasher;
 use sp_core::{storage::StateVersion, traits::Externalities};
 #[cfg(test)]
 use sp_runtime::traits::BlakeTwo256;
-use sp_trie::PrefixedMemoryDB;
 use std::collections::BTreeMap;
+use trie_db::node_db::Hasher;
 
 #[derive(Arbitrary, Debug, Clone)]
 enum DataLength {
@@ -84,9 +83,9 @@ impl SimpleOverlay {
 		&mut self,
 		key: Vec<u8>,
 		value: Vec<u8>,
-		backend: &mut TrieBackend<PrefixedMemoryDB<H>, H>,
+		backend: &mut TrieBackend<H>,
 	) where
-		H: Hasher,
+		H: Hasher + 'static,
 		H::Out: codec::Decode + codec::Encode + 'static,
 	{
 		let current_value = self
@@ -137,7 +136,7 @@ struct FuzzAppendState<H: Hasher> {
 	reference: SimpleOverlay,
 
 	// trie backend
-	backend: TrieBackend<PrefixedMemoryDB<H>, H>,
+	backend: TrieBackend<H>,
 	// Standard Overlay
 	overlay: OverlayedChanges<H>,
 
@@ -147,7 +146,7 @@ struct FuzzAppendState<H: Hasher> {
 
 impl<H> FuzzAppendState<H>
 where
-	H: Hasher,
+	H: Hasher + 'static,
 	H::Out: codec::Decode + codec::Encode + 'static,
 {
 	fn process_item(&mut self, item: FuzzAppendItem) {
@@ -289,7 +288,7 @@ fn fuzz_scenarii() {
 /// Test append operation for a given fuzzing payload.
 pub fn fuzz_append<H>(payload: FuzzAppendPayload)
 where
-	H: Hasher,
+	H: Hasher + 'static,
 	H::Out: codec::Decode + codec::Encode + 'static,
 {
 	let FuzzAppendPayload(to_fuzz, initial) = payload;
