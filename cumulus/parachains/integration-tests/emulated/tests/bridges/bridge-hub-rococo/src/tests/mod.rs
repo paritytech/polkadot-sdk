8f42b3e1d0267bb564facaadb90d2391dd58ff0d
ndk <ndk@parity.io>
2024-10-21 15:33:01 +0300

    formatted the imports

--- cumulus/parachains/integration-tests/emulated/tests/bridges/bridge-hub-rococo/src/tests/mod.rs
+++ cumulus/parachains/integration-tests/emulated/tests/bridges/bridge-hub-rococo/src/tests/mod.rs
@@ -13,8 +13,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use xcm::opaque::v5;
 use crate::imports::*;
+use xcm::opaque::v5;
 
 mod asset_transfers;
 mod claim_assets;
