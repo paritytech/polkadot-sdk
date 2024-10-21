0f3c3998a7f13e6498377d66b0057fe8bf97fc83
Kian Paimani <5588131+kianenigma@users.noreply.github.com>
2024-10-21 13:35:14 +0100

    Update docs/sdk/src/external_resources.rs
    Co-authored-by: Shawn Tabrizi <shawntabrizi@gmail.com>

--- docs/sdk/src/external_resources.rs
+++ docs/sdk/src/external_resources.rs
@@ -7,7 +7,7 @@
 //! third-parties, and are therefore subject to more variability. Any further resources may be added
 //! by opening a pull request to the `polkadot-sdk` repository.
 //!
-//! - [Polkadot NFT Tutorial by Polkadot Fellow Shawn Tabrizi](https://www.shawntabrizi.com/substrate-collectables-workshop/0/index.html)
+//! - [Polkadot NFT Marketplace Tutorial by Polkadot Fellow Shawn Tabrizi](https://www.shawntabrizi.com/substrate-collectables-workshop/)
 //! - [DOT Code School](https://dotcodeschool.com/)
 //! - [Polkadot Developers](https://github.com/polkadot-developers/)
 //! - [Polkadot Blockchain Academy](https://github.com/Polkadot-Blockchain-Academy)
