6e6f1ace9d4dbc26294426f25523d6f676f25b0a
giuseppere <giuseppe.re@parity.io>
2024-10-22 17:04:03 +0200

    Revert "moving imports from support to umbrella crate"
    This reverts commit 2c910cb410c1de3ac860993cc7c0f19342311ff0.

--- substrate/frame/support/src/lib.rs
+++ substrate/frame/support/src/lib.rs
@@ -887,7 +887,10 @@ pub mod testing_prelude {
 pub mod pallet_prelude {
 	pub use crate::{
 		defensive, defensive_assert,
-		dispatch::{DispatchClass, DispatchResult, DispatchResultWithPostInfo, Parameter, Pays},
+		dispatch::{
+			DispatchClass, DispatchResult, DispatchResultWithPostInfo, GetDispatchInfo, Parameter,
+			Pays, PostDispatchInfo,
+		},
 		ensure,
 		inherent::{InherentData, InherentIdentifier, ProvideInherent},
 		storage,
@@ -903,8 +906,9 @@ pub mod pallet_prelude {
 			StorageList,
 		},
 		traits::{
-			BuildGenesisConfig, ConstU32, EnsureOrigin, Get, GetDefault, GetStorageVersion, Hooks,
-			IsType, PalletInfoAccess, StorageInfoTrait, StorageVersion, Task, TypedGet,
+			BuildGenesisConfig, ConstU32, Contains, EnsureOrigin, Get, GetDefault,
+			GetStorageVersion, Hooks, IsSubType, IsType, OnRuntimeUpgrade, PalletInfoAccess,
+			StorageInfoTrait, StorageVersion, Task, TypedGet,
 		},
 		Blake2_128, Blake2_128Concat, Blake2_256, CloneNoBound, DebugNoBound, EqNoBound, Identity,
 		PartialEqNoBound, RuntimeDebugNoBound, Twox128, Twox256, Twox64Concat,
