70350bb7d3f8564789319df01962e58c848fc9ec
Maksym H <mordamax@gmail.com>
2024-10-25 19:32:40 +0100

    add cache and remove forklift

--- .github/scripts/cmd/cmd.py
+++ .github/scripts/cmd/cmd.py
@@ -142,7 +142,7 @@ def main():
 
         # loop over remaining runtimes to collect available pallets
         for runtime in runtimesMatrix.values():
-            build_command = f"forklift cargo build -p {runtime['package']} --profile {profile} --features={runtime['bench_features']}"
+            build_command = f"cargo build -p {runtime['package']} --profile {profile} --features={runtime['bench_features']}"
             print(f'-- building "{runtime["name"]}" with `{build_command}`')
             os.system(build_command)
             print(f'-- listing pallets for benchmark for {runtime["name"]}')
@@ -286,7 +286,7 @@ def main():
 
         # loop over remaining runtimes to collect available pallets
         for runtime in runtimesMatrix.values():
-            build_command = f"forklift cargo build -p {runtime['old_package']} --profile {profile} --features={runtime['bench_features']} --locked"
+            build_command = f"cargo build -p {runtime['old_package']} --profile {profile} --features={runtime['bench_features']} --locked"
             print(f'-- building {runtime["name"]} with `{build_command}`')
             os.system(build_command)
             
