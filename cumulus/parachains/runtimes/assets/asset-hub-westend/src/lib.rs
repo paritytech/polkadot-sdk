f618c3f4d673eecd2361d3940daeea1b15054003
pgherveou <pgherveou@gmail.com>
2024-10-31 16:50:43 +0100

    Bump asset-hub-westend spec version

--- cumulus/parachains/runtimes/assets/asset-hub-westend/src/lib.rs
+++ cumulus/parachains/runtimes/assets/asset-hub-westend/src/lib.rs
@@ -124,7 +124,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("westmint"),
 	impl_name: create_runtime_str!("westmint"),
 	authoring_version: 1,
-	spec_version: 1_016_002,
+	spec_version: 1_016_003,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 16,
