0460a797e4b2887b425c493092cb8ed28424a39b
Adrian Catangiu <adrian@parity.io>
2024-11-05 11:47:22 +0200

    snowbridge: allow account conversion for Ethereum accounts (#6221)
    Replace `GlobalConsensusEthereumConvertsFor` with
    `EthereumLocationsConverterFor` that allows `Location` to `AccountId`
    conversion for the Ethereum network root as before, but also for
    Ethereum contracts and accounts.
    
    The new converter only matches explicit `parents: 2` Ethereum locations,
    meaning it should be used only on/by parachains.

--- cumulus/parachains/runtimes/assets/asset-hub-rococo/src/xcm_config.rs
+++ cumulus/parachains/runtimes/assets/asset-hub-rococo/src/xcm_config.rs
@@ -42,7 +42,7 @@ use parachains_common::{
 };
 use polkadot_parachain_primitives::primitives::Sibling;
 use polkadot_runtime_common::xcm_sender::ExponentialPrice;
-use snowbridge_router_primitives::inbound::GlobalConsensusEthereumConvertsFor;
+use snowbridge_router_primitives::inbound::EthereumLocationsConverterFor;
 use sp_runtime::traits::{AccountIdConversion, ConvertInto, TryConvertInto};
 use testnet_parachains_constants::rococo::snowbridge::{
 	EthereumNetwork, INBOUND_QUEUE_PALLET_INDEX,
@@ -104,7 +104,7 @@ pub type LocationToAccountId = (
 	GlobalConsensusParachainConvertsFor<UniversalLocation, AccountId>,
 	// Ethereum contract sovereign account.
 	// (Used to get convert ethereum contract locations to sovereign account)
-	GlobalConsensusEthereumConvertsFor<AccountId>,
+	EthereumLocationsConverterFor<AccountId>,
 );
 
 /// Means for transacting the native currency on this chain.
