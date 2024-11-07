553d896fdcad3a8679372b0df26e61e4fbf22293
Andrei Eres <eresav@me.com>
2024-11-07 12:32:55 +0100

    Add licenses

--- substrate/client/network/benches/notifications_protocol.rs
+++ substrate/client/network/benches/notifications_protocol.rs
@@ -1,3 +1,21 @@
+// This file is part of Substrate.
+
+// Copyright (C) Parity Technologies (UK) Ltd.
+// SPDX-License-Identifier: GPL-3.0-or-later WITH Classpath-exception-2.0
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 use criterion::{
 	criterion_group, criterion_main, AxisScale, BenchmarkId, Criterion, PlotConfiguration,
 	Throughput,
@@ -33,6 +51,7 @@ const EXPONENTS: &[(u32, &'static str)] = &[
 	(27, "128MB"),
 ];
 
+// TODO: It's be better to bind system-provided port when initializing the worker
 fn get_listen_address() -> sc_network::Multiaddr {
 	let ip = Ipv4Addr::from_str("127.0.0.1").unwrap();
 	let listener = TcpListener::bind((IpAddr::V4(ip), 0)).unwrap(); // Bind to a random port
