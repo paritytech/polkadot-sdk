c3bf0e80a60e8698d69c528213ea2a5d3b504943
georgepisaltu <george.pisaltu@parity.io>
2024-11-11 11:43:47 +0200

    Fix weight compilation
    Signed-off-by: georgepisaltu <george.pisaltu@parity.io>

--- substrate/frame/proxy/src/weights.rs
+++ substrate/frame/proxy/src/weights.rs
@@ -46,8 +46,7 @@
 #![allow(unused_imports)]
 #![allow(missing_docs)]
 
-use frame_support::{traits::Get, weights::{Weight, constants::RocksDbWeight}};
-use core::marker::PhantomData;
+use frame::weights_prelude::*;
 
 /// Weight functions needed for `pallet_proxy`.
 pub trait WeightInfo {
