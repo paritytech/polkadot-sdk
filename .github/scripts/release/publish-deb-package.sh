2536740533200f971d121d4e095960b956c2939f
EgorPopelyaev <egor@parity.io>
2024-11-12 16:49:26 +0100

    try installing awscli directly

--- .github/scripts/release/publish-deb-package.sh
+++ .github/scripts/release/publish-deb-package.sh
@@ -12,7 +12,9 @@ aws_deb_path="s3://releases-package-repos/deb"
 
 # Add a deb to our apt repo
 add_deb(){
-  alias aws='podman run --rm -it -e AWS_ACCESS_KEY_ID=${AWS_RELEASE_ACCESS_KEY_ID} -e AWS_SECRET_ACCESS_KEY=${AWS_RELEASE_SECRET_ACCESS_KEY} -e AWS_BUCKET docker.io/paritytech/awscli aws'
+  #alias aws='podman run --rm -it -e AWS_ACCESS_KEY_ID=${AWS_RELEASE_ACCESS_KEY_ID} -e AWS_SECRET_ACCESS_KEY=${AWS_RELEASE_SECRET_ACCESS_KEY} -e AWS_BUCKET docker.io/paritytech/awscli aws'
+  export AWS_ACCESS_KEY_ID=${AWS_RELEASE_ACCESS_KEY_ID}
+  export AWS_SECRET_ACCESS_KEY=${AWS_RELEASE_SECRET_ACCESS_KEY}
 
   # Download the current state of the deb repo
   aws s3 sync "$aws_deb_path/db" "$local_deb_repo_path/db"
