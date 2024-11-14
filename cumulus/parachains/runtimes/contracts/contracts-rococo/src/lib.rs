0ad2f2be608b7fbc867fb55b938874e0aad701da
dmoka <mokabme@gmail.com>
2024-11-14 13:50:16 +0100

    add missing hooks

--- cumulus/parachains/runtimes/contracts/contracts-rococo/src/lib.rs
+++ cumulus/parachains/runtimes/contracts/contracts-rococo/src/lib.rs
@@ -272,6 +272,8 @@ impl pallet_utility::Config for Runtime {
 	type RuntimeEvent = RuntimeEvent;
 	type RuntimeCall = RuntimeCall;
 	type PalletsOrigin = OriginCaller;
+	type BatchPreHook = ();
+	type BatchPostHook = ();
 	type WeightInfo = pallet_utility::weights::SubstrateWeight<Runtime>;
 }
 
