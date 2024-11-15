d7d4e9083df648007108565bea9a47f957473dc8
PG Herveou <pgherveou@gmail.com>
2024-11-15 09:31:25 +0100

    [pallet-revive] use evm decimals in call host fn (#6466)
    This PR update the pallet to use the EVM 18 decimal balance in contracts
    call and host functions instead of the native balance.
    
    It also updates the js example to add the piggy-bank solidity contract
    that expose the problem
    
    ---------
    
    Co-authored-by: GitHub Action <action@github.com>

--- substrate/frame/revive/rpc/examples/js/src/event.ts
+++ substrate/frame/revive/rpc/examples/js/src/event.ts
@@ -3,8 +3,8 @@ import { call, getContract, deploy } from './lib.ts'
 
 try {
 	const { abi, bytecode } = getContract('event')
-	const address = await deploy(bytecode, abi)
-	const receipt = await call('triggerEvent', address, abi)
+	const contract = await deploy(bytecode, abi)
+	const receipt = await call('triggerEvent', await contract.getAddress(), abi)
 	if (receipt) {
 		for (const log of receipt.logs) {
 			console.log('Event log:', JSON.stringify(log, null, 2))
