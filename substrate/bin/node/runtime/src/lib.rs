c6faf727cb6d53aa84d67cf4e5357251dc16ebca
Dom <polkadotdom@gmail.com>
2024-11-19 10:18:57 -0500

    Add BlockNumberProvider to kitchen sink

--- substrate/bin/node/runtime/src/lib.rs
+++ substrate/bin/node/runtime/src/lib.rs
@@ -1988,6 +1988,7 @@ impl pallet_core_fellowship::Config for Runtime {
 	type FastPromoteOrigin = Self::PromoteOrigin;
 	type EvidenceSize = ConstU32<16_384>;
 	type MaxRank = ConstU32<9>;
+	type BlockNumberProvider = System;
 }
 
 parameter_types! {
