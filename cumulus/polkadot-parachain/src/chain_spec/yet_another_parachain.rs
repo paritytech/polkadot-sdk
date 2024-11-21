1884efc7761715c8099eff8592677ca250a66c6d
ordian <noreply@reusable.software>
2024-11-21 11:15:50 +0100

    Revert "remove prefunded accounts"
    This reverts commit 10725ea15cfc04e3536a48a4c5f0524453a47594.

--- cumulus/polkadot-parachain/src/chain_spec/yet_another_parachain.rs
+++ cumulus/polkadot-parachain/src/chain_spec/yet_another_parachain.rs
@@ -19,9 +19,43 @@
 use parachains_common::AccountId;
 use polkadot_parachain_lib::chain_spec::{Extensions, GenericChainSpec};
 use sc_chain_spec::ChainType;
-use sp_core::hex2array;
+use sp_core::{hex2array, sr25519::Pair as SrPair, Pair};
 use sp_keyring::Sr25519Keyring as Keyring;
 use yet_another_parachain_runtime::AuraId;
+
+const NUM_ACCOUNT_PAIRS: usize = 16000;
+
+fn derive_accounts(n: usize, seed: String) -> Vec<SrPair> {
+	let t = std::cmp::min(
+		n,
+		std::thread::available_parallelism().unwrap_or(1usize.try_into().unwrap()).get(),
+	);
+
+	let mut tn = (0..t).cycle();
+	let mut tranges: Vec<_> = (0..t).map(|_| Vec::new()).collect();
+	(0..n).for_each(|i| tranges[tn.next().unwrap()].push(i));
+	let mut threads = Vec::new();
+
+	tranges.into_iter().for_each(|chunk| {
+		let seed = seed.clone();
+		threads.push(std::thread::spawn(move || {
+			chunk
+				.into_iter()
+				.map(move |i| {
+					let derivation = format!("{seed}/{i}");
+					<SrPair as Pair>::from_string(&derivation, None).unwrap()
+				})
+				.collect::<Vec<_>>()
+		}));
+	});
+
+	threads
+		.into_iter()
+		.map(|h| h.join().unwrap())
+		.flatten()
+		// .map(|p| (p, funds))
+		.collect()
+}
 
 pub fn yet_another_parachain_config(
 	relay: impl Into<String>,
@@ -38,13 +72,24 @@ pub fn yet_another_parachain_config(
 	//   SS58 Address:       EnqtFmsXcGdSnWk5JWUMXyPVamjiFQurXxcNgJEg1C3sw6W
 	let yap_sudo: AccountId =
 		hex2array!("6205a2a2aecb71c13d8ad3197e12c10bcdcaa0c9f176997bc236c6b39143aa15").into();
-	let endowed_accounts = vec![
+	let mut endowed_accounts = vec![
 		yap_sudo.clone(),
 		Keyring::Alice.to_account_id(),
 		Keyring::Bob.to_account_id(),
 		Keyring::AliceStash.to_account_id(),
 		Keyring::BobStash.to_account_id(),
 	];
+
+	endowed_accounts.extend(
+		derive_accounts(NUM_ACCOUNT_PAIRS, "//Sender".into())
+			.into_iter()
+			.map(|k| k.public().into()),
+	);
+	endowed_accounts.extend(
+		derive_accounts(NUM_ACCOUNT_PAIRS, "//Receiver".into())
+			.into_iter()
+			.map(|k| k.public().into()),
+	);
 
 	GenericChainSpec::builder(
 		yet_another_parachain_runtime::WASM_BINARY
