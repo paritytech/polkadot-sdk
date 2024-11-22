acd4282db2c98eb1114b1cfc90a4cbb4aa4a14c4
gui <gui.thiolliere@gmail.com>
2024-11-22 11:34:08 +0900

    update doc + example weight is zero

--- substrate/frame/support/procedural/src/lib.rs
+++ substrate/frame/support/procedural/src/lib.rs
@@ -1480,10 +1480,12 @@ pub fn authorize(_: TokenStream, _: TokenStream) -> TokenStream {
 ///
 /// See [`frame_support_procedural::authorize`] for more information on how authorization works.
 ///
-/// # Example:
-/// ```
-/// // TODO TODO: copy paste
-/// ```
+/// Defines the value of the weight of the authorize function. This attribute is similar to
+/// `#[pallet::weight]`:
+/// * it can be ignore in `dev_mode`
+/// * it can be automatically infered from weight info. For the call `foo` the function
+///   `authorize_foo` in the weight info will be used.
+/// * it can be a fixed value like `Weight::from_all(0)` (not recommended in production).
 #[proc_macro_attribute]
 pub fn weight_of_authorize(_: TokenStream, _: TokenStream) -> TokenStream {
 	pallet_macro_stub()
