eb1d5783a4879f3b55d6d4e5108f4e3bb4a12797
Dom <polkadotdom@gmail.com>
2024-11-23 11:26:41 -0500

    Update migration

--- substrate/frame/core-fellowship/src/migration.rs
+++ substrate/frame/core-fellowship/src/migration.rs
@@ -68,6 +68,17 @@ mod v0 {
 		StorageValue<Pallet<T, I>, ParamsOf<T, I>, ValueQuery>;
 }
 
+mod v1 {
+	use super::*;
+	use frame_system::pallet_prelude::BlockNumberFor as LocalBlockNumberFor;
+
+	pub type ParamsOf<T, I> = ParamsType<<T as Config<I>>::Balance, LocalBlockNumberFor<T>, <T as Config<I>>::MaxRank>;
+	/// V1 type for [`crate::Params`].
+	#[storage_alias]
+	pub type Params<T: Config<I>, I: 'static> =
+		StorageValue<Pallet<T, I>, ParamsOf<T, I>, ValueQuery>;
+}
+
 pub struct MigrateToV1<T, I = ()>(PhantomData<(T, I)>);
 impl<T: Config<I>, I: 'static> UncheckedOnRuntimeUpgrade for MigrateToV1<T, I> {
 	#[cfg(feature = "try-runtime")]
@@ -94,8 +105,54 @@ impl<T: Config<I>, I: 'static> UncheckedOnRuntimeUpgrade for MigrateToV1<T, I> {
 			),
 			offboard_timeout: old_value.offboard_timeout,
 		};
-		crate::Params::<T, I>::put(new);
+		v1::Params::<T, I>::put(new);
 		T::DbWeight::get().reads_writes(1, 1)
+	}
+}
+
+mod v2 {
+	use super::*;
+	use crate::BlockNumberFor as NewBlockNumberFor;
+	use frame_system::pallet_prelude::BlockNumberFor as LocalBlockNumberFor;
+
+	pub trait ConvertBlockNumber<L, N>
+	{
+		/// Converts the local block number to the new block number type.
+		fn convert(local: L) -> N;
+
+		/// Adds an offset to new block number if necessary
+		/// 
+		/// For instance - if your new version uses the relay chain number, you'll want to add (RC Num - Local Num)
+		fn add_offset(current: N) -> N;
+
+		/// Returns the equivalent time duration as the previous type when represented as the new type
+		/// 
+		/// For instance - If you previously had 12s blocks and are now following the relay chain's 6
+		/// values should be 2x the old to achieve the same duration in time
+		fn equivalent_time(local: L) -> N;
+	}
+
+	pub struct MigrateToV2<T, BlockNumberConverter, I = ()>(PhantomData<(T, BlockNumberConverter, I)>);
+	impl<T: Config<I>, BlockNumberConverter, I: 'static> UncheckedOnRuntimeUpgrade
+		for MigrateToV2<T, I, BlockNumberConverter>
+	where
+		BlockNumberConverter: ConvertBlockNumber<LocalBlockNumberFor<T>, NewBlockNumberFor<T, I>>,
+	{
+		fn on_runtime_upgrade() -> frame_support::weights::Weight {
+			// Params conversion
+			let _ = Params::<T, I>::translate::<v1::ParamsOf<T, I>, _>(|x| {
+				let prev = x.unwrap();
+				let _ = prev.demotion_period.into_iter().map(|block_number| {
+					BlockNumberConverter::equivalent_time(block_number);
+					T::DbWeight::get().reads_writes(1, 1);
+				});
+				None
+			});
+
+			// Member conversion
+
+			T::DbWeight::get().reads_writes(1, 1)
+		}
 	}
 }
 
@@ -108,6 +165,19 @@ pub type MigrateV0ToV1<T, I> = frame_support::migrations::VersionedMigration<
 	0, // The migration will only execute when the on-chain storage version is 0
 	1, // The on-chain storage version will be set to 1 after the migration is complete
 	MigrateToV1<T, I>,
+	crate::pallet::Pallet<T, I>,
+	<T as frame_system::Config>::DbWeight,
+>;
+
+/// [`UncheckedOnRuntimeUpgrade`] implementation [`MigrateToV2`] wrapped in a
+/// [`VersionedMigration`](frame_support::migrations::VersionedMigration), which ensures that:
+/// - The migration only runs once when the on-chain storage version is `1`.
+/// - The on-chain storage version is updated to `2` after the migration executes.
+/// - Reads/Writes from checking/settings the on-chain storage version are accounted for.
+pub type MigrateV1ToV2<T, BC, I> = frame_support::migrations::VersionedMigration<
+	1, // The migration will only execute when the on-chain storage version is 0
+	2, // The on-chain storage version will be set to 1 after the migration is complete
+	v2::MigrateToV2<T, BC, I>,
 	crate::pallet::Pallet<T, I>,
 	<T as frame_system::Config>::DbWeight,
 >;
