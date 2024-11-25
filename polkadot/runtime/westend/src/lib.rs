21744f698c9e649b87b8e92f0e082d811ffccd5a
muharem <ismailov.m.h@gmail.com>
2024-11-25 15:36:10 +0100

    undo fmt

--- polkadot/runtime/westend/src/lib.rs
+++ polkadot/runtime/westend/src/lib.rs
@@ -1140,7 +1140,8 @@ impl InstanceFilter<RuntimeCall> for ProxyType {
 				matches!(
 					c,
 					RuntimeCall::Staking(..) |
-						RuntimeCall::Session(..) | RuntimeCall::Utility(..) |
+						RuntimeCall::Session(..) |
+						RuntimeCall::Utility(..) |
 						RuntimeCall::FastUnstake(..) |
 						RuntimeCall::VoterList(..) |
 						RuntimeCall::NominationPools(..)
