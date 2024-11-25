d4e79119438b4d7443e705aae586c4936d1d355d
muharem <ismailov.m.h@gmail.com>
2024-11-25 17:41:41 +0100

    remove unstable

--- substrate/frame/meta-tx/src/lib.rs
+++ substrate/frame/meta-tx/src/lib.rs
@@ -105,16 +105,7 @@ pub mod pallet {
 	use super::*;
 
 	#[pallet::config]
-	pub trait Config:
-		frame_system::Config<
-		RuntimeCall: Dispatchable<
-			Info = DispatchInfo,
-			PostInfo = PostDispatchInfo,
-			RuntimeOrigin = <Self as frame_system::Config>::RuntimeOrigin,
-		>,
-		RuntimeOrigin: AsTransactionAuthorizedOrigin + From<SystemOrigin<Self::AccountId>>,
-	>
-	{
+	pub trait Config: frame_system::Config {
 		/// Weight information for calls in this pallet.
 		type WeightInfo: WeightInfo;
 		/// The overarching origin type.
