75d3c064dd781fbef03cb5c24adcde299028b9ce
claravanstaden <claravanstaden64@gmail.com>
2024-11-27 11:44:21 +0200

    adds test for xcm register command

--- cumulus/parachains/integration-tests/emulated/tests/bridges/bridge-hub-westend/src/tests/snowbridge_v2.rs
+++ cumulus/parachains/integration-tests/emulated/tests/bridges/bridge-hub-westend/src/tests/snowbridge_v2.rs
@@ -17,6 +17,11 @@ use bridge_hub_westend_runtime::EthereumInboundQueueV2;
 use frame_support::weights::WeightToFee;
 use snowbridge_router_primitives::inbound::v2::{Asset::NativeTokenERC20, Message};
 use sp_core::H160;
+use sp_core::H256;
+use codec::Encode;
+use sp_runtime::MultiAddress;
+use bridge_hub_westend_runtime::bridge_to_ethereum_config::CreateAssetCall;
+use bridge_hub_westend_runtime::bridge_to_ethereum_config::CreateAssetDeposit;
 use testnet_parachains_constants::westend::fee::WeightToFee as WeightCalculator;
 
 /// Calculates the XCM prologue fee for sending an XCM to AH.
@@ -108,3 +113,58 @@ fn xcm_prologue_fee() {
 	let buffered_fee = execution_fee * 2;
 	println!("buffered execution fee for prologue for 8 assets: {}", buffered_fee);
 }
+
+#[test]
+fn register_token_xcm() {
+	BridgeHubWestend::execute_with(|| {
+		let weth_contract = H160::zero(); // weth contract placeholder
+		let token: H160 = H160::zero(); // token id placeholder
+		let owner: H256 = H256::zero(); // bridge placeholder
+		let dot_to_eth_rate: u128 = 2_500_000_000_000_000;
+
+		let ethereum_network_v5: NetworkId = EthereumNetwork::get().into();
+		let weth_asset = Location::new(
+			2,
+			[
+				GlobalConsensus(ethereum_network_v5),
+				AccountKey20 { network: None, key: weth_contract.into() },
+			],
+		);
+		let dot_asset = Location::new(1, Here);
+
+		let weth_asset_creation: u128 = (CreateAssetDeposit::get() * dot_to_eth_rate) / 10_u128.pow(18);
+		let weth_fee: xcm::prelude::Asset = (weth_asset, weth_asset_creation).into();
+		let dot_fee: xcm::prelude::Asset = (dot_asset, CreateAssetDeposit::get()).into();
+
+		println!("weth_asset_creation: {:?}", weth_asset_creation);
+
+		let asset_id = Location::new(
+			2,
+			[GlobalConsensus(ethereum_network_v5), AccountKey20 { network: None, key: token.into() }],
+			);
+
+		let register_token_xcm =
+			vec![
+				ExchangeAsset { give: weth_fee.into(), want: dot_fee.clone().into(), maximal: false },
+				PayFees { asset: dot_fee },
+				Transact {
+					origin_kind: OriginKind::Xcm,
+					call: (
+						CreateAssetCall::get(),
+						asset_id,
+						MultiAddress::<[u8; 32], ()>::Id(owner.into()),
+						1,
+					)
+					.encode()
+					.into(),
+				},
+			];
+		let message_xcm: Xcm<()> = register_token_xcm.into();
+		let versioned_message_xcm = VersionedXcm::V5(message_xcm);
+
+		let xcm_bytes = versioned_message_xcm.encode();
+		let hex_string = hex::encode(xcm_bytes);
+		println!("register token hex: {:x?}", hex_string);
+	});
+}
+
