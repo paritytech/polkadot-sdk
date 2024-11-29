10dcaf57980e6c291b8a968bd8605bcb1e30374f
ron <yrong1997@gmail.com>
2024-11-29 16:26:01 +0800

    More cleanup

--- bridges/snowbridge/primitives/core/src/operating_mode.rs
+++ bridges/snowbridge/primitives/core/src/operating_mode.rs
@@ -4,6 +4,7 @@ use sp_runtime::RuntimeDebug;
 
 /// Basic operating modes for a bridges module (Normal/Halted).
 #[derive(Encode, Decode, Clone, Copy, PartialEq, Eq, RuntimeDebug, TypeInfo, MaxEncodedLen)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub enum BasicOperatingMode {
 	/// Normal mode, when all operations are allowed.
 	Normal,
