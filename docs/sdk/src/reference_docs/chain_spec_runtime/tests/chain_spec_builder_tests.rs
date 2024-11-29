18f87be50b3260aa11d46bee2417360f966b7735
ndkazu <ndongmefane@gmail.com>
2024-11-30 00:58:23 +0900

    variable name

--- docs/sdk/src/reference_docs/chain_spec_runtime/tests/chain_spec_builder_tests.rs
+++ docs/sdk/src/reference_docs/chain_spec_runtime/tests/chain_spec_builder_tests.rs
@@ -31,9 +31,9 @@ fn get_chain_spec_builder_path() -> &'static str {
 }
 
 #[docify::export_content]
-fn cmd_list_presets(path_runtime: &str) -> String {
+fn cmd_list_presets(runtime_path: &str) -> String {
 	bash!(
-		chain-spec-builder list-presets -r $path_runtime
+		chain-spec-builder list-presets -r $runtime_path
 	)
 }
 
@@ -57,9 +57,9 @@ fn list_presets() {
 }
 
 #[docify::export_content]
-fn cmd_get_preset(path_runtime: &str) -> String {
+fn cmd_get_preset(runtime_path: &str) -> String {
 	bash!(
-		chain-spec-builder display-preset -r $path_runtime -p preset_2
+		chain-spec-builder display-preset -r $runtime_path -p preset_2
 	)
 }
 
@@ -87,9 +87,9 @@ fn get_preset() {
 }
 
 #[docify::export_content]
-fn cmd_generate_chain_spec(path_runtime: &str) -> String {
+fn cmd_generate_chain_spec(runtime_path: &str) -> String {
 	bash!(
-		chain-spec-builder -c /dev/stdout create -r $path_runtime named-preset preset_2
+		chain-spec-builder -c /dev/stdout create -r $runtime_path named-preset preset_2
 	)
 }
 
@@ -136,9 +136,9 @@ fn generate_chain_spec() {
 }
 
 #[docify::export_content]
-fn cmd_generate_para_chain_spec(path_runtime: &str) -> String {
+fn cmd_generate_para_chain_spec(runtime_path: &str) -> String {
 	bash!(
-		chain-spec-builder -c /dev/stdout create -c polkadot -p 1000 -r $path_runtime named-preset preset_2
+		chain-spec-builder -c /dev/stdout create -c polkadot -p 1000 -r $runtime_path named-preset preset_2
 	)
 }
 
