5d32b1699743a52933a91b2fb3f88d64e67ff529
Iulian Barbu <iulian.barbu@parity.io>
2024-11-30 23:32:04 +0000

    revert MinimumPeriod
    Signed-off-by: Iulian Barbu <iulian.barbu@parity.io>

--- templates/parachain/runtime/src/configs/mod.rs
+++ templates/parachain/runtime/src/configs/mod.rs
@@ -134,7 +134,7 @@ impl pallet_timestamp::Config for Runtime {
 	/// A timestamp: milliseconds since the unix epoch.
 	type Moment = u64;
 	type OnTimestampSet = Aura;
-	type MinimumPeriod = ConstU64<MINIMUM_PERIOD>;
+	type MinimumPeriod = ConstU64<0>;
 	type WeightInfo = ();
 }
 
