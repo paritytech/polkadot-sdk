68ba8b7bedeeb907f84b4419f104a012adbd9afd
Branislav Kontur <bkontur@gmail.com>
2024-12-03 23:53:28 +0100

    Update cumulus/parachains/runtimes/people/people-rococo/tests/tests.rs

--- cumulus/parachains/runtimes/people/people-rococo/tests/tests.rs
+++ cumulus/parachains/runtimes/people/people-rococo/tests/tests.rs
@@ -16,7 +16,7 @@
 
 #![cfg(test)]
 
-use people_rococo_runtime::Block, Runtime, RuntimeCall, RuntimeOrigin;
+use people_rococo_runtime::{Block, Runtime, RuntimeCall, RuntimeOrigin};
 
 #[test]
 fn xcm_payment_api_works() {
