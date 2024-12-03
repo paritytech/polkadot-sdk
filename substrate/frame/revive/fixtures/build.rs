0183ff5fdb13cf94f8d253175ec8feb6d5ac8ecd
Alexander Theißen <alex.theissen@me.com>
2024-12-03 13:44:52 +0100

    pallet-revive-fixtures: Try not to re-create fixture dir (#6735)
    On some systems trying to re-create the output directory will lead to an
    error.
    
    Fixes https://github.com/paritytech/subxt/issues/1876
    
    ---------
    
    Co-authored-by: Bastian Köcher <git@kchr.de>

--- substrate/frame/revive/fixtures/build.rs
+++ substrate/frame/revive/fixtures/build.rs
@@ -204,10 +204,15 @@ fn create_out_dir() -> Result<PathBuf> {
 	.join("pallet-revive-fixtures");
 
 	// clean up some leftover symlink from previous versions of this script
-	if out_dir.exists() && !out_dir.is_dir() {
+	let mut out_exists = out_dir.exists();
+	if out_exists && !out_dir.is_dir() {
 		fs::remove_file(&out_dir)?;
+		out_exists = false;
 	}
-	fs::create_dir_all(&out_dir).context("Failed to create output directory")?;
+
+	if !out_exists {
+		fs::create_dir(&out_dir).context("Failed to create output directory")?;
+	}
 
 	// write the location of the out dir so it can be found later
 	let mut file = fs::File::create(temp_dir.join("fixture_location.rs"))
