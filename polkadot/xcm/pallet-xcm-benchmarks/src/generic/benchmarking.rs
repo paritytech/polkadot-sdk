464631c3549b59bf782973511123117a8687f972
Francisco Aguirre <franciscoaguirreperez@gmail.com>
2024-12-05 17:27:39 +0100

    Added fallback_max_weight to Transact for sending messages to V4 chains (#6643)
    Closes: https://github.com/paritytech/polkadot-sdk/issues/6585
    
    Removing the `require_weight_at_most` parameter in V5 Transact had only
    one problem. Converting a message from V5 to V4 to send to chains that
    didn't upgrade yet. The conversion would not know what weight to give to
    the Transact, since V4 and below require it.
    
    To fix this, I added back the weight in the form of an `Option<Weight>`
    called `fallback_max_weight`. This can be set to `None` if you don't
    intend to deal with a chain that hasn't upgraded yet. If you set it to
    `Some(_)`, the behaviour is the same. The plan is to totally remove this
    in V6 since there will be a good conversion path from V6 to V5.
    
    ---------
    
    Co-authored-by: GitHub Action <action@github.com>
    Co-authored-by: Adrian Catangiu <adrian@parity.io>

--- polkadot/xcm/pallet-xcm-benchmarks/src/generic/benchmarking.rs
+++ polkadot/xcm/pallet-xcm-benchmarks/src/generic/benchmarking.rs
@@ -152,6 +152,7 @@ benchmarks! {
 		let instruction = Instruction::Transact {
 			origin_kind: OriginKind::SovereignAccount,
 			call: double_encoded_noop_call,
+			fallback_max_weight: None,
 		};
 		let xcm = Xcm(vec![instruction]);
 	}: {
