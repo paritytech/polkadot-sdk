17847f423c75455fb1cc33a4f5828ca89ff9c097
github-action-benchmark <github@users.noreply.github.com>
2024-12-09 00:35:29 +0000

    add availability-distribution-regression-bench (customSmallerIsBetter) benchmark result for 906fa9e51306635245a22e03160d1c761fae6cc3

--- bench/availability-distribution-regression-bench/data.js
+++ bench/availability-distribution-regression-bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1733495114400,
+  "lastUpdate": 1733704528586,
   "repoUrl": "https://github.com/paritytech/polkadot-sdk",
   "entries": {
     "availability-distribution-regression-bench": [
@@ -41227,6 +41227,60 @@ window.BENCHMARK_DATA = {
           {
             "name": "bitfield-distribution",
             "value": 0.024702893313333325,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "franciscoaguirreperez@gmail.com",
+            "name": "Francisco Aguirre",
+            "username": "franciscoaguirre"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "906fa9e51306635245a22e03160d1c761fae6cc3",
+          "message": "XCM V5 - SetHints instruction (#6566)\n\nLast feature we wanted for V5, changing `SetAssetClaimer` to be just one\nof many possible \"hints\" that you can specify at the beginning of your\nprogram to change its behaviour.\n\nThis makes it easier to add new hints in the future and have barriers\naccept them.\n\n---------\n\nCo-authored-by: GitHub Action <action@github.com>",
+          "timestamp": "2024-12-08T23:02:38Z",
+          "tree_id": "eb5adfc56c5c6a486ea39ced93a49577a136f4a6",
+          "url": "https://github.com/paritytech/polkadot-sdk/commit/906fa9e51306635245a22e03160d1c761fae6cc3"
+        },
+        "date": 1733704510294,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Sent to peers",
+            "value": 18481.666666666653,
+            "unit": "KiB"
+          },
+          {
+            "name": "Received from peers",
+            "value": 433.3333333333332,
+            "unit": "KiB"
+          },
+          {
+            "name": "test-environment",
+            "value": 0.008758708920000082,
+            "unit": "seconds"
+          },
+          {
+            "name": "availability-distribution",
+            "value": 0.015918193706666674,
+            "unit": "seconds"
+          },
+          {
+            "name": "availability-store",
+            "value": 0.17697349099999993,
+            "unit": "seconds"
+          },
+          {
+            "name": "bitfield-distribution",
+            "value": 0.024576211853333328,
             "unit": "seconds"
           }
         ]
