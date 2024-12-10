9832fc5898a42d7479876239c6267641058fba64
Francisco Aguirre <franciscoaguirreperez@gmail.com>
2024-12-09 23:27:43 -0300

    fix(pallet-xcm-benchmarks): rename set_asset_claimer to just asset_claimer

--- polkadot/runtime/westend/src/weights/xcm/pallet_xcm_benchmarks_generic.rs
+++ polkadot/runtime/westend/src/weights/xcm/pallet_xcm_benchmarks_generic.rs
@@ -84,7 +84,7 @@ impl<T: frame_system::Config> WeightInfo<T> {
 		// Minimum execution time: 3_491_000 picoseconds.
 		Weight::from_parts(3_667_000, 0)
 	}
-	pub(crate) fn set_asset_claimer() -> Weight {
+	pub(crate) fn asset_claimer() -> Weight {
 		// Proof Size summary in bytes:
 		//  Measured:  `0`
 		//  Estimated: `0`
