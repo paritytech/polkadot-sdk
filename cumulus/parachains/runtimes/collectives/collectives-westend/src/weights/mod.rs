da5ab678297686e6bac73e65772b83257a430e9a
Francisco Aguirre <franciscoaguirreperez@gmail.com>
2024-12-09 23:55:45 -0300

    feat(collectives-westend-runtime): add benchmarks

--- cumulus/parachains/runtimes/collectives/collectives-westend/src/weights/mod.rs
+++ cumulus/parachains/runtimes/collectives/collectives-westend/src/weights/mod.rs
@@ -46,6 +46,7 @@ pub mod pallet_utility;
 pub mod pallet_xcm;
 pub mod paritydb_weights;
 pub mod rocksdb_weights;
+pub mod xcm;
 
 pub use block_weights::constants::BlockExecutionWeight;
 pub use extrinsic_weights::constants::ExtrinsicBaseWeight;
