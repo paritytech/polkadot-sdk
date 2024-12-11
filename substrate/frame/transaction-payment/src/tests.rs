fa11988f1d6e9256d5bd9a90d95fd9475a4bb131
EgorPopelyaev <egor@parity.io>
2024-12-11 13:19:05 +0100

    delete extra test

--- substrate/frame/transaction-payment/src/tests.rs
+++ substrate/frame/transaction-payment/src/tests.rs
@@ -843,43 +843,6 @@ fn genesis_default_works() {
 }
 
 #[test]
-fn no_fee_and_no_weight_for_other_origins() {
-	ExtBuilder::default().build().execute_with(|| {
-		let ext = Ext::from(0);
-
-		let mut info = CALL.get_dispatch_info();
-		info.extension_weight = ext.weight(CALL);
-
-		// Ensure we test the refund.
-		assert!(info.extension_weight != Weight::zero());
-
-		let len = CALL.encoded_size();
-
-		let origin = frame_system::RawOrigin::Root.into();
-		let (pre, origin) = ext.validate_and_prepare(origin, CALL, &info, len, 0).unwrap();
-
-		assert!(origin.as_system_ref().unwrap().is_root());
-
-		let pd_res = Ok(());
-		let mut post_info = frame_support::dispatch::PostDispatchInfo {
-			actual_weight: Some(info.total_weight()),
-			pays_fee: Default::default(),
-		};
-
-		<Ext as TransactionExtension<RuntimeCall>>::post_dispatch(
-			pre,
-			&info,
-			&mut post_info,
-			len,
-			&pd_res,
-		)
-		.unwrap();
-
-		assert_eq!(post_info.actual_weight, Some(info.call_weight));
-	})
-}
-
-#[test]
 fn fungible_adapter_no_zero_refund_action() {
 	type FungibleAdapterT = payment::FungibleAdapter<Balances, DealWithFees>;
 
