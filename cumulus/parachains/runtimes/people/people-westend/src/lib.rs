3e07289f86712c093a3ad4c245f80b6b6f9f1aa9
EgorPopelyaev <egor@parity.io>
2024-12-12 10:54:11 +0100

    Bump spec_version

--- cumulus/parachains/runtimes/people/people-westend/src/lib.rs
+++ cumulus/parachains/runtimes/people/people-westend/src/lib.rs
@@ -136,7 +136,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: alloc::borrow::Cow::Borrowed("people-westend"),
 	impl_name: alloc::borrow::Cow::Borrowed("people-westend"),
 	authoring_version: 1,
-	spec_version: 1_017_000,
+	spec_version: 1_017_001,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 2,
