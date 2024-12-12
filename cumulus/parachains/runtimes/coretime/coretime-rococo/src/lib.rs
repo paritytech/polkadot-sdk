5ef398e414d094331a37736a84e92f4f41de0e25
EgorPopelyaev <egor@parity.io>
2024-12-12 10:54:11 +0100

    Bump spec_version

--- cumulus/parachains/runtimes/coretime/coretime-rococo/src/lib.rs
+++ cumulus/parachains/runtimes/coretime/coretime-rococo/src/lib.rs
@@ -149,7 +149,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: alloc::borrow::Cow::Borrowed("coretime-rococo"),
 	impl_name: alloc::borrow::Cow::Borrowed("coretime-rococo"),
 	authoring_version: 1,
-	spec_version: 1_017_000,
+	spec_version: 1_017_001,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 2,
