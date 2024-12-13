d0bf749eb6a37b4c31de9177eaadcbffbde1f2e5
DavidK <davidk@parity.io>
2024-12-13 15:12:17 +0200

    Revert irrelevant

--- substrate/frame/revive/fixtures/contracts/return_with_data.rs
+++ substrate/frame/revive/fixtures/contracts/return_with_data.rs
@@ -40,7 +40,6 @@ pub extern "C" fn call() {
 
 	// Burn some PoV, clear_storage consumes some PoV as in order to clear the storage we need to we
 	// need to read its size first.
-	#[cfg(feature = "unstable-api")]
 	api::clear_storage(StorageFlags::empty(), b"");
 
 	let exit_status = uapi::ReturnFlags::from_bits(exit_status[0] as u32).unwrap();
