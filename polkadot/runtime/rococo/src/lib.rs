0c162bd6521b60ec89c53780ce1a994687f25d69
Jarkko Sakkinen <jarkko@parity.io>
2024-12-14 12:10:40 +0200

    Revert "rococo-runtime: Double min_stack_size"
    This reverts commit 44e1dabfbeac7cad9046e7c73784179eace676fe.

--- polkadot/runtime/rococo/src/lib.rs
+++ polkadot/runtime/rococo/src/lib.rs
@@ -19,7 +19,6 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 // `construct_runtime!` does a lot of recursion and requires us to increase the limit.
 #![recursion_limit = "512"]
-#![min_stack_size = "16384"]
 
 extern crate alloc;
 
