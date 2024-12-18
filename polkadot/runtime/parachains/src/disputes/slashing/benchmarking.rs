07c4ae40004ad49f57b056bdd10769b35761d94b
clangenb <37865735+clangenb@users.noreply.github.com>
2024-12-18 08:53:12 +0100

    Update polkadot/runtime/parachains/src/disputes/slashing/benchmarking.rs
    Co-authored-by: Bastian Köcher <git@kchr.de>

--- polkadot/runtime/parachains/src/disputes/slashing/benchmarking.rs
+++ polkadot/runtime/parachains/src/disputes/slashing/benchmarking.rs
@@ -153,15 +153,8 @@ mod benchmarks {
 		let (session_index, key_owner_proof, validator_id) = setup_validator_set::<T>(n);
 		let dispute_proof = setup_dispute::<T>(session_index, validator_id);
 
-		#[block]
-		{
-			let result = Pallet::<T>::report_dispute_lost_unsigned(
-				origin,
-				Box::new(dispute_proof),
-				key_owner_proof,
-			);
-			assert!(result.is_ok());
-		}
+		#[extrinsic_call]
+		_(origin, Box::new(dispute_proof), key_owner_proof);
 
 		let unapplied = <UnappliedSlashes<T>>::get(session_index, CANDIDATE_HASH);
 		assert!(unapplied.is_none());
