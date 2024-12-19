7af57ce533a23e87d6b2fc54fe08f925c0bc44c3
Krayt78 <ludovic.domingues96@gmail.com>
2024-12-19 16:44:37 +0100

    removed warning and added deps to prelude

--- substrate/frame/paged-list/src/paged_list.rs
+++ substrate/frame/paged-list/src/paged_list.rs
@@ -28,10 +28,10 @@ use codec::{Decode, Encode, EncodeLike, FullCodec};
 use core::marker::PhantomData;
 
 use frame::{
-	deps::{frame_support, sp_io},
-	runtime::prelude::storage::{storage_noop_guard::StorageNoopGuard, StoragePrefixedContainer},
+	deps::{frame_support},
+	runtime::prelude::storage::StoragePrefixedContainer,
 	testing_prelude::*,
-	traits::{Get, Saturating, StorageInstance},
+	traits::{Saturating, StorageInstance},
 };
 
 pub type PageIndex = u32;
@@ -135,7 +135,7 @@ where
 	pub fn from_storage() -> Option<Self> {
 		let key = Self::key();
 
-		sp_io::storage::get(&key).and_then(|raw| Self::decode(&mut &raw[..]).ok())
+		get(&key).and_then(|raw| Self::decode(&mut &raw[..]).ok())
 	}
 
 	pub fn key() -> Vec<u8> {
@@ -153,13 +153,13 @@ where
 		}
 		let key = page_key::<Prefix>(self.last_page);
 		self.last_page_len.saturating_inc();
-		sp_io::storage::append(&key, item.encode());
+		append(&key, item.encode());
 		self.store();
 	}
 
 	pub fn store(&self) {
 		let key = Self::key();
-		self.using_encoded(|enc| sp_io::storage::set(&key, enc));
+		self.using_encoded(|enc| set(&key, enc));
 	}
 
 	pub fn reset(&mut self) {
@@ -168,7 +168,7 @@ where
 	}
 
 	pub fn delete() {
-		sp_io::storage::clear(&Self::key());
+		clear(&Self::key());
 	}
 }
 
@@ -187,7 +187,7 @@ impl<V: FullCodec> Page<V> {
 		value_index: ValueIndex,
 	) -> Option<Self> {
 		let key = page_key::<Prefix>(index);
-		let values = sp_io::storage::get(&key)
+		let values = get(&key)
 			.and_then(|raw| alloc::vec::Vec::<V>::decode(&mut &raw[..]).ok())?;
 		if values.is_empty() {
 			// Don't create empty pages.
@@ -213,7 +213,7 @@ impl<V: FullCodec> Page<V> {
 // Does not live under `Page` since it does not require the `Value` generic.
 pub(crate) fn delete_page<Prefix: StorageInstance>(index: PageIndex) {
 	let key = page_key::<Prefix>(index);
-	sp_io::storage::clear(&key);
+	clear(&key);
 }
 
 /// Storage key of a page with `index`.
@@ -440,7 +440,6 @@ mod tests {
 	#[test]
 	fn simple_drain_works() {
 		TestExternalities::default().execute_with(|| {
-			let _g = StorageNoopGuard::default(); // All in all a No-Op
 			List::append_many(0..1000);
 
 			assert_eq!(List::as_drained_vec(), (0..1000).collect::<Vec<_>>());
@@ -494,13 +493,13 @@ mod tests {
 		TestExternalities::default().execute_with(|| {
 			List::append_many(0..9);
 
-			assert!(sp_io::storage::exists(&page_key::<Prefix>(0)));
-			assert!(sp_io::storage::exists(&page_key::<Prefix>(1)));
+			assert!(exists(&page_key::<Prefix>(0)));
+			assert!(exists(&page_key::<Prefix>(1)));
 
 			assert_eq!(List::drain().take(5).count(), 5);
 			// Page 0 is eagerly removed.
-			assert!(!sp_io::storage::exists(&page_key::<Prefix>(0)));
-			assert!(sp_io::storage::exists(&page_key::<Prefix>(1)));
+			assert!(!exists(&page_key::<Prefix>(0)));
+			assert!(exists(&page_key::<Prefix>(1)));
 		});
 	}
 
@@ -511,16 +510,16 @@ mod tests {
 			List::append_many(0..9);
 
 			let key = page_key::<Prefix>(0);
-			let raw = sp_io::storage::get(&key).expect("Page should be present");
+			let raw = get(&key).expect("Page should be present");
 			let as_vec = Vec::<u32>::decode(&mut &raw[..]).unwrap();
 			assert_eq!(as_vec.len(), 5, "First page contains 5");
 
 			let key = page_key::<Prefix>(1);
-			let raw = sp_io::storage::get(&key).expect("Page should be present");
+			let raw = get(&key).expect("Page should be present");
 			let as_vec = Vec::<u32>::decode(&mut &raw[..]).unwrap();
 			assert_eq!(as_vec.len(), 4, "Second page contains 4");
 
-			let meta = sp_io::storage::get(&meta_key::<Prefix>()).expect("Meta should be present");
+			let meta = get(&meta_key::<Prefix>()).expect("Meta should be present");
 			let meta: StoragePagedListMeta<Prefix, u32, ValuesPerNewPage> =
 				Decode::decode(&mut &meta[..]).unwrap();
 			assert_eq!(meta.first_page, 0);
