483eb03d6408d7b88cae3c835d2fd1b025f6d1ce
Giuseppe Re <giuseppe.re@parity.io>
2024-12-20 13:19:27 +0100

    Update polkadot/runtime/test-runtime/src/lib.rs
    Co-authored-by: Kian Paimani <5588131+kianenigma@users.noreply.github.com>

--- polkadot/runtime/test-runtime/src/lib.rs
+++ polkadot/runtime/test-runtime/src/lib.rs
@@ -340,7 +340,7 @@ parameter_types! {
 	pub storage SessionsPerEra: SessionIndex = 6;
 	// 28 eras for unbonding (7 days).
 	pub storage BondingDuration: sp_staking::EraIndex = 28;
-	pub storage MaxBondedEras: u32 = (BondingDuration::get() as u32) + 1;
+	pub const MaxBondedEras: u32 = (BondingDuration::get() as u32) + 1;
 	// 27 eras in which slashes can be cancelled (a bit less than 7 days).
 	pub storage SlashDeferDuration: sp_staking::EraIndex = 27;
 	pub const RewardCurve: &'static PiecewiseLinear<'static> = &REWARD_CURVE;
