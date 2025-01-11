d13f0d692eb4545d9a0b5356dec9a99a9cf53364
runcomet <runcomet@protonmail.com>
2025-01-11 02:52:57 +0100

    add dev_accounts field in missing pallet tests

--- substrate/frame/election-provider-multi-phase/test-staking-e2e/src/mock.rs
+++ substrate/frame/election-provider-multi-phase/test-staking-e2e/src/mock.rs
@@ -546,7 +546,7 @@ impl ExtBuilder {
 
 		let _ = pallet_balances::GenesisConfig::<Runtime> {
 			balances: self.balances_builder.balances.clone(),
-			..Dfault::default()
+			..Default::default()
 		}
 		.assimilate_storage(&mut storage);
 
