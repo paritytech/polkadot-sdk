d5539aa63edc8068eff9c4cbb78214c3a5ab66b2
Sebastian Kunert <skunert49@gmail.com>
2025-01-14 23:47:19 +0100

    Parachains: Use relay chain slot for velocity measurement (#6825)
    closes #3967 
    
    ## Changes
    We now use relay chain slots to measure velocity on chain. Previously we
    were storing the current parachain slot. Then in `on_state_proof` of the
    `ConsensusHook` we were checking how many blocks were athored in the
    current parachain slot. This works well when the parachain slot time and
    relay chain slot time is the same. With elastic scaling, we can have
    parachain slot times lower than that of the relay chain. In these cases
    we want to measure velocity in relation to the relay chain. This PR
    adjusts that.
    
    
    ##  Migration
    This PR includes a migration. Storage item `SlotInfo` of pallet
    `aura-ext` is renamed to `RelaySlotInfo` to better reflect its new
    content. A migration has been added that just kills the old storage
    item. `RelaySlotInfo` will be `None` initially but its value will be
    adjusted after one new relay chain slot arrives.
    
    ---------
    
    Co-authored-by: command-bot <>
    Co-authored-by: Bastian Köcher <git@kchr.de>

--- cumulus/parachains/runtimes/assets/asset-hub-westend/src/lib.rs
+++ cumulus/parachains/runtimes/assets/asset-hub-westend/src/lib.rs
@@ -341,7 +341,6 @@ pub type LocalAndForeignAssets = fungibles::UnionOf<
 	xcm::v5::Location,
 	AccountId,
 >;
-
 /// Union fungibles implementation for [`LocalAndForeignAssets`] and `Balances`.
 pub type NativeAndAssets = fungible::UnionOf<
 	Balances,
@@ -1129,6 +1128,7 @@ pub type Migrations = (
 	>,
 	// permanent
 	pallet_xcm::migration::MigrateToLatestXcmVersion<Runtime>,
+	cumulus_pallet_aura_ext::migration::MigrateV0ToV1<Runtime>,
 );
 
 /// Asset Hub Westend has some undecodable storage, delete it.
