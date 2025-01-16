ff9250739407a80a595243ee51a5076981dc2a4b
pgherveou <pgherveou@gmail.com>
2025-01-16 10:03:22 +0100

    fix debug_rpc_types

--- substrate/frame/revive/src/evm/api/debug_rpc_types.rs
+++ substrate/frame/revive/src/evm/api/debug_rpc_types.rs
@@ -2,21 +2,115 @@ use crate::{
 	evm::{extract_revert_message, runtime::gas_from_weight, Bytes},
 	ExecReturnValue, Weight,
 };
-use alloc::{string::String, vec::Vec};
+use alloc::{fmt, string::String, vec::Vec};
 use codec::{Decode, Encode};
 use scale_info::TypeInfo;
-use serde::{Deserialize, Serialize};
+use serde::{
+	de::{self, MapAccess, Visitor},
+	Deserialize, Deserializer, Serialize,
+};
 use sp_core::{H160, H256, U256};
 
 /// Tracer configuration used to trace calls.
-#[derive(TypeInfo, Debug, Clone, Encode, Decode, Serialize, Deserialize)]
+#[derive(TypeInfo, Debug, Clone, Encode, Decode, Serialize, PartialEq)]
+#[serde(tag = "tracer", content = "tracerConfig")]
 pub enum TracerConfig {
 	/// A tracer that captures call traces.
+	#[serde(rename = "callTracer")]
 	CallTracer {
 		/// Whether or not to capture logs.
+		#[serde(rename = "withLog")]
 		with_logs: bool,
 	},
 }
+
+/// Custom deserializer to support the following JSON format:
+///
+/// ```json
+/// { "tracer": "callTracer", "tracerConfig": { "withLogs": false } }
+/// ```
+///
+/// ```json
+/// { "tracer": "callTracer" }
+/// ```
+impl<'de> Deserialize<'de> for TracerConfig {
+	fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+	where
+		D: Deserializer<'de>,
+	{
+		struct TracerConfigVisitor;
+
+		impl<'de> Visitor<'de> for TracerConfigVisitor {
+			type Value = TracerConfig;
+
+			fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+				formatter.write_str("a map with tracer and optional tracerConfig")
+			}
+
+			fn visit_map<M>(self, mut map: M) -> Result<Self::Value, M::Error>
+			where
+				M: MapAccess<'de>,
+			{
+				let mut tracer_type: Option<String> = None;
+				let mut with_logs = None;
+
+				while let Some(key) = map.next_key::<String>()? {
+					match key.as_str() {
+						"tracer" => {
+							tracer_type = map.next_value()?;
+						},
+						"tracerConfig" => {
+							#[derive(Deserialize)]
+							struct CallTracerConfig {
+								#[serde(rename = "withLogs")]
+								with_logs: Option<bool>,
+							}
+							let inner: CallTracerConfig = map.next_value()?;
+							with_logs = inner.with_logs;
+						},
+						_ => {},
+					}
+				}
+
+				match tracer_type.as_deref() {
+					Some("callTracer") =>
+						Ok(TracerConfig::CallTracer { with_logs: with_logs.unwrap_or(true) }),
+					_ => Err(de::Error::custom("Unsupported or missing tracer type")),
+				}
+			}
+		}
+
+		deserializer.deserialize_map(TracerConfigVisitor)
+	}
+}
+
+#[test]
+fn test_deserialize_call_tracer_with_logs_true() {
+	let tracers = vec![
+		(r#"{"tracer": "callTracer"}"#, TracerConfig::CallTracer { with_logs: true }),
+		(
+			r#"{"tracer": "callTracer", "tracerConfig": { "withLogs": true }}"#,
+			TracerConfig::CallTracer { with_logs: true },
+		),
+		(
+			r#"{"tracer": "callTracer", "tracerConfig": { "withLogs": false }}"#,
+			TracerConfig::CallTracer { with_logs: false },
+		),
+	];
+
+	for (json_data, expected) in tracers {
+		let result: TracerConfig =
+			serde_json::from_str(json_data).expect("Deserialization should succeed");
+		assert_eq!(result, expected);
+	}
+}
+
+impl Default for TracerConfig {
+	fn default() -> Self {
+		TracerConfig::CallTracer { with_logs: false }
+	}
+}
+
 /// The type of call that was executed.
 #[derive(
 	Default, TypeInfo, Encode, Decode, Serialize, Deserialize, Eq, PartialEq, Clone, Debug,
@@ -90,6 +184,12 @@ impl<Gas, Output: Default + PartialEq> Traces<Gas, Output> {
 }
 
 impl Traces {
+	/// Return true if the traces are empty.
+	pub fn is_empty(&self) -> bool {
+		match self {
+			Traces::CallTraces(traces) => traces.is_empty(),
+		}
+	}
 	/// Return the traces as Ethereum traces.
 	pub fn as_eth_traces<T: crate::Config>(self) -> EthTraces
 	where
