a78449ebf96728d2243242743de4e2256ee4a564
ron <yrong1997@gmail.com>
2025-01-17 18:55:51 +0800

    Fix comments

--- polkadot/xcm/xcm-executor/src/traits/should_execute.rs
+++ polkadot/xcm/xcm-executor/src/traits/should_execute.rs
@@ -131,7 +131,8 @@ impl CheckSuspension for Tuple {
 /// Trait to determine whether the execution engine should not execute a given XCM.
 ///
 /// Can be amalgamated into a tuple to have multiple trials. If any of the tuple elements returns
-/// `Err(())`, the execution stops. Else, `Ok(_)` is returned if all elements accept the message.
+/// `Some(ProcessMessageError)`, the execution stops. Else, `None` is returned if all elements
+/// accept the message.
 pub trait DenyExecution {
 	/// Returns `None` if there is no reason to deny execution,
 	/// while `Some(ProcessMessageError)` indicates there is a reason to deny execution.
