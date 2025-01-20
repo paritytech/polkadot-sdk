a9e74f89a2b4baf2cc1528437cb9ada0e3f36535
runcomet <runcomet@protonmail.com>
2025-01-20 20:04:07 +0100

    remove extra mod

--- substrate/frame/src/lib.rs
+++ substrate/frame/src/lib.rs
@@ -229,9 +229,6 @@ pub mod prelude {
 	/// All arithmetic types and traits used for safe math.
 	pub use super::arithmetic::*;
 
-	/// All account management related things.
-	pub use super::account::*;
-
 	/// Runtime traits
 	#[doc(no_inline)]
 	pub use sp_runtime::traits::{
@@ -572,16 +569,6 @@ pub mod cryptography {
 		Pair, H160, H256, H512, U256, U512,
 	};
 	pub use sp_runtime::traits::{BlakeTwo256, Hash, Keccak256};
-}
-
-/// All account management related traits.
-///
-/// This is already part of the [`prelude`].
-pub mod account {
-	pub use frame_support::traits::{
-		AsEnsureOriginWithArg, ChangeMembers, EitherOfDiverse, InitializeMembers,
-	};
-	pub use sp_runtime::traits::{IdentifyAccount, IdentityLookup};
 }
 
 /// Access to all of the dependencies of this crate. In case the prelude re-exports are not enough,
