88d20a339b3507fbe1cfb57a8b013cd773c9f2e4
Giuseppe Re <giuseppe.re@parity.io>
2025-01-21 17:56:30 +0100

    Update substrate/frame/staking/src/lib.rs
    Co-authored-by: Guillaume Thiolliere <gui.thiolliere@gmail.com>

--- substrate/frame/staking/src/lib.rs
+++ substrate/frame/staking/src/lib.rs
@@ -1108,9 +1108,6 @@ impl<T: Config> EraInfo<T> {
 		validator: &T::AccountId,
 	) -> Exposure<T::AccountId, BalanceOf<T>> {
 		let Some(overview) = <ErasStakersOverview<T>>::get(&era, validator) else { return Exposure::default(); };
-		}
-
-		let overview = overview.expect("checked above; qed");
 
 		let mut others = Vec::with_capacity(overview.nominator_count as usize);
 		for page in 0..overview.page_count {
