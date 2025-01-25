c998988524c1b8ceded1576fe88e354a47cc4271
Alexander Theißen <alex.theissen@me.com>
2025-01-25 16:47:58 +0100

    Manually add new benchmark to all runtimes

--- polkadot/runtime/rococo/src/weights/pallet_migrations.rs
+++ polkadot/runtime/rococo/src/weights/pallet_migrations.rs
@@ -170,4 +170,20 @@ impl<T: frame_system::Config> pallet_migrations::WeightInfo for WeightInfo<T> {
 			.saturating_add(T::DbWeight::get().writes((1_u64).saturating_mul(n.into())))
 			.saturating_add(Weight::from_parts(0, 2740).saturating_mul(n.into()))
 	}
+	/// Storage: `Skipped::Metadata` (r:0 w:0)
+	/// Proof: `Skipped::Metadata` (`max_values`: None, `max_size`: None, mode: `Measured`)
+	/// The range of component `n` is `[0, 2048]`.
+	fn reset_pallet_migration(n: u32, ) -> Weight {
+		// Proof Size summary in bytes:
+		//  Measured:  `1053 + n * (6 ±0)`
+		//  Estimated: `612 + n * (7 ±0)`
+		// Minimum execution time: 2_182_000 picoseconds.
+		Weight::from_parts(2_281_000, 0)
+			.saturating_add(Weight::from_parts(0, 612))
+			// Standard Error: 494
+			.saturating_add(Weight::from_parts(630_585, 0).saturating_mul(n.into()))
+			.saturating_add(T::DbWeight::get().reads((1_u64).saturating_mul(n.into())))
+			.saturating_add(T::DbWeight::get().writes((1_u64).saturating_mul(n.into())))
+			.saturating_add(Weight::from_parts(0, 7).saturating_mul(n.into()))
+	}
 }
