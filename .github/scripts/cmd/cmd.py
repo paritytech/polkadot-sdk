7806f13504bbbf0cb5e5dede11fb9d1ac0d7f3b7
Maksym H <mordamax@gmail.com>
2025-01-28 21:15:56 +0000

    move compilation of frame-omni-bencher into a cmd.py itself

--- .github/scripts/cmd/cmd.py
+++ .github/scripts/cmd/cmd.py
@@ -112,6 +112,11 @@ def main():
         runtimesMatrix = {x['name']: x for x in runtimesMatrix}
         print(f'Filtered out runtimes: {runtimesMatrix}')
 
+        compile_bencher = os.system(f"cargo install --path substrate/utils/frame/omni-bencher --locked --profile {profile}")
+        if compile_bencher != 0:
+            print_and_log('❌ Failed to compile frame-omni-bencher')
+            sys.exit(1)
+
         # loop over remaining runtimes to collect available pallets
         for runtime in runtimesMatrix.values():
             build_command = f"forklift cargo build -p {runtime['package']} --profile {profile} --features={runtime['bench_features']}"
