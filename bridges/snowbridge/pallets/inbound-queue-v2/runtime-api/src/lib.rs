70a9e4274293b95a69ef3ba728c8b896572299fd
Clara van Staden <claravanstaden64@gmail.com>
2025-02-03 14:24:53 +0200

    Update bridges/snowbridge/pallets/inbound-queue-v2/runtime-api/src/lib.rs
    Co-authored-by: Adrian Catangiu <adrian@parity.io>

--- bridges/snowbridge/pallets/inbound-queue-v2/runtime-api/src/lib.rs
+++ bridges/snowbridge/pallets/inbound-queue-v2/runtime-api/src/lib.rs
@@ -10,7 +10,7 @@ use xcm::latest::Xcm;
 sp_api::decl_runtime_apis! {
 	pub trait InboundQueueApiV2<Balance> where Balance: BalanceT
 	{
-		/// Dry runs the provided message on AH to provide the XCM payload and execution cost.
+		/// Dry runs the provided message to provide the XCM payload and execution cost.
 		fn dry_run(message: Message) -> Result<(Xcm<()>, Balance), DispatchError>;
 	}
 }
