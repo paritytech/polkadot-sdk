c30f602b216456fafc803d5f9c50d6986a29be93
Iulian Barbu <iulian.barbu@parity.io>
2025-02-04 17:10:33 +0200

    added test - it fails though
    Signed-off-by: Iulian Barbu <iulian.barbu@parity.io>

--- substrate/client/transaction-pool/tests/integration.rs
+++ substrate/client/transaction-pool/tests/integration.rs
@@ -22,15 +22,34 @@ pub mod zombienet;
 
 use std::time::Duration;
 
+use tokio::join;
 use zombienet::NetworkSpawner;
-use zombienet_sdk::NetworkConfigExt;
+use zombienet_sdk::subxt::OnlineClient;
 
 #[tokio::test(flavor = "multi_thread")]
-// TODO: continue this scenario
-async fn send_future_and_then_ready() {
+async fn send_future_and_then_ready_from_single_account() {
 	let _ = env_logger::try_init_from_env(
 		env_logger::Env::default().filter_or(env_logger::DEFAULT_FILTER_ENV, "info"),
 	);
 	let net = NetworkSpawner::init_from_asset_hub_fatp_low_pool_limit_spec().await.unwrap();
-	tokio::time::sleep(Duration::from_secs(1400)).await;
+	let collator = net.get_node("charlie").unwrap();
+	let _client: OnlineClient<zombienet_sdk::subxt::SubstrateConfig> =
+		collator.wait_client_with_timeout(120u64).await.unwrap();
+	let ws = "ws://127.0.0.1:9933";
+	let mut nonce = 0;
+	for _ in 0..3 {
+		// Spawn future TXs.
+		let future_start = nonce + 5;
+		let handle1 = tokio::spawn(async move {
+			cmd_lib::run_cmd!(RUST_LOG=info ttxt tx --chain=sub --ws=$ws from-single-account --account 0 --count 5 --from $future_start)
+		});
+		tokio::time::sleep(Duration::from_secs(5)).await;
+		let handle2 = tokio::spawn(async move {
+			cmd_lib::run_cmd!(RUST_LOG=info ttxt tx --chain=sub --ws=$ws from-single-account --account 0 --count 5 --from $nonce)
+		});
+		nonce = future_start + 10;
+		let (res1, res2) = join!(handle1, handle2);
+		assert!(res1.is_ok());
+		assert!(res2.is_ok());
+	}
 }
