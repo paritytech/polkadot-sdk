d93bac9d297172fa9995356dff1cbad9590234c8
Michal Kucharczyk <1728078+michalkucharczyk@users.noreply.github.com>
2025-02-06 17:54:45 +0100

    initial sketch

--- substrate/client/transaction-pool/src/fork_aware_txpool/metrics.rs
+++ substrate/client/transaction-pool/src/fork_aware_txpool/metrics.rs
@@ -19,9 +19,18 @@
 //! Prometheus's metrics for a fork-aware transaction pool.
 
 use crate::common::metrics::{GenericMetricsLink, MetricsRegistrant};
+use futures::FutureExt;
 use prometheus_endpoint::{
 	histogram_opts, linear_buckets, register, Counter, Gauge, Histogram, PrometheusError, Registry,
 	U64,
+};
+use sc_transaction_pool_api::TransactionStatus;
+use sc_utils::mpsc;
+use std::{
+	collections::HashMap,
+	future::Future,
+	pin::Pin,
+	time::{Duration, Instant},
 };
 
 /// A helper alias for the Prometheus's metrics endpoint.
@@ -59,6 +68,138 @@ pub struct Metrics {
 	pub view_revalidation_duration: Histogram,
 	/// Total number of the views created w/o cloning existing view.
 	pub non_cloned_views: Counter<U64>,
+	/// Histograms to track the timing distribution of individual transaction pool events.
+	pub events_histograms: EventsHistograms,
+}
+
+pub struct EventsHistograms {
+	/// Histogram of timings for reporting `TransactionStatus::Future` event
+	pub future: Histogram,
+	/// Histogram of timings for reporting `TransactionStatus::Ready` event
+	pub ready: Histogram,
+	/// Histogram of timings for reporting `TransactionStatus::Broadcast` event
+	pub broadcast: Histogram,
+	/// Histogram of timings for reporting `TransactionStatus::InBlock` event
+	pub in_block: Histogram,
+	/// Histogram of timings for reporting `TransactionStatus::Retracted` event
+	pub retracted: Histogram,
+	/// Histogram of timings for reporting `TransactionStatus::FinalityTimeout` event
+	pub finality_timeout: Histogram,
+	/// Histogram of timings for reporting `TransactionStatus::Finalized` event
+	pub finalized: Histogram,
+	/// Histogram of timings for reporting `TransactionStatus::Usurped(Hash)` event
+	pub usurped: Histogram,
+	/// Histogram of timings for reporting `TransactionStatus::Dropped` event
+	pub dropped: Histogram,
+	/// Histogram of timings for reporting `TransactionStatus::Invalid` event
+	pub invalid: Histogram,
+}
+
+impl EventsHistograms {
+	fn register(registry: &Registry) -> Result<Self, PrometheusError> {
+		Ok(Self {
+			future: register(
+				Histogram::with_opts(histogram_opts!(
+					"substrate_sub_txpool_timing_event_future",
+					"Histogram of timings for reporting Future event",
+					linear_buckets(0.0, 0.25, 13).unwrap()
+				))?,
+				registry,
+			)?,
+			ready: register(
+				Histogram::with_opts(histogram_opts!(
+					"substrate_sub_txpool_timing_event_ready",
+					"Histogram of timings for reporting Ready event",
+					linear_buckets(0.0, 0.25, 13).unwrap()
+				))?,
+				registry,
+			)?,
+			broadcast: register(
+				Histogram::with_opts(histogram_opts!(
+					"substrate_sub_txpool_timing_event_broadcast",
+					"Histogram of timings for reporting Broadcast event",
+					linear_buckets(0.0, 0.25, 13).unwrap()
+				))?,
+				registry,
+			)?,
+			in_block: register(
+				Histogram::with_opts(histogram_opts!(
+					"substrate_sub_txpool_timing_event_in_block",
+					"Histogram of timings for reporting InBlock event",
+					linear_buckets(0.0, 0.25, 13).unwrap()
+				))?,
+				registry,
+			)?,
+			retracted: register(
+				Histogram::with_opts(histogram_opts!(
+					"substrate_sub_txpool_timing_event_retracted",
+					"Histogram of timings for reporting Retracted event",
+					linear_buckets(0.0, 0.25, 13).unwrap()
+				))?,
+				registry,
+			)?,
+			finality_timeout: register(
+				Histogram::with_opts(histogram_opts!(
+					"substrate_sub_txpool_timing_event_finality_timeout",
+					"Histogram of timings for reporting FinalityTimeout event",
+					linear_buckets(0.0, 0.25, 13).unwrap()
+				))?,
+				registry,
+			)?,
+			finalized: register(
+				Histogram::with_opts(histogram_opts!(
+					"substrate_sub_txpool_timing_event_finalized",
+					"Histogram of timings for reporting Finalized event",
+					linear_buckets(0.0, 0.25, 13).unwrap()
+				))?,
+				registry,
+			)?,
+			usurped: register(
+				Histogram::with_opts(histogram_opts!(
+					"substrate_sub_txpool_timing_event_usurped",
+					"Histogram of timings for reporting Usurped event",
+					linear_buckets(0.0, 0.25, 13).unwrap()
+				))?,
+				registry,
+			)?,
+			dropped: register(
+				Histogram::with_opts(histogram_opts!(
+					"substrate_sub_txpool_timing_event_dropped",
+					"Histogram of timings for reporting Dropped event",
+					linear_buckets(0.0, 0.25, 13).unwrap()
+				))?,
+				registry,
+			)?,
+			invalid: register(
+				Histogram::with_opts(histogram_opts!(
+					"substrate_sub_txpool_timing_event_invalid",
+					"Histogram of timings for reporting Invalid event",
+					linear_buckets(0.0, 0.25, 13).unwrap()
+				))?,
+				registry,
+			)?,
+		})
+	}
+
+	fn observe<Hash, BlockHash>(
+		&self,
+		status: TransactionStatus<Hash, BlockHash>,
+		duration: Duration,
+	) {
+		let duration = duration.as_secs_f64();
+		match status {
+			TransactionStatus::Future => self.future.observe(duration),
+			TransactionStatus::Ready => self.ready.observe(duration),
+			TransactionStatus::Broadcast(..) => self.broadcast.observe(duration),
+			TransactionStatus::InBlock(..) => self.in_block.observe(duration),
+			TransactionStatus::Retracted(..) => self.retracted.observe(duration),
+			TransactionStatus::FinalityTimeout(..) => self.finality_timeout.observe(duration),
+			TransactionStatus::Finalized(..) => self.finalized.observe(duration),
+			TransactionStatus::Usurped(..) => self.usurped.observe(duration),
+			TransactionStatus::Dropped => self.dropped.observe(duration),
+			TransactionStatus::Invalid => self.invalid.observe(duration),
+		}
+	}
 }
 
 impl MetricsRegistrant for Metrics {
@@ -171,6 +312,79 @@ impl MetricsRegistrant for Metrics {
 				)?,
 				registry,
 			)?,
+			events_histograms: EventsHistograms::register(registry)?,
 		}))
+	}
+}
+
+enum EventMetricsMessage<Hash, BlockHash> {
+	Submitted(Instant, Hash),
+	Status(Instant, Hash, TransactionStatus<Hash, BlockHash>),
+}
+
+use crate::graph::{self, BlockHash, ExtrinsicHash};
+use futures::StreamExt;
+
+struct EventsMetricsCollector<ChainApi: graph::ChainApi> {
+	metrics_message_sink: MessageSink<ExtrinsicHash<ChainApi>, BlockHash<ChainApi>>,
+}
+
+pub type EventsMetricsCollectorTask = Pin<Box<dyn Future<Output = ()> + Send>>;
+
+type MessageSink<Hash, BlockHash> =
+	mpsc::TracingUnboundedSender<EventMetricsMessage<Hash, BlockHash>>;
+type MessageReceiver<Hash, BlockHash> =
+	mpsc::TracingUnboundedReceiver<EventMetricsMessage<Hash, BlockHash>>;
+
+impl<ChainApi> EventsMetricsCollector<ChainApi>
+where
+	ChainApi: graph::ChainApi + 'static,
+{
+	fn handle_status(
+		hash: ExtrinsicHash<ChainApi>,
+		status: TransactionStatus<ExtrinsicHash<ChainApi>, BlockHash<ChainApi>>,
+		timestamp: Instant,
+		submitted_timestamp_map: &HashMap<ExtrinsicHash<ChainApi>, Instant>,
+		metrics: &MetricsLink,
+	) {
+		let Some(submitted_timestamp) = submitted_timestamp_map.get(&hash) else { return };
+		metrics.report(|metrics| {
+			metrics
+				.events_histograms
+				.observe(status, timestamp.duration_since(*submitted_timestamp))
+		});
+	}
+
+	async fn task(
+		mut rx: MessageReceiver<ExtrinsicHash<ChainApi>, BlockHash<ChainApi>>,
+		metrics: MetricsLink,
+	) {
+		let mut submitted_timestamp = HashMap::<ExtrinsicHash<ChainApi>, Instant>::default();
+
+		loop {
+			tokio::select! {
+				biased;
+				cmd = rx.next() => {
+					match cmd {
+						Some(EventMetricsMessage::Submitted(timestamp, hash)) => {
+							submitted_timestamp.insert(hash, timestamp);
+						},
+						Some(EventMetricsMessage::Status(timestamp,hash,status)) => {
+							Self::handle_status(hash, status, timestamp, &submitted_timestamp, &metrics);
+						},
+						None => { return /* ? */ }
+					}
+				},
+			};
+		}
+	}
+
+	pub fn new_with_worker(metrics: MetricsLink) -> (Self, EventsMetricsCollectorTask) {
+		const QUEUE_WARN_SIZE: usize = 100_000;
+		let (metrics_message_sink, rx) =
+			mpsc::tracing_unbounded("txpool-event-metrics-collector", QUEUE_WARN_SIZE);
+		let task = Self::task(rx, metrics);
+
+		(Self { metrics_message_sink }, task.boxed())
 	}
 }
