60e2e13ddb258d88a172bd25a2a2cdffcb5e6b72
Branislav Kontur <bkontur@gmail.com>
2025-02-11 13:54:04 +0100

    Adjust zombienet tests

--- bridges/testing/framework/js-helpers/relayer-rewards.js
+++ bridges/testing/framework/js-helpers/relayer-rewards.js
@@ -3,16 +3,11 @@ async function run(nodeName, networkInfo, args) {
     const api = await zombie.connect(wsUri, userDefinedTypes);
 
     // TODO: could be replaced with https://github.com/polkadot-js/api/issues/4930 (depends on metadata v15) later
-    const relayerAccountAddress = args[0];
-    const laneId = args[1];
-    const bridgedChainId = args[2];
-    const relayerFundOwner = args[3];
-    const expectedRelayerReward = BigInt(args[4]);
+    const relayerAccountAddress = args.relayerAccountAddress;
+    const reward_kind = args.reward_kind;
+    const expectedRelayerReward = BigInt(args.expectedRelayerReward);
     while (true) {
-        const relayerReward = await api.query.bridgeRelayers.relayerRewards(
-            relayerAccountAddress,
-            { laneId: laneId, bridgedChainId: bridgedChainId, owner: relayerFundOwner }
-        );
+        const relayerReward = await api.query.bridgeRelayers.relayerRewards(relayerAccountAddress, reward_kind);
         if (relayerReward.isSome) {
             const relayerRewardBalance = relayerReward.unwrap().toBigInt();
             if (relayerRewardBalance > expectedRelayerReward) {
