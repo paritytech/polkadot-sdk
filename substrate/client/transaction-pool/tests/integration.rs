8de7754a65661ac4f04d0155ad6201c98c85c1a0
Iulian Barbu <iulian.barbu@parity.io>
2025-02-11 22:49:22 +0000

    created test scenario
    Signed-off-by: Iulian Barbu <iulian.barbu@parity.io>

--- substrate/client/transaction-pool/tests/integration.rs
+++ substrate/client/transaction-pool/tests/integration.rs
@@ -20,36 +20,73 @@
 
 pub mod zombienet;
 
-use std::time::Duration;
-
-use tokio::join;
+use futures::future::join;
+use txtesttool::{
+	cli::SendingScenario,
+	execution_log::ExecutionLog,
+	runner::RunnerFactory,
+	scenario::ScenarioExecutor,
+	subxt_transaction::{generate_sr25519_keypair, SubstrateTransactionsSink},
+	transaction::TransactionRecipe,
+	SubstrateTransactionBuilder,
+};
 use zombienet::NetworkSpawner;
 use zombienet_sdk::subxt::OnlineClient;
 
+// Test which sends future and ready txs from many accounts
+// to an unlimited pool.
 #[tokio::test(flavor = "multi_thread")]
-async fn send_future_and_then_ready_from_single_account() {
+async fn send_future_and_then_ready_from_many_accounts() {
 	let _ = env_logger::try_init_from_env(
 		env_logger::Env::default().filter_or(env_logger::DEFAULT_FILTER_ENV, "info"),
 	);
-	let net = NetworkSpawner::init_from_asset_hub_fatp_low_pool_limit_spec().await.unwrap();
+	let net = NetworkSpawner::init_from_asset_hub_fatp_high_pool_limit_spec().await.unwrap();
 	let collator = net.get_node("charlie").unwrap();
-	let _client: OnlineClient<zombienet_sdk::subxt::SubstrateConfig> =
+
+	// Wait for one of the collators to come online.
+	let client: OnlineClient<zombienet_sdk::subxt::config::SubstrateConfig> =
 		collator.wait_client_with_timeout(120u64).await.unwrap();
-	let ws = "ws://127.0.0.1:9933";
-	let mut nonce = 0;
-	for _ in 0..3 {
-		// Spawn future TXs.
-		let future_start = nonce + 5;
-		let handle1 = tokio::spawn(async move {
-			cmd_lib::run_cmd!(RUST_LOG=info ttxt tx --chain=sub --ws=$ws from-single-account --account 0 --count 5 --from $future_start)
-		});
-		tokio::time::sleep(Duration::from_secs(5)).await;
-		let handle2 = tokio::spawn(async move {
-			cmd_lib::run_cmd!(RUST_LOG=info ttxt tx --chain=sub --ws=$ws from-single-account --account 0  --count 5 --from $nonce)
-		});
-		nonce = future_start + 5;
-		let (res1, res2) = join!(handle1, handle2);
-		assert!(res1.is_ok());
-		assert!(res2.is_ok());
+	let mut stream = client.blocks().subscribe_best().await.unwrap();
+	loop {
+		let Some(block) = stream.next().await else {
+			continue;
+		};
+
+		if block.is_ok() {
+			tracing::info!("found best block: {:#?}", block.unwrap().hash());
+			break;
+		}
 	}
+
+	// Shared params.
+	let send_threshold = 20_000;
+	let ws = "ws://127.0.0.1:9933";
+	let recipe_future = TransactionRecipe::transfer();
+	let recipe_ready = recipe_future.clone();
+	let block_monitor = false;
+	let unwatched = false;
+
+	// Scenarios and sinks.
+	let scenario_future =
+		SendingScenario::FromManyAccounts { start_id: 0, last_id: 99, from: Some(100), count: 100 };
+	let scenario_ready =
+		SendingScenario::FromManyAccounts { start_id: 0, last_id: 99, from: Some(0), count: 100 };
+
+	let future_scenario_executor =
+		ScenarioExecutor::new(ws, scenario_future, recipe_future, block_monitor).await;
+	let ready_scenario_executor =
+		ScenarioExecutor::new(ws, scenario_ready, recipe_ready, block_monitor).await;
+
+	let ((future_stop_runner_tx, mut runner_future), future_queue_task) =
+		RunnerFactory::substrate_runner(future_scenario_executor, send_threshold, unwatched).await;
+	let ((ready_stop_runner_tx, mut runner_ready), ready_queue_task) =
+		RunnerFactory::substrate_runner(ready_scenario_executor, send_threshold, unwatched).await;
+
+	let (future_logs, ready_logs) = join(runner_future.run_poc2(), runner_ready.run_poc2()).await;
+	let finalized_future =
+		future_logs.values().filter_map(|default_log| default_log.finalized()).count();
+	let finalized_ready =
+		ready_logs.values().filter_map(|default_log| default_log.finalized()).count();
+	assert_eq!(finalized_future, 100);
+	assert_eq!(finalized_ready, 100);
 }
