f3a92c1ee4e4600559d1cd4962fcdb29b2adf988
Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
2025-02-14 19:06:46 +0100

    docs
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>

--- substrate/frame/multisig/src/lib.rs
+++ substrate/frame/multisig/src/lib.rs
@@ -157,7 +157,23 @@ pub mod pallet {
 		/// Weight information for extrinsics in this pallet.
 		type WeightInfo: weights::WeightInfo;
 
-		/// Provider for the block number. Normally this is the `frame_system` pallet.
+		/// Query the current block number.
+		///
+		/// Must return monotonically increasing values when called from consecutive blocks.
+		/// Can be configured to return either:
+		/// - the local block number of the runtime via `frame_system::Pallet`
+		/// - a remote block number, eg from the relay chain through `RelaychainDataProvider`
+		/// - an arbitrary value through a custom implementation of the trait
+		///
+		/// There is currently no migration provided to "hot-swap" block number providers and it may
+		/// result in undefined behavior when doing so. Parachains are therefore best of to set this
+		/// to their local block number provider if they have the pallet already deployed.
+		///
+		/// Suggested values:
+		/// - Solo- and Relay-chains: `frame_system::Pallet`
+		/// - PLO- and Coretime-Parachains that:
+		///   - already have the pallet deployed: `frame_system::Pallet`
+		///   - are freshly deploying this pallet: `RelaychainDataProvider`
 		type BlockNumberProvider: BlockNumberProvider;
 	}
 
