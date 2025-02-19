5776359b0c097d8e86c88c72b6d88ecaa345238b
Nathaniel Bajo <73991674+Nathy-bajo@users.noreply.github.com>
2025-02-19 15:30:20 +0100

    Update polkadot/xcm/xcm-builder/src/location_conversion.rs
    Co-authored-by: Adrian Catangiu <adrian@parity.io>

--- polkadot/xcm/xcm-builder/src/location_conversion.rs
+++ polkadot/xcm/xcm-builder/src/location_conversion.rs
@@ -467,7 +467,7 @@ impl<UniversalLocation: Get<InteriorLocation>, AccountId: From<[u8; 32]> + Clone
 {
     fn convert_location(location: &Location) -> Option<AccountId> {
         let universal_source = UniversalLocation::get();
-        let (remote_network, remote_location) = ensure_is_remote(universal_source, location.clone()).ok()?;
+        let (remote_network, remote_location) = ensure_is_remote(universal_source, location).ok()?;
 
         let consensus_bytes = (b"external_consensus_", remote_network).using_encoded(blake2_256);
 
