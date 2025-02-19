ed654883a726fd39069736957d002461fed5157f
Tiago Bandeira <tiago.bandeira@parity.io>
2025-02-19 20:50:45 +0000

    Add the ERC20 contract

---
+++ cumulus/parachains/integration-tests/chopsticks/tests/xcm-sc-tests/contracts/ERC20.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+
+contract MyToken is ERC20, ERC20Permit {
+    constructor() ERC20("MyToken", "MTK") ERC20Permit("MyToken") {}
+
+    function mint(uint256 amount) public {
+        _mint(msg.sender, amount);
+    }
+
+    function burn(uint256 amount) public {
+        _burn(msg.sender, amount);
+    }
+}
