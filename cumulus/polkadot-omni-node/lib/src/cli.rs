c26a9bc2ee287de2eb1252710f1f215f2e7d0194
Seemant Aggarwal <seemant.aggarwal@parity.io>
2025-02-19 20:53:54 +0530

    adding rustdoc for chain-spec-builder subcommand

--- cumulus/polkadot-omni-node/lib/src/cli.rs
+++ cumulus/polkadot-omni-node/lib/src/cli.rs
@@ -89,6 +89,7 @@ pub enum Subcommand {
 	/// Revert the chain to a previous state.
 	Revert(sc_cli::RevertCmd),
 
+	/// Subcommand for generating and managing chain specifications.
 	#[command(alias = "chain-spec-builder")]
 	#[command(subcommand)]
 	ChainSpecBuilder(ChainSpecBuilderCmd),
