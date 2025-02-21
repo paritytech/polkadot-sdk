03eac67fb6a65fcf0a589f21ee084b6129f3b9b2
Ayevbeosa Iyamu <ayevbeosa.j@gmail.com>
2025-02-21 18:48:03 +0100

    xcm-builder: added logging for xcm filters/helpers/matchers/types (#2408) (#7003)
    # Description
    
    Added logs in pallet-xcm to help in debugging, fixes #2408, and in
    continuation of #4982
    
    # Checklist
    
    - [x]
    https://github.com/paritytech/polkadot-sdk/blob/master/polkadot/xcm/xcm-builder/src/
    - [x]
    https://github.com/paritytech/polkadot-sdk/tree/master/cumulus/parachains/runtimes/assets/common/src
    - [x] runtime-defined XCM filters/converters (just [one
    example](https://github.com/paritytech/polkadot-sdk/blob/b12bc895b7aa12584c2278e418df47c531c15b5e/cumulus/parachains/runtimes/bridge-hubs/bridge-hub-westend/src/xcm_config.rs#L284))
    
    Polkadot Address: 1Gz5aLtEu2n4jsfA6XwtZnuaRymJrDDw4kEGdNHTdxrpzrc
    
    ---------
    
    Co-authored-by: Ayevbeosa Iyamu <aiyamu@vatebra.com>
    Co-authored-by: Bastian Köcher <git@kchr.de>
    Co-authored-by: Adrian Catangiu <adrian@parity.io>
    Co-authored-by: Raymond Cheung <178801527+raymondkfcheung@users.noreply.github.com>
    Co-authored-by: cmd[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- polkadot/xcm/xcm-builder/src/currency_adapter.rs
+++ polkadot/xcm/xcm-builder/src/currency_adapter.rs
@@ -142,8 +142,8 @@ impl<
 	> TransactAsset
 	for CurrencyAdapter<Currency, Matcher, AccountIdConverter, AccountId, CheckedAccount>
 {
-	fn can_check_in(_origin: &Location, what: &Asset, _context: &XcmContext) -> Result {
-		log::trace!(target: "xcm::currency_adapter", "can_check_in origin: {:?}, what: {:?}", _origin, what);
+	fn can_check_in(origin: &Location, what: &Asset, _context: &XcmContext) -> Result {
+		tracing::trace!(target: "xcm::currency_adapter", ?origin, ?what, "can_check_in origin");
 		// Check we handle this asset.
 		let amount: Currency::Balance =
 			Matcher::matches_fungible(what).ok_or(Error::AssetNotHandled)?;
@@ -156,8 +156,8 @@ impl<
 		}
 	}
 
-	fn check_in(_origin: &Location, what: &Asset, _context: &XcmContext) {
-		log::trace!(target: "xcm::currency_adapter", "check_in origin: {:?}, what: {:?}", _origin, what);
+	fn check_in(origin: &Location, what: &Asset, _context: &XcmContext) {
+		tracing::trace!(target: "xcm::currency_adapter", ?origin, ?what, "check_in origin");
 		if let Some(amount) = Matcher::matches_fungible(what) {
 			match CheckedAccount::get() {
 				Some((checked_account, MintLocation::Local)) =>
@@ -169,8 +169,8 @@ impl<
 		}
 	}
 
-	fn can_check_out(_dest: &Location, what: &Asset, _context: &XcmContext) -> Result {
-		log::trace!(target: "xcm::currency_adapter", "can_check_out dest: {:?}, what: {:?}", _dest, what);
+	fn can_check_out(dest: &Location, what: &Asset, _context: &XcmContext) -> Result {
+		tracing::trace!(target: "xcm::currency_adapter", ?dest, ?what, "can_check_out");
 		let amount = Matcher::matches_fungible(what).ok_or(Error::AssetNotHandled)?;
 		match CheckedAccount::get() {
 			Some((checked_account, MintLocation::Local)) =>
@@ -181,8 +181,8 @@ impl<
 		}
 	}
 
-	fn check_out(_dest: &Location, what: &Asset, _context: &XcmContext) {
-		log::trace!(target: "xcm::currency_adapter", "check_out dest: {:?}, what: {:?}", _dest, what);
+	fn check_out(dest: &Location, what: &Asset, _context: &XcmContext) {
+		tracing::trace!(target: "xcm::currency_adapter", ?dest, ?what, "check_out");
 		if let Some(amount) = Matcher::matches_fungible(what) {
 			match CheckedAccount::get() {
 				Some((checked_account, MintLocation::Local)) =>
@@ -195,7 +195,7 @@ impl<
 	}
 
 	fn deposit_asset(what: &Asset, who: &Location, _context: Option<&XcmContext>) -> Result {
-		log::trace!(target: "xcm::currency_adapter", "deposit_asset what: {:?}, who: {:?}", what, who);
+		tracing::trace!(target: "xcm::currency_adapter", ?what, ?who, "deposit_asset");
 		// Check we handle this asset.
 		let amount = Matcher::matches_fungible(&what).ok_or(Error::AssetNotHandled)?;
 		let who =
@@ -209,7 +209,7 @@ impl<
 		who: &Location,
 		_maybe_context: Option<&XcmContext>,
 	) -> result::Result<AssetsInHolding, XcmError> {
-		log::trace!(target: "xcm::currency_adapter", "withdraw_asset what: {:?}, who: {:?}", what, who);
+		tracing::trace!(target: "xcm::currency_adapter", ?what, ?who, "withdraw_asset");
 		// Check we handle this asset.
 		let amount = Matcher::matches_fungible(what).ok_or(Error::AssetNotHandled)?;
 		let who =
@@ -225,7 +225,7 @@ impl<
 		to: &Location,
 		_context: &XcmContext,
 	) -> result::Result<AssetsInHolding, XcmError> {
-		log::trace!(target: "xcm::currency_adapter", "internal_transfer_asset asset: {:?}, from: {:?}, to: {:?}", asset, from, to);
+		tracing::trace!(target: "xcm::currency_adapter", ?asset, ?from, ?to, "internal_transfer_asset");
 		let amount = Matcher::matches_fungible(asset).ok_or(Error::AssetNotHandled)?;
 		let from =
 			AccountIdConverter::convert_location(from).ok_or(Error::AccountIdConversionFailed)?;
