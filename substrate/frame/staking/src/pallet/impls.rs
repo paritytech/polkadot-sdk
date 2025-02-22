7b89287cb9449a9b12cfcd19b5b4b9f2fafd240d
dharjeezy <dharjeezy@gmail.com>
2025-02-22 21:02:45 +0100

    fix test

--- substrate/frame/staking/src/pallet/impls.rs
+++ substrate/frame/staking/src/pallet/impls.rs
@@ -50,7 +50,7 @@ use crate::{
 	BalanceOf, BoundedExposuresOf, EraInfo, EraPayout, Exposure, Forcing, IndividualExposure,
 	LedgerIntegrityState, MaxNominationsOf, MaxWinnersOf, MaxWinnersPerPageOf, Nominations,
 	NominationsQuota, PositiveImbalanceOf, RewardDestination, SessionInterface, SnapshotStatus,
-	StakingLedger, ValidatorPrefs, STAKING_ID,
+	UnlockChunk, StakingLedger, ValidatorPrefs, STAKING_ID,
 };
 use alloc::{boxed::Box, vec, vec::Vec};
 
