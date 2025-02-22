ac16055bd06d12f22a93e10c05dcf1a341fe0d0c
Bastian Köcher <git@kchr.de>
2025-02-22 17:22:13 +0100

    Apply suggestions from code review

--- cumulus/client/consensus/aura/src/collators/slot_based/collation_task.rs
+++ cumulus/client/consensus/aura/src/collators/slot_based/collation_task.rs
@@ -162,7 +162,7 @@ async fn handle_collation_message<Block: BlockT, RClient: RelayChainInterface + 
 					relay_parent_header.number,
 				);
 			} else {
-				tracing::error!(target: LOG_TARGET, "Failed to get relay parent header from hash: {:?relay_parent}");
+				tracing::error!(target: LOG_TARGET, "Failed to get relay parent header from hash: {relay_parent:?}");
 			}
 		}
 
