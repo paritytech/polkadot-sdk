fd65576cfaa651fa9e15104c7433163690baf62e
Javier Viola <javier@parity.io>
2025-02-24 19:12:46 +0100

    fmt

--- polkadot/zombienet-sdk-tests/tests/functional/mod.rs
+++ polkadot/zombienet-sdk-tests/tests/functional/mod.rs
@@ -1,7 +1,7 @@
 // Copyright (C) Parity Technologies (UK) Ltd.
 // SPDX-License-Identifier: Apache-2.0
 
+mod approval_voting_coalescing;
 mod async_backing_6_seconds_rate;
 mod duplicate_collations;
 mod sync_backing;
-mod approval_voting_coalescing;
