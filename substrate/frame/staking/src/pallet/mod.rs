867a8a31d0355e6879fcc8788c4e08a1994ca65b
Ankan <ankan.anurag@gmail.com>
2025-02-26 22:05:59 +0530

    Filter for AllPoolMembers

--- substrate/frame/staking/src/pallet/mod.rs
+++ substrate/frame/staking/src/pallet/mod.rs
@@ -2606,3 +2606,11 @@ pub mod pallet {
 		}
 	}
 }
+
+/// A utility struct that provides a way to check if a given account is a pool member.
+pub struct AllPoolMembers<T: Config>(PhantomData<T>);
+impl<T: Config> Contains<T::AccountId> for AllPoolMembers<T> {
+	fn contains(t: &T::AccountId) -> bool {
+		PoolMembers::<T>::contains_key(t)
+	}
+}
