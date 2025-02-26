8dbb672703c04459789bfaa53e2b7cce2afc7f68
Serban Iorga <serban@parity.io>
2025-02-26 15:38:48 +0200

    Code review comments

--- cumulus/parachains/runtimes/testing/penpal/src/lib.rs
+++ cumulus/parachains/runtimes/testing/penpal/src/lib.rs
@@ -827,7 +827,6 @@ impl pallet_asset_tx_payment::Config for Runtime {
 }
 
 parameter_types! {
-
 	pub const DepositPerItem: Balance = 0;
 	pub const DepositPerByte: Balance = 0;
 	pub CodeHashLockupDepositPercent: Perbill = Perbill::from_percent(30);
@@ -852,7 +851,7 @@ impl pallet_revive::Config for Runtime {
 	type InstantiateOrigin = EnsureSigned<Self::AccountId>;
 	type RuntimeHoldReason = RuntimeHoldReason;
 	type CodeHashLockupDepositPercent = CodeHashLockupDepositPercent;
-	type Xcm = pallet_xcm::Pallet<Self>;
+	type Xcm = PolkadotXcm;
 	type ChainId = ConstU64<420_420_999>;
 	type NativeToEthRatio = ConstU32<1_000_000>; // 10^(18 - 12) Eth is 10^18, Native is 10^12.
 	type EthGasEncoder = ();
