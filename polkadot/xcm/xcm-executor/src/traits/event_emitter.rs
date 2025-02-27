b5db8d55530768d2a74b537f52360a9173a7f023
Raymond Cheung <178801527+raymondkfcheung@users.noreply.github.com>
2025-02-27 14:24:34 +0000

    Update docs

--- polkadot/xcm/xcm-executor/src/traits/event_emitter.rs
+++ polkadot/xcm/xcm-executor/src/traits/event_emitter.rs
@@ -14,27 +14,60 @@
 // You should have received a copy of the GNU General Public License
 // along with Polkadot.  If not, see <http://www.gnu.org/licenses/>.
 
-use xcm::latest::prelude::*;
+use xcm::{
+	latest::{Location, SendError, Xcm, XcmHash},
+	prelude::XcmError,
+};
 
 /// Defines the event emitter for the XCM executor.
+/// This trait allows implementations to emit events related to XCM handling, including successful
+/// sends and failure cases.
 pub trait EventEmitter {
-	fn emit_sent_event(origin: Location, destination: Location, message_id: XcmHash);
+	/// Emits an event when an XCM is successfully sent.
+	///
+	/// # Parameters
+	/// - `origin`: The origin location of the XCM.
+	/// - `destination`: The target location where the message is sent.
+	/// - `message_id`: A unique identifier for the XCM.
+	/// - `message`: The full message for `pallet_xcm::Event::Sent`; otherwise, `Xcm::default()`.
+	fn emit_sent_event(
+		origin: Location,
+		destination: Location,
+		message_id: XcmHash,
+		message: Xcm<()>,
+	);
 
+	/// Emits an event when an XCM fails to send.
+	///
+	/// # Parameters
+	/// - `origin`: The origin location of the XCM.
+	/// - `destination`: The intended target location.
+	/// - `error`: The error encountered while sending.
+	/// - `message_id`: The unique identifier for the failed message.
 	fn emit_send_failure_event(
 		origin: Location,
 		destination: Location,
 		error: SendError,
 		message_id: XcmHash,
 	);
+
+	/// Emits an event when an XCM fails to process.
+	///
+	/// # Parameters
+	/// - `origin`: The origin location of the message.
+	/// - `error`: The error encountered while processing.
+	/// - `message_id`: The unique identifier for the failed message.
 	fn emit_process_failure_event(origin: Location, error: XcmError, message_id: XcmHash);
 }
 
+/// A no-op implementation of `EventEmitter` for unit type `()`.
+/// This can be used when event emission is not required.
 impl EventEmitter for () {
 	fn emit_sent_event(
 		_origin: Location,
 		_destination: Location,
-		_message: Xcm<()>,
 		_message_id: XcmHash,
+		_message: Xcm<()>,
 	) {
 	}
 
