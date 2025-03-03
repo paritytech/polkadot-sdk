ddc978ed0005f9273cb7d1b489e09b5587ebbb5f
clangenb <37865735+clangenb@users.noreply.github.com>
2025-03-03 13:58:57 +0100

    Fix unnecessary borrow
    Co-authored-by: Michal Kucharczyk <1728078+michalkucharczyk@users.noreply.github.com>

--- substrate/bin/node/runtime/src/genesis_config_presets.rs
+++ substrate/bin/node/runtime/src/genesis_config_presets.rs
@@ -115,7 +115,7 @@ pub fn kitchensink_genesis(
 pub fn get_preset(id: &PresetId) -> Option<Vec<u8>> {
 	// Note: Can't use `Sr25519Keyring::Alice.to_seed()` because the seed comes with `//`.
 	let (alice_stash, alice, alice_session_keys) = authority_keys_from_seed("Alice");
-	let (bob_stash, _bob, bob_session_keys) = authority_keys_from_seed(&"Bob");
+	let (bob_stash, _bob, bob_session_keys) = authority_keys_from_seed("Bob");
 
 	let endowed = well_known_including_eth_accounts();
 
