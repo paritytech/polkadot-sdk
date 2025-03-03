d6d73b3acb95eecf623bf6a2b040041c26403eb1
Giuseppe Re <giuseppe.re@parity.io>
2025-03-03 17:19:11 +0100

    [Test] Testing benchmarking command new features (#7761)
    Testing #7747

--- .github/scripts/cmd/cmd.py
+++ .github/scripts/cmd/cmd.py
@@ -58,7 +58,7 @@ bench_example = '''**Examples**:
  %(prog)s --runtime westend rococo --pallet pallet_balances pallet_multisig --quiet --clean
 
  Runs benchamrks for pallet_election_provider_multi_block for westend runtime with env VALIDATOR_COUNT=300 and heap_pages set to 65000
- %(prog)s bench --pallet pallet-election-provider-multi-block --runtime westend --env_vars VALIDATOR_COUNT=300 -- --heap-pages 65000
+ %(prog)s bench --pallet pallet-election-provider-multi-block --runtime westend --env-vars VALIDATOR_COUNT=300 -- --heap-pages 65000
 '''
 
 parser_bench = subparsers.add_parser('bench', aliases=['bench-omni'], help='Runs benchmarks (frame omni bencher)', epilog=bench_example, formatter_class=argparse.RawDescriptionHelpFormatter)
@@ -70,7 +70,7 @@ parser_bench.add_argument('--runtime', help='Runtime(s) space separated', choice
 parser_bench.add_argument('--pallet', help='Pallet(s) space separated', nargs='*', default=[])
 parser_bench.add_argument('--fail-fast', help='Fail fast on first failed benchmark', action='store_true')
 parser_bench.add_argument('--env-vars', help='Environmental variables in KEY=VALUE format, only used to run the frame-omni-bencher command', nargs='*', default=[])
-parser_bench.add_argument('extra_flags', help='Extra flags for the frame-omni-bencher command', nargs=argparse.REMAINDER)
+parser_bench.add_argument('extra_flags', help='Extra flags for the frame-omni-bencher command', nargs=argparse.REMAINDER, default=['--'])
 
 
 """
@@ -225,10 +225,10 @@ def main():
                         template = config['template']
                         output_path = xcm_path
                 
-                env_vars = " ".join(args.env_vars + [runtime['bench_env_vars']])
-                # Notice that extra_args can override some default args, like --steps or --repeat,
+                env_vars = " ".join(args.env_vars + [config['bench_env_vars']])
+                # Notice that extra_flags can override some default args, like --steps or --repeat,
                 # since Python only considers the last occurence as valid in such cases.
-                extra_flags = " ".join(args.extra_flags + [runtime['bench_flags']])
+                extra_flags = " ".join(args.extra_flags[1:] + [config['bench_flags']])
 
                 print(f'-- benchmarking {pallet} in {runtime} into {output_path}')
                 cmd = f"{env_vars} frame-omni-bencher v1 benchmark pallet " \
