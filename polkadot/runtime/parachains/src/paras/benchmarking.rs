4ef9b78ea9dff44cf77d8191db6de7d861bcede5
Branislav Kontur <bkontur@gmail.com>
2025-03-04 16:51:51 +0100

    ehm

--- polkadot/runtime/parachains/src/paras/benchmarking.rs
+++ polkadot/runtime/parachains/src/paras/benchmarking.rs
@@ -37,7 +37,6 @@ const SAMPLE_SIZE: u32 = 1024;
 
 fn assert_last_event<T: Config>(generic_event: <T as Config>::RuntimeEvent) {
 	let events = frame_system::Pallet::<T>::events();
-	println!("{:?}", events);
 	let system_event: <T as frame_system::Config>::RuntimeEvent = generic_event.into();
 	// compare to the last event record
 	let frame_system::EventRecord { event, .. } = &events[events.len() - 1];
