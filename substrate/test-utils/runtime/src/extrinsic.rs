a308b05a668e54c2d810adc2edf91f9494cc1d40
Serban Iorga <serban@parity.io>
2025-03-05 20:44:42 +0200

    Add lazy decoded call field to UncheckedExtrinsic

--- substrate/test-utils/runtime/src/extrinsic.rs
+++ substrate/test-utils/runtime/src/extrinsic.rs
@@ -70,10 +70,12 @@ impl TryFrom<&Extrinsic> for TransferData {
 			Extrinsic {
 				function: RuntimeCall::Balances(BalancesCall::transfer_allow_death { dest, value }),
 				preamble: Preamble::Signed(from, _, ((CheckNonce(nonce), ..), ..)),
+				..
 			} => Ok(TransferData { from: *from, to: *dest, amount: *value, nonce: *nonce }),
 			Extrinsic {
 				function: RuntimeCall::SubstrateTest(PalletCall::bench_call { transfer }),
 				preamble: Preamble::Bare(_),
+				..
 			} => Ok(transfer.clone()),
 			_ => Err(()),
 		}
