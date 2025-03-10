5a38f5f5726c480f584da6c8617fe1f84ff4d9a7
Alexandru Vasile <alexandru.vasile@parity.io>
2025-03-10 22:11:28 +0200

    cumulus: Remove unneeded space
    Signed-off-by: Alexandru Vasile <alexandru.vasile@parity.io>

--- cumulus/polkadot-omni-node/lib/src/common/spec.rs
+++ cumulus/polkadot-omni-node/lib/src/common/spec.rs
@@ -464,7 +464,6 @@ where
 		let default_backend = parachain_config.chain_spec.network_backend();
 		let network_backend =
 			parachain_config.network.network_backend.clone().unwrap_or(default_backend);
-
 		match network_backend {
 			sc_network::config::NetworkBackendType::Libp2p =>
 				<Self as NodeSpec>::start_node::<sc_network::NetworkWorker<_, _>>(
