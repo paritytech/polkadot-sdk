d342e825ccde630affd5c42534784667d0079c05
Andrei Eres <eresav@me.com>
2025-03-10 19:02:06 +0100

    Update

--- polkadot/zombienet-sdk-tests/tests/parachains/weights.rs
+++ polkadot/zombienet-sdk-tests/tests/parachains/weights.rs
@@ -1,8 +1,6 @@
 // Copyright (C) Parity Technologies (UK) Ltd.
 // SPDX-License-Identifier: Apache-2.0
 
-use anyhow::anyhow;
-
 use crate::helpers::asset_hub_westend::{
 	self,
 	runtime_types::{
@@ -10,9 +8,15 @@ use crate::helpers::asset_hub_westend::{
 		sp_weights::weight_v2::Weight,
 	},
 };
+use anyhow::anyhow;
+use futures::{stream::FuturesUnordered, StreamExt};
 use pallet_revive::AddressMapper;
-use subxt::{OnlineClient, PolkadotConfig};
-use subxt_signer::sr25519::dev;
+use std::str::FromStr;
+use subxt::{tx::SubmittableExtrinsic, OnlineClient, PolkadotConfig};
+use subxt_signer::{
+	sr25519::{dev, Keypair},
+	SecretUri,
+};
 use zombienet_sdk::NetworkConfigBuilder;
 
 #[tokio::test(flavor = "multi_thread")]
@@ -22,7 +26,6 @@ async fn weights_test() -> Result<(), anyhow::Error> {
 	);
 
 	let images = zombienet_sdk::environment::get_images_from_env();
-
 	let config = NetworkConfigBuilder::new()
 		.with_relaychain(|r| {
 			r.with_chain("westend-local")
@@ -44,7 +47,7 @@ async fn weights_test() -> Result<(), anyhow::Error> {
 				.with_collator(|n| {
 					n.with_name("collator").validator(true).with_args(vec![
 						("--force-authoring").into(),
-						("-ltxpool=trace").into(),
+						("-lerror,runtime=trace").into(),
 						("--pool-type=fork-aware").into(),
 					])
 				})
@@ -54,7 +57,6 @@ async fn weights_test() -> Result<(), anyhow::Error> {
 			let errs = e.into_iter().map(|e| e.to_string()).collect::<Vec<_>>().join(" ");
 			anyhow!("config errs: {errs}")
 		})?;
-
 	let spawn_fn = zombienet_sdk::environment::get_spawn_fn();
 	let network = spawn_fn(config).await?;
 
@@ -69,47 +71,97 @@ async fn weights_test() -> Result<(), anyhow::Error> {
 	validator1.assert("node_roles", 4.0).await?;
 	collator.assert("node_roles", 4.0).await?;
 
-	let alice_signer = dev::alice();
-	let alice_public = alice_signer.public_key();
-	let alice_account_id = alice_public.0.into();
-	let alice_public_bytes: &[u8] = alice_public.as_ref();
+	log::info!("Network is ready");
+
+	let alice = dev::alice();
+
+	let alice_account_id = alice.public_key().to_account_id();
 	let alice_h160 =
 		<asset_hub_westend_runtime::Runtime as pallet_revive::Config>::AddressMapper::to_address(
-			&alice_account_id,
+			&alice.public_key().0.into(),
 		);
-	println!("alice_h160: {:?}", alice_h160);
+	log::info!("alice_h160: {:?}", alice_h160);
+	let keys = (0..10)
+		.map(|i| {
+			let uri = SecretUri::from_str(&format!("//key{}", i)).unwrap();
+			let key = Keypair::from_uri(&uri).unwrap();
+			log::info!("key {i}: {:?}", <asset_hub_westend_runtime::Runtime as pallet_revive::Config>::AddressMapper::to_address(
+				&key.public_key().0.into(),
+			));
+			key
+		})
+		.collect::<Vec<_>>();
 
-	para_client
-		.tx()
-		.sign_and_submit_then_watch_default(
-			&asset_hub_westend::tx().revive().map_account(),
-			&alice_signer,
-		)
-		.await?
-		.wait_for_finalized_success()
-		.await?;
+	let alice_nonce = para_client.tx().account_nonce(&alice_account_id).await?;
+	let transfer_txs = keys
+		.iter()
+		.enumerate()
+		.map(|(i, key)| {
+			let params = subxt::config::polkadot::PolkadotExtrinsicParamsBuilder::new()
+				.nonce(alice_nonce + i as u64)
+				.build();
+			para_client.tx().create_signed_offline(
+				&asset_hub_westend::tx()
+					.balances()
+					.transfer_keep_alive(key.public_key().into(), 1000000000000),
+				&alice,
+				params,
+			)
+		})
+		.collect::<Result<Vec<_>, _>>()?;
+	submit_txs(transfer_txs).await?;
+
+	log::info!("Accounts ready");
+
+	let mut mapping_txs = keys
+		.iter()
+		.map(|key| {
+			let params =
+				subxt::config::polkadot::PolkadotExtrinsicParamsBuilder::new().nonce(0).build();
+			para_client.tx().create_signed_offline(
+				&asset_hub_westend::tx().revive().map_account(),
+				key,
+				params,
+			)
+		})
+		.collect::<Result<Vec<_>, _>>()?;
+	let alice_nonce = para_client.tx().account_nonce(&alice_account_id).await?;
+	let params = subxt::config::polkadot::PolkadotExtrinsicParamsBuilder::new()
+		.nonce(alice_nonce)
+		.build();
+	mapping_txs.push(para_client.tx().create_signed_offline(
+		&asset_hub_westend::tx().revive().map_account(),
+		&alice,
+		params,
+	)?);
+	submit_txs(mapping_txs).await?;
+
+	log::info!("Accounts mapped");
 
 	let code_path = std::env::current_dir().unwrap().join("tests/parachains/contract.polkavm");
 	let code = std::fs::read(code_path)?;
+	let contract_data = sp_core::hex2array!(
+		"6057361d0000000000000000000000000000000000000000000000000000000000000002"
+	)
+	.to_vec();
 
-	let alice_public = alice_signer.public_key();
 	let contract_dry_run_call = asset_hub_westend::apis().revive_api().instantiate(
-		alice_public.into(),
+		alice_account_id.clone(),
 		0,
 		None,
 		None,
 		Code::Upload(code.clone()),
-		b"0x".to_vec(),
+		vec![],
 		None,
 	);
 	let contract_dry_run =
 		para_client.runtime_api().at_latest().await?.call(contract_dry_run_call).await?;
+	log::info!("Contract dry run: {:?}", contract_dry_run);
 	let storage_deposit = match contract_dry_run.storage_deposit {
 		StorageDeposit::Charge(c) => c,
 		StorageDeposit::Refund(_) => 0,
 	};
-
-	let contract = para_client
+	para_client
 		.tx()
 		.sign_and_submit_then_watch_default(
 			&asset_hub_westend::tx().revive().instantiate_with_code(
@@ -120,26 +172,28 @@ async fn weights_test() -> Result<(), anyhow::Error> {
 				},
 				storage_deposit,
 				code,
-				b"0x".to_vec(),
+				vec![],
 				None,
 			),
-			&alice_signer,
+			&alice,
 		)
 		.await?
 		.wait_for_finalized_success()
 		.await?;
+	let contract_address = pallet_revive::create1(
+		&alice_h160,
+		para_client.tx().account_nonce(&alice_account_id).await? - 1,
+	);
 
-	assert!(contract.find::<asset_hub_westend::system::events::NewAccount>().count() > 0);
+	log::info!("Contract instantiated: {:?}", contract_address);
 
-	let alice_public = alice_signer.public_key();
-	let contract_address = pallet_revive::create1(&alice_h160, 1);
 	let contract_call_dry_run_call = asset_hub_westend::apis().revive_api().call(
-		alice_public.into(),
+		alice_account_id.clone(),
 		contract_address,
 		0,
 		None,
 		None,
-		b"0xa0712d680000000000000000000000000000000000000000000000000000000000000001".to_vec(),
+		contract_data.clone(),
 	);
 	let contract_call_dry_run = para_client
 		.runtime_api()
@@ -151,34 +205,81 @@ async fn weights_test() -> Result<(), anyhow::Error> {
 		StorageDeposit::Charge(c) => c,
 		StorageDeposit::Refund(_) => 0,
 	};
-	let contract_call = para_client
-		.tx()
-		.sign_and_submit_then_watch_default(
-			&asset_hub_westend::tx().revive().call(
-				contract_address,
-				0,
-				Weight {
-					ref_time: contract_call_dry_run.gas_required.ref_time,
-					proof_size: contract_call_dry_run.gas_required.proof_size,
-				},
-				storage_deposit,
-				b"0xa0712d680000000000000000000000000000000000000000000000000000000000000001"
-					.to_vec(),
-			),
-			&alice_signer,
-		)
-		.await?
-		.wait_for_finalized_success()
-		.await?;
+
+	let mut call_txs = keys
+		.iter()
+		.map(|key| {
+			let params =
+				subxt::config::polkadot::PolkadotExtrinsicParamsBuilder::new().nonce(1).build();
+			para_client.tx().create_signed_offline(
+				&asset_hub_westend::tx().revive().call(
+					contract_address,
+					0,
+					Weight {
+						ref_time: contract_call_dry_run.gas_required.ref_time,
+						proof_size: contract_call_dry_run.gas_required.proof_size,
+					},
+					storage_deposit,
+					contract_data.clone(),
+				),
+				key,
+				params,
+			)
+		})
+		.collect::<Result<Vec<_>, _>>()?;
+	let alice_nonce = para_client.tx().account_nonce(&alice_account_id).await?;
+	let params = subxt::config::polkadot::PolkadotExtrinsicParamsBuilder::new()
+		.nonce(alice_nonce)
+		.build();
+	call_txs.push(para_client.tx().create_signed_offline(
+		&asset_hub_westend::tx().revive().call(
+			contract_address,
+			0,
+			Weight {
+				ref_time: contract_call_dry_run.gas_required.ref_time,
+				proof_size: contract_call_dry_run.gas_required.proof_size,
+			},
+			storage_deposit,
+			contract_data.clone(),
+		),
+		&alice,
+		params,
+	)?);
+	submit_txs(call_txs).await?;
+
+	log::info!("Test finished, sleeping for 600 seconds to allow for manual inspection");
 
 	// Wait to interact with PolkadotJS
-	tokio::time::sleep(std::time::Duration::from_secs(600)).await;
+	tokio::time::sleep(std::time::Duration::from_secs(6000)).await;
 
-	assert!(
-		contract_call
-			.find::<asset_hub_westend::revive::events::ContractEmitted>()
-			.count() > 0
-	);
+	Ok(())
+}
 
+async fn submit_txs(
+	txs: Vec<SubmittableExtrinsic<PolkadotConfig, OnlineClient<PolkadotConfig>>>,
+) -> Result<(), anyhow::Error> {
+	let futs = txs.iter().map(|tx| tx.submit_and_watch()).collect::<FuturesUnordered<_>>();
+	let res = futs.collect::<Vec<_>>().await;
+	let res: Result<Vec<_>, _> = res.into_iter().collect();
+	let res = res.expect("All the transactions submitted successfully");
+	let mut statuses = futures::stream::select_all(res);
+	while let Some(a) = statuses.next().await {
+		match a {
+			Ok(st) => match st {
+				subxt::tx::TxStatus::Validated => log::trace!("VALIDATED"),
+				subxt::tx::TxStatus::Broadcasted { num_peers } =>
+					log::trace!("BROADCASTED TO {num_peers}"),
+				subxt::tx::TxStatus::NoLongerInBestBlock => log::warn!("NO LONGER IN BEST BLOCK"),
+				subxt::tx::TxStatus::InBestBlock(_) => log::trace!("IN BEST BLOCK"),
+				subxt::tx::TxStatus::InFinalizedBlock(_) => log::trace!("IN FINALIZED BLOCK"),
+				subxt::tx::TxStatus::Error { message } => log::warn!("ERROR: {message}"),
+				subxt::tx::TxStatus::Invalid { message } => log::trace!("INVALID: {message}"),
+				subxt::tx::TxStatus::Dropped { message } => log::trace!("DROPPED: {message}"),
+			},
+			Err(e) => {
+				println!("Error status {:?}", e);
+			},
+		}
+	}
 	Ok(())
 }
