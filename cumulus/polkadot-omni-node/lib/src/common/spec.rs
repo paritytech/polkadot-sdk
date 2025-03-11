4c6c3fa1f6a2f88dcb41e456480106df3a90ba51
Alexandru Vasile <alexandru.vasile@parity.io>
2025-03-11 12:14:59 +0200

    cumulus: Apply cargo fmt
    Signed-off-by: Alexandru Vasile <alexandru.vasile@parity.io>

--- cumulus/polkadot-omni-node/lib/src/common/spec.rs
+++ cumulus/polkadot-omni-node/lib/src/common/spec.rs
@@ -462,8 +462,7 @@ where
 	) -> Pin<Box<dyn Future<Output = sc_service::error::Result<TaskManager>>>> {
 		// If the network backend is unspecified, use the default for the given chain.
 		let default_backend = parachain_config.chain_spec.network_backend();
-		let network_backend =
-			parachain_config.network.network_backend.unwrap_or(default_backend);
+		let network_backend = parachain_config.network.network_backend.unwrap_or(default_backend);
 		match network_backend {
 			sc_network::config::NetworkBackendType::Libp2p =>
 				<Self as NodeSpec>::start_node::<sc_network::NetworkWorker<_, _>>(
