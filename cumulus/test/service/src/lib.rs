1661753ff606e0f9c385ad3a25e57f7ede075b0c
Alexandru Vasile <60601340+lexnv@users.noreply.github.com>
2025-03-11 12:12:28 +0200

    Update cumulus/test/service/src/lib.rs
    Co-authored-by: Bastian Köcher <git@kchr.de>

--- cumulus/test/service/src/lib.rs
+++ cumulus/test/service/src/lib.rs
@@ -762,7 +762,7 @@ impl TestNodeBuilder {
 		// If the network backend is unspecified, use the default for the given chain.
 		let default_backend = relay_chain_config.chain_spec.network_backend();
 		let network_backend =
-			relay_chain_config.network.network_backend.clone().unwrap_or(default_backend);
+			relay_chain_config.network.network_backend.unwrap_or(default_backend);
 		let (task_manager, client, network, rpc_handlers, transaction_pool, backend) =
 			match network_backend {
 				sc_network::config::NetworkBackendType::Libp2p =>
