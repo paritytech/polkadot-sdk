85cc9abc5123fb68b3e79d7362941ded2817cbe7
Angelly Rivera Roque <lumosksm@gmail.com>
2025-03-12 22:55:09 +0800

    Reorganize Westend transfer tests

---
+++ cumulus/parachains/integration-tests/emulated/tests/assets/asset-hub-westend/src/tests/transfer/mod.rs
@@ -0,0 +1,12 @@
+// AssetHubWestend <> parachain
+pub(crate) mod ah_para;
+// AssetHubWestend <> system parachain
+pub(crate) mod ah_system;
+// Westend <> parachain
+pub(crate) mod relay_para;
+// Westend <> system parachain
+pub(crate) mod relay_system;
+// X <> AssetHubWestend <> Y
+pub(crate) mod through_ah;
+// X <> Westend <> Y
+pub(crate) mod through_relay;
