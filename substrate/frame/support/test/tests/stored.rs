4eb6bd3c569d2ea74a9c3f51c11fe38b827f1ed7
Dom <polkadotdom@gmail.com>
2025-03-12 10:48:00 -0400

    Fix issue with stored tests

--- substrate/frame/support/test/tests/stored.rs
+++ substrate/frame/support/test/tests/stored.rs
@@ -17,42 +17,42 @@ pub type UncheckedExtrinsic = generic::UncheckedExtrinsic<u32, RuntimeCall, Sign
 
 impl Config for Runtime {}
 
+#[derive_impl(frame_system::config_preludes::TestDefaultConfig)]
+impl frame_system::Config for Runtime {
+    type BaseCallFilter = frame_support::traits::Everything;
+    type RuntimeOrigin = RuntimeOrigin;
+    type Nonce = u64;
+    type RuntimeCall = RuntimeCall;
+    type Hash = sp_runtime::testing::H256;
+    type Hashing = sp_runtime::traits::BlakeTwo256;
+    type AccountId = u64;
+    type Lookup = sp_runtime::traits::IdentityLookup<Self::AccountId>;
+    type Block = Block;
+    type RuntimeEvent = RuntimeEvent;
+    type BlockHashCount = frame_support::traits::ConstU32<250>;
+    type BlockWeights = ();
+    type BlockLength = ();
+    type DbWeight = ();
+    type Version = ();
+    type PalletInfo = PalletInfo;
+    type AccountData = ();
+    type OnNewAccount = ();
+    type OnKilledAccount = ();
+    type SystemWeightInfo = ();
+    type SS58Prefix = ();
+    type OnSetCode = ();
+    type MaxConsumers = frame_support::traits::ConstU32<16>;
+}
+
+construct_runtime! {
+    pub struct Runtime
+    {
+        System: frame_system,
+        TestPallet: test_pallet,
+    }
+}
+
 fn main() {
-	#[derive_impl(frame_system::config_preludes::TestDefaultConfig)]
-	impl frame_system::Config for Runtime {
-		type BaseCallFilter = frame_support::traits::Everything;
-		type RuntimeOrigin = RuntimeOrigin;
-		type Nonce = u64;
-		type RuntimeCall = RuntimeCall;
-		type Hash = sp_runtime::testing::H256;
-		type Hashing = sp_runtime::traits::BlakeTwo256;
-		type AccountId = u64;
-		type Lookup = sp_runtime::traits::IdentityLookup<Self::AccountId>;
-		type Block = Block;
-		type RuntimeEvent = RuntimeEvent;
-		type BlockHashCount = frame_support::traits::ConstU32<250>;
-		type BlockWeights = ();
-		type BlockLength = ();
-		type DbWeight = ();
-		type Version = ();
-		type PalletInfo = PalletInfo;
-		type AccountData = ();
-		type OnNewAccount = ();
-		type OnKilledAccount = ();
-		type SystemWeightInfo = ();
-		type SS58Prefix = ();
-		type OnSetCode = ();
-		type MaxConsumers = frame_support::traits::ConstU32<16>;
-	}
-
-	construct_runtime! {
-		pub struct Runtime
-		{
-			System: frame_system,
-			TestPallet: test_pallet,
-		}
-	}
-
 	// Unit struct
 	#[stored]
 	struct UnitStruct;
