e383613e4b74c3be7a184abd8c0deb5887d0c848
Andrei Sandu <andrei-mihail@parity.io>
2025-03-13 22:24:18 +0200

    sync pov export fomat with DA format
    Signed-off-by: Andrei Sandu <andrei-mihail@parity.io>

--- cumulus/client/consensus/aura/src/collators/slot_based/mod.rs
+++ cumulus/client/consensus/aura/src/collators/slot_based/mod.rs
@@ -255,6 +255,8 @@ struct CollatorMessage<Block: BlockT> {
 	pub validation_code_hash: ValidationCodeHash,
 	/// Core index that this block should be submitted on
 	pub core_index: CoreIndex,
+	/// Maximum pov size. Currently needed only for exporting PoV. 
+	pub max_pov_size: usize,
 }
 
 /// Fetch the `CoreSelector` and `ClaimQueueOffset` for `parent_hash`.
