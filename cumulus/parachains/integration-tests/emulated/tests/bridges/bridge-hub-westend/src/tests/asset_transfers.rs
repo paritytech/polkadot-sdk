f05b6bf7e3a016dd4e48d8e76f948a47c7adcd25
claravanstaden <claravanstaden64@gmail.com>
2025-03-13 16:05:28 +0200

    fix test

--- cumulus/parachains/integration-tests/emulated/tests/bridges/bridge-hub-westend/src/tests/asset_transfers.rs
+++ cumulus/parachains/integration-tests/emulated/tests/bridges/bridge-hub-westend/src/tests/asset_transfers.rs
@@ -73,6 +73,7 @@ fn send_assets_from_penpal_westend_through_westend_ah_to_rococo_ah(
 		let sov_penpal_on_ahw = AssetHubWestend::sovereign_account_id_of(
 			AssetHubWestend::sibling_location_of(PenpalB::para_id()),
 		);
+		println!("sov_penpal_on_ahw: {:?}", sov_penpal_on_ahw);
 		let sov_ahr_on_ahw =
 			AssetHubWestend::sovereign_account_of_parachain_on_other_global_consensus(
 				ByGenesis(ROCOCO_GENESIS_HASH),
@@ -395,7 +396,7 @@ fn send_wnds_from_penpal_westend_through_asset_hub_westend_to_asset_hub_rococo()
 			vec![
 				// issue WNDs on AHR
 				RuntimeEvent::ForeignAssets(pallet_assets::Event::Issued { asset_id, owner, .. }) => {
-					asset_id: *asset_id == wnd_at_westend_parachains.clone(),
+					asset_id: *asset_id == wnd_at_asset_hub_rococo.clone(),
 					owner: owner == &receiver,
 				},
 				// message processed successfully
