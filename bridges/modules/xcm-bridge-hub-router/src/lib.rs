66e3196a623ee69bba0118883c97f6f633fbadf5
Matteo Muraca <56828990+muraca@users.noreply.github.com>
2025-03-13 11:36:07 +0100

    Remove `pallet::getter` usage from bridges/modules (#7120)
    # Description
    
    Part of #3326 
    
    As per title, the `pallet:getter` usage has been removed from:
    - `pallet-bridge-beefy`
    - `pallet-bridge-grandpa`
    - `pallet-bridge-messages`
    - `pallet-bridge-relayers`
    - `pallet-xcm-bridge-hub-router`
    
    
    polkadot address: 12poSUQPtcF1HUPQGY3zZu2P8emuW9YnsPduA4XG3oCEfJVp
    
    ---------
    
    Co-authored-by: Branislav Kontur <bkontur@gmail.com>
    Co-authored-by: cmd[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- bridges/modules/xcm-bridge-hub-router/src/lib.rs
+++ bridges/modules/xcm-bridge-hub-router/src/lib.rs
@@ -120,18 +120,18 @@ pub mod pallet {
 		fn on_initialize(_n: BlockNumberFor<T>) -> Weight {
 			// if XCM channel is still congested, we don't change anything
 			if T::LocalXcmChannelManager::is_congested(&T::SiblingBridgeHubLocation::get()) {
-				return T::WeightInfo::on_initialize_when_congested()
+				return T::WeightInfo::on_initialize_when_congested();
 			}
 
 			// if bridge has reported congestion, we don't change anything
 			let mut bridge = Self::bridge();
 			if bridge.is_congested {
-				return T::WeightInfo::on_initialize_when_congested()
+				return T::WeightInfo::on_initialize_when_congested();
 			}
 
 			// if we can't decrease the delivery fee factor anymore, we don't change anything
 			if bridge.delivery_fee_factor == MINIMAL_DELIVERY_FEE_FACTOR {
-				return T::WeightInfo::on_initialize_when_congested()
+				return T::WeightInfo::on_initialize_when_congested();
 			}
 
 			let previous_factor = bridge.delivery_fee_factor;
@@ -190,10 +190,14 @@ pub mod pallet {
 	/// primitives (lane-id aka bridge-id, derived from XCM locations) to support multiple  bridges
 	/// by the same pallet instance.
 	#[pallet::storage]
-	#[pallet::getter(fn bridge)]
 	pub type Bridge<T: Config<I>, I: 'static = ()> = StorageValue<_, BridgeState, ValueQuery>;
 
 	impl<T: Config<I>, I: 'static> Pallet<T, I> {
+		/// Bridge that we are using.
+		pub fn bridge() -> BridgeState {
+			Bridge::<T, I>::get()
+		}
+
 		/// Called when new message is sent (queued to local outbound XCM queue) over the bridge.
 		pub(crate) fn on_message_sent_to_bridge(message_size: u32) {
 			log::trace!(
@@ -208,7 +212,7 @@ pub mod pallet {
 				// if outbound queue is not congested AND bridge has not reported congestion, do
 				// nothing
 				if !is_channel_with_bridge_hub_congested && !is_bridge_congested {
-					return Err(())
+					return Err(());
 				}
 
 				// ok - we need to increase the fee factor, let's do that
@@ -276,7 +280,7 @@ impl<T: Config<I>, I: 'static> ExporterFor for Pallet<T, I> {
 					target: LOG_TARGET,
 					"Router with bridged_network_id {bridged_network:?} does not support bridging to network {network:?}!",
 				);
-				return None
+				return None;
 			}
 		}
 
@@ -298,7 +302,7 @@ impl<T: Config<I>, I: 'static> ExporterFor for Pallet<T, I> {
 					network,
 					remote_location,
 				);
-				return None
+				return None;
 			},
 		};
 
@@ -318,7 +322,7 @@ impl<T: Config<I>, I: 'static> ExporterFor for Pallet<T, I> {
 						network,
 						remote_location,
 					);
-					return None
+					return None;
 				},
 			},
 			None => 0,
@@ -388,7 +392,7 @@ impl<T: Config<I>, I: 'static> SendXcm for Pallet<T, I> {
 				// better to drop such messages here rather than at the bridge hub. Let's check the
 				// message size."
 				if message_size > HARD_MESSAGE_SIZE_LIMIT {
-					return Err(SendError::ExceedsMaxMessageSize)
+					return Err(SendError::ExceedsMaxMessageSize);
 				}
 
 				// We need to ensure that the known `dest`'s XCM version can comprehend the current
