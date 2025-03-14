58eace8609225e5658b28383be64d843f063ed38
pgherveou <pgherveou@gmail.com>
2025-03-14 12:06:38 +0100

    Fix pallet-revive-fixtures build.rs
    Fix pallet-revive-uapi resolution when building pallet-revive-fixtures
    contracts

--- substrate/frame/revive/fixtures/build.rs
+++ substrate/frame/revive/fixtures/build.rs
@@ -85,19 +85,36 @@ fn create_cargo_toml<'a>(
 	let mut cargo_toml: toml::Value = toml::from_str(include_str!("./build/_Cargo.toml"))?;
 	let uapi_dep = cargo_toml["dependencies"]["uapi"].as_table_mut().unwrap();
 
-	let metadata = MetadataCommand::new()
-		.manifest_path(fixtures_dir.join("Cargo.toml"))
-		.exec()
-		.expect("Failed to fetch cargo metadata");
+	let manifest_path = fixtures_dir.join("Cargo.toml");
+	let metadata = MetadataCommand::new().manifest_path(&manifest_path).exec().unwrap();
+	let dependency_graph = metadata.resolve.unwrap();
 
-	let mut uapi_pkgs: Vec<_> = metadata
+	// Get the pallet-revive-fixtures package id
+	let pallet_revive_fixtures_id = metadata
 		.packages
 		.iter()
-		.filter(|pkg| pkg.name == "pallet-revive-uapi")
-		.collect();
+		.find(|package| package.manifest_path.as_std_path() == manifest_path)
+		.map(|package| package.id.clone())
+		.unwrap();
+	let pallet_revive_fixtures_node = dependency_graph
+		.nodes
+		.iter()
+		.find(|node| node.id == pallet_revive_fixtures_id)
+		.unwrap();
 
-	uapi_pkgs.sort_by(|a, b| b.version.cmp(&a.version));
-	let uapi_pkg = uapi_pkgs.first().unwrap();
+	// Get the pallet-revive-uapi package id
+	let pallet_revive_uapi_dep = pallet_revive_fixtures_node
+		.deps
+		.iter()
+		.find(|pkg| pkg.name == "pallet_revive_uapi")
+		.expect("pallet-revive-uapi is a build dependency of pallet-revive-fixtures; qed");
+
+	// Get pallet-revive-uapi package
+	let uapi_pkg = metadata
+		.packages
+		.iter()
+		.find(|pkg| pkg.id == pallet_revive_uapi_dep.pkg)
+		.unwrap();
 
 	if uapi_pkg.source.is_none() {
 		uapi_dep.insert(
