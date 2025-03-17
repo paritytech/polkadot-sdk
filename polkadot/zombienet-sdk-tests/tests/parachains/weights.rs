468afc0c7493a4052c4dc900baf0bfb1b3a8bef0
Andrei Eres <eresav@me.com>
2025-03-17 12:36:35 +0100

    Update test

--- polkadot/zombienet-sdk-tests/tests/parachains/weights.rs
+++ polkadot/zombienet-sdk-tests/tests/parachains/weights.rs
@@ -11,14 +11,18 @@ use crate::helpers::asset_hub_westend::{
 use anyhow::anyhow;
 use futures::{stream::FuturesUnordered, StreamExt};
 use pallet_revive::AddressMapper;
-use sp_core::H256;
+use sp_core::{H160, H256};
 use std::str::FromStr;
 use subxt::{tx::SubmittableExtrinsic, OnlineClient, PolkadotConfig};
 use subxt_signer::{
 	sr25519::{dev, Keypair},
 	SecretUri,
 };
-use zombienet_sdk::{AddCollatorOptions, NetworkConfigBuilder};
+use zombienet_sdk::{LocalFileSystem, Network, NetworkConfigBuilder};
+
+const KEYS_COUNT: usize = 3000;
+const CHUNK_SIZE: usize = 500;
+const CALL_CHUNK_SIZE: usize = 1000;
 
 #[tokio::test(flavor = "multi_thread")]
 async fn weights_test() -> Result<(), anyhow::Error> {
@@ -26,6 +30,32 @@ async fn weights_test() -> Result<(), anyhow::Error> {
 		env_logger::Env::default().filter_or(env_logger::DEFAULT_FILTER_ENV, "info"),
 	);
 
+	let network = setup_network().await?;
+	let collator = network.get_node("collator")?;
+	let para_client: OnlineClient<PolkadotConfig> = collator.wait_client().await?;
+	let mut call_clients = vec![];
+	for _ in 0..(KEYS_COUNT / CALL_CHUNK_SIZE) {
+		let call_client: OnlineClient<PolkadotConfig> = collator.wait_client().await?;
+		call_clients.push(call_client);
+	}
+	log::info!("Network is ready");
+
+	let alice = dev::alice();
+	let keys = create_keys(KEYS_COUNT);
+	setup_accounts(&para_client, &alice, &keys).await?;
+	log::info!("Accounts ready");
+
+	let contract_address = instantiate_contract(&para_client, &alice).await?;
+	log::info!("Contract instantiated: {:?}", contract_address);
+
+	call_contract(&para_client, call_clients, contract_address, &alice, &keys).await?;
+	log::info!("Test finished, sleeping for 6000 seconds to allow for manual inspection");
+	tokio::time::sleep(std::time::Duration::from_secs(6000)).await;
+
+	Ok(())
+}
+
+async fn setup_network() -> Result<Network<LocalFileSystem>, anyhow::Error> {
 	let images = zombienet_sdk::environment::get_images_from_env();
 	let config = NetworkConfigBuilder::new()
 		.with_relaychain(|r| {
@@ -46,9 +76,10 @@ async fn weights_test() -> Result<(), anyhow::Error> {
 				)
 				.with_chain("asset-hub-westend-local")
 				.with_collator(|n| {
-					n.with_name("collator")
-						.validator(true)
-						.with_args(vec![("--force-in-memory-trie-cache").into(), ("-linfo").into()])
+					n.with_name("collator").validator(true).with_args(vec![
+						// ("--force-in-memory-trie-cache").into(),
+						("-linfo").into(),
+					])
 				})
 		})
 		.build()
@@ -59,39 +90,90 @@ async fn weights_test() -> Result<(), anyhow::Error> {
 	let spawn_fn = zombienet_sdk::environment::get_spawn_fn();
 	let network = spawn_fn(config).await?;
 
-	let validator0 = network.get_node("validator-0")?;
-	let validator1 = network.get_node("validator-1")?;
-	let collator = network.get_node("collator")?;
+	Ok(network)
+}
 
-	let _relay_client: OnlineClient<PolkadotConfig> = validator0.wait_client().await?;
-	let para_client: OnlineClient<PolkadotConfig> = collator.wait_client().await?;
+fn create_keys(n: usize) -> Vec<Keypair> {
+	(0..n)
+		.map(|i| {
+			let uri = SecretUri::from_str(&format!("//key{}", i)).unwrap();
+			Keypair::from_uri(&uri).unwrap()
+		})
+		.collect()
+}
 
-	validator0.assert("node_roles", 4.0).await?;
-	validator1.assert("node_roles", 4.0).await?;
-	collator.assert("node_roles", 4.0).await?;
+async fn setup_accounts(
+	para_client: &OnlineClient<PolkadotConfig>,
+	alice: &Keypair,
+	keys: &[Keypair],
+) -> Result<(), anyhow::Error> {
+	let alice_account_id = alice.public_key().to_account_id();
+	for chunk in keys.chunks(CHUNK_SIZE) {
+		let alice_nonce = para_client.tx().account_nonce(&alice_account_id).await?;
+		let txs = chunk
+			.iter()
+			.enumerate()
+			.map(|(i, key)| {
+				let params = subxt::config::polkadot::PolkadotExtrinsicParamsBuilder::new()
+					.nonce(alice_nonce + i as u64)
+					.build();
+				para_client.tx().create_signed_offline(
+					&asset_hub_westend::tx()
+						.balances()
+						.transfer_keep_alive(key.public_key().into(), 1000000000000),
+					alice,
+					params,
+				)
+			})
+			.collect::<Result<Vec<_>, _>>()?;
+		submit_txs(txs).await?;
+	}
+	log::info!("Sleeping for 30 seconds to finalize the transfer");
+	tokio::time::sleep(std::time::Duration::from_secs(30)).await;
 
-	log::info!("Network is ready");
+	let alice_nonce = para_client.tx().account_nonce(&alice_account_id).await?;
+	let params = subxt::config::polkadot::PolkadotExtrinsicParamsBuilder::new()
+		.nonce(alice_nonce)
+		.build();
+	para_client
+		.tx()
+		.sign_and_submit_then_watch(&asset_hub_westend::tx().revive().map_account(), alice, params)
+		.await?
+		.wait_for_finalized_success()
+		.await?;
 
-	let alice = dev::alice();
+	for chunk in keys.chunks(CHUNK_SIZE) {
+		let txs = chunk
+			.iter()
+			.map(|key| {
+				let params =
+					subxt::config::polkadot::PolkadotExtrinsicParamsBuilder::new().nonce(0).build();
+				para_client.tx().create_signed_offline(
+					&asset_hub_westend::tx().revive().map_account(),
+					key,
+					params,
+				)
+			})
+			.collect::<Result<Vec<_>, _>>()?;
+		submit_txs(txs).await?;
+	}
+	log::info!("Sleeping for 30 seconds to finalize the mapping");
+	tokio::time::sleep(std::time::Duration::from_secs(30)).await;
+
+	Ok(())
+}
+
+async fn instantiate_contract(
+	para_client: &OnlineClient<PolkadotConfig>,
+	alice: &Keypair,
+) -> Result<H160, anyhow::Error> {
+	let code_path = std::env::current_dir().unwrap().join("tests/parachains/contract.polkavm");
+	let code = std::fs::read(code_path)?;
 	let alice_account_id = alice.public_key().to_account_id();
 	let alice_h160 =
 		<asset_hub_westend_runtime::Runtime as pallet_revive::Config>::AddressMapper::to_address(
 			&alice.public_key().0.into(),
 		);
-
-	para_client
-		.tx()
-		.sign_and_submit_then_watch_default(&asset_hub_westend::tx().revive().map_account(), &alice)
-		.await?
-		.wait_for_finalized_success()
-		.await?;
-
-	let code_path = std::env::current_dir().unwrap().join("tests/parachains/contract.polkavm");
-	let code = std::fs::read(code_path)?;
-	let mint_100 = sp_core::hex2array!(
-		"a0712d680000000000000000000000000000000000000000000000000000000000000064"
-	)
-	.to_vec();
 
 	let upload_dry_run = para_client
 		.runtime_api()
@@ -106,18 +188,17 @@ async fn weights_test() -> Result<(), anyhow::Error> {
 		.unwrap();
 	let code_hash = upload_dry_run.code_hash;
 	let deposit = upload_dry_run.deposit;
-
 	para_client
 		.tx()
 		.sign_and_submit_then_watch_default(
 			&asset_hub_westend::tx().revive().upload_code(code.clone(), deposit),
-			&alice,
+			alice,
 		)
 		.await?
 		.wait_for_finalized_success()
 		.await?;
 
-	let contract_dry_run = para_client
+	let instantiate_dry_run = para_client
 		.runtime_api()
 		.at_latest()
 		.await?
@@ -131,7 +212,6 @@ async fn weights_test() -> Result<(), anyhow::Error> {
 			None,
 		))
 		.await?;
-
 	// We need a nonce before instantiating the contract
 	let alice_revive_nonce = para_client
 		.runtime_api()
@@ -140,16 +220,14 @@ async fn weights_test() -> Result<(), anyhow::Error> {
 		.call(asset_hub_westend::apis().revive_api().nonce(alice_h160))
 		.await?;
 	let contract_address = pallet_revive::create1(&alice_h160, alice_revive_nonce.into());
-	log::info!("Contract address: {:?}", contract_address);
 	let weight = Weight {
-		ref_time: contract_dry_run.gas_required.ref_time,
-		proof_size: contract_dry_run.gas_required.proof_size,
+		ref_time: instantiate_dry_run.gas_required.ref_time,
+		proof_size: instantiate_dry_run.gas_required.proof_size,
 	};
-	let deposit = match contract_dry_run.storage_deposit {
+	let deposit = match instantiate_dry_run.storage_deposit {
 		StorageDeposit::Charge(c) => c,
 		StorageDeposit::Refund(_) => 0,
 	};
-
 	para_client
 		.tx()
 		.sign_and_submit_then_watch_default(
@@ -161,65 +239,28 @@ async fn weights_test() -> Result<(), anyhow::Error> {
 				vec![],
 				None,
 			),
-			&alice,
+			alice,
 		)
 		.await?
 		.wait_for_finalized_success()
 		.await?;
 
-	let keys = (0..3000)
-		.map(|i| {
-			let uri = SecretUri::from_str(&format!("//key{}", i)).unwrap();
-			Keypair::from_uri(&uri).unwrap()
-		})
-		.collect::<Vec<_>>();
+	Ok(contract_address)
+}
 
-	for keys_chunk in keys.chunks(500) {
-		let alice_nonce = para_client.tx().account_nonce(&alice_account_id).await?;
-		let transfer_txs = keys_chunk
-			.iter()
-			.enumerate()
-			.map(|(i, key)| {
-				let params = subxt::config::polkadot::PolkadotExtrinsicParamsBuilder::new()
-					.nonce(alice_nonce + i as u64)
-					.build();
-				para_client.tx().create_signed_offline(
-					&asset_hub_westend::tx()
-						.balances()
-						.transfer_keep_alive(key.public_key().into(), 1000000000000),
-					&alice,
-					params,
-				)
-			})
-			.collect::<Result<Vec<_>, _>>()?;
-		submit_txs(transfer_txs).await?;
-		log::info!("Sleeping for 60 seconds to finalize the transfer"); // Should have use wait_for_finalized_success, but needs to rewrite the code
-		tokio::time::sleep(std::time::Duration::from_secs(60)).await;
-	}
-
-	log::info!("Accounts ready");
-
-	for keys_chunk in keys.chunks(500) {
-		let mapping_txs = keys_chunk
-			.iter()
-			.map(|key| {
-				let params =
-					subxt::config::polkadot::PolkadotExtrinsicParamsBuilder::new().nonce(0).build();
-				para_client.tx().create_signed_offline(
-					&asset_hub_westend::tx().revive().map_account(),
-					key,
-					params,
-				)
-			})
-			.collect::<Result<Vec<_>, _>>()?;
-		submit_txs(mapping_txs).await?;
-		log::info!("Sleeping for 60 seconds to finalize the mapping"); // Should have use wait_for_finalized_success, but needs to rewrite the code
-		tokio::time::sleep(std::time::Duration::from_secs(60)).await;
-	}
-
-	log::info!("Accounts mapped");
-
-	let contract_call_dry_run = para_client
+async fn call_contract(
+	para_client: &OnlineClient<PolkadotConfig>,
+	mut call_clients: Vec<OnlineClient<PolkadotConfig>>,
+	contract_address: H160,
+	alice: &Keypair,
+	keys: &[Keypair],
+) -> Result<(), anyhow::Error> {
+	let mint_100 = sp_core::hex2array!(
+		"a0712d680000000000000000000000000000000000000000000000000000000000000064"
+	)
+	.to_vec();
+	let alice_account_id = alice.public_key().to_account_id();
+	let call_dry_run = para_client
 		.runtime_api()
 		.at_latest()
 		.await?
@@ -232,26 +273,19 @@ async fn weights_test() -> Result<(), anyhow::Error> {
 			mint_100.clone(),
 		))
 		.await?;
-	let storage_deposit = match contract_call_dry_run.storage_deposit {
+	let deposit = match call_dry_run.storage_deposit {
 		StorageDeposit::Charge(c) => c,
 		StorageDeposit::Refund(_) => 0,
 	};
-
-	let call_chunk_size = 1000; // para_client has a limit to 1024 rpc calls
-	let mut para_clients: Vec<OnlineClient<PolkadotConfig>> = vec![];
-	for _ in 0..(keys.len() / call_chunk_size) {
-		let para_client: OnlineClient<PolkadotConfig> = collator.wait_client().await?;
-		para_clients.push(para_client);
-	}
-	let mut call_txs = vec![];
-	for keys_chunk in keys.chunks(call_chunk_size) {
-		let para_client = para_clients.pop().unwrap();
-		let call_chunk = keys_chunk
+	let mut txs = vec![];
+	for chunk in keys.chunks(CALL_CHUNK_SIZE) {
+		let para_client = call_clients.pop().unwrap();
+		let txs_chunk = chunk
 			.iter()
 			.map(|key| {
 				let weight = Weight {
-					ref_time: contract_call_dry_run.gas_required.ref_time,
-					proof_size: contract_call_dry_run.gas_required.proof_size,
+					ref_time: call_dry_run.gas_required.ref_time,
+					proof_size: call_dry_run.gas_required.proof_size,
 				};
 				let params =
 					subxt::config::polkadot::PolkadotExtrinsicParamsBuilder::new().nonce(1).build();
@@ -260,7 +294,7 @@ async fn weights_test() -> Result<(), anyhow::Error> {
 						contract_address,
 						0,
 						weight,
-						storage_deposit,
+						deposit,
 						mint_100.clone(),
 					),
 					key,
@@ -268,24 +302,17 @@ async fn weights_test() -> Result<(), anyhow::Error> {
 				)
 			})
 			.collect::<Result<Vec<_>, _>>()?;
-		call_txs.extend(call_chunk);
+		txs.extend(txs_chunk);
 	}
-	let finalized_blocks = submit_txs(call_txs).await?;
-
-	for block_hash in finalized_blocks {
+	let finalized_blocks = submit_txs(txs).await?;
+	for block in finalized_blocks {
 		let weight = para_client
 			.storage()
-			.at(block_hash)
+			.at(block)
 			.fetch(&asset_hub_westend::storage().system().block_weight())
 			.await?;
-
-		log::info!("Weight of block {:?}: {:?}", block_hash, weight);
+		log::info!("Weight of block {:?}: {:?}", block, weight);
 	}
-
-	log::info!("Test finished, sleeping for 6000 seconds to allow for manual inspection");
-
-	// Wait to interact with PolkadotJS
-	tokio::time::sleep(std::time::Duration::from_secs(6000)).await;
 
 	Ok(())
 }
