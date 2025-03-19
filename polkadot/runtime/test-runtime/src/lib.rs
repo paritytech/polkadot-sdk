b8fa3e2369058a32e1f1a3958bed88855106a37b
Andrei Sandu <andrei-mihail@parity.io>
2025-03-19 17:26:58 +0200

    fix tests
    Signed-off-by: Andrei Sandu <andrei-mihail@parity.io>

--- polkadot/runtime/test-runtime/src/lib.rs
+++ polkadot/runtime/test-runtime/src/lib.rs
@@ -32,12 +32,14 @@ use pallet_transaction_payment::FungibleAdapter;
 
 use polkadot_runtime_parachains::{
 	assigner_coretime as parachains_assigner_coretime, configuration as parachains_configuration,
-	configuration::ActiveConfigHrmpChannelSizeAndCapacityRatio, coretime,
-	disputes as parachains_disputes, disputes::slashing as parachains_slashing,
+	configuration::ActiveConfigHrmpChannelSizeAndCapacityRatio,
+	coretime, disputes as parachains_disputes,
+	disputes::slashing as parachains_slashing,
 	dmp as parachains_dmp, hrmp as parachains_hrmp, inclusion as parachains_inclusion,
 	initializer as parachains_initializer, on_demand as parachains_on_demand,
 	origin as parachains_origin, paras as parachains_paras,
-	paras_inherent as parachains_paras_inherent, runtime_api_impl::v11 as runtime_impl,
+	paras_inherent as parachains_paras_inherent,
+	runtime_api_impl::{v11 as runtime_impl, vstaging as parachains_staging_runtime_api_impl},
 	scheduler as parachains_scheduler, session_info as parachains_session_info,
 	shared as parachains_shared,
 };
