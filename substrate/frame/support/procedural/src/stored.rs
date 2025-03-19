b870085a25d0f6204f33a70d1302fb843f553445
Dom <polkadotdom@gmail.com>
2025-03-19 15:54:13 -0400

    mel_bounds usage first attempt

--- substrate/frame/support/procedural/src/stored.rs
+++ substrate/frame/support/procedural/src/stored.rs
@@ -1,10 +1,10 @@
 use proc_macro::TokenStream;
 use quote::quote;
 use syn::{
-    parse_macro_input, Data, DeriveInput, Ident, Token, TypeParamBound,
+    parse_macro_input, Data, DeriveInput, Ident, Token, TypeParamBound, Type,
 };
 use syn::punctuated::Punctuated;
-use syn::parse::{Parse, ParseStream, Parser}; // Added Parser trait
+use syn::parse::{Parse, ParseStream, Parser}; // Import Parser trait
 
 /// A helper struct to hold a comma-separated list of identifiers, ie no_bounds(A, B, C).
 #[derive(Default)]
@@ -17,36 +17,84 @@ impl Parse for IdentList {
     }
 }
 
+/// Represents a single mel_bounds item, e.g.:
+/// - `S`        (shorthand for `S: MaxEncodedLen`)
+/// - `T: Default` (explicit bound, used as-is)
+struct MelBoundItem {
+    ident: Ident,
+    bound: Option<Type>,
+}
+
+impl Parse for MelBoundItem {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        let ident: Ident = input.parse()?;
+        let bound = if input.peek(Token![:]) {
+            let _colon: Token![:] = input.parse()?;
+            Some(input.parse()?)
+        } else {
+            None
+        };
+        Ok(MelBoundItem { ident, bound })
+    }
+}
+
+/// A helper struct to hold a comma-separated list of MelBoundItem.
+#[derive(Default)]
+struct MelBoundList(Punctuated<MelBoundItem, Token![,]>);
+
+impl Parse for MelBoundList {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        let list = Punctuated::parse_terminated(input)?;
+        Ok(MelBoundList(list))
+    }
+}
+
 /// The #[stored] attribute. To be used for structs or enums that will find themselves placed in
 /// runtime storage.
 ///
 /// It does the following:
-/// - Parses attribute no_bounds and mel_bounds arguments to determine type parameters that should not be bounded,
-///   and those for which additional bounds may be applied later (mel_bounds is not used for now).
+/// - Parses attribute `no_bounds` and `mel_bounds` arguments. If `mel_bounds` is provided, it takes
+///   precedence over `no_bounds` for generating the codec attribute.
 /// - Adds default trait bounds to any type parameter that should be bounded.
-/// - Conditionally adds #[codec(mel_bound(...))] for generic parameters not listed in no_bounds.
-///   If all generics are listed, it will still add #[codec(mel_bound())].
-/// - Generates the necessary derive attributes and other metadata required for storage in a substrate runtime.
-/// - Adjusts those attributes based on whether the item is a struct or an enum.
-/// - Utilizes the NoBound version of the derives if there's a type parameter that should be unbounded.
+/// - Conditionally adds `#[codec(mel_bound(...))]`:
+///     - If `mel_bounds` is provided, each bare generic (e.g. `S`) is expanded to `S: MaxEncodedLen`,
+///       while an explicit bound (e.g. `T: Default`) is used as-is.
+///     - Otherwise, if `no_bounds` is provided, then all generics not listed in `no_bounds` get
+///       the `: MaxEncodedLen` bound.
+/// - Generates the necessary derive attributes and other metadata required for storage.
 pub fn stored(attr: TokenStream, input: TokenStream) -> TokenStream {
-    // Initial parsing.
-    let (no_bound_params, _mel_bound_params) = parse_stored_args(attr);
+    // Parse the stored attribute arguments.
+    // Returns a tuple: (no_bounds identifiers, optional mel_bounds items)
+    let (no_bound_params, mel_bound_params) = parse_stored_args(attr);
     let mut input = parse_macro_input!(input as DeriveInput);
 
     // Remove the #[stored] attribute to prevent re-emission.
     input.attrs.retain(|attr| !attr.path().is_ident("stored"));
 
-    // Should we use NoBounds version of derives?
+    // Should we use the NoBound version of derives?
     let should_nobound_derive = !no_bound_params.is_empty();
     if should_nobound_derive {
-        // Add standard trait bounds to any generics that need bounds still.
         add_normal_trait_bounds(&mut input.generics, &no_bound_params);
     }
 
-    // Compute #[codec(mel_bound(...))] if no_bounds is provided.
-    // We add bounds for any generic type parameter not listed in no_bounds.
-    let codec_mel_bound_attr = if !no_bound_params.is_empty() {
+    // Compute the #[codec(mel_bound(...))] attribute.
+    // If mel_bounds is provided, use it exclusively.
+    // For each mel_bounds item, if no explicit bound is provided, add `: MaxEncodedLen`.
+    let codec_mel_bound_attr = if let Some(mel_bounds) = mel_bound_params {
+        let bounds = mel_bounds.into_iter().map(|item| {
+            let ident = item.ident;
+            if let Some(bound) = item.bound {
+                quote! { #ident: #bound }
+            } else {
+                quote! { #ident: MaxEncodedLen }
+            }
+        });
+        quote! {
+            #[codec(mel_bound( #(#bounds),* ))]
+        }
+    } else if !no_bound_params.is_empty() {
+        // Otherwise, if no_bounds is provided, add MaxEncodedLen for every generic
+        // not listed in no_bounds.
         let all_generics: Vec<_> = input.generics.params.iter().filter_map(|param| {
             if let syn::GenericParam::Type(type_param) = param {
                 Some(&type_param.ident)
@@ -94,31 +142,30 @@ pub fn stored(attr: TokenStream, input: TokenStream) -> TokenStream {
         quote! {}
     };
 
-    // Add cfg_attr for DecodeWithMemTracking
+    // Add cfg_attr for DecodeWithMemTracking.
     let mem_tracking_derive = quote! {
         #[cfg_attr(test, derive(DecodeWithMemTracking))]
     };
 
-    // Input extraction.
+    // Extract input parts.
     let struct_ident = &input.ident;
     let (_generics, _ty_generics, where_clause) = input.generics.split_for_impl();
     let generics = &input.generics;
     let attrs = &input.attrs;
     let vis = &input.vis;
 
-    let common_derives =
-        quote! {
-            #[derive(
-                #partial_eq_i,
-                #clone_i,
-                #eq_i,
-                #debug_i,
-                Encode,
-                Decode,
-                TypeInfo,
-                MaxEncodedLen
-            )]
-        };
+    let common_derives = quote! {
+        #[derive(
+            #partial_eq_i,
+            #clone_i,
+            #eq_i,
+            #debug_i,
+            Encode,
+            Decode,
+            TypeInfo,
+            MaxEncodedLen
+        )]
+    };
 
     let common_attrs = quote! {
         #common_derives
@@ -128,24 +175,21 @@ pub fn stored(attr: TokenStream, input: TokenStream) -> TokenStream {
         #(#attrs)*
     };
 
-    // Appropriate ordering for each type of input.
+    // Generate the final output based on whether the input is a struct or an enum.
     let expanded = match input.data {
         Data::Struct(ref data_struct) => match data_struct.fields {
-            // Named-field structs.
             syn::Fields::Named(ref fields) => {
                 quote! {
                     #common_attrs
                     #vis struct #struct_ident #generics #where_clause #fields
                 }
             },
-            // Tuple structs.
             syn::Fields::Unnamed(ref fields) => {
                 quote! {
                     #common_attrs
                     #vis struct #struct_ident #generics #fields #where_clause;
                 }
             },
-            // Unit structs.
             syn::Fields::Unit => {
                 quote! {
                     #common_attrs
@@ -154,7 +198,6 @@ pub fn stored(attr: TokenStream, input: TokenStream) -> TokenStream {
             },
         },
         Data::Enum(ref data_enum) => {
-            // Enums.
             let variant_tokens: Vec<_> = data_enum.variants
                 .iter()
                 .map(|variant| quote! { #variant })
@@ -167,7 +210,6 @@ pub fn stored(attr: TokenStream, input: TokenStream) -> TokenStream {
             }
         },
         Data::Union(_) => {
-            // Unions are not supported.
             return syn::Error::new_spanned(
                 &input,
                 "The #[stored] attribute cannot be used on unions."
@@ -181,13 +223,14 @@ pub fn stored(attr: TokenStream, input: TokenStream) -> TokenStream {
 }
 
 /// Extracts type parameters from the attribute arguments for no_bounds and mel_bounds.
-/// For example, given #[stored(no_bounds(A, B), mel_bounds(X, Y))], this function extracts
-/// A and B into no_bounds and X and Y into mel_bounds.
-fn parse_stored_args(args: TokenStream) -> (Vec<Ident>, Vec<Ident>) {
+/// For example, given:
+///   #[stored(no_bounds(A, B), mel_bounds(S, T: Default))]
+/// this function extracts A and B into no_bounds and S, T (with their optional bounds) into mel_bounds.
+fn parse_stored_args(args: TokenStream) -> (Vec<Ident>, Option<Vec<MelBoundItem>>) {
     let mut no_bounds = Vec::new();
-    let mut mel_bounds = Vec::new();
+    let mut mel_bounds: Option<Vec<MelBoundItem>> = None;
     if args.is_empty() {
-        return (no_bounds, mel_bounds);
+        return (no_bounds, None);
     }
     let parsed = Punctuated::<syn::Meta, Token![,]>::parse_terminated
         .parse2(args.into())
@@ -199,8 +242,8 @@ fn parse_stored_args(args: TokenStream) -> (Vec<Ident>, Vec<Ident>) {
                     let ident_list: IdentList = syn::parse2(meta_list.tokens).unwrap_or_default();
                     no_bounds.extend(ident_list.0.into_iter());
                 } else if ident == "mel_bounds" {
-                    let ident_list: IdentList = syn::parse2(meta_list.tokens).unwrap_or_default();
-                    mel_bounds.extend(ident_list.0.into_iter());
+                    let mel_bound_list: MelBoundList = syn::parse2(meta_list.tokens).unwrap_or_default();
+                    mel_bounds = Some(mel_bound_list.0.into_iter().collect());
                 }
             }
         }
@@ -221,13 +264,10 @@ fn add_normal_trait_bounds(
         "core::fmt::Debug",
     ];
 
-    // For each type parameter.
     for param in &mut generics.params {
         if let syn::GenericParam::Type(type_param) = param {
-            // Skip parameters specified in no_bound_params.
             if !no_bound_params.contains(&type_param.ident) {
                 for bound_name in &normal_bounds {
-                    // Add the bound.
                     let bound: TypeParamBound = syn::parse_str(bound_name)
                         .unwrap_or_else(|_| panic!("Failed to parse bound: {}", bound_name));
                     type_param.bounds.push(bound);
