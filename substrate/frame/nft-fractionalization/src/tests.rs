baeda0219fedb55242b80857481f23bb67800fcf
kR1s0147 <giridharchukkala@gmail.com>
2025-03-19 17:32:40 +0530

    Migrate pallet-nft-fractioinalization to umbrella crate

--- substrate/frame/nft-fractionalization/src/tests.rs
+++ substrate/frame/nft-fractionalization/src/tests.rs
@@ -18,15 +18,16 @@
 //! Tests for Nft fractionalization pallet.
 
 use crate::{mock::*, *};
-use frame_support::{
-	assert_noop, assert_ok,
-	traits::{
-		fungible::{hold::Inspect as InspectHold, Mutate as MutateFungible},
-		fungibles::{metadata::Inspect, InspectEnumerable},
-	},
+
+use frame::{
+	TokenError::FundsUnavailable,
+	testing_prelude::*,
+	deps::sp_runtime::ModuleError,
 };
+use fungible::{hold::Inspect as InspectHold, Mutate as MutateFungible};
+use fungibles::{metadata::Inspect, InspectEnumerable};
+
 use pallet_nfts::CollectionConfig;
-use sp_runtime::{DispatchError, ModuleError, TokenError::FundsUnavailable};
 
 fn assets() -> Vec<u32> {
 	let mut s: Vec<_> = <<Test as Config>::Assets>::asset_ids().collect();
@@ -93,7 +94,7 @@ fn fractionalize_should_work() {
 		assert_eq!(assets(), vec![asset_id]);
 		assert_eq!(Assets::balance(asset_id, account(2)), fractions);
 		assert_eq!(Balances::total_balance_on_hold(&account(1)), 2);
-		assert_eq!(String::from_utf8(Assets::name(0)).unwrap(), "Frac 0-0");
+		assert_eq!(String::from_utf8(<Assets as Inspect<<Test as frame_system::Config>::AccountId>>::name(0)).unwrap(), "Frac 0-0");
 		assert_eq!(String::from_utf8(Assets::symbol(0)).unwrap(), "FRAC");
 		assert_eq!(Nfts::owner(nft_collection_id, nft_id), Some(account(1)));
 		assert_noop!(
