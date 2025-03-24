74d6792c6c1a0ff5a662ca27644fafc37d08ab31
Adrian Catangiu <adrian@parity.io>
2025-03-24 18:11:44 +0200

    more aliasing tests

--- cumulus/parachains/integration-tests/emulated/tests/people/people-westend/src/tests/aliases.rs
+++ cumulus/parachains/integration-tests/emulated/tests/people/people-westend/src/tests/aliases.rs
@@ -124,3 +124,54 @@ fn aliasing_child_locations() {
 	let target = Location::new(0, X1([AccountId32 { network: None, id: [1u8; 32] }].into()));
 	assert!(!<XcmConfig as xcm_executor::Config>::Aliasers::contains(&origin, &target));
 }
+
+#[test]
+fn asset_hub_root_aliases_anything() {
+	use PeopleWestendXcmConfig as XcmConfig;
+	// Allows AH root to alias anything.
+	let origin = Location::new(1, X1([Parachain(1000)].into()));
+
+	let target = Location::new(1, X1([Parachain(2000)].into()));
+	assert!(<XcmConfig as xcm_executor::Config>::Aliasers::contains(&origin, &target));
+	let target = Location::new(1, X1([AccountId32 { network: None, id: [1u8; 32] }].into()));
+	assert!(<XcmConfig as xcm_executor::Config>::Aliasers::contains(&origin, &target));
+	let target =
+		Location::new(1, X2([Parachain(8), AccountId32 { network: None, id: [1u8; 32] }].into()));
+	assert!(<XcmConfig as xcm_executor::Config>::Aliasers::contains(&origin, &target));
+	let target = Location::new(1, X3([Parachain(42), PalletInstance(8), GeneralIndex(9)].into()));
+	assert!(<XcmConfig as xcm_executor::Config>::Aliasers::contains(&origin, &target));
+	let target = Location::new(2, X1([GlobalConsensus(Ethereum { chain_id: 1 })].into()));
+	assert!(<XcmConfig as xcm_executor::Config>::Aliasers::contains(&origin, &target));
+	let target = Location::new(2, X2([GlobalConsensus(Polkadot), Parachain(1000)].into()));
+	assert!(<XcmConfig as xcm_executor::Config>::Aliasers::contains(&origin, &target));
+	let target = Location::new(0, X2([PalletInstance(8), GeneralIndex(9)].into()));
+	assert!(<XcmConfig as xcm_executor::Config>::Aliasers::contains(&origin, &target));
+
+	// Other AH locations cannot alias anything.
+	let origin = Location::new(1, X2([Parachain(1000), GeneralIndex(9)].into()));
+	assert!(!<XcmConfig as xcm_executor::Config>::Aliasers::contains(&origin, &target));
+	let origin = Location::new(1, X2([Parachain(1000), PalletInstance(9)].into()));
+	assert!(!<XcmConfig as xcm_executor::Config>::Aliasers::contains(&origin, &target));
+	let origin = Location::new(
+		1,
+		X2([Parachain(1000), AccountId32 { network: None, id: [1u8; 32] }].into()),
+	);
+	assert!(!<XcmConfig as xcm_executor::Config>::Aliasers::contains(&origin, &target));
+
+	// Other root locations cannot alias anything.
+	let origin = Location::new(1, Here);
+	let target = Location::new(2, X1([GlobalConsensus(Ethereum { chain_id: 1 })].into()));
+	assert!(!<XcmConfig as xcm_executor::Config>::Aliasers::contains(&origin, &target));
+	let target = Location::new(2, X2([GlobalConsensus(Polkadot), Parachain(1000)].into()));
+	assert!(!<XcmConfig as xcm_executor::Config>::Aliasers::contains(&origin, &target));
+	let target = Location::new(0, X2([PalletInstance(8), GeneralIndex(9)].into()));
+	assert!(!<XcmConfig as xcm_executor::Config>::Aliasers::contains(&origin, &target));
+
+	let origin = Location::new(0, Here);
+	let target = Location::new(1, X1([Parachain(2000)].into()));
+	assert!(!<XcmConfig as xcm_executor::Config>::Aliasers::contains(&origin, &target));
+	let origin = Location::new(1, X1([Parachain(1001)].into()));
+	assert!(!<XcmConfig as xcm_executor::Config>::Aliasers::contains(&origin, &target));
+	let origin = Location::new(1, X1([Parachain(1002)].into()));
+	assert!(!<XcmConfig as xcm_executor::Config>::Aliasers::contains(&origin, &target));
+}
