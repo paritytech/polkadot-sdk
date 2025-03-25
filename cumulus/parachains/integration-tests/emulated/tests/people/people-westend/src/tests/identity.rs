eaaac2436a2bea05994cf77a3266d657a402b649
Adrian Catangiu <adrian@parity.io>
2025-03-25 10:14:37 +0200

    fix clippy

--- cumulus/parachains/integration-tests/emulated/tests/people/people-westend/src/tests/identity.rs
+++ cumulus/parachains/integration-tests/emulated/tests/people/people-westend/src/tests/identity.rs
@@ -86,7 +86,7 @@ fn set_identity_cross_chain() {
 		]);
 
 		let signed_origin =
-			<AssetHubWestend as Chain>::RuntimeOrigin::signed(asset_hub_westend_alice.into());
+			<AssetHubWestend as Chain>::RuntimeOrigin::signed(asset_hub_westend_alice);
 		assert_ok!(<AssetHubWestend as AssetHubWestendPallet>::PolkadotXcm::execute(
 			signed_origin,
 			bx!(xcm::VersionedXcm::from(xcm_message.into())),
