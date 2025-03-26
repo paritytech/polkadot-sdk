aec4409aa746b4cb676ca985ad1ca3ced90295ff
Raymond Cheung <178801527+raymondkfcheung@users.noreply.github.com>
2025-03-26 10:45:56 +0000

    Fix test

--- cumulus/parachains/integration-tests/emulated/tests/assets/asset-hub-westend/src/tests/exchange_asset.rs
+++ cumulus/parachains/integration-tests/emulated/tests/assets/asset-hub-westend/src/tests/exchange_asset.rs
@@ -13,18 +13,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use crate::{
-	create_pool_with_wnd_on,
-	imports::{asset_hub_westend_runtime::ExistentialDeposit, *},
-};
+use crate::{create_pool_with_wnd_on, imports::*};
 use asset_hub_westend_runtime::{
 	xcm_config::WestendLocation, Balances, ForeignAssets, PolkadotXcm, RuntimeOrigin,
 };
 use emulated_integration_tests_common::{accounts::ALICE, xcm_emulator::TestExt};
-use frame_support::{
-	assert_ok,
-	traits::fungible::{Inspect, Mutate},
-};
+use frame_support::{assert_ok, traits::fungible::Inspect};
 use parachains_common::{AccountId, Balance};
 use std::convert::Into;
 use xcm::latest::{Assets, Location, Xcm};
@@ -41,40 +35,46 @@ fn exchange_asset_success() {
 	let asset_id = AssetId(asset_location.clone());
 
 	AssetHubWestend::execute_with(|| {
-		assert_ok!(<Balances as Mutate<_>>::mint_into(
-			&alice,
-			ExistentialDeposit::get() + (2_000 * UNITS)
-		));
-
 		assert_ok!(ForeignAssets::force_create(
 			RuntimeOrigin::root(),
 			asset_location.clone().into(),
 			alice.clone().into(),
 			true,
-			1,
+			1
 		));
 	});
 
 	create_pool_with_wnd_on!(AssetHubWestend, asset_location.clone(), true, alice.clone());
 
 	AssetHubWestend::execute_with(|| {
-		assert_ok!(Balances::mint_into(&alice, 1_000 * UNITS));
-
 		let give: Assets = (native_asset_id, 500 * UNITS).into();
 		let want: Assets = (asset_id, 660 * UNITS).into();
 
-		let xcm = Xcm(vec![ExchangeAsset { give: give.into(), want: want.into(), maximal: true }]);
+		let xcm = Xcm(vec![
+			WithdrawAsset(give.clone().into()),
+			ExchangeAsset { give: give.into(), want: want.into(), maximal: true },
+			DepositAsset {
+				assets: Wild(All),
+				beneficiary: alice.clone().into(),
+			},
+		]);
+
+		let foreign_balance_before = ForeignAssets::balance(asset_location.clone(), &alice);
+		let wnd_balance_before = Balances::total_balance(&alice);
 
 		assert_ok!(PolkadotXcm::execute(origin, bx!(xcm::VersionedXcm::from(xcm)), Weight::MAX));
 
-		let foreign_balance = ForeignAssets::balance(asset_location, &alice);
+		let foreign_balance_after = ForeignAssets::balance(asset_location, &alice);
 		assert!(
-			foreign_balance >= 660 * UNITS,
-			"Expected at least 660 units, got {foreign_balance}"
+			foreign_balance_after >= foreign_balance_before + 660 * UNITS,
+			"Expected foreign balance to increase by at least 660 units, got {foreign_balance_after} from {foreign_balance_before}"
 		);
 
-		let wnd_balance = Balances::total_balance(&alice);
-		assert!(wnd_balance < 1_500 * UNITS, "Expected WND balance to decrease, got {wnd_balance}");
+		let wnd_balance_after = Balances::total_balance(&alice);
+		assert!(
+			wnd_balance_after <= wnd_balance_before - 500 * UNITS,
+			"Expected WND balance to decrease by at least 500 units, got {wnd_balance_after} from {wnd_balance_before}"
+		);
 	});
 }
 /*
