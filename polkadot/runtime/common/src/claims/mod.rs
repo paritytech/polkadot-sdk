2f1dc11b488590cce21e835ceb0a872e0204dcad
Sebastian Kunert <skunert49@gmail.com>
2025-03-27 16:00:25 +0200

    Remove broken URL to claim statement.

--- polkadot/runtime/common/src/claims/mod.rs
+++ polkadot/runtime/common/src/claims/mod.rs
@@ -109,12 +109,10 @@ impl StatementKind {
 		match self {
 			StatementKind::Regular =>
 				&b"I hereby agree to the terms of the statement whose SHA-256 multihash is \
-				Qmc1XYqT6S39WNp2UeiRUrZichUWUPpGEThDE6dAb3f6Ny. (This may be found at the URL: \
-				https://statement.polkadot.network/regular.html)"[..],
+				Qmc1XYqT6S39WNp2UeiRUrZichUWUPpGEThDE6dAb3f6Ny."[..],
 			StatementKind::Saft =>
 				&b"I hereby agree to the terms of the statement whose SHA-256 multihash is \
-				QmXEkMahfhHJPzT3RjkXiZVFi77ZeVeuxtAjhojGRNYckz. (This may be found at the URL: \
-				https://statement.polkadot.network/saft.html)"[..],
+				QmXEkMahfhHJPzT3RjkXiZVFi77ZeVeuxtAjhojGRNYckz."[..],
 		}
 	}
 }
