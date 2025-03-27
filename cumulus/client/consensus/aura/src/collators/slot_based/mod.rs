bce7593e84fd7c5125cb3a2b164bb79dc553182e
Alexandru Gheorghe <49718502+alexggh@users.noreply.github.com>
2025-03-27 09:41:00 +0200

    Update cumulus/client/consensus/aura/src/collators/slot_based/mod.rs
    Co-authored-by: Bastian Köcher <git@kchr.de>

--- cumulus/client/consensus/aura/src/collators/slot_based/mod.rs
+++ cumulus/client/consensus/aura/src/collators/slot_based/mod.rs
@@ -146,8 +146,6 @@ pub struct Params<Block, BI, CIDP, Client, Backend, RClient, CHP, Proposer, CS, 
 	/// This is an experimental cli arg, it is meant to be used only if collator is overshooting
 	/// the PoV size, and building blocks that do not fit in the max_pov_size. It is a percentage
 	/// of the max_pov_size configuration of the relay-chain.
-	///
-	/// It will be removed once <https://github.com/paritytech/polkadot-sdk/issues/6020> is fixed.
 	pub experimental_max_pov_percentage: Option<u32>,
 }
 
