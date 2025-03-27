87efc24434340593bbb4f082aecb71b160975b7e
Raymond Cheung <178801527+raymondkfcheung@users.noreply.github.com>
2025-03-27 14:55:17 +0000

    Fix fmt

--- cumulus/parachains/runtimes/test-utils/src/test_cases.rs
+++ cumulus/parachains/runtimes/test-utils/src/test_cases.rs
@@ -22,15 +22,18 @@ use crate::{
 };
 use codec::Encode;
 use frame_support::{
-	assert_ok,
-	traits::{Get, OriginTrait},
+	assert_err_ignore_postinfo, assert_ok,
+	traits::{fungible::Inspect, Get, OriginTrait},
 };
-use parachains_common::AccountId;
+use parachains_common::{AccountId, Balance};
 use sp_runtime::{
 	traits::{Block as BlockT, StaticLookup},
 	DispatchError, Either,
 };
-use xcm::prelude::XcmError;
+use xcm::{
+	latest::{AssetId, Assets, Location, Weight, Xcm},
+	prelude::{All, DepositAsset, ExchangeAsset, Parachain, Wild, WithdrawAsset, XcmError},
+};
 use xcm_runtime_apis::fees::{
 	runtime_decl_for_xcm_payment_api::XcmPaymentApiV1, Error as XcmPaymentApiError,
 };
@@ -233,4 +236,121 @@ where
 			Some(_) => Ok(()),
 		}
 	})
+}
+
+pub fn exchange_asset_on_asset_hub_works<Runtime, RuntimeCall, RuntimeOrigin, Block>(
+	collator_session_key: CollatorSessionKeys<Runtime>,
+	runtime_para_id: u32,
+	alice: AccountId,
+	native_asset_location: Location,
+	create_pool: bool,
+	give_amount: Balance,
+	want_amount: Balance,
+	should_succeed: bool,
+) where
+	Runtime: XcmPaymentApiV1<Block>
+		+ frame_system::Config<RuntimeOrigin = RuntimeOrigin, AccountId = AccountId>
+		+ pallet_assets::Config
+		+ pallet_balances::Config<Balance = u128>
+		+ pallet_session::Config
+		+ pallet_xcm::Config
+		+ parachain_info::Config
+		+ pallet_collator_selection::Config
+		+ cumulus_pallet_parachain_system::Config,
+	ValidatorIdOf<Runtime>: From<AccountIdOf<Runtime>>,
+	RuntimeOrigin: OriginTrait<AccountId = <Runtime as frame_system::Config>::AccountId>,
+	<<Runtime as frame_system::Config>::Lookup as StaticLookup>::Source:
+		From<<Runtime as frame_system::Config>::AccountId>,
+	Block: BlockT,
+{
+	const UNITS: Balance = 1_000_000_000;
+
+	ExtBuilder::<Runtime>::default()
+		.with_collators(collator_session_key.collators())
+		.with_session_keys(collator_session_key.session_keys())
+		.with_para_id(runtime_para_id.into())
+		.with_tracing()
+		.build()
+		.execute_with(|| {
+			let native_asset_id = AssetId(native_asset_location.clone());
+			let origin = RuntimeOrigin::signed(alice.clone());
+			let asset_location = Location::new(1, [Parachain(2001)]);
+			let asset_id = AssetId(asset_location.clone());
+
+			// Setup initial state
+			assert_ok!(<pallet_balances::Pallet<Runtime> as frame_support::traits::fungible::Mutate<_>>::mint_into(
+				&alice,
+				10_000 * UNITS // Enough balance for most test cases
+			));
+
+			assert_ok!(pallet_assets::Pallet::<Runtime>::force_create(
+				RuntimeOrigin::root(),
+				asset_location.clone().into(),
+				<Runtime as frame_system::Config>::Lookup::unlookup(alice.clone()),
+				true,
+				1
+			));
+
+			// Simulate pool creation if required
+			if create_pool {
+				// In the original, this was `create_pool_with_wnd_on!`
+				// Here we simulate liquidity by minting assets to Alice
+				assert_ok!(pallet_assets::Pallet::<Runtime>::mint(
+					RuntimeOrigin::signed(alice.clone()),
+					asset_location.clone().into(),
+					<Runtime as frame_system::Config>::Lookup::unlookup(alice.clone()),
+					10_000 * UNITS
+				));
+			}
+
+			// Execute the exchange
+			let foreign_balance_before = pallet_assets::Pallet::<Runtime>::balance(asset_location.clone().into(), &alice);
+			let native_balance_before = pallet_balances::Pallet::<Runtime>::total_balance(&alice);
+
+			let give: Assets = (native_asset_id, give_amount).into();
+			let want: Assets = (asset_id, want_amount).into();
+			let xcm = Xcm(vec![
+				WithdrawAsset(give.clone().into()),
+				ExchangeAsset { give: give.into(), want: want.into(), maximal: true },
+				DepositAsset { assets: Wild(All), beneficiary: alice.clone().into() },
+			]);
+
+			let result = pallet_xcm::Pallet::<Runtime>::execute(
+				origin,
+				xcm::VersionedXcm::from(xcm).into(),
+				Weight::MAX
+			);
+
+			// Verify results
+			let foreign_balance_after = pallet_assets::Pallet::<Runtime>::balance(asset_location.into(), &alice);
+			let native_balance_after = pallet_balances::Pallet::<Runtime>::total_balance(&alice);
+
+			if should_succeed {
+				assert_ok!(result);
+				assert!(
+					foreign_balance_after >= foreign_balance_before + want_amount,
+					"Expected foreign balance to increase by at least {want_amount}, got {foreign_balance_after} from {foreign_balance_before}"
+				);
+				assert_eq!(
+					native_balance_after,
+					native_balance_before - give_amount,
+					"Expected native balance to decrease by {give_amount}, got {native_balance_after} from {native_balance_before}"
+				);
+			} else {
+				assert_err_ignore_postinfo!(
+					result,
+					pallet_xcm::Error::<Runtime>::LocalExecutionIncomplete
+				);
+				assert_eq!(
+					foreign_balance_after,
+					foreign_balance_before,
+					"Foreign balance changed unexpectedly: got {foreign_balance_after}, expected {foreign_balance_before}"
+				);
+				assert_eq!(
+					native_balance_after,
+					native_balance_before,
+					"Native balance changed unexpectedly: got {native_balance_after}, expected {native_balance_before}"
+				);
+			}
+		});
 }
