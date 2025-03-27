dda84920c5c2ef9cc3341e8ee52dce2329e1d6de
Andrei Eres <eresav@me.com>
2025-03-27 16:31:31 +0200

    Run empty validation

--- substrate/utils/frame/benchmarking-cli/src/storage/read.rs
+++ substrate/utils/frame/benchmarking-cli/src/storage/read.rs
@@ -137,7 +137,40 @@ impl StorageCmd {
 			}
 		}
 
-		// frame_storage_access_test_runtime
+		let wasm_module = get_wasm_module();
+		let mut instance = wasm_module.new_instance().unwrap();
+		let res = instance.call_export("validate_block", &[]).unwrap();
+
+		println!("res: {:?}", res);
+
 		Ok(record)
 	}
+}
+
+fn get_wasm_module() -> Box<dyn sc_executor_common::wasm_runtime::WasmModule> {
+	let blob = sc_executor_common::runtime_blob::RuntimeBlob::uncompress_if_needed(
+		frame_storage_access_test_runtime::WASM_BINARY
+			.expect("You need to build the WASM binaries to run the benchmark!"),
+	)
+	.unwrap();
+
+	let config = sc_executor_wasmtime::Config {
+		allow_missing_func_imports: true,
+		cache_path: None,
+		semantics: sc_executor_wasmtime::Semantics {
+			heap_alloc_strategy: sc_executor::DEFAULT_HEAP_ALLOC_STRATEGY,
+			instantiation_strategy: sc_executor::WasmtimeInstantiationStrategy::PoolingCopyOnWrite,
+			deterministic_stack_limit: None,
+			canonicalize_nans: false,
+			parallel_compilation: true,
+			wasm_multi_value: false,
+			wasm_bulk_memory: false,
+			wasm_reference_types: false,
+			wasm_simd: false,
+		},
+	};
+	Box::new(
+		sc_executor_wasmtime::create_runtime::<sp_io::SubstrateHostFunctions>(blob, config)
+			.expect("Unable to create wasm module."),
+	)
 }
