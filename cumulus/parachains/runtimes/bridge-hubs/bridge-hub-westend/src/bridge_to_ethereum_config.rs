db34f2e9b45f730e12b1787d96a1d2ce42aebde5
Agustín Rodriguez <agusrodriguez2456@gmail.com>
2025-03-28 11:47:31 -0300

    Update cumulus/parachains/runtimes/bridge-hubs/bridge-hub-westend/src/bridge_to_ethereum_config.rs
    Co-authored-by: Adrian Catangiu <adrian@parity.io>

--- cumulus/parachains/runtimes/bridge-hubs/bridge-hub-westend/src/bridge_to_ethereum_config.rs
+++ cumulus/parachains/runtimes/bridge-hubs/bridge-hub-westend/src/bridge_to_ethereum_config.rs
@@ -197,6 +197,7 @@ impl snowbridge_pallet_outbound_queue_v2::Config for Runtime {
 
 	type DefaultRewardKind = SnowbridgeReward;
 	type RewardPayment = BridgeRelayers;
+	// No consumers of this notifier on Bridge Hub.
 	type OnNewCommitment = ();
 }
 
