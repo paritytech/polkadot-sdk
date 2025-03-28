63476ef2dc1d3ef29ab22c92b1349ef00220e20e
Sebastian Kunert <skunert49@gmail.com>
2025-03-28 09:45:01 +0200

    Revert "Remove broken URL to claim statement."
    This reverts commit 2f1dc11b488590cce21e835ceb0a872e0204dcad.

--- polkadot/runtime/common/src/claims/mod.rs
+++ polkadot/runtime/common/src/claims/mod.rs
@@ -109,10 +109,12 @@ impl StatementKind {
 		match self {
 			StatementKind::Regular =>
 				&b"I hereby agree to the terms of the statement whose SHA-256 multihash is \
-				Qmc1XYqT6S39WNp2UeiRUrZichUWUPpGEThDE6dAb3f6Ny."[..],
+				Qmc1XYqT6S39WNp2UeiRUrZichUWUPpGEThDE6dAb3f6Ny. (This may be found at the URL: \
+				https://statement.polkadot.network/regular.html)"[..],
 			StatementKind::Saft =>
 				&b"I hereby agree to the terms of the statement whose SHA-256 multihash is \
-				QmXEkMahfhHJPzT3RjkXiZVFi77ZeVeuxtAjhojGRNYckz."[..],
+				QmXEkMahfhHJPzT3RjkXiZVFi77ZeVeuxtAjhojGRNYckz. (This may be found at the URL: \
+				https://statement.polkadot.network/saft.html)"[..],
 		}
 	}
 }
