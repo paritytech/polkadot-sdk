7dbbf3693b2b36f9b7faf1145dc1782dc432cf34
Ankan <ankan.anurag@gmail.com>
2025-04-01 12:24:58 +0200

    rename staking next to staking async

---
+++ substrate/frame/staking-async/ahm-test/src/rc/mock.rs
@@ -0,0 +1,307 @@
+use frame::{
+	deps::sp_runtime::testing::UintAuthorityId, testing_prelude::*, traits::fungible::Mutate,
+};
+use pallet_staking_async_ah_client as ah_client;
+use sp_staking::SessionIndex;
+
+use crate::shared;
+
+pub type T = Runtime;
+
+construct_runtime! {
+	pub enum Runtime {
+		System: frame_system,
+		Balances: pallet_balances,
+		Timestamp: pallet_timestamp,
+
+		Session: pallet_session,
+		SessionHistorical: pallet_session::historical,
+		StakingAhClient: pallet_staking_async_ah_client,
+	}
+}
+
+pub fn roll_next() {
+	let now = System::block_number();
+	let next = now + 1;
+
+	System::set_block_number(next);
+	// Timestamp is always the RC block number * 1000
+	Timestamp::set_timestamp(next * 1000);
+
+	Session::on_initialize(next);
+	StakingAhClient::on_initialize(next);
+}
+
+pub fn roll_until_matches(criteria: impl Fn() -> bool, with_ah: bool) {
+	while !criteria() {
+		roll_next();
+		if with_ah {
+			if LocalQueue::get().is_some() {
+				panic!("when local queue is set, you cannot roll ah forward as well!")
+			}
+			shared::in_ah(|| {
+				crate::ah::roll_next();
+			});
+		}
+	}
+}
+
+pub type AccountId = <Runtime as frame_system::Config>::AccountId;
+pub type Balance = <Runtime as pallet_balances::Config>::Balance;
+pub type Hash = <Runtime as frame_system::Config>::Hash;
+pub type BlockNumber = BlockNumberFor<Runtime>;
+
+#[derive_impl(frame_system::config_preludes::TestDefaultConfig)]
+impl frame_system::Config for Runtime {
+	type Block = MockBlock<Self>;
+	type AccountData = pallet_balances::AccountData<u64>;
+}
+
+#[derive_impl(pallet_balances::config_preludes::TestDefaultConfig)]
+impl pallet_balances::Config for Runtime {
+	type AccountStore = System;
+}
+
+impl pallet_timestamp::Config for Runtime {
+	type Moment = u64;
+	type OnTimestampSet = ();
+	type MinimumPeriod = ConstU64<3>;
+	type WeightInfo = ();
+}
+
+pub struct ValidatorIdOf;
+impl Convert<AccountId, Option<AccountId>> for ValidatorIdOf {
+	fn convert(a: AccountId) -> Option<AccountId> {
+		Some(a)
+	}
+}
+
+pub struct OtherSessionHandler;
+impl OneSessionHandler<AccountId> for OtherSessionHandler {
+	type Key = UintAuthorityId;
+
+	fn on_genesis_session<'a, I: 'a>(_: I)
+	where
+		I: Iterator<Item = (&'a AccountId, Self::Key)>,
+		AccountId: 'a,
+	{
+	}
+
+	fn on_new_session<'a, I: 'a>(_: bool, _: I, _: I)
+	where
+		I: Iterator<Item = (&'a AccountId, Self::Key)>,
+		AccountId: 'a,
+	{
+	}
+
+	fn on_disabled(_validator_index: u32) {}
+}
+
+impl BoundToRuntimeAppPublic for OtherSessionHandler {
+	type Public = UintAuthorityId;
+}
+
+frame::deps::sp_runtime::impl_opaque_keys! {
+	pub struct SessionKeys {
+		pub other: OtherSessionHandler,
+	}
+}
+
+parameter_types! {
+	pub static Period: BlockNumber = 30;
+	pub static Offset: BlockNumber = 0;
+}
+
+impl pallet_session::historical::Config for Runtime {
+	type RuntimeEvent = RuntimeEvent;
+	type FullIdentification = ();
+	type FullIdentificationOf = pallet_staking_async::NullIdentity;
+}
+
+impl pallet_session::Config for Runtime {
+	type RuntimeEvent = RuntimeEvent;
+
+	type ValidatorIdOf = ValidatorIdOf;
+	type ValidatorId = AccountId;
+
+	type DisablingStrategy = ();
+
+	type Keys = SessionKeys;
+	type SessionHandler = <SessionKeys as frame::traits::OpaqueKeys>::KeyTypeIdProviders;
+
+	type NextSessionRotation = Self::ShouldEndSession;
+	type ShouldEndSession = pallet_session::PeriodicSessions<Period, Offset>;
+
+	// Should be AH-client
+	type SessionManager = pallet_session::historical::NoteHistoricalRoot<Self, StakingAhClient>;
+
+	type WeightInfo = ();
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum OutgoingMessages {
+	SessionReport(rc_client::SessionReport<AccountId>),
+	OffenceReport(SessionIndex, Vec<rc_client::Offence<AccountId>>),
+}
+
+parameter_types! {
+	pub static MinimumValidatorSetSize: u32 = 4;
+	pub static LocalQueue: Option<Vec<(BlockNumber, OutgoingMessages)>> = None;
+	pub static LocalQueueLastIndex: usize = 0;
+}
+
+impl LocalQueue {
+	pub fn get_since_last_call() -> Vec<(BlockNumber, OutgoingMessages)> {
+		if let Some(all) = Self::get() {
+			let last = LocalQueueLastIndex::get();
+			LocalQueueLastIndex::set(all.len());
+			all.into_iter().skip(last).collect()
+		} else {
+			panic!("Must set local_queue()!")
+		}
+	}
+}
+
+impl ah_client::Config for Runtime {
+	type RuntimeEvent = RuntimeEvent;
+	type AdminOrigin = EnsureRoot<AccountId>;
+	type SendToAssetHub = DeliverToAH;
+	type AssetHubOrigin = EnsureSigned<AccountId>;
+	type UnixTime = Timestamp;
+	type MinimumValidatorSetSize = MinimumValidatorSetSize;
+	type PointsPerBlock = ConstU32<20>;
+	type SessionInterface = Self;
+}
+
+use pallet_staking_async_rc_client as rc_client;
+pub struct DeliverToAH;
+impl ah_client::SendToAssetHub for DeliverToAH {
+	type AccountId = AccountId;
+	fn relay_new_offence(
+		session_index: SessionIndex,
+		offences: Vec<rc_client::Offence<Self::AccountId>>,
+	) {
+		if let Some(mut local_queue) = LocalQueue::get() {
+			local_queue.push((
+				System::block_number(),
+				OutgoingMessages::OffenceReport(session_index, offences),
+			));
+			LocalQueue::set(Some(local_queue));
+		} else {
+			shared::in_ah(|| {
+				let origin = crate::ah::RuntimeOrigin::root();
+				rc_client::Pallet::<crate::ah::Runtime>::relay_new_offence(
+					origin,
+					session_index,
+					offences.clone(),
+				)
+				.unwrap();
+			});
+		}
+	}
+
+	fn relay_session_report(session_report: rc_client::SessionReport<Self::AccountId>) {
+		if let Some(mut local_queue) = LocalQueue::get() {
+			local_queue
+				.push((System::block_number(), OutgoingMessages::SessionReport(session_report)));
+			LocalQueue::set(Some(local_queue));
+		} else {
+			shared::in_ah(|| {
+				let origin = crate::ah::RuntimeOrigin::root();
+				rc_client::Pallet::<crate::ah::Runtime>::relay_session_report(
+					origin,
+					session_report.clone(),
+				)
+				.unwrap();
+			});
+		}
+	}
+}
+
+parameter_types! {
+	pub static SessionEventsIndex: usize = 0;
+	pub static HistoricalEventsIndex: usize = 0;
+	pub static AhClientEventsIndex: usize = 0;
+}
+
+pub fn historical_events_since_last_call() -> Vec<pallet_session::historical::Event<Runtime>> {
+	let all = frame_system::Pallet::<Runtime>::read_events_for_pallet::<
+		pallet_session::historical::Event<Runtime>,
+	>();
+	let seen = HistoricalEventsIndex::get();
+	HistoricalEventsIndex::set(all.len());
+	all.into_iter().skip(seen).collect()
+}
+
+pub fn session_events_since_last_call() -> Vec<pallet_session::Event<Runtime>> {
+	let all =
+		frame_system::Pallet::<Runtime>::read_events_for_pallet::<pallet_session::Event<Runtime>>();
+	let seen = SessionEventsIndex::get();
+	SessionEventsIndex::set(all.len());
+	all.into_iter().skip(seen).collect()
+}
+
+pub fn ah_client_events_since_last_call() -> Vec<ah_client::Event<Runtime>> {
+	let all =
+		frame_system::Pallet::<Runtime>::read_events_for_pallet::<ah_client::Event<Runtime>>();
+	let seen = AhClientEventsIndex::get();
+	AhClientEventsIndex::set(all.len());
+	all.into_iter().skip(seen).collect()
+}
+
+pub struct ExtBuilder {
+	session_keys: Vec<AccountId>,
+	blocked: ah_client::Blocked,
+}
+
+impl Default for ExtBuilder {
+	fn default() -> Self {
+		Self { session_keys: vec![], blocked: ah_client::Blocked::default() }
+	}
+}
+
+impl ExtBuilder {
+	/// Set this if you want to test the rc-runtime locally. This will push outgoing messages to
+	/// `LocalQueue` instead of enacting them on AH.
+	pub fn local_queue(self) -> Self {
+		LocalQueue::set(Some(Default::default()));
+		self
+	}
+
+	/// Set the session keys for the given accounts.
+	pub fn session_keys(mut self, session_keys: Vec<AccountId>) -> Self {
+		self.session_keys = session_keys;
+		self
+	}
+
+	/// Set the blocked status.
+	pub fn blocked(mut self, blocked: ah_client::Blocked) -> Self {
+		self.blocked = blocked;
+		self
+	}
+
+	pub fn build(self) -> TestState {
+		let _ = sp_tracing::try_init_simple();
+		let t = frame_system::GenesisConfig::<T>::default().build_storage().unwrap();
+		let mut state: TestState = t.into();
+		state.execute_with(|| {
+			// so events can be deposited.
+			frame_system::Pallet::<T>::set_block_number(1);
+
+			for v in self.session_keys {
+				// min some funds, create account and ref counts
+				pallet_balances::Pallet::<T>::mint_into(&v, 1).unwrap();
+				pallet_session::Pallet::<T>::set_keys(
+					RuntimeOrigin::signed(v),
+					SessionKeys { other: UintAuthorityId(v) },
+					vec![],
+				)
+				.unwrap();
+			}
+
+			ah_client::IsBlocked::<T>::put(self.blocked);
+		});
+
+		state
+	}
+}
---
+++ substrate/frame/staking-async/runtimes/parachain/build-and-run-zn.sh
@@ -0,0 +1,34 @@
+echo "✅ building chain-spec-builder and pallet-staking-async-rc-runtime and pallet-staking-async-parachain-runtime"
+
+LOG="runtime::multiblock-election=info,runtime::staking=info"
+
+if [ "$1" != "no-compile" ]; then
+    RUST_LOG=${LOG} cargo build --release -p pallet-staking-async-rc-runtime -p pallet-staking-async-parachain-runtime -p staging-chain-spec-builder
+else
+    echo "Skipping compilation because 'no-compile' argument was provided."
+fi
+
+echo "✅ removing any old chain-spec file"
+rm ./parachain.json
+rm ./rc.json
+
+echo "✅ creating ah-next chain specs"
+RUST_LOG=${LOG} ../../../../../target/release/chain-spec-builder \
+    create \
+    -t development \
+    --runtime ../../../../../target/release/wbuild/pallet-staking-async-parachain-runtime/pallet_staking_async_parachain_runtime.compact.compressed.wasm \
+    --relay-chain rococo-local \
+    --para-id 1100 \
+    named-preset development
+mv ./chain_spec.json ./parachain.json
+
+echo "✅ creating westend-next chain specs"
+RUST_LOG=${LOG} ../../../../../target/release/chain-spec-builder \
+    create \
+    -t development \
+    --runtime ../../../../../target/release/wbuild/pallet-staking-async-rc-runtime/fast_runtime_binary.rs.wasm \
+    named-preset local_testnet
+mv ./chain_spec.json ./rc.json
+
+echo "✅ launching ZN"
+zombienet --provider native -l text spawn zombienet-staking-runtimes.toml
