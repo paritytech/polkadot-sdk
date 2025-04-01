33275788fcf07ef46c8d01efe4e4f4cf82f9eb59
Alexander Theißen <alex.theissen@me.com>
2025-04-01 12:41:03 +0200

    Remove bandersnatch-experimental feature from cI scripts

--- substrate/primitives/core/check-features-variants.sh
+++ substrate/primitives/core/check-features-variants.sh
@@ -5,9 +5,8 @@ T=wasm32-unknown-unknown
 
 cargo check --target=$T --release --no-default-features  --features="bls-experimental"
 cargo check --target=$T --release --no-default-features  --features="full_crypto,bls-experimental"
-cargo check --target=$T --release --no-default-features  --features="bandersnatch-experimental"
-cargo check --target=$T --release --no-default-features  --features="full_crypto,serde,bandersnatch-experimental"
+cargo check --target=$T --release --no-default-features  --features="full_crypto,serde"
 cargo check --target=$T --release --no-default-features  --features="full_crypto,serde"
 cargo check --target=$T --release --no-default-features  --features="full_crypto"
 cargo check --target=$T --release --no-default-features  --features="serde"
-cargo check --target=$T --release --no-default-features  
+cargo check --target=$T --release --no-default-features
