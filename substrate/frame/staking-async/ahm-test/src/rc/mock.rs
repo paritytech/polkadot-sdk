538853058a675cb0cde21b51554b97594c3dd46c
Ankan <ankan.anurag@gmail.com>
2025-04-04 12:38:00 +0200

    remove old name

--- substrate/frame/staking-async/ahm-test/src/rc/mock.rs
+++ substrate/frame/staking-async/ahm-test/src/rc/mock.rs
@@ -171,7 +171,7 @@ impl ah_client::Config for Runtime {
 	type MinimumValidatorSetSize = MinimumValidatorSetSize;
 	type PointsPerBlock = ConstU32<20>;
 	type SessionInterface = Self;
-	type StakingClassic = ();
+	// TODO type Fallback = Staking;
 }
 
 use pallet_staking_async_rc_client as rc_client;
