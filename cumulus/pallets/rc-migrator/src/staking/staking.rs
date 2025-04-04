9ceb69dd922e28f558078bf2de1835e982c98761
Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
2025-04-04 22:23:11 +0200

    staking
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>

--- cumulus/pallets/rc-migrator/src/staking/staking.rs
+++ cumulus/pallets/rc-migrator/src/staking/staking.rs
@@ -635,7 +635,7 @@ impl<T: Config> PalletMigration for StakingMigrator<T> {
 		if !messages.is_empty() {
 			Pallet::<T>::send_chunked_xcm(messages, |messages| {
 				types::AhMigratorCall::<T>::ReceiveStakingMessages { messages }
-			})?;
+			}, |_len| Weight::from_all(1))?;
 		}
 
 		if inner_key == StakingStage::Finished {
