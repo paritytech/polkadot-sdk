7d2a97d127f549a5a1ff84f5265185de11644071
Iulian Barbu <iulian.barbu@parity.io>
2025-04-07 12:34:20 +0300

    fix more compilation issues
    Signed-off-by: Iulian Barbu <iulian.barbu@parity.io>

--- substrate/frame/nomination-pools/src/mock.rs
+++ substrate/frame/nomination-pools/src/mock.rs
@@ -737,7 +737,7 @@ mod test {
 	use super::*;
 	#[test]
 	fn u256_to_balance_convert_works() {
-		assert_eq!(U256ToBalance::convert(0u32.into()), Zero::zero());
+		assert_eq!(U256ToBalance::convert(0u32.into()), u128::zero());
 		assert_eq!(U256ToBalance::convert(Balance::max_value().into()), Balance::max_value())
 	}
 
