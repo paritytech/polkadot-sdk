672c57c25489377f149e5f4d39c4c7a904870bfb
s0me0ne-unkn0wn <48632512+s0me0ne-unkn0wn@users.noreply.github.com>
2025-04-08 11:26:37 +0200

    Update substrate/client/transaction-pool/src/graph/validated_pool.rs
    Co-authored-by: ordian <write@reusable.software>

--- substrate/client/transaction-pool/src/graph/validated_pool.rs
+++ substrate/client/transaction-pool/src/graph/validated_pool.rs
@@ -702,7 +702,7 @@ impl<B: ChainApi, L: EventHandler<B>> ValidatedPool<B, L> {
 	/// Consumers of this stream should use the `ready` method to actually get the
 	/// pending transactions in the right order.
 	pub fn import_notification_stream(&self) -> EventStream<ExtrinsicHash<B>> {
-		const CHANNEL_BUFFER_SIZE: usize = 8192;
+		const CHANNEL_BUFFER_SIZE: usize = 1024;
 
 		let (sink, stream) = channel(CHANNEL_BUFFER_SIZE);
 		self.import_notification_sinks.lock().push(sink);
