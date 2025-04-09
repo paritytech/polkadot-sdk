809ab9d18a34e9bac18e352ef07a55ca67b4ed3e
Ankan <ankan.anurag@gmail.com>
2025-04-09 23:43:55 +0200

    fix root offences

--- substrate/frame/root-offences/src/mock.rs
+++ substrate/frame/root-offences/src/mock.rs
@@ -148,8 +148,8 @@ impl pallet_staking::Config for Test {
 
 impl pallet_session::historical::Config for Test {
 	type RuntimeEvent = RuntimeEvent;
-	type FullIdentification = pallet_staking::Existence;
-	type FullIdentificationOf = pallet_staking::ExistenceOf<Test>;
+	type FullIdentification = pallet_staking::ExistenceOrLegacyExposure<AccountId, Balance>;
+	type FullIdentificationOf = pallet_staking::ExistenceOrLegacyExposureOf<Test>;
 }
 
 sp_runtime::impl_opaque_keys! {
