2c378d7452abb530249b67347b527227e6ffdb21
girazoki <gorka.irazoki@gmail.com>
2025-04-11 13:10:11 +0200

    add alias origin to bridgehub westend

--- cumulus/parachains/runtimes/bridge-hubs/bridge-hub-westend/src/lib.rs
+++ cumulus/parachains/runtimes/bridge-hubs/bridge-hub-westend/src/lib.rs
@@ -1264,7 +1264,11 @@ impl_runtime_apis! {
 				}
 
 				fn alias_origin() -> Result<(Location, Location), BenchmarkError> {
-					Err(BenchmarkError::Skip)
+					// Any location can alias to an internal location.
+					// Here parachain 1000 aliases to an internal account.
+					let origin = Location::new(1, [Parachain(1000)]);
+					let target = Location::new(1, [Parachain(1000), AccountId32 { id: [128u8; 32], network: None }]);
+					Ok((origin, target))
 				}
 			}
 
