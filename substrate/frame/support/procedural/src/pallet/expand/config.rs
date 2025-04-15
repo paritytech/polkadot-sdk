2c183bff6496c748e6b96aae6a38792a48c785b6
Bastian Köcher <git@kchr.de>
2025-04-15 23:44:38 +0200

    Update substrate/frame/support/procedural/src/pallet/expand/config.rs

--- substrate/frame/support/procedural/src/pallet/expand/config.rs
+++ substrate/frame/support/procedural/src/pallet/expand/config.rs
@@ -66,7 +66,7 @@ Consequently, a runtime that wants to include this pallet must implement this tr
 			let supertrait_with_event_bound = syn::parse2::<syn::TypeParamBound>(
 				quote! { #frame_system::Config<RuntimeEvent: From<Event<#event_use_gen>>> },
 			)
-			.expect("Parsing super trait doesn't fail; qedt");
+			.expect("Parsing super trait doesn't fail; qed");
 
 			config_item.supertraits.push(supertrait_with_event_bound.into());
 		}
