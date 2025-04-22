8fe3c9df9ac700c3cc656ad613ae307e04ce4ad2
Iulian Barbu <iulian.barbu@parity.io>
2025-04-22 13:55:02 +0000

    Revert "Update substrate/client/transaction-pool/src/graph/pool.rs"
    This reverts commit f9f68a43c70085ca1aaef0f5891e5a15afe10777.

--- substrate/client/transaction-pool/src/graph/pool.rs
+++ substrate/client/transaction-pool/src/graph/pool.rs
@@ -374,7 +374,7 @@ impl<B: ChainApi, L: EventHandler<B>> Pool<B, L> {
 		}
 
 		let known_txs_count = known_provides_tags.map(|inner| inner.len()).unwrap_or(0);
-		log::debug!(target: LOG_TARGET, validated_counter, known_txs_count, unknown_txs_count, reused_txs_count, duration = now.elapsed(),"prune");
+		log::debug!(target: LOG_TARGET, validated_counter, known_txs_count, unknown_txs_count, reused_txs_count, duration= now.elapsed(),"prune");
 
 		self.prune_tags(at, future_tags, in_pool_hashes).await
 	}
