58f3931228015fb3d381f780cfdd7d552b0f1375
Branislav Kontur <bkontur@gmail.com>
2025-04-24 01:38:24 +0200

    Add proof root sync (`pallet-bridge-proof-root-sync`) as an ring buffer with on_idle callback

---
+++ bridges/modules/proof-root-sync/src/impls.rs
@@ -0,0 +1,42 @@
+// Copyright (C) Parity Technologies (UK) Ltd.
+// SPDX-License-Identifier: Apache-2.0
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//! Various predefined implementations for the pallet.
+
+use crate::{Config, Pallet};
+use bp_parachains::OnNewHead;
+use bp_polkadot_core::parachains::{ParaHash, ParaHead, ParaId};
+use bp_runtime::Parachain;
+use frame_support::traits::Get;
+use frame_support::weights::Weight;
+
+/// An adapter `OnNewHead` implementation that listens for parachain head updates and schedules them for syncing.
+pub struct SyncParaHeadersFor<T, I, C>(core::marker::PhantomData<(T, I, C)>);
+impl<T: Config<I, Key = ParaId, Value = ParaHead>, I: 'static, C: Parachain<Hash = ParaHash>>
+	OnNewHead for SyncParaHeadersFor<T, I, C>
+{
+	fn on_new_head(id: ParaId, head: &ParaHead) -> Weight {
+		// Filter by para ID.
+		if C::PARACHAIN_ID != id.0 {
+			return Weight::zero();
+		}
+
+		// Schedule for syncing.
+		Pallet::<T, I>::schedule_for_sync(id, head.clone());
+
+		// Schedule does one read and one write.
+		T::DbWeight::get().reads_writes(1, 1)
+	}
+}
