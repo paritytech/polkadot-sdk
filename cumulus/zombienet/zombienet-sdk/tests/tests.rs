86a9fc9a800d1a870f70ec826e322367f4e4fa74
Lukasz Rubaszewski <117115317+lrubasze@users.noreply.github.com>
2025-04-24 19:15:29 +0200

    Reorganize `zombienet-sdk` tests

--- cumulus/zombienet/zombienet-sdk/tests/tests.rs
+++ cumulus/zombienet/zombienet-sdk/tests/tests.rs
@@ -1,4 +1,5 @@
 // Copyright (C) Parity Technologies (UK) Ltd.
 // SPDX-License-Identifier: Apache-2.0
 
-mod sync_blocks_from_tip_without_connected_collator;
+#[cfg(feature = "zombie-ci")]
+mod zombie_ci;
---
+++ cumulus/zombienet/zombienet-sdk/tests/zombie_ci/mod.rs
@@ -0,0 +1,4 @@
+mod elastic_scaling;
+mod pov_recovery;
+mod runtime_upgrade;
+mod sync_blocks;
