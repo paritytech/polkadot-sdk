281e3abe23e5e4f47ad551165c36fe5e55dd57b2
Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
2025-04-28 21:16:46 +0200

    derive partial eq
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>

--- substrate/frame/staking-async/src/slashing.rs
+++ substrate/frame/staking-async/src/slashing.rs
@@ -91,7 +91,7 @@ impl SlashingSpan {
 }
 
 /// An encoding of all of a nominator's slashing spans.
-#[derive(Encode, Decode, RuntimeDebug, TypeInfo)]
+#[derive(Encode, Decode, RuntimeDebug, TypeInfo, PartialEq, Eq, Clone, DecodeWithMemTracking)]
 pub struct SlashingSpans {
 	// the index of the current slashing span of the nominator. different for
 	// every stash, resets when the account hits free balance 0.
