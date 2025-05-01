01278f3b9fe7edbc099aa206eef5fbdb3e3ab7e0
Kian Paimani <5588131+kianenigma@users.noreply.github.com>
2025-05-01 11:18:53 +0100

    fix

--- substrate/frame/election-provider-multi-block/src/signed/mod.rs
+++ substrate/frame/election-provider-multi-block/src/signed/mod.rs
@@ -893,7 +893,7 @@ pub mod pallet {
 		/// This allows an offchain application to know what [`Config::DepositPerPage`] and
 		/// [`Config::DepositBase`] are doing under the hood.
 		pub fn deposit_for(page: u32) -> BalanceOf<T> {
-			Self::deposit_for(p as usize)
+			Self::deposit_for(pages as usize)
 		}
 	}
 
