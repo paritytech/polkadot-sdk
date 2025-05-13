293f1fe0954eedb326af4c8044c6f4e0bd6cb4fa
James Wilson <james@jsdw.me>
2025-05-13 17:14:42 +0100

    Prevent V16 metadata from being fetched by the benchmarking tool until it's supported

--- substrate/client/runtime-utilities/src/lib.rs
+++ substrate/client/runtime-utilities/src/lib.rs
@@ -48,7 +48,10 @@ pub fn fetch_latest_metadata_from_code_blob<HF: HostFunctions>(
 			let supported_versions = Vec::<u32>::decode(&mut supported_versions.as_slice())?;
 			let latest_stable = supported_versions
 				.into_iter()
-				.filter(|v| *v != u32::MAX)
+				// version >=16 metadata is not supported by the version of Subxt in use here yet,
+				// so restrict to fetching v15 metadata until Subxt is updated to 0.42, which depends
+				// on frame-metadata 23 and can decode the newly stable V16 metadata.
+				.filter(|v| *v != u32::MAX && *v < 16) 
 				.max()
 				.ok_or(Error::StableMetadataVersionNotFound)?;
 
