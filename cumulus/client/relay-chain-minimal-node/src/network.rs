ad15c40f10fdd43ac4e09d3135f14c968181372b
Alexandru Vasile <alexandru.vasile@parity.io>
2025-05-13 14:39:39 +0000

    cumulus: Allow requests to happen in the background
    Signed-off-by: Alexandru Vasile <alexandru.vasile@parity.io>

--- cumulus/client/relay-chain-minimal-node/src/network.rs
+++ cumulus/client/relay-chain-minimal-node/src/network.rs
@@ -44,7 +44,7 @@ pub(crate) fn build_collator_network<Network: NetworkBackend<Block, Hash>>(
 	notification_metrics: NotificationMetrics,
 ) -> Result<(Arc<dyn NetworkService>, Arc<dyn sp_consensus::SyncOracle + Send + Sync>), Error> {
 	let protocol_id = config.protocol_id();
-	let (block_announce_config, _notification_service) = get_block_announce_proto_config::<Network>(
+	let (block_announce_config, notification_service) = get_block_announce_proto_config::<Network>(
 		protocol_id.clone(),
 		&None,
 		Roles::from(&config.role),
@@ -90,7 +90,12 @@ pub(crate) fn build_collator_network<Network: NetworkBackend<Block, Hash>>(
 	// issue, and ideally we would like to fix the network future to take as little time as
 	// possible, but we also take the extra harm-prevention measure to execute the networking
 	// future using `spawn_blocking`.
-	spawn_handle.spawn_blocking("network-worker", Some("networking"), network_worker.run());
+	spawn_handle.spawn_blocking("network-worker", Some("networking"), async move {
+		// The notification service must be kept alive to allow litep2p to handle
+		// requests under the hood.
+		let _notification_service = notification_service;
+		network_worker.run().await;
+	});
 
 	Ok((network_service, Arc::new(SyncOracle {})))
 }
