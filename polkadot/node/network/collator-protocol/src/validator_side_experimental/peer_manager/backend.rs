d908352cda11bfd06fefe53d83b085bfc6d33692
Alin Dima <alin@parity.io>
2025-05-14 14:55:11 +0300

    collator-protocol-revamp: add PeerManager (#8191)
    Implements https://github.com/paritytech/polkadot-sdk/issues/8181
    Add peer manager for experimental collator protocol,. The code is
    feature-gated by a compile-time feature (which is why I added the silent
    label and no prdoc).
    
    PeerManager consists of two components:
    - `ConnectedPeers`, which keeps track of currently connected peers as
    well as:
        - their reputations for the currently scheduled paras.
        - their protocol versions and the declared paraid.
        - making the decision of whether or not to keep a new connection
    - the reputation DB (`Backend` trait implementation), which is left as a
    dummy impl, to be implemented in a separate PR:
    https://github.com/paritytech/polkadot-sdk/issues/7751
    
    As the wholistic implementation proceeds, the peer manager will likely
    suffer some changes that will come in future PRs.
    The current state is the best informed guess of all the functionality it
    requires, so that the whole implementation is not a big bang and can be
    reviewed piece by piece.
    
    TODO:
    - [x] add unit tests
    - [x] update after merging
    https://github.com/paritytech/polkadot-sdk/pull/7955

---
+++ polkadot/node/network/collator-protocol/src/validator_side_experimental/peer_manager/backend.rs
@@ -0,0 +1,45 @@
+// Copyright (C) Parity Technologies (UK) Ltd.
+// This file is part of Polkadot.
+
+// Polkadot is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Polkadot is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Polkadot.  If not, see <http://www.gnu.org/licenses/>.
+
+use crate::validator_side_experimental::{common::Score, peer_manager::ReputationUpdate};
+use async_trait::async_trait;
+use polkadot_node_network_protocol::PeerId;
+use polkadot_primitives::{BlockNumber, Id as ParaId};
+use std::collections::{BTreeMap, BTreeSet, HashMap};
+
+/// Trait describing the interface of the reputation database.
+#[async_trait]
+pub trait Backend {
+	/// Instantiate a new backend.
+	async fn new() -> Self;
+	/// Return the latest finalized block for which the backend processed bumps.
+	async fn processed_finalized_block_number(&self) -> Option<BlockNumber>;
+	/// Get the peer's stored reputation for this paraid, if any.
+	async fn query(&self, peer_id: &PeerId, para_id: &ParaId) -> Option<Score>;
+	/// Slash the peer's reputation for this paraid, with the given value.
+	async fn slash(&mut self, peer_id: &PeerId, para_id: &ParaId, value: Score);
+	/// Prune all data for paraids that are no longer in this registered set.
+	async fn prune_paras(&mut self, registered_paras: BTreeSet<ParaId>);
+	/// Process the reputation bumps, returning all the reputation changes that were done in
+	/// consequence. This is needed because a reputation bump for a para also means a reputation
+	/// decay for the other collators of that para (if the `decay_value` param is present).
+	async fn process_bumps(
+		&mut self,
+		leaf_number: BlockNumber,
+		bumps: BTreeMap<ParaId, HashMap<PeerId, Score>>,
+		decay_value: Option<Score>,
+	) -> Vec<ReputationUpdate>;
+}
