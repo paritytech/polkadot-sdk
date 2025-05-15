68cb29ca31863784f0908ebaa0ae4595e9ff5bde
Dónal Murray <donal.murray@parity.io>
2025-05-15 11:15:34 +0100

    Update cumulus/pallets/parachain-system/src/lib.rs
    Co-authored-by: Branislav Kontur <bkontur@gmail.com>

--- cumulus/pallets/parachain-system/src/lib.rs
+++ cumulus/pallets/parachain-system/src/lib.rs
@@ -1768,7 +1768,7 @@ impl<T: Config> BlockNumberProvider for RelaychainDataProvider<T> {
 			.unwrap_or_else(|| Pallet::<T>::last_relay_block_number())
 	}
 
-	#[cfg(any(feature = "runtime-benchmarks", test))]
+	#[cfg(any(feature = "std", feature = "runtime-benchmarks", test))]
 	fn set_block_number(block: Self::BlockNumber) {
 		let mut validation_data = ValidationData::<T>::get().unwrap_or_else(||
 			// PersistedValidationData does not impl default in non-std
