06432fbe91f30bc0796021ae2b8bebd24c71c224
Lukasz Rubaszewski <117115317+lrubasze@users.noreply.github.com>
2025-05-15 12:26:23 +0200

    elastic-slot-based-authoring: migrated from zombienet

--- cumulus/zombienet/zombienet-sdk/tests/zombie_ci/mod.rs
+++ cumulus/zombienet/zombienet-sdk/tests/zombie_ci/mod.rs
@@ -4,6 +4,7 @@
 mod bootnodes;
 mod elastic_scaling_multiple_blocks_per_slot;
 mod elastic_scaling_pov_recovery;
+mod elastic_scaling_slot_based_authoring;
 mod full_node_catching_up;
 mod pov_recovery;
 mod rpc_collator_build_blocks;
