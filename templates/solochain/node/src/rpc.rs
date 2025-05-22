7b9e4fef18b5d65b1e3e9b53345c62ce0b77b1f7
Seemant Aggarwal <seemant.aggarwal@parity.io>
2025-05-22 14:08:45 +0530

    further improvements. still WIP

--- templates/solochain/node/src/rpc.rs
+++ templates/solochain/node/src/rpc.rs
@@ -9,7 +9,7 @@ use std::sync::Arc;
 
 use jsonrpsee::RpcModule;
 use sc_transaction_pool_api::TransactionPool;
-use solochain_template_runtime::{opaque::Block, AccountId, Balance, Nonce};
+use solochain_template_runtime::interface::{AccountId, Balance, Nonce, OpaqueBlock as Block};
 use sp_api::ProvideRuntimeApi;
 use sp_block_builder::BlockBuilder;
 use sp_blockchain::{Error as BlockChainError, HeaderBackend, HeaderMetadata};
