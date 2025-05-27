08d9a0ffe653cb909a0cef9847808e91b21e0374
Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
2025-05-27 21:32:14 +0200

    Derp, bump spec
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>

--- cumulus/parachains/runtimes/assets/asset-hub-westend/src/lib.rs
+++ cumulus/parachains/runtimes/assets/asset-hub-westend/src/lib.rs
@@ -150,7 +150,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: alloc::borrow::Cow::Borrowed("westmint"),
 	impl_name: alloc::borrow::Cow::Borrowed("westmint"),
 	authoring_version: 1,
-	spec_version: 1_018_007,
+	spec_version: 1_018_008,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 16,
