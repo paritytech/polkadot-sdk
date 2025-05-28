a28ba8f4c99555804f1f0b108d61652e135592ec
Andrei Eres <eresav@me.com>
2025-05-28 17:19:59 +0200

    Make it work again

--- polkadot/zombienet-sdk-tests/tests/parachains/weights.rs
+++ polkadot/zombienet-sdk-tests/tests/parachains/weights.rs
@@ -1,26 +1,34 @@
 // Copyright (C) Parity Technologies (UK) Ltd.
 // SPDX-License-Identifier: Apache-2.0
 
-use crate::helpers::asset_hub_westend::{
-	self,
-	runtime_types::{
-		pallet_revive::primitives::{Code, StorageDeposit},
-		sp_weights::weight_v2::Weight,
-	},
+#[zombienet_sdk::subxt::subxt(
+	runtime_metadata_path = "metadata-files/asset-hub-westend-local.scale"
+)]
+mod ahw {}
+
+use ahw::runtime_types::{
+	pallet_revive::primitives::{Code, StorageDeposit},
+	sp_weights::weight_v2::Weight,
 };
 use anyhow::anyhow;
+use asset_hub_westend_runtime::Runtime as AHWRuntime;
 use futures::{stream::FuturesUnordered, StreamExt};
 use pallet_revive::AddressMapper;
 use sp_core::{H160, H256};
 use std::str::FromStr;
-use subxt::{tx::SubmittableExtrinsic, OnlineClient, PolkadotConfig};
-use subxt_signer::{
-	sr25519::{dev, Keypair},
-	SecretUri,
+use zombienet_sdk::{
+	subxt::{
+		self, config::polkadot::PolkadotExtrinsicParamsBuilder, tx::SubmittableExtrinsic,
+		OnlineClient, PolkadotConfig,
+	},
+	subxt_signer::{
+		sr25519::{dev, Keypair},
+		SecretUri,
+	},
+	LocalFileSystem, Network, NetworkConfigBuilder,
 };
-use zombienet_sdk::{LocalFileSystem, Network, NetworkConfigBuilder};
 
-const KEYS_COUNT: usize = 3000;
+const KEYS_COUNT: usize = 100;
 const CHUNK_SIZE: usize = 500;
 const CALL_CHUNK_SIZE: usize = 1000;
 
@@ -34,7 +42,7 @@ async fn weights_test() -> Result<(), anyhow::Error> {
 	let collator = network.get_node("collator")?;
 	let para_client: OnlineClient<PolkadotConfig> = collator.wait_client().await?;
 	let mut call_clients = vec![];
-	for _ in 0..(KEYS_COUNT / CALL_CHUNK_SIZE) {
+	for _ in 0..(KEYS_COUNT / CALL_CHUNK_SIZE + 1) {
 		let call_client: OnlineClient<PolkadotConfig> = collator.wait_client().await?;
 		call_clients.push(call_client);
 	}
@@ -42,13 +50,20 @@ async fn weights_test() -> Result<(), anyhow::Error> {
 
 	let alice = dev::alice();
 	let keys = create_keys(KEYS_COUNT);
-	setup_accounts(&para_client, &alice, &keys).await?;
+	let mut nonce = 0;
+	let mut nonce = || {
+		let current_nonce = nonce;
+		nonce += 1;
+		current_nonce
+	};
+
+	setup_accounts(&para_client, &alice, &keys, nonce()).await?;
 	log::info!("Accounts ready");
 
 	let contract_address = instantiate_contract(&para_client, &alice).await?;
 	log::info!("Contract instantiated: {:?}", contract_address);
 
-	call_contract(&para_client, call_clients, contract_address, &alice, &keys).await?;
+	call_contract(&para_client, call_clients, contract_address, &alice, &keys, nonce()).await?;
 	log::info!("Test finished, sleeping for 6000 seconds to allow for manual inspection");
 	tokio::time::sleep(std::time::Duration::from_secs(6000)).await;
 
@@ -76,7 +91,9 @@ async fn setup_network() -> Result<Network<LocalFileSystem>, anyhow::Error> {
 				)
 				.with_chain("asset-hub-westend-local")
 				.with_collator(|n| {
-					n.with_name("collator").validator(true).with_args(vec![("-linfo").into()])
+					n.with_name("collator")
+						.validator(true)
+						.with_args(vec![("--warm-up-trie-cache").into(), ("-linfo").into()])
 				})
 		})
 		.build()
@@ -99,139 +116,77 @@ fn create_keys(n: usize) -> Vec<Keypair> {
 		.collect()
 }
 
+fn tx_params<T: subxt::Config>(
+	nonce: u64,
+) -> <subxt::config::DefaultExtrinsicParams<T> as subxt::config::ExtrinsicParams<T>>::Params {
+	PolkadotExtrinsicParamsBuilder::<T>::new().nonce(nonce).build()
+}
+
 async fn setup_accounts(
-	para_client: &OnlineClient<PolkadotConfig>,
-	alice: &Keypair,
+	client: &OnlineClient<PolkadotConfig>,
+	caller: &Keypair,
 	keys: &[Keypair],
+	nonce: u64,
 ) -> Result<(), anyhow::Error> {
-	let alice_account_id = alice.public_key().to_account_id();
+	let caller_account_id = caller.public_key().to_account_id();
+	let caller_nonce = client.tx().account_nonce(&caller_account_id).await?;
 	for chunk in keys.chunks(CHUNK_SIZE) {
-		let alice_nonce = para_client.tx().account_nonce(&alice_account_id).await?;
 		let txs = chunk
 			.iter()
 			.enumerate()
 			.map(|(i, key)| {
-				let params = subxt::config::polkadot::PolkadotExtrinsicParamsBuilder::new()
-					.nonce(alice_nonce + i as u64)
-					.build();
-				para_client.tx().create_signed_offline(
-					&asset_hub_westend::tx()
-						.balances()
-						.transfer_keep_alive(key.public_key().into(), 1000000000000),
-					alice,
-					params,
-				)
+				let key = key.public_key().into();
+				let call = &ahw::tx().balances().transfer_keep_alive(key, 1000000000000);
+				let params = tx_params(caller_nonce + i as u64);
+				client.tx().create_signed_offline(call, caller, params)
 			})
 			.collect::<Result<Vec<_>, _>>()?;
 		submit_txs(txs).await?;
 	}
 
-	let alice_nonce = para_client.tx().account_nonce(&alice_account_id).await?;
-	let params = subxt::config::polkadot::PolkadotExtrinsicParamsBuilder::new()
-		.nonce(alice_nonce)
-		.build();
-	para_client
-		.tx()
-		.sign_and_submit(&asset_hub_westend::tx().revive().map_account(), alice, params)
-		.await?;
-
+	let map_call = &ahw::tx().revive().map_account();
+	let mut is_caller_mapped = false;
 	for chunk in keys.chunks(CHUNK_SIZE) {
-		let txs = chunk
+		let mappings = chunk
 			.iter()
-			.map(|key| {
-				let params =
-					subxt::config::polkadot::PolkadotExtrinsicParamsBuilder::new().nonce(0).build();
-				para_client.tx().create_signed_offline(
-					&asset_hub_westend::tx().revive().map_account(),
-					key,
-					params,
-				)
+			.map(|key| (key, nonce))
+			.chain(if is_caller_mapped {
+				None
+			} else {
+				is_caller_mapped = true;
+				Some((caller, caller_nonce + keys.len() as u64))
 			})
+			.map(|(k, n)| client.tx().create_signed_offline(map_call, k, tx_params(n)))
 			.collect::<Result<Vec<_>, _>>()?;
-		submit_txs(txs).await?;
+		submit_txs(mappings).await?;
 	}
 
 	Ok(())
 }
 
 async fn instantiate_contract(
-	para_client: &OnlineClient<PolkadotConfig>,
-	alice: &Keypair,
+	client: &OnlineClient<PolkadotConfig>,
+	caller: &Keypair,
 ) -> Result<H160, anyhow::Error> {
 	let code_path = std::env::current_dir().unwrap().join("tests/parachains/contract.polkavm");
 	let code = std::fs::read(code_path)?;
-	let alice_account_id = alice.public_key().to_account_id();
-	let alice_h160 =
-		<asset_hub_westend_runtime::Runtime as pallet_revive::Config>::AddressMapper::to_address(
-			&alice.public_key().0.into(),
-		);
+	let (ref_time, proof_size, deposit) = instantiate_params(client, code.clone(), caller).await?;
 
-	let upload_dry_run = para_client
-		.runtime_api()
-		.at_latest()
-		.await?
-		.call(asset_hub_westend::apis().revive_api().upload_code(
-			alice_account_id.clone(),
-			code.clone(),
-			None,
-		))
-		.await?
-		.unwrap();
-	let code_hash = upload_dry_run.code_hash;
-	let deposit = upload_dry_run.deposit;
-	para_client
-		.tx()
-		.sign_and_submit_then_watch_default(
-			&asset_hub_westend::tx().revive().upload_code(code.clone(), deposit),
-			alice,
-		)
-		.await?
-		.wait_for_finalized_success()
-		.await?;
-
-	let instantiate_dry_run = para_client
-		.runtime_api()
-		.at_latest()
-		.await?
-		.call(asset_hub_westend::apis().revive_api().instantiate(
-			alice_account_id.clone(),
-			0,
-			None,
-			None,
-			Code::Existing(code_hash),
-			vec![],
-			None,
-		))
-		.await?;
 	// We need a nonce before instantiating the contract
-	let alice_revive_nonce = para_client
+	let account_id = caller.public_key().0.into();
+	let caller_h160 = <AHWRuntime as pallet_revive::Config>::AddressMapper::to_address(&account_id);
+	let caller_revive_nonce = client
 		.runtime_api()
 		.at_latest()
 		.await?
-		.call(asset_hub_westend::apis().revive_api().nonce(alice_h160))
+		.call(ahw::apis().revive_api().nonce(caller_h160))
 		.await?;
-	let contract_address = pallet_revive::create1(&alice_h160, alice_revive_nonce.into());
-	let weight = Weight {
-		ref_time: instantiate_dry_run.gas_required.ref_time,
-		proof_size: instantiate_dry_run.gas_required.proof_size,
-	};
-	let deposit = match instantiate_dry_run.storage_deposit {
-		StorageDeposit::Charge(c) => c,
-		StorageDeposit::Refund(_) => 0,
-	};
-	para_client
+	let contract_address = pallet_revive::create1(&caller_h160, caller_revive_nonce.into());
+	let weight = Weight { ref_time, proof_size };
+	let call = &ahw::tx().revive().instantiate_with_code(0, weight, deposit, code, vec![], None);
+	client
 		.tx()
-		.sign_and_submit_then_watch_default(
-			&asset_hub_westend::tx().revive().instantiate(
-				0,
-				weight,
-				deposit,
-				code_hash,
-				vec![],
-				None,
-			),
-			alice,
-		)
+		.sign_and_submit_then_watch_default(call, caller)
 		.await?
 		.wait_for_finalized_success()
 		.await?;
@@ -239,68 +194,74 @@ async fn instantiate_contract(
 	Ok(contract_address)
 }
 
+async fn instantiate_params(
+	client: &OnlineClient<PolkadotConfig>,
+	code: Vec<u8>,
+	caller: &Keypair,
+) -> Result<(u64, u64, u128), anyhow::Error> {
+	let account_id = caller.public_key().to_account_id();
+	let code = Code::Upload(code);
+	let call = ahw::apis()
+		.revive_api()
+		.instantiate(account_id, 0, None, None, code, vec![], None);
+	let dry_run = client.runtime_api().at_latest().await?.call(call).await?;
+	let deposit = match dry_run.storage_deposit {
+		StorageDeposit::Charge(c) => c,
+		StorageDeposit::Refund(_) => 0,
+	};
+
+	Ok((dry_run.gas_required.ref_time, dry_run.gas_required.proof_size, deposit))
+}
+
+async fn call_params(
+	client: &OnlineClient<PolkadotConfig>,
+	contract: H160,
+	payload: Vec<u8>,
+	caller: &Keypair,
+) -> Result<(u64, u64, u128), anyhow::Error> {
+	let account_id = caller.public_key().to_account_id();
+	let call = ahw::apis().revive_api().call(account_id, contract, 0, None, None, payload);
+	let dry_run = client.runtime_api().at_latest().await?.call(call).await?;
+	let deposit = match dry_run.storage_deposit {
+		StorageDeposit::Charge(c) => c,
+		StorageDeposit::Refund(_) => 0,
+	};
+
+	Ok((dry_run.gas_required.ref_time, dry_run.gas_required.proof_size, deposit))
+}
+
 async fn call_contract(
-	para_client: &OnlineClient<PolkadotConfig>,
+	client: &OnlineClient<PolkadotConfig>,
 	mut call_clients: Vec<OnlineClient<PolkadotConfig>>,
-	contract_address: H160,
-	alice: &Keypair,
+	contract: H160,
+	caller: &Keypair,
 	keys: &[Keypair],
+	nonce: u64,
 ) -> Result<(), anyhow::Error> {
 	let mint_100 = sp_core::hex2array!(
 		"a0712d680000000000000000000000000000000000000000000000000000000000000064"
 	)
 	.to_vec();
-	let alice_account_id = alice.public_key().to_account_id();
-	let call_dry_run = para_client
-		.runtime_api()
-		.at_latest()
-		.await?
-		.call(asset_hub_westend::apis().revive_api().call(
-			alice_account_id.clone(),
-			contract_address,
-			0,
-			None,
-			None,
-			mint_100.clone(),
-		))
-		.await?;
-	let deposit = match call_dry_run.storage_deposit {
-		StorageDeposit::Charge(c) => c,
-		StorageDeposit::Refund(_) => 0,
-	};
+	let (ref_time, proof_size, deposit) =
+		call_params(client, contract, mint_100.clone(), caller).await?;
+	let weight = Weight { ref_time, proof_size };
+	let call = &ahw::tx().revive().call(contract, 0, weight, deposit, mint_100.clone());
+
 	let mut txs = vec![];
 	for chunk in keys.chunks(CALL_CHUNK_SIZE) {
 		let para_client = call_clients.pop().unwrap();
 		let txs_chunk = chunk
 			.iter()
-			.map(|key| {
-				let weight = Weight {
-					ref_time: call_dry_run.gas_required.ref_time,
-					proof_size: call_dry_run.gas_required.proof_size,
-				};
-				let params =
-					subxt::config::polkadot::PolkadotExtrinsicParamsBuilder::new().nonce(1).build();
-				para_client.tx().create_signed_offline(
-					&asset_hub_westend::tx().revive().call(
-						contract_address,
-						0,
-						weight,
-						deposit,
-						mint_100.clone(),
-					),
-					key,
-					params,
-				)
-			})
+			.map(|key| para_client.tx().create_signed_offline(call, key, tx_params(nonce)))
 			.collect::<Result<Vec<_>, _>>()?;
 		txs.extend(txs_chunk);
 	}
 	let finalized_blocks = submit_txs(txs).await?;
 	for block in finalized_blocks {
-		let weight = para_client
+		let weight = client
 			.storage()
 			.at(block)
-			.fetch(&asset_hub_westend::storage().system().block_weight())
+			.fetch(&ahw::storage().system().block_weight())
 			.await?;
 		log::info!("Weight of block {:?}: {:?}", block, weight);
 	}
