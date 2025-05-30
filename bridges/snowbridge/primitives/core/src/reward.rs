b5727880ca3f49d2d9c583f4d2cb7a1d5c6e69bf
Francisco Aguirre <franciscoaguirreperez@gmail.com>
2025-05-30 00:40:37 -0300

    fix: more tests

--- bridges/snowbridge/primitives/core/src/reward.rs
+++ bridges/snowbridge/primitives/core/src/reward.rs
@@ -194,8 +194,8 @@ mod tests {
 	pub struct MockXcmExecutor;
 	impl<C> ExecuteXcm<C> for MockXcmExecutor {
 		type Prepared = Weightless;
-		fn prepare(message: Xcm<C>) -> Result<Self::Prepared, Xcm<C>> {
-			Err(message)
+		fn prepare(message: Xcm<C>) -> Result<Self::Prepared, u8> {
+			Err(0)
 		}
 		fn execute(
 			_: impl Into<Location>,
@@ -313,8 +313,8 @@ mod tests {
 		struct FailingXcmExecutor;
 		impl<C> ExecuteXcm<C> for FailingXcmExecutor {
 			type Prepared = Weightless;
-			fn prepare(message: Xcm<C>) -> Result<Self::Prepared, Xcm<C>> {
-				Err(message)
+			fn prepare(message: Xcm<C>) -> Result<Self::Prepared, u8> {
+				Err(0)
 			}
 			fn execute(
 				_: impl Into<Location>,
