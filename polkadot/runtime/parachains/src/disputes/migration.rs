8b3dd2f64bab235dd44601bcf7d5a94fff230455
Overkillus <maciej.zyszkiewicz@parity.io>
2025-05-30 11:46:05 +0100

    migration removal, decoding is backwards compatible

--- polkadot/runtime/parachains/src/disputes/migration.rs
+++ polkadot/runtime/parachains/src/disputes/migration.rs
@@ -18,45 +18,6 @@
 
 use frame_support::traits::StorageVersion;
 
-pub mod v2 {
-	use super::*;
-	use crate::disputes::slashing::{UnappliedSlashes, Config, Pallet};
-	use frame_support::{
-		pallet_prelude::*, traits::UncheckedOnRuntimeUpgrade, weights::Weight, migrations::VersionedMigration,
-	};
-	use polkadot_primitives::vstaging::PendingSlashes as PendingSlashesV2;
-
-	pub struct VersionUncheckedMigrateV1ToV2<T>(core::marker::PhantomData<T>);
-	impl<T: Config> UncheckedOnRuntimeUpgrade for VersionUncheckedMigrateV1ToV2<T> {
-		fn on_runtime_upgrade() -> Weight {
-			log::info!(target: crate::disputes::LOG_TARGET, "Migrating disputes storage from v1 to v2");
-
-			let mut read_count = 0;
-			let mut write_count = 0;
-	
-			for (session_index, candidate_hash, v1_slash) in v1::UnappliedSlashes::<T>::drain() {
-				read_count += 1;
-	
-				let v2_slash: PendingSlashesV2 = v1_slash.into();
-				UnappliedSlashes::<T>::insert(session_index, candidate_hash, v2_slash);
-	
-				write_count += 1;
-			}
-
-			log::info!(target: crate::disputes::LOG_TARGET, "Migration to v2 applied successfully");
-			T::DbWeight::get().reads_writes(read_count+1, write_count+1)
-		}
-	}
-
-	pub type MigrateV1ToV2<T> = VersionedMigration<
-		1,
-		2,
-		VersionUncheckedMigrateV1ToV2<T>,
-		Pallet<T>,
-		<T as frame_system::Config>::DbWeight,
-	>;
-}
-
 pub mod v1 {
 	use super::*;
 	use crate::disputes::{Config, Pallet};
@@ -64,20 +25,10 @@ pub mod v1 {
 	use frame_support::{
 		pallet_prelude::*, storage_alias, traits::OnRuntimeUpgrade, weights::Weight,
 	};
-	use polkadot_primitives::{SessionIndex, CandidateHash, slashing::PendingSlashes as PendingSlashesV1};
+	use polkadot_primitives::SessionIndex;
 
 	#[storage_alias]
 	type SpamSlots<T: Config> = StorageMap<Pallet<T>, Twox64Concat, SessionIndex, Vec<u32>>;
-
-	#[storage_alias]
-	pub type UnappliedSlashes<T: Config> = StorageDoubleMap<
-		Pallet<T>,
-		Twox64Concat,
-		SessionIndex,
-		Blake2_128Concat,
-		CandidateHash,
-		PendingSlashesV1,
-	>;
 
 	pub struct MigrateToV1<T>(core::marker::PhantomData<T>);
 	impl<T: Config> OnRuntimeUpgrade for MigrateToV1<T> {
