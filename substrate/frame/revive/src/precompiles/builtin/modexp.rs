bf67b1e75321fefcc5b9f60e9cfafb8601b0ff7e
Francisco Aguirre <franciscoaguirreperez@gmail.com>
2025-06-03 12:19:10 -0300

    Update substrate/frame/revive/src/precompiles/builtin/modexp.rs
    Co-authored-by: PG Herveou <pgherveou@gmail.com>

--- substrate/frame/revive/src/precompiles/builtin/modexp.rs
+++ substrate/frame/revive/src/precompiles/builtin/modexp.rs
@@ -87,9 +87,9 @@ impl<T: Config> PrimitivePrecompile for Modexp<T> {
 		}
 
 		// bounds check handled above
-		let base_len: usize = base_len_big.to_usize().expect("base_len out of bounds");
-		let exp_len: usize = exp_len_big.to_usize().expect("exp_len out of bounds");
-		let mod_len: usize = mod_len_big.to_usize().expect("mod_len out of bounds");
+		let base_len = base_len_big.to_usize().expect("base_len out of bounds");
+		let exp_len = exp_len_big.to_usize().expect("exp_len out of bounds");
+		let mod_len = mod_len_big.to_usize().expect("mod_len out of bounds");
 
 		// if mod_len is 0 output must be empty
 		if mod_len == 0 {
