737a12b1e4f5f5af724e016e014811e9190d22d2
clangenb <37865735+clangenb@users.noreply.github.com>
2025-06-05 23:08:31 +0200

    Update cumulus/parachains/runtimes/testing/penpal/src/genesis_config_presets.rs
    Co-authored-by: Iulian Barbu <14218860+iulianbarbu@users.noreply.github.com>

--- cumulus/parachains/runtimes/testing/penpal/src/genesis_config_presets.rs
+++ cumulus/parachains/runtimes/testing/penpal/src/genesis_config_presets.rs
@@ -13,7 +13,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//! Rococo Parachain Runtime genesis config presets
+//! Penpal Parachain Runtime genesis config presets
 
 use crate::*;
 use alloc::{vec, vec::Vec};
