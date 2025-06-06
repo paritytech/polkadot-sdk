b1f3d935e4d4b487a30cac0dbf6922e933ba0ad7
clangenb <37865735+clangenb@users.noreply.github.com>
2025-06-06 09:46:14 +0200

    Add genesis presets for remaining runtimes in polkadot-parachain-bin (#8426)
    Namely, this adds the genesis presets for: penpal,
    rococo-parachain-runtime and yet-another-parachain-runtime.
    
    This is a follow-up issue of
    https://github.com/paritytech/polkadot-sdk/issues/5704. Some runtimes
    were added after the initial refactor was done, and they were still
    following the old, obsolete paradigm.
    
    ---------
    
    Co-authored-by: Iulian Barbu <14218860+iulianbarbu@users.noreply.github.com>

--- cumulus/parachains/runtimes/testing/penpal/src/lib.rs
+++ cumulus/parachains/runtimes/testing/penpal/src/lib.rs
@@ -41,6 +41,7 @@
 #[cfg(feature = "std")]
 include!(concat!(env!("OUT_DIR"), "/wasm_binary.rs"));
 
+mod genesis_config_presets;
 mod weights;
 pub mod xcm_config;
 
@@ -1210,11 +1211,11 @@ impl_runtime_apis! {
 		}
 
 		fn get_preset(id: &Option<sp_genesis_builder::PresetId>) -> Option<Vec<u8>> {
-			get_preset::<RuntimeGenesisConfig>(id, |_| None)
+			get_preset::<RuntimeGenesisConfig>(id, &genesis_config_presets::get_preset)
 		}
 
 		fn preset_names() -> Vec<sp_genesis_builder::PresetId> {
-			vec![]
+			genesis_config_presets::preset_names()
 		}
 	}
 }
