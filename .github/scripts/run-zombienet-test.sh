b8e1eeaf09065c4ce9b9401c83442a4b820e5be4
Lukasz Rubaszewski <117115317+lrubasze@users.noreply.github.com>
2025-06-06 13:40:27 +0200

    ci: run zombienet tests with native provider

---
+++ .github/scripts/run-zombienet-test.sh
@@ -0,0 +1,73 @@
+#!/usr/bin/env bash
+
+function run_test {
+  cd "${OUTPUT_DIR}"
+  for i in $(find ${OUTPUT_DIR} -name "${TEST_TO_RUN}"| head -1); do
+    TEST_FOUND=1
+    ${ZOMBIE_COMMAND} -p native -c $CONCURRENCY test $i
+    EXIT_STATUS=$?
+  done;
+  if [[ $TEST_FOUND -lt 1 ]]; then
+    EXIT_STATUS=1
+  fi;
+}
+
+function create_isolated_dir {
+  TS=$(date +%s)
+  ISOLATED=${OUTPUT_DIR}/${TS}
+  mkdir -p ${ISOLATED}
+  OUTPUT_DIR="${ISOLATED}"
+}
+
+function copy_to_isolated {
+  cd "${SCRIPT_PATH}"
+  echo $(pwd)
+  cp -r "${LOCAL_DIR}"/* "${OUTPUT_DIR}"
+}
+
+function rm_isolated_dir {
+  echo "Removing ${OUTPUT_DIR}"
+  rm -rf "${OUTPUT_DIR}"
+}
+
+function log {
+  local lvl msg fmt
+  lvl=$1 msg=$2
+  fmt='+%Y-%m-%d %H:%M:%S'
+  lg_date=$(date "${fmt}")
+  if [[ "${lvl}" = "DIE" ]] ; then
+    lvl="ERROR"
+   echo -e "\n${lg_date} - ${lvl} - ${msg}"
+   exit 1
+  else
+    echo -e "\n${lg_date} - ${lvl} - ${msg}"
+  fi
+}
+
+set -x
+
+SCRIPT_NAME="$0"
+SCRIPT_PATH=$(dirname "$0")               # relative
+SCRIPT_PATH=$(cd "${SCRIPT_PATH}" && pwd) # absolutized and normalized
+
+ZOMBIE_COMMAND=zombie
+EXIT_STATUS=0
+
+# args
+LOCAL_DIR="$1"
+CONCURRENCY="$2"
+TEST_TO_RUN="$3"
+
+cd "${SCRIPT_PATH}"
+
+OUTPUT_DIR="${SCRIPT_PATH}"
+
+create_isolated_dir
+copy_to_isolated
+run_test
+rm_isolated_dir
+
+log INFO "Exit status is ${EXIT_STATUS}"
+exit "${EXIT_STATUS}"
+
+
