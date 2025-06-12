8c83b60d75c4a39196014fd2fc80169650524762
Robert van Eerdewijk <robert@Roberts-MacBook-Pro.local>
2025-06-12 09:38:41 +0200

    re-apply pr suggestions

--- substrate/frame/revive/proc-macro/src/lib.rs
+++ substrate/frame/revive/proc-macro/src/lib.rs
@@ -39,13 +39,13 @@ pub fn unstable_hostfn(_attr: TokenStream, item: TokenStream) -> TokenStream {
 /// Defines a host functions set that can be imported by contract polkavm code.
 ///
 /// **CAUTION**: Be advised that all functions defined by this macro
-/// will panic if called with unexpected arguments.
+/// cause undefined behaviour inside the contract if the signature does not match.
 ///
-/// WARNING: It is CRITICAL for the user of this macro to thoroughly check the signatures of polkavm
-/// code to be executed. Failure to do so may result in undefined behavior, including runtime panics
-/// or security vulnerabilities. For example, if a function is called with an incorrect signature,
-/// it could lead to memory corruption or unexpected results. and reject the code if any imported
-/// function has a mismatched signature.
+/// WARNING: It is CRITICAL for contracts to make sure that the signatures match exactly.
+/// Failure to do so may result in undefined behavior, traps or security vulnerabilities inside the
+/// contract. The runtime itself is unharmed due to sandboxing.
+/// For example, if a function is called with an incorrect signature, it could lead to memory corruption or
+/// unexpected results within the contract.
 #[proc_macro_attribute]
 pub fn define_env(attr: TokenStream, item: TokenStream) -> TokenStream {
 	if !attr.is_empty() {
