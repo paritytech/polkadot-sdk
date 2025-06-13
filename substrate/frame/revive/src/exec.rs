0fdcbaceaf5983933787dcf996ca1c84e9af4e55
PG Herveou <pgherveou@gmail.com>
2025-06-13 13:28:21 +0200

    [pallet-revive] prestate tracer (#8742)
    - Add prestate tracer, with identical API as
    https://geth.ethereum.org/docs/developers/evm-tracing/built-in-tracers#prestate-tracer
    - Add coinbase runtime API to get the miner's address
    tested against geth with
    https://github.com/paritytech/evm-test-suite/pull/93
    
    fixes https://github.com/paritytech/revive-differential-tests/issues/7
    
    ---------
    
    Co-authored-by: cmd[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- substrate/frame/revive/src/exec.rs
+++ substrate/frame/revive/src/exec.rs
@@ -37,7 +37,7 @@ use frame_support::{
 	traits::{
 		fungible::{Inspect, Mutate},
 		tokens::{Fortitude, Preservation},
-		FindAuthor, Time,
+		Time,
 	},
 	weights::Weight,
 	Blake2_128Concat, BoundedVec, StorageHasher,
@@ -83,11 +83,6 @@ pub enum Key {
 
 impl Key {
 	/// Reference to the raw unhashed key.
-	///
-	/// # Note
-	///
-	/// Only used by benchmarking in order to generate storage collisions on purpose.
-	#[cfg(feature = "runtime-benchmarks")]
 	pub fn unhashed(&self) -> &[u8] {
 		match self {
 			Key::Fix(v) => v.as_ref(),
@@ -390,7 +385,7 @@ pub trait PrecompileExt: sealing::Sealed {
 	fn block_hash(&self, block_number: U256) -> Option<H256>;
 
 	/// Returns the author of the current block.
-	fn block_author(&self) -> Option<AccountIdOf<Self::T>>;
+	fn block_author(&self) -> Option<H160>;
 
 	/// Returns the maximum allowed size of a storage item.
 	fn max_value_size(&self) -> u32;
@@ -779,7 +774,6 @@ where
 		)? {
 			stack.run(executable, input_data).map(|_| stack.first_frame.last_frame_output)
 		} else {
-			let result = Self::transfer_from_origin(&origin, &origin, &dest, value, storage_meter);
 			if_tracing(|t| {
 				t.enter_child_span(
 					origin.account_id().map(T::AddressMapper::to_address).unwrap_or_default(),
@@ -790,10 +784,13 @@ where
 					&input_data,
 					Weight::zero(),
 				);
-				match result {
-					Ok(ref output) => t.exit_child_span(&output, Weight::zero()),
-					Err(e) => t.exit_child_span_with_error(e.error.into(), Weight::zero()),
-				}
+			});
+
+			let result = Self::transfer_from_origin(&origin, &origin, &dest, value, storage_meter);
+
+			if_tracing(|t| match result {
+				Ok(ref output) => t.exit_child_span(&output, Weight::zero()),
+				Err(e) => t.exit_child_span_with_error(e.error.into(), Weight::zero()),
 			});
 
 			result
@@ -1759,6 +1756,18 @@ where
 			)? {
 				self.run(executable, input_data)
 			} else {
+				if_tracing(|t| {
+					t.enter_child_span(
+						T::AddressMapper::to_address(self.account_id()),
+						T::AddressMapper::to_address(&dest),
+						false,
+						is_read_only,
+						value,
+						&input_data,
+						Weight::zero(),
+					);
+				});
+
 				let result = if is_read_only && value.is_zero() {
 					Ok(Default::default())
 				} else if is_read_only {
@@ -1775,21 +1784,11 @@ where
 					)
 				};
 
-				if_tracing(|t| {
-					t.enter_child_span(
-						T::AddressMapper::to_address(self.account_id()),
-						T::AddressMapper::to_address(&dest),
-						false,
-						is_read_only,
-						value,
-						&input_data,
-						Weight::zero(),
-					);
-					match result {
-						Ok(ref output) => t.exit_child_span(&output, Weight::zero()),
-						Err(e) => t.exit_child_span_with_error(e.error.into(), Weight::zero()),
-					}
+				if_tracing(|t| match result {
+					Ok(ref output) => t.exit_child_span(&output, Weight::zero()),
+					Err(e) => t.exit_child_span_with_error(e.error.into(), Weight::zero()),
 				});
+
 				result.map(|_| ())
 			}
 		};
@@ -1882,7 +1881,12 @@ where
 	}
 
 	fn balance_of(&self, address: &H160) -> U256 {
-		self.account_balance(&<Self::T as Config>::AddressMapper::to_account_id(address))
+		let balance =
+			self.account_balance(&<Self::T as Config>::AddressMapper::to_account_id(address));
+		if_tracing(|tracer| {
+			tracer.balance_read(address, balance);
+		});
+		balance
 	}
 
 	fn value_transferred(&self) -> U256 {
@@ -1913,11 +1917,8 @@ where
 		self.block_hash(block_number)
 	}
 
-	fn block_author(&self) -> Option<AccountIdOf<Self::T>> {
-		let digest = <frame_system::Pallet<T>>::digest();
-		let pre_runtime_digests = digest.logs.iter().filter_map(|d| d.as_pre_runtime());
-
-		T::FindAuthor::find_author(pre_runtime_digests)
+	fn block_author(&self) -> Option<H160> {
+		crate::Pallet::<T>::block_author()
 	}
 
 	fn max_value_size(&self) -> u32 {
