2d0b018b4952817526ab76bcf695115bba88eaab
ordian <noreply@reusable.software>
2025-06-16 10:56:18 +0200

    move the zeroing to inclusion

--- polkadot/runtime/parachains/src/paras_inherent/mod.rs
+++ polkadot/runtime/parachains/src/paras_inherent/mod.rs
@@ -511,13 +511,7 @@ impl<T: Config> Pallet<T> {
 				&validator_public[..],
 				bitfields.clone(),
 			);
-		// For relay chain blocks, we're (ab)using the proof size
-		// to limit the raw transaction size of `ParaInherent` and
-		// there's no state proof (aka PoV) associated with it.
-		// Since we already accounted for bitfields size, we should
-		// not include `enact_candidate` PoV impact here.
-		all_weight_after.saturating_accrue(enact_weight.set_proof_size(0));
-
+		all_weight_after.saturating_accrue(enact_weight);
 		log::debug!(
 			target: LOG_TARGET,
 			"Enacting weight: {}, all weight: {}",
