8608884823572158a3024c1d16c925fad9eaee5c
Karol Kokoszka <karol@parity.io>
2025-06-23 22:41:42 +0200

    Introduce LocalPayArguments to fix benchmark builds

--- cumulus/parachains/runtimes/assets/asset-hub-westend/src/governance/mod.rs
+++ cumulus/parachains/runtimes/assets/asset-hub-westend/src/governance/mod.rs
@@ -24,7 +24,7 @@ use frame_support::{
 };
 use frame_system::EnsureRootWithSuccess;
 use pallet_xcm::{EnsureXcm, IsVoiceOfBody};
-use parachains_common::pay::{LocalPay, VersionedLocatableAccount};
+use parachains_common::pay::{benchmarks::LocalPayArguments, LocalPay, VersionedLocatableAccount};
 use polkadot_runtime_common::{
 	impls::{ContainsParts, VersionedLocatableAsset},
 	prod_or_fast,
