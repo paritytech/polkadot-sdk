feb325127c076132f17354652098cf95eed1918f
Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
2025-06-25 15:12:09 +0200

    Update substrate/frame/revive/fixtures/build.rs
    Co-authored-by: Alexander Theißen <alex.theissen@me.com>

--- substrate/frame/revive/fixtures/build.rs
+++ substrate/frame/revive/fixtures/build.rs
@@ -294,8 +294,8 @@ pub fn main() -> Result<()> {
 	println!("cargo::rerun-if-env-changed={OVERRIDE_RUSTUP_TOOLCHAIN_ENV_VAR}");
 	println!("cargo::rerun-if-env-changed={OVERRIDE_STRIP_ENV_VAR}");
 	println!("cargo::rerun-if-env-changed={OVERRIDE_OPTIMIZE_ENV_VAR}");
-
 	println!("cargo::rerun-if-changed={}", out_dir.display());
+
 	// the fixtures have a dependency on the uapi crate
 	println!("cargo::rerun-if-changed={}", fixtures_dir.display());
 	let uapi_dir = fixtures_dir.parent().expect("parent dir exits; qed").join("uapi");
