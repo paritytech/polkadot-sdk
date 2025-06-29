cd1c95913b1448eebd93a6c933d2dddbe9c737df
EleisonC <ckalule7@gmail.com>
2025-06-29 20:32:17 +0300

    [bridge-hub-rococo] update AssetHubLocation imports and usage

--- cumulus/parachains/runtimes/bridge-hubs/bridge-hub-rococo/src/lib.rs
+++ cumulus/parachains/runtimes/bridge-hubs/bridge-hub-rococo/src/lib.rs
@@ -1116,13 +1116,12 @@ impl_runtime_apis! {
 
 			use xcm::latest::prelude::*;
 			use xcm_config::TokenLocation;
-			use xcm_config::{AssetHubId, AssetHubParaLocation};
+			use testnet_parachains_constants::rococo::locations::{AssetHubParaId, AssetHubLocation};
 			parameter_types! {
 				pub ExistentialDepositAsset: Option<Asset> = Some((
 					TokenLocation::get(),
 					ExistentialDeposit::get()
 				).into());
-				pub AssetHubParaId: ParaId = ParaId::new(AssetHubId::get());
 			}
 
 			use pallet_xcm::benchmarking::Pallet as PalletXcmExtrinsicsBenchmark;
@@ -1136,7 +1135,7 @@ impl_runtime_apis! {
 					>;
 
 				fn reachable_dest() -> Option<Location> {
-					Some(AssetHubParaLocation::get())
+					Some(AssetHubLocation::get())
 				}
 
 				fn teleportable_asset_and_dest() -> Option<(Asset, Location)> {
@@ -1146,7 +1145,7 @@ impl_runtime_apis! {
 							fun: Fungible(ExistentialDeposit::get()),
 							id: AssetId(TokenLocation::get())
 						},
-						AssetHubParaLocation::get(),
+						AssetHubLocation::get(),
 					))
 				}
 
@@ -1160,7 +1159,7 @@ impl_runtime_apis! {
 					// BH only supports teleports to system parachain.
 					// Relay/native token can be teleported between BH and Relay.
 					let native_location = TokenLocation::get();
-					let dest = AssetHubParaLocation::get();
+					let dest = AssetHubLocation::get();
 					pallet_xcm::benchmarking::helpers::native_teleport_as_asset_transfer::<Runtime>(
 						native_location,
 						dest
@@ -1186,7 +1185,7 @@ impl_runtime_apis! {
 						ParachainSystem,
 					>;
 				fn valid_destination() -> Result<Location, BenchmarkError> {
-					Ok(AssetHubParaLocation::get())
+					Ok(AssetHubLocation::get())
 				}
 				fn worst_case_holding(_depositable_count: u32) -> Assets {
 					// just concrete assets according to relay chain.
@@ -1202,7 +1201,7 @@ impl_runtime_apis! {
 
 			parameter_types! {
 				pub TrustedTeleporter: Option<(Location, Asset)> = Some((
-					AssetHubParaLocation::get(),
+					AssetHubLocation::get(),
 					Asset { fun: Fungible(UNITS), id: AssetId(TokenLocation::get()) },
 				));
 				pub const CheckedAccount: Option<(AccountId, xcm_builder::MintLocation)> = None;
@@ -1241,15 +1240,15 @@ impl_runtime_apis! {
 				}
 
 				fn transact_origin_and_runtime_call() -> Result<(Location, RuntimeCall), BenchmarkError> {
-					Ok((AssetHubParaLocation::get(), frame_system::Call::remark_with_event { remark: vec![] }.into()))
+					Ok((AssetHubLocation::get(), frame_system::Call::remark_with_event { remark: vec![] }.into()))
 				}
 
 				fn subscribe_origin() -> Result<Location, BenchmarkError> {
-					Ok(AssetHubParaLocation::get())
+					Ok(AssetHubLocation::get())
 				}
 
 				fn claimable_asset() -> Result<(Location, Location, Assets), BenchmarkError> {
-					let origin = AssetHubParaLocation::get();
+					let origin = AssetHubLocation::get();
 					let assets: Assets = (AssetId(TokenLocation::get()), 1_000 * UNITS).into();
 					let ticket = Location { parents: 0, interior: Here };
 					Ok((origin, ticket, assets))
