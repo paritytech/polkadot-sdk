0dd37f56e26ccfa4c53db35e7c38480404e0c31d
James Wilson <james@jsdw.me>
2025-07-01 14:31:49 +0100

    Try recursion limit of 2048

--- substrate/test-utils/runtime/src/lib.rs
+++ substrate/test-utils/runtime/src/lib.rs
@@ -18,7 +18,7 @@
 //! The Substrate runtime. This can be compiled with `#[no_std]`, ready for Wasm.
 
 #![cfg_attr(not(feature = "std"), no_std)]
-#![recursion_limit = "1024"]
+#![recursion_limit = "2048"]
 
 extern crate alloc;
 
