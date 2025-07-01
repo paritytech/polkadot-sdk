6824a6782354e274cf308be314f142551c5457ab
Nikolai Kozlov <1431590+nkpar@users.noreply.github.com>
2025-07-01 18:34:56 +0100

    Add optional auto-rebag within on-idle (#8684)
    Implemented an `on_idle` hook for automatic rebagging of accounts,
    enabling incremental correction of account positions within the
    bags-list during the idle phase of block execution. Introduced new
    storages and configurable constants to support this functionality, along
    with improvements in logging and internal helpers.
    
    ---------
    
    Co-authored-by: cmd[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Ankan <10196091+Ank4n@users.noreply.github.com>
    Co-authored-by: Kian Paimani <5588131+kianenigma@users.noreply.github.com>

--- cumulus/parachains/runtimes/assets/asset-hub-westend/src/staking.rs
+++ cumulus/parachains/runtimes/assets/asset-hub-westend/src/staking.rs
@@ -184,6 +184,7 @@ impl multi_block::unsigned::miner::MinerConfig for Runtime {
 
 parameter_types! {
 	pub const BagThresholds: &'static [u64] = &bag_thresholds::THRESHOLDS;
+	pub const AutoRebagNumber: u32 = 10;
 }
 
 type VoterBagsListInstance = pallet_bags_list::Instance1;
@@ -193,6 +194,7 @@ impl pallet_bags_list::Config<VoterBagsListInstance> for Runtime {
 	type WeightInfo = weights::pallet_bags_list::WeightInfo<Runtime>;
 	type BagThresholds = BagThresholds;
 	type Score = sp_npos_elections::VoteWeight;
+	type MaxAutoRebagPerBlock = AutoRebagNumber;
 }
 
 pub struct EraPayout;
