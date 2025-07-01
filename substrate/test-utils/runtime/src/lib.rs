db37012feee0098be158aed57d952ca9a9880dca
James Wilson <james@jsdw.me>
2025-07-01 15:05:23 +0100

    Try again for 1.88 instead

--- substrate/test-utils/runtime/src/lib.rs
+++ substrate/test-utils/runtime/src/lib.rs
@@ -18,7 +18,6 @@
 //! The Substrate runtime. This can be compiled with `#[no_std]`, ready for Wasm.
 
 #![cfg_attr(not(feature = "std"), no_std)]
-#![recursion_limit = "2048"]
 
 extern crate alloc;
 
