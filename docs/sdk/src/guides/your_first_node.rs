21152af2da32e538dd968438f124f067359201be
fuder.eth <vtljgrinkiv@gmail.com>
2025-07-03 12:24:23 +0200

    Update your_first_node.rs

--- docs/sdk/src/guides/your_first_node.rs
+++ docs/sdk/src/guides/your_first_node.rs
@@ -330,7 +330,7 @@ mod tests {
 	}
 
 	#[tokio::test]
-	// This is a regresion test so that we still remain compatible with runtimes that use
+	// This is a regression test so that we still remain compatible with runtimes that use
 	// `para-id` in chain specs, instead of implementing the
 	// `cumulus_primitives_core::GetParachainInfo`.
 	async fn omni_node_dev_mode_works_without_getparachaininfo() {
