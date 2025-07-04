5f5bec730f7704d2413d09819ef0dc45b8299b5a
Lukasz Rubaszewski <117115317+lrubasze@users.noreply.github.com>
2025-07-04 18:23:48 +0200

    slot_based_authoring-test: reduce number of validators to 5

--- cumulus/zombienet/zombienet-sdk/tests/zombie_ci/elastic_scaling/slot_based_authoring.rs
+++ cumulus/zombienet/zombienet-sdk/tests/zombie_ci/elastic_scaling/slot_based_authoring.rs
@@ -93,7 +93,7 @@ async fn build_network_config() -> Result<NetworkConfig, anyhow::Error> {
 	// - relaychain nodes:
 	// 	 - alice
 	// 	   - validator
-	// 	 - validator[0-7]
+	// 	 - validator[0-4]
 	// 	   - validator
 	// 	   - synchronize only with alice
 	// - parachain nodes
@@ -125,7 +125,7 @@ async fn build_network_config() -> Result<NetworkConfig, anyhow::Error> {
 				// type.
 				.with_node(|node| node.with_name("alice").with_args(vec![]));
 
-			(0..8).fold(r, |acc, i| {
+			(0..5).fold(r, |acc, i| {
 				acc.with_node(|node| {
 					node.with_name(&format!("validator-{i}")).with_args(vec![
 						("-lruntime=debug,parachain=trace").into(),
