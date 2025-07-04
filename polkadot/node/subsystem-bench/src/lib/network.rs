0f78710f8485e6e590bbe1ae6b8390bc18cb0e30
James Wilson <james@jsdw.me>
2025-07-04 22:54:27 +0100

    BACKPORT-CONFLICT

--- polkadot/node/subsystem-bench/src/lib/network.rs
+++ polkadot/node/subsystem-bench/src/lib/network.rs
@@ -155,7 +155,7 @@ pub enum NetworkMessage {
 	/// A gossip message from node to a peer.
 	MessageFromNode(AuthorityDiscoveryId, VersionedValidationProtocol),
 	/// A request originating from our node
-	RequestFromNode(AuthorityDiscoveryId, Requests),
+	RequestFromNode(AuthorityDiscoveryId, Box<Requests>),
 	/// A request originating from an emulated peer
 	RequestFromPeer(IncomingRequest),
 }
@@ -351,7 +351,7 @@ impl NetworkInterface {
 							// usage for the node.
 							let send_task = Self::proxy_send_request(
 								peer.clone(),
-								request,
+								*request,
 								tx_network.clone(),
 								task_rx_limiter.clone(),
 							)
@@ -784,7 +784,7 @@ pub fn new_network(
 	handlers: Vec<Arc<dyn HandleNetworkMessage + Sync + Send>>,
 ) -> (NetworkEmulatorHandle, NetworkInterface, NetworkInterfaceReceiver) {
 	let n_peers = config.n_validators;
-	gum::info!(target: LOG_TARGET, "{}",format!("Initializing emulation for a {} peer network.", n_peers).bright_blue());
+	gum::info!(target: LOG_TARGET, "{}",format!("Initializing emulation for a {n_peers} peer network.").bright_blue());
 	gum::info!(target: LOG_TARGET, "{}",format!("connectivity {}%, latency {:?}", config.connectivity, config.latency).bright_black());
 
 	let metrics =
@@ -828,7 +828,7 @@ pub fn new_network(
 		peers[*peer].disconnect();
 	}
 
-	gum::info!(target: LOG_TARGET, "{}",format!("Network created, connected validator count {}", connected_count).bright_black());
+	gum::info!(target: LOG_TARGET, "{}",format!("Network created, connected validator count {connected_count}").bright_black());
 
 	let handle = NetworkEmulatorHandle {
 		peers,
@@ -876,7 +876,8 @@ impl NetworkEmulatorHandle {
 	pub fn send_request_to_peer(&self, peer_id: &AuthorityDiscoveryId, request: Requests) {
 		let peer = self.peer(peer_id);
 		assert!(peer.is_connected(), "forward request only for connected peers.");
-		peer.handle().receive(NetworkMessage::RequestFromNode(peer_id.clone(), request));
+		peer.handle()
+			.receive(NetworkMessage::RequestFromNode(peer_id.clone(), Box::new(request)));
 	}
 
 	/// Send a message from a peer to the node.
@@ -982,14 +983,14 @@ impl Metrics {
 	/// Increment total sent for a peer.
 	pub fn on_peer_sent(&self, peer_index: usize, bytes: usize) {
 		self.peer_total_sent
-			.with_label_values(vec![format!("node{}", peer_index).as_str()].as_slice())
+			.with_label_values(vec![format!("node{peer_index}").as_str()].as_slice())
 			.inc_by(bytes as u64);
 	}
 
 	/// Increment total received for a peer.
 	pub fn on_peer_received(&self, peer_index: usize, bytes: usize) {
 		self.peer_total_received
-			.with_label_values(vec![format!("node{}", peer_index).as_str()].as_slice())
+			.with_label_values(vec![format!("node{peer_index}").as_str()].as_slice())
 			.inc_by(bytes as u64);
 	}
 }
