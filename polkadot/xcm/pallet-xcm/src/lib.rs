09675ad40502ee977937ff2bb07899a8a386d873
Francisco Aguirre <franciscoaguirreperez@gmail.com>
2025-07-11 14:02:52 -0300

    chore: move patch to its own mod

--- polkadot/xcm/pallet-xcm/src/lib.rs
+++ polkadot/xcm/pallet-xcm/src/lib.rs
@@ -25,6 +25,7 @@ mod mock;
 pub mod precompiles;
 #[cfg(test)]
 mod tests;
+mod transfer_assets_validation;
 
 pub mod migration;
 #[cfg(any(test, feature = "test-utils"))]
@@ -1492,21 +1493,10 @@ pub mod pallet {
 			// We check for network native asset reserve transfers in preparation for the Asset Hub
 			// Migration. This check will be removed after the migration and the determined
 			// reserve location adjusted accordingly. For more information, see https://github.com/paritytech/polkadot-sdk/issues/9054.
-
-			// Extract fee asset and check both assets and fees separately.
-			let fee_asset = assets.get(fee_asset_item as usize).ok_or(Error::<T>::Empty)?;
-			let mut remaining_assets = assets.clone();
-			remaining_assets.remove(fee_asset_item as usize);
-
-			// Check remaining assets with their transfer type
 			Self::ensure_network_asset_reserve_transfer_allowed(
-				&remaining_assets,
+				&assets,
+				fee_asset_item,
 				&assets_transfer_type,
-			)?;
-
-			// Check fee asset with its transfer type
-			Self::ensure_network_asset_reserve_transfer_allowed(
-				&[fee_asset.clone()],
 				&fees_transfer_type,
 			)?;
 
@@ -2034,108 +2024,6 @@ impl<T: Config> Pallet<T> {
 			fees_transfer_type.ok_or(Error::<T>::Empty)?,
 			assets_transfer_type.ok_or(Error::<T>::Empty)?,
 		))
-	}
-
-	/// Check if the given asset ID represents a network native asset based on our
-	/// UniversalLocation.
-	///
-	/// Returns true if the asset is a native network asset (DOT, KSM, WND, PAS) that should be
-	/// blocked during Asset Hub Migration.
-	///
-	/// NOTE: Paseo uses `NetworkId::Polkadot`, so it is covered here.
-	fn is_network_native_asset(asset_id: &AssetId) -> bool {
-		let universal_location = T::UniversalLocation::get();
-		let asset_location = &asset_id.0;
-
-		match universal_location.len() {
-			// Case 1: We are on the Relay Chain itself.
-			// UniversalLocation: GlobalConsensus(Network).
-			// Network asset ID: Here.
-			1 => {
-				if let Some(Junction::GlobalConsensus(network)) = universal_location.first() {
-					let is_target_network = match network {
-						NetworkId::Polkadot | NetworkId::Kusama => true,
-						NetworkId::ByGenesis(genesis_hash) => {
-							// Check if this is Westend by genesis hash
-							*genesis_hash == xcm::v5::WESTEND_GENESIS_HASH ||
-								*genesis_hash == xcm::v5::ROCOCO_GENESIS_HASH // Used in tests.
-						},
-						_ => false,
-					};
-					is_target_network && asset_location.is_here()
-				} else {
-					false
-				}
-			},
-			// Case 2: We are on a parachain within one of the specified networks.
-			// UniversalLocation: GlobalConsensus(Network)/Parachain(id).
-			// Network asset ID: Parent.
-			2 => {
-				if let (Some(Junction::GlobalConsensus(network)), Some(Junction::Parachain(_))) =
-					(universal_location.first(), universal_location.last())
-				{
-					let is_target_network = match network {
-						NetworkId::Polkadot | NetworkId::Kusama => true,
-						NetworkId::ByGenesis(genesis_hash) => {
-							// Check if this is Westend by genesis hash
-							*genesis_hash == xcm::v5::WESTEND_GENESIS_HASH ||
-								*genesis_hash == xcm::v5::ROCOCO_GENESIS_HASH // Used in tests.
-						},
-						_ => false,
-					};
-					is_target_network && *asset_location == Location::parent()
-				} else {
-					false
-				}
-			},
-			// Case 3: We are not on a relay or parachain. We return false.
-			_ => false,
-		}
-	}
-
-	/// Check if network native asset reserve transfers should be blocked during Asset Hub
-	/// Migration.
-	///
-	/// During the Asset Hub Migration (AHM), the native network asset's reserve will move
-	/// from the Relay Chain to Asset Hub. The `transfer_assets` function automatically determines
-	/// reserves based on asset ID location, which would incorrectly assume Relay Chain as the
-	/// reserve.
-	///
-	/// This function blocks native network asset reserve transfers to prevent issues during
-	/// the migration. Users should use `limited_reserve_transfer_assets` instead, which allows
-	/// explicit reserve specification.
-	fn ensure_network_asset_reserve_transfer_allowed(
-		assets: &[Asset],
-		transfer_type: &TransferType,
-	) -> Result<(), Error<T>> {
-		// Check if any reserve transfer (LocalReserve, DestinationReserve, or RemoteReserve)
-		// is being attempted.
-		let is_reserve_transfer = matches!(
-			transfer_type,
-			TransferType::LocalReserve |
-				TransferType::DestinationReserve |
-				TransferType::RemoteReserve(_)
-		);
-
-		if !is_reserve_transfer {
-			// If not a reserve transfer (e.g., teleport), allow it.
-			return Ok(());
-		}
-
-		// Check if any asset is a network native asset.
-		for asset in assets {
-			if Self::is_network_native_asset(&asset.id) {
-				tracing::debug!(
-					target: "xcm::pallet_xcm::transfer_assets",
-					asset_id = ?asset.id, ?transfer_type,
-					"Network native asset reserve transfer blocked during Asset Hub Migration. Use `limited_reserve_transfer_assets` instead."
-				);
-				// It's error-prone to try to determine the reserve in this circumstances.
-				return Err(Error::<T>::InvalidAssetUnknownReserve);
-			}
-		}
-
-		Ok(())
 	}
 
 	fn do_reserve_transfer_assets(
