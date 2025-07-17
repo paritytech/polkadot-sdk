774f3ce5124aa1a6b87ab2c1cadbffc22c825e8d
EleisonC <ckalule7@gmail.com>
2025-07-18 01:10:02 +0300

    [westend] move AssetHubParaId & PeopleParaId to westend_runtime_constants::system_parachain

--- cumulus/parachains/runtimes/constants/src/westend.rs
+++ cumulus/parachains/runtimes/constants/src/westend.rs
@@ -197,14 +197,12 @@ pub mod xcm_version {
 }
 
 pub mod locations {
-	use cumulus_primitives_core::ParaId;
 	use frame_support::parameter_types;
 	pub use westend_runtime_constants::system_parachain::{AssetHubParaId, PeopleParaId};
+	use westend_runtime_constants::system_parachain::{ASSET_HUB_ID, PEOPLE_ID};
 	use xcm::latest::prelude::{Location, Parachain};
 
 	parameter_types! {
-		pub AssetHubParaId: ParaId = ASSET_HUB_ID.into();
-		pub PeopleParaId: ParaId = PEOPLE_ID.into();
 		pub AssetHubLocation: Location = Location::new(1, Parachain(ASSET_HUB_ID));
 		pub PeopleLocation: Location = Location::new(1, Parachain(PEOPLE_ID));
 	}
