7a747ffbe6dcaa83e5a1e76d2b752a5b4669e7c3
PG Herveou <pgherveou@gmail.com>
2025-07-21 15:17:27 +0200

    [revive] eth-decimals (#9101)
    On Ethereum, 1 ETH is represented as 10^18 wei (wei being the smallest
    unit).
    On Polkadot 1 DOT is defined as 1010 plancks. It means that any value
    smaller than 10^8 wei can not be expressed with the native balance. Any
    contract that attempts to use such a value currently reverts with a
    DecimalPrecisionLoss error.
    
    In theory, RPC can define a decimal representation different from
    Ethereum mainnet (10^18). In practice tools (frontend libraries,
    wallets, and compilers) ignore it and expect 18 decimals.
    
    The current behaviour breaks eth compatibility and needs to be updated.
    See issue #109 for more details.
    
    
    Fix  https://github.com/paritytech/contract-issues/issues/109
    [weights
    compare](https://weights.tasty.limo/compare?unit=weight&ignore_errors=true&threshold=10&method=asymptotic&repo=polkadot-sdk&old=master&new=pg/eth-decimals&path_pattern=substrate/frame/**/src/weights.rs,polkadot/runtime/*/src/weights/**/*.rs,polkadot/bridges/modules/*/src/weights.rs,cumulus/**/weights/*.rs,cumulus/**/weights/xcm/*.rs,cumulus/**/src/weights.rs)
    
    ---------
    
    Co-authored-by: cmd[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Alexander Theißen <alex.theissen@me.com>
    Co-authored-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>

--- substrate/frame/revive/src/call_builder.rs
+++ substrate/frame/revive/src/call_builder.rs
@@ -32,8 +32,9 @@ use crate::{
 	storage::meter::Meter,
 	transient_storage::MeterEntry,
 	vm::{PreparedCall, Runtime},
-	BalanceOf, BumpNonce, Code, CodeInfoOf, Config, ContractBlob, ContractInfo, ContractInfoOf,
-	DepositLimit, Error, GasMeter, MomentOf, Origin, Pallet as Contracts, PristineCode, Weight,
+	AccountInfo, BalanceOf, BalanceWithDust, BumpNonce, Code, CodeInfoOf, Config, ContractBlob,
+	ContractInfo, DepositLimit, Error, GasMeter, MomentOf, Origin, Pallet as Contracts,
+	PristineCode, Weight,
 };
 use alloc::{vec, vec::Vec};
 use frame_support::{storage::child, traits::fungible::Mutate};
@@ -94,7 +95,7 @@ where
 			// Whitelist the contract's contractInfo as it is already accounted for in the call
 			// benchmark
 			frame_benchmarking::benchmarking::add_to_whitelist(
-				crate::ContractInfoOf::<T>::hashed_key_for(&T::AddressMapper::to_address(
+				crate::AccountInfoOf::<T>::hashed_key_for(&T::AddressMapper::to_address(
 					&contract.account_id,
 				))
 				.into(),
@@ -124,7 +125,7 @@ where
 	}
 
 	/// Set the contract's balance.
-	pub fn set_balance(&mut self, value: BalanceOf<T>) {
+	pub fn set_balance(&mut self, value: impl Into<BalanceWithDust<BalanceOf<T>>>) {
 		self.contract.set_balance(value);
 	}
 
@@ -264,7 +265,7 @@ where
 
 		let outcome = Contracts::<T>::bare_instantiate(
 			origin,
-			0u32.into(),
+			U256::zero(),
 			Weight::MAX,
 			DepositLimit::Balance(default_deposit_limit::<T>()),
 			Code::Upload(module.code),
@@ -277,8 +278,7 @@ where
 		let account_id = T::AddressMapper::to_fallback_account_id(&address);
 		let result = Contract { caller, address, account_id };
 
-		ContractInfoOf::<T>::insert(&address, result.info()?);
-
+		AccountInfo::<T>::insert_contract(&address, result.info()?);
 		Ok(result)
 	}
 
@@ -309,7 +309,8 @@ where
 			info.write(&Key::Fix(item.0), Some(item.1.clone()), None, false)
 				.map_err(|_| "Failed to write storage to restoration dest")?;
 		}
-		<ContractInfoOf<T>>::insert(&self.address, info);
+
+		AccountInfo::<T>::insert_contract(&self.address, info);
 		Ok(())
 	}
 
@@ -351,7 +352,7 @@ where
 
 	/// Get the `ContractInfo` of the `addr` or an error if it no longer exists.
 	pub fn address_info(addr: &T::AccountId) -> Result<ContractInfo<T>, &'static str> {
-		ContractInfoOf::<T>::get(T::AddressMapper::to_address(addr))
+		<AccountInfo<T>>::load_contract(&T::AddressMapper::to_address(addr))
 			.ok_or("Expected contract to exist at this point.")
 	}
 
@@ -361,8 +362,12 @@ where
 	}
 
 	/// Set the balance of the contract to the supplied amount.
-	pub fn set_balance(&self, balance: BalanceOf<T>) {
-		T::Currency::set_balance(&self.account_id, balance);
+	pub fn set_balance(&self, value: impl Into<BalanceWithDust<BalanceOf<T>>>) {
+		let (value, dust) = value.into().deconstruct();
+		T::Currency::set_balance(&self.account_id, value);
+		crate::AccountInfoOf::<T>::mutate(&self.address, |account| {
+			account.as_mut().map(|a| a.dust = dust);
+		});
 	}
 
 	/// Returns `true` iff all storage entries related to code storage exist.
