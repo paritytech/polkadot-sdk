27b62f7cde98f779acb12d37db69d6733d570700
pgherveou <pgherveou@gmail.com>
2025-07-24 21:18:54 +0000

    add skeleton for fixtures

--- substrate/frame/revive/fixtures/contracts/BlockInfo.sol
+++ substrate/frame/revive/fixtures/contracts/BlockInfo.sol
@@ -12,4 +12,32 @@ contract BlockInfo {
 	function blockNumber() public view returns (uint) {
 		return block.number;
 	}
+
+	function coinbase() public view returns (address) {
+		return block.coinbase;
+	}
+
+	function timestamp() public view returns (uint) {
+		return block.timestamp;
+	}
+
+	function difficulty() public view returns (uint) {
+		return block.difficulty;
+	}
+
+	function gaslimit() public view returns (uint) {
+		return block.gaslimit;
+	}
+
+	function chainid() public view returns (uint) {
+		return block.chainid;
+	}
+
+	function basefee() public view returns (uint) {
+		return block.basefee;
+	}
+
+	function blobBasefee() public view returns (uint) {
+		return block.blobbasefee;
+	}
 }
