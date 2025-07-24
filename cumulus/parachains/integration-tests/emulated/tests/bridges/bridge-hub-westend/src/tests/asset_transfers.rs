5302e308456a362fcd1557d89969cda83738f7f9
Raymond Cheung <178801527+raymondkfcheung@users.noreply.github.com>
2025-07-24 13:16:57 +0100

    Fix import

--- cumulus/parachains/integration-tests/emulated/tests/bridges/bridge-hub-westend/src/tests/asset_transfers.rs
+++ cumulus/parachains/integration-tests/emulated/tests/bridges/bridge-hub-westend/src/tests/asset_transfers.rs
@@ -19,7 +19,6 @@ use emulated_integration_tests_common::{
 	xcm_helpers::{find_all_mq_processed_ids, find_mq_processed_id, find_xcm_sent_message_id},
 	xcm_simulator::helpers::TopicIdTracker,
 };
-use sp_runtime::testing::H256;
 use xcm::latest::AssetTransferFilter;
 
 fn send_assets_over_bridge<F: FnOnce()>(send_fn: F) {
