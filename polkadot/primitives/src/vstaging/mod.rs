fca77ef362c459ecb2aee65e7b0eda12317e19eb
Bastian Köcher <info@kchr.de>
2025-07-30 23:45:54 +0200

    CoreIndexMismatch: Include more information in the error

--- polkadot/primitives/src/vstaging/mod.rs
+++ polkadot/primitives/src/vstaging/mod.rs
@@ -594,9 +594,14 @@ pub enum CommittedCandidateReceiptError {
 	/// The core index in commitments doesn't match the one in descriptor
 	#[cfg_attr(
 		feature = "std",
-		error("The core index in commitments doesn't match the one in descriptor")
+		error("The core index in commitments ({commitments:?}) doesn't match the one in descriptor ({descriptor:?})")
 	)]
-	CoreIndexMismatch,
+	CoreIndexMismatch {
+		/// The core index as found in the descriptor.
+		descriptor: CoreIndex,
+		/// The core index as found in the commitments.
+		commitments: CoreIndex,
+	},
 	/// The core selector or claim queue offset is invalid.
 	#[cfg_attr(feature = "std", error("The core selector or claim queue offset is invalid"))]
 	InvalidSelectedCore,
@@ -787,7 +792,10 @@ impl<H: Copy> CommittedCandidateReceiptV2<H> {
 			.copied()?;
 
 		if core_index != descriptor_core_index {
-			return Err(CommittedCandidateReceiptError::CoreIndexMismatch)
+			return Err(CommittedCandidateReceiptError::CoreIndexMismatch {
+				descriptor: descriptor_core_index,
+				commitments: core_index,
+			})
 		}
 
 		Ok(())
@@ -1368,7 +1376,10 @@ mod candidate_receipt_tests {
 			new_ccr.descriptor.set_core_index(CoreIndex(1));
 			assert_eq!(
 				new_ccr.parse_ump_signals(&cq),
-				Err(CommittedCandidateReceiptError::CoreIndexMismatch)
+				Err(CommittedCandidateReceiptError::CoreIndexMismatch {
+					descriptor: CoreIndex(1),
+					commitments: CoreIndex(0),
+				})
 			);
 		}
 
