83dd8e35b5e7c07410ecaf7dcb26bacc5cb0e559
christopher k <30932534+EleisonC@users.noreply.github.com>
2025-07-31 13:31:41 +0300

    Update XCM benchmarks delivery to sibling parachains not parent/relay chain (#7321)
    ### Title: Update XCM benchmarks for sibling parachain delivery (closes
    #7211)
    
    ### Description:
    This PR updates XCM benchmarking configurations for testnet system
    parachains to reflect delivery to sibling parachains instead of the
    Parent relay chain.
    
    Integration:
    
    Replaced ToParentDeliveryHelper with ToParachainDeliveryHelper. 
    
    Updated benchmark destinations
    
    ---------
    
    Co-authored-by: Branislav Kontur <bkontur@gmail.com>
    Co-authored-by: cmd[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Karol Kokoszka <karol.k91@gmail.com>
    Co-authored-by: Karol Kokoszka <karol@parity.io>

--- cumulus/parachains/runtimes/assets/asset-hub-westend/src/lib.rs
+++ cumulus/parachains/runtimes/assets/asset-hub-westend/src/lib.rs
@@ -1584,7 +1584,7 @@ impl
 	fn create_asset_id_parameter(
 		seed: u32,
 	) -> (cumulus_primitives_core::Location, cumulus_primitives_core::Location) {
-		// Use a different parachain' foreign assets pallet so that the asset is indeed foreign.
+		// Use a different parachain foreign assets pallet so that the asset is indeed foreign.
 		let asset_id = cumulus_primitives_core::Location::new(
 			1,
 			[
@@ -2177,7 +2177,6 @@ pallet_revive::impl_runtime_apis_plus_revive!(
 			use frame_benchmarking::{BenchmarkBatch, BenchmarkError};
 			use frame_support::assert_ok;
 			use sp_storage::TrackedStorageKey;
-
 			use frame_system_benchmarking::Pallet as SystemBench;
 			use frame_system_benchmarking::extensions::Pallet as SystemExtensionsBench;
 			impl frame_system_benchmarking::Config for Runtime {
@@ -2192,35 +2191,39 @@ pallet_revive::impl_runtime_apis_plus_revive!(
 			}
 
 			use cumulus_pallet_session_benchmarking::Pallet as SessionBench;
-			impl cumulus_pallet_session_benchmarking::Config for Runtime {}
+			use xcm_config::{MaxAssetsIntoHolding, WestendLocation};
 
+			impl cumulus_pallet_session_benchmarking::Config for Runtime {}
+			use testnet_parachains_constants::westend::locations::{PeopleParaId, PeopleLocation};
 			parameter_types! {
 				pub ExistentialDepositAsset: Option<Asset> = Some((
 					WestendLocation::get(),
 					ExistentialDeposit::get()
 				).into());
-				pub const RandomParaId: ParaId = ParaId::new(43211234);
+
+				pub RandomParaId: ParaId = ParaId::new(43211234);
 			}
 
 			use pallet_xcm::benchmarking::Pallet as PalletXcmExtrinsicsBenchmark;
 			impl pallet_xcm::benchmarking::Config for Runtime {
 				type DeliveryHelper = (
-					cumulus_primitives_utility::ToParentDeliveryHelper<
-						xcm_config::XcmConfig,
-						ExistentialDepositAsset,
-						xcm_config::PriceForParentDelivery,
-					>,
-					polkadot_runtime_common::xcm_sender::ToParachainDeliveryHelper<
+				polkadot_runtime_common::xcm_sender::ToParachainDeliveryHelper<
 						xcm_config::XcmConfig,
 						ExistentialDepositAsset,
 						PriceForSiblingParachainDelivery,
 						RandomParaId,
-						ParachainSystem,
-					>
-				);
+						ParachainSystem
+					>,
+				polkadot_runtime_common::xcm_sender::ToParachainDeliveryHelper<
+						xcm_config::XcmConfig,
+						ExistentialDepositAsset,
+						PriceForSiblingParachainDelivery,
+						PeopleParaId,
+						ParachainSystem
+					>);
 
 				fn reachable_dest() -> Option<Location> {
-					Some(Parent.into())
+					Some(PeopleLocation::get())
 				}
 
 				fn teleportable_asset_and_dest() -> Option<(Asset, Location)> {
@@ -2228,19 +2231,15 @@ pallet_revive::impl_runtime_apis_plus_revive!(
 					Some((
 						Asset {
 							fun: Fungible(ExistentialDeposit::get()),
-							id: AssetId(Parent.into())
+							id: AssetId(WestendLocation::get())
 						},
-						Parent.into(),
+						PeopleLocation::get(),
 					))
 				}
 
 				fn reserve_transferable_asset_and_dest() -> Option<(Asset, Location)> {
 					Some((
-						Asset {
-							fun: Fungible(ExistentialDeposit::get()),
-							id: AssetId(Parent.into())
-						},
-						// AH can reserve transfer native token to some random parachain.
+						Asset { fun: Fungible(ExistentialDeposit::get()), id: AssetId(Parent.into()) },
 						ParentThen(Parachain(RandomParaId::get().into()).into()).into(),
 					))
 				}
@@ -2250,10 +2249,10 @@ pallet_revive::impl_runtime_apis_plus_revive!(
 					// Transfer to Relay some local AH asset (local-reserve-transfer) while paying
 					// fees using teleported native token.
 					// (We don't care that Relay doesn't accept incoming unknown AH local asset)
-					let dest = Parent.into();
+					let dest: Location = PeopleLocation::get();
 
 					let fee_amount = EXISTENTIAL_DEPOSIT;
-					let fee_asset: Asset = (Location::parent(), fee_amount).into();
+					let fee_asset: Asset = (WestendLocation::get(), fee_amount).into();
 
 					let who = frame_benchmarking::whitelisted_caller();
 					// Give some multiple of the existential deposit
@@ -2355,19 +2354,20 @@ pallet_revive::impl_runtime_apis_plus_revive!(
 				}
 			}
 
-			use xcm_config::{MaxAssetsIntoHolding, WestendLocation};
 			use pallet_xcm_benchmarks::asset_instance_from;
 
 			impl pallet_xcm_benchmarks::Config for Runtime {
 				type XcmConfig = xcm_config::XcmConfig;
 				type AccountIdConverter = xcm_config::LocationToAccountId;
-				type DeliveryHelper = cumulus_primitives_utility::ToParentDeliveryHelper<
-					xcm_config::XcmConfig,
-					ExistentialDepositAsset,
-					xcm_config::PriceForParentDelivery,
+				type DeliveryHelper = polkadot_runtime_common::xcm_sender::ToParachainDeliveryHelper<
+										xcm_config::XcmConfig,
+										ExistentialDepositAsset,
+										PriceForSiblingParachainDelivery,
+										PeopleParaId,
+										ParachainSystem
 				>;
 				fn valid_destination() -> Result<Location, BenchmarkError> {
-					Ok(WestendLocation::get())
+					Ok(PeopleLocation::get())
 				}
 				fn worst_case_holding(depositable_count: u32) -> XcmAssets {
 					// A mix of fungible, non-fungible, and concrete assets.
@@ -2393,11 +2393,10 @@ pallet_revive::impl_runtime_apis_plus_revive!(
 			}
 
 			parameter_types! {
-				pub const TrustedTeleporter: Option<(Location, Asset)> = Some((
-					WestendLocation::get(),
+				pub TrustedTeleporter: Option<(Location, Asset)> = Some((
+					PeopleLocation::get(),
 					Asset { fun: Fungible(UNITS), id: AssetId(WestendLocation::get()) },
 				));
-				pub const CheckedAccount: Option<(AccountId, xcm_builder::MintLocation)> = None;
 				// AssetHubWestend trusts AssetHubRococo as reserve for ROCs
 				pub TrustedReserve: Option<(Location, Asset)> = Some(
 					(
@@ -2410,7 +2409,7 @@ pallet_revive::impl_runtime_apis_plus_revive!(
 			impl pallet_xcm_benchmarks::fungible::Config for Runtime {
 				type TransactAsset = Balances;
 
-				type CheckedAccount = CheckedAccount;
+				type CheckedAccount = xcm_config::TeleportTracking;
 				type TrustedTeleporter = TrustedTeleporter;
 				type TrustedReserve = TrustedReserve;
 
@@ -2504,15 +2503,18 @@ pallet_revive::impl_runtime_apis_plus_revive!(
 				}
 
 				fn transact_origin_and_runtime_call() -> Result<(Location, RuntimeCall), BenchmarkError> {
-					Ok((WestendLocation::get(), frame_system::Call::remark_with_event { remark: vec![] }.into()))
+					Ok((
+						PeopleLocation::get(),
+						frame_system::Call::remark_with_event { remark: vec![] }.into()
+					))
 				}
 
 				fn subscribe_origin() -> Result<Location, BenchmarkError> {
-					Ok(WestendLocation::get())
+					Ok(PeopleLocation::get())
 				}
 
 				fn claimable_asset() -> Result<(Location, Location, XcmAssets), BenchmarkError> {
-					let origin = WestendLocation::get();
+					let origin = PeopleLocation::get();
 					let assets: XcmAssets = (AssetId(WestendLocation::get()), 1_000 * UNITS).into();
 					let ticket = Location { parents: 0, interior: Here };
 					Ok((origin, ticket, assets))
