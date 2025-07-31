07132ebbf4ad99dd0c61698c5ca1783a486be311
Raymond Cheung <178801527+raymondkfcheung@users.noreply.github.com>
2025-07-31 11:05:10 +0100

    Update tracing

--- bridges/primitives/runtime/src/lib.rs
+++ bridges/primitives/runtime/src/lib.rs
@@ -415,11 +415,11 @@ pub trait OwnedBridgeModule<T: frame_system::Config> {
 		match maybe_owner {
 			Some(owner) => {
 				Self::OwnerStorage::put(&owner);
-				tracing::info!(target: COMMON_LOG_TARGET, ?owner, "Setting pallet.");
+				tracing::info!(target: COMMON_LOG_TARGET, module=%Self::LOG_TARGET, ?owner, "Setting pallet.");
 			},
 			None => {
 				Self::OwnerStorage::kill();
-				tracing::info!(target: COMMON_LOG_TARGET, "Removed Owner of pallet.");
+				tracing::info!(target: COMMON_LOG_TARGET, module=%Self::LOG_TARGET, "Removed Owner of pallet.");
 			},
 		}
 
@@ -433,7 +433,7 @@ pub trait OwnedBridgeModule<T: frame_system::Config> {
 	) -> DispatchResult {
 		Self::ensure_owner_or_root(origin)?;
 		Self::OperatingModeStorage::put(operating_mode);
-		tracing::info!(target: COMMON_LOG_TARGET, ?operating_mode, "Setting operating mode.");
+		tracing::info!(target: COMMON_LOG_TARGET, module=%Self::LOG_TARGET, ?operating_mode, "Setting operating mode.");
 		Ok(())
 	}
 
