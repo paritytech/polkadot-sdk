39fd2560e82ea9890a3c3974f75a45b6f8a2a154
Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
2025-07-31 15:29:50 +0200

    [AHM] Make stuff public and derive (#9384)
    Make some stuff public and derive traits. Also removes one silently
    truncating constructor from ParaId.
    
    ---------
    
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>

--- substrate/frame/staking-async/src/lib.rs
+++ substrate/frame/staking-async/src/lib.rs
@@ -429,7 +429,9 @@ impl<AccountId, Balance: HasCompact + Copy + AtLeast32BitUnsigned + codec::MaxEn
 
 /// A pending slash record. The value of the slash has been computed but not applied yet,
 /// rather deferred for several eras.
-#[derive(Encode, Decode, RuntimeDebugNoBound, TypeInfo, MaxEncodedLen, PartialEqNoBound)]
+#[derive(
+	Encode, Decode, RuntimeDebugNoBound, TypeInfo, MaxEncodedLen, PartialEqNoBound, EqNoBound,
+)]
 #[scale_info(skip_type_params(T))]
 pub struct UnappliedSlash<T: Config> {
 	/// The stash ID of the offending validator.
