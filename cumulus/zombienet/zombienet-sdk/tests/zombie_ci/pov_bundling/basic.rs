93ce4621395dfa40a70b77f5178f2131bae7bf10
Bastian Köcher <info@kchr.de>
2025-08-01 22:10:14 +0200

    Okay

--- cumulus/zombienet/zombienet-sdk/tests/zombie_ci/pov_bundling/basic.rs
+++ cumulus/zombienet/zombienet-sdk/tests/zombie_ci/pov_bundling/basic.rs
@@ -65,8 +65,8 @@ async fn pov_bundling_basic() -> Result<(), anyhow::Error> {
 		[(ParaId::from(PARA_ID), (para_client.clone(), 48..73))],
 	)
 	.await?;
-	// 5 relay chain blocks
-	assert_finality_lag(&para_client, 60).await?;
+	// 6 relay chain blocks
+	assert_finality_lag(&para_client, 72).await?;
 
 	let assign_cores_call = create_assign_core_call(&[(0, PARA_ID), (1, PARA_ID)]);
 
@@ -96,7 +96,7 @@ async fn pov_bundling_basic() -> Result<(), anyhow::Error> {
 		[(ParaId::from(PARA_ID), (para_client.clone(), 48..73))],
 	)
 	.await?;
-	assert_finality_lag(&para_client, 60).await?;
+	assert_finality_lag(&para_client, 72).await?;
 
 	let assign_cores_call = create_assign_core_call(&[(2, PARA_ID), (3, PARA_ID), (4, PARA_ID)]);
 	// Assign two extra cores to each parachain.
