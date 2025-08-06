30a5f041748c147ba814f098390f8dbb6679eaef
Alexandru Vasile <alexandru.vasile@parity.io>
2025-08-06 16:58:13 +0000

    substrate/revive: Use StateRoot as pallet_revive::DeterministicStateRoot<Self::Version>
    Signed-off-by: Alexandru Vasile <alexandru.vasile@parity.io>

--- substrate/bin/node/runtime/src/lib.rs
+++ substrate/bin/node/runtime/src/lib.rs
@@ -1469,6 +1469,7 @@ impl pallet_contracts::Config for Runtime {
 
 impl pallet_revive::Config for Runtime {
 	type Time = Timestamp;
+	type StateRoot = pallet_revive::DeterministicStateRoot<Self::Version>;
 	type Currency = Balances;
 	type RuntimeEvent = RuntimeEvent;
 	type RuntimeCall = RuntimeCall;
