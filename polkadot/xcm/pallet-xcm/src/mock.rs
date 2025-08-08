8193881f903c5827954dadfe074e866fd58ff38f
Alexandru Vasile <alexandru.vasile@parity.io>
2025-08-08 09:25:31 +0000

    xcm/mock: Add tx payment pallet
    Signed-off-by: Alexandru Vasile <alexandru.vasile@parity.io>

--- polkadot/xcm/pallet-xcm/src/mock.rs
+++ polkadot/xcm/pallet-xcm/src/mock.rs
@@ -21,7 +21,7 @@ use frame_support::{
 		fungible::HoldConsideration, AsEnsureOriginWithArg, ConstU128, ConstU32, Contains, Equals,
 		Everything, EverythingBut, Footprint, Nothing,
 	},
-	weights::Weight,
+	weights::{FixedFee, IdentityFee, Weight},
 };
 use frame_system::EnsureRoot;
 use polkadot_parachain_primitives::primitives::Id as ParaId;
@@ -162,6 +162,7 @@ construct_runtime!(
 		TestNotifier: pallet_test_notifier,
 		Revive: pallet_revive,
 		Timestamp: pallet_timestamp,
+		TransactionPayment: pallet_transaction_payment,
 	}
 );
 
@@ -615,6 +616,18 @@ impl pallet_test_notifier::Config for Test {
 	type RuntimeEvent = RuntimeEvent;
 	type RuntimeOrigin = RuntimeOrigin;
 	type RuntimeCall = RuntimeCall;
+}
+
+parameter_types! {
+	pub FeeMultiplier: pallet_transaction_payment::Multiplier = pallet_transaction_payment::Multiplier::one();
+}
+
+#[derive_impl(pallet_transaction_payment::config_preludes::TestDefaultConfig)]
+impl pallet_transaction_payment::Config for Test {
+	type OnChargeTransaction = pallet_transaction_payment::FungibleAdapter<Balances, ()>;
+	type WeightToFee = IdentityFee<<Self as pallet_balances::Config>::Balance>;
+	type LengthToFee = FixedFee<100, <Self as pallet_balances::Config>::Balance>;
+	type FeeMultiplierUpdate = pallet_transaction_payment::ConstFeeMultiplier<FeeMultiplier>;
 }
 
 #[cfg(feature = "runtime-benchmarks")]
