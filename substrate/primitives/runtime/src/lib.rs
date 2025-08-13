29a0c4a746a61a690df06822c52266ef69bf6b64
sistemd <enntheprogrammer@gmail.com>
2025-08-13 16:50:47 +0200

    store headers and justifications during warp sync (#9424)
    Closes https://github.com/paritytech/polkadot-sdk/issues/2738.
    
    Still need to add tests for this - but I think the easiest way might be
    after the zombienet tests are converted to Rust, in the warp sync test
    maybe we can just request the headers (and justifications?) from
    JSON-RPC? Though I'm not sure there is an API for the justifications.
    But in any case we can in theory make a P2P justifications request as
    well and the node should be able to respond. Let me know if anybody has
    some better ideas.
    
    ---------
    
    Signed-off-by: sistemd <enntheprogrammer@gmail.com>
    Co-authored-by: cmd[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Iulian Barbu <14218860+iulianbarbu@users.noreply.github.com>

--- substrate/primitives/runtime/src/lib.rs
+++ substrate/primitives/runtime/src/lib.rs
@@ -156,10 +156,15 @@ pub type EncodedJustification = Vec<u8>;
 /// Collection of justifications for a given block, multiple justifications may
 /// be provided by different consensus engines for the same block.
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
-#[derive(Debug, Clone, PartialEq, Eq, Encode, Decode)]
+#[derive(Default, Debug, Clone, PartialEq, Eq, Encode, Decode)]
 pub struct Justifications(Vec<Justification>);
 
 impl Justifications {
+	/// Create a new `Justifications` instance with the given justifications.
+	pub fn new(justifications: Vec<Justification>) -> Self {
+		Self(justifications)
+	}
+
 	/// Return an iterator over the justifications.
 	pub fn iter(&self) -> impl Iterator<Item = &Justification> {
 		self.0.iter()
