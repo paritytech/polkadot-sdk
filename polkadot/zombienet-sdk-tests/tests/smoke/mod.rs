869d4e50d9bf79acf8c144d0df71b2e92b77c84d
Andrei Eres <eresav@me.com>
2025-08-14 09:20:18 +0200

    Temporary update zombinet test for debugging

--- polkadot/zombienet-sdk-tests/tests/smoke/mod.rs
+++ polkadot/zombienet-sdk-tests/tests/smoke/mod.rs
@@ -1,5 +1,11 @@
 // Copyright (C) Parity Technologies (UK) Ltd.
 // SPDX-License-Identifier: Apache-2.0
 
-#[cfg(feature = "zombie-metadata")]
+// TODO: It sets right feature name, because we don't use `zombie-metadata` anymore.
+// But better to delete the change and fix it in the other PR.
+// I use it here only to debug the collator metrics during PR development spawning zombinet test
+// ZOMBIE_PROVIDER=native cargo test -p polkadot-zombienet-sdk-tests \
+// --features zombie-ci smoke::coretime_revenue::coretime_revenue_test \
+// -- --exact --nocapture
+#[cfg(feature = "zombie-ci")]
 mod coretime_revenue;
