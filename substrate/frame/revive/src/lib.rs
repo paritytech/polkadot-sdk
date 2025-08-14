fd417de617f303b84a0cac1972cf5d7090000d2f
Pavlo Khrystenko <45178695+pkhry@users.noreply.github.com>
2025-08-14 14:06:16 +0200

    [pallet-revive] expose `exec::Key` (#9482)
    # Description
    
    This is a fix for the fact that `exec::Key` is exposed from within
    `pallet_revive::tracing::Tracing` interface, but not from the crate
    itself making custom tracers effectively unimplementable outside said
    crate.
    
    In my case it's useful for implementing custom tracers for integration
    with `foundry`
    
    ## Integration
    
    Requires no downstream changes
    
    ## Review Notes
    
    This is a fix for the fact that `exec::Key` is exposed from within
    `pallet_revive::tracing::Tracing` interface, but not from the crate
    itself making custom tracers effectively unimplementable outside said
    crate.
    
    see here for one of the methods: [`exec::Key` exposed to the
    implementor, despite not being exported by the
    `crate`](https://github.com/paritytech/polkadot-sdk/blob/pkhry/expose_key_pallet_revive/substrate/frame/revive/src/tracing.rs#L68)
    
    ---------
    
    Co-authored-by: cmd[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- substrate/frame/revive/src/lib.rs
+++ substrate/frame/revive/src/lib.rs
@@ -48,7 +48,7 @@ use crate::{
 		runtime::GAS_PRICE, CallTracer, GasEncoder, GenericTransaction, PrestateTracer, Trace,
 		Tracer, TracerType, TYPE_EIP1559,
 	},
-	exec::{AccountIdOf, ExecError, Executable, Key, Stack as ExecStack},
+	exec::{AccountIdOf, ExecError, Executable, Stack as ExecStack},
 	gas::GasMeter,
 	storage::{
 		meter::Meter as StorageMeter, AccountInfo, AccountType, ContractInfo, DeletionQueueManager,
@@ -89,7 +89,7 @@ pub use crate::{
 	address::{
 		create1, create2, is_eth_derived, AccountId32Mapper, AddressMapper, TestAccountMapper,
 	},
-	exec::{MomentOf, Origin},
+	exec::{Key, MomentOf, Origin},
 	pallet::*,
 };
 pub use codec;
