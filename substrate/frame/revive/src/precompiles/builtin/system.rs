082f6fa90dd1365de93cc74b14b3e6161c7f63cf
Michael Müller <mich@elmueller.net>
2025-08-18 20:40:41 +0200

    BACKPORT-CONFLICT

--- substrate/frame/revive/src/precompiles/builtin/system.rs
+++ substrate/frame/revive/src/precompiles/builtin/system.rs
@@ -31,6 +31,21 @@ sol! {
 	interface ISystem {
 		/// Computes the BLAKE2 256-bit hash on the given input.
 		function hashBlake256(bytes memory input) external pure returns (bytes32 digest);
+<<<<<<< HEAD
+=======
+		/// Computes the BLAKE2 128-bit hash on the given input.
+		function hashBlake128(bytes memory input) external pure returns (bytes32 digest);
+		/// Retrieve the account id for a specified `H160` address.
+		///
+		/// Calling this function on a native `H160` chain (`type AccountId = H160`)
+		/// does not make sense, as it would just return the `address` that it was
+		/// called with.
+		///
+		/// # Note
+		///
+		/// If no mapping exists for `addr`, the fallback account id will be returned.
+		function toAccountId(address input) external view returns (bytes memory account_id);
+>>>>>>> 7ede4fd ([pallet-revive] Move `blake2_128` host function to `System` pre-compile (#9454))
 	}
 }
 
@@ -53,6 +68,22 @@ impl<T: Config> BuiltinPrecompile for System<T> {
 				let output = sp_io::hashing::blake2_256(input.as_bytes_ref());
 				Ok(output.to_vec())
 			},
+<<<<<<< HEAD
+=======
+			ISystemCalls::hashBlake128(ISystem::hashBlake128Call { input }) => {
+				env.gas_meter_mut().charge(RuntimeCosts::HashBlake128(input.len() as u32))?;
+				let output = sp_io::hashing::blake2_128(input.as_bytes_ref());
+				Ok(output.to_vec())
+			},
+			ISystemCalls::toAccountId(ISystem::toAccountIdCall { input }) => {
+				use crate::address::AddressMapper;
+				use codec::Encode;
+				env.gas_meter_mut().charge(RuntimeCosts::ToAccountId)?;
+				let account_id =
+					T::AddressMapper::to_account_id(&crate::H160::from_slice(input.as_slice()));
+				Ok(account_id.encode())
+			},
+>>>>>>> 7ede4fd ([pallet-revive] Move `blake2_128` host function to `System` pre-compile (#9454))
 		}
 	}
 }
@@ -65,5 +96,70 @@ mod tests {
 	#[test]
 	fn test_system_precompile() {
 		run_test_vectors::<System<Test>>(include_str!("testdata/900-blake2_256.json"));
+<<<<<<< HEAD
+=======
+		run_test_vectors::<System<Test>>(include_str!("testdata/900-blake2_128.json"));
+		run_test_vectors::<System<Test>>(include_str!("testdata/900-to_account_id.json"));
+	}
+
+	#[test]
+	fn test_system_precompile_unmapped_account() {
+		ExtBuilder::default().build().execute_with(|| {
+			// given
+			let mut call_setup = CallSetup::<Test>::default();
+			let (mut ext, _) = call_setup.ext();
+			let unmapped_address = H160::zero();
+
+			// when
+			let input = ISystem::ISystemCalls::toAccountId(ISystem::toAccountIdCall {
+				input: unmapped_address.0.into(),
+			});
+			let expected_fallback_account_id =
+				<System<Test>>::call(&<System<Test>>::MATCHER.base_address(), &input, &mut ext)
+					.unwrap();
+
+			// then
+			assert_eq!(
+				expected_fallback_account_id[20..32],
+				[0xEE; 12],
+				"no fallback suffix found where one should be"
+			);
+		})
+	}
+
+	#[test]
+	fn test_system_precompile_mapped_account() {
+		use crate::test_utils::EVE;
+		ExtBuilder::default().build().execute_with(|| {
+			// given
+			let mapped_address = {
+				<Test as pallet::Config>::Currency::set_balance(&EVE, caller_funding::<Test>());
+				let _ = <Test as pallet::Config>::AddressMapper::map(&EVE);
+				<Test as pallet::Config>::AddressMapper::to_address(&EVE)
+			};
+
+			let mut call_setup = CallSetup::<Test>::default();
+			let (mut ext, _) = call_setup.ext();
+
+			// when
+			let input = ISystem::ISystemCalls::toAccountId(ISystem::toAccountIdCall {
+				input: mapped_address.0.into(),
+			});
+			let data =
+				<System<Test>>::call(&<System<Test>>::MATCHER.base_address(), &input, &mut ext)
+					.unwrap();
+
+			// then
+			assert_ne!(
+				data.as_slice()[20..32],
+				[0xEE; 12],
+				"fallback suffix found where none should be"
+			);
+			assert_eq!(
+				<Test as frame_system::Config>::AccountId::decode(&mut data.as_slice()),
+				Ok(EVE),
+			);
+		})
+>>>>>>> 7ede4fd ([pallet-revive] Move `blake2_128` host function to `System` pre-compile (#9454))
 	}
 }
