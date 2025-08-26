b58ad5bc223968dab8a1f7e74a0e7f3e893ff55d
Dom <polkadotdom@gmail.com>
2025-08-26 12:16:46 -0400

    Update docs

--- substrate/primitives/runtime/src/curve.rs
+++ substrate/primitives/runtime/src/curve.rs
@@ -15,7 +15,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//! Provides various curves and their evaluations.
+//! Provides utilities for various curves.
 
 use crate::{
 	traits::{AtLeast32BitUnsigned, SaturatedConversion},
