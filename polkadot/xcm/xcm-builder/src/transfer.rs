180ca2393091a9cd98dec4f21644d9e1b0e40dff
Christian Langenbacher <clangenb+gh@protonmail.ch>
2025-08-27 16:09:20 +0200

    Fix licence

--- polkadot/xcm/xcm-builder/src/transfer.rs
+++ polkadot/xcm/xcm-builder/src/transfer.rs
@@ -1,18 +1,18 @@
-// Copyright (c) 2023 Encointer Association
-// This file is part of Encointer
-//
-// Encointer is free software: you can redistribute it and/or modify
+// Copyright (C) Parity Technologies (UK) Ltd.
+// This file is part of Polkadot.
+
+// Polkadot is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-//
-// Encointer is distributed in the hope that it will be useful,
+
+// Polkadot is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-//
+
 // You should have received a copy of the GNU General Public License
-// along with Encointer.  If not, see <http://www.gnu.org/licenses/>.
+// along with Polkadot.  If not, see <http://www.gnu.org/licenses/>.
 
 //! `TransferOverXcm` struct for paying through XCM and getting the status back.
 
