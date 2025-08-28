8b8d904ece034327259920be433c6ff4283c4004
Javier Viola <javier@parity.io>
2025-08-28 13:21:54 +0200

    use para builder

--- cumulus/zombienet/zombienet-sdk-helpers/src/lib.rs
+++ cumulus/zombienet/zombienet-sdk-helpers/src/lib.rs
@@ -14,9 +14,7 @@ use tokio::{
 use zombienet_sdk::subxt::{
 	self,
 	blocks::Block,
-	config::{
-		substrate::DigestItem, transaction_extensions::CheckMortalityParams, ExtrinsicParams,
-	},
+	config::{polkadot::PolkadotExtrinsicParamsBuilder, substrate::DigestItem, ExtrinsicParams},
 	dynamic::Value,
 	events::Events,
 	ext::scale_value::value,
@@ -305,11 +303,7 @@ pub async fn submit_extrinsic_and_wait_for_finalization_success<S: Signer<Polkad
 	call: &DynamicPayload,
 	signer: &S,
 ) -> Result<(), anyhow::Error> {
-	let mut extensions: <<PolkadotConfig as Config>::ExtrinsicParams as ExtrinsicParams<
-		PolkadotConfig,
-	>>::Params = Default::default();
-
-	extensions.4 = CheckMortalityParams::<PolkadotConfig>::immortal();
+	let extensions = PolkadotExtrinsicParamsBuilder::new().immortal().build();
 
 	let mut tx = client
 		.tx()
