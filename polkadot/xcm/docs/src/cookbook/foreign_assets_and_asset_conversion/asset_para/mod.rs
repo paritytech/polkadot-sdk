c732fda707164081f892e46cf964d8185a2c2157
Christian Langenbacher <clangenb+gh@protonmail.ch>
2025-08-29 10:25:14 +0200

    Revert "wip with emulator"
    This reverts commit f53ffb64cfcf579326219c44075e4a2c6f6ef3c6.

--- polkadot/xcm/docs/src/cookbook/foreign_assets_and_asset_conversion/asset_para/mod.rs
+++ polkadot/xcm/docs/src/cookbook/foreign_assets_and_asset_conversion/asset_para/mod.rs
@@ -17,17 +17,9 @@
 
 //! # Runtime
 
-use cumulus_pallet_parachain_system::consensus_hook::RequireParentIncluded;
-use cumulus_pallet_parachain_system::RelayNumberStrictlyIncreases;
-use frame::{
-	deps::{frame_support, frame_system, sp_core},
-	runtime::prelude::*,
-	traits::IdentityLookup,
-};
-use parachains_common::message_queue::NarrowOriginToSibling;
+use frame::{deps::frame_system, runtime::prelude::*, traits::IdentityLookup};
 use xcm_executor::XcmExecutor;
 use xcm_simulator::mock_message_queue;
-use cumulus_primitives_core::AggregateMessageOrigin;
 
 mod xcm_config;
 
@@ -42,18 +34,13 @@ pub type Balance = u128;
 construct_runtime! {
 	pub struct Runtime {
 		System: frame_system,
-		ParachainSystem: cumulus_pallet_parachain_system,
 		ParachainInfo: parachain_info,
+		MessageQueue: mock_message_queue,
 		ForeignAssets: pallet_assets::<Instance1>,
 		PoolAssets: pallet_assets::<Instance2>,
 		AssetConversion: pallet_asset_conversion,
 		Balances: pallet_balances,
-
-		// Xcm Helpers
-		XcmpQueue: cumulus_pallet_xcmp_queue,
 		XcmPallet: pallet_xcm,
-		CumulusXcm: cumulus_pallet_xcm,
-		MessageQueue: pallet_message_queue
 	}
 }
 
@@ -63,7 +50,11 @@ impl frame_system::Config for Runtime {
 	type AccountId = AccountId;
 	type Lookup = IdentityLookup<AccountId>;
 	type AccountData = pallet_balances::AccountData<Balance>;
-	type OnSetCode = cumulus_pallet_parachain_system::ParachainSetCode<Self>;
+}
+
+impl mock_message_queue::Config for Runtime {
+	type RuntimeEvent = RuntimeEvent;
+	type XcmExecutor = XcmExecutor<XcmConfig>;
 }
 
 pub const UNITS: Balance = 10_000_000_000;
@@ -80,42 +71,3 @@ impl pallet_balances::Config for Runtime {
 }
 
 impl parachain_info::Config for Runtime {}
-
-parameter_types! {
-	pub const RelayOrigin: AggregateMessageOrigin = AggregateMessageOrigin::Parent;
-}
-
-impl cumulus_pallet_parachain_system::Config for Runtime {
-	type WeightInfo = ();
-	type RuntimeEvent = RuntimeEvent;
-	type OnSystemEvent = ();
-	type SelfParaId = parachain_info::Pallet<Runtime>;
-	type DmpQueue =
-		frame_support::traits::EnqueueWithOrigin<MessageQueue, RelayOrigin>;
-	type ReservedDmpWeight = ();
-	type OutboundXcmpMessageSource = XcmpQueue;
-	type XcmpMessageHandler = XcmpQueue;
-	type ReservedXcmpWeight = ();
-	type CheckAssociatedRelayNumber = RelayNumberStrictlyIncreases;
-	type ConsensusHook = RequireParentIncluded;
-	type SelectCore = cumulus_pallet_parachain_system::DefaultCoreSelector<Runtime>;
-	type RelayParentOffset = ();
-}
-
-impl pallet_message_queue::Config for Runtime {
-	type RuntimeEvent = RuntimeEvent;
-	type WeightInfo = ();
-	type MessageProcessor = xcm_builder::ProcessXcmMessage<
-		AggregateMessageOrigin,
-		xcm_executor::XcmExecutor<xcm_config::XcmConfig>,
-		RuntimeCall,
-	>;
-	type Size = u32;
-	// The XCMP queue pallet is only ever able to handle the `Sibling(ParaId)` origin:
-	type QueueChangeHandler = NarrowOriginToSibling<XcmpQueue>;
-	type QueuePausedQuery = NarrowOriginToSibling<XcmpQueue>;
-	type HeapSize = sp_core::ConstU32<{ 64 * 1024 }>;
-	type MaxStale = sp_core::ConstU32<8>;
-	type ServiceWeight = ();
-	type IdleMaxServiceWeight = ();
-}
