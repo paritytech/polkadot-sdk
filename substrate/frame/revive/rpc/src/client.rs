eb33290e9d3afdd073b80c58358d5df5a0061fda
Javier Viola <363911+pepoviola@users.noreply.github.com>
2025-09-02 16:31:43 +0200

    bump zombienet-sdk and subxt versions (#9587)
    Replace https://github.com/paritytech/polkadot-sdk/pull/9506
    
    ---------
    
    Co-authored-by: Bastian Köcher <git@kchr.de>
    Co-authored-by: Bastian Köcher <info@kchr.de>

--- substrate/frame/revive/rpc/src/client.rs
+++ substrate/frame/revive/rpc/src/client.rs
@@ -47,8 +47,7 @@ use subxt::{
 			RpcClient,
 		},
 	},
-	config::Header,
-	ext::subxt_rpcs::rpc_params,
+	config::{HashFor, Header},
 	Config, OnlineClient,
 };
 use thiserror::Error;
@@ -64,7 +63,7 @@ pub type SubstrateBlockHeader = <SrcChainConfig as Config>::Header;
 pub type SubstrateBlockNumber = <SubstrateBlockHeader as Header>::Number;
 
 /// The substrate block hash type.
-pub type SubstrateBlockHash = <SrcChainConfig as Config>::Hash;
+pub type SubstrateBlockHash = HashFor<SrcChainConfig>;
 
 /// The runtime balance type.
 pub type Balance = u128;
