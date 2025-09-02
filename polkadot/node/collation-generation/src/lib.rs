83aa9ddd4247ecf14edefe692b173ea78685ada5
Dmitry Sinyavin <dmitry.sinyavin@parity.io>
2025-09-02 13:10:52 +0200

    Fix rustdoc

--- polkadot/node/collation-generation/src/lib.rs
+++ polkadot/node/collation-generation/src/lib.rs
@@ -25,8 +25,8 @@
 //!   * Determine if the para is scheduled on any core by fetching the `availability_cores` Runtime
 //!     API.
 //!   * Use the Runtime API subsystem to fetch the full validation data.
-//!   * Invoke the `collator`, and use its outputs to produce a [`CandidateReceiptV2`], signed with
-//!     the configuration's `key`.
+//!   * Invoke the `collator`, and use its outputs to produce a
+//!     [`polkadot_primitives::CandidateReceiptV2`], signed with the configuration's `key`.
 //!   * Dispatch a [`CollatorProtocolMessage::DistributeCollation`]`(receipt, pov)`.
 
 #![deny(missing_docs)]
