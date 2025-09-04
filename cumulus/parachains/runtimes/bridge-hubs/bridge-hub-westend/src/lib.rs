2caeef482a437414c6bed2395a16abe08fccbfbb
Egor_P <egor@parity.io>
2025-09-04 21:10:59 +0200

    [stable2506] Post crates release stable2506-2 (#9649)
    This PR contains regular changes after crates release:
    - crates versions bumps
    - node version bumps
    - spec version bumps
    - prdocs reordering

--- cumulus/parachains/runtimes/bridge-hubs/bridge-hub-westend/src/lib.rs
+++ cumulus/parachains/runtimes/bridge-hubs/bridge-hub-westend/src/lib.rs
@@ -243,7 +243,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: alloc::borrow::Cow::Borrowed("bridge-hub-westend"),
 	impl_name: alloc::borrow::Cow::Borrowed("bridge-hub-westend"),
 	authoring_version: 1,
-	spec_version: 1_019_003,
+	spec_version: 1_019_004,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 6,
