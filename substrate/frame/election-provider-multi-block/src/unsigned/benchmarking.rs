8310c67a658ed3871d616aeed7c3922333e1aa0d
kianenigma <kian@parity.io>
2025-09-08 16:58:40 +0100

    fix

--- substrate/frame/election-provider-multi-block/src/unsigned/benchmarking.rs
+++ substrate/frame/election-provider-multi-block/src/unsigned/benchmarking.rs
@@ -78,7 +78,6 @@ mod benchmarks {
 	}
 
 	#[benchmark(extra, pov_mode = Measured)]
-	#[extra]
 	fn mine_solution(p: Linear<1, { T::Pages::get() }>) -> Result<(), BenchmarkError> {
 		#[cfg(test)]
 		crate::mock::ElectionStart::set(sp_runtime::traits::Bounded::max_value());
