c245a58bc4642003b29426a100c7b102dccd9024
Andrei Sandu <andrei-mihail@parity.io>
2025-09-10 18:46:22 +0300

    fmt
    Signed-off-by: Andrei Sandu <andrei-mihail@parity.io>

--- docs/sdk/src/guides/enable_elastic_scaling.rs
+++ docs/sdk/src/guides/enable_elastic_scaling.rs
@@ -106,9 +106,10 @@
 //!
 //! Example configuration for a 3 core parachain:
 //!  ```ignore
-//!     /// The upper limit of how many parachain blocks are processed by the relay chain per parent. 
-//!     /// Limits the number of blocks authored per slot. This determines the minimum block time 
-//!     /// of the parachain: `RELAY_CHAIN_SLOT_DURATION_MILLIS/BLOCK_PROCESSING_VELOCITY`
+//!     /// The upper limit of how many parachain blocks are processed by the relay chain per
+//!     /// parent. Limits the number of blocks authored per slot. This determines the minimum
+//!     /// block time of the parachain:
+//!     /// `RELAY_CHAIN_SLOT_DURATION_MILLIS/BLOCK_PROCESSING_VELOCITY`
 //!     const BLOCK_PROCESSING_VELOCITY: u32 = 3;
 //!
 //!     /// Maximum number of blocks simultaneously accepted by the Runtime, not yet included
