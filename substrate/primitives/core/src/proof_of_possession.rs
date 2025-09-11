3ada0c19394d6ea289e7fcd31e26312337a02901
Skalman <skalman@riseup.net>
2025-09-11 09:06:48 -0400

    make all comment paragraphs in `proof_of_possession.rs` 100 char wide.

--- substrate/primitives/core/src/proof_of_possession.rs
+++ substrate/primitives/core/src/proof_of_possession.rs
@@ -1,19 +1,16 @@
 // This file is part of Substrate.
 
-// Copyright (C) Parity Technologies (UK) Ltd.
-// SPDX-License-Identifier: Apache-2.0
+// Copyright (C) Parity Technologies (UK) Ltd.  SPDX-License-Identifier: Apache-2.0
 
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+// in compliance with the License.  You may obtain a copy of the License at
 //
 // 	http://www.apache.org/licenses/LICENSE-2.0
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// Unless required by applicable law or agreed to in writing, software distributed under the License
+// is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+// or implied.  See the License for the specific language governing permissions and limitations
+// under the License.
 
 //! Utilities for proving possession of a particular public key
 
@@ -30,32 +27,30 @@ where
 {
 	/// Generate proof of possession.
 	///
-	/// This is usually done by signing the owner's identifier, this is prevent
-	/// front runner to claim ownership of public keys of other entities.
+	/// This is usually done by signing the owner's identifier, this is prevent front runner to
+	/// claim ownership of public keys of other entities.
 	///
-	/// However, for aggregatable signature
-	/// the proof of possession generator is supposed to
-	/// produce a "signature" with unique hash context that should
-	/// never be used in other signatures. While this proves that
-	/// the secret key is known to the prover, it prevents
-	/// malicious actors to trick an honest party to sign an
-	/// unpossessed public key resulting in a rogue key attack (See: Section 4.3 of
-	/// - Ristenpart, T., & Yilek, S. (2007). The power of proofs-of-possession: Securing multiparty
-	///   signatures against rogue-key attacks. In , Annual {{International Conference}} on the
-	///   {{Theory}} and {{Applications}} of {{Cryptographic Techniques} (pp. 228–245). : Springer).
+	/// However, for aggregatable signature the proof of possession generator is supposed to
+	/// produce a "signature" with unique hash context that should never be used in other
+	/// signatures. While this proves that the secret key is known to the prover, it prevents
+	/// malicious actors to trick an honest party to sign an unpossessed public key resulting in
+	/// a rogue key attack (See: Section 4.3 of - Ristenpart, T., & Yilek, S. (2007). The power
+	/// of proofs-of-possession: Securing multiparty signatures against rogue-key attacks. In ,
+	/// Annual {{International Conference}} on the {{Theory}} and {{Applications}} of
+	/// {{Cryptographic Techniques} (pp. 228–245). : Springer).
 	///
 	/// As such, for aggregatable signatures, proof of possession consists of two signatures one
-	/// regular signature signing the owner identity and the second one with unique context which
-	/// signs the correspoding public key (and nothing else).
+	/// regular signature signing the owner identity and the second one with unique context
+	/// which signs the correspoding public key (and nothing else).
 	#[cfg(feature = "full_crypto")]
 	fn generate_proof_of_possession(&mut self, owner: &[u8]) -> Self::ProofOfPossession;
 }
 
 /// Pair which is able to verify proof of possession.
 ///
-/// While you don't need a keypair to verify a proof of possession (you only need a public key)
-/// we constrain on Pair to use the Public and Signature types associated to Pair.
-/// This is implemented in different trait (than Public Key) to provide default behavior.
+/// While you don't need a keypair to verify a proof of possession (you only need a public key) we
+/// constrain on Pair to use the Public and Signature types associated to Pair.  This is implemented
+/// in different trait (than Public Key) to provide default behavior.
 pub trait ProofOfPossessionVerifier: Pair
 where
 	Self::Public: CryptoType,
@@ -82,21 +77,21 @@ pub fn statement_of_ownership(owner: &[u8]) -> Vec<u8> {
 
 /// Marker trait to identify whether the scheme is not aggregatable.
 ///
-/// Aggregatable schemes may change/optimize implementation parts such as Proof Of Possession
-/// or other specifics.
+/// Aggregatable schemes may change/optimize implementation parts such as Proof Of Possession or
+/// other specifics.
 ///
-/// This is specifically because implementation of proof of possession for aggregatable schemes
-/// is security critical.
+/// This is specifically because implementation of proof of possession for aggregatable schemes is
+/// security critical.
 ///
-/// We would like to prevent aggregatable scheme from unknowingly generating signatures
-/// which aggregate to false albeit valid proof of possession aka rogue key attack.
-/// We ensure that by separating signing and generating proof_of_possession at the API level.
+/// We would like to prevent aggregatable scheme from unknowingly generating signatures which
+/// aggregate to false albeit valid proof of possession aka rogue key attack.  We ensure that by
+/// separating signing and generating proof_of_possession at the API level.
 ///
-/// Rogue key attack however is not immediately applicable to non-aggregatable scheme
-/// when even if an honest signing oracle is tricked to sign a rogue proof_of_possession, it is not
-/// possible to aggregate it to generate a valid proof for a key the attack does not
-/// possess. Therefore we do not require non-aggregatable schemes to prevent proof_of_possession
-/// confirming signatures at API level
+/// Rogue key attack however is not immediately applicable to non-aggregatable scheme when even if
+/// an honest signing oracle is tricked to sign a rogue proof_of_possession, it is not possible to
+/// aggregate it to generate a valid proof for a key the attack does not possess. Therefore we do
+/// not require non-aggregatable schemes to prevent proof_of_possession confirming signatures at API
+/// level
 pub trait NonAggregatable: Pair {
 	/// Default proof_of_possession statement.
 	fn proof_of_possession_statement(owner: &[u8]) -> Vec<u8> {
