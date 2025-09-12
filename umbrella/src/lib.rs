6283709fd6b82ad78ba903c5e1e443788da613fe
Tiago Tavares <tiago.tavares@parity.io>
2025-09-12 13:34:49 +0100

    fix umbrella

--- umbrella/src/lib.rs
+++ umbrella/src/lib.rs
@@ -531,13 +531,13 @@ pub use pallet_migrations;
 #[cfg(feature = "pallet-mixnet")]
 pub use pallet_mixnet;
 
-/// FRAME pallet to manage multi-asset bounties.
-#[cfg(feature = "pallet-multi-asset-bounties")]
-pub use pallet_multi_asset_bounties;
-
 /// FRAME Merkle Mountain Range pallet.
 #[cfg(feature = "pallet-mmr")]
 pub use pallet_mmr;
+
+/// FRAME pallet to manage multi-asset and cross-chain bounties.
+#[cfg(feature = "pallet-multi-asset-bounties")]
+pub use pallet_multi_asset_bounties;
 
 /// FRAME multi-signature dispatch pallet.
 #[cfg(feature = "pallet-multisig")]
