e38a6815c01a0d4e8fcf7c9e38a283d8ef0f14b3
Christian Langenbacher <clangenb+gh@protonmail.ch>
2025-09-13 13:16:26 +0200

    create the pool in the tests

--- polkadot/xcm/docs/src/cookbook/foreign_assets_and_asset_conversion/tests.rs
+++ polkadot/xcm/docs/src/cookbook/foreign_assets_and_asset_conversion/tests.rs
@@ -20,8 +20,9 @@ use super::{
 	network::{AssetPara, MockNet, SimplePara, ASSET_PARA_ID, SIMPLE_PARA_ID, UNITS},
 	simple_para,
 };
-use frame::{prelude::fungible::Mutate, testing_prelude::*};
+use frame::{prelude::fungible::Mutate, testing_prelude::*, traits::TryConvert};
 use test_log::test;
+use sp_runtime::AccountId32;
 use xcm::prelude::*;
 use xcm_executor::traits::ConvertLocation;
 use xcm_simulator::TestExt;
@@ -36,6 +37,8 @@ fn registering_foreign_assets_work() {
 		&Location::new(1, Parachain(SIMPLE_PARA_ID)),
 	)
 	.expect("Can convert");
+
+	let simple_para_asset_location = Location::new(1, Parachain(SIMPLE_PARA_ID));
 
 	// We ensure that Simple Para's sovereign account has funds on the Asset Para to pay for the
 	// deposits needed to create the foreign asset.
@@ -57,7 +60,7 @@ fn registering_foreign_assets_work() {
 				origin_kind: OriginKind::Xcm,
 				fallback_max_weight: None,
 				call: asset_para::RuntimeCall::ForeignAssets(pallet_assets::Call::create {
-					id: Location::new(1, Parachain(SIMPLE_PARA_ID)),
+					id: simple_para_asset_location.clone(),
 					admin: simple_para_sovereign.clone().into(),
 					min_balance: 1_000_000_000,
 				})
@@ -68,7 +71,7 @@ fn registering_foreign_assets_work() {
 				origin_kind: OriginKind::SovereignAccount,
 				fallback_max_weight: None,
 				call: asset_para::RuntimeCall::ForeignAssets(pallet_assets::Call::set_metadata {
-					id: Location::new(1, Parachain(SIMPLE_PARA_ID)),
+					id: simple_para_asset_location.clone(),
 					name: "Simple Para Token".into(),
 					symbol: "TOK".into(),
 					decimals: 12,
@@ -91,7 +94,7 @@ fn registering_foreign_assets_work() {
 		// Confirm that we have successfully created the asset.
 		asset_para::System::assert_has_event(asset_para::RuntimeEvent::ForeignAssets(
 			pallet_assets::Event::Created {
-				asset_id: Location::new(1, Parachain(SIMPLE_PARA_ID)),
+				asset_id: simple_para_asset_location.clone(),
 				creator: simple_para_sovereign.clone().into(),
 				owner: simple_para_sovereign.clone().into(),
 			},
@@ -108,7 +111,7 @@ fn registering_foreign_assets_work() {
 		// Confirm that we have successfully set the metadata.
 		asset_para::System::assert_has_event(asset_para::RuntimeEvent::ForeignAssets(
 			pallet_assets::Event::MetadataSet {
-				asset_id: Location::new(1, Parachain(SIMPLE_PARA_ID)),
+				asset_id: simple_para_asset_location.clone(),
 				name: "Simple Para Token".into(),
 				symbol: "TOK".into(),
 				decimals: 12,
@@ -119,7 +122,7 @@ fn registering_foreign_assets_work() {
 		// The setting of the metadata required a deposit too.
 		asset_para::System::assert_has_event(asset_para::RuntimeEvent::Balances(
 			pallet_balances::Event::Reserved {
-				who: simple_para_sovereign.into(),
+				who: simple_para_sovereign.clone().into(),
 				// T::MetadataDepositBase + T::MetadataDepositPerByte * metadata_bytes
 				amount: 30,
 			},
@@ -130,7 +133,52 @@ fn registering_foreign_assets_work() {
 	});
 
 	// Todo: Step 2. Create a pool with the AssetPara's native asset and the foreign asset that
-	// we just registered.
+	AssetPara::execute_with(|| {
+		use asset_para::assets::PoolIdToAccountId;
+
+		// Create some liquidity of the foreign asset on the Asset Para.
+		assert_ok!(asset_para::ForeignAssets::mint(
+			asset_para::RuntimeOrigin::signed(simple_para_sovereign.clone().into()),
+			simple_para_asset_location.clone(),
+			simple_para_sovereign.clone().into(),
+			3_000_000_000_000,
+		));
+		asset_para::System::reset_events();
+
+		// Anyone can create a liquidy pool that doesn't exist yet.
+		assert_ok!(asset_para::AssetConversion::create_pool(
+			asset_para::RuntimeOrigin::signed(simple_para_sovereign.clone().into()),
+			Box::new(Location::here()),
+			Box::new(simple_para_asset_location.clone()),
+		));
+
+		// The setting of the metadata required a deposit too.
+		let pool_id = (Location::here(), simple_para_asset_location.clone());
+		let pool_account: AccountId32 = PoolIdToAccountId::try_convert(&pool_id).unwrap();
+		assert_eq!(pool_account.to_string(), "5CWpVFvs5i7vvo9ZNNycgtRpqSCKosH9B4wJTs6H8LLC6NPD");
+
+		asset_para::System::assert_has_event(asset_para::RuntimeEvent::AssetConversion(
+			pallet_asset_conversion::Event::PoolCreated {
+				creator: simple_para_sovereign.clone().into(),
+				pool_id: pool_id.clone(),
+				pool_account: PoolIdToAccountId::try_convert(&pool_id).unwrap(),
+				lp_token: 0,
+			},
+		));
+
+		asset_para::System::assert_has_event(asset_para::RuntimeEvent::PoolAssets(
+			pallet_assets::Event::ForceCreated {
+				asset_id: 0,
+				owner: pool_account.clone(),
+			},
+		));
+
+		// There are also balance events regarding the deposits, but we will ignore them for
+		// conciseness.
+
+		// clear events that we do not want later.
+		asset_para::System::reset_events();
+	});
 
 	// Todo: Step 3. Show how we can transfer our asset to the relay chain, and pay XCM-execution
 	// fees with it.
