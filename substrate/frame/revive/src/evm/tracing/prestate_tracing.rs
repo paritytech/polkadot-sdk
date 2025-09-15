9237ec2a152dbc5f1a58c64675b516cf76af6b5e
PG Herveou <pgherveou@gmail.com>
2025-09-15 09:34:28 +0200

    Update substrate/frame/revive/src/evm/tracing/prestate_tracing.rs

--- substrate/frame/revive/src/evm/tracing/prestate_tracing.rs
+++ substrate/frame/revive/src/evm/tracing/prestate_tracing.rs
@@ -64,8 +64,7 @@ where
 
 	/// Collect the traces and return them.
 	pub fn collect_trace(self) -> PrestateTrace {
-		let Self { trace, .. } = self;
-		let (mut pre, mut post) = trace;
+		let (mut pre, mut post) = self.trace;
 		let include_code = !self.config.disable_code;
 
 		let is_empty = |info: &PrestateTraceInfo| {
