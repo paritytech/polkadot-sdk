058b9acd9f850539c6afe0c021bcf34f9789b589
Bastian Köcher <git@kchr.de>
2025-09-15 22:56:00 +0200

    Apply suggestion from @bkchr

--- substrate/frame/support/src/lib.rs
+++ substrate/frame/support/src/lib.rs
@@ -405,8 +405,8 @@ pub use macro_magic;
 pub mod testing_prelude {
 	pub use super::traits::Get;
 	pub use crate::{
-		assert_err, assert_err_ignore_postinfo, assert_err_with_weight, assert_noop, assert_ok,
-		assert_storage_noop, parameter_types,
+		assert_err, assert_err_ignore_postinfo, assert_err_with_weight, assert_error_encoded_size, 
+		assert_noop, assert_ok, assert_storage_noop, parameter_types,
 	};
 	pub use sp_arithmetic::assert_eq_error_rate;
 	pub use sp_runtime::{bounded_btree_map, bounded_vec};
