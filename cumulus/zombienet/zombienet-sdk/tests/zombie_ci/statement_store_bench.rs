ba1102a028f2542a19fdde69c952450965471a5d
Andrei Eres <eresav@me.com>
2025-09-16 20:05:41 +0200

    Save other session keys

--- cumulus/zombienet/zombienet-sdk/tests/zombie_ci/statement_store_bench.rs
+++ cumulus/zombienet/zombienet-sdk/tests/zombie_ci/statement_store_bench.rs
@@ -5,10 +5,13 @@
 // propagated to peers.
 
 use anyhow::anyhow;
-use sp_core::{ed25519, Bytes, Decode, Encode, Pair};
+use sp_core::{sr25519, Bytes, Decode, Encode, Pair};
 use sp_keyring::Sr25519Keyring;
 use sp_statement_store::{Statement, Topic};
+use std::collections::HashMap;
 use zombienet_sdk::{subxt::ext::subxt_rpcs::rpc_params, NetworkConfigBuilder};
+
+const GROUP_SIZE: usize = 6;
 
 #[tokio::test(flavor = "multi_thread")]
 async fn statement_store() -> Result<(), anyhow::Error> {
@@ -61,7 +64,7 @@ async fn statement_store() -> Result<(), anyhow::Error> {
 	let collator_node = network.get_node("charlie")?;
 	let collator_rpc = collator_node.rpc().await?;
 
-	let participants: Vec<_> = [
+	let mut participants: Vec<_> = [
 		Sr25519Keyring::Alice,
 		Sr25519Keyring::Bob,
 		Sr25519Keyring::Charlie,
@@ -70,7 +73,8 @@ async fn statement_store() -> Result<(), anyhow::Error> {
 		Sr25519Keyring::Ferdie,
 	]
 	.into_iter()
-	.map(Participant::new)
+	.enumerate()
+	.map(|(idx, keyring)| Participant::new(keyring, idx))
 	.collect();
 
 	let mut statements = Vec::new();
@@ -79,24 +83,48 @@ async fn statement_store() -> Result<(), anyhow::Error> {
 		let statement_bytes: Bytes = statement.encode().into();
 		statements.push(statement_bytes.clone());
 
-		// Submit each participant's statement to charlie.
 		let _: () = collator_rpc.request("statement_submit", rpc_params![statement_bytes]).await?;
 	}
 
-	// Ensure that charlie stored all statements.
-	let charlie_dump: Vec<Bytes> = collator_rpc.request("statement_dump", rpc_params![]).await?;
-	if charlie_dump.len() != statements.len() {
-		return Err(anyhow!(
-			"charlie did not store all statements, expected {}, got {}",
-			statements.len(),
-			charlie_dump.len()
-		));
-	}
+	for participant in &mut participants {
+		println!("Participant {:?} polling for statements from others...", participant.keyring);
 
-	for statement_bytes in &charlie_dump {
-		if let Ok(statement) = sp_statement_store::Statement::decode(&mut &statement_bytes[..]) {
-			println!("{:?}", statement);
+		let topic = vec![topic_public_key()];
+
+		let statements: Vec<Bytes> = collator_rpc
+			.request("statement_broadcastsStatement", rpc_params![topic])
+			.await?;
+
+		for statement_bytes in &statements {
+			if let Ok(statement) = sp_statement_store::Statement::decode(&mut &statement_bytes[..])
+			{
+				if let Some(topic_1) = statement.topic(1) {
+					let other_idx = usize::from_le_bytes({
+						let mut bytes = [0u8; 8];
+						bytes.copy_from_slice(&topic_1[..8]);
+						bytes
+					});
+
+					if let Some(data) = statement.data() {
+						let slice = data.as_slice();
+						if slice.len() == 32 {
+							let mut array = [0u8; 32];
+							array.copy_from_slice(slice);
+							let session_key = sr25519::Public::from_raw(array);
+
+							participant.add_group_member(other_idx, session_key);
+
+							println!(
+								"Participant {:?} found session key {:?} for idx {}",
+								participant.keyring, session_key, other_idx
+							);
+						}
+					}
+				}
+			}
 		}
+
+		println!("Participant {:?} group members: {:?}", participant.keyring, participant.group);
 	}
 
 	Ok(())
@@ -104,23 +132,36 @@ async fn statement_store() -> Result<(), anyhow::Error> {
 
 struct Participant {
 	keyring: Sr25519Keyring,
-	session_key: ed25519::Pair,
+	session_key: sr25519::Pair,
+	idx: usize,
+	group: HashMap<usize, sr25519::Public>,
 }
 
 impl Participant {
-	fn new(keyring: Sr25519Keyring) -> Self {
-		let (session_key, _) = ed25519::Pair::generate();
-		Self { keyring, session_key }
+	fn new(keyring: Sr25519Keyring, idx: usize) -> Self {
+		let (session_key, _) = sr25519::Pair::generate();
+		Self { keyring, session_key, idx, group: HashMap::new() }
 	}
 
 	fn public_key_statement(&self) -> Statement {
 		let mut statement = Statement::new();
 		statement.set_channel([0u8; 32]);
 		statement.set_topic(0, topic_public_key());
+		statement.set_topic(1, topic_idx(self.idx));
 		statement.set_plain_data(self.session_key.public().to_vec());
 		statement.sign_sr25519_private(&self.keyring.pair());
 
 		statement
+	}
+
+	fn is_in_same_group(&self, other_idx: usize) -> bool {
+		self.idx / GROUP_SIZE == other_idx / GROUP_SIZE
+	}
+
+	fn add_group_member(&mut self, idx: usize, session_key: sr25519::Public) {
+		if self.is_in_same_group(idx) && idx != self.idx {
+			self.group.insert(idx, session_key);
+		}
 	}
 }
 
@@ -129,5 +170,11 @@ fn topic_public_key() -> Topic {
 	let source = b"public key";
 	let len = source.len().min(32);
 	topic[..len].copy_from_slice(&source[..len]);
+	topic
+}
+
+fn topic_idx(idx: usize) -> Topic {
+	let mut topic = [0u8; 32];
+	topic[..8].copy_from_slice(&idx.to_le_bytes());
 	topic
 }
