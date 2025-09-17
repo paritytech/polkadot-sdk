4331b282ecc6b6e911eadb322c600cbea2c4541a
Michael Müller <mich@elmueller.net>
2025-09-17 02:09:01 +0200

    [pallet-revive] Migrate various getters to `System` pre-compile (#9517)
    Part of closing https://github.com/paritytech/polkadot-sdk/issues/8572.
    
    Migrates:
    * `own_code_hash`
    * `caller_is_origin`
    * `caller_is_root`
    * `weight_left`
    * `minimum_balance`
    
    There are some minor other fixes in there (removing leftovers from
    deprecating chain extensions, stabilizing `block_hash` in overlooked
    crates, etc.).
    
    cc @athei @pgherveou
    
    ---------
    
    Co-authored-by: Alexander Theißen <alex.theissen@me.com>

--- substrate/frame/revive/src/tests/pvm.rs
+++ substrate/frame/revive/src/tests/pvm.rs
@@ -27,6 +27,10 @@ use crate::{
 	evm::{runtime::GAS_PRICE, CallTrace, CallTracer, CallType, GenericTransaction},
 	exec::Key,
 	limits,
+	precompiles::alloy::sol_types::{
+		sol_data::{Bool, FixedBytes},
+		SolType,
+	},
 	storage::DeletionQueueManager,
 	test_utils::builder::Contract,
 	tests::{
@@ -471,7 +475,7 @@ fn run_out_of_fuel_host() {
 
 #[test]
 fn gas_syncs_work() {
-	let (code, _code_hash) = compile_module("caller_is_origin_n").unwrap();
+	let (code, _code_hash) = compile_module("gas_price_n").unwrap();
 	ExtBuilder::default().existential_deposit(200).build().execute_with(|| {
 		let _ = <Test as Config>::Currency::set_balance(&ALICE, 1_000_000);
 		let contract = builder::bare_instantiate(Code::Upload(code)).build_and_unwrap_contract();
@@ -483,7 +487,7 @@ fn gas_syncs_work() {
 		let result = builder::bare_call(contract.addr).data(1u32.encode()).build();
 		assert_ok!(result.result);
 		let gas_consumed_once = result.gas_consumed.ref_time();
-		let host_consumed_once = <Test as Config>::WeightInfo::seal_caller_is_origin().ref_time();
+		let host_consumed_once = <Test as Config>::WeightInfo::seal_gas_price().ref_time();
 		let engine_consumed_once = gas_consumed_once - host_consumed_once - engine_consumed_noop;
 
 		let result = builder::bare_call(contract.addr).data(2u32.encode()).build();
@@ -3466,6 +3470,74 @@ fn call_diverging_out_len_works() {
 		// correct output if the supplied output length was smaller than
 		// than what the callee returned.
 		assert_ok!(builder::call(addr).build());
+	});
+}
+
+#[test]
+fn call_own_code_hash_works() {
+	let (code, code_hash) = compile_module("call_own_code_hash").unwrap();
+
+	ExtBuilder::default().existential_deposit(100).build().execute_with(|| {
+		let _ = <Test as Config>::Currency::set_balance(&ALICE, 1_000_000);
+
+		// Create the contract: Constructor does nothing
+		let Contract { addr, .. } =
+			builder::bare_instantiate(Code::Upload(code)).build_and_unwrap_contract();
+
+		let ret = builder::bare_call(addr).build_and_unwrap_result();
+		let ret_hash = FixedBytes::<32>::abi_decode(&ret.data).unwrap();
+		assert_eq!(ret_hash, code_hash.0);
+	});
+}
+
+#[test]
+fn call_caller_is_root() {
+	let (code, _) = compile_module("call_caller_is_root").unwrap();
+
+	ExtBuilder::default().existential_deposit(100).build().execute_with(|| {
+		let _ = <Test as Config>::Currency::set_balance(&ALICE, 1_000_000);
+
+		// Create the contract: Constructor does nothing
+		let Contract { addr, .. } =
+			builder::bare_instantiate(Code::Upload(code)).build_and_unwrap_contract();
+
+		let ret = builder::bare_call(addr).origin(RuntimeOrigin::root()).build_and_unwrap_result();
+		let is_root = Bool::abi_decode(&ret.data).expect("decoding failed");
+		assert!(is_root);
+	});
+}
+
+#[test]
+fn call_caller_is_root_from_non_root() {
+	let (code, _) = compile_module("call_caller_is_root").unwrap();
+
+	ExtBuilder::default().existential_deposit(100).build().execute_with(|| {
+		let _ = <Test as Config>::Currency::set_balance(&ALICE, 1_000_000);
+
+		// Create the contract: Constructor does nothing
+		let Contract { addr, .. } =
+			builder::bare_instantiate(Code::Upload(code)).build_and_unwrap_contract();
+
+		let ret = builder::bare_call(addr).build_and_unwrap_result();
+		let is_root = Bool::abi_decode(&ret.data).expect("decoding failed");
+		assert!(!is_root);
+	});
+}
+
+#[test]
+fn call_caller_is_origin() {
+	let (code, _) = compile_module("call_caller_is_origin").unwrap();
+
+	ExtBuilder::default().existential_deposit(100).build().execute_with(|| {
+		let _ = <Test as Config>::Currency::set_balance(&ALICE, 1_000_000);
+
+		// Create the contract: Constructor does nothing
+		let Contract { addr, .. } =
+			builder::bare_instantiate(Code::Upload(code)).build_and_unwrap_contract();
+
+		let ret = builder::bare_call(addr).build_and_unwrap_result();
+		let is_origin = Bool::abi_decode(&ret.data).expect("decoding failed");
+		assert!(is_origin);
 	});
 }
 
