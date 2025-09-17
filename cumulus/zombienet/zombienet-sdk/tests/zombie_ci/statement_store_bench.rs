016953c5df04233169ff6ff82740fb83e661047d
Andrei Eres <eresav@me.com>
2025-09-17 17:31:13 +0200

    Extract network spawning

--- cumulus/zombienet/zombienet-sdk/tests/zombie_ci/statement_store_bench.rs
+++ cumulus/zombienet/zombienet-sdk/tests/zombie_ci/statement_store_bench.rs
@@ -11,23 +11,14 @@ use sp_core::{blake2_256, ed25519, sr25519, Bytes, Pair};
 use sp_keyring::Sr25519Keyring;
 use sp_statement_store::{Statement, Topic};
 use std::collections::{HashMap, HashSet};
-use zombienet_sdk::{subxt::ext::subxt_rpcs::rpc_params, NetworkConfigBuilder};
+use zombienet_sdk::{
+	subxt::{backend::rpc::RpcClient, ext::subxt_rpcs::rpc_params},
+	NetworkConfigBuilder,
+};
 
 const GROUP_SIZE: usize = 6;
 const MESSAGE_SIZE: usize = 5 * 1024; // 5KiB
 const MESSAGE_COUNT: usize = 2;
-
-#[derive(Encode, Decode, Clone)]
-struct StatementRequest {
-	request_id: u64,
-	data: Vec<u8>,
-}
-
-#[derive(Encode, Decode, Clone)]
-struct StatementResponse {
-	request_id: u64,
-	response_code: u8,
-}
 
 #[tokio::test(flavor = "multi_thread")]
 async fn statement_store() -> Result<(), anyhow::Error> {
@@ -35,52 +26,7 @@ async fn statement_store() -> Result<(), anyhow::Error> {
 		env_logger::Env::default().filter_or(env_logger::DEFAULT_FILTER_ENV, "info"),
 	);
 
-	println!("=== Spawning the network ===");
-
-	// images are not relevant for `native`, but we leave it here in case we use `k8s` some day
-	let images = zombienet_sdk::environment::get_images_from_env();
-	let config = NetworkConfigBuilder::new()
-		.with_relaychain(|r| {
-			r.with_chain("westend-local")
-				.with_default_command("polkadot")
-				.with_default_image(images.polkadot.as_str())
-				.with_default_args(vec!["-lparachain=debug".into()])
-				.with_node(|node| node.with_name("validator-0"))
-				.with_node(|node| node.with_name("validator-1"))
-		})
-		.with_parachain(|p| {
-			p.with_id(2400)
-				.with_default_command("polkadot-parachain")
-				.with_default_image(images.cumulus.as_str())
-				.with_chain_spec_path("tests/zombie_ci/people-rococo-spec.json")
-				.with_default_args(vec![
-					"--force-authoring".into(),
-					"-lstatement-store=trace".into(),
-					"--enable-statement-store".into(),
-				])
-				.with_collator(|n| n.with_name("alice"))
-				.with_collator(|n| n.with_name("bob"))
-				.with_collator(|n| n.with_name("charlie"))
-				.with_collator(|n| n.with_name("dave"))
-				.with_collator(|n| n.with_name("eve"))
-				.with_collator(|n| n.with_name("ferdie"))
-		})
-		.with_global_settings(|global_settings| match std::env::var("ZOMBIENET_SDK_BASE_DIR") {
-			Ok(val) => global_settings.with_base_dir(val),
-			_ => global_settings,
-		})
-		.build()
-		.map_err(|e| {
-			let errs = e.into_iter().map(|e| e.to_string()).collect::<Vec<_>>().join(" ");
-			anyhow!("config errs: {errs}")
-		})?;
-
-	let spawn_fn = zombienet_sdk::environment::get_spawn_fn();
-	let network = spawn_fn(config).await?;
-	assert!(network.wait_until_is_up(60).await.is_ok());
-
-	let collator_node = network.get_node("charlie")?;
-	let collator_rpc = collator_node.rpc().await?;
+	let collator_rpc = spawn_network().await?;
 
 	let mut participants: Vec<_> = [
 		Sr25519Keyring::Alice,
@@ -283,6 +229,66 @@ async fn statement_store() -> Result<(), anyhow::Error> {
 	}
 
 	Ok(())
+}
+
+async fn spawn_network() -> Result<RpcClient, anyhow::Error> {
+	let images = zombienet_sdk::environment::get_images_from_env();
+	let config = NetworkConfigBuilder::new()
+		.with_relaychain(|r| {
+			r.with_chain("westend-local")
+				.with_default_command("polkadot")
+				.with_default_image(images.polkadot.as_str())
+				.with_default_args(vec!["-lparachain=debug".into()])
+				.with_node(|node| node.with_name("validator-0"))
+				.with_node(|node| node.with_name("validator-1"))
+		})
+		.with_parachain(|p| {
+			p.with_id(2400)
+				.with_default_command("polkadot-parachain")
+				.with_default_image(images.cumulus.as_str())
+				.with_chain_spec_path("tests/zombie_ci/people-rococo-spec.json")
+				.with_default_args(vec![
+					"--force-authoring".into(),
+					"-lstatement-store=trace".into(),
+					"--enable-statement-store".into(),
+				])
+				.with_collator(|n| n.with_name("alice"))
+				.with_collator(|n| n.with_name("bob"))
+				.with_collator(|n| n.with_name("charlie"))
+				.with_collator(|n| n.with_name("dave"))
+				.with_collator(|n| n.with_name("eve"))
+				.with_collator(|n| n.with_name("ferdie"))
+		})
+		.with_global_settings(|global_settings| match std::env::var("ZOMBIENET_SDK_BASE_DIR") {
+			Ok(val) => global_settings.with_base_dir(val),
+			_ => global_settings,
+		})
+		.build()
+		.map_err(|e| {
+			let errs = e.into_iter().map(|e| e.to_string()).collect::<Vec<_>>().join(" ");
+			anyhow!("config errs: {errs}")
+		})?;
+
+	let spawn_fn = zombienet_sdk::environment::get_spawn_fn();
+	let network = spawn_fn(config).await?;
+	assert!(network.wait_until_is_up(60).await.is_ok());
+
+	let collator_node = network.get_node("charlie")?;
+	let collator_rpc = collator_node.rpc().await?;
+
+	Ok(collator_rpc)
+}
+
+#[derive(Encode, Decode, Clone)]
+struct StatementRequest {
+	request_id: u64,
+	data: Vec<u8>,
+}
+
+#[derive(Encode, Decode, Clone)]
+struct StatementResponse {
+	request_id: u64,
+	response_code: u8,
 }
 
 struct Participant {
