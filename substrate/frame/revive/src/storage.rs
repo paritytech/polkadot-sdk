26d8e7aa5cf3efaf6a073c6a73ba0c226c9b9339
Alexandru Gheorghe <alexandru.gheorghe@parity.io>
2025-09-22 11:59:02 +0300

    remove unneeded whitespace
    Signed-off-by: Alexandru Gheorghe <alexandru.gheorghe@parity.io>

--- substrate/frame/revive/src/storage.rs
+++ substrate/frame/revive/src/storage.rs
@@ -455,7 +455,6 @@ impl<T: Config> ContractInfo<T> {
 
 /// Information about what happened to the pre-existing value when calling [`ContractInfo::write`].
 #[derive(Clone, Eq, PartialEq, Encode, Decode, RuntimeDebug, TypeInfo)]
-
 pub enum WriteOutcome {
 	/// No value existed at the specified key.
 	New,
