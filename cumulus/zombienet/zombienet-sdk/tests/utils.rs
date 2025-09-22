ee17be73368402bfce0734461ff2555526e52ee1
Andrei Sandu <andrei-mihail@parity.io>
2025-09-22 17:43:20 +0300

    assign cores utility
    Signed-off-by: Andrei Sandu <andrei-mihail@parity.io>

--- cumulus/zombienet/zombienet-sdk/tests/utils.rs
+++ cumulus/zombienet/zombienet-sdk/tests/utils.rs
@@ -1,7 +1,13 @@
 // Copyright (C) Parity Technologies (UK) Ltd.
 // SPDX-License-Identifier: Apache-2.0
 
-use zombienet_sdk::{LocalFileSystem, Network, NetworkConfig};
+use cumulus_zombienet_sdk_helpers::{
+	create_assign_core_call, submit_extrinsic_and_wait_for_finalization_success_with_timeout,
+};
+use zombienet_sdk::{
+	subxt::{OnlineClient, PolkadotConfig},
+	LocalFileSystem, Network, NetworkConfig, NetworkNode,
+};
 
 pub const BEST_BLOCK_METRIC: &str = "block_height{status=\"best\"}";
 
@@ -19,4 +25,28 @@ pub async fn initialize_network(
 	network.detach().await;
 
 	Ok(network)
+}
+
+pub async fn assign_cores(
+	node: &NetworkNode,
+	para_id: u32,
+	cores: Vec<u32>,
+) -> Result<(), anyhow::Error> {
+	log::info!("Assigning {:?} cores to parachain {}", cores, para_id);
+
+	let assign_cores_call =
+		create_assign_core_call(&cores.into_iter().map(|core| (core, para_id)).collect::<Vec<_>>());
+
+	let client: OnlineClient<PolkadotConfig> = node.wait_client().await?;
+	let res = submit_extrinsic_and_wait_for_finalization_success_with_timeout(
+		&client,
+		&assign_cores_call,
+		&zombienet_sdk::subxt_signer::sr25519::dev::alice(),
+		60u64,
+	)
+	.await;
+	assert!(res.is_ok(), "Extrinsic failed to finalize: {:?}", res.unwrap_err());
+	log::info!("Cores assigned to the parachain");
+
+	Ok(())
 }
