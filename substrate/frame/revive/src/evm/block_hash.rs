5f905b31d578630f3ca56d8385012d4985a38f56
Alexandru Vasile <alexandru.vasile@parity.io>
2025-09-25 14:31:33 +0000

    revive: Move receipt and logs bloom to dedicated module
    Signed-off-by: Alexandru Vasile <alexandru.vasile@parity.io>

--- substrate/frame/revive/src/evm/block_hash.rs
+++ substrate/frame/revive/src/evm/block_hash.rs
@@ -17,8 +17,12 @@
 //!Types, and traits to integrate pallet-revive with EVM.
 #![warn(missing_docs)]
 
+mod receipt;
+pub use receipt::{AccumulateReceipt, LogsBloom};
+
 use crate::evm::{
-	Block, HashesOrTransactionInfos, TYPE_EIP1559, TYPE_EIP2930, TYPE_EIP4844, TYPE_EIP7702,
+	block_hash::receipt::BLOOM_SIZE_BYTES, Block, HashesOrTransactionInfos, TYPE_EIP1559,
+	TYPE_EIP2930, TYPE_EIP4844, TYPE_EIP7702,
 };
 
 use alloc::{vec, vec::Vec};
@@ -327,172 +331,6 @@ impl IncrementalHashBuilder {
 		self.hash_builder.root().0.into()
 	}
 }
-
-/// Accumulate events (logs) into a stream of RLP encoded bytes.
-/// This is a very straight forward implementation that RLP encodes logs as they are added.
-///
-/// The main goal is to generate the RLP-encoded representation of receipts
-/// which is required to compute the receipt root hash, without storing the full receipt
-/// data in memory.
-///
-/// One approach is to store the full receipt in memory, together with the RLP encoding
-/// of the receipt.
-///
-/// However, since we only care about the RLP encoding of the receipt, we can optimize the memory
-/// usage by only storing the RLP encoded value and the logs directly. This effectively saves
-/// the need to store the full receipt (which can grow unboundedly due to the number of logs), and
-/// builds the RLP encoding incrementally as logs are added.
-///
-/// The implementation leverages the RLP encoding details of the receipt:
-///
-/// ```ignore
-/// // Memory representation of the RLP encoded receipt:
-/// [
-/// 	ReceiptHeader ++ rlp(status) ++ rlp(gas) ++ rlp(bloom)
-/// 			++ LogsHeader ++ rlp(log1) ++ rlp(log2) ++ ... ++ rlp(logN)
-/// ]
-/// ```
-///
-/// The optimization comes from the fact that `rlp(log1) ++ rlp(log2) ++ ... ++ rlp(logN)`
-/// can be built incrementally.
-///
-/// On average, from the real ethereum block, this implementation reduces the memory usage by 30%.
-///  `EncodedReceipt Space optimization (on average): 0.6995642434146292`
-pub struct AccumulateReceipt {
-	/// The RLP bytes where the logs are accumulated.
-	pub encoding: Vec<u8>,
-	/// The bloom filter collected from accumulating logs.
-	pub bloom: LogsBloom,
-}
-
-/// Bloom log filter compatible with Ethereum implementation.
-///
-/// This structure avoids conversions between substrate to alloy types
-/// to optimally compute the bloom.
-#[derive(Clone, Copy)]
-pub struct LogsBloom {
-	/// The bloom bytes used to store logs.
-	pub bloom: [u8; BLOOM_SIZE_BYTES],
-}
-
-impl Default for LogsBloom {
-	fn default() -> Self {
-		Self::new()
-	}
-}
-
-impl LogsBloom {
-	/// Constructs a new [`LogsBloom`].
-	pub const fn new() -> Self {
-		Self { bloom: [0u8; BLOOM_SIZE_BYTES] }
-	}
-
-	/// Ingests a raw log (event) into the bloom filter.
-	pub fn accrue_log(&mut self, contract: &H160, topics: &[H256]) {
-		Self::m3_2048(&mut self.bloom, contract.as_ref());
-
-		for topic in topics {
-			Self::m3_2048(&mut self.bloom, topic.as_ref());
-		}
-	}
-
-	/// Accrues the input into the bloom filter.
-	pub fn accrue_bloom(&mut self, other: &Self) {
-		for i in 0..BLOOM_SIZE_BYTES {
-			self.bloom[i] |= other.bloom[i];
-		}
-	}
-
-	/// Specialized Bloom filter that sets three bits out of 2048, given an
-	/// arbitrary byte sequence.
-	///
-	/// See Section 4.3.1 "Transaction Receipt" of the
-	/// [Ethereum Yellow Paper][ref] (page 6).
-	///
-	/// [ref]: https://ethereum.github.io/yellowpaper/paper.pdf
-	fn m3_2048(bloom: &mut [u8; 256], bytes: &[u8]) {
-		let hash = keccak_256(bytes);
-		for i in [0, 2, 4] {
-			let bit = (hash[i + 1] as usize + ((hash[i] as usize) << 8)) & 0x7FF;
-			bloom[256 - 1 - bit / 8] |= 1 << (bit % 8);
-		}
-	}
-}
-
-impl AccumulateReceipt {
-	/// Constructs a new [`AccumulateReceipt`].
-	pub const fn new() -> Self {
-		Self { encoding: Vec::new(), bloom: LogsBloom::new() }
-	}
-
-	/// Add the log into the accumulated receipt.
-	///
-	/// This accrues the log bloom and keeps track of the RLP encoding of the log.
-	pub fn add_log(&mut self, contract: &H160, data: &[u8], topics: &[H256]) {
-		// Accrue the log bloom.
-		self.bloom.accrue_log(contract, topics);
-
-		// Determine the length of the log RLP encoding.
-		let mut topics_len: usize = 0;
-		for topic in topics {
-			// Topics are represented by 32 bytes. However, their encoding
-			// can produce different lengths depending on their value.
-			topics_len = topics_len.saturating_add(rlp::Encodable::length(&topic.0));
-		}
-		// Account for the size of the list header.
-		let topics_list_header_length = topics_len + rlp::length_of_length(topics_len);
-		// Compute the total payload length of the log.
-		let payload_length = rlp::Encodable::length(&contract.0) +
-			rlp::Encodable::length(&data) +
-			topics_list_header_length;
-
-		let header = rlp::Header { list: true, payload_length };
-		header.encode(&mut self.encoding);
-		rlp::Encodable::encode(&contract.0, &mut self.encoding);
-		// Encode the topics as a list
-		rlp::Header { list: true, payload_length: topics_len }.encode(&mut self.encoding);
-		for topic in topics {
-			rlp::Encodable::encode(&topic.0, &mut self.encoding);
-		}
-		rlp::Encodable::encode(&data, &mut self.encoding);
-	}
-
-	/// Finalize the accumulated receipt and return the RLP encoded bytes.
-	pub fn encoded_receipt(
-		encoded_logs: Vec<u8>,
-		bloom: LogsBloom,
-		status: bool,
-		gas: u64,
-		transaction_type: Vec<u8>,
-	) -> Vec<u8> {
-		let logs_length = encoded_logs.len();
-		let list_header_length = logs_length + rlp::length_of_length(logs_length);
-
-		let header = rlp::Header {
-			list: true,
-			payload_length: rlp::Encodable::length(&status) +
-				rlp::Encodable::length(&gas) +
-				rlp::Encodable::length(&bloom.bloom) +
-				list_header_length,
-		};
-
-		let mut encoded = transaction_type;
-		header.encode(&mut encoded);
-		rlp::Encodable::encode(&status, &mut encoded);
-		rlp::Encodable::encode(&gas, &mut encoded);
-		rlp::Encodable::encode(&bloom.bloom, &mut encoded);
-
-		let logs_header = rlp::Header { list: true, payload_length: logs_length };
-		logs_header.encode(&mut encoded);
-
-		encoded.extend(encoded_logs);
-
-		encoded
-	}
-}
-
-/// Number of bytes that a bloom stores.
-const BLOOM_SIZE_BYTES: usize = 256;
 
 /// The intermediate representation of the Ethereum block builder.
 #[derive(Encode, Decode, TypeInfo)]
