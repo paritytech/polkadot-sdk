66e9b9a941acd8ffa0b16b796323dc91dd5d25cf
Robert Hambrock <roberthambrock@gmail.com>
2025-09-29 19:19:47 +0200

    Add `mmr_generateAncestryProof` rpc call (#9295)
    # Description
    
    Adds `generateAncestryProof` to the mmr RPC. An RPC method for
    generating ancestry proofs is required for cross-chain slashing by
    cross-chain fishermen https://github.com/Snowfork/snowbridge/pull/1493.
    Consequently, this PR also adds the mmr runtime api method
    `generate_ancestry_proof`. While such a method was already exposed by
    the beefy-mmr runtime api, this PR opts for moving it to the mmr runtime
    api instead due to the following considerations:
    1. Invoking beefy-mmr's `generate_proof` method via RPC would require
    adding the offchain-db extension to the beefy-rpc, which is a more
    invasive change with boilerplate that's not needed since the mmr RPC
    already uses the offchain-db extension for generating leaf proofs.
    2. Since the ancestry proofs are for MMR, it is more natural to expose
    the method directly on the mmr runtime api - the beefy-mmr pallet's
    `generate_proof` method is merely a wrapper around the mmr pallet's
    `generate_ancestry_proof` method.
    
    Some other misc. changes documented under `Review Notes`.
    
    ## Integration
    
    The integration is the same as for the beefy-mmr runtime api's
    `generate_proof` method.
    
    ~~The integration is the same as for the beefy-mmr runtime api's
    `generate_proof` method, except that the optional `at` specifier is
    removed for the method here since the method is idempotent wrt. the
    block height invoked at, so long as `at` >= `best_known_block_number`.
    Removing the specifier reduces likelihood of spurious errors from
    incorrect usage. I can revert the `at` specifier removal however if
    desired for compatibility.~~
    
    For example use, see https://github.com/Snowfork/snowbridge/pull/1493.
    
    ## Review Notes
    
    - Adds `generate_ancestry_proof` method to mmr runtime api
    (https://github.com/lederstrumpf/polkadot-sdk/commit/682eb4a1411f7194a7277606b7ebe3688e8d5df1)
    - Adds `mmr_generateAncestryProof` rpc method
    (https://github.com/lederstrumpf/polkadot-sdk/commit/5d0eac9f1f48f049bec9846ef497763f5c2fc950,
    https://github.com/lederstrumpf/polkadot-sdk/commit/5d0eac9f1f)
    - Adds new `InvalidEquivocationProofSessionMember` error to beefy pallet
    (https://github.com/lederstrumpf/polkadot-sdk/commit/682eb4a141) (note:
    this change is unrelated to the PR's main purpose, but helps
    implementers with more granular error reporting. I'm open to removing
    this change).
    - Deprecates
    `pallet_beefy::generate_ancestry_proof::generate_ancestry_proof` and
    `pallet_beefy::AncestryHelper::generate_proof`. Deprecation penciled in
    for September 2025 - I'm open to change this date or undo the
    deprecation.
    (https://github.com/lederstrumpf/polkadot-sdk/commit/6619169ecd)
    - ~~Removes `at` specifier for `pallet_mmr::generate_ancestry_proof`
    (https://github.com/lederstrumpf/polkadot-sdk/commit/bcadda2ce67cdb472b19db722d1ea3827e5869a5)
    (as mentioned in the `Integration` section, fine to undo)~~ *(Update:
    reverted removal of `at` specifier to allow fork handling).*
    
    PR can be tested using https://github.com/Snowfork/snowbridge/pull/1493.
    
    If PR's approach is accepted, will open the associated PRs in
    https://github.com/polkadot-fellows/runtimes &
    https://github.com/polkadot-js/api.
    
    ---------
    
    Co-authored-by: Adrian Catangiu <adrian@parity.io>

--- substrate/frame/staking-async/runtimes/rc/src/lib.rs
+++ substrate/frame/staking-async/runtimes/rc/src/lib.rs
@@ -2373,7 +2373,7 @@ sp_api::impl_runtime_apis! {
 		}
 	}
 
-	#[api_version(5)]
+	#[api_version(6)]
 	impl sp_consensus_beefy::BeefyApi<Block, BeefyId> for Runtime {
 		fn beefy_genesis() -> Option<BlockNumber> {
 			pallet_beefy::GenesisBlock::<Runtime>::get()
@@ -2434,17 +2434,6 @@ sp_api::impl_runtime_apis! {
 				.map(|p| p.encode())
 				.map(sp_consensus_beefy::OpaqueKeyOwnershipProof::new)
 		}
-
-		fn generate_ancestry_proof(
-			prev_block_number: BlockNumber,
-			best_known_block_number: Option<BlockNumber>,
-		) -> Option<sp_runtime::OpaqueValue> {
-			use sp_consensus_beefy::AncestryHelper;
-
-			BeefyMmrLeaf::generate_proof(prev_block_number, best_known_block_number)
-				.map(|p| p.encode())
-				.map(sp_runtime::OpaqueValue::new)
-		}
 	}
 
 	impl mmr::MmrApi<Block, Hash, BlockNumber> for Runtime {
@@ -2471,6 +2460,13 @@ sp_api::impl_runtime_apis! {
 					)
 				},
 			)
+		}
+
+		fn generate_ancestry_proof(
+			prev_block_number: BlockNumber,
+			best_known_block_number: Option<BlockNumber>,
+		) -> Result<mmr::AncestryProof<mmr::Hash>, mmr::Error> {
+			Mmr::generate_ancestry_proof(prev_block_number, best_known_block_number)
 		}
 
 		fn verify_proof(leaves: Vec<mmr::EncodableOpaqueLeaf>, proof: mmr::LeafProof<mmr::Hash>)
