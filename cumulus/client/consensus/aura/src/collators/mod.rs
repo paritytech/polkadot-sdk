34cd5d8d3416dc4e81ba3adf5f4fe0c17a824d41
Andrei Sandu <andrei-mihail@parity.io>
2025-10-01 17:20:55 +0300

    add more details
    Signed-off-by: Andrei Sandu <andrei-mihail@parity.io>

--- cumulus/client/consensus/aura/src/collators/mod.rs
+++ cumulus/client/consensus/aura/src/collators/mod.rs
@@ -44,7 +44,11 @@ pub mod lookahead;
 pub mod slot_based;
 
 // This is an arbitrary value which is guaranteed to exceed the required depth for 500ms blocks
-// built with a relay parent offset of 1.
+// built with a relay parent offset of 1. It must be larger than the unincluded segment capacity.
+//
+// The formula we use to compute the capacity of the unincluded segment in the parachain runtime
+// is:
+// UNINCLUDED_SEGMENT_CAPACITY = (2 + RELAY_PARENT_OFFSET) * BLOCK_PROCESSING_VELOCITY + 1.
 //
 // Since we only search for parent blocks which have already been imported,
 // we can guarantee that all imported blocks respect the unincluded segment
