660d586f261128848ef6674a759ddc87f83c3b33
Bastian Köcher <info@kchr.de>
2025-10-01 12:55:36 +0200

    FinalityNotification: Directly include stale blocks
    The finality notification was already carrying the information about the stale heads.
    However, most users of the stale heads were expanding these stale heads to all the stale blocks.
    So, we were iterating the same forks multiple times in the node for each finality notification.
    Also in a possible future where we start actually pruning headers as well, expanding these forks
    would fail.
    
    So, this pull request is changing the finality notification to directly carry the
    stale blocks (which were calculated any way already).

--- substrate/client/consensus/babe/src/lib.rs
+++ substrate/client/consensus/babe/src/lib.rs
@@ -561,16 +561,7 @@ fn aux_storage_cleanup<C: HeaderMetadata<Block> + HeaderBackend<Block>, Block: B
 			.filter(|h| **h != notification.hash),
 	);
 
-	// Cleans data for stale forks.
-	let stale_forks = match client.expand_forks(&notification.stale_heads) {
-		Ok(stale_forks) => stale_forks,
-		Err(e) => {
-			warn!(target: LOG_TARGET, "{:?}", e);
-
-			Default::default()
-		},
-	};
-	hashes.extend(stale_forks.iter());
+	hashes.extend(notification.stale_blocks.iter().map(|b| b.hash));
 
 	hashes
 		.into_iter()
