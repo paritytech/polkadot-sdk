3b24eea9f5e8983130f8fe520650ad2f808193f5
krayt78 <ludovic.domingues96@gmail.com>
2025-10-08 18:53:11 +0200

    runtime configs updated

--- cumulus/parachains/runtimes/assets/asset-hub-westend/src/lib.rs
+++ cumulus/parachains/runtimes/assets/asset-hub-westend/src/lib.rs
@@ -1287,6 +1287,7 @@ impl pallet_indices::Config for Runtime {
 	type Deposit = IndexDeposit;
 	type RuntimeEvent = RuntimeEvent;
 	type WeightInfo = weights::pallet_indices::WeightInfo<Runtime>;
+	type RuntimeHoldReason = RuntimeHoldReason;
 }
 
 impl pallet_ah_ops::Config for Runtime {
--- substrate/bin/node/runtime/src/lib.rs
+++ substrate/bin/node/runtime/src/lib.rs
@@ -552,6 +552,7 @@ impl pallet_indices::Config for Runtime {
 	type Deposit = IndexDeposit;
 	type RuntimeEvent = RuntimeEvent;
 	type WeightInfo = pallet_indices::weights::SubstrateWeight<Runtime>;
+	type RuntimeHoldReason = RuntimeHoldReason;
 }
 
 parameter_types! {
