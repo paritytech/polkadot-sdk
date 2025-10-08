24f2af9eea3ead4649c88c6068bc6c822e56dcb3
krayt78 <ludovic.domingues96@gmail.com>
2025-10-08 17:25:53 +0200

    removed unused imports

--- substrate/frame/indices/src/migration/mod.rs
+++ substrate/frame/indices/src/migration/mod.rs
@@ -15,8 +15,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use super::*;
-
 /// Storage migrations for the Indices pallet.
 pub mod v1;
 
