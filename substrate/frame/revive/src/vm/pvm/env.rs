7140887222978a3ab6999cc9f682708384d05c50
Cyrill Leutwiler <bigcyrill@hotmail.com>
2025-10-10 17:17:38 +0200

    rm spurious import
    Signed-off-by: Cyrill Leutwiler <bigcyrill@hotmail.com>

--- substrate/frame/revive/src/vm/pvm/env.rs
+++ substrate/frame/revive/src/vm/pvm/env.rs
@@ -185,8 +185,6 @@ impl<'a, E: Ext, M: PolkaVmInstance<E::T>> Runtime<'a, E, M> {
 // for every function.
 #[define_env]
 pub mod env {
-	use pallet_revive_uapi::ReturnFlags;
-
 	/// Noop function used to benchmark the time it takes to execute an empty function.
 	///
 	/// Marked as stable because it needs to be called from benchmarks even when the benchmarked
