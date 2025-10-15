3a2008517f4f9e6ef6fc97f9ecf7fa3f5b8c18e9
Egor_P <egor@parity.io>
2025-10-15 10:34:35 +0200

    Version bumps and prdocs reordering from stable2509 (#9974)
    This PR backports regular version bumps and prdocs reordering from the
    stable2509 branch back to master
    
    ---------
    
    Co-authored-by: ParityReleases <release-team@parity.io>

--- cumulus/parachains/runtimes/assets/asset-hub-westend/src/lib.rs
+++ cumulus/parachains/runtimes/assets/asset-hub-westend/src/lib.rs
@@ -144,7 +144,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: alloc::borrow::Cow::Borrowed("westmint"),
 	impl_name: alloc::borrow::Cow::Borrowed("westmint"),
 	authoring_version: 1,
-	spec_version: 1_019_004,
+	spec_version: 1_020_001,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 16,
