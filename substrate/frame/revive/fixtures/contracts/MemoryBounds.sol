b7c8a513805a4d28978a1d9498ad645a5e490eff
xermicus <cyrill@parity.io>
2025-10-16 09:44:54 +0200

    adjust fixture
    Signed-off-by: xermicus <cyrill@parity.io>

--- substrate/frame/revive/fixtures/contracts/MemoryBounds.sol
+++ substrate/frame/revive/fixtures/contracts/MemoryBounds.sol
@@ -3,10 +3,11 @@
 pragma solidity ^0.8.24;
 
 contract MemoryBounds {
-	fallback() external payable {
+	fallback() external {
 		assembly {
+
 			// Accessing OOB offsets should always work when the length is 0.
-			return(exp(128, 128), 0)
+			return(100000, 0)
 		}
 	}
 }
