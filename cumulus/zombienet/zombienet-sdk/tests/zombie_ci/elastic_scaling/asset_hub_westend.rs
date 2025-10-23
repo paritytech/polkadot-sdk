5c437bd2538e4defa76a3b03531d8b89abf2d354
Alexandru Vasile <alexandru.vasile@parity.io>
2025-10-23 14:54:13 +0000

    zombinet: Ensure ES works for AHWestend
    Signed-off-by: Alexandru Vasile <alexandru.vasile@parity.io>

---
+++ cumulus/zombienet/zombienet-sdk/tests/zombie_ci/elastic_scaling/asset_hub_westend.rs
@@ -0,0 +1,90 @@
+// Copyright (C) Parity Technologies (UK) Ltd.
+// SPDX-License-Identifier: Apache-2.0
+
+use crate::utils::initialize_network;
+
+use anyhow::anyhow;
+use cumulus_zombienet_sdk_helpers::{assert_para_throughput, assign_cores};
+use polkadot_primitives::Id as ParaId;
+use zombienet_sdk::{
+	subxt::{OnlineClient, PolkadotConfig},
+	NetworkConfig, NetworkConfigBuilder,
+};
+
+const PARA_ID: u32 = 1000;
+
+async fn build_network_config() -> Result<NetworkConfig, anyhow::Error> {
+	// images are not relevant for `native`, but we leave it here in case we use `k8s` some day
+	let images = zombienet_sdk::environment::get_images_from_env();
+	log::info!("Using images: {images:?}");
+
+	NetworkConfigBuilder::new()
+		.with_relaychain(|r| {
+			let r = r
+				.with_chain("westend-local")
+				.with_default_command("polkadot")
+				.with_default_image(images.polkadot.as_str())
+				.with_default_args(vec![("-lparachain=trace").into()])
+				.with_default_resources(|resources| {
+					// These settings are applicable only for `k8s` provider.
+					// Leaving them in case we switch to `k8s` some day.
+					resources.with_request_cpu(4).with_request_memory("4G")
+				})
+				// Have to set a `with_node` outside of the loop below, so that `r` has the right
+				// type.
+				.with_node(|node| node.with_name("validator-0"));
+			(1..3).fold(r, |acc, i| acc.with_node(|node| node.with_name(&format!("validator-{i}"))))
+		})
+		.with_parachain(|p| {
+			p.with_id(PARA_ID)
+				.with_default_command("polkadot-parachain")
+				.with_default_image(images.cumulus.as_str())
+				.with_chain("asset-hub-westend-local")
+				.with_default_args(vec![
+					("--authoring").into(),
+					("slot-based").into(),
+					("-lparachain=trace,aura=debug").into(),
+				])
+				.with_collator(|n| n.with_name("collator-0"))
+				.with_collator(|n| n.with_name("collator-1"))
+				.with_collator(|n| n.with_name("collator-2"))
+		})
+		.with_global_settings(|global_settings| match std::env::var("ZOMBIENET_SDK_BASE_DIR") {
+			Ok(val) => global_settings.with_base_dir(val),
+			_ => global_settings,
+		})
+		.build()
+		.map_err(|e| {
+			let errs = e.into_iter().map(|e| e.to_string()).collect::<Vec<_>>().join(" ");
+			anyhow!("config errs: {errs}")
+		})
+}
+
+#[tokio::test(flavor = "multi_thread")]
+async fn elastic_scaling_asset_hub_westend() -> Result<(), anyhow::Error> {
+	let _ = env_logger::try_init_from_env(
+		env_logger::Env::default().filter_or(env_logger::DEFAULT_FILTER_ENV, "info"),
+	);
+
+	log::info!("Spawning network");
+	let config = build_network_config().await?;
+	let network = initialize_network(config).await?;
+
+	let relay_node = network.get_node("validator-0")?;
+	let relay_client: OnlineClient<PolkadotConfig> = relay_node.wait_client().await?;
+
+	assign_cores(relay_node, PARA_ID, vec![0, 1, 2]).await?;
+
+	log::info!("Ensure elastic scaling works, 3 blocks should be produced in each 6s slot");
+
+	assert_para_throughput(
+		&relay_client,
+		20,
+		[(ParaId::from(PARA_ID), 50..61)].into_iter().collect(),
+	)
+	.await?;
+
+	log::info!("Test finished successfully.");
+
+	Ok(())
+}
