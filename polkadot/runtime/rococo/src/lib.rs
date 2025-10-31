e846236a0303584327a58dda76aa462cf3d0ce44
metricaez <emiliano@blockdeep.io>
2025-10-31 11:09:03 -0300

    fix: unnecesary config trait

--- polkadot/runtime/rococo/src/lib.rs
+++ polkadot/runtime/rococo/src/lib.rs
@@ -1232,7 +1232,6 @@ parameter_types! {
 }
 
 impl polkadot_runtime_parachains::broadcaster::Config for Runtime {
-	type RuntimeEvent = RuntimeEvent;
 	type MaxPublishItems = MaxPublishItems;
 	type MaxKeyLength = MaxKeyLength;
 	type MaxValueLength = MaxValueLength;
