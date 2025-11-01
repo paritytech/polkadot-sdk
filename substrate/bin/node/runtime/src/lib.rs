85b39dc0b82926d16c7555a202a689c6af1cdad7
WonderJL <jbleunghk@gmail.com>
2025-11-01 18:08:47 +0800

    wip

--- substrate/bin/node/runtime/src/lib.rs
+++ substrate/bin/node/runtime/src/lib.rs
@@ -1697,6 +1697,7 @@ parameter_types! {
 impl pallet_grandpa::Config for Runtime {
 	type RuntimeEvent = RuntimeEvent;
 	type WeightInfo = ();
+    type SessionWeightInfo = ();
 	type MaxAuthorities = MaxAuthorities;
 	type MaxNominators = MaxNominators;
 	type MaxSetIdSessionEntries = MaxSetIdSessionEntries;
