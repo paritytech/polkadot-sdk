5758fe620ad3dd8aef9da9446b96e49b8c26a09c
Ankan <ankan.anurag@gmail.com>
2025-11-12 16:05:48 +0530

    remove try state checks from legacy staking pallet

--- substrate/frame/staking/src/pallet/mod.rs
+++ substrate/frame/staking/src/pallet/mod.rs
@@ -1005,7 +1005,8 @@ pub mod pallet {
 
 		#[cfg(feature = "try-runtime")]
 		fn try_state(n: BlockNumberFor<T>) -> Result<(), sp_runtime::TryRuntimeError> {
-			Self::do_try_state(n)
+			// Don't run the try state checks on this pallet since Post-AHM, this pallet only
+			// remains on RC with no data.
 		}
 	}
 
