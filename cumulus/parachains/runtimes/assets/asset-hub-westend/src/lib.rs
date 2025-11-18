d90026cc4f229a12666bf528d05547b73d36ffa1
ParityReleases <release-team@parity.io>
2025-11-18 16:34:16 +0000

    Bump spec_version to 1_021_000

--- cumulus/parachains/runtimes/assets/asset-hub-westend/src/lib.rs
+++ cumulus/parachains/runtimes/assets/asset-hub-westend/src/lib.rs
@@ -147,7 +147,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: alloc::borrow::Cow::Borrowed("westmint"),
 	impl_name: alloc::borrow::Cow::Borrowed("westmint"),
 	authoring_version: 1,
-	spec_version: 1_020_004,
+	spec_version: 1_021_000,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 16,
