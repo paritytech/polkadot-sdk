5be5e10130e158eb4495bb4f896215a7bc8a9f5a
Lucas Grasso <lucasgrassoramos@gmail.com>
2025-11-19 11:29:54 -0300

    better docs for Preimage precompile interface

--- substrate/frame/preimage/precompiles/src/interface/IPreimage.sol
+++ substrate/frame/preimage/precompiles/src/interface/IPreimage.sol
@@ -4,13 +4,23 @@ pragma solidity ^0.8.30;
 /// @dev The on-chain address of the Preimage precompile.
 address constant PREIMAGE_PRECOMPILE_ADDRESS = address(0xD0000);
 
+/// @title `pallet_preimage` Precompile Interface
+/// @notice An interface for interacting with `pallet_preimage`.
+/// It forwards calls directly to the corresponding dispatchable functions.
 interface IPreimage {
-	/// @dev Register a preimage on-chain.
-	/// @param preImage The preimage to be registered on-chain
+	/// @notice Register a preimage on-chain.
+	/// @dev This transaction is free when called by a manager or when `preImage` was previously requested.
+	/// @param preImage The preimage to be registered on-chain.
+	/// @custom:reverts If `preImage` is larger than the allowed `CALLDATA_BYTES`.
+	/// @custom:reverts If `preImage` is already noted when called by a non-manager.
+	/// @custom:reverts If caller has unsufficient funds for deposit.
 	/// @return hash The hash of the preimage
-	function notePreimage(bytes calldata preImage) external returns (bytes32 hash);
+	function notePreimage(bytes memory preImage) external returns (bytes32 hash);
 
-	/// @dev Clear an unrequested preimage from storage.
+	/// @notice Clear an unrequested preimage from storage.
+	/// @dev If succesfull, will return any held deposits.
 	/// @param hash The preimage to be cleared from storage
+	/// @custom:reverts If there is no preimage noted under `hash`.
+	/// @custom:reverts If a non-manager attemps to unnote a preimage they did not note.
 	function unnotePreimage(bytes32 hash) external;
 }
