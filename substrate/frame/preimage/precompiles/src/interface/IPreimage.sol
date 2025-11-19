66e2cd814a6637d577b28b2b8313f10b5900fcd6
Lucas Grasso <lucasgrassoramos@gmail.com>
2025-11-19 10:26:28 -0300

    change bytes memory to bytes calldata

--- substrate/frame/preimage/precompiles/src/interface/IPreimage.sol
+++ substrate/frame/preimage/precompiles/src/interface/IPreimage.sol
@@ -8,7 +8,7 @@ interface IPreimage {
 	/// @dev Register a preimage on-chain.
 	/// @param preImage The preimage to be registered on-chain
 	/// @return hash The hash of the preimage
-	function notePreimage(bytes memory preImage) external returns (bytes32 hash);
+	function notePreimage(bytes calldata preImage) external returns (bytes32 hash);
 
 	/// @dev Clear an unrequested preimage from storage.
 	/// @param hash The preimage to be cleared from storage
