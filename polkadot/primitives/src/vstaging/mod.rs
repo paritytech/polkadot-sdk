f5352e2d287a9af7a92f1f047f365c14e8255100
EclesioMeloJunior <eclesiomelo.1@gmail.com>
2025-11-19 11:09:14 -0300

    chore: addressing comments

--- polkadot/primitives/src/vstaging/mod.rs
+++ polkadot/primitives/src/vstaging/mod.rs
@@ -15,32 +15,3 @@
 // along with Polkadot.  If not, see <http://www.gnu.org/licenses/>.
 
 //! Staging Primitives.
-
-use scale_info::TypeInfo;
-use sp_api::__private::{Decode, Encode};
-use sp_application_crypto::RuntimeDebug;
-use sp_core::DecodeWithMemTracking;
-use sp_staking::SessionIndex;
-use crate::ValidatorIndex;
-
-/// A reward tally line represent the collected statistics about
-/// approvals voting for a given validator, how much successful approvals
-/// was collected and how many times the given validator no-showed
-#[derive(Encode, Decode, DecodeWithMemTracking, Clone, Copy, PartialEq, RuntimeDebug, TypeInfo)]
-pub struct ApprovalStatisticsTallyLine {
-    pub validator_index: ValidatorIndex,
-    pub approvals_usage: u32,
-    pub no_shows: u32,
-}
-
-/// ApprovalRewards is the set of tallies where each tally represents
-/// a given validator and its approval voting statistics
-#[derive(Clone, Encode, Decode, TypeInfo, RuntimeDebug)]
-pub struct ApprovalStatistics(SessionIndex, Vec<ApprovalStatisticsTallyLine>);
-
-impl ApprovalStatistics {
-    pub fn signing_payload(&self) -> Vec<u8> {
-        const MAGIC: [u8; 4] = *b"APST"; // for "approval statistics"
-        (MAGIC, self.0, self.1.clone()).encode()
-    }
-}
