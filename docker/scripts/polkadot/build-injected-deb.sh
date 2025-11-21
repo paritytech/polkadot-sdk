756260c5e6e5e023f08d852b19e7d600c17cd334
EgorPopelyaev <egor@parity.io>
2025-11-21 18:31:21 +0100

    Use build-injected.sh directly to build polkadot container

--- docker/scripts/polkadot/build-injected-deb.sh
+++ docker/scripts/polkadot/build-injected-deb.sh
@@ -7,21 +7,9 @@
 
 PROJECT_ROOT=`git rev-parse --show-toplevel`
 
-echo "========================================"
-echo "DEBUG: build-injected-deb.sh is running"
-echo "DEBUG: VERSION argument: $1"
-echo "========================================"
-
 export BINARY=polkadot,polkadot-execute-worker,polkadot-prepare-worker
 export DOCKERFILE="docker/dockerfiles/polkadot/polkadot_injected_debian.Dockerfile"
 export POLKADOT_DEB=true
 export VERSION=$1
-
-echo "DEBUG: Exported variables:"
-echo "  BINARY=$BINARY"
-echo "  DOCKERFILE=$DOCKERFILE"
-echo "  POLKADOT_DEB=$POLKADOT_DEB"
-echo "  VERSION=$VERSION"
-echo "========================================"
 
 $PROJECT_ROOT/docker/scripts/build-injected.sh
