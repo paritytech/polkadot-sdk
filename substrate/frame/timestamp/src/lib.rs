0f85d17e800f866f74ac0e97b48537ddfb3b17e1
Nnamdi Aninye <unix1gl@gmail.com>
2025-11-26 00:40:13 +0100

    import ensure into timestamp lib.rs

--- substrate/frame/timestamp/src/lib.rs
+++ substrate/frame/timestamp/src/lib.rs
@@ -137,7 +137,7 @@ mod tests;
 pub mod weights;
 
 use core::{cmp, result};
-use frame_support::traits::{OnTimestampSet, Time, UnixTime};
+use frame_support::{ensure, traits::{OnTimestampSet, Time, UnixTime}};
 use sp_runtime::traits::{AtLeast32Bit, SaturatedConversion, Scale, Zero};
 use sp_timestamp::{InherentError, InherentType, INHERENT_IDENTIFIER};
 pub use weights::WeightInfo;
