ca6700f91f71270c7f09fdc8d0c5d31a10dd3bda
Alexander Theißen <alex.theissen@me.com>
2025-11-25 08:55:24 +0100

    Fix termination (#10302)
    This PR fixes up termination by changing the behavior to:
    
    - The free balance (without ed) should be send away right away to the
    beneficiary and not be delayed like the contract deletion.
    - The ed and storage deposit will be send away only when terminating but
    to the origin (delayed).
    - The scheduling of the terminate needs to be reverted if the scheduling
    frame reverts.
    - `SELFDESTRUCT` should be allowed inside the constructor. The issuing
    contract will exist as account without code for the remainder of the
    transaction.
    - The `terminate` pre-compile should revert if delegate called or its
    caller was delegate called. This is just my opinion but if we are
    changing semantics we can might as well add some security. We are
    increasing the attack surface by allowing the destruction of any
    contract (not only created in the current tx).
    
    
    ## Other fixes
    - Storage refunds should no longer use `BestEffort`. This is necessary
    to fail refunds in case some other locks (due to participation in gov
    for example) prevent sending them away. This is in anticipation of new
    pre-compiles.
    - Moved pre-compile interfaces to sol files and made them available to
    fixtures
    - Added some Solidity written tests to exercise error cases
    
    
    ## Further tests needed
    
    Those should all be written in Solidity to test both backends at the
    same time. No more Rust fixtures.
    
    @0xRVE can you take those over as I am ooo.
    
    - Test that checks that scheduled deletions do properly roll back if a
    frame fails
    - Test that value send to a contract after scheduling for deletion is
    send to the beneficiary (different from Eth where this balance is lost)
    - Add tests that use `SELFDESTRUCT` to `Terminate.sol`. Need
    https://github.com/paritytech/devops/issues/4508 but can be tested
    locally with newest `resolc`.
    
    ---------
    
    Signed-off-by: Alexandru Gheorghe <alexandru.gheorghe@parity.io>
    Signed-off-by: Andrei Sandu <andrei-mihail@parity.io>
    Co-authored-by: 0xRVE <robertvaneerdewijk@gmail.com>
    Co-authored-by: cmd[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: pgherveou <pgherveou@gmail.com>
    Co-authored-by: Alin Dima <alin@parity.io>
    Co-authored-by: Bastian Köcher <git@kchr.de>
    Co-authored-by: Kian Paimani <5588131+kianenigma@users.noreply.github.com>
    Co-authored-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
    Co-authored-by: Marian Radu <marian@parity.io>
    Co-authored-by: Karol Kokoszka <karol@parity.io>
    Co-authored-by: Francisco Aguirre <franciscoaguirreperez@gmail.com>
    Co-authored-by: Alexandru Gheorghe <49718502+alexggh@users.noreply.github.com>
    Co-authored-by: girazoki <gorka.irazoki@gmail.com>
    Co-authored-by: Adrian Catangiu <adrian@parity.io>
    Co-authored-by: Lukasz Rubaszewski <117115317+lrubasze@users.noreply.github.com>
    Co-authored-by: Paolo La Camera <paolo@parity.io>
    Co-authored-by: Andrei Eres <eresav@me.com>
    Co-authored-by: Alexandru Gheorghe <alexandru.gheorghe@parity.io>
    Co-authored-by: Egor_P <egor@parity.io>
    Co-authored-by: Agustín Rodriguez <agusrodriguez2456@gmail.com>
    Co-authored-by: Branislav Kontur <bkontur@gmail.com>
    Co-authored-by: Andrei Sandu <54316454+sandreim@users.noreply.github.com>
    Co-authored-by: Thang X. Vu <zthangxv@gmail.com>
    Co-authored-by: DenzelPenzel <15388928+DenzelPenzel@users.noreply.github.com>
    Co-authored-by: Javier Viola <363911+pepoviola@users.noreply.github.com>
    Co-authored-by: Alexander Cyon <Sajjon@users.noreply.github.com>
    Co-authored-by: Sebastian Kunert <skunert49@gmail.com>
    Co-authored-by: Alexander Samusev <41779041+alvicsam@users.noreply.github.com>
    Co-authored-by: Omar <OmarAbdulla7@hotmail.com>
    Co-authored-by: Guillaume Thiolliere <gui.thiolliere@gmail.com>
    Co-authored-by: BDevParity <bruno.devic@parity.io>

--- substrate/frame/revive/src/lib.rs
+++ substrate/frame/revive/src/lib.rs
@@ -576,6 +576,20 @@ pub mod pallet {
 		/// EVM contracts can only be instantiated via code upload as no initcode is
 		/// stored on-chain.
 		EvmConstructedFromHash = 0x37,
+		/// The contract does not have enough balance to refund the storage deposit.
+		///
+		/// This is a bug and should never happen. It means the accounting got out of sync.
+		StorageRefundNotEnoughFunds = 0x38,
+		/// This means there are locks on the contracts storage deposit that prevents refunding it.
+		///
+		/// This would be the case if the contract used its storage deposits for governance
+		/// or other pallets that allow creating locks over held balance.
+		StorageRefundLocked = 0x39,
+		/// Called a pre-compile that is not allowed to be delegate called.
+		///
+		/// Some pre-compile functions will trap the caller context if being delegate
+		/// called or if their caller was being delegate called.
+		PrecompileDelegateDenied = 0x40,
 		/// Benchmarking only error.
 		#[cfg(feature = "runtime-benchmarks")]
 		BenchmarkingError = 0xFF,
@@ -2249,6 +2263,11 @@ impl<T: Config> Pallet<T> {
 		exec_config: &ExecConfig<T>,
 	) -> DispatchResult {
 		use frame_support::traits::tokens::{Fortitude, Precision, Preservation};
+
+		if amount.is_zero() {
+			return Ok(());
+		}
+
 		match (exec_config.collect_deposit_from_hold.is_some(), hold_reason) {
 			(true, hold_reason) => {
 				T::FeeInfo::withdraw_txfee(amount)
@@ -2291,46 +2310,68 @@ impl<T: Config> Pallet<T> {
 		from: &T::AccountId,
 		to: &T::AccountId,
 		amount: BalanceOf<T>,
-		exec_config: &ExecConfig<T>,
-	) -> Result<BalanceOf<T>, DispatchError> {
+		exec_config: Option<&ExecConfig<T>>,
+	) -> Result<(), DispatchError> {
 		use frame_support::traits::{
+			fungible::InspectHold,
 			tokens::{Fortitude, Precision, Preservation, Restriction},
-			Imbalance,
 		};
-		if exec_config.collect_deposit_from_hold.is_some() {
-			let amount =
-				T::Currency::release(&hold_reason.into(), from, amount, Precision::BestEffort)
-					.and_then(|amount| {
-						T::Currency::withdraw(
-							from,
-							amount,
-							Precision::Exact,
-							Preservation::Preserve,
-							Fortitude::Polite,
-						)
-						.and_then(|credit| {
-							let amount = credit.peek();
-							T::FeeInfo::deposit_txfee(credit);
-							Ok(amount)
-						})
-					})
-					.map_err(|_| Error::<T>::StorageDepositNotEnoughFunds)?;
-			amount
+
+		if amount.is_zero() {
+			return Ok(());
+		}
+
+		let hold_reason = hold_reason.into();
+		let result = if exec_config.map(|c| c.collect_deposit_from_hold.is_some()).unwrap_or(false)
+		{
+			T::Currency::release(&hold_reason, from, amount, Precision::Exact)
+				.and_then(|amount| {
+					T::Currency::withdraw(
+						from,
+						amount,
+						Precision::Exact,
+						Preservation::Preserve,
+						Fortitude::Polite,
+					)
+				})
+				.map(T::FeeInfo::deposit_txfee)
 		} else {
-			let amount = T::Currency::transfer_on_hold(
-				&hold_reason.into(),
+			T::Currency::transfer_on_hold(
+				&hold_reason,
 				from,
 				to,
 				amount,
-				Precision::BestEffort,
+				Precision::Exact,
 				Restriction::Free,
 				Fortitude::Polite,
 			)
-			.map_err(|_| Error::<T>::StorageDepositNotEnoughFunds)?;
-			amount
+			.map(|_| ())
 		};
 
-		Ok(amount)
+		result.map_err(|_| {
+			let available = T::Currency::balance_on_hold(&hold_reason, from);
+			if available < amount {
+				// The storage deposit accounting got out of sync with the balance: This would be a
+				// straight up bug in this pallet.
+				log::error!(
+					target: LOG_TARGET,
+					"Failed to refund storage deposit {:?} from contract {:?} to origin {:?}. Not enough deposit: {:?}. This is a bug.",
+					amount, from, to, available,
+				);
+				Error::<T>::StorageRefundNotEnoughFunds.into()
+			} else {
+				// There are some locks preventing the refund. This could be the case if the
+				// contract participates in government. The consequence is that if a contract votes
+				// with its storage deposit it would no longer be possible to remove storage without first
+				// reducing the lock.
+				log::warn!(
+					target: LOG_TARGET,
+					"Failed to refund storage deposit {:?} from contract {:?} to origin {:?}. First remove locks (staking, governance) from the contracts account.",
+					amount, from, to,
+				);
+				Error::<T>::StorageRefundLocked.into()
+			}
+		})
 	}
 
 	/// Returns true if the evm value carries dust.
