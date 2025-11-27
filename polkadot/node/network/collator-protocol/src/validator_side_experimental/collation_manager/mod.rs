8c5a23afbefa80461dbe1eb44f5ba557592e0a60
Tsvetomir Dimitrov <tsvetomir@parity.io>
2025-11-27 14:22:23 +0200

    Additional logs

--- polkadot/node/network/collator-protocol/src/validator_side_experimental/collation_manager/mod.rs
+++ polkadot/node/network/collator-protocol/src/validator_side_experimental/collation_manager/mod.rs
@@ -677,6 +677,8 @@ impl CollationManager {
 				target: LOG_TARGET,
 				?time_since_advertisement,
 				?highest_rep_of_para,
+				maybe_candidate_hash=?advertisement.candidate_hash(),
+				peer_id=?advertisement.peer_id,
 				"Skipping advertisement, as the peer doesn't have a high enough reputation to warrant a fetch now"
 			);
 
