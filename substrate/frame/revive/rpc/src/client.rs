45fdcec0c7a8b6bd5382a5fe80e839e476e13aba
Alexandru Cihodaru <alexandru.cihodaru@parity.io>
2025-11-28 10:12:29 +0200

    feedback v1
    Signed-off-by: Alexandru Cihodaru <alexandru.cihodaru@parity.io>

--- substrate/frame/revive/rpc/src/client.rs
+++ substrate/frame/revive/rpc/src/client.rs
@@ -257,9 +257,13 @@ impl Client {
 	}
 
 	/// Creates a block notifier instance.
-	pub fn create_block_notifier(&mut self, capacity: Option<usize>) {
-		self.block_notifier =
-			Some(tokio::sync::broadcast::channel::<H256>(capacity.unwrap_or(NOTIFIER_CAPACITY)).0);
+	pub fn create_block_notifier(&mut self) {
+		self.block_notifier = Some(tokio::sync::broadcast::channel::<H256>(NOTIFIER_CAPACITY).0);
+	}
+
+	/// Sets a block notifier
+	pub fn set_block_notifier(&mut self, notifier: Option<tokio::sync::broadcast::Sender<H256>>) {
+		self.block_notifier = notifier;
 	}
 
 	/// Subscribe to past blocks executing the callback for each block in `range`.
