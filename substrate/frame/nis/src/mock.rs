928ca73036f6ecb25cb66e9992ca9b34eb10fd64
Paolo La Camera <paolo@parity.io>
2025-12-05 18:43:59 +0100

    fix NIS pallet

--- substrate/frame/nis/src/mock.rs
+++ substrate/frame/nis/src/mock.rs
@@ -71,6 +71,7 @@ impl pallet_balances::Config<pallet_balances::Instance1> for Test {
 	type RuntimeHoldReason = RuntimeHoldReason;
 	type RuntimeFreezeReason = RuntimeFreezeReason;
 	type DoneSlashHandler = ();
+	type BurnDestination = ();
 }
 
 impl pallet_balances::Config<pallet_balances::Instance2> for Test {
@@ -92,6 +93,7 @@ impl pallet_balances::Config<pallet_balances::Instance2> for Test {
 	type RuntimeHoldReason = ();
 	type RuntimeFreezeReason = ();
 	type DoneSlashHandler = ();
+	type BurnDestination = ();
 }
 
 parameter_types! {
