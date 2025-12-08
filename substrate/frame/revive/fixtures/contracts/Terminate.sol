ab596291db6ce2598752449e0769b66ac1bc603c
Robert van Eerdewijk <robertvaneerdewijk@gmail.com>
2025-12-08 16:44:38 +0100

    undo change

--- substrate/frame/revive/fixtures/contracts/Terminate.sol
+++ substrate/frame/revive/fixtures/contracts/Terminate.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.28;
+pragma solidity ^0.8.30;
 
 import "@revive/ISystem.sol";
 
--- substrate/frame/revive/fixtures/contracts/TerminateCaller.sol
+++ substrate/frame/revive/fixtures/contracts/TerminateCaller.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.28;
+pragma solidity ^0.8.30;
 
 import { Terminate } from "./Terminate.sol";
 import { TerminateDelegator } from "./TerminateDelegator.sol";
