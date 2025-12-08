9b430a9b752770c2d00d363131301505cf335c9d
Alexandru Vasile <alexandru.vasile@parity.io>
2025-12-08 13:25:02 +0000

    Revert "TEMP: Enable elastic scaling for AHWestend to filter out tests"
    This reverts commit cb2444a18a63d5592c05efda637a11f0a2aaef38.

--- cumulus/parachains/runtimes/assets/asset-hub-westend/src/lib.rs
+++ cumulus/parachains/runtimes/assets/asset-hub-westend/src/lib.rs
@@ -133,22 +133,6 @@ use xcm::latest::prelude::{
 	NetworkId, NonFungible, ParentThen, Response, WeightLimit, XCM_VERSION,
 };
 
-/// Build with an offset of 1 behind the relay chain.
-const RELAY_PARENT_OFFSET: u32 = 1;
-
-/// The upper limit of how many parachain blocks are processed by the relay chain per
-/// parent. Limits the number of blocks authored per slot. This determines the minimum
-/// block time of the parachain:
-/// `RELAY_CHAIN_SLOT_DURATION_MILLIS/BLOCK_PROCESSING_VELOCITY`
-const BLOCK_PROCESSING_VELOCITY: u32 = 3;
-
-/// Maximum number of blocks simultaneously accepted by the Runtime, not yet included
-/// into the relay chain.
-const UNINCLUDED_SEGMENT_CAPACITY: u32 = (2 + RELAY_PARENT_OFFSET) * BLOCK_PROCESSING_VELOCITY + 1;
-
-/// Relay chain slot duration, in milliseconds.
-const RELAY_CHAIN_SLOT_DURATION_MILLIS: u32 = 6000;
-
 impl_opaque_keys! {
 	pub struct SessionKeys {
 		pub aura: Aura,
@@ -918,7 +902,7 @@ impl cumulus_pallet_parachain_system::Config for Runtime {
 	type ReservedXcmpWeight = ReservedXcmpWeight;
 	type CheckAssociatedRelayNumber = RelayNumberMonotonicallyIncreases;
 	type ConsensusHook = ConsensusHook;
-	type RelayParentOffset = ConstU32<RELAY_PARENT_OFFSET>;
+	type RelayParentOffset = ConstU32<0>;
 }
 
 type ConsensusHook = cumulus_pallet_aura_ext::FixedVelocityConsensusHook<
@@ -1762,7 +1746,7 @@ pallet_revive::impl_runtime_apis_plus_revive_traits!(
 
 	impl cumulus_primitives_core::RelayParentOffsetApi<Block> for Runtime {
 		fn relay_parent_offset() -> u32 {
-			RELAY_PARENT_OFFSET
+			0
 		}
 	}
 
