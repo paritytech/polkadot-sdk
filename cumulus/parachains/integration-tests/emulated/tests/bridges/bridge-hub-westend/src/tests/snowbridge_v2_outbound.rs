c5711dcd21482fc60c8283374e12b63e6e255457
Adrian Catangiu <adrian@parity.io>
2025-12-12 19:52:23 +0200

    fix bridge-hubs emulated tests

--- cumulus/parachains/integration-tests/emulated/tests/bridges/bridge-hub-westend/src/tests/snowbridge_v2_outbound.rs
+++ cumulus/parachains/integration-tests/emulated/tests/bridges/bridge-hub-westend/src/tests/snowbridge_v2_outbound.rs
@@ -390,7 +390,7 @@ fn transfer_relay_token_from_ah() {
 		assert!(
 			events.iter().any(|event| matches!(
 				event,
-				RuntimeEvent::Balances(pallet_balances::Event::Minted { who, amount})
+				RuntimeEvent::Balances(pallet_balances::Event::Deposit { who, amount})
 					if *who == ethereum_sovereign.clone() && *amount == TOKEN_AMOUNT,
 			)),
 			"native token reserved to Ethereum sovereign account."
@@ -781,7 +781,7 @@ fn register_token_from_penpal() {
 		type RuntimeEvent = <AssetHubWestend as Chain>::RuntimeEvent;
 		assert_expected_events!(
 			AssetHubWestend,
-			vec![RuntimeEvent::ForeignAssets(pallet_assets::Event::Burned { .. }) => {},]
+			vec![RuntimeEvent::ForeignAssets(pallet_assets::Event::Withdrawn { .. }) => {},]
 		);
 	});
 
@@ -929,7 +929,7 @@ fn send_message_from_penpal_to_ethereum(sudo: bool) {
 		);
 		assert_expected_events!(
 			AssetHubWestend,
-			vec![RuntimeEvent::ForeignAssets(pallet_assets::Event::Issued { .. }) => {},]
+			vec![RuntimeEvent::ForeignAssets(pallet_assets::Event::Deposited { .. }) => {},]
 		);
 	});
 
