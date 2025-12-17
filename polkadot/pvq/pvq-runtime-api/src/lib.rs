22d3772245fe402ff98e3228ad64c91a510261b3
Jiyuan Zheng <indirection42@outlook.com>
2025-12-17 14:00:23 +0800

    Remove PVQ executor and runtime API crates
    This commit deletes the `pvq-executor` and `pvq-runtime-api` crates, along with their associated README files. The `pvq-executor` crate, which provided functionality for executing PVQ programs on PolkaVM, and the `pvq-runtime-api` crate, which defined the runtime API for PVQ, have been removed from the project. Additionally, the `pvq-primitives` crate remains intact, as it defines shared types for PVQ execution.
    
    - Deleted `pvq-executor` crate and its README.
    - Deleted `pvq-runtime-api` crate and its README.
    - Retained `pvq-primitives` crate for shared types.

--- polkadot/pvq/pvq-runtime-api/src/lib.rs
+++ polkadot/pvq/pvq-runtime-api/src/lib.rs
@@ -1,4 +1,39 @@
-#![doc = include_str!("../README.md")]
+// This file is part of Substrate.
+
+// Copyright (C) Parity Technologies (UK) Ltd.
+// SPDX-License-Identifier: GPL-3.0-or-later WITH Classpath-exception-2.0
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+//! Runtime API for PVQ (PolkaVM Query).
+//!
+//! Defines [`PvqApi`] for executing PolkaVM programs within the runtime.
+//!
+//! # Guest ABI
+//!
+//! The `args` parameter to [`PvqApi::execute_query`] is SCALE-encoded as:
+//!
+//! ```text
+//! [selector: u32][arg1][arg2]...[argN]
+//! ```
+//!
+//! Where `selector` identifies the runtime extension to invoke.
+//!
+//! # Safety
+//!
+//! Guest programs run sandboxed with gas metering. Invalid programs return [`PvqError`].
+
 #![cfg_attr(not(feature = "std"), no_std)]
 
 extern crate alloc;
