d93725f2625182012e52ac5cbb21cb103b2f0258
Robert van Eerdewijk <robertvaneerdewijk@gmail.com>
2025-12-19 14:27:01 +0100

    NonZero -> NonZeroU32

--- substrate/frame/revive/ui-tests/src/ui/precompiles_ui.rs
+++ substrate/frame/revive/ui-tests/src/ui/precompiles_ui.rs
@@ -1,7 +1,7 @@
 use polkadot_sdk::pallet_revive::precompiles::PrimitivePrecompile;
 use polkadot_sdk::pallet_revive::Config;
 use polkadot_sdk::pallet_revive::precompiles::BuiltinAddressMatcher;
-use core::num::NonZero;
+use core::num::NonZeroU32;
 
 use ui_tests::runtime::Runtime;
 use core::marker::PhantomData;
@@ -10,7 +10,7 @@ pub struct PrecompileA<T>(PhantomData<T>);
 
 impl<T: Config> PrimitivePrecompile for PrecompileA<T> {
 	type T = T;
-	const MATCHER: BuiltinAddressMatcher = BuiltinAddressMatcher::Fixed(NonZero::new(0x666).unwrap());
+	const MATCHER: BuiltinAddressMatcher = BuiltinAddressMatcher::Fixed(NonZeroU32::new(0x666).unwrap());
 	const HAS_CONTRACT_INFO: bool = false;
 }
 
@@ -18,7 +18,7 @@ pub struct PrecompileB<T>(PhantomData<T>);
 
 impl<T: Config> PrimitivePrecompile for PrecompileB<T> {
 	type T = T;
-	const MATCHER: BuiltinAddressMatcher = BuiltinAddressMatcher::Fixed(NonZero::new(0x666).unwrap());
+	const MATCHER: BuiltinAddressMatcher = BuiltinAddressMatcher::Fixed(NonZeroU32::new(0x666).unwrap());
 	const HAS_CONTRACT_INFO: bool = false;
 }
 
