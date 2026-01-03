492957c4f3d270ed4202c06ddd0b216a44d8f1f4
Lucas Grasso <lucasgrassoramos@gmail.com>
2026-01-03 07:48:08 -0300

    fix product references

--- substrate/frame/referenda/precompiles/src/lib.rs
+++ substrate/frame/referenda/precompiles/src/lib.rs
@@ -1,5 +1,5 @@
 // Copyright (C) Parity Technologies (UK) Ltd.
-// This file is part of Polkadot.
+// This file is part of Substrate.
 // SPDX-License-Identifier: Apache-2.0
 
 // Licensed under the Apache License, Version 2.0 (the "License");
