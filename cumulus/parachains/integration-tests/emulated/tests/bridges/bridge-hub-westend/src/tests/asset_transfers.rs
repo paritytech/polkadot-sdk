77245cd0340b0fc91ece99d4defbde15937d777e
Christian Langenbacher <clangenb+gh@protonmail.ch>
2026-01-07 17:38:42 +0100

    [integration-tests] fix send_wnds_from_westend_relay_through_asset_hub_westend_to_asset_hub_rococo_to_penpal_rococo

--- cumulus/parachains/integration-tests/emulated/tests/bridges/bridge-hub-westend/src/tests/asset_transfers.rs
+++ cumulus/parachains/integration-tests/emulated/tests/bridges/bridge-hub-westend/src/tests/asset_transfers.rs
@@ -591,7 +591,7 @@ fn send_wnds_from_westend_relay_through_asset_hub_westend_to_asset_hub_rococo_to
 			)],
 		));
 	});
-	create_foreign_pool_with_parent_native_on!(
+	create_foreign_pool_with_native_on!(
 		PenpalA,
 		wnd_at_rococo_parachains.clone(),
 		asset_owner.clone()
