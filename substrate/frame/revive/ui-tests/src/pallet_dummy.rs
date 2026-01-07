f876025f991031ed713408fe3c91bcdeaa31dc61
Robert van Eerdewijk <robertvaneerdewijk@gmail.com>
2026-01-07 11:13:21 +0100

    clippy

--- substrate/frame/revive/ui-tests/src/pallet_dummy.rs
+++ substrate/frame/revive/ui-tests/src/pallet_dummy.rs
@@ -17,37 +17,11 @@
 
 #[polkadot_sdk::frame_support::pallet(dev_mode)]
 pub mod pallet {
-	use polkadot_sdk::{
-		frame_support::{
-			dispatch::{Pays, PostDispatchInfo},
-			ensure,
-			pallet_prelude::DispatchResultWithPostInfo,
-			weights::Weight,
-		},
-		frame_system::pallet_prelude::*,
-	};
+    #![allow(dead_code, unused)]
 
 	#[pallet::pallet]
 	pub struct Pallet<T>(_);
 
 	#[pallet::config]
 	pub trait Config: polkadot_sdk::frame_system::Config {}
-
-	#[pallet::call]
-	impl<T: Config> Pallet<T> {
-		/// Dummy function that overcharges the predispatch weight, allowing us to test the correct
-		/// values of [`ContractResult::gas_consumed`] and [`ContractResult::gas_required`] in
-		/// tests.
-		#[pallet::call_index(1)]
-		#[pallet::weight(*pre_charge)]
-		pub fn overestimate_pre_charge(
-			origin: OriginFor<T>,
-			pre_charge: Weight,
-			actual_weight: Weight,
-		) -> DispatchResultWithPostInfo {
-			ensure_signed(origin)?;
-			ensure!(pre_charge.any_gt(actual_weight), "pre_charge must be > actual_weight");
-			Ok(PostDispatchInfo { actual_weight: Some(actual_weight), pays_fee: Pays::Yes })
-		}
-	}
 }
