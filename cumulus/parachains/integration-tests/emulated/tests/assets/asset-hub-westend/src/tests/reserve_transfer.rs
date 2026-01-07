b4b0fab8abc88d67d2268b1e7a2b48868668d8e7
Christian Langenbacher <clangenb+gh@protonmail.ch>
2026-01-07 14:38:28 +0100

    [integration-tests] fix reserve_transfer_native_asset_from_para_to_relay

--- cumulus/parachains/integration-tests/emulated/tests/assets/asset-hub-westend/src/tests/reserve_transfer.rs
+++ cumulus/parachains/integration-tests/emulated/tests/assets/asset-hub-westend/src/tests/reserve_transfer.rs
@@ -869,8 +869,8 @@ fn reserve_transfer_native_asset_from_para_to_relay() {
 	let sender_assets_after = foreign_balance_on!(PenpalA, relay_native_asset_location, &sender);
 	let receiver_balance_after = test.receiver.balance;
 
-	// Sender's balance is reduced by amount sent plus delivery fees
-	assert!(sender_assets_after < sender_assets_before - amount_to_send);
+	// Sender's balance is reduced by amount sent (delivery fees are charged in native tokens).
+	assert_eq!(sender_assets_after, sender_assets_before - amount_to_send);
 	// Receiver's asset balance is increased
 	assert!(receiver_balance_after > receiver_balance_before);
 	// Receiver's asset balance increased by `amount_to_send - delivery_fees - bought_execution`;
