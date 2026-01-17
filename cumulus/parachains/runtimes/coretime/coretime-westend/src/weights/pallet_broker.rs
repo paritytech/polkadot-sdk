fe9f4f40691b71890e37410c00b76415a2a53af2
mertwole <mertwole@gmail.com>
2026-01-17 10:54:10 +0100

    Add weight placeholder in the coretime-westend runtime

--- cumulus/parachains/runtimes/coretime/coretime-westend/src/weights/pallet_broker.rs
+++ cumulus/parachains/runtimes/coretime/coretime-westend/src/weights/pallet_broker.rs
@@ -640,4 +640,8 @@ impl<T: frame_system::Config> pallet_broker::WeightInfo for WeightInfo<T> {
 			.saturating_add(T::DbWeight::get().reads(1))
 			.saturating_add(T::DbWeight::get().writes(1))
 	}
+
+	fn remove_potential_renewal() -> Weight {
+		Weight::from_parts(0, 0)
+	}
 }
