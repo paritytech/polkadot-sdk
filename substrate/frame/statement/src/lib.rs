4b73fcc764fb2aed4cd53eaa2c2e02967beecf25
Andrei Eres <eresav@me.com>
2026-01-19 17:54:54 +0100

    Fix

--- substrate/frame/statement/src/lib.rs
+++ substrate/frame/statement/src/lib.rs
@@ -48,10 +48,7 @@ pub mod pallet {
 	pub type AccountIdOf<T> = <T as frame_system::Config>::AccountId;
 
 	#[pallet::config]
-	pub trait Config: frame_system::Config
-	where
-		<Self as frame_system::Config>::AccountId: From<sp_statement_store::AccountId>,
-	{
+	pub trait Config: frame_system::Config {
 		/// The overarching event type.
 		#[allow(deprecated)]
 		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
