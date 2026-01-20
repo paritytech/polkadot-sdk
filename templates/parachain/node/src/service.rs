014ffcbeeae35446988c1d83393dfcf1a7f5c3ea
Alexandru Vasile <alexandru.vasile@parity.io>
2026-01-20 15:43:25 +0000

    net: Spawn network backend as essential task
    Signed-off-by: Alexandru Vasile <alexandru.vasile@parity.io>

--- templates/parachain/node/src/service.rs
+++ templates/parachain/node/src/service.rs
@@ -293,6 +293,7 @@ pub async fn start_parachain_node(
 			transaction_pool: transaction_pool.clone(),
 			para_id,
 			spawn_handle: task_manager.spawn_handle(),
+			spawn_essential_handle: task_manager.spawn_essential_handle(),
 			relay_chain_interface: relay_chain_interface.clone(),
 			import_queue: params.import_queue,
 			sybil_resistance_level: CollatorSybilResistance::Resistant, // because of Aura
