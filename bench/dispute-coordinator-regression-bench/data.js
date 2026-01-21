aaec9101dc7e8f0a0577e581d51329e11526dacb
github-action-benchmark <github@users.noreply.github.com>
2026-01-21 10:14:53 +0000

    add dispute-coordinator-regression-bench (customSmallerIsBetter) benchmark result for 97e54c54a9573daf4d4a5234a42a3d1f054d7f91

--- bench/dispute-coordinator-regression-bench/data.js
+++ bench/dispute-coordinator-regression-bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1768986712608,
+  "lastUpdate": 1768990492226,
   "repoUrl": "https://github.com/paritytech/polkadot-sdk",
   "entries": {
     "dispute-coordinator-regression-bench": [
@@ -21755,6 +21755,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "dispute-coordinator",
             "value": 0.0026597235500000005,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "bruno.devic@parity.io",
+            "name": "BDevParity",
+            "username": "BDevParity"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "97e54c54a9573daf4d4a5234a42a3d1f054d7f91",
+          "message": "[CI|Release] Improve post crates action (#10803)\n\nhttps://github.com/paritytech/release-engineering/issues/280\n\n---------\n\nCo-authored-by: Egor_P <egor@parity.io>",
+          "timestamp": "2026-01-21T08:47:44Z",
+          "tree_id": "601aeb5b6aba51b4e1ad695e6818647d4b4f5427",
+          "url": "https://github.com/paritytech/polkadot-sdk/commit/97e54c54a9573daf4d4a5234a42a3d1f054d7f91"
+        },
+        "date": 1768990470006,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Received from peers",
+            "value": 23.800000000000004,
+            "unit": "KiB"
+          },
+          {
+            "name": "Sent to peers",
+            "value": 227.09999999999997,
+            "unit": "KiB"
+          },
+          {
+            "name": "dispute-coordinator",
+            "value": 0.0027100123199999995,
+            "unit": "seconds"
+          },
+          {
+            "name": "test-environment",
+            "value": 0.006610870189999998,
+            "unit": "seconds"
+          },
+          {
+            "name": "dispute-distribution",
+            "value": 0.00927155994999997,
             "unit": "seconds"
           }
         ]
