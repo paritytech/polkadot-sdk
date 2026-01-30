dbc218f6612362891291283448cf45259fd15860
github-action-benchmark <github@users.noreply.github.com>
2026-01-30 15:53:00 +0000

    add availability-recovery-regression-bench (customSmallerIsBetter) benchmark result for 8807c22f2a3507af2401e99a8c420f68e0ce7dfd

--- bench/availability-recovery-regression-bench/data.js
+++ bench/availability-recovery-regression-bench/data.js
@@ -1,52 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1769785912881,
+  "lastUpdate": 1769788379911,
   "repoUrl": "https://github.com/paritytech/polkadot-sdk",
   "entries": {
     "availability-recovery-regression-bench": [
-      {
-        "commit": {
-          "author": {
-            "email": "karol.k91@gmail.com",
-            "name": "Karol Kokoszka",
-            "username": "karolk91"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": false,
-          "id": "cc5065dab6ab4c40d2901981481afdad8e34d4c2",
-          "message": "transfer_assets benchmarking and weights for coretime chains (#8752)\n\nIntroduces implementation of `set_up_complex_asset_transfer()` to\ncorrectly benchmark weights for `transfer_assets` extrinsics on Rococo\nCoretime and Westend Coretime. Introducing also test scenarios to cover\ncommon xcm teleport use cases\n\n---------\n\nCo-authored-by: cmd[bot] <41898282+github-actions[bot]@users.noreply.github.com>",
-          "timestamp": "2025-06-25T10:15:30Z",
-          "tree_id": "b73dcd429b1c64f19dff4df585da6daa472bf178",
-          "url": "https://github.com/paritytech/polkadot-sdk/commit/cc5065dab6ab4c40d2901981481afdad8e34d4c2"
-        },
-        "date": 1750851393874,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Received from peers",
-            "value": 307203,
-            "unit": "KiB"
-          },
-          {
-            "name": "Sent to peers",
-            "value": 1.6666666666666665,
-            "unit": "KiB"
-          },
-          {
-            "name": "availability-recovery",
-            "value": 11.23164101236667,
-            "unit": "seconds"
-          },
-          {
-            "name": "test-environment",
-            "value": 0.19994607900000003,
-            "unit": "seconds"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -21999,6 +21955,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "test-environment",
             "value": 0.1273800988,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "60601340+lexnv@users.noreply.github.com",
+            "name": "Alexandru Vasile",
+            "username": "lexnv"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "8807c22f2a3507af2401e99a8c420f68e0ce7dfd",
+          "message": "telemtry: Downgrade spam log to debug (#10201)\n\nThis PR downgrade the telemetry warning log to debug.\n- The log is causing a lot of noise in our test nets:\nhttps://grafana.teleport.parity.io/goto/fjTQ_vzDg?orgId=1\n\n---------\n\nSigned-off-by: Alexandru Vasile <alexandru.vasile@parity.io>\nCo-authored-by: Bastian Köcher <git@kchr.de>\nCo-authored-by: cmd[bot] <41898282+github-actions[bot]@users.noreply.github.com>",
+          "timestamp": "2026-01-30T14:40:21Z",
+          "tree_id": "9dc6998aca49d4847b64529b702da9955d0b891b",
+          "url": "https://github.com/paritytech/polkadot-sdk/commit/8807c22f2a3507af2401e99a8c420f68e0ce7dfd"
+        },
+        "date": 1769788354277,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Sent to peers",
+            "value": 1.6666666666666665,
+            "unit": "KiB"
+          },
+          {
+            "name": "Received from peers",
+            "value": 307203,
+            "unit": "KiB"
+          },
+          {
+            "name": "availability-recovery",
+            "value": 11.088220575533335,
+            "unit": "seconds"
+          },
+          {
+            "name": "test-environment",
+            "value": 0.11656056453333334,
             "unit": "seconds"
           }
         ]
