547143dbe9ad6ce5c4016dc37af90ee961fed518
Leonardo Razovic <lrazovic@gmail.com>
2026-02-05 12:28:20 +0000

    chore: add header license

--- substrate/frame/vaults/src/migrations/v1.rs
+++ substrate/frame/vaults/src/migrations/v1.rs
@@ -1,3 +1,20 @@
+// This file is part of Substrate.
+
+// Copyright (C) Amforc AG.
+// SPDX-License-Identifier: Apache-2.0
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 //! Migration to V1: Initialize pallet parameters.
 //!
 //! This migration sets initial values for all pallet parameters when deploying
@@ -90,7 +107,7 @@ pub trait InitialVaultsConfig<T: Config> {
 /// This struct implements the actual migration. It is wrapped by
 /// [`MigrateToV1`] which uses [`VersionedMigration`] to handle version
 /// checking and updating automatically.
-pub struct InnerMigrateV0ToV1<T, I>(core::marker::PhantomData<(T, I)>);
+struct InnerMigrateV0ToV1<T, I>(core::marker::PhantomData<(T, I)>);
 
 impl<T: Config, I: InitialVaultsConfig<T>> UncheckedOnRuntimeUpgrade for InnerMigrateV0ToV1<T, I> {
 	fn on_runtime_upgrade() -> Weight {
