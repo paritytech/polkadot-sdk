b484e5de215c429060acfeb6ae83d9747edaf72b
krayt78 <ludovic.domingues96@gmail.com>
2026-02-15 10:03:52 +0100

    added preimage migration to two runtimes

--- substrate/frame/staking-async/runtimes/rc/src/lib.rs
+++ substrate/frame/staking-async/runtimes/rc/src/lib.rs
@@ -1742,7 +1742,10 @@ parameter_types! {
 impl pallet_migrations::Config for Runtime {
 	type RuntimeEvent = RuntimeEvent;
 	#[cfg(not(feature = "runtime-benchmarks"))]
-	type Migrations = pallet_identity::migration::v2::LazyMigrationV1ToV2<Runtime>;
+	type Migrations = (
+		pallet_identity::migration::v2::LazyMigrationV1ToV2<Runtime>,
+		pallet_preimage::migration::v2::LazyMigrationV1ToV2<Runtime, Balances>,
+	);
 	// Benchmarks need mocked migrations to guarantee that they succeed.
 	#[cfg(feature = "runtime-benchmarks")]
 	type Migrations = pallet_migrations::mock_helpers::MockedMigrations;
