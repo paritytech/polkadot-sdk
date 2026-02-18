2f322097c86f68e2437786b35e3bca3cbe8c9adb
krayt78 <ludovic.domingues96@gmail.com>
2026-02-18 16:56:59 +0100

    fmt

--- cumulus/parachains/runtimes/bridge-hubs/bridge-hub-westend/src/lib.rs
+++ cumulus/parachains/runtimes/bridge-hubs/bridge-hub-westend/src/lib.rs
@@ -555,9 +555,7 @@ parameter_types! {
 impl pallet_migrations::Config for Runtime {
 	type RuntimeEvent = RuntimeEvent;
 	#[cfg(not(feature = "runtime-benchmarks"))]
-	type Migrations = (
-		pallet_multisig::migrations::v2::LazyMigrationV1ToV2<Runtime, Balances>,
-	);
+	type Migrations = (pallet_multisig::migrations::v2::LazyMigrationV1ToV2<Runtime, Balances>,);
 	#[cfg(feature = "runtime-benchmarks")]
 	type Migrations = pallet_migrations::mock_helpers::MockedMigrations;
 	type CursorMaxLen = ConstU32<65_536>;
