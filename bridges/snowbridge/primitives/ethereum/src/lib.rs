237f0bd16d5be035e79c8e3b0165c045758517b6
ron <yrong1997@gmail.com>
2026-02-23 01:32:07 +0800

    Receipt verification with alloy primitives

--- bridges/snowbridge/primitives/ethereum/src/lib.rs
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: Apache-2.0
-// SPDX-FileCopyrightText: 2023 Snowfork <hello@snowfork.com>
-#![cfg_attr(not(feature = "std"), no_std)]
-
-pub mod mpt;
-pub use alloy_consensus::{Receipt, ReceiptEnvelope, ReceiptWithBloom, RlpDecodableReceipt};
-pub use alloy_primitives::Log;
-pub use alloy_rlp::{Decodable, Encodable};
