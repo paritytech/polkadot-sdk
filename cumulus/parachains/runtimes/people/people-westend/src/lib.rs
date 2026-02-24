b0f21b4b69831a62574c1d27553e33f9e73a6193
Paolo La Camera <paolo@parity.io>
2026-02-24 14:46:06 +0100

    people-westend: add frame-metadata-hash-extension

--- cumulus/parachains/runtimes/people/people-westend/src/lib.rs
+++ cumulus/parachains/runtimes/people/people-westend/src/lib.rs
@@ -105,6 +105,7 @@ pub type TxExtension = cumulus_pallet_weight_reclaim::StorageWeightReclaim<
 		frame_system::CheckNonce<Runtime>,
 		frame_system::CheckWeight<Runtime>,
 		pallet_transaction_payment::ChargeTransactionPayment<Runtime>,
+		frame_metadata_hash_extension::CheckMetadataHash<Runtime>,
 	),
 >;
 
