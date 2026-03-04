cc625d4d0e820ec0f0cafe811698ba495fe61226
Giovanny Gongora <gioyik@gmail.com>
2026-03-04 01:13:35 +0100

    Update substrate/primitives/consensus/beefy/src/lib.rs
    Co-authored-by: Bastian Köcher <git@kchr.de>

--- substrate/primitives/consensus/beefy/src/lib.rs
+++ substrate/primitives/consensus/beefy/src/lib.rs
@@ -149,7 +149,9 @@ pub mod ecdsa_crypto {
 				Ok(arr) => arr,
 				Err(_) => return false,
 			};
-			if !sp_core::ecdsa::is_signature_normalized(sig_array) {
+			let Some(sig_array) : Option<&[u8; 65]> = match sig_bytes.try_into().ok() else {
+				return false
+			};
 				return false;
 			}
 			let msg_hash = keccak_256(msg);
