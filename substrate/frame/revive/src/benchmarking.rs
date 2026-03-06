e4de7cbcb06a18999f5d08d96ffa1de8f7fbe8f5
pgherveou <pgherveou@gmail.com>
2026-03-06 22:02:06 +0000

    pallet-revive: fix benchmark compilation with runtime-benchmarks feature

--- substrate/frame/revive/src/benchmarking.rs
+++ substrate/frame/revive/src/benchmarking.rs
@@ -148,7 +148,8 @@ mod benchmarks {
 		let mut authorization_list = vec![];
 		for i in 0..n {
 			let key_material = keccak_256(&(i as u32).to_le_bytes());
-			let pair = sp_core::ecdsa::Pair::from_seed_slice(&key_material).expect("valid key; qed");
+			let pair =
+				sp_core::ecdsa::Pair::from_seed_slice(&key_material).expect("valid key; qed");
 			let signed_auth = eip7702::sign_authorization(&pair, chain_id, target, U256::zero());
 			authorization_list.push(signed_auth);
 		}
@@ -186,7 +187,8 @@ mod benchmarks {
 		let mut authorization_list = vec![];
 		for i in 0..n {
 			let key_material = keccak_256(&(i as u32).to_le_bytes());
-			let pair = sp_core::ecdsa::Pair::from_seed_slice(&key_material).expect("valid key; qed");
+			let pair =
+				sp_core::ecdsa::Pair::from_seed_slice(&key_material).expect("valid key; qed");
 
 			let eth_address = eip7702::eth_address(&pair);
 			let account_id = T::AddressMapper::to_account_id(&eth_address);
