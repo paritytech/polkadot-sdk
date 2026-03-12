0dfe01672944362796daa307f861d965045cf971
ron <yrong1997@gmail.com>
2026-03-12 21:41:46 +0800

    Cleanup

--- bridges/snowbridge/pallets/inbound-queue-v2/src/api.rs
+++ bridges/snowbridge/pallets/inbound-queue-v2/src/api.rs
@@ -1,10 +1,9 @@
-use crate::Config;
-use crate::Nonce;
+use crate::{Config, Nonce};
 use snowbridge_core::sparse_bitmap::SparseBitmap;
 
 pub fn is_message_relayed<T>(nonce: u64) -> bool
 where
 	T: Config,
 {
-	return Nonce::<T>::get(nonce);
+	Nonce::<T>::get(nonce)
 }
