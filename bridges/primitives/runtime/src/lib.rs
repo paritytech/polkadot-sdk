ef267dc5233faca43b1760eb0fad52a793fff5f6
nasihudeen04@gmail.com <nasihudeen04@gmail.com>
2026-03-17 18:43:27 +0100

    fix rustfmt import order

--- bridges/primitives/runtime/src/lib.rs
+++ bridges/primitives/runtime/src/lib.rs
@@ -36,7 +36,7 @@ use sp_std::{ops::RangeInclusive, vec, vec::Vec};
 pub use chain::{
 	AccountIdOf, AccountPublicOf, BalanceOf, BlockNumberOf, Chain, EncodedOrDecodedCall,
 	HashOf, HasherOf, HeaderOf, NonceOf, Parachain, ParachainIdOf, SignatureOf, TransactionEraOf,
-	UnderlyingChainOf, UnderlyingChainProvider, __private
+	UnderlyingChainOf, UnderlyingChainProvider, __private,
 };
 pub use frame_support::storage::storage_prefix as storage_value_final_key;
 use num_traits::{CheckedAdd, CheckedSub, One, SaturatingAdd, Zero};
