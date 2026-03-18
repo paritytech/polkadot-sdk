9d5b25544298e97a639584329a3c48148698d82d
pgherveou <pgherveou@gmail.com>
2026-03-18 16:28:00 +0100

    revive: Use if let-chains in AutoMapper

--- substrate/frame/revive/src/address.rs
+++ substrate/frame/revive/src/address.rs
@@ -272,26 +272,26 @@ pub struct AutoMapper<T>(PhantomData<T>);
 
 impl<T: Config> OnNewAccount<T::AccountId> for AutoMapper<T> {
 	fn on_new_account(who: &T::AccountId) {
-		if T::AutoMap::get() {
-			if let Err(err) = T::AddressMapper::map_no_deposit(who) {
-				log::warn!(
-					target: crate::LOG_TARGET,
-					"Failed to auto-map account {who:?}: {err:?}",
-				);
-			}
+		if T::AutoMap::get() &&
+			let Err(err) = T::AddressMapper::map_no_deposit(who)
+		{
+			log::warn!(
+				target: crate::LOG_TARGET,
+				"Failed to auto-map account {who:?}: {err:?}",
+			);
 		}
 	}
 }
 
 impl<T: Config> OnKilledAccount<T::AccountId> for AutoMapper<T> {
 	fn on_killed_account(who: &T::AccountId) {
-		if T::AutoMap::get() {
-			if let Err(err) = T::AddressMapper::unmap(who) {
-				log::warn!(
-					target: crate::LOG_TARGET,
-					"Failed to auto-unmap account {who:?}: {err:?}",
-				);
-			}
+		if T::AutoMap::get() &&
+			let Err(err) = T::AddressMapper::unmap(who)
+		{
+			log::warn!(
+				target: crate::LOG_TARGET,
+				"Failed to auto-unmap account {who:?}: {err:?}",
+			);
 		}
 	}
 }
