b7071e29ef5469e90ee1bf11c0f31a818aa07867
Tsvetomir Dimitrov <tsvetomir@parity.io>
2026-03-18 16:43:09 +0200

    Decrease the log level for claim queue inconsistency in `ClaimQueueState`

--- polkadot/node/network/collator-protocol/src/validator_side/claim_queue_state/basic.rs
+++ polkadot/node/network/collator-protocol/src/validator_side/claim_queue_state/basic.rs
@@ -173,11 +173,12 @@ impl ClaimQueueState {
 				Some(future_block) => {
 					let future_claim = future_block.claim.as_ref();
 					if future_claim != Some(expected_claim) {
-						gum::warn!(
+						gum::debug!(
 							target: LOG_TARGET,
 							?future_claim,
 							?expected_claim,
-							"Inconsistency while adding a leaf to the `ClaimQueueState`."
+							leaf=hash,
+							"Inconsistency while adding a leaf to the `ClaimQueueState`. Expected on session change."
 						);
 
 						// There is an inconsistency. Update our view with the one from the claim
