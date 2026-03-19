6c81137f2446ae42b34c0c1f3a04c557004322a6
muharem <ismailov.m.h@gmail.com>
2026-03-19 16:55:55 +0100

    fmt

--- substrate/frame/support/src/traits/tokens/fungible/mod.rs
+++ substrate/frame/support/src/traits/tokens/fungible/mod.rs
@@ -40,8 +40,7 @@
 //!   which guarantee eventual book-keeping. May be useful for some sophisticated operations where
 //!   funds must be removed from an account before it is known precisely what should be done with
 //!   them.
-//! - [`metadata::Inspect`]: Inspector functions for token metadata (name,
-//!   symbol, decimals).
+//! - [`metadata::Inspect`]: Inspector functions for token metadata (name, symbol, decimals).
 //! - [`metadata::Mutate`]: Mutator functions for token metadata.
 //!
 //! ## Terminology
