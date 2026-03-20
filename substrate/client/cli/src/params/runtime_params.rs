cb97b867aca68337ef9b03f6ab6d22d0afa67a39
prop-opentensor <162177546+prop-opentensor@users.noreply.github.com>
2026-03-20 02:28:11 +0100

    bump --max-runtime-instances cap from 32 to 128
    addresses underutilization on high corecount nodes.

--- substrate/client/cli/src/params/runtime_params.rs
+++ substrate/client/cli/src/params/runtime_params.rs
@@ -37,8 +37,8 @@ fn parse_max_runtime_instances(s: &str) -> Result<usize, String> {
 	let max_runtime_instances = usize::from_str(s)
 		.map_err(|_err| format!("Illegal `--max-runtime-instances` value: {s}"))?;
 
-	if max_runtime_instances > 32 {
-		Err(format!("Illegal `--max-runtime-instances` value: {max_runtime_instances} is more than the allowed maximum of `32` "))
+	if max_runtime_instances > 128 {
+		Err(format!("Illegal `--max-runtime-instances` value: {max_runtime_instances} is more than the allowed maximum of `128` "))
 	} else {
 		Ok(max_runtime_instances)
 	}
