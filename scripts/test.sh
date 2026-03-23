c7ec86e1e9c790a23d89bb08b68b12385f88886c
Loris Moulin <contact@lorismoulin.com>
2026-03-23 18:54:10 +0100

    fix taplo config and added scripts

---
+++ scripts/test.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+export RUSTFLAGS="${RUSTFLAGS:--Cdebug-assertions=y}"
+export SUBSTRATE_ALLOW_TX_REPLACEMENT=1
+export SKIP_WASM_BUILD=1
+
+echo "RUSTFLAGS=${RUSTFLAGS}"
+echo "SUBSTRATE_ALLOW_TX_REPLACEMENT=${SUBSTRATE_ALLOW_TX_REPLACEMENT}"
+echo "SKIP_WASM_BUILD=${SKIP_WASM_BUILD}"
+echo
+
+cargo nextest run --release --workspace --no-fail-fast \
+	--exclude 'asset-hub-*' \
+	--exclude 'asset-test-utils' \
+	--exclude 'assets-common' \
+	--exclude 'bp-*' \
+	--exclude 'bridge-hub-*' \
+	--exclude 'bridge-runtime-common' \
+	--exclude 'chain-spec-guide-runtime' \
+	--exclude 'collectives-*' \
+	--exclude 'coretime-*' \
+	--exclude 'cumulus-*' \
+	--exclude 'emulated-*' \
+	--exclude 'glutton-*' \
+	--exclude 'governance-*' \
+	--exclude 'minimal-template-*' \
+	--exclude 'pallet-bridge-*' \
+	--exclude 'pallet-collator-selection' \
+	--exclude 'pallet-collective-content' \
+	--exclude 'pallet-minimal-template' \
+	--exclude 'pallet-parachain-template' \
+	--exclude 'pallet-xcm*' \
+	--exclude 'parachain-template*' \
+	--exclude 'parachains-*' \
+	--exclude 'penpal-*' \
+	--exclude 'people-*' \
+	--exclude 'polkadot-*' \
+	--exclude 'relay-*' \
+	--exclude 'rococo-*' \
+	--exclude 'snowbridge-*' \
+	--exclude 'staging-parachain-info' \
+	--exclude 'staging-xcm*' \
+	--exclude 'substrate-relay-helper' \
+	--exclude 'template-zombienet-*' \
+	--exclude 'test-parachain-*' \
+	--exclude 'test-runtime-constants' \
+	--exclude 'testnet-*' \
+	--exclude 'tracing-gum*' \
+	--exclude 'westend-*' \
+	--exclude 'xcm-*' \
+	--exclude 'yet-another-*' \
+	--exclude 'zombienet-*' \
+	"$@"
