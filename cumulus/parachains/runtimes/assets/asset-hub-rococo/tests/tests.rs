bd302adb366ae2b0b9b2e145bc28ac06d2369a51
Adrian Catangiu <adrian@parity.io>
2026-04-08 17:37:20 +0300

    fix tests

--- cumulus/parachains/runtimes/assets/asset-hub-rococo/tests/tests.rs
+++ cumulus/parachains/runtimes/assets/asset-hub-rococo/tests/tests.rs
@@ -813,7 +813,7 @@ fn limited_reserve_transfer_assets_for_native_asset_over_bridge_works(
 		}),
 		bridging_configuration,
 		WeightLimit::Unlimited,
-		Some(xcm_config::bridging::XcmBridgeHubRouterFeeAssetId::get()),
+		None,
 		Some(xcm_config::TreasuryAccount::get()),
 	)
 }
