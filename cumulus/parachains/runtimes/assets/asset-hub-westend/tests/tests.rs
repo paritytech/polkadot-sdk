23dd768b19b4e113bcd11c660727fdc5e94bfea0
Paolo La Camera <paolo@parity.io>
2026-04-09 08:46:10 +0200

    fix integration test

--- cumulus/parachains/runtimes/assets/asset-hub-westend/tests/tests.rs
+++ cumulus/parachains/runtimes/assets/asset-hub-westend/tests/tests.rs
@@ -1205,7 +1205,7 @@ fn limited_reserve_transfer_assets_for_native_asset_to_asset_hub_rococo_works() 
 		bridging_to_asset_hub_rococo,
 		WeightLimit::Unlimited,
 		Some(xcm_config::bridging::XcmBridgeHubRouterFeeAssetId::get()),
-		Some(governance::TreasuryAccount::get()),
+		Some(xcm_config::DapBufferAccount::get()),
 	)
 }
 
