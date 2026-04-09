7f64812f73df8a814512fcc39a7e0ff798c43960
Sebastian Kunert <mail@skunert.dev>
2026-04-09 12:38:08 +0200

    Perform additional check

--- cumulus/pallets/parachain-system/src/validate_block/implementation.rs
+++ cumulus/pallets/parachain-system/src/validate_block/implementation.rs
@@ -435,6 +435,15 @@ fn verify_blocks_form_chain<B: BlockT>(blocks: &[B::LazyBlock], parent_header: &
 					block_index, info.index
 				);
 
+				if block_index + 1 < num_blocks {
+					assert!(
+						!info.is_last,
+						"Intermediate block at index {} has `is_last` set, \
+						but more blocks follow in the PoV",
+						block_index
+					);
+				}
+
 				if block_index + 1 == num_blocks &&
 					!CumulusDigestItem::is_last_block_in_core(block.header().digest())
 						.unwrap_or(true)
