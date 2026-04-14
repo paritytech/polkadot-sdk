5b2d5dde4df550709d5ac32b8034e012461cf774
Bastian Köcher <git@kchr.de>
2026-04-14 13:01:17 +0200

    BACKPORT-CONFLICT

--- cumulus/client/consensus/aura/src/collators/slot_based/block_builder_task.rs
+++ cumulus/client/consensus/aura/src/collators/slot_based/block_builder_task.rs
@@ -341,7 +341,8 @@ where
 
 			{
 				let mut runtime_api = para_client.runtime_api();
-				runtime_api.set_call_context(sp_core::traits::CallContext::Onchain);
+				runtime_api
+					.set_call_context(sp_core::traits::CallContext::Onchain { import: false });
 				if let Ok(authorities) = runtime_api.authorities(parent_hash) {
 					connection_helper.update::<P>(para_slot.slot, &authorities).await;
 				}
