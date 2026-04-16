3867a9cf7b1cf793534b606db5ed2ed036d0435e
Bob Singh <bobsingh.dev@users.noreply.gitlab.com>
2026-04-15 23:59:00 -0500

    refactor: use is_empty for active_requests check

--- polkadot/node/core/runtime-api/src/lib.rs
+++ polkadot/node/core/runtime-api/src/lib.rs
@@ -501,7 +501,7 @@ where
 	/// Poll the active runtime API requests.
 	async fn poll_requests(&mut self) {
 		// If there are no active requests, this future should be pending forever.
-		if self.active_requests.len() == 0 {
+		if self.active_requests.is_empty() {
 			return futures::pending!();
 		}
 
