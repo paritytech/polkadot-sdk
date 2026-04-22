4f30a7aaf2d8049a81f07e06c426b7af01e9ac2d
DenzelPenzel <denis.samsonov@parity.io>
2026-04-22 08:49:32 +0100

    statement-store: major sync test incr timeout to 15 sec

--- cumulus/zombienet/zombienet-sdk/tests/zombie_ci/statement_store/integration.rs
+++ cumulus/zombienet/zombienet-sdk/tests/zombie_ci/statement_store/integration.rs
@@ -415,12 +415,12 @@ async fn statement_store_recovery_after_major_sync() -> Result<(), anyhow::Error
 				"block_height{status=\"best\"}",
 				|v| {
 					h.set(v);
-					v >= 15.0
+					v >= 30.0
 				},
-				180u64,
+				360u64,
 			)
 			.await
-			.map_err(|_| anyhow::anyhow!("Charlie did not reach block 15 within 180s"))?;
+			.map_err(|_| anyhow::anyhow!("Charlie did not reach block 30 within 360s"))?;
 		h.get()
 	};
 	info!("Charlie at block {:.0} before dave joins", charlie_height);
