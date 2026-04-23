27269993805edd02c6604ef757da6032be7ad761
pgherveou <pgherveou@gmail.com>
2026-04-23 07:56:40 +0200

    fmt

--- substrate/frame/transaction-payment/pgas-allowance/src/tests.rs
+++ substrate/frame/transaction-payment/pgas-allowance/src/tests.rs
@@ -13,7 +13,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use crate::{mock::*, ChargePGAS, Event, Val};
+use crate::{ChargePGAS, Event, Val, mock::*};
 
 use frame_support::{assert_ok, weights::Weight};
 use pallet_balances::Call as BalancesCall;
