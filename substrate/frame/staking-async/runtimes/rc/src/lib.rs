a8780ebd050b034827f32c141f458ebdb65d0cda
Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
2026-04-28 12:28:33 +0200

    deploy migrations
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>

--- substrate/frame/staking-async/runtimes/rc/src/lib.rs
+++ substrate/frame/staking-async/runtimes/rc/src/lib.rs
@@ -1739,7 +1739,10 @@ parameter_types! {
 impl pallet_migrations::Config for Runtime {
 	type RuntimeEvent = RuntimeEvent;
 	#[cfg(not(feature = "runtime-benchmarks"))]
-	type Migrations = pallet_identity::migration::v2::LazyMigrationV1ToV2<Runtime>;
+	type Migrations = (
+		pallet_identity::migration::v2::LazyMigrationV1ToV2<Runtime>,
+		parachains_dmp::migration::MigrateV0ToV1<Runtime>,
+	);
 	// Benchmarks need mocked migrations to guarantee that they succeed.
 	#[cfg(feature = "runtime-benchmarks")]
 	type Migrations = pallet_migrations::mock_helpers::MockedMigrations;
