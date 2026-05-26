176576e0de317b399db6424cb0889f342648775d
EgorPopelyaev <egor@parity.io>
2026-05-26 09:42:34 +0200

    Fix failing tests job

--- substrate/bin/node/cli/tests/version.rs
+++ substrate/bin/node/cli/tests/version.rs
@@ -14,7 +14,7 @@
 // GNU General Public License for more details.
 
 // You should have received a copy of the GNU General Public License
-// along with this program. If not, see <https://www.gnu.org/licenses/>.
+// along with this program. If not, see <https://www.gnu.org/licenses/>. 
 
 use assert_cmd::cargo::cargo_bin;
 use regex::Regex;
