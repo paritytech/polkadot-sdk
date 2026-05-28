e41d52c2c27b6859042116931e80f419af44985b
Bastian Köcher <git@kchr.de>
2026-05-28 15:54:18 +0100

    xcmp-queue: Store the bytes in the channel status (#12176)
    This improves the performance of xcmp-queue by not requiring to check
    all pages individually.
    
    ---------
    
    Co-authored-by: cmd[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
    (cherry picked from commit 9cd6318cfd2a65da30c8c7f08b49568efd7374d6)

--- cumulus/parachains/runtimes/contracts/contracts-rococo/src/lib.rs
+++ cumulus/parachains/runtimes/contracts/contracts-rococo/src/lib.rs
@@ -115,6 +115,7 @@ pub type Migrations = (
 	cumulus_pallet_xcmp_queue::migration::v4::MigrationToV4<Runtime>,
 	cumulus_pallet_xcmp_queue::migration::v5::MigrateV4ToV5<Runtime>,
 	cumulus_pallet_xcmp_queue::migration::v6::MigrateV5ToV6<Runtime>,
+	cumulus_pallet_xcmp_queue::migration::v7::MigrateV6ToV7<Runtime>,
 	// permanent
 	pallet_xcm::migration::MigrateToLatestXcmVersion<Runtime>,
 );
