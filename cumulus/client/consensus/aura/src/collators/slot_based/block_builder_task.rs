ca1c2d37afc0b154a7765a29cbc4641563740b19
Iulian Barbu <iulian.barbu@parity.io>
2026-06-08 19:33:37 +0000

    cumulus: carry unincluded segment in CollatorResubmitSegment
    Add an `unincluded_segment: Vec<Block::Header>` field to
    `CollatorResubmitSegment`. Both V3 send sites in the slot-based block builder
    populate it with `Vec::new()` for now; `handle_resubmit_segment` destructures
    and ignores it with a TODO for the eventual `SubmitSegment` hydration wiring.
    
    Signed-off-by: Iulian Barbu <iulian.barbu@parity.io>

--- cumulus/client/consensus/aura/src/collators/slot_based/block_builder_task.rs
+++ cumulus/client/consensus/aura/src/collators/slot_based/block_builder_task.rs
@@ -606,6 +606,7 @@ where
 								let _ = resubmit_sender.unbounded_send(CollatorResubmitSegment {
 									scheduling_proof: proof,
 									kind: SegmentKind::ResubmitOnly { core_index: this_core_index },
+									unincluded_segment: Vec::new(),
 								});
 							}
 						}
@@ -996,6 +997,7 @@ where
 						core_index,
 					},
 				},
+				unincluded_segment: Vec::new(),
 			})
 			.is_ok()
 	} else {
