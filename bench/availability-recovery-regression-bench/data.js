46136d9c0724779218dddc3ad9d08c9899e5f430
github-action-benchmark <github@users.noreply.github.com>
2026-06-15 14:23:53 +0000

    add availability-recovery-regression-bench (customSmallerIsBetter) benchmark result for 81e6d5ac17544a9b11a177e5e16c8ca5c3887a6f

--- bench/availability-recovery-regression-bench/data.js
+++ bench/availability-recovery-regression-bench/data.js
@@ -1,52 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1781300943445,
+  "lastUpdate": 1781533432779,
   "repoUrl": "https://github.com/paritytech/polkadot-sdk",
   "entries": {
     "availability-recovery-regression-bench": [
-      {
-        "commit": {
-          "author": {
-            "email": "49718502+alexggh@users.noreply.github.com",
-            "name": "Alexandru Gheorghe",
-            "username": "alexggh"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": false,
-          "id": "74ac323a85458e8d88e8be98bd24cfb9adad835a",
-          "message": "pallet-revive: add interface to implement mocks and pranks (#9909)\n\nNeeded for: https://github.com/paritytech/foundry-polkadot/pull/334.\n\nIn foundry-polkadot we need the ability to be able to manipulate the\n`msg.sender` and the `tx.origin` that a solidity contract sees cheatcode\ndocumentation, plus the ability to mock calls and functions.\n\nCurrently all create/call methods use the `bare_instantiate`/`bare_call`\nto run things in pallet-revive, the caller then normally gets set\nautomatically, based on what is the call stack, but for `forge test` we\nneed to be able to manipulate, so that we can set it to custom values.\n\nAdditionally, for delegate_call, bare_call is used, so there is no way\nto specify we are dealing with a delegate call, so the call is not\nworking correcly.\n \nFor both this paths, we need a way to inject this information into the\nexecution environment, hence I added an optional hooks interface that we\nimplement from foundry cheatcodes for prank and mock functionality.\n\n## TODO\n- [x] Add tests to make sure the hooks functionality does not regress.\n\n---------\n\nSigned-off-by: Alexandru Gheorghe <alexandru.gheorghe@parity.io>\nCo-authored-by: cmd[bot] <41898282+github-actions[bot]@users.noreply.github.com>",
-          "timestamp": "2025-10-28T13:19:37Z",
-          "tree_id": "9d9e73ab388e6d3b9b0c2b0ab988c017e3e0665b",
-          "url": "https://github.com/paritytech/polkadot-sdk/commit/74ac323a85458e8d88e8be98bd24cfb9adad835a"
-        },
-        "date": 1761661571519,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Sent to peers",
-            "value": 1.6666666666666665,
-            "unit": "KiB"
-          },
-          {
-            "name": "Received from peers",
-            "value": 307203,
-            "unit": "KiB"
-          },
-          {
-            "name": "availability-recovery",
-            "value": 11.642184602500002,
-            "unit": "seconds"
-          },
-          {
-            "name": "test-environment",
-            "value": 0.20282772856666664,
-            "unit": "seconds"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -21999,6 +21955,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "test-environment",
             "value": 0.13560670083333332,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "tsvetomir@parity.io",
+            "name": "Tsvetomir Dimitrov",
+            "username": "tdimitrov"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "81e6d5ac17544a9b11a177e5e16c8ca5c3887a6f",
+          "message": "Logging improvements for the collator revamp (#12282)\n\nSome logging updates:\n\n- Decrease log levels in `wait_for_first_leaf` to DEBUG, to avoid\nstartup spam\n- Use `warn_if_frequent` for fetch errors\n- Log assignment changes on view change\n- pick_best_advertisement: trace logs for each outcome\n- update_view: log scheduling parent <-> assigned core mapping\n- update_view: log sp removal\n- handle_seconded_collation: logs for each error case\n- PeerManager: log reputation updates\n\nPartially addresses\nhttps://github.com/paritytech/polkadot-sdk/issues/10402\n\n---------\n\nCo-authored-by: cmd[bot] <41898282+github-actions[bot]@users.noreply.github.com>",
+          "timestamp": "2026-06-15T12:53:09Z",
+          "tree_id": "556214ed9b85c112876cb7a4238f6be37b5641fb",
+          "url": "https://github.com/paritytech/polkadot-sdk/commit/81e6d5ac17544a9b11a177e5e16c8ca5c3887a6f"
+        },
+        "date": 1781533406730,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Received from peers",
+            "value": 307203,
+            "unit": "KiB"
+          },
+          {
+            "name": "Sent to peers",
+            "value": 1.6666666666666665,
+            "unit": "KiB"
+          },
+          {
+            "name": "test-environment",
+            "value": 0.1407040865666667,
+            "unit": "seconds"
+          },
+          {
+            "name": "availability-recovery",
+            "value": 11.681101861433334,
             "unit": "seconds"
           }
         ]
