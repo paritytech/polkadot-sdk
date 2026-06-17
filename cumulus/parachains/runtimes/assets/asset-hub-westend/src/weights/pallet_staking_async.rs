28ed33b7636f545ff29b04a6253fdedefb753231
mertwole <mertwole@gmail.com>
2026-06-17 14:32:59 +0200

    Add chill_inactive weight to AHW

--- cumulus/parachains/runtimes/assets/asset-hub-westend/src/weights/pallet_staking_async.rs
+++ cumulus/parachains/runtimes/assets/asset-hub-westend/src/weights/pallet_staking_async.rs
@@ -50,6 +50,9 @@ use core::marker::PhantomData;
 /// Weight functions for `pallet_staking_async`.
 pub struct WeightInfo<T>(PhantomData<T>);
 impl<T: frame_system::Config> pallet_staking_async::WeightInfo for WeightInfo<T> {
+	fn chill_inactive(_v: u32, ) -> Weight {
+		Weight::zero()
+	}
 	/// Storage: `Staking::Bonded` (r:1 w:1)
 	/// Proof: `Staking::Bonded` (`max_values`: None, `max_size`: Some(72), added: 2547, mode: `MaxEncodedLen`)
 	/// Storage: `Staking::Ledger` (r:1 w:1)
