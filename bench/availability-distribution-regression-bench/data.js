aaa3947fdff8af15a20cc93827109193cc7155ac
github-action-benchmark <github@users.noreply.github.com>
2026-06-23 09:56:38 +0000

    add availability-distribution-regression-bench (customSmallerIsBetter) benchmark result for 782fb2c3925a86b3e30b2e9c7f73c9fc4836c183

--- bench/availability-distribution-regression-bench/data.js
+++ bench/availability-distribution-regression-bench/data.js
@@ -1,62 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1782200959637,
+  "lastUpdate": 1782208598464,
   "repoUrl": "https://github.com/paritytech/polkadot-sdk",
   "entries": {
     "availability-distribution-regression-bench": [
-      {
-        "commit": {
-          "author": {
-            "email": "skunert49@gmail.com",
-            "name": "Sebastian Kunert",
-            "username": "skunert"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": false,
-          "id": "69f210b33fce91b23570f3bda64f8e3deff04843",
-          "message": "RawIter: stop iterating on incomplete DB only with `stop_on_incomplete_database`  (#10155)\n\nOn RawIter there is an argument `stop_on_incomplete_database`. In\ncontrast to what it is saying, the Iterator was always stopping on\nincomplete DB.\n\n---------\n\nCo-authored-by: cmd[bot] <41898282+github-actions[bot]@users.noreply.github.com>",
-          "timestamp": "2025-10-30T19:18:17Z",
-          "tree_id": "75255a00186882577559ec9e361e913084f476b5",
-          "url": "https://github.com/paritytech/polkadot-sdk/commit/69f210b33fce91b23570f3bda64f8e3deff04843"
-        },
-        "date": 1761857088439,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "Received from peers",
-            "value": 433.3333333333332,
-            "unit": "KiB"
-          },
-          {
-            "name": "Sent to peers",
-            "value": 18481.666666666653,
-            "unit": "KiB"
-          },
-          {
-            "name": "availability-distribution",
-            "value": 0.013020520999999998,
-            "unit": "seconds"
-          },
-          {
-            "name": "bitfield-distribution",
-            "value": 0.022547311706666663,
-            "unit": "seconds"
-          },
-          {
-            "name": "test-environment",
-            "value": 0.007331542359999984,
-            "unit": "seconds"
-          },
-          {
-            "name": "availability-store",
-            "value": 0.15992226191333328,
-            "unit": "seconds"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -26999,6 +26945,60 @@ window.BENCHMARK_DATA = {
           {
             "name": "availability-store",
             "value": 0.14512326885333338,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "60601340+lexnv@users.noreply.github.com",
+            "name": "Alexandru Vasile",
+            "username": "lexnv"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "782fb2c3925a86b3e30b2e9c7f73c9fc4836c183",
+          "message": "net/litep2p: Install the default crypto provider (#11253)\n\nThis PR installs the default crypto provider before TLS connections are\nmade.\n\nIn case another provider was already installed (unexpected but\nparachains dev might have enabled it), a warning is printed.\n\nWithout the fix, litep2p websocket (tokio tungstenite that uses rustls)\nwould panic under the hood:\n\n```rust\nThread 'tokio-runtime-worker' panicked at '\nCould not automatically determine the process-level CryptoProvider from Rustls crate features.\nCall CryptoProvider::install_default() before this point to select a provider manually, or make sure exactly one of the 'aws-lc-rs' and 'ring' features is enabled.\nSee the documentation of the CryptoProvider type for more information.\n            ', /home/akru/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rustls-0.23.36/src/crypto/mod.rs:249\n```\n\nThis happens speficially when both `aws-lc-rs` (legacy) and `ring`\nfeatures are enabled by some dependencies that get into the\n`Cargo.lock`.\n\nCloses:\n- https://github.com/paritytech/polkadot-sdk/issues/11164\n\n---------\n\nSigned-off-by: Alexandru Vasile <alexandru.vasile@parity.io>\nCo-authored-by: cmd[bot] <41898282+github-actions[bot]@users.noreply.github.com>",
+          "timestamp": "2026-06-23T08:21:22Z",
+          "tree_id": "e459f7f5c7c641b09bdd99d8e1789da44f963225",
+          "url": "https://github.com/paritytech/polkadot-sdk/commit/782fb2c3925a86b3e30b2e9c7f73c9fc4836c183"
+        },
+        "date": 1782208570836,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Sent to peers",
+            "value": 18481.666666666653,
+            "unit": "KiB"
+          },
+          {
+            "name": "Received from peers",
+            "value": 433.3333333333332,
+            "unit": "KiB"
+          },
+          {
+            "name": "availability-distribution",
+            "value": 0.008184529093333334,
+            "unit": "seconds"
+          },
+          {
+            "name": "bitfield-distribution",
+            "value": 0.02389295358,
+            "unit": "seconds"
+          },
+          {
+            "name": "availability-store",
+            "value": 0.14618367224666665,
+            "unit": "seconds"
+          },
+          {
+            "name": "test-environment",
+            "value": 0.010089329966666642,
             "unit": "seconds"
           }
         ]
