be5e6acc26a7413076f38b9c1e1492a81d607c37
Andrei Eres <eresav@me.com>
2026-06-23 08:28:17 +0200

    statement-store: use shared test_helpers::peer in peer_steering tests

--- substrate/client/network/statement/src/v2dht/peer_steering.rs
+++ substrate/client/network/statement/src/v2dht/peer_steering.rs
@@ -141,13 +141,7 @@ impl PeerSteering {
 #[cfg(test)]
 mod tests {
 	use super::*;
-
-	fn peer(seed: u8) -> PeerId {
-		let mut bytes = [seed; 34];
-		bytes[0] = 0;
-		bytes[1] = 32;
-		PeerId::from_bytes(&bytes).expect("identity multihash peer id")
-	}
+	use crate::test_helpers::peer;
 
 	#[test]
 	fn substream_events_maintain_the_connected_set() {
