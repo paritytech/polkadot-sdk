d05d3f4bb00399401f7ea15ac711494512f3e48b
Serban Iorga <serban300@gmail.com>
2026-06-24 13:53:58 +0300

    add comment

--- polkadot/zombienet-sdk-tests/tests/functional/scheduling_v3.rs
+++ polkadot/zombienet-sdk-tests/tests/functional/scheduling_v3.rs
@@ -27,6 +27,8 @@ use crate::utils::{assert_candidates_version, assert_validator_backed_candidates
 /// - a V2 parachain with async backing
 /// - a V3 parachain with async backing
 /// and checks that the candidates for both parachains are being backed at expected throughput.
+///
+/// RPO = relay parent offset
 #[rstest]
 #[case::rpo_0_max_session_age_0("v3", 0, 20, 16..21)]
 #[case::rpo_2_max_session_age_0("v3-rpo-2", 0, 20, 8..13)]
