6f236c791f0f9848f907b11f20c08b4882371f58
Robert van Eerdewijk <robertvaneerdewijk@gmail.com>
2026-07-14 15:59:51 +0200

    format

--- substrate/frame/revive/src/tests/pvm.rs
+++ substrate/frame/revive/src/tests/pvm.rs
@@ -75,7 +75,9 @@ fn eth_call_transfer_with_dust_works() {
 		let Contract { addr, .. } =
 			builder::bare_instantiate(Code::Upload(binary)).build_and_unwrap_contract();
 
-		<Test as Config>::FeeInfo::deposit_txfee(<Test as Config>::Currency::issue(100_000_000_000));
+		<Test as Config>::FeeInfo::deposit_txfee(<Test as Config>::Currency::issue(
+			100_000_000_000,
+		));
 
 		let balance =
 			Pallet::<Test>::convert_native_to_evm(BalanceWithDust::new_unchecked::<Test>(100, 10));
