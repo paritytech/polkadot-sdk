a089f971315b9bad7e8fe2ad0de8048c00e54da5
Marios Christou <marios@parity.io>
2026-07-16 16:52:37 +0300

    More v3 candidates zombienet-sdk tests

--- polkadot/zombienet-sdk-tests/tests/elastic_scaling/doesnt_break_parachains.rs
+++ polkadot/zombienet-sdk-tests/tests/elastic_scaling/doesnt_break_parachains.rs
@@ -9,21 +9,61 @@ use codec::Decode;
 use cumulus_zombienet_sdk_helpers::{
 	assert_finality_lag, assert_para_throughput, assign_cores, wait_for_pvf_prepare,
 };
-use polkadot_primitives::{CoreIndex, Id as ParaId};
+use polkadot_primitives::{CandidateDescriptorVersion, CoreIndex, Id as ParaId};
+use rstest::rstest;
 use serde_json::json;
-use std::collections::{BTreeMap, VecDeque};
+use std::collections::{BTreeMap, HashMap, VecDeque};
 use zombienet_sdk::{
 	subxt::{OnlineClient, PolkadotConfig},
 	NetworkConfigBuilder,
 };
 
+#[rstest]
+#[case::v2("test-parachain", None, false)]
+#[case::v3("test-parachain", Some("v3"), true)]
 #[tokio::test(flavor = "multi_thread")]
-async fn doesnt_break_parachains_test() -> Result<(), anyhow::Error> {
+async fn doesnt_break_parachains_test(
+	#[case] collator_command: &str,
+	#[case] collator_chain: Option<&str>,
+	#[case] use_v3: bool,
+) -> Result<(), anyhow::Error> {
 	let _ = env_logger::try_init_from_env(
 		env_logger::Env::default().filter_or(env_logger::DEFAULT_FILTER_ENV, "info"),
 	);
 
 	let images = zombienet_sdk::environment::get_images_from_env();
+
+	// V3 case additionally sets node-features bits 3+4 so the collator emits V3 descriptors.
+	let genesis_overrides = if use_v3 {
+		json!({
+			"configuration": {
+				"config": {
+					"scheduler_params": {
+						"num_cores": 1,
+						"max_validators_per_core": 2,
+					},
+					"node_features": {"bits": 8, "data": [0b00011000]}
+				}
+			}
+		})
+	} else {
+		json!({
+			"configuration": {
+				"config": {
+					"scheduler_params": {
+						"num_cores": 1,
+						"max_validators_per_core": 2,
+					}
+				}
+			}
+		})
+	};
+
+	// `--authoring=slot-based` only for the V3 case.
+	let mut collator_args = vec![("-lparachain=debug,aura=debug").into()];
+	if use_v3 {
+		collator_args.push("--authoring=slot-based".into());
+	}
 
 	let config = NetworkConfigBuilder::new()
 		.with_relaychain(|r| {
@@ -32,16 +72,7 @@ async fn doesnt_break_parachains_test() -> Result<(), anyhow::Error> {
 				.with_default_command("polkadot")
 				.with_default_image(images.polkadot.as_str())
 				.with_default_args(vec![("-lparachain=debug").into()])
-				.with_genesis_overrides(json!({
-					"configuration": {
-						"config": {
-							"scheduler_params": {
-								"num_cores": 1,
-								"max_validators_per_core": 2,
-							}
-						}
-					}
-				}))
+				.with_genesis_overrides(genesis_overrides)
 				// Have to set a `with_validator` outside of the loop below, so that `r` has the
 				// right type.
 				.with_validator(|node| node.with_name("validator-0"));
@@ -51,12 +82,16 @@ async fn doesnt_break_parachains_test() -> Result<(), anyhow::Error> {
 			})
 		})
 		.with_parachain(|p| {
-			// Use default, which has 6 second slot time. Also, don't use slot-based collator.
-			p.with_id(2000)
-				.with_default_command("polkadot-parachain")
+			let p = p
+				.with_id(2000)
+				.with_default_command(collator_command)
 				.with_default_image(images.cumulus.as_str())
-				.with_default_args(vec![("-lparachain=debug,aura=debug").into()])
-				.with_collator(|n| n.with_name("collator-2000"))
+				.with_default_args(collator_args);
+			let p = match collator_chain {
+				Some(chain) => p.with_chain(chain),
+				None => p,
+			};
+			p.with_collator(|n| n.with_name("collator-2000"))
 		})
 		.build()
 		.map_err(|e| {
@@ -77,9 +112,21 @@ async fn doesnt_break_parachains_test() -> Result<(), anyhow::Error> {
 	let para_id = ParaId::from(2000);
 	// Wait for PVF preparation to complete.
 	wait_for_pvf_prepare(&network, 1).await?;
-	// Expect the parachain to be making normal progress, 1 candidate backed per relay chain block.
-	// Lowering to 12 to make sure CI passes.
-	assert_para_throughput(&relay_client, 15, [(para_id, 12..16)], []).await?;
+
+	if use_v3 {
+		// V3 candidates at single-core throughput.
+		crate::utils::assert_candidates_version(
+			&relay_client,
+			CandidateDescriptorVersion::V3,
+			HashMap::from([(para_id, 12..16)]),
+			15,
+		)
+		.await?;
+	} else {
+		// Expect the parachain to be making normal progress, 1 candidate backed per relay chain
+		// block. Lowering to 12 to make sure CI passes.
+		assert_para_throughput(&relay_client, 15, [(para_id, 12..16)], []).await?;
+	}
 
 	let para_client = para_node.wait_client().await?;
 	// Assert the parachain finalized block height is also on par with the number of backed
@@ -97,7 +144,7 @@ async fn doesnt_break_parachains_test() -> Result<(), anyhow::Error> {
 			.await?[..],
 	)?;
 
-	// Get looakahead config
+	// Get lookahead config
 	let lookahead = u32::decode(
 		&mut &relay_client
 			.runtime_api()
