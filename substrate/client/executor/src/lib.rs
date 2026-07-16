ed20a2af1de403ceeb2263f8002f28cd326fd71b
nasihudeen04 <nasihudeen04@gmail.com>
2026-07-16 14:52:11 +0100

    Remove deprecated NativeElseWasmExecutor.
    Past the end-of-2024 removal date. Use WasmExecutor instead.

--- substrate/client/executor/src/lib.rs
+++ substrate/client/executor/src/lib.rs
@@ -37,9 +37,7 @@ mod integration_tests;
 mod wasm_runtime;
 
 pub use codec::Codec;
-#[allow(deprecated)]
-pub use executor::NativeElseWasmExecutor;
-pub use executor::{with_externalities_safe, NativeExecutionDispatch, WasmExecutor};
+pub use executor::{with_externalities_safe, WasmExecutor};
 #[doc(hidden)]
 pub use sp_core::traits::Externalities;
 pub use sp_version::{NativeVersion, RuntimeVersion};
