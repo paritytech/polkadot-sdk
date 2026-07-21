1e0b467ecafc3ca58e504ac4390735ffff3f6bfc
Alistair Singh <alistair.singh7@gmail.com>
2026-07-21 23:43:15 +0200

    remove log and verbose comment

--- bridges/snowbridge/pallets/inbound-queue/src/lib.rs
+++ bridges/snowbridge/pallets/inbound-queue/src/lib.rs
@@ -296,19 +296,8 @@ pub mod pallet {
 			)
 			.min(owed);
 			if !amount.is_zero() {
-				// Best-effort: a failed reimbursement (e.g. the transfer would dust either account
-				// below the existential deposit) must never revert message processing, otherwise
-				// the nonce would roll back and stall the channel. The relayer simply goes
-				// unrewarded for this message.
-				if let Err(error) =
-					T::Token::transfer(&sovereign_account, &who, amount, Preservation::Preserve)
-				{
-					tracing::error!(
-						target: LOG_TARGET,
-						?error,
-						"Failed to reimburse relayer from sovereign account"
-					);
-				}
+				// Best effort attempt to reward relayer if there are funds available.
+				let _ = T::Token::transfer(&sovereign_account, &who, amount, Preservation::Preserve);
 			}
 
 			// Attempt to send XCM to a dest parachain
