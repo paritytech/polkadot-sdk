1719eeeba8025bed661a978247c48d316a169762
Serban Iorga <serban300@gmail.com>
2026-08-17 16:54:31 +0300

    Remove collation_req_v1_receiver

--- polkadot/node/service/src/builder/mod.rs
+++ polkadot/node/service/src/builder/mod.rs
@@ -347,9 +347,6 @@ where
 
 		let req_protocol_names = ReqProtocolNames::new(&genesis_hash, config.chain_spec.fork_id());
 
-		let (collation_req_v1_receiver, cfg) =
-			IncomingRequest::get_config_receiver::<_, Network>(&req_protocol_names);
-		net_config.add_request_response_protocol(cfg);
 		let (collation_req_v2_receiver, cfg) =
 			IncomingRequest::get_config_receiver::<_, Network>(&req_protocol_names);
 		net_config.add_request_response_protocol(cfg);
@@ -632,7 +629,6 @@ where
 						network_service: network.clone(),
 						sync_service: sync_service.clone(),
 						authority_discovery_service,
-						collation_req_v1_receiver,
 						collation_req_v2_receiver,
 						available_data_req_receiver,
 						registry: prometheus_registry.as_ref(),
