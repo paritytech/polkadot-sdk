933a19fe409115584d431b129e47c82780c1bb4a
Robert van Eerdewijk <robertvaneerdewijk@gmail.com>
2026-09-26 16:19:53 +0200

    resolve prestate tracer code via Pallet::code

--- substrate/frame/revive/src/evm/tracing/prestate_tracing.rs
+++ substrate/frame/revive/src/evm/tracing/prestate_tracing.rs
@@ -15,7 +15,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 use crate::{
-	AccountInfo, Code, Config, ExecReturnValue, Key, Pallet, PristineCode, Weight,
+	Code, Config, ExecReturnValue, Key, Pallet, Weight,
 	evm::{Bytes, PrestateTrace, PrestateTraceInfo, PrestateTracerConfig},
 	tracing::Tracing,
 };
@@ -182,19 +182,8 @@ where
 {
 	/// Get the code of the contract.
 	fn bytecode(address: &H160) -> Option<Bytes> {
-		use crate::precompiles::{All, Precompiles};
-		// Precompiles: report the code stub, matching eth_getCode / EXTCODEHASH. An empty
-		// stub reports as no code, like eth_getCode's `0x`.
-		if let Some(code) = <All<T>>::code(address.as_fixed_bytes()) {
-			return (!code.is_empty()).then(|| code.to_vec().into());
-		}
-		// EIP-7702: report the delegation indicator, matching eth_getCode / EXTCODEHASH.
-		if let Some(target) = AccountInfo::<T>::get_delegation_target(address) {
-			return Some(AccountInfo::<T>::delegation_indicator(&target).to_vec().into());
-		}
-		let code_hash = AccountInfo::<T>::load_contract(address)?.code_hash;
-		let code: Vec<u8> = PristineCode::<T>::get(&code_hash)?.into();
-		return Some(code.into());
+		let code = Pallet::<T>::code(address);
+		(!code.is_empty()).then(|| code.into())
 	}
 
 	/// Update the prestate info for the given address.
